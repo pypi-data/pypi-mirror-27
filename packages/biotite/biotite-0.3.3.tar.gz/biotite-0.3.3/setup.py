35ceb3fae8f4e36799a2e7ec5837ec4734a5b1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:45 +0000

    Add package biotite/0.3.3/biotite-0.3.3.tar.gz

---
+++ packages/biotite/biotite-0.3.3.tar.gz/biotite-0.3.3/setup.py
@@ -0,0 +1,118 @@
+# Copyright 2017 Patrick Kunzmann.
+# This source code is part of the Biotite package and is distributed under the
+# 3-Clause BSD License.  Please see 'LICENSE.rst' for further information.
+
+from setuptools import setup, find_packages, Extension
+from setuptools.command.test import test as TestCommand
+import sys
+import shlex
+from os.path import join, abspath, dirname
+from src.biotite import __version__
+
+release = __version__
+
+long_description = """
+The Biotite package bundles popular tools in computational biology into an
+unifying framework. It offers file I/O operations, analyses and manipulations
+for biological sequence and structure data. Furthermore, the package provides
+interfaces for popular biological databases and external software.
+
+The internal structure and sequence representations are based on *NumPy*
+`ndarrays`, taking the advantage of C-accelerated operations. Time consuming
+operations that could not be vectorised are mostly implemented in *Cython* in
+order to achieve C-accelerations in those places, too.
+
+Additionally the package aims for simple usability and extensibility: The
+objects representing structures and sequences can be indexed and scliced like
+an `ndarray`. Even the actual internal `ndarrays` are easily accessible
+allowing advanced users to implement their own algorithms upon the existing
+types.
+"""
+
+
+if "sdist" in sys.argv:
+    # Source distributions do not have extension modules
+    # and therefore are using not C-accelerated functions
+    # Simple 'install' command uses source distribution
+    ext_modules = None
+else:
+    from Cython.Build import cythonize
+    try:
+        import numpy
+        ext_modules = cythonize(
+            [Extension("biotite.sequence.align.calign",
+                ["src/biotite/sequence/align/calign.pyx"],
+                include_dirs=[numpy.get_include()]
+             ),
+             Extension("biotite.structure.io.pdbx.cprocessloop",
+                ["src/biotite/structure/io/pdbx/cprocessloop.pyx"]
+             ),
+             Extension("biotite.cextensions",
+                ["src/biotite/cextensions.pyx"]
+             )]
+        )
+    except ValueError:
+        # In case of installing a source distribution,
+        # the *.pyx files cannot be found
+        ext_modules = None
+
+
+class PyTestCommand(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        super().initialize_options()
+        self.pytest_args = ''
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+setup(
+    name="biotite",
+    version = release,
+    description = "A general framework for computational biology",
+    long_description = long_description,
+    author = "The Biotite contributors",
+    url = "https://github.com/biotite-dev/biotite",
+    license = "BSD 3-Clause",
+    classifiers = (
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ),
+    
+    zip_safe = False,
+    packages = find_packages("src"),
+    package_dir = {"" : "src"},
+    
+    ext_modules = ext_modules,
+    
+    # Including substitution matrix data
+    package_data = {"biotite.sequence.align" : ["matrix_data/*.mat"]},
+    
+    install_requires = ["requests",
+                        "numpy",
+                        "matplotlib"],
+    extras_require = {'trajectory':  ["mdtraj"]},
+    python_requires = ">=3.6",
+    
+    cmdclass = {"test": PyTestCommand},
+    tests_require = ["pytest"],
+    
+    command_options = {
+        'build_sphinx':
+            {"source_dir" : ("setup.py", "./doc"),
+             "build_dir"  : ("setup.py", "./doc/_build"),
+             "release"    : ("setup.py", __version__)}
+    }
+)
