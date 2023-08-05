703f41588f6a074164bda99004d2fd5558edbdbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:02 +0000

    Add package httpie-asap-auth/0.0.2/httpie-asap-auth-0.0.2.tar.gz

---
+++ packages/httpie-asap-auth/httpie-asap-auth-0.0.2.tar.gz/httpie-asap-auth-0.0.2/httpie_asap_auth.py
@@ -0,0 +1,94 @@
+"""
+ASAP Auth plugin for HTTPie.
+"""
+from __future__ import print_function
+
+import json
+import sys
+
+from collections import namedtuple
+import atlassian_jwt_auth
+
+from httpie import ExitStatus
+from httpie.plugins import AuthPlugin
+
+
+__version__ = '0.0.2'
+__author__ = 'Jason Friedland'
+__licence__ = 'MIT'
+
+
+class AsapAuth:
+    """
+    Implements ASAP Auth.
+    """
+    AsapConfig = namedtuple('AsapConfig', ['iss', 'kid', 'aud', 'sub', 'private_key'])
+
+    def __init__(self, asap_config_file):
+        asap_config = self.parse_config(asap_config_file)
+        self.iss = asap_config.iss
+        self.kid = asap_config.kid
+        self.aud = asap_config.aud
+        self.sub = asap_config.sub
+        self.private_key = asap_config.private_key
+
+    def __call__(self, r):
+        kwargs = {}
+        if self.sub is not None:
+            kwargs = {'additional_claims': {'sub': self.sub}}
+        signer = atlassian_jwt_auth.create_signer(self.iss, self.kid, self.private_key)
+        token = signer.generate_jwt(self.aud, **kwargs)
+
+        r.headers['Authorization'] = 'Bearer {}'.format(token.decode('utf-8'))
+
+        return r
+
+    @staticmethod
+    def parse_config(asap_config_file):
+        """
+        Parse ``asap_config_file`` JSON and return the config required
+        to make a valid ASAP/JWT request.
+        """
+        try:
+            with open(asap_config_file) as f:
+                config = json.load(f)
+        except IOError:
+            print('file not found: {}'.format(asap_config_file), file=sys.stderr)
+            sys.exit(ExitStatus.PLUGIN_ERROR)
+        except ValueError:
+            print('invalid JSON config: {}'.format(asap_config_file), file=sys.stderr)
+            sys.exit(ExitStatus.PLUGIN_ERROR)
+
+        try:
+            asap_config = AsapAuth.AsapConfig(
+                iss=config['issuer'], aud=config['audience'], kid=config['kid'], private_key=config['privateKey'],  # Required
+                sub=config.get('sub')  # Optional
+            )
+        except (ValueError, AttributeError, KeyError):
+            print('malformed JSON config: {}'.format(asap_config_file), file=sys.stderr)
+            sys.exit(ExitStatus.PLUGIN_ERROR)
+
+        return asap_config
+
+
+class AsapAuthPlugin(AuthPlugin):
+    """
+    ASAP Auth plugin for HTTPie.
+    """
+    name = 'ASAP Auth'
+    auth_type = 'asap'
+    description = 'See: https://s2sauth.bitbucket.io/spec/ for details.'
+
+    # Require but don't parse auth
+    auth_require = True
+    auth_parse = False
+    prompt_password = False
+
+    def get_auth(self, username=None, password=None):
+        """
+        Get ASAP Auth.
+
+        NB. the --auth, -a option is used as the file path to the ASAP config, and
+            is accessible in self.raw_auth.
+        """
+        return AsapAuth(self.raw_auth)
---
+++ packages/httpie-asap-auth/httpie-asap-auth-0.0.2.tar.gz/httpie-asap-auth-0.0.2/setup.py
@@ -0,0 +1,41 @@
+"""
+setup.py for httpie-asap-auth.
+"""
+from setuptools import setup
+
+
+setup(
+    name='httpie-asap-auth',
+    description='ASAP Auth plugin for HTTPie.',
+    long_description='ASAP Auth plugin for HTTPie.',
+    version='0.0.2',
+    author='Jason Friedland',
+    author_email='jason@friedland.id.au',
+    license='MIT',
+    url='https://github.com/jasonfriedland/httpie-asap-auth',
+    download_url='https://github.com/jasonfriedland/httpie-asap-auth',
+    py_modules=['httpie_asap_auth'],
+    zip_safe=False,
+    entry_points={
+        'httpie.plugins.auth.v1': [
+            'httpie_asap_auth = httpie_asap_auth:AsapAuthPlugin'
+        ]
+    },
+    install_requires=[
+        'httpie>=0.9.9',
+        'atlassian-jwt-auth>=2.10.2'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Environment :: Plugins',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities'
+    ],
+)
