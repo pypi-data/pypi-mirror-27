dbf43aa8f127c8f69bcb98eb2d6af33be6f056f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:48 +0000

    Add package tradingeconomics/0.2.80/tradingeconomics-0.2.80.tar.gz

---
+++ packages/tradingeconomics/tradingeconomics-0.2.80.tar.gz/tradingeconomics-0.2.80/tradingeconomics/glob.py
@@ -0,0 +1,17 @@
+from . import functions as fn
+
+def login(userkey = None):
+    global apikey
+    if userkey == None:
+        apikey = 'guest:guest'
+    else:
+        apikey = userkey
+    if apikey != 'guest:guest':
+        fn.credCheck(apikey)
+    return apikey
+
+
+def subscribe(ev):
+	global _event
+	_event = ev
+	return _event
