a2c269fa3b9571c76339918cdc45ec3038b4bb0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package wenna-pypi/1.0.0/wenna_pypi-1.0.0.tar.gz

---
+++ packages/wenna-pypi/wenna_pypi-1.0.0.tar.gz/wenna_pypi-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+DESCRIPTION = "This package is first release."
+AUTHOR = "Wenna Zhang"
+AUTHOR_EMAIL = "nana_love1@126.com"
+URL = "https://www.baidu.com"
+
+setup(
+    name='wenna_pypi',
+    version='1.0.0',
+    description=DESCRIPTION,
+    # long_description=read("README.md"),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="Apache License, Version 2.0",
+    url=URL,
+)
