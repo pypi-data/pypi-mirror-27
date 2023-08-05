b8219be1ef0a1a7b0798a79e1c4ed909c09cd80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package firanka/0.1.6/firanka-0.1.6.tar.gz

---
+++ packages/firanka/firanka-0.1.6.tar.gz/firanka-0.1.6/firanka/series/__init__.py
@@ -0,0 +1,11 @@
+# coding=UTF-8
+from __future__ import print_function, absolute_import, division
+
+from .series import DiscreteSeries, FunctionSeries, ModuloSeries, Series
+
+__all__ = [
+    'FunctionSeries',
+    'DiscreteSeries',
+    'ModuloSeries',
+    'Series',
+]
