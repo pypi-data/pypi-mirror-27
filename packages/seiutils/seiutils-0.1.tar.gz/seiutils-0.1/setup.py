17f6f677e9adbcc6c6745ca36beb56193fb3d213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:40 +0000

    Add package seiutils/0.1/seiutils-0.1.tar.gz

---
+++ packages/seiutils/seiutils-0.1.tar.gz/seiutils-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'seiutils',
+  packages = ['seiutils'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Utilities for crawling web/twitter',
+  author = 'Michael Yang',
+  author_email = 'michael.kevin@gmail.com',
+  url = 'https://github.com/ahuei123456/Seiyuu-Utils', # use the URL to the github repo
+  download_url = 'https://github.com/ahuei123456/Seiyuu-Utils/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'crawling'], # arbitrary keywords
+  classifiers = [],
+)
