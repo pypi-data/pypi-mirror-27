d42e4ec3e6bdf1923d331014219d8bcb9067f356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:13 +0000

    Add package tjango/0.1.0/tjango-0.1.0.tar.gz

---
+++ packages/tjango/tjango-0.1.0.tar.gz/tjango-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# setup for tjango
+setup(
+    name='tjango',
+    version='0.1.0',
+    keywords='admin',
+    description='A package to develop tornado in django style',
+    license='MIT License',
+    author='Kido Zhang',
+    author_email='kidozh@kidozh.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=['tornado', 'peewee', 'psutil', 'bcrypt'],
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
