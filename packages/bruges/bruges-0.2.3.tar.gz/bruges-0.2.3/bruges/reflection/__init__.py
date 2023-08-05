84d6232f373c8cfe9004681769f76ea272a401e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:26 +0000

    Add package bruges/0.2.3/bruges-0.2.3.tar.gz

---
+++ packages/bruges/bruges-0.2.3.tar.gz/bruges-0.2.3/bruges/reflection/__init__.py
@@ -0,0 +1,20 @@
+from .reflection import zoeppritz, zoeppritz_element
+from .reflection import zoeppritz_rpp
+from .reflection import akirichards, akirichards_alt
+from .reflection import fatti
+from .reflection import shuey2, shuey3
+from .reflection import bortfeld2, bortfeld3
+from .reflection import hilterman
+
+__all__ = ["zoeppritz",
+           "zoeppritz_element",
+           "zoeppritz_rpp",
+           "akirichards",
+           "akirichards_alt",
+           "fatti",
+           "shuey2",
+           "shuey3",
+           "bortfeld2",
+           "bortfeld3",
+           "hilterman",
+           ]
