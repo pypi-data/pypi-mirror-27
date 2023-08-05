46a21c271996a9af1d7ccd1574bc32c08453267e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:53 +0000

    Add package nesterfeng/1.2.0/nesterfeng-1.2.0.tar.gz

---
+++ packages/nesterfeng/nesterfeng-1.2.0.tar.gz/nesterfeng-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "nesterfeng",
+    version = "1.2.0",
+    py_modules = ["nesterfeng"],
+    author = "fengzhiwen",
+    author_email = "fengzhiwen@rd.hgits.cn",
+    description = "a simple print of nested lists",
+    )
