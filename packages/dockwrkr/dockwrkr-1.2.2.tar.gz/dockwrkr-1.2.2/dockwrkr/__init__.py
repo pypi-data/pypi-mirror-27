25049e8f5aa3f841ca52642b6db35b58dd39c95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:20 +0000

    Add package dockwrkr/1.2.2/dockwrkr-1.2.2.tar.gz

---
+++ packages/dockwrkr/dockwrkr-1.2.2.tar.gz/dockwrkr-1.2.2/dockwrkr/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+import sys
+
+from .console import Command
+from .console import Program
+from .console import SubProgram
+from .core import Core
---
+++ packages/dockwrkr/dockwrkr-1.2.2.tar.gz/dockwrkr-1.2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+execfile('dockwrkr/_version.py')
+
+install_requires = ['setuptools', 'PyYAML', 'arrow', 'tabulate']
+
+setup(name='dockwrkr',
+      version=__version__,
+      author='Turbulent inc.',
+      author_email='oss@turbulent.ca',
+      license='Apache License 2.0',
+      long_description=readme,
+      description='dockwrkr - docker container launch wrapper',
+      install_requires=install_requires,
+      test_suite='tests',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'dockwrkr = dockwrkr.cli:cli',
+          ],
+      })
