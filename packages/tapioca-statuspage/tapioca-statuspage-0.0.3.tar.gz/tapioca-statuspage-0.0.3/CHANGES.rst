87a8fc8fb2c4bdc77de38c30ab429d9b7736fb4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:26 +0000

    Add package tapioca-statuspage/0.0.3/tapioca-statuspage-0.0.3.tar.gz

---
+++ packages/tapioca-statuspage/tapioca-statuspage-0.0.3.tar.gz/tapioca-statuspage-0.0.3/CHANGES.rst
@@ -0,0 +1,21 @@
+Changelog
+---------
+
+0.0.3
+~~~~~
+
+* Add authorization token formating
+
+0.0.2
+~~~~~
+
+* Add MANISFEST.in
+
+
+0.0.1
+~~~~~
+
+* Initial release
+* Pages retrieval endpoint
+* Components retrieval endpoint
+* incidents retrieval endpoint
---
+++ packages/tapioca-statuspage/tapioca-statuspage-0.0.3.tar.gz/tapioca-statuspage-0.0.3/tapioca_statuspage/tapioca_statuspage.py
@@ -0,0 +1,29 @@
+from tapioca import (
+    TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
+
+
+from .resource_mapping import RESOURCE_MAPPING
+
+
+class StatuspageClientAdapter(JSONAdapterMixin, TapiocaAdapter):
+    api_root = 'https://api.statuspage.io/v1/'
+    resource_mapping = RESOURCE_MAPPING
+
+    def get_request_kwargs(self, api_params, *args, **kwargs):
+        params = super(StatuspageClientAdapter, self).get_request_kwargs(
+            api_params, *args, **kwargs)
+
+        params['headers']['Authorization'] = 'OAuth {}'.format(api_params.get('token', ''))
+        params['timeout'] = api_params.get('timeout', 5)
+
+        return params
+
+    def get_iterator_list(self, response_data):
+        return response_data
+
+    def get_iterator_next_request_kwargs(self, iterator_request_kwargs,
+                                         response_data, response):
+        pass
+
+
+Statuspage = generate_wrapper_from_adapter(StatuspageClientAdapter)
