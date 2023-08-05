40668a6f2fc8cf73e1e3fb2f285dfa7ee8038890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package ohashi/0.2.10/ohashi-0.2.10.tar.gz

---
+++ packages/ohashi/ohashi-0.2.10.tar.gz/ohashi-0.2.10/ohashi/db/models/__init__.py
@@ -0,0 +1,5 @@
+from django.db.models import *
+
+from ohashi.db.models.fields import *
+from ohashi.db.models.fields.birthdays import BirthdayField
+from ohashi.db.models.managers.birthdays import BirthdayManager
