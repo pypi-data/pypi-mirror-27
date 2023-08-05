4dd54762b02ae9f723297dcf49ee8d44e0871b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package pydeps/1.3.8/pydeps-1.3.8.tar.gz

---
+++ packages/pydeps/pydeps-1.3.8.tar.gz/pydeps-1.3.8/setup.py
@@ -0,0 +1,34 @@
+# pragma: nocover
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='pydeps',
+    version='1.3.8',
+    packages=['pydeps'],
+    install_requires=[
+        'enum34',
+        'stdlib_list',
+    ],
+    long_description=open('README.rst').read(),
+    entry_points={
+        'console_scripts': [
+            'pydeps = pydeps.pydeps:pydeps',
+        ]
+    },
+    url='https://github.com/thebjorn/pydeps',
+    license='BSD',
+    author='bjorn',
+    author_email='bp@datakortet.no',
+    description='Display module dependencies',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
