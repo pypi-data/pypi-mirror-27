348405c2bdc3e3e8adbe5cca9adbed68ad620afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:52 +0000

    Add package candis/0.0.3/candis-0.0.3-py2.py3-none-any.whl

---
+++ packages/candis/candis-0.0.3-py2.py3-none-any.whl/candis/__init__.py
@@ -0,0 +1,14 @@
+# module - candis
+from candis.config       import Config, get_config, CONFIG
+from candis.manager      import Cache
+from candis.ios.cdata    import CData
+from candis.ios.pipeline import Pipeline
+from candis.ios          import cdata, pipeline
+# candis.cli
+from candis.cli          import main
+
+cache       = Cache()
+cache.create()
+
+config      = cache.get_config()
+CONFIG.update(config)
