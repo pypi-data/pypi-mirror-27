da00af0d6534d0ad04aaef88c582fef21cadd656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:15 +0000

    Add package PyDSWS/0.04/PyDSWS-0.04.tar.gz

---
+++ packages/PyDSWS/PyDSWS-0.04.tar.gz/PyDSWS-0.04/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='PyDSWS',
+    version='0.04',
+    packages=['PyDSWS'],
+    url='https://github.com/hoenie-ams/PyDSWS',
+    license='MIT',
+    author='Joris Hoendervangers',
+    author_email='j.h.hoendervangers@gmail.com',
+    description='Python package for Datastream Webservices',
+    long_description='Python package for Datastream Webservices',
+    install_requires=[
+        'pandas',
+        'requests',
+        'urllib',
+        'datetime',
+        'json'
+      ],
+    python_requires='>=3'
+
+)
