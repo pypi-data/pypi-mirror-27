2ed7b756f742f4e3e9c9395320f9779bfc2c4e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:36 +0000

    Add package dxl-dxpy/0.7.2/dxl-dxpy-0.7.2.tar.gz

---
+++ packages/dxl-dxpy/dxl-dxpy-0.7.2.tar.gz/dxl-dxpy-0.7.2/dxpy/learn/utils/general.py
@@ -0,0 +1,16 @@
+
+def device_name(device_type, device_id=None):
+    if device_id is None:
+        device_id = 0
+    return '/{t}:{i}'.format(t=device_type, i=device_id)
+
+
+def refined_tensor_or_graph_name(tensor_or_graph):
+    import tensorflow as tf
+    if isinstance(tensor_or_graph, tf.Tensor):
+        return tensor_or_graph.name.replace(':', '_')
+    return tensor_or_graph.name
+
+def pre_work():
+    from dxpy.learn.scalar import create_global_scalars
+    create_global_scalars()
