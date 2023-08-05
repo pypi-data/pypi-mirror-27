04520c96e2a6010a82ad414cb631f000baeab50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package currency-cloud/1.1.0/currency_cloud-1.1.0.tar.gz

---
+++ packages/currency-cloud/currency_cloud-1.1.0.tar.gz/currency_cloud-1.1.0/src/currencycloud/resources/reference.py
@@ -0,0 +1,17 @@
+from currencycloud.resources.resource import Resource
+
+
+class Currency(Resource):
+    pass
+
+
+class ConversionDates(Resource):
+    pass
+
+
+class SettlementAccount(Resource):
+    pass
+
+
+class BeneficiaryRequiredDetails(Resource):
+    pass
