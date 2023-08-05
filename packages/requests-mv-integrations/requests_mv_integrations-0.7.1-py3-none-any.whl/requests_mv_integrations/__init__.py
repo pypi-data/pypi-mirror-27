ff9999662cfefb1cb7f0483e866728bdd7424e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:23 +0000

    Add package requests-mv-integrations/0.7.1/requests_mv_integrations-0.7.1-py3-none-any.whl

---
+++ packages/requests-mv-integrations/requests_mv_integrations-0.7.1-py3-none-any.whl/requests_mv_integrations/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  @copyright 2017 TUNE, Inc. (http://www.tune.com)
+
+__title__ = 'requests-mv-integrations'
+__version__ = '0.7.1'
+__build__ = 0x000701
+__version_info__ = tuple(__version__.split('.'))
+
+__author__ = 'jefft@tune.com'
+__license__ = 'MIT License'
+
+__python_required_version__ = (3, 0)
+
+from requests_mv_integrations.support.tune_request import (TuneRequest)
+
+from .request_mv_integration import (RequestMvIntegration)
+from .request_mv_integration_download import (RequestMvIntegrationDownload)
+from .request_mv_integration_upload import (RequestMvIntegrationUpload)
