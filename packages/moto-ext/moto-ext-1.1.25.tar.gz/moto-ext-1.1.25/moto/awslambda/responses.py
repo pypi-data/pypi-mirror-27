8469d67b25bf63fddd345c3251c3a01a06f39978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:18 +0000

    Add package moto-ext/1.1.25/moto-ext-1.1.25.tar.gz

---
+++ packages/moto-ext/moto-ext-1.1.25.tar.gz/moto-ext-1.1.25/moto/awslambda/responses.py
@@ -0,0 +1,216 @@
+from __future__ import unicode_literals
+
+import json
+import re
+
+try:
+    from urllib import unquote
+    from urlparse import urlparse, parse_qs
+except:
+    from urllib.parse import unquote, urlparse, parse_qs
+
+from moto.core.utils import amz_crc32, amzn_request_id
+from moto.core.responses import BaseResponse
+
+
+class LambdaResponse(BaseResponse):
+
+    def root(self, request, full_url, headers):
+        self.setup_class(request, full_url, headers)
+        if request.method == 'GET':
+            return self._list_functions(request, full_url, headers)
+        elif request.method == 'POST':
+            return self._create_function(request, full_url, headers)
+        else:
+            raise ValueError("Cannot handle request")
+
+    def function(self, request, full_url, headers):
+        self.setup_class(request, full_url, headers)
+        if request.method == 'GET':
+            return self._get_function(request, full_url, headers)
+        elif request.method == 'DELETE':
+            return self._delete_function(request, full_url, headers)
+        else:
+            raise ValueError("Cannot handle request")
+
+    @amz_crc32
+    @amzn_request_id
+    def invoke(self, request, full_url, headers):
+        self.setup_class(request, full_url, headers)
+        if request.method == 'POST':
+            return self._invoke(request, full_url)
+        else:
+            raise ValueError("Cannot handle request")
+
+    @amz_crc32
+    @amzn_request_id
+    def invoke_async(self, request, full_url, headers):
+        self.setup_class(request, full_url, headers)
+        if request.method == 'POST':
+            return self._invoke_async(request, full_url)
+        else:
+            raise ValueError("Cannot handle request")
+
+    def tag(self, request, full_url, headers):
+        self.setup_class(request, full_url, headers)
+        if request.method == 'GET':
+            return self._list_tags(request, full_url)
+        elif request.method == 'POST':
+            return self._tag_resource(request, full_url)
+        elif request.method == 'DELETE':
+            return self._untag_resource(request, full_url)
+        else:
+            raise ValueError("Cannot handle {0} request".format(request.method))
+
+    def policy(self, request, full_url, headers):
+        if request.method == 'GET':
+            return self._get_policy(request, full_url, headers)
+        if request.method == 'POST':
+            return self._add_policy(request, full_url, headers)
+
+    def _add_policy(self, request, full_url, headers):
+        lambda_backend = self.get_lambda_backend(full_url)
+
+        path = request.path if hasattr(request, 'path') else request.path_url
+        function_name = path.split('/')[-2]
+        if lambda_backend.has_function(function_name):
+            policy = request.body.decode('utf8')
+            lambda_backend.add_policy(function_name, policy)
+            return 200, {}, json.dumps(dict(Statement=policy))
+        else:
+            return 404, {}, "{}"
+
+    def _get_policy(self, request, full_url, headers):
+        lambda_backend = self.get_lambda_backend(full_url)
+
+        path = request.path if hasattr(request, 'path') else request.path_url
+        function_name = path.split('/')[-2]
+        if lambda_backend.has_function(function_name):
+            function = lambda_backend.get_function(function_name)
+            return 200, {}, json.dumps(dict(Policy="{\"Statement\":[" + function.policy + "]}"))
+        else:
+            return 404, {}, "{}"
+
+    def _invoke(self, request, full_url):
+        response_headers = {}
+        lambda_backend = self.get_lambda_backend(full_url)
+
+        path = request.path if hasattr(request, 'path') else request.path_url
+        function_name = path.split('/')[-2]
+
+        if lambda_backend.has_function(function_name):
+            fn = lambda_backend.get_function(function_name)
+            payload = fn.invoke(self.body, self.headers, response_headers)
+            response_headers['Content-Length'] = str(len(payload))
+            return 202, response_headers, payload
+        else:
+            return 404, response_headers, "{}"
+
+    def _invoke_async(self, request, full_url):
+        response_headers = {}
+        lambda_backend = self.get_lambda_backend(full_url)
+
+        path = request.path if hasattr(request, 'path') else request.path_url
+        function_name = path.split('/')[-3]
+        if lambda_backend.has_function(function_name):
+            fn = lambda_backend.get_function(function_name)
+            fn.invoke(self.body, self.headers, response_headers)
+            response_headers['Content-Length'] = str(0)
+            return 202, response_headers, ""
+        else:
+            return 404, response_headers, "{}"
+
+    def _list_functions(self, request, full_url, headers):
+        lambda_backend = self.get_lambda_backend(full_url)
+        return 200, {}, json.dumps({
+            "Functions": [fn.get_configuration() for fn in lambda_backend.list_functions()],
+            # "NextMarker": str(uuid.uuid4()),
+        })
+
+    def _create_function(self, request, full_url, headers):
+        lambda_backend = self.get_lambda_backend(full_url)
+        spec = json.loads(self.body)
+        try:
+            fn = lambda_backend.create_function(spec)
+        except ValueError as e:
+            return 400, {}, json.dumps({"Error": {"Code": e.args[0], "Message": e.args[1]}})
+        else:
+            config = fn.get_configuration()
+            return 201, {}, json.dumps(config)
+
+    def _delete_function(self, request, full_url, headers):
+        lambda_backend = self.get_lambda_backend(full_url)
+
+        path = request.path if hasattr(request, 'path') else request.path_url
+        function_name = path.split('/')[-1]
+
+        if lambda_backend.has_function(function_name):
+            lambda_backend.delete_function(function_name)
+            return 204, {}, ""
+        else:
+            return 404, {}, "{}"
+
+    def _get_function(self, request, full_url, headers):
+        lambda_backend = self.get_lambda_backend(full_url)
+
+        path = request.path if hasattr(request, 'path') else request.path_url
+        function_name = path.split('/')[-1]
+
+        if lambda_backend.has_function(function_name):
+            fn = lambda_backend.get_function(function_name)
+            code = fn.get_code()
+            return 200, {}, json.dumps(code)
+        else:
+            return 404, {}, "{}"
+
+    def get_lambda_backend(self, full_url):
+        from moto.awslambda.models import lambda_backends
+        region = self._get_aws_region(full_url)
+        return lambda_backends[region]
+
+    def _get_aws_region(self, full_url):
+        region = re.search(self.region_regex, full_url)
+        if region:
+            return region.group(1)
+        else:
+            return self.default_region
+
+    def _list_tags(self, request, full_url):
+        lambda_backend = self.get_lambda_backend(full_url)
+
+        path = request.path if hasattr(request, 'path') else request.path_url
+        function_arn = unquote(path.split('/')[-1])
+
+        if lambda_backend.has_function_arn(function_arn):
+            function = lambda_backend.get_function_by_arn(function_arn)
+            return 200, {}, json.dumps(dict(Tags=function.tags))
+        else:
+            return 404, {}, "{}"
+
+    def _tag_resource(self, request, full_url):
+        lambda_backend = self.get_lambda_backend(full_url)
+
+        path = request.path if hasattr(request, 'path') else request.path_url
+        function_arn = unquote(path.split('/')[-1])
+
+        spec = json.loads(self.body)
+
+        if lambda_backend.has_function_arn(function_arn):
+            lambda_backend.tag_resource(function_arn, spec['Tags'])
+            return 200, {}, "{}"
+        else:
+            return 404, {}, "{}"
+
+    def _untag_resource(self, request, full_url):
+        lambda_backend = self.get_lambda_backend(full_url)
+
+        path = request.path if hasattr(request, 'path') else request.path_url
+        function_arn = unquote(path.split('/')[-1].split('?')[0])
+
+        tag_keys = parse_qs(urlparse(full_url).query)['tagKeys']
+
+        if lambda_backend.has_function_arn(function_arn):
+            lambda_backend.untag_resource(function_arn, tag_keys)
+            return 204, {}, "{}"
+        else:
+            return 404, {}, "{}"
---
+++ packages/moto-ext/moto-ext-1.1.25.tar.gz/moto-ext-1.1.25/moto/cloudwatch/responses.py
@@ -0,0 +1,364 @@
+import json
+from moto.core.utils import amzn_request_id
+from moto.core.responses import BaseResponse
+from .models import cloudwatch_backends
+
+
+class CloudWatchResponse(BaseResponse):
+
+    @property
+    def cloudwatch_backend(self):
+        return cloudwatch_backends[self.region]
+
+    def _error(self, code, message, status=400):
+        template = self.response_template(ERROR_RESPONSE_TEMPLATE)
+        return template.render(code=code, message=message), dict(status=status)
+
+    @amzn_request_id
+    def put_metric_alarm(self):
+        name = self._get_param('AlarmName')
+        namespace = self._get_param('Namespace')
+        metric_name = self._get_param('MetricName')
+        comparison_operator = self._get_param('ComparisonOperator')
+        evaluation_periods = self._get_param('EvaluationPeriods')
+        period = self._get_param('Period')
+        threshold = self._get_param('Threshold')
+        statistic = self._get_param('Statistic')
+        description = self._get_param('AlarmDescription')
+        dimensions = self._get_list_prefix('Dimensions.member')
+        alarm_actions = self._get_multi_param('AlarmActions.member')
+        ok_actions = self._get_multi_param('OKActions.member')
+        insufficient_data_actions = self._get_multi_param(
+            "InsufficientDataActions.member")
+        unit = self._get_param('Unit')
+        alarm = self.cloudwatch_backend.put_metric_alarm(name, namespace, metric_name,
+                                                         comparison_operator,
+                                                         evaluation_periods, period,
+                                                         threshold, statistic,
+                                                         description, dimensions,
+                                                         alarm_actions, ok_actions,
+                                                         insufficient_data_actions,
+                                                         unit)
+        template = self.response_template(PUT_METRIC_ALARM_TEMPLATE)
+        return template.render(alarm=alarm)
+
+    @amzn_request_id
+    def describe_alarms(self):
+        action_prefix = self._get_param('ActionPrefix')
+        alarm_name_prefix = self._get_param('AlarmNamePrefix')
+        alarm_names = self._get_multi_param('AlarmNames.member')
+        state_value = self._get_param('StateValue')
+
+        if action_prefix:
+            alarms = self.cloudwatch_backend.get_alarms_by_action_prefix(
+                action_prefix)
+        elif alarm_name_prefix:
+            alarms = self.cloudwatch_backend.get_alarms_by_alarm_name_prefix(
+                alarm_name_prefix)
+        elif alarm_names:
+            alarms = self.cloudwatch_backend.get_alarms_by_alarm_names(alarm_names)
+        elif state_value:
+            alarms = self.cloudwatch_backend.get_alarms_by_state_value(state_value)
+        else:
+            alarms = self.cloudwatch_backend.get_all_alarms()
+
+        template = self.response_template(DESCRIBE_ALARMS_TEMPLATE)
+        return template.render(alarms=alarms)
+
+    @amzn_request_id
+    def delete_alarms(self):
+        alarm_names = self._get_multi_param('AlarmNames.member')
+        self.cloudwatch_backend.delete_alarms(alarm_names)
+        template = self.response_template(DELETE_METRIC_ALARMS_TEMPLATE)
+        return template.render()
+
+    @amzn_request_id
+    def put_metric_data(self):
+        namespace = self._get_param('Namespace')
+        metric_data = []
+        metric_index = 1
+        while True:
+            try:
+                metric_name = self.querystring[
+                    'MetricData.member.{0}.MetricName'.format(metric_index)][0]
+            except KeyError:
+                break
+            value = self.querystring.get(
+                'MetricData.member.{0}.Value'.format(metric_index), [None])[0]
+            dimensions = []
+            dimension_index = 1
+            while True:
+                try:
+                    dimension_name = self.querystring[
+                        'MetricData.member.{0}.Dimensions.member.{1}.Name'.format(metric_index, dimension_index)][0]
+                except KeyError:
+                    break
+                dimension_value = self.querystring[
+                    'MetricData.member.{0}.Dimensions.member.{1}.Value'.format(metric_index, dimension_index)][0]
+                dimensions.append(
+                    {'name': dimension_name, 'value': dimension_value})
+                dimension_index += 1
+            metric_data.append([metric_name, value, dimensions])
+            metric_index += 1
+        self.cloudwatch_backend.put_metric_data(namespace, metric_data)
+        template = self.response_template(PUT_METRIC_DATA_TEMPLATE)
+        return template.render()
+
+    @amzn_request_id
+    def list_metrics(self):
+        metrics = self.cloudwatch_backend.get_all_metrics()
+        template = self.response_template(LIST_METRICS_TEMPLATE)
+        return template.render(metrics=metrics)
+
+    @amzn_request_id
+    def delete_dashboards(self):
+        dashboards = self._get_multi_param('DashboardNames.member')
+        if dashboards is None:
+            return self._error('InvalidParameterValue', 'Need at least 1 dashboard')
+
+        status, error = self.cloudwatch_backend.delete_dashboards(dashboards)
+        if not status:
+            return self._error('ResourceNotFound', error)
+
+        template = self.response_template(DELETE_DASHBOARD_TEMPLATE)
+        return template.render()
+
+    @amzn_request_id
+    def describe_alarm_history(self):
+        raise NotImplementedError()
+
+    @amzn_request_id
+    def describe_alarms_for_metric(self):
+        raise NotImplementedError()
+
+    @amzn_request_id
+    def disable_alarm_actions(self):
+        raise NotImplementedError()
+
+    @amzn_request_id
+    def enable_alarm_actions(self):
+        raise NotImplementedError()
+
+    @amzn_request_id
+    def get_dashboard(self):
+        dashboard_name = self._get_param('DashboardName')
+
+        dashboard = self.cloudwatch_backend.get_dashboard(dashboard_name)
+        if dashboard is None:
+            return self._error('ResourceNotFound', 'Dashboard does not exist')
+
+        template = self.response_template(GET_DASHBOARD_TEMPLATE)
+        return template.render(dashboard=dashboard)
+
+    @amzn_request_id
+    def get_metric_statistics(self):
+        raise NotImplementedError()
+
+    @amzn_request_id
+    def list_dashboards(self):
+        prefix = self._get_param('DashboardNamePrefix', '')
+
+        dashboards = self.cloudwatch_backend.list_dashboards(prefix)
+
+        template = self.response_template(LIST_DASHBOARD_RESPONSE)
+        return template.render(dashboards=dashboards)
+
+    @amzn_request_id
+    def put_dashboard(self):
+        name = self._get_param('DashboardName')
+        body = self._get_param('DashboardBody')
+
+        try:
+            json.loads(body)
+        except ValueError:
+            return self._error('InvalidParameterInput', 'Body is invalid JSON')
+
+        self.cloudwatch_backend.put_dashboard(name, body)
+
+        template = self.response_template(PUT_DASHBOARD_RESPONSE)
+        return template.render()
+
+    @amzn_request_id
+    def set_alarm_state(self):
+        alarm_name = self._get_param('AlarmName')
+        reason = self._get_param('StateReason')
+        reason_data = self._get_param('StateReasonData')
+        state_value = self._get_param('StateValue')
+
+        self.cloudwatch_backend.set_alarm_state(alarm_name, reason, reason_data, state_value)
+
+        template = self.response_template(SET_ALARM_STATE_TEMPLATE)
+        return template.render()
+
+    # TODO (whummer): temporary hack until get_metric_statistics is available in moto
+    def get_metric_values(self):
+        import json
+        cloudwatch_backend = cloudwatch_backends[self.region]
+        metrics = [{
+            'Namespace': m.namespace,
+            'Name': m.name,
+            'Value': m.value,
+            'Dimensions': [{'Name': d.name, 'Value': d.value} for d in m.dimensions]}
+            for m in cloudwatch_backend.get_all_metrics()]
+        return json.dumps(metrics)
+
+
+PUT_METRIC_ALARM_TEMPLATE = """<PutMetricAlarmResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+   <ResponseMetadata>
+      <RequestId>
+         {{ request_id }}
+      </RequestId>
+   </ResponseMetadata>
+</PutMetricAlarmResponse>"""
+
+DESCRIBE_ALARMS_TEMPLATE = """<DescribeAlarmsResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+    <DescribeAlarmsResult>
+        <MetricAlarms>
+            {% for alarm in alarms %}
+            <member>
+                <ActionsEnabled>{{ alarm.actions_enabled }}</ActionsEnabled>
+                <AlarmActions>
+                    {% for action in alarm.alarm_actions %}
+                    <member>{{ action }}</member>
+                    {% endfor %}
+                </AlarmActions>
+                <AlarmArn>{{ alarm.arn }}</AlarmArn>
+                <AlarmConfigurationUpdatedTimestamp>{{ alarm.configuration_updated_timestamp }}</AlarmConfigurationUpdatedTimestamp>
+                <AlarmDescription>{{ alarm.description }}</AlarmDescription>
+                <AlarmName>{{ alarm.name }}</AlarmName>
+                <ComparisonOperator>{{ alarm.comparison_operator }}</ComparisonOperator>
+                <Dimensions>
+                    {% for dimension in alarm.dimensions %}
+                    <member>
+                        <Name>{{ dimension.name }}</Name>
+                        <Value>{{ dimension.value }}</Value>
+                    </member>
+                    {% endfor %}
+                </Dimensions>
+                <EvaluationPeriods>{{ alarm.evaluation_periods }}</EvaluationPeriods>
+                <InsufficientDataActions>
+                    {% for action in alarm.insufficient_data_actions %}
+                    <member>{{ action }}</member>
+                    {% endfor %}
+                </InsufficientDataActions>
+                <MetricName>{{ alarm.metric_name }}</MetricName>
+                <Namespace>{{ alarm.namespace }}</Namespace>
+                <OKActions>
+                    {% for action in alarm.ok_actions %}
+                    <member>{{ action }}</member>
+                    {% endfor %}
+                </OKActions>
+                <Period>{{ alarm.period }}</Period>
+                <StateReason>{{ alarm.state_reason }}</StateReason>
+                <StateReasonData>{{ alarm.state_reason_data }}</StateReasonData>
+                <StateUpdatedTimestamp>{{ alarm.state_updated_timestamp }}</StateUpdatedTimestamp>
+                <StateValue>{{ alarm.state_value }}</StateValue>
+                <Statistic>{{ alarm.statistic }}</Statistic>
+                <Threshold>{{ alarm.threshold }}</Threshold>
+                <Unit>{{ alarm.unit }}</Unit>
+            </member>
+            {% endfor %}
+        </MetricAlarms>
+    </DescribeAlarmsResult>
+</DescribeAlarmsResponse>"""
+
+DELETE_METRIC_ALARMS_TEMPLATE = """<DeleteMetricAlarmResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+   <ResponseMetadata>
+      <RequestId>
+         {{ request_id }}
+      </RequestId>
+   </ResponseMetadata>
+</DeleteMetricAlarmResponse>"""
+
+PUT_METRIC_DATA_TEMPLATE = """<PutMetricDataResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+   <ResponseMetadata>
+      <RequestId>
+         {{ request_id }}
+      </RequestId>
+   </ResponseMetadata>
+</PutMetricDataResponse>"""
+
+LIST_METRICS_TEMPLATE = """<ListMetricsResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+    <ListMetricsResult>
+        <Metrics>
+            {% for metric in metrics %}
+            <member>
+                <Dimensions>
+                    {% for dimension in metric.dimensions %}
+                    <member>
+                        <Name>{{ dimension.name }}</Name>
+                        <Value>{{ dimension.value }}</Value>
+                    </member>
+                    {% endfor %}
+                </Dimensions>
+                <MetricName>{{ metric.name }}</MetricName>
+                <Namespace>{{ metric.namespace }}</Namespace>
+            </member>
+            {% endfor %}
+        </Metrics>
+        <NextToken>
+            96e88479-4662-450b-8a13-239ded6ce9fe
+        </NextToken>
+    </ListMetricsResult>
+</ListMetricsResponse>"""
+
+PUT_DASHBOARD_RESPONSE = """<PutDashboardResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+  <PutDashboardResult>
+    <DashboardValidationMessages/>
+  </PutDashboardResult>
+  <ResponseMetadata>
+    <RequestId>{{ request_id }}</RequestId>
+  </ResponseMetadata>
+</PutDashboardResponse>"""
+
+LIST_DASHBOARD_RESPONSE = """<ListDashboardsResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+  <ListDashboardsResult>
+    <DashboardEntries>
+      {% for dashboard in dashboards %}
+      <member>
+        <DashboardArn>{{ dashboard.arn }}</DashboardArn>
+        <LastModified>{{ dashboard.last_modified_iso }}</LastModified>
+        <Size>{{ dashboard.size }}</Size>
+        <DashboardName>{{ dashboard.name }}</DashboardName>
+      </member>
+      {% endfor %}
+    </DashboardEntries>
+  </ListDashboardsResult>
+  <ResponseMetadata>
+    <RequestId>{{ request_id }}</RequestId>
+  </ResponseMetadata>
+</ListDashboardsResponse>"""
+
+DELETE_DASHBOARD_TEMPLATE = """<DeleteDashboardsResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+  <DeleteDashboardsResult/>
+  <ResponseMetadata>
+    <RequestId>{{ request_id }}</RequestId>
+  </ResponseMetadata>
+</DeleteDashboardsResponse>"""
+
+GET_DASHBOARD_TEMPLATE = """<GetDashboardResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+  <GetDashboardResult>
+    <DashboardArn>{{ dashboard.arn }}</DashboardArn>
+    <DashboardBody>{{ dashboard.body }}</DashboardBody>
+    <DashboardName>{{ dashboard.name }}</DashboardName>
+  </GetDashboardResult>
+  <ResponseMetadata>
+    <RequestId>{{ request_id }}</RequestId>
+  </ResponseMetadata>
+</GetDashboardResponse>
+"""
+
+SET_ALARM_STATE_TEMPLATE = """<SetAlarmStateResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+  <ResponseMetadata>
+    <RequestId>{{ request_id }}</RequestId>
+  </ResponseMetadata>
+</SetAlarmStateResponse>"""
+
+ERROR_RESPONSE_TEMPLATE = """<ErrorResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+  <Error>
+    <Type>Sender</Type>
+    <Code>{{ code }}</Code>
+    <Message>{{ message }}</Message>
+  </Error>
+  <RequestId>{{ request_id }}</RequestId>
+</ErrorResponse>"""
