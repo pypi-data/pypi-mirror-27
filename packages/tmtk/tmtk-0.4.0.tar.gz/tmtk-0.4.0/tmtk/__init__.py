76bfc9eb139ebdd0f694e6aaa1cc4320b9468e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:12 +0000

    Add package tmtk/0.4.0/tmtk-0.4.0.tar.gz

---
+++ packages/tmtk/tmtk-0.4.0.tar.gz/tmtk-0.4.0/tmtk/__init__.py
@@ -0,0 +1,11 @@
+"""tmtk - A toolkit for ETL curation for the tranSMART data warehouse."""
+from .study import Study
+from .clinical import Clinical
+from .toolbox import wizard
+
+__version__ = "0.4.0"
+__author__ = 'Jochem Bijlard <j.bijlard@gmail.com>'
+__all__ = []
+
+if __name__ == '__main__':
+    print('This is not meant to run directly.')
