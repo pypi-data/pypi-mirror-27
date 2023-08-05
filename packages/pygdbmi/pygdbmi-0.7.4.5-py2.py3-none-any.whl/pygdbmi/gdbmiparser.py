7f4f3e98e51941f9c3daa0c61c7055295b994f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package pygdbmi/0.7.4.5/pygdbmi-0.7.4.5-py2.py3-none-any.whl

---
+++ packages/pygdbmi/pygdbmi-0.7.4.5-py2.py3-none-any.whl/pygdbmi/gdbmiparser.py
@@ -0,0 +1,393 @@
+"""
+Python parser for gdb's machine interface interpreter.
+
+Parses string output from gdb with the "--interpreter=mi2" flag into
+structured objects.
+
+See more at https://sourceware.org/gdb/onlinedocs/gdb/GDB_002fMI.html#GDB_002fMI
+
+"""
+
+import re
+from pygdbmi.printcolor import print_cyan, print_red, print_green
+from pprint import pprint
+
+# Print text to console as it's being parsed to help debug
+_DEBUG = False
+
+
+def parse_response(gdb_mi_text):
+    """Parse gdb mi text and turn it into a dictionary.
+
+    See https://sourceware.org/gdb/onlinedocs/gdb/GDB_002fMI-Stream-Records.html#GDB_002fMI-Stream-Records
+    for details on types of gdb mi output.
+
+    Args:
+        gdb_mi_text (str): String output from gdb
+
+    Returns:
+        dict with the following keys:
+        type (either 'notify', 'result', 'console', 'log', 'target', 'done'),
+        message (str or None),
+        payload (str, list, dict, or None)
+    """
+    if _GDB_MI_NOTIFY_RE.match(gdb_mi_text):
+        token, message, payload = _get_notify_msg_and_payload(gdb_mi_text)
+        return {'type': 'notify',
+                'message': message,
+                'payload': payload,
+                'token': token}
+
+    elif _GDB_MI_RESULT_RE.match(gdb_mi_text):
+        token, message, payload = _get_result_msg_and_paylod(gdb_mi_text)
+        return {'type': 'result',
+                'message': message,
+                'payload': payload,
+                'token': token}
+
+    elif _GDB_MI_CONSOLE_RE.match(gdb_mi_text):
+        return {'type': 'console',
+                'message': None,
+                'payload': _GDB_MI_CONSOLE_RE.match(gdb_mi_text).groups()[0]}
+
+    elif _GDB_MI_LOG_RE.match(gdb_mi_text):
+        return {'type': 'log',
+                'message': None,
+                'payload': _GDB_MI_LOG_RE.match(gdb_mi_text).groups()[0]}
+
+    elif _GDB_MI_TARGET_OUTPUT_RE.match(gdb_mi_text):
+        return {'type': 'target',
+                'message': None,
+                'payload': _GDB_MI_TARGET_OUTPUT_RE.match(gdb_mi_text).groups()[0]}
+
+    elif response_is_finished(gdb_mi_text):
+        return {'type': 'done',
+                'message': None,
+                'payload': None}
+
+    else:
+        # This was not gdb mi output, so it must have just been printed by
+        # the inferior program that's being debugged
+        return {'type': 'output',
+                'message': None,
+                'payload': gdb_mi_text}
+
+
+def response_is_finished(gdb_mi_text):
+    """Return true if the gdb mi response is ending
+    Returns: True if gdb response is finished"""
+    if _GDB_MI_RESPONSE_FINISHED_RE.match(gdb_mi_text):
+        return True
+    else:
+        return False
+
+
+def assert_match(actual_char_or_str, expected_char_or_str):
+    """If values don't match, print them and raise a ValueError, otherwise,
+    continue
+    Raises: ValueError if argumetns do not match"""
+    if expected_char_or_str != actual_char_or_str:
+        print('Expected')
+        pprint(expected_char_or_str)
+        print('')
+        print('Got')
+        pprint(actual_char_or_str)
+        raise ValueError()
+
+
+# ========================================================================
+# All functions and variables below are used internally to parse mi output
+# ========================================================================
+
+
+# GDB machine interface output patterns to match
+# https://sourceware.org/gdb/onlinedocs/gdb/GDB_002fMI-Stream-Records.html#GDB_002fMI-Stream-Records
+
+# https://sourceware.org/gdb/onlinedocs/gdb/GDB_002fMI-Result-Records.html#GDB_002fMI-Result-Records
+# In addition to a number of out-of-band notifications,
+# the response to a gdb/mi command includes one of the following result indications:
+# done, running, connected, error, exit
+_GDB_MI_RESULT_RE = re.compile('^(\d*)\^(\S+?)(,(.*))?$')
+
+# https://sourceware.org/gdb/onlinedocs/gdb/GDB_002fMI-Async-Records.html#GDB_002fMI-Async-Records
+# Async records are used to notify the gdb/mi client of additional
+# changes that have occurred. Those changes can either be a consequence
+# of gdb/mi commands (e.g., a breakpoint modified) or a result of target activity
+# (e.g., target stopped).
+_GDB_MI_NOTIFY_RE = re.compile('^(\d*)[*=](\S+?),(.*)$')
+
+# https://sourceware.org/gdb/onlinedocs/gdb/GDB_002fMI-Stream-Records.html#GDB_002fMI-Stream-Records
+# "~" string-output
+# The console output stream contains text that should be displayed
+# in the CLI console window. It contains the textual responses to CLI commands.
+_GDB_MI_CONSOLE_RE = re.compile('~"(.*)"', re.DOTALL)
+
+# https://sourceware.org/gdb/onlinedocs/gdb/GDB_002fMI-Stream-Records.html#GDB_002fMI-Stream-Records
+# "&" string-output
+# The log stream contains debugging messages being produced by gdb's internals.
+_GDB_MI_LOG_RE = re.compile('&"(.*)"', re.DOTALL)
+
+# https://sourceware.org/gdb/onlinedocs/gdb/GDB_002fMI-Stream-Records.html#GDB_002fMI-Stream-Records
+# "@" string-output
+# The target output stream contains any textual output from the
+# running target. This is only present when GDB's event loop is truly asynchronous,
+# which is currently only the case for remote targets.
+_GDB_MI_TARGET_OUTPUT_RE = re.compile('@"(.*)"', re.DOTALL)
+
+# Response finished
+_GDB_MI_RESPONSE_FINISHED_RE = re.compile('^\(gdb\)\s*$')
+
+_WHITESPACE = [' ', '\t', '\r', '\n']
+
+_GDB_MI_CHAR_DICT_START = '{'
+_GDB_MI_CHAR_ARRAY_START = '['
+_GDB_MI_CHAR_STRING_START = '"'
+_GDB_MI_VALUE_START_CHARS = [_GDB_MI_CHAR_DICT_START, _GDB_MI_CHAR_ARRAY_START, _GDB_MI_CHAR_STRING_START]
+
+
+def _get_notify_msg_and_payload(result):
+    """Get notify message and payload dict"""
+    groups = _GDB_MI_NOTIFY_RE.match(result).groups()
+    token = int(groups[0]) if groups[0] != '' else None
+    message = groups[1].strip()
+    result_of_status = groups[2].strip()
+    _, payload = _parse_dict(result_of_status)
+    return token, message, payload
+
+
+def _get_result_msg_and_paylod(result):
+    """Get result message and payload dict"""
+    groups = _GDB_MI_RESULT_RE.match(result).groups()
+    token = int(groups[0]) if groups[0] != '' else None
+    message = groups[1]
+    if groups[2] is None:
+        payload = None
+    else:
+        str_to_parse = groups[2].strip()
+        i, payload = _parse_dict(str_to_parse)
+    return token, message, payload
+
+
+def _parse_dict(to_parse):
+    """Parse dictionary, with optional starting character '{'
+    return (tuple):
+        Number of characters parsed from to_parse
+        Parsed dictionary
+    """
+    if _DEBUG:
+        print_red('obj')
+        print_red(to_parse)
+    i = 0
+    obj = {}
+    while i < len(to_parse):
+        c = to_parse[i]
+        if c in _WHITESPACE:
+            pass
+        elif c in ['{', ',']:
+            pass
+        elif c == '}':
+            # end of object, exit loop
+            break
+        else:
+            chars_used, key, val = _parse_key_val(to_parse[i:])
+            i = i + chars_used
+            if key in obj:
+                # This is a gdb bug. We should never get repeated keys in a dict!
+                # See https://sourceware.org/bugzilla/show_bug.cgi?id=22217
+                # and https://github.com/cs01/pygdbmi/issues/19
+                # Example:
+                #   thread-ids={thread-id="1",thread-id="2"}
+                # Results in:
+                #   thread-ids: {{'thread-id': ['1', '2']}}
+                # Rather than the lossy
+                #   thread-ids: {'thread-id': 2}  # '1' got overwritten!
+                if isinstance(obj[key], list):
+                    obj[key].append(val)
+                else:
+                    obj[key] = [obj[key], val]
+            else:
+                obj[key] = val
+        i += 1
+    if _DEBUG:
+        print_green(obj)
+    return i, obj
+
+
+def _parse_key_val(to_parse):
+    """Parse key, value combination
+    return (tuple):
+        Number of characters parsed from to_parse
+        Parsed key (string)
+        Parsed value (either a string, array, or dict)
+    """
+    if _DEBUG:
+        print_red('keyval')
+        print_red(to_parse)
+
+    i = 0
+    size, key = _parse_key(to_parse[i:])
+    i += size
+    size, val = _parse_val(to_parse[i:])
+    i += size
+    if _DEBUG:
+        print_green(key)
+        print_green(val)
+    return i, key, val
+
+
+def _parse_key(to_parse):
+    """Parse key, value combination
+    return (tuple):
+        Number of characters parsed from to_parse
+        Parsed key (string)
+    """
+    if _DEBUG:
+        print_red('key')
+        print_red(to_parse)
+
+    buf = ''
+    i = 0
+    key = ''
+    while i < len(to_parse) - 1:
+        c = to_parse[i]
+        if c == '=':
+            key = buf
+            # consume '=' sign so caller doesn't get it again
+            i += 1
+            break
+        buf += c
+        i += 1
+    if _DEBUG:
+        print_green(key)
+    return i, key
+
+
+def _parse_val(to_parse):
+    """Parse value from string
+    return (tuple):
+        Number of characters parsed from to_parse
+        Parsed value (either a string, array, or dict)
+    """
+    if _DEBUG:
+        print_red('val')
+        print_red(to_parse)
+
+    i = 0
+    val = ''
+    buf = ''
+    while i < len(to_parse) - 1:
+        c = to_parse[i]
+
+        if c == '{':
+            # Start object
+            size, val = _parse_dict(to_parse[i:])
+            i += size
+            break
+        elif c == '[':
+            # Start of an array
+            size, val = _parse_array(to_parse[i:])
+            i += size
+            break
+        elif c == '"':
+            # Start of a string
+            size, val = _parse_str(to_parse[i:])
+            i += size
+            break
+        else:
+            buf += c
+        i += 1
+
+    if _DEBUG:
+        print_green(val)
+    return i, val
+
+
+def _parse_array(to_parse):
+    """Parse an array
+    return (tuple):
+        Number of characters parsed from to_parse
+        Parsed array
+    """
+    if _DEBUG:
+        print_red('array')
+        print_red(to_parse)
+
+    assert_match(_GDB_MI_CHAR_ARRAY_START, to_parse[0])
+
+    # Skip first open bracket so we don't end up in an
+    # endless loop trying to re-parse the array
+    i = 1
+
+    arr = []
+    while i < len(to_parse) - 1:
+        c = to_parse[i]
+
+        if c in _GDB_MI_VALUE_START_CHARS:
+            size, val = _parse_val(to_parse[i:])
+            arr.append(val)
+            i += size
+        elif c in _WHITESPACE:
+            pass
+        elif c == ',':
+            pass
+        elif c == ']':
+            # Stop when this array has finished. Note
+            # that elements of this array can be also be arrays.
+            break
+        i += 1
+    if _DEBUG:
+        print_green(arr)
+    return i, arr
+
+
+def _parse_str(to_parse):
+    """Parse a string
+    return (tuple):
+        Number of characters parsed from to_parse
+        Parsed string, without surrounding quotes
+    """
+
+    # characters that gdb escapes that should not be
+    # escaped by this parser
+    CHARS_TO_REMOVE_GDB_ESCAPE = ['"']
+
+    if _DEBUG:
+        print_red('string')
+        print_red(to_parse)
+
+    assert_match(_GDB_MI_CHAR_STRING_START, to_parse[0])
+    i = 1  # Skip the opening quote
+    buf = ''
+    while i < len(to_parse) - 1:
+        c = to_parse[i]
+        if _DEBUG:
+            print_cyan(c)
+
+        if c == '\\' and i < (len(to_parse) - 1):
+            # We are on a backslash and there is another character after the backslash
+            # to parse. Handle this case specially since gdb escaped it for us
+
+            # Get the next char that is being escaped
+            c2 = to_parse[i + 1]
+            if c2 in CHARS_TO_REMOVE_GDB_ESCAPE:
+                # only store the escaped character in the buffer; don't store the backslash
+                # (don't leave it escaped)
+                buf += to_parse[i + 1]
+            else:
+                # store the backslash and the following character in the buffer (leave it escaped)
+                buf += c + to_parse[i + 1]
+
+            # consume the backslash and the next character
+            i += 2
+
+        else:
+            # Quote is closed. Exit (and don't include the end quote).
+            if c == '"':
+                break
+            buf += c
+            i += 1
+
+    string = buf
+    if _DEBUG:
+        print_green(string)
+    return i, string
---
+++ packages/pygdbmi/pygdbmi-0.7.4.5-py2.py3-none-any.whl/pygdbmi/tests/test_app.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+
+"""
+Unit tests
+
+Run from top level directory: ./tests/test_app.py
+"""
+
+import os
+import random
+import unittest
+import subprocess
+from pygdbmi.gdbmiparser import parse_response, assert_match
+from pygdbmi.gdbcontroller import GdbController, NoGdbProcessError
+
+
+class TestPyGdbMi(unittest.TestCase):
+
+    def test_parser(self):
+        """Test that the parser returns dictionaries from gdb mi strings as expected"""
+
+        # Test basic types
+        assert_match(parse_response('^done'), {'type': 'result', 'payload': None, 'message': 'done', "token": None})
+        assert_match(parse_response('~"done"'), {'type': 'console', 'payload': 'done', 'message': None})
+        assert_match(parse_response('@"done"'), {"type": 'target', "payload": 'done', 'message': None})
+        assert_match(parse_response('&"done"'), {"type": 'log', "payload": 'done', 'message': None})
+        assert_match(parse_response('done'), {'type': 'output', 'payload': 'done', 'message': None})
+
+        # Test escape sequences
+        assert_match(parse_response('~""'), {"type": "console", "payload": "", 'message': None})
+        assert_match(parse_response('~"\b\f\n\r\t\""'), {"type": "console", "payload": '\b\f\n\r\t\"', 'message': None})
+        assert_match(parse_response('@""'), {"type": "target", "payload": "", 'message': None})
+        assert_match(parse_response('@"\b\f\n\r\t\""'), {"type": "target", "payload": '\b\f\n\r\t\"', 'message': None})
+        assert_match(parse_response('&""'), {"type": "log", "payload": "", 'message': None})
+        assert_match(parse_response('&"\b\f\n\r\t\""'), {"type": "log", "payload": '\b\f\n\r\t\"', 'message': None})
+
+        # Test that a dictionary with repeated keys (a gdb bug) is gracefully worked-around  by pygdbmi
+        # See https://sourceware.org/bugzilla/show_bug.cgi?id=22217
+        # and https://github.com/cs01/pygdbmi/issues/19
+        assert_match(parse_response('^done,thread-ids={thread-id="3",thread-id="2",thread-id="1"}, current-thread-id="1",number-of-threads="3"'),
+                                    {"type": "result",
+                                        "payload": {'thread-ids': {'thread-id': ['3', '2', '1']},
+                                                    'current-thread-id': '1',
+                                                    'number-of-threads': '3',
+                                                    },
+                                        'message': 'done',
+                                        'token': None})
+
+        # Test a real world Dictionary
+        assert_match(parse_response('=breakpoint-modified,bkpt={number="1",empty_arr=[],type="breakpoint",disp="keep",enabled="y",addr="0x000000000040059c",func="main",file="hello.c",fullname="/home/git/pygdbmi/tests/sample_c_app/hello.c",line="9",thread-groups=["i1"],times="1",original-location="hello.c:9"}'),
+                                    {'message': 'breakpoint-modified',
+                                    'payload': {'bkpt': {'addr': '0x000000000040059c',
+                                                         'disp': 'keep',
+                                                         'enabled': 'y',
+                                                         'file': 'hello.c',
+                                                         'fullname': '/home/git/pygdbmi/tests/sample_c_app/hello.c',
+                                                         'func': 'main',
+                                                         'line': '9',
+                                                         'number': '1',
+                                                         'empty_arr': [],
+                                                         'original-location': 'hello.c:9',
+                                                         'thread-groups': ['i1'],
+                                                         'times': '1',
+                                                         'type': 'breakpoint'}},
+                                     'type': 'notify',
+                                     'token': None})
+
+        # Test records with token
+        assert_match(parse_response('1342^done'), {'type': 'result', 'payload': None, 'message': 'done', "token": 1342})
+
+    def _get_c_program(self):
+        """build c program and return path to binary"""
+        FILENAME = 'pygdbmiapp.a'
+        SAMPLE_C_CODE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'sample_c_app')
+        SAMPLE_C_BINARY = os.path.join(SAMPLE_C_CODE_DIR, FILENAME)
+        # Build C program
+        subprocess.check_output(["make", "-C", SAMPLE_C_CODE_DIR, '--quiet'])
+        return SAMPLE_C_BINARY
+
+    def test_controller(self):
+        """Build a simple C program, then run it with GdbController and verify the output is parsed
+        as expected"""
+
+        # Initialize object that manages gdb subprocess
+        gdbmi = GdbController()
+
+        c_binary_path = self._get_c_program()
+        # Load the binary and its symbols in the gdb subprocess
+        responses = gdbmi.write('-file-exec-and-symbols %s' % c_binary_path, timeout_sec=1)
+
+        # Verify output was parsed into a list of responses
+        assert(len(responses) != 0)
+        response = responses[0]
+        assert(set(response.keys()) == set(['message', 'type', 'payload', 'stream', 'token']))
+        assert(response['message'] == 'thread-group-added')
+        assert(response['type'] == 'notify')
+        assert(response['payload'] == {'id': 'i1'})
+        assert(response['stream'] == 'stdout')
+        assert(response['token'] is None)
+
+        responses = gdbmi.write(['-file-list-exec-source-files', '-break-insert main'])
+        assert(len(responses) != 0)
+
+        responses = gdbmi.write(['-exec-run', '-exec-continue'], timeout_sec=3)
+        found_match = False
+        for r in responses:
+            if r.get('payload', '') == '  leading spaces should be preserved. So should trailing spaces.  ':
+                found_match = True
+        assert(found_match is True)
+
+        # Close gdb subprocess
+        responses = gdbmi.exit()
+        assert(responses is None)
+        assert(gdbmi.gdb_process is None)
+
+        # Test NoGdbProcessError exception
+        got_no_process_exception = False
+        try:
+            responses = gdbmi.write('-file-exec-and-symbols %s' % c_binary_path)
+        except NoGdbProcessError:
+            got_no_process_exception = True
+        assert(got_no_process_exception is True)
+
+    def test_controller_buffer(self):
+        """test that a partial response gets successfully buffered
+        by the controller, then fully read when more data arrives"""
+        gdbmi = GdbController()
+        to_be_buffered = b'^done,BreakpointTable={nr_rows="1",nr_'
+
+        stream = 'teststream'
+        verbose = False
+        response = gdbmi._get_responses_list(to_be_buffered, stream, verbose)
+        # Nothing should have been parsed yet
+        assert(len(response) == 0)
+        assert(gdbmi._incomplete_output[stream] == to_be_buffered)
+
+        remaining_gdb_output = b'cols="6"}\n(gdb) \n'
+        response = gdbmi._get_responses_list(remaining_gdb_output, stream, verbose)
+
+        # Should have parsed response at this point
+        assert(len(response) == 1)
+        r = response[0]
+        assert(r['stream'] == 'teststream')
+        assert(r['type'] == 'result')
+        assert(r['payload'] == {'BreakpointTable': {'nr_cols': '6', 'nr_rows': '1'}})
+
+    def test_controller_buffer_randomized(self):
+        """
+        The following code reads a sample gdb mi stream randomly to ensure partial
+        output is read and that the buffer is working as expected on all streams.
+        """
+        test_directory = os.path.dirname(os.path.abspath(__file__))
+        datafile_path = '%s/response_samples.txt' % (test_directory)
+
+        gdbmi = GdbController()
+        for stream in gdbmi._incomplete_output.keys():
+            responses = []
+            with open(datafile_path, 'rb') as f:
+                while(True):
+                    n = random.randint(1, 100)
+                    # read random number of bytes to simulate incomplete responses
+                    gdb_mi_simulated_output = f.read(n)
+                    if gdb_mi_simulated_output == b'':
+                        break  # EOF
+                    # let the controller try to parse this additional raw gdb output
+                    responses += gdbmi._get_responses_list(gdb_mi_simulated_output, stream, False)
+            assert(len(responses) == 139)
+
+            # spot check a few
+            assert(responses[0] == {'message': None, 'type': 'console', 'payload': u'0x00007fe2c5c58920 in __nanosleep_nocancel () at ../sysdeps/unix/syscall-template.S:81\\n', 'stream': stream})
+            assert(responses[71] == {'stream': stream, 'message': u'done', 'type': 'result', 'payload': None, 'token': None})
+            assert(responses[82] == {'message': None, 'type': 'output', 'payload': u'The inferior program printed this! Can you still parse it?', 'stream': stream})
+            assert(responses[-2] == {'stream': stream, 'message': u'thread-group-exited', 'type': 'notify', 'payload': {u'exit-code': u'0', u'id': u'i1'}, 'token': None})
+            assert(responses[-1] == {'stream': stream, 'message': u'thread-group-started', 'type': 'notify', 'payload': {u'pid': u'48337', u'id': u'i1'}, 'token': None})
+
+            for stream in gdbmi._incomplete_output.keys():
+                assert(gdbmi._incomplete_output[stream] is None)
+
+
+def main():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+
+    suite.addTests(loader.loadTestsFromTestCase(TestPyGdbMi))
+
+    runner = unittest.TextTestRunner(verbosity=1)
+    result = runner.run(suite)
+
+    num_failures = len(result.errors) + len(result.failures)
+    return num_failures
+
+
+if __name__ == '__main__':
+    main()
