85af3047ac2d45fc464fe5aa3a8accdf3af95fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:51 +0000

    Add package raygun-django-middleware/1.0.6/raygun-django-middleware-1.0.6.tar.gz

---
+++ packages/raygun-django-middleware/raygun-django-middleware-1.0.6.tar.gz/raygun-django-middleware-1.0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="raygun-django-middleware",
+    version="1.0.6",
+    description="Raygun Django Middleware",
+    author="Mirus Research",
+    author_email="frank@mirusresearch.com",
+    packages=find_packages(),
+    url='https://github.com/mirusresearch/raygun-django-middleware',
+    license='MIT license, see LICENSE.txt',
+    # install_requires=[
+    #     ],
+)
