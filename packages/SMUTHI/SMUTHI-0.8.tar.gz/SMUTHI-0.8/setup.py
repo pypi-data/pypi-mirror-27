3c721bf08b80dfd49ba289d02a8c4e8f8e004bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:23 +0000

    Add package SMUTHI/0.8/SMUTHI-0.8.tar.gz

---
+++ packages/SMUTHI/SMUTHI-0.8.tar.gz/SMUTHI-0.8/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="SMUTHI",
+    version="0.8",
+    author="Amos Egel",
+    author_email="amos.egel@kit.edu",
+    url='https://gitlab.com/AmosEgel/smuthi',
+    description="Light scattering by multiple particles in thin-film systems",
+    long_description=read('README.rst'),
+    packages=['smuthi', 'smuthi.nfmds'],
+    package_data={'smuthi.nfmds': ['NFM-DS/*.txt', 'NFM-DS/TMATSOURCES/*', 'NFM-DS/TMATFILES/*',
+                                   'NFM-DS/INPUTFILES/*.dat', 'NFM-DS/OUTPUTFILES/*'],
+                  'smuthi': ['data/*']},
+    install_requires=['numpy', 'scipy', 'mpmath', 'matplotlib', 'pyyaml', 'argparse', 'imageio', 'sympy', 'tqdm'],
+    extras_require={'cuda':  ['PyCuda']},
+    entry_points={'console_scripts': ['smuthi = smuthi.__main__:main']},
+    license='MIT',
+)
