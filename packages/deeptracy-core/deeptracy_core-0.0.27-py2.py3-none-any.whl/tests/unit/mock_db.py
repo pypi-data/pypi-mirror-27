ebbc112872f03f28ff4f2dd44d002705126d3ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:17 +0000

    Add package deeptracy-core/0.0.27/deeptracy_core-0.0.27-py2.py3-none-any.whl

---
+++ packages/deeptracy-core/deeptracy_core-0.0.27-py2.py3-none-any.whl/tests/unit/mock_db.py
@@ -0,0 +1,47 @@
+# Copyright 2017 BBVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class MockQuery:
+    _ret_val = None
+
+    def __init__(self, model):
+        self._model = model
+
+    def all(self):
+        return [self._ret_val]
+
+    def get(self, id):
+        return self._ret_val
+
+    def update(self, condition):
+        return self.condition
+
+
+class MockSession:
+    query = MockQuery
+
+    def close(self):
+        pass
+
+    def add(self, object):
+        pass
+
+
+class MockDeeptracyDBEngine:
+
+    engine = None
+    Base = None
+    Session = MockSession
+    created_session = None
