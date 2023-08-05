ec097f9a753d69de4bdf8f615938511661a4d1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:44 +0000

    Add package slitscan/0.3/slitscan-0.3.tar.gz

---
+++ packages/slitscan/slitscan-0.3.tar.gz/slitscan-0.3/README.md
@@ -0,0 +1,14 @@
+Slit-scan Photography
+=====================
+
+# Install
+
+```
+pip install slitscan
+```
+
+# Usage
+
+```
+slitscan -i movie.mp4
+```
---
+++ packages/slitscan/slitscan-0.3.tar.gz/slitscan-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='slitscan',
+      version='0.3',
+      description='Slit-scan photography.',
+      url='http://github.com/markjarzynski/slitscan',
+      author='Mark Jarzynski',
+      author_email='mark.jarzynski@gmail.com',
+      license='MIT',
+      packages=['slitscan'],
+      install_requires=[
+          'numpy',
+          'imageio'
+      ],
+      entry_points={
+          'console_scripts': ['slitscan=slitscan.__main__:main'],
+      },
+      zip_safe=False)
