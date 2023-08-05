15974826f8621f9b7f4cf798d10717d66f1796db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:11 +0000

    Add package pygments-lexers-graphql/1.0.1/pygments-lexers-graphql-1.0.1.tar.gz

---
+++ packages/pygments-lexers-graphql/pygments-lexers-graphql-1.0.1.tar.gz/pygments-lexers-graphql-1.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+def readme():
+	'A GraphQL lexer for Pygments.'
+
+setuptools.setup(
+    name='pygments-lexers-graphql',
+    license='MIT',
+    author='Ryan Parman',
+    author_email='ryan@ryanparman.com',
+    url="https://github.com/skyzyx/pygments.lexers.graphql",
+    install_requires=[
+        'Pygments >=2.2.0',
+    ],
+    version='1.0.1',
+    description='A GraphQL lexer for Pygments.',
+    long_description=readme(),
+    keywords='graphql pygments',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+    entry_points =
+    """
+    [pygments.lexers]
+    graphqllexer=pygments.lexers.graphql.lexer:GraphqlLexer
+    """,
+)
