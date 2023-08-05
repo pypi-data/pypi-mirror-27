19f5509aad860cf77cb6d676e55de0607894f856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:00 +0000

    Add package dictorm/3.7.1/dictorm-3.7.1.tar.gz

---
+++ packages/dictorm/dictorm-3.7.1.tar.gz/dictorm-3.7.1/setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+from setuptools import setup
+from dictorm.dictorm import __version__, __doc__ as ddoc
+
+config = {
+    'name':'dictorm',
+    'version':str(__version__),
+    'author':'rolobio',
+    'author_email':'rolobio+dictorm@rolobio.com',
+    'description':ddoc,
+    'license':'Apache2',
+    'keywords':'psycopg2 dictionary python dict',
+    'url':'https://github.com/rolobio/DictORM',
+    'packages':[
+        'dictorm',
+        ],
+    'long_description':ddoc,
+    'classifiers':[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        ],
+    'test_suite':'dictorm.test_dictorm',
+    'extras_require':{
+        'Postgresql': ['psycopg2'],
+        }
+    }
+
+setup(**config)
+
