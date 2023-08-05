e2dc8b2529d6380a30b71d55811fcb8ab5e4b0da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package lib.com.pip/1.0/lib.com.pip-1.0.zip

---
+++ packages/lib.com.pip/lib.com.pip-1.0.zip/lib.com.pip-1.0/setup.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from distutils.core import setup
+
+setup(name='lib.com.pip',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='王俊',
+      author_email='hiwjcn@live.com',
+      url='https://github.com/hiwjcn',
+      packages=[],
+      )
