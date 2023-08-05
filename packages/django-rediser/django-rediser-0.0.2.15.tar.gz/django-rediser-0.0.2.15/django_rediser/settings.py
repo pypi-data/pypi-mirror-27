24c666eebcc93b57a398b37e90ea530d7f30740e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package django-rediser/0.0.2.15/django-rediser-0.0.2.15.tar.gz

---
+++ packages/django-rediser/django-rediser-0.0.2.15.tar.gz/django-rediser-0.0.2.15/django_rediser/settings.py
@@ -0,0 +1,31 @@
+import threading
+
+try:
+    from django.conf import settings
+except ImportError:
+    settings = object()
+
+
+class RediserSettings(dict):
+    __singleton_lock = threading.Lock()
+    __singleton_instance = None
+
+    @classmethod
+    def __new__(cls, *args, **kwargs):
+        if not cls.__singleton_instance:
+            with cls.__singleton_lock:
+                if not cls.__singleton_instance:
+                    cls.__singleton_instance = super().__new__(cls)
+        return cls.__singleton_instance
+
+    def __init__(self):
+        super().__init__()
+        _django_rediser_settings = getattr(settings, 'DJANGO_REDISER', {})
+        self.update({
+            'REDIS_HOST': _django_rediser_settings.get('REDIS_HOST', 'localhost'),
+            'REDIS_PORT': _django_rediser_settings.get('REDIS_PORT', 6379),
+            'REDIS_DB': _django_rediser_settings.get('REDIS_DB', 0),
+        })
+
+
+rediser_settings = RediserSettings()
