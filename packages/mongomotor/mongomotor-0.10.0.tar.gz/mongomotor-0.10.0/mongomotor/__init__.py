5dc38a72a34d4c04afe56148c550c7f42fc13eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:57 +0000

    Add package mongomotor/0.10.0/mongomotor-0.10.0.tar.gz

---
+++ packages/mongomotor/mongomotor-0.10.0.tar.gz/mongomotor-0.10.0/mongomotor/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# flake8: noqa
+
+import sys
+PY35 = sys.version_info[:2] >= (3, 5)
+
+from mongomotor.monkey import MonkeyPatcher
+
+patcher = MonkeyPatcher()
+patcher.patch_dereference()
+
+from mongomotor.connection import connect, disconnect
+from mongoengine.document import (MapReduceDocument, EmbeddedDocument,
+                                  DynamicEmbeddedDocument)
+from mongomotor.document import (Document,
+                                 DynamicDocument)
+
+VERSION = '0.10.0'
+
+__all__ = ['connect', 'disconnect', 'Document', 'DynamicDocument',
+           'EmbeddedDocument', 'DynamicEmbeddedDocument', 'MapReduceDocument']
