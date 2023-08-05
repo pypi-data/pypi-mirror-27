a0bfc4199c20215c18f1f5942ce11794530bbb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:40 +0000

    Add package sanskrit-util/0.1.1.dev1/sanskrit_util-0.1.1.dev1.tar.gz

---
+++ packages/sanskrit-util/sanskrit_util-0.1.1.dev1.tar.gz/sanskrit_util-0.1.1.dev1/sanskrit_util/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+sanskrit
+~~~~~~~~
+
+A general-purpose library for Sanskrit.
+
+:license: MIT and BSD
+"""
+
+from .context import Context
+from .transliterate import betacode, sanscript
