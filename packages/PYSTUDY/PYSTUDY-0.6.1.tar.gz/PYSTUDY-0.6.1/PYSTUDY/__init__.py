4c56f5e0736f496e073e04fc134b2292bf396729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:59 +0000

    Add package PYSTUDY/0.6.1/PYSTUDY-0.6.1.tar.gz

---
+++ packages/PYSTUDY/PYSTUDY-0.6.1.tar.gz/PYSTUDY-0.6.1/PYSTUDY/__init__.py
@@ -0,0 +1,19 @@
+"""
+看书，学习，慢慢积累，不作重复的事情
+"""
+
+__title__ = 'PYSTUDY'
+__version__ = '0.6.1'
+__author__ = 'shi-cong'
+__license__ = 'Apache License'
+__copyright__ = 'Copyright 2017 shi-cong'
+__docformat__ = 'html'
+
+def version():
+    return __version__
+
+
+__all__ = ['collectionslib', 'ddoslib', 'debuglib', 'encodelib', 'encryptlib', 
+           'html_parserlib', 'jsonlib', 'modulelib', 'numpylib', 'oslib', 
+           'randomlib', 'stringlib', 'timelib', 'yamllib', 'api', 'image', 
+           'middleware', 'ml', 'net', 'office', 'living', 'selenium', 'loglib',]
