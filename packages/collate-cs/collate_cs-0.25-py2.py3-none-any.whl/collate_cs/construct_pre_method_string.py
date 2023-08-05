78483ed44d8bc233aef4698fa5d56de21575c639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:14 +0000

    Add package collate-cs/0.25/collate_cs-0.25-py2.py3-none-any.whl

---
+++ packages/collate-cs/collate_cs-0.25-py2.py3-none-any.whl/collate_cs/construct_pre_method_string.py
@@ -0,0 +1,34 @@
+import namespace_override as override
+
+def _(methods, address, class_name):
+    ret = []
+
+    if 'using.cs' in methods:
+        for line in methods['using.cs']:
+            ret += [line]
+        ret += ['']
+        del(methods['using.cs'])
+
+    address = map(lambda s: override._(s), address)
+    ret += ['namespace ' + '.'.join(address)]
+    ret += ['{']
+    if 'test' in class_name:
+        ret += ["    using NUnit.Framework;"]
+        ret += [""]
+        ret += ["    [TestFixture]"]
+    ret += ["    [System.Serializable]"]
+
+
+    if '_.cs' in methods:
+        for line in methods['_.cs']:
+            ret += ['    ' + line]
+        del(methods['_.cs'])
+    else:
+        ret += ["    public class " + class_name]
+
+    if '}' not in ret[-1]:
+        ret += ["    {"]
+        ret += ["    }"]
+
+    ret = '\n' + '\n'.join(ret[:-1])
+    return ret
