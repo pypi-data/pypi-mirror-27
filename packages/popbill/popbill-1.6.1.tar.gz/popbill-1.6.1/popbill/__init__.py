f003b4dda0d8856f6e1d5b3c867a4efcc13c10cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:07 +0000

    Add package popbill/1.6.1/popbill-1.6.1.tar.gz

---
+++ packages/popbill/popbill-1.6.1.tar.gz/popbill-1.6.1/popbill/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '1.6.1'
+Version = __version__  # for backward compatibility
+__all__ = [ "PopbillException","JoinForm","ContactInfo","CorpInfo",
+			"TaxinvoiceService","Taxinvoice","TaxinvoiceDetail","Contact",
+			"FaxService","FaxReceiver",
+			"StatementService", "Statement","StatementDetail",
+			"CashbillService", "Cashbill",
+			"MessageService", "MessageReceiver",
+            "HTTaxinvoiceService", "HTCashbillService",
+            "ClosedownService"]
+
+from .base import PopbillException , JoinForm, ContactInfo, CorpInfo
+from .taxinvoiceService import *
+from .statementService import *
+from .faxService import *
+from .cashbillService import *
+from .messageService import *
+from .htTaxinvoiceService import *
+from .htCashbillService import *
+from .closedownService import *
