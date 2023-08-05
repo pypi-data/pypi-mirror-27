78c53c35463b35041cacb8e582fc433f7b067bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:42 +0000

    Add package hca/3.0.2/hca-3.0.2.tar.gz

---
+++ packages/hca/hca-3.0.2.tar.gz/hca-3.0.2/hca/upload/cli/list_area_command.py
@@ -0,0 +1,28 @@
+from .. import list_current_area
+
+
+class ListAreaCommand:
+    """
+    List contents of currently selected upload area.
+    """
+    @classmethod
+    def add_parser(cls, staging_subparsers):
+        list_area_parser = staging_subparsers.add_parser(
+            'list', description=cls.__doc__, help=cls.__doc__)
+        list_area_parser.set_defaults(func=ListAreaCommand)
+        list_area_parser.add_argument('-l', '--long', action='store_true', help="Long listing - show file details.")
+
+    def __init__(self, args):
+        files = list_current_area()
+        for f in files:
+            print(f['name'])
+            if args.long:
+                print("\t%-12s %d bytes\n\t%-12s %s\n\t%-12s %s" % (
+                    'size', f['size'],
+                    'URL', f['url'],
+                    'Content-Type', f['content_type']
+                ))
+                if 'checksums' in f:
+                    for checksum in ('S3_Etag', 'CEC32C', 'SHA1', 'SHA256'):
+                        if checksum.lower() in f['checksums']:
+                            print("\t%-12s %s" % (checksum, f['checksums'][checksum.lower()]))
