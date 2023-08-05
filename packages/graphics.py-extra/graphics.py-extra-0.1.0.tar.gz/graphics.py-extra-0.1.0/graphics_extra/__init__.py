2afadac75cbf1d5cff3f8313cb1792fb6ec78e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:20 +0000

    Add package graphics.py-extra/0.1.0/graphics.py-extra-0.1.0.tar.gz

---
+++ packages/graphics.py-extra/graphics.py-extra-0.1.0.tar.gz/graphics.py-extra-0.1.0/graphics_extra/__init__.py
@@ -0,0 +1,3 @@
+from graphics_extra.free_text import FreeText
+from graphics_extra.rounded_rectangle import RoundedRectangle
+from graphics_extra.triangle import Triangle
---
+++ packages/graphics.py-extra/graphics.py-extra-0.1.0.tar.gz/graphics.py-extra-0.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+version = "0.1.0"
+
+requirements = [
+    "graphics.py",
+    "trianglesolver"
+]
+
+with open("README.rst") as file:
+    readme = file.read()
+
+setup(
+    name="graphics.py-extra",
+    version=version,
+    description="Extra objects for the graphics.py package",
+    long_description=readme,
+    author="yurihs",
+    url='https://github.com/yurihs/graphics_extra/',
+    packages=["graphics_extra"],
+    packagedir={"graphics_extra": "graphics_extra"},
+    include_package_data=True,
+    python_requires=">=3",
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: Portuguese (Brazilian)",
+        "Topic :: Multimedia :: Graphics"
+    ]
+)
