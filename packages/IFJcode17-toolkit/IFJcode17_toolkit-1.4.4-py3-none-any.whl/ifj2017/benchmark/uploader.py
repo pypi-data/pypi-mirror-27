0c2068d258c8134f9b39f3370505e13667555318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:51 +0000

    Add package IFJcode17-toolkit/1.4.4/IFJcode17_toolkit-1.4.4-py3-none-any.whl

---
+++ packages/IFJcode17-toolkit/IFJcode17_toolkit-1.4.4-py3-none-any.whl/ifj2017/benchmark/uploader.py
@@ -0,0 +1,124 @@
+# coding=utf-8
+
+import urllib
+import urllib.request
+import uuid
+from json import dumps, loads
+from os.path import basename
+from urllib.error import URLError
+
+import ifj2017
+
+from ..test.base import TestReport
+from ..test.logger import TestLogger
+
+
+class BenchmarkUploader(object):
+    def __init__(self, _api_hostname, token_file):
+        self._api_hostname = _api_hostname
+        self._reports = []  # type: list[TestReport]
+        self._token = None
+        self._has_connection = False
+        self._token_file = token_file
+        self._last_response = None
+        self.version = None
+
+    @property
+    def has_connection(self):
+        return self._has_connection
+
+    def check_connection(self):
+        request = urllib.request.Request(self._api_hostname)
+        request.get_method = lambda: 'GET'
+
+        try:
+            data = self._request('/api/v1/service-online', {}, force=True)
+        except URLError as e:
+            TestLogger.log_warning('Problem with connecting to {} ({}).'.format(self._api_hostname, e))
+            return
+        else:
+            self._has_connection = (
+                200 >= self._last_response.status < 400
+            ) and data.get('success')
+            if data.get('msg'):
+                TestLogger.log_warning(data.get('msg'))
+            self.version = data.get('version')
+
+    def authenticate_user(self):
+        if not self._has_connection:
+            return
+        try:
+            with open(self._token_file) as f:
+                token = str(uuid.UUID(f.read(), version=4))
+        except (ValueError, OSError):
+            token = None
+
+        if not token:
+            token = self._generate_token()
+            if not token:
+                return False
+            self._save_token(token=token)
+        self._token = token
+
+    def _generate_token(self):
+        TestLogger.log(
+            TestLogger.HEADER,
+            'At first, please, provide your FIT login names for sending benchmark results:'
+        )
+
+        def _ask():
+            leader = input('Login of your team leader? ')
+            login = input('Your login? ')
+
+            return self._request('/api/v1/generate-author-token', dict(
+                leader=leader,
+                login=login
+            ))
+
+        response = _ask()
+        while not response.get('success'):
+            TestLogger.log_warning('Invalid credentials ({}).'.format(response.get('message')))
+            response = _ask()
+
+        return response.get('token')
+
+    def collect_report(self, report):
+        # type: (TestReport) -> None
+        self._reports.append(report)
+
+    def send_reports(self):
+        # type: (dict) -> dict
+        if not self._reports:
+            return dict(success=True)
+        response = self._request(
+            '/api/v1/benchmark-result',
+            dict(
+                token=self._token,
+                reports=[
+                    dict(
+                        section=basename(report.test_info.section_dir),
+                        name=report.test_info.name,
+                        operand_price=report.state.operand_price,
+                        instruction_price=report.state.instruction_price,
+                    ) for report in self._reports  # type: TestReport
+                ]
+            )
+        )
+        return response
+
+    def _save_token(self, token):
+        with open(self._token_file, 'w') as f:
+            f.write(token)
+
+    def _request(self, url, data, force=False):
+        if not self._has_connection and not force:
+            return {}
+        request = urllib.request.Request(''.join((self._api_hostname, url)))
+        request.add_header('Content-type', 'application/json')
+        response = urllib.request.urlopen(request, bytes(dumps(
+            data
+        ), encoding='utf-8') if data else None)
+        body = response.read()
+        response.close()
+        self._last_response = response
+        return loads(str(body, encoding='utf-8'), encoding='utf-8')
---
+++ packages/IFJcode17-toolkit/IFJcode17_toolkit-1.4.4-py3-none-any.whl/ifj2017/ide/main.py
@@ -0,0 +1,116 @@
+# coding=utf-8
+import sys
+import signal
+
+from os import path
+from platform import system
+
+import ifj2017
+from PyQt5.QtCore import (QSize, QtFatalMsg, QtCriticalMsg, QtWarningMsg, QtInfoMsg,
+                          qInstallMessageHandler, QtDebugMsg)
+from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QIcon
+from PyQt5.QtQml import QQmlApplicationEngine, qmlRegisterSingletonType, qmlRegisterType
+from PyQt5.QtWidgets import QApplication
+
+from ifj2017.ide.code.diff_code_analyzer import DiffCodeAnalyzer
+from ifj2017.ide.code.expression import ExpSyntaxHighlighter, ExpAnalyzer
+from ifj2017.ide.code_analyzer import CodeAnalyzer
+from ifj2017.ide.core.core import Core
+from ifj2017.ide.core.file_io import FileIO, FILE_PREFIX
+from ifj2017.ide.core.formatted_text_writer import FormattedTextWriter
+from ifj2017.ide.core.tree_view_model import TreeViewModel
+from ifj2017.ide.debugger_wrapper import DebuggerWrapper
+from ifj2017.ide.io_wrapper import IOWrapper
+from ifj2017.ide.settings import ICON_SIZES
+
+try:
+    from termcolor import colored
+except ImportError:
+    def colored(mode, *args, **kwargs):
+        return mode
+
+# See more at https://coldfix.de/2016/11/08/pyqt-boilerplate/#keyboardinterrupt-ctrl-c
+def setup_interrupt_handling():
+    signal.signal(signal.SIGINT, _interrupt_handler)
+    safe_timer(50, lambda: None)
+
+def _interrupt_handler(signum, frame):
+    QApplication.quit()
+
+
+def safe_timer(timeout, func, *args, **kwargs):
+    def timer_event():
+        try:
+            func(*args, **kwargs)
+        finally:
+            QTimer.singleShot(timeout, timer_event)
+    QTimer.singleShot(timeout, timer_event)
+
+def qt_message_handler(mode, context, message):
+    modes = {
+        QtInfoMsg: "Info",
+        QtWarningMsg: "Warning",
+        QtCriticalMsg: "Critical",
+        QtFatalMsg: "Fatal",
+        QtDebugMsg: "Debug"
+    }
+
+    modes_colors = {
+        QtInfoMsg: "blue",
+        QtWarningMsg: "yellow",
+        QtCriticalMsg: "red",
+        QtFatalMsg: "red",
+        QtDebugMsg: "green"
+    }
+
+    mode = colored(modes[mode], modes_colors[mode])
+
+    if context.file is None:
+        print('{mode}: {msg}'.format(mode=mode, msg=message))
+    else:
+        print('{mode}: {msg}\t\t\t\tline: {line}, function: {func}, file: {file}'.format(
+            mode=mode, line=context.line, func=context.function, file=context.file, msg=message))
+
+
+qInstallMessageHandler(qt_message_handler)
+
+app = QApplication(sys.argv)
+
+base_url = QUrl("{}{}/".format(FILE_PREFIX[system()], str(path.abspath(path.dirname(__file__))).replace('\\', '/')))
+
+icon = QIcon()
+for size in ICON_SIZES:
+    path = FileIO.removeFilePrefix(base_url.resolved(QUrl("assets/icons/{}x{}.png".format(size, size))).url())
+    icon.addFile(path, QSize(size, size))
+app.setWindowIcon(icon)
+
+qmlRegisterSingletonType(base_url.resolved(QUrl("assets/styles/UIStyles.qml")), "StyleSettings", 1, 0, "StyleSettings")
+qmlRegisterSingletonType(CodeAnalyzer, "CodeAnalyzer", 1, 0, "CodeAnalyzer", CodeAnalyzer.singletonProvider)
+qmlRegisterType(ExpSyntaxHighlighter, "ExpSyntaxHighlighter", 1, 0, "ExpSyntaxHighlighter")
+qmlRegisterType(DiffCodeAnalyzer, "DiffCodeAnalyzer", 1, 0, "DiffCodeAnalyzer")
+qmlRegisterType(ExpAnalyzer, "ExpAnalyzer", 1, 0, "ExpAnalyzer")
+qmlRegisterType(TreeViewModel, "TreeViewModel", 1, 0, "TreeViewModel")
+qmlRegisterType(DebuggerWrapper, "Debugger", 1, 0, "Debugger")
+qmlRegisterType(IOWrapper, "IOWrapper", 1, 0, "IOWrapper")
+qmlRegisterType(FileIO, "FileIO", 1, 0, "FileIO")
+qmlRegisterType(FormattedTextWriter, "FormattedTextWriter", 1, 0, "FormattedTextWriter")
+
+core = Core()
+
+engine = QQmlApplicationEngine()
+engine.rootContext().setContextProperty("ossystem", system())
+engine.rootContext().setContextProperty("packageVersion", ifj2017.version)
+engine.rootContext().setContextProperty("rootDir", base_url.toString())
+engine.rootContext().setContextProperty("Core", core)
+engine.load(base_url.resolved(QUrl("qml/main.qml")))
+
+
+def main():
+    return app.exec()
+
+
+if __name__ == '__main__':
+    setup_interrupt_handling()
+    main()
