12d223565202f3ce40ef8552b1bbea79b695a1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:56 +0000

    Add package nummaster/1.2.0.dev1/nummaster-1.2.0.dev1.tar.gz

---
+++ packages/nummaster/nummaster-1.2.0.dev1.tar.gz/nummaster-1.2.0.dev1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'nummaster',
+  packages = ['nummaster'], # this must be the same as the name above
+  version = '1.2.0.dev1',
+  description = 'A library for computational number theory , contains functions for prime numbers , factorizations and other number theortic functions',
+  author = 'Shivam Patel',
+  author_email = 'patelshivam99@gmail.com',
+  url = '', # use the URL to the github repo
+  download_url = '', 
+  keywords = ['number-theory', 'number', 'theory','computation','primes','shivam','patel'], # arbitrary keywords
+  classifiers = ['Development Status :: 3 - Alpha', 'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',],
+)
