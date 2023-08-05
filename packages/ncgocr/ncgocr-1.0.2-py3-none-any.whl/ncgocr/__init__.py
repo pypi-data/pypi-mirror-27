12f2f7ec2499b6c01e782e0ff1b0c6e28605b529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:12 +0000

    Add package ncgocr/1.0.2/ncgocr-1.0.2-py3-none-any.whl

---
+++ packages/ncgocr/ncgocr-1.0.2-py3-none-any.whl/ncgocr/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+import os
+__author__ = 'Chia-Jung, Yang'
+__email__ = 'jeroyang@gmail.com'
+__version__ = '1.0.2'
+
+from ncgocr.concept import *
+from ncgocr.corpus_craft import *
+from ncgocr.pattern_regex import *
+from ncgocr.ncgocr import *
