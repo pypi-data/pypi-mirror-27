f3aefb8b21e353bfd60a8308cf0ea490882542c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:56 +0000

    Add package pypbcms/1.1.1/pypbcms-1.1.1.tar.gz

---
+++ packages/pypbcms/pypbcms-1.1.1.tar.gz/pypbcms-1.1.1/setup.py
@@ -0,0 +1,33 @@
+"""setup.py for pypbcms."""
+
+from setuptools import setup
+
+
+setup(
+    # TODO: the following values are defined in two places at once. This should be fixed.
+    name="pypbcms",
+    version="1.1.1",
+    author="bennr01",
+    author_email="benjamin99.vogt@web.de",
+    description="A plugin based cluster management system written in python",
+    long_description=open("README.md").read(),
+    license="MIT",
+    keywords="cluster tools plugin plugins cmd rpc management",
+    url="https://github.com/bennr01/pypbcms/",
+    classifiers=[
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        ],
+    py_modules=[
+        "pypbcms",
+        ],
+    install_requires=[
+        "zope.interface",
+        "Twisted",
+        ],
+    entry_points={
+        "console_scripts": [
+            "pypbcms = pypbcms:main"
+            ],
+        },
+    )
