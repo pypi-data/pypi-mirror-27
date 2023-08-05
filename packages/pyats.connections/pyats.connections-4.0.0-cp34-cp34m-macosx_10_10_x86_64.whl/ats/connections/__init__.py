76abb137cc49baf8056d36869015e97036986a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:18 +0000

    Add package pyats.connections/4.0.0/pyats.connections-4.0.0-cp34-cp34m-macosx_10_10_x86_64.whl

---
+++ packages/pyats.connections/pyats.connections-4.0.0-cp34-cp34m-macosx_10_10_x86_64.whl/ats/connections/__init__.py
@@ -0,0 +1,9 @@
+# metadata
+__version__ = '4.0.0'
+__author__ = 'Cisco Systems'
+__contact__ = 'pyats-support-ext@cisco.com'
+__copyright__ = 'Copyright (c) 2017, Cisco Systems Inc.'
+
+from .manager import ConnectionManager
+from .bases import BaseConnection
+from .hooks import ServiceHook
