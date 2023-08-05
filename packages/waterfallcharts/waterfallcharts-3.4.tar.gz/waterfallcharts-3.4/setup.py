e9d48f3df14b4b2a09254a4470299204cad2ebe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:42 +0000

    Add package waterfallcharts/3.4/waterfallcharts-3.4.tar.gz

---
+++ packages/waterfallcharts/waterfallcharts-3.4.tar.gz/waterfallcharts-3.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'waterfallcharts',
+  py_modules=['waterfall_chart'],
+  version = '3.4',
+  description = 'Quickly generates standard waterfall charts, takes two ordered lists as inputs.',
+  author = 'Christopher Paul Csiszar',
+  author_email = 'christopher.csiszar@gmail.com',
+  url = 'https://github.com/chrispaulca/waterfall', 
+  download_url = 'https://github.com/chrispaulca/waterfall/archive/v3.4.tar.gz',
+  keywords = ['waterfall', 'chart', 'charts', 'accounting', 'automate', 'random forest', 'decision tree'],
+  classifiers = [],
+)
