0395dcb0d2895beed70efb9636bf61f153c259bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package shuup-attrim/0.9.0.6/shuup_attrim-0.9.0.6-py3-none-any.whl

---
+++ packages/shuup-attrim/shuup_attrim-0.9.0.6-py3-none-any.whl/attrim/admin/cls/views/__init__.py
@@ -0,0 +1,10 @@
+from .list import ClassListView
+from .edit import ClassEditView
+from .delete import ClassDeleteView
+
+
+__all__ = [
+    'ClassListView',
+    'ClassEditView',
+    'ClassDeleteView',
+]
---
+++ packages/shuup-attrim/shuup_attrim-0.9.0.6-py3-none-any.whl/attrim/static/attrim/admin/e2e/utils/django.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DJANGO = {
+    url: 'http://localhost:8082',
+    mockClsCode: 'language',
+    user: {
+        username: 'test@localhost',
+        password: 'test@localhost',
+    },
+};
+//# sourceMappingURL=django.js.map
