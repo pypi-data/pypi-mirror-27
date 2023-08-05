57464ba5cae68c559d00fa997fa2bf85569480bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:07 +0000

    Add package pelican-shiori/0.2.0/pelican_shiori-0.2.0.tar.gz

---
+++ packages/pelican-shiori/pelican_shiori-0.2.0.tar.gz/pelican_shiori-0.2.0/pelican_shiori/__init__.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+def asset_config(theme='default'):
+    return (
+        ('JINJA2_CONTEXT', {'theme': theme}),
+    )
+
+
+def path():
+    """https://github.com/getpelican/pelican/issues/1564#issuecomment-282136049
+    """
+    return str(Path(__file__).parent)
