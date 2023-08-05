6c9ddb30e501f43ec66233ffca72c62542a9d464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:09 +0000

    Add package text2html/0.0.1.dev0/text2html-0.0.1.dev0.tar.gz

---
+++ packages/text2html/text2html-0.0.1.dev0.tar.gz/text2html-0.0.1.dev0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
+
+setup(name='text2html',
+      version=version,
+      description="create HTML files from some text files",
+      long_description="""\
+create HTML files from some text files""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='HTML text file create',
+      author='solima',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      sohtml = text2html.sohtml:main
+      """,
+      )
---
+++ packages/text2html/text2html-0.0.1.dev0.tar.gz/text2html-0.0.1.dev0/text2html/page.py
@@ -0,0 +1,14 @@
+class Page:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return 'Pageクラスです'
+
+    def __repr__(self):
+        return 'Paga class'
+
+
+if __name__ == '__main__':
+    p = Page()
+    print(p)
