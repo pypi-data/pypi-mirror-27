31f9f8237e96dc593bcaa17567bd123890f694dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:13 +0000

    Add package kcmApp/1.4/kcmApp-1.4.tar.gz

---
+++ packages/kcmApp/kcmApp-1.4.tar.gz/kcmApp-1.4/kcmApp/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.http import JsonResponse
+from djangoApiDec.djangoApiDec import queryString_required
+from KCM.__main__ import KCM
+from udic_nlp_API.settings_database import uri
+kcmObject = KCM(lang, lang, uri=uri)
+
+@queryString_required(['lang', 'keyword'])
+def kcm(request):
+    """Generate list of term data source files
+    Returns:
+        if contains invalid queryString key, it will raise exception.
+    """
+    keyword = request.GET['keyword']
+    lang = request.GET['lang']
+
+    result = kcmObject.get(keyword, int(request.GET['num']) if 'num' in request.GET else 10)
+    return JsonResponse(result, safe=False)
---
+++ packages/kcmApp/kcmApp-1.4.tar.gz/kcmApp-1.4/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = 'kcmApp',
+    packages = ['kcmApp'],
+    version = '1.4',
+    description = 'A django App for kcm',
+    author = 'davidtnfsh',
+    author_email = 'davidtnfsh@gmail.com',
+    url = 'https://github.com/udicatnchu/kcmApp',
+    download_url = 'https://github.com/udicatnchu/kcmApp/archive/v1.4.tar.gz',
+    keywords = ['kcmApp'],
+    classifiers = [],
+    license='GPL3.0',
+    install_requires=[
+    ],
+    zip_safe=True,
+)
