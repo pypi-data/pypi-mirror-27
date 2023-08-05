e00ebd686ed8dacae074368d186441fd7627d198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:13 +0000

    Add package hotmic/0.1.5/hotmic-0.1.5.tar.gz

---
+++ packages/hotmic/hotmic-0.1.5.tar.gz/hotmic-0.1.5/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'hotmic'
+    ,version = '0.1.5'
+    ,author = 'frank2'
+    ,author_email = 'frank2@dc949.org'
+    ,description = 'A Galois LFSR implementation with lots of polys.'
+    ,license = 'GPLv3'
+    ,keywords = 'lfsr'
+    ,url = 'https://github.com/frank2/hotmic'
+    ,package_dir = {'hotmic': 'lib'}
+    ,packages = ['hotmic']
+    ,long_description = '''hotmic is simply an LFSR library with polys pulled from
+this document: http://web.archive.org/web/20161007061934/http://courses.cse.tamu.edu/csce680/walker/lfsr_table.pdf.'''
+    ,classifiers = [
+        'Development Status :: 3 - Alpha'
+        ,'Topic :: Software Development :: Libraries'
+        ,'License :: OSI Approved :: GNU General Public License v3 (GPLv3)']
+)
