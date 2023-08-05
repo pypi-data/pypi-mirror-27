9da46e7a083260d24af1422dc6362152a5d4bdd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:49 +0000

    Add package reactome-stats/0.5.0/reactome_stats-0.5.0.tar.gz

---
+++ packages/reactome-stats/reactome_stats-0.5.0.tar.gz/reactome_stats-0.5.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='reactome_stats',
+      version='0.5.0',
+      description='Reactome Metrics and Coverage',
+      url='https://github.com/corinabioinformatic/Neo4J_at_Reactome/tree/ReactomeStats',
+      author='CorinaBioinformatic',
+      author_email='corina.bioinformatic@gmail.com',
+      license='GNU GPLv3',
+      packages=['reactome_stats'],
+      py_modules=['uniprot', 'kegg', 'goc', 'analysis', 'reactomegraphdb'],
+      install_requires=['pandas', 'neo4j_driver', 'matplotlib_venn'],
+      zip_safe=False)
