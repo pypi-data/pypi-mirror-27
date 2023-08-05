0627ca62eb17a8d3f4f52c7fe186e437f17d861e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:07 +0000

    Add package shodan/1.7.6/shodan-1.7.6.tar.gz

---
+++ packages/shodan/shodan-1.7.6.tar.gz/shodan-1.7.6/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+dependencies = open('requirements.txt', 'r').read().split('\n')
+
+setup(
+    name = 'shodan',
+    version = '1.7.6',
+    description = 'Python library and command-line utility for Shodan (https://developer.shodan.io)',
+    author = 'John Matherly',
+    author_email = 'jmath@shodan.io',
+    url = 'http://github.com/achillean/shodan-python/tree/master',
+    packages = ['shodan', 'shodan.cli', 'shodan.cli.converter'],
+    scripts = ['bin/shodan'],
+    install_requires = dependencies,
+    keywords = ['security', 'network'],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
