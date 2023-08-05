4e70fd086285ac02547abd75029e38550f80ed96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:24 +0000

    Add package oktopus/0.1.dev12/oktopus-0.1.dev12.tar.gz

---
+++ packages/oktopus/oktopus-0.1.dev12.tar.gz/oktopus-0.1.dev12/README.rst
@@ -0,0 +1,20 @@
+🐙 oktopus
+==========
+|ci-badge| |cov-badge|
+
+.. |ci-badge| image:: https://travis-ci.org/KeplerGO/oktopus.svg?branch=master
+.. |cov-badge| image:: https://codecov.io/gh/mirca/oktopus/branch/master/graph/badge.svg
+
+A soft-bodied, eight-armed package for beautiful inference.
+
+To install ``oktopus``, run::
+
+    pip install oktopus
+
+or::
+
+    git clone https://github.com/KeplerGO/oktopus.git
+    cd oktopus
+    pip install -e .
+
+And you're all set!
---
+++ packages/oktopus/oktopus-0.1.dev12.tar.gz/oktopus-0.1.dev12/oktopus/models.py
@@ -0,0 +1,89 @@
+from abc import abstractmethod
+import autograd.numpy as np
+from scipy.special import erf
+import math
+
+
+class Model(object):
+    def __call__(self, *params):
+        return self.evaluate(*params)
+
+    @abstractmethod
+    def evaluate(self, *params):
+        pass
+
+
+class ConstantModel(Model):
+    def evaluate(self, c):
+        return np.array([c])
+
+    def gradient(self, c):
+        return [1.]
+
+
+class LinearModel(Model):
+    def __init__(self, X):
+        self.X = np.asarray(X)
+
+    def evaluate(self, w, b):
+        return np.dot(self.X, w) + b
+
+    def gradient(self, m, b):
+        return [self.X, np.ones(len(self.X))]
+
+
+class SymmetricGaussian2D(Model):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def evaluate(self, A, xo, yo, s):
+        return A * np.exp(-0.5 * (((self.x - xo) ** 2 + (self.y - yo) ** 2)) / s ** 2)
+
+
+class Gaussian2D(Model):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def evaluate(self, A, xo, yo, a, b, c):
+        return A * np.exp(-(a * (self.x - xo) ** 2
+                            - 2 * b * (self.x - xo) * (self.y - yo)
+                            + c * (self.y - yo) ** 2))
+
+
+class Gaussian2DPlusBkg(Gaussian2D):
+    def __init__(self, x, y):
+        super(Gaussian2DPlusBkg, self).__init__(x, y)
+
+    def evaluate(self, A, xo, yo, a, b, c, B):
+        return super(Gaussian2DPlusBkg, self).evaluate(A, xo, yo, a, b, c) + B
+
+
+class IntegratedSymmetricGaussian2D(Model):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def evaluate(self, F, xo, yo, s):
+        return (F / 4 *
+                ((erf((self.x - xo + 0.5) / (np.sqrt(2) * s)) -
+                  erf((self.x - xo - 0.5) / (np.sqrt(2) * s))) *
+                 (erf((self.y - yo + 0.5) / (np.sqrt(2) * s)) -
+                  erf((self.y - yo - 0.5) / (np.sqrt(2) * s)))))
+
+
+class ExpSquaredKernel(Model):
+    def __init__(self, t):
+        self.t = t
+
+    def evaluate(self, k, l):
+        return k * np.exp(- l ** 2 * (self.t[:, None] - self.t[None, :]) ** 2)
+
+
+class WhiteNoiseKernel(Model):
+    def __init__(self, n):
+        self.n = n
+
+    def evaluate(self, s):
+        return np.diag(np.ones(self.n) * s ** 2)
