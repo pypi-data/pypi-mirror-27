93d5241157b4f9421be84a8cec83123646805db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:27 +0000

    Add package async_v20/2.2.4b3/async_v20-2.2.4b3.tar.gz

---
+++ packages/async_v20/async_v20-2.2.4b3.tar.gz/async_v20-2.2.4b3/tests/test_definitions/test_primitives.py
@@ -0,0 +1,58 @@
+from inspect import isclass
+
+import pytest
+
+from async_v20.definitions import primitives
+from async_v20.definitions.primitives import ClientComment, ClientID, ClientTag
+from async_v20.definitions.primitives import OrderSpecifier, TradeSpecifier
+from async_v20.definitions.primitives import TransactionID
+from tests.test_definitions.helpers import get_valid_primitive_data
+
+
+@pytest.mark.parametrize('primitive', map(lambda x: getattr(primitives, x), primitives.__all__))
+def test_get_valid_primitive_data(primitive):
+    """Test the helper function can provide valid data for all primitives"""
+    assert get_valid_primitive_data(primitive)
+
+
+# Get All user defined class' in primitive package
+@pytest.mark.parametrize('primitive', map(lambda x: getattr(primitives, x), primitives.__all__))
+def test_all_incorrect_primitive_values_cannot_be_assigned(primitive):
+    if isclass(primitive):
+        if getattr(primitive, 'values', None):
+            for key in primitive.values.keys():
+                with pytest.raises(ValueError):
+                    primitive(key + '_test')
+
+
+# Get All user defined class' in primitive package
+@pytest.mark.parametrize('primitive', map(lambda x: getattr(primitives, x), primitives.__all__))
+def test_primitive_values_have_length_checking(primitive):
+    if isclass(primitive):
+        if getattr(primitive, 'values', None):
+            for key in primitive.values.keys():
+                with pytest.raises(ValueError):
+                    primitive(key + '_test')
+
+
+# Get All user defined class' in primitive package
+@pytest.mark.parametrize('primitive', map(lambda x: getattr(primitives, x), primitives.__all__))
+def test_primitives_enforce_length_checking(primitive):
+    if isclass(primitive):
+        if getattr(primitive, 'example', None) and primitive not in \
+                (OrderSpecifier, ClientComment, ClientID, ClientTag, TransactionID, TradeSpecifier):
+            with pytest.raises(ValueError):
+                primitive(primitive.example + '_')
+
+
+@pytest.mark.parametrize('primitive', map(lambda x: getattr(primitives, x), primitives.__all__))
+def test_primitives_return_correct_type_when_initialized_with_value(primitive):
+    assert type(primitive(get_valid_primitive_data(primitive))) == primitive
+
+
+def test_PriceValue_rounds_floats_to_the_correct_accuracy():
+    assert primitives.PriceValue(0.123456) == 0.12346
+
+
+def test_Unit_rounds_floats_to_the_correct_accuracy():
+    assert primitives.Unit(0.6) == 1
---
+++ packages/async_v20/async_v20-2.2.4b3.tar.gz/async_v20-2.2.4b3/tests/test_interface/test_api_methods.py
@@ -0,0 +1,71 @@
+import inspect
+
+import pytest
+from aiohttp.client_exceptions import ServerDisconnectedError, ContentTypeError
+
+from async_v20 import OandaClient
+from tests.test_definitions.helpers import get_valid_primitive_data
+from ..fixtures import server as server_module
+from ..fixtures.client import client
+
+client = client
+server = server_module.server
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('method', inspect.getmembers(OandaClient, lambda x: hasattr(x, 'endpoint')))
+async def test_client_initializes_automatically_with_every_api_method(method, server, client):
+    global received
+    global status
+
+    data = tuple(get_valid_primitive_data(param.annotation)
+                 for param in method[1].__signature__.parameters.values()
+                 if param.name != 'self')
+    status = 200
+    method = getattr(client, method[0])
+    try:
+        resp = await method(*data)
+    except (KeyError, ServerDisconnectedError, ContentTypeError, AttributeError, ConnectionError):
+        pass  # Caused by incorrect response status being returned
+        # Server not keeping a data stream open
+        # Response Not containing expected data
+    assert client.initialized
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('method', inspect.getmembers(OandaClient, lambda x: hasattr(x, 'endpoint')))
+async def test_client_methods_send_correct_data(method, server, client):
+    data = tuple(get_valid_primitive_data(param.annotation)
+                 for param in method[1].__signature__.parameters.values()
+                 if param.name != 'self')
+    async with client as client:  # initialize first
+        method = getattr(client, method[0])
+        server_module.status = next(iter(method.endpoint.responses))
+        try:
+            resp = await method(*data)
+        except (KeyError, ServerDisconnectedError, ContentTypeError, AttributeError):
+            pass  # Caused by incorrect response status being returned
+            # Server not keeping a data stream open
+            # Response Not containing expected data
+        # TODO Added meaning full asserts
+        print(server_module.received)
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('method', inspect.getmembers(OandaClient, lambda x: hasattr(x, 'shortcut')))
+async def test_client_methods_shortcut_api_methods(method, server, client):
+    data = tuple(get_valid_primitive_data(param.annotation)
+                 for param in method[1].__signature__.parameters.values()
+                 if param.name not in 'self cls')
+    async with client as client:  # initialize first
+        method = getattr(client, method[0])
+        server_module.status = 201
+
+        print(method)
+        try:
+            resp = await method(*data)
+        except (KeyError, ServerDisconnectedError, ContentTypeError, AttributeError):
+            pass  # Caused by incorrect response status being returned
+            # Server not keeping a data stream open
+            # Response Not containing expected data
+        # TODO Added meaning full asserts
+        print(server_module.received)
