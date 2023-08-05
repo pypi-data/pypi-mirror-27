be695de6b9033b6f35fe3db11a01ae874058ca06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:56 +0000

    Add package flask_beautiful_messages/1.2.7/flask_beautiful_messages-1.2.7.tar.gz

---
+++ packages/flask_beautiful_messages/flask_beautiful_messages-1.2.7.tar.gz/flask_beautiful_messages-1.2.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+  name = 'flask_beautiful_messages',
+  packages = ['flask_beautiful_messages'],
+  package_dir={'flask_beautiful_messages': 'flask_beautiful_messages'},
+  package_data={'flask_beautiful_messages': ['flask_beautiful_messages/*']},
+  include_package_data=True,
+  version = '1.2.7',
+  description = 'This library allows Flask developers to quickly create beautiful email and webpage templates',
+  author = 'Herbert Dawkins',
+  author_email = 'DrDawkins@ClearScienceInc.com',
+  url = 'https://github.com/sudouser2010/flask_beautiful_messages',
+  download_url = 'https://github.com/sudouser2010/flask_beautiful_messages/archive/1.2.6.tar.gz',
+  keywords = ['flask', 'beautiful', 'messages', 'email', 'webpage', 'template'],
+  classifiers = [],
+  install_requires=['flask==0.12.2'],
+)
