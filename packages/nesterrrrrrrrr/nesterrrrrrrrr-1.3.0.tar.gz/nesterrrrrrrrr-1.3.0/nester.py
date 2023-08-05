bc531328c4dad0dd2b4819938760b5926c660488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:13 +0000

    Add package nesterrrrrrrrr/1.3.0/nesterrrrrrrrr-1.3.0.tar.gz

---
+++ packages/nesterrrrrrrrr/nesterrrrrrrrr-1.3.0.tar.gz/nesterrrrrrrrr-1.3.0/nester.py
@@ -0,0 +1,14 @@
+def print_lol(the_list, level=0):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for num in range(level):
+                print('\t', end='')
+            print(each_item)
+
+movies = ['The Holy Grail', 1975, 'Terry Jones & Terry Gilliam', 91,
+    ['Graham Chapman', ['Michael Palin', 'John Cleese',
+        'Terry Gilliam', 'Eric Idle', 'Terry Jones']]]
+
+print_lol(movies)
