8074c9be112a5b75408502afcfa86a4424bcfdf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:57 +0000

    Add package pixiedust-twitterdemo1/0.1/pixiedust_twitterdemo1-0.1.tar.gz

---
+++ packages/pixiedust-twitterdemo1/pixiedust_twitterdemo1-0.1.tar.gz/pixiedust_twitterdemo1-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pixiedust_twitterdemo1',
+      version='0.1',
+      description='Pixiedust demo of the Twitter Sentiment Analysis tutorials',
+      url='https://github.com/lgellis/STEM-twitter-wall/tree/master/twitterdemo',
+      install_requires=['pixiedust'],
+      author='David Taieb the genius and slightly hacked by STEM Women',
+      author_email='david_taieb@us.ibm.com',
+      license='Apache 2.0',
+      packages=['pixiedust_twitterdemo1'],
+      include_package_data=True,
+      zip_safe=False)
