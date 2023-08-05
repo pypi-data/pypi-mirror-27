a29336efdb2d3acdebcb868bda0bf648b76b8ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:19 +0000

    Add package tensorci/0.0.1/tensorci-0.0.1-py2.7.egg

---
+++ packages/tensorci/tensorci-0.0.1-py2.7.egg/tensorci/config.py
@@ -0,0 +1,44 @@
+import os
+from utils.env import env
+
+
+class Config:
+  DEBUG = True
+
+
+class ProdConfig(Config):
+  DEBUG = False
+
+  def __init__(self):
+    self.DOMAIN = os.environ.get('DOMAIN') or 'app.tensorci.com'
+    self.API_URL = 'http://{}/api'.format(self.DOMAIN)
+
+
+class StagingConfig(Config):
+  DEBUG = False
+
+  def __init__(self):
+    self.DOMAIN = os.environ.get('DOMAIN') or 'staging.app.tensorci.com'
+    self.API_URL = 'http://{}/api'.format(self.DOMAIN)
+
+
+class DevConfig(Config):
+
+  def __init__(self):
+    self.DOMAIN = os.environ.get('DOMAIN') or 'dev.app.tensorci.com'
+    self.API_URL = 'http://{}/api'.format(self.DOMAIN)
+
+
+class TestConfig(Config):
+
+  def __init__(self):
+    self.DOMAIN = os.environ.get('DOMAIN') or 'localhost'
+    self.API_URL = 'http://{}/api'.format(self.DOMAIN)
+
+
+def get_config():
+  config_class = globals().get('{}Config'.format(env().capitalize()))
+  return config_class()
+
+
+config = get_config()
