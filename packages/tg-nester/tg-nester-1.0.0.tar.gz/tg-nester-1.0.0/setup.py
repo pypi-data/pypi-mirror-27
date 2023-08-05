a2019103ac92a6aa80ce28b3d7618b922aef362b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:42 +0000

    Add package tg-nester/1.0.0/tg-nester-1.0.0.tar.gz

---
+++ packages/tg-nester/tg-nester-1.0.0.tar.gz/tg-nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+    name            = 'tg-nester',
+    version         = '1.0.0',
+    py_modules      = ['tg-nester'],
+    author          = 'tonnytg',
+    author_email    = 'tonnytg@gmail.com',
+    description     = 'A simple printer list',
+)
---
+++ packages/tg-nester/tg-nester-1.0.0.tar.gz/tg-nester-1.0.0/tg-nester.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Este módulo é somente para teste
+"""
+
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
