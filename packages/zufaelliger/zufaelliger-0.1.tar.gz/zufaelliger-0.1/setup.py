f759f81c9ab82d0756b2b2032c519666c523aa49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:40 +0000

    Add package zufaelliger/0.1/zufaelliger-0.1.tar.gz

---
+++ packages/zufaelliger/zufaelliger-0.1.tar.gz/zufaelliger-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='zufaelliger',
+      version='0.1',
+      description='Der komischsten Witz im Welt',
+      url='https://github.com/alexbmp/zufaelliger',
+      author='Fliegender Kirkus',
+      author_email='sm.alex.choi@gmail.com',
+      license='MIT',
+      packages=['zufaelliger'],
+      zip_safe=False)
+
+      
---
+++ packages/zufaelliger/zufaelliger-0.1.tar.gz/zufaelliger-0.1/zufaelliger/__init__.py
@@ -0,0 +1,4 @@
+def joke():
+    return (u'Wenn ist das Nunst\u00ffcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gespurt.')
+
