a38125ce14cf9973361b0080dac14b5af6dd0a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:18 +0000

    Add package tablo/1.0.1/tablo-1.0.1.tar.gz

---
+++ packages/tablo/tablo-1.0.1.tar.gz/tablo-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='tablo',
+    version='1.0.1',
+    author='khardi',
+    author_email='kharchilindanila@gmail.com',
+    url='https://khardi.github.io/',
+    description='Simple text table with separator'
+)
