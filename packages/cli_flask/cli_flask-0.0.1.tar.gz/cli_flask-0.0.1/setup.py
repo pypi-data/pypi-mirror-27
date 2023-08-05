8119e5d91dd1cd8fdb2c5d5ddaddb00e7ba29b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:34 +0000

    Add package cli_flask/0.0.1/cli_flask-0.0.1.tar.gz

---
+++ packages/cli_flask/cli_flask-0.0.1.tar.gz/cli_flask-0.0.1/setup.py
@@ -0,0 +1,54 @@
+from distutils.core import setup
+import re
+
+def find_version(fname):
+    '''Attempts to find the version number in the file names fname.
+    Raises RuntimeError if not found.
+    '''
+    version = ''
+    with open(fname, 'r') as fp:
+        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+        for line in fp:
+            m = reg.match(line)
+            if m:
+                version = m.group(1)
+                break
+    if not version:
+        raise RuntimeError('Cannot find version information')
+    return version
+
+__version__ = find_version('src/__init__.py')
+
+with open("README.md") as f:
+        long_description=f.read()
+
+setup(name='cli_flask',
+      version=__version__,
+      description='Flask cli (command line interface) for Flask application based project',
+      long_description=long_description,
+      licence="MIT",
+      author='Vijay ',
+      author_email='struts2spring@gmail.com',
+      url='https://github.com/struts2spring/flask_cli',
+      zip_safe=False,
+      platforms='any',
+      packages=['src', ],
+      classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      install_requires=['Click','colorama'],
+      scripts=[],
+      entry_points={
+            'console_scripts':['fl = src.main:cli']
+      },
+
+      
+
+     )
