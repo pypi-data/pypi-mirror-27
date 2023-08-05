3805e50297b690c9ddc802c02c48c2d780cd8505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:48 +0000

    Add package fpr-infra/0.1.13/fpr_infra-0.1.13-py2.py3-none-any.whl

---
+++ packages/fpr-infra/fpr_infra-0.1.13-py2.py3-none-any.whl/fpr_infra/modules/nginx.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+from pyinfra.api import deploy
+from pyinfra.modules import files, server
+from fpr_infra.modules import files as fpr_files
+
+
+@deploy("Enable nginx application configuration")
+def enable_conf(state, host, conf_file, dest=None):
+    dest = dest or conf_file
+
+    # set available conf
+    if host.fact.file(f"/etc/nginx/sites-available/{dest}"):
+        server.shell(
+            state, host,
+            f"rm /etc/nginx/sites-available/{dest}",
+            sudo=True
+        )
+    fpr_files.move(
+        state, host,
+        conf_file, f"/etc/nginx/sites-available/{dest}",
+        sudo=True
+    )
+
+    # set available conf as enabled
+    if host.fact.file(f"/etc/nginx/sites-enabled/{dest}"):
+        server.shell(
+            state, host,
+            f"rm /etc/nginx/sites-enabled/{dest}",
+            sudo=True
+        )
+    files.link(
+        state, host,
+        {f"Enable {dest} site on http"},
+        f"/etc/nginx/sites-enabled/{dest}", f"/etc/nginx/sites-available/{dest}",
+        sudo=True
+    )
