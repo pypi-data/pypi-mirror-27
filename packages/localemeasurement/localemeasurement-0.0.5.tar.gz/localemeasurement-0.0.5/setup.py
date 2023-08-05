725034d0fa048f3d3816b643f6120b215f0bce5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:13 +0000

    Add package localemeasurement/0.0.5/localemeasurement-0.0.5.tar.gz

---
+++ packages/localemeasurement/localemeasurement-0.0.5.tar.gz/localemeasurement-0.0.5/setup.py
@@ -0,0 +1,27 @@
+"""
+Python Locale Measurement
+========================
+Developed by: Chirag Rathod (Srce Cde)
+Email: chiragr83@gmail.com
+========================
+"""
+from setuptools import setup
+
+setup(name='localemeasurement',
+      version='0.0.5',
+      description='locale based measurement',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'],
+      keywords = ['locale', 'measurement', 'unit', 'conversion'],\
+      url='https://github.com/srcecde/locale-measurement',
+      author='Srce Cde (Chirag Rathod)',
+      author_email='chiragr83@gmail.com',
+      license='MIT',
+      packages=['localemeasurement'],
+      install_requires=[
+          'measurement', 'quantities', 'babel', 'pytz', 'simple-date'
+      ],
+      include_package_data=True,
+      zip_safe=False)
