96acfdd8553fb17d6381303ddba4779f41b0b2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:51 +0000

    Add package exchangelib/1.10.6/exchangelib-1.10.6.tar.gz

---
+++ packages/exchangelib/exchangelib-1.10.6.tar.gz/exchangelib-1.10.6/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+To upload to PyPI:
+   python setup.py sdist bdist_wheel upload
+"""
+import io
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    with io.open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='exchangelib',
+    version='1.10.6',
+    author='Erik Cederstrand',
+    author_email='erik@cederstrand.dk',
+    description='Client for Microsoft Exchange Web Services (EWS)',
+    long_description=read('README.rst'),
+    license='BSD',
+    keywords='Exchange EWS autodiscover',
+    install_requires=['requests>=2.7', 'requests_ntlm>=0.2.0', 'dnspython>=1.14.0', 'pytz', 'lxml',
+                      'cached_property', 'future', 'six', 'tzlocal', 'python-dateutil', 'pygments'],
+    packages=['exchangelib'],
+    tests_require=['PyYAML', 'requests_mock', 'psutil'],
+    test_suite='tests',
+    zip_safe=False,
+    url='https://github.com/ecederstrand/exchangelib',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Communications',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
