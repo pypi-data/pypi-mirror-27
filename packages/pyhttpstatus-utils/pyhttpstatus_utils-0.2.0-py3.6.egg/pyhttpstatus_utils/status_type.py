555dccca0f121a7bc040aa68cc1093a7e084b0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:31 +0000

    Add package pyhttpstatus-utils/0.2.0/pyhttpstatus_utils-0.2.0-py3.6.egg

---
+++ packages/pyhttpstatus-utils/pyhttpstatus_utils-0.2.0-py3.6.egg/pyhttpstatus_utils/status_type.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  @namespace pyhttpstatus_utils
+"""
+HTTP Status Types
+"""
+
+
+class HttpStatusType(object):
+    """HTTP Status Code Types
+    """
+
+    INFORMATIONAL = 'Informational'
+    SUCCESSFUL = 'Successful'
+    REDIRECTION = 'Redirection'
+    CLIENT_ERROR = 'Client Error'
+    SERVER_ERROR = 'Server Error'
