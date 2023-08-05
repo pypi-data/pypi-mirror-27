f421c93e571c6cdf8c4a3748343541d0c79c45f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:55 +0000

    Add package npcompare/0.13.1/npcompare-0.13.1-py2.py3-none-any.whl

---
+++ packages/npcompare/npcompare-0.13.1-py2.py3-none-any.whl/npcompare/fourierseries.py
@@ -0,0 +1,45 @@
+#----------------------------------------------------------------------
+# Copyright 2017 Marco Inacio <pythonpackages@marcoinacio.com>
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, version 3 of the License.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.    If not, see <http://www.gnu.org/licenses/>.
+#----------------------------------------------------------------------
+
+import numpy as np
+
+def fourierseries(x, ncomponents):
+    """Calculate Fourier Series Expansion.
+
+    Parameters
+    ----------
+    x : 1D numpy.array, list or tuple of numbers to calculate
+        fourier series expansion
+    ncomponents : int
+        number of components of the series
+
+    Returns
+    ----------
+    2D numpy.array where each line is the Fourier series expansion of
+    each component of x.
+    """
+    from numpy import sqrt, sin, cos, pi
+    x = np.array(x, ndmin=1)
+    results = np.array(np.empty((x.size, ncomponents)))
+
+    for i in range(x.size):
+        for j in range(ncomponents):
+            if j%2 == 0:
+                results[i, j] = sqrt(2) * sin((j+2) * pi * x[i])
+            else:
+                results[i, j] = sqrt(2) * cos((j+1) * pi * x[i])
+
+    return(results)
