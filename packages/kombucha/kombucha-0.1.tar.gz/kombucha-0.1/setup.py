c0bb024bbb8e6615fa8bd43ea94f589e9964228b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:45 +0000

    Add package kombucha/0.1/kombucha-0.1.tar.gz

---
+++ packages/kombucha/kombucha-0.1.tar.gz/kombucha-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kombucha',
+      version='0.1',
+      description='Drink a dict',
+      url='https://github.com/eL0ck/Kombucha',
+      author='eL0ck',
+      author_email='tpj800@gmail.com',
+      license='MIT',
+      packages=['kombucha'],
+      zip_safe=False)
