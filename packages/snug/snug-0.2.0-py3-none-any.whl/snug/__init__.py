f34af7eaa59f9858335d0eaf0f612feb9ba30fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:16 +0000

    Add package snug/0.2.0/snug-0.2.0-py3-none-any.whl

---
+++ packages/snug/snug-0.2.0-py3-none-any.whl/snug/__init__.py
@@ -0,0 +1,7 @@
+"""a toolkit for wrapping REST APIs"""
+from .http import *  # noqa
+from .query import *  # noqa
+
+from . import http, load, query, xml  # noqa
+
+from .__about__ import __version__, __author__, __copyright__  # noqa
