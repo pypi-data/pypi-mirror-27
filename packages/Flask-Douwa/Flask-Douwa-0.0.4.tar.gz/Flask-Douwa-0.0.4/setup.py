1b39916245b3b0326ca3f734380b5cc28e710ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package Flask-Douwa/0.0.4/Flask-Douwa-0.0.4.tar.gz

---
+++ packages/Flask-Douwa/Flask-Douwa-0.0.4.tar.gz/Flask-Douwa-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+setup(
+    name='Flask-Douwa',
+    version='0.0.4',
+    license='MIT',
+    author='jhchen',
+    author_email='watasihakamidesu@sina.cn',
+    description=u'一个flask插件',
+    long_description=__doc__,
+    packages=['flask_douwa', 'flask_douwa.rpc', 'flask_douwa.protoparser'],
+    zip_safe=False,
+    #include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+    ],
+    # classifiers=[
+    #     'Framework :: Flask',
+    #     'Natural Language :: English',
+    #     'Environment :: Web Environment',
+    #     'Intended Audience :: Developers',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Operating System :: OS Independent',
+    #     'Programming Language :: Python',
+    #     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    #     'Topic :: Software Development :: Libraries :: Python Modules'
+    # ]
+)
