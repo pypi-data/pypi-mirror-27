7861421e832e16489540383c72a899b24dcfb249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:20 +0000

    Add package mxlearn/0.1.0/mxlearn-0.1.0-py2.py3-none-any.whl

---
+++ packages/mxlearn/mxlearn-0.1.0-py2.py3-none-any.whl/mxlearn/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for mxlearn."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for mxlearn."""
+    click.echo("Replace this message by putting your code into "
+               "mxlearn.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
