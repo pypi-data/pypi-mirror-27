f0e02c81726e3838d932b887028b4e3769061194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:03 +0000

    Add package nester-lkx/1.4.0/nester-lkx-1.4.0.tar.gz

---
+++ packages/nester-lkx/nester-lkx-1.4.0.tar.gz/nester-lkx-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="nester-lkx",
+    version="1.4.0",
+    py_modules=["nester"],
+    author="Liu KangXin",
+    author_email="robinliu97@outlook.com",
+    url="http://www.sjtu.edu.cn",
+    description="A simple printer of nested lists"
+)
