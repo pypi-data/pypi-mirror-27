f233dda6b0471a38fd4d7c7b2f53ddd11118ec32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:29 +0000

    Add package pyne-xmas-elves/0.1/pyne_xmas_elves-0.1-py3-none-any.whl

---
+++ packages/pyne-xmas-elves/pyne_xmas_elves-0.1-py3-none-any.whl/pyne_xmas_elves-0.1.data/scripts/runserver.py
@@ -0,0 +1,16 @@
+#!/Users/scott/Documents/xmas-elves/venv/bin/python
+import subprocess
+import sys
+
+
+def run():
+    if not getattr(sys, 'base_prefix', ''):
+        print('This must be run from within an virtualenv')
+        exit(1)
+
+    print('Starting server...')
+    subprocess.run(['python', 'server/manage.py', 'runserver'])
+
+
+if __name__ == '__main__':
+    run()
