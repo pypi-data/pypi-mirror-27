ef56e44f4abc581d840d6e8ba5244849b300852c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:57 +0000

    Add package tf-run-manager/1.0.6/tf-run-manager-1.0.6.tar.gz

---
+++ packages/tf-run-manager/tf-run-manager-1.0.6.tar.gz/tf-run-manager-1.0.6/setup.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(name='tf-run-manager',
+      version='1.0.6',
+      description='A utility for tracking and reproducing Tensorflow runs.',
+      long_description=long_description,
+      url='https://github.com/lobachevzky/tf-run-manager',
+      author='Ethan Brooks',
+      author_email='ethanbrooks@gmail.com',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+      keywords='tensorflow utilities development',
+      py_modules=['runs'],
+      entry_points={
+          'console_scripts': [
+              'runs = runs:main',
+          ],
+      },
+      install_requires=[
+          'paramiko==2.3.1',
+          'GitPython==2.1.7',
+          'termcolor==1.1.0',
+          'PyYAML==3.12',
+          'tabulate==0.8.1',
+      ])
