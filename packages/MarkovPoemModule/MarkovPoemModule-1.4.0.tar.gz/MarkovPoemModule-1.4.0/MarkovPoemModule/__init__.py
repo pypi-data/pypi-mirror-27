70f397ba9019514f9d24dd14889dc494d2d468fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:27 +0000

    Add package MarkovPoemModule/1.4.0/MarkovPoemModule-1.4.0.tar.gz

---
+++ packages/MarkovPoemModule/MarkovPoemModule-1.4.0.tar.gz/MarkovPoemModule-1.4.0/MarkovPoemModule/__init__.py
@@ -0,0 +1,3 @@
+
+__author__ = "Vladya"
+__version__ = "1.4.0"
---
+++ packages/MarkovPoemModule/MarkovPoemModule-1.4.0.tar.gz/MarkovPoemModule-1.4.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import MarkovPoemModule
+
+setup(
+    name="MarkovPoemModule",
+    version=MarkovPoemModule.__version__,
+    author=MarkovPoemModule.__author__,
+    url="https://github.com/NyashniyVladya/PoemModule",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3.6"
+    ],
+    install_requires=[
+        "MarkovTextGenerator>=1.5.8",
+        "bs4",
+        "selenium"
+    ],
+    keywords=(
+        "vladya markovgenerator markov_generator MarkovPoemModule "
+        "markov_chain poem_module poem_generator poem"
+    ),
+    description=(
+        "A poetry generator, based on the text generator on the Markov chains "
+        "(MarkovTextGenerator)."
+    ),
+    long_description=(
+        "Генератор стихов, на основе генератора текста по цепям Маркова "
+        "(MarkovTextGenerator)."
+    )
+)
