d4a9aac9cc72d2ae6acb1b9402060730d450e7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:27 +0000

    Add package reportgen/0.1.6/reportgen-0.1.6.tar.gz

---
+++ packages/reportgen/reportgen-0.1.6.tar.gz/reportgen-0.1.6/reportgen/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8
+
+from reportgen.report import Report
+from reportgen.report import df_to_table
+from reportgen.report import df_to_chartdata
+from reportgen.report import plot_table
+from reportgen.report import plot_textbox
+from reportgen.report import plot_chart
+from reportgen.report import plot_picture
+from reportgen.report import slides_data_gen
+from reportgen import questionnaire
+from reportgen.analysis import AnalysisReport
+from reportgen.analysis import describe
+from reportgen.analysis import plot
+
+__version__ = '0.1.6'
