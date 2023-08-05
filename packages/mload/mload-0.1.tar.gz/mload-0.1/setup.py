e4a7f0ab838b0487f4f44c59ce42e8cd58ee78cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:42 +0000

    Add package mload/0.1/mload-0.1.tar.gz

---
+++ packages/mload/mload-0.1.tar.gz/mload-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mload',
+      version='0.1',
+      description='Machine Learning for Outlier and Anomaly Detection.',
+      url='http://github.com/chaosimpler/aodetect',
+      author='Wang Chao',
+      author_email='chaosimpler@gmail.com',
+      license='MIT',
+      packages=['mload'],
+      zip_safe=False)   
