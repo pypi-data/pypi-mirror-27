4af9241a95066e7e6ac818ce3995a72162c9bb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:18 +0000

    Add package retype/17.12.0/retype-17.12.0.tar.gz

---
+++ packages/retype/retype-17.12.0.tar.gz/retype-17.12.0/types/tests/test_retype.pyi
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from typing import Type, TypeVar
+
+_E = TypeVar('_E', bound=Exception)
+
+class RetypeTestCase(TestCase):
+    def assertReapply(self, pyi_txt: str, src_txt: str, expected_txt: str, *, incremental: bool = False) -> None: ...
+    def assertReapplyVisible(self, pyi_txt: str, src_txt: str, expected_txt: str, *, incremental: bool = False) -> None: ...
+    def assertReapplyRaises(self, pyi_txt: str, src_txt: str, expected_exception: Type[_E], *, incremental: bool = False) -> _E: ...
