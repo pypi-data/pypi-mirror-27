b224dc48f912bbddb22288ba5708ec111f0f2d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package tlseparation/1.1.5.1/tlseparation-1.1.5.1-py2.py3-none-any.whl

---
+++ packages/tlseparation/tlseparation-1.1.5.1-py2.py3-none-any.whl/tlseparation/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Matheus Boni Vicari (matheus.boni.vicari@gmail.com)
+"""
+
+__all__ = ['classification', 'utility']
+
+from auto_separation_2 import wlseparate_abs
+from auto_separation_2 import wlseparate_ref
+from auto_separation_2 import wlseparate_ref_voting
+
+from tlseparation.auto_separation_2 import wlseparate_abs
+from tlseparation.auto_separation_2 import wlseparate_ref
+from tlseparation.auto_separation_2 import wlseparate_ref_voting
+
+from classification import *
+from utility import *
