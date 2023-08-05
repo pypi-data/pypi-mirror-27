04c91ccac2b16819f62bb94cecfa56feb3cfbea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:32 +0000

    Add package plantgateway/0.4.0/plantgateway-0.4.0.tar.gz

---
+++ packages/plantgateway/plantgateway-0.4.0.tar.gz/plantgateway-0.4.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##############################################
+#
+# This is open source software licensed under the Apache License 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+##############################################
+
+import sys
+from distutils.core import setup
+
+install_requires = ['bluepy==1.1.4', "paho-mqtt", 'pyyaml']
+if sys.version_info < (3, 0):
+    install_requires.append('mock')
+
+
+setup(name='plantgateway',
+      version='0.4.0',
+      description='Bluetooth to mqtt gateway for Xiaomi Mi plant sensors',
+      author='Christian Kühnel',
+      author_email='christian.kuehnel@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['plantgw'],
+      install_requires=install_requires,
+      scripts=['plantgw/plantgateway'],
+      )
