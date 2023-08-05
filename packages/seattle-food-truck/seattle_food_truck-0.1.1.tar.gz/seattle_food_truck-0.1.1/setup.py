648014c61813a513f8746bc24b7b00bcc27c6c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:33 +0000

    Add package seattle-food-truck/0.1.1/seattle_food_truck-0.1.1.tar.gz

---
+++ packages/seattle-food-truck/seattle_food_truck-0.1.1.tar.gz/seattle_food_truck-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seattle_food_truck',
+    version='0.1.1',
+    author='clintval',
+    author_email='valentine.clint@gmail.com',
+    url='https://github.com/clintval/seattle-food-truck',
+    download_url='https://github.com/clintval/seattle-food-truck/releases/tag/0.1.1',
+    license='MIT',
+    keywords=['seattle', 'food', 'truck'],
+    install_requires=[
+        'click',
+        'python-dateutil',
+        'requests',
+        'terminaltables'],
+    scripts=['scripts/sft'],
+    packages=find_packages())
+
+print("""
+-------------------------------------------
+ seattle_food_truck installation complete!
+-------------------------------------------
+""")
