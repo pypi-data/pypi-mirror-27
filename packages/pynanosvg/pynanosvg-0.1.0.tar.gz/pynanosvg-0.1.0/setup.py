62e39099a0b8880ee1ca56158262f03c01c3361e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:36 +0000

    Add package pynanosvg/0.1.0/pynanosvg-0.1.0.tar.gz

---
+++ packages/pynanosvg/pynanosvg-0.1.0.tar.gz/pynanosvg-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import os
+import sys
+
+
+if sys.version_info < (3, 5) and os.name == 'nt':
+    print("Nanosvg cannot be built on Python 3.4 or below on Windows.")
+    sys.exit(1)
+
+ext = [Extension('svg', sources=['svg/__init__.pyx'],
+                 include_dirs=['nanosvg/src/', 'pynanosvg/'])]
+
+setup(
+    name="pynanosvg",
+    version="0.1.0",
+    description="Wrapper around nanosvg",
+    author="Ethan Smith",
+    author_email="ethan@ethanhs.me",
+    url="https://github.com/ethanhs/pynanosvg",
+    license="MIT",
+    ext_modules=cythonize(ext),
+    packages=['svg_stubs'],
+    package_data={'svg_stubs': ['py.typed', 'svg.pyi']}
+)
