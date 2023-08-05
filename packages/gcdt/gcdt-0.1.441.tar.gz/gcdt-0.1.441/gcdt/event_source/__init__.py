d8face62c7de3b888e45f57fc7b99ca3829010a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:52 +0000

    Add package gcdt/0.1.441/gcdt-0.1.441.tar.gz

---
+++ packages/gcdt/gcdt-0.1.441.tar.gz/gcdt-0.1.441/gcdt/event_source/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+from .base import EventSource
+from .cloudwatch import CloudWatchEventSource
+from .dynamodb_stream import DynamoDBStreamEventSource
+from .kinesis import KinesisEventSource
+from .s3 import S3EventSource
+from .sns import SNSEventSource
+from .cloudfront import CloudFrontEventSource
