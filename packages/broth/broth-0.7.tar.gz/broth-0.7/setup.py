42f1018200a62792b4d71bc44fe220f835268051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:18 +0000

    Add package broth/0.7/broth-0.7.tar.gz

---
+++ packages/broth/broth-0.7.tar.gz/broth-0.7/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name = 'broth',
+  packages = ['broth'],
+  package_dir = {'broth': 'broth'},
+  package_data = {'broth': ['__init__.py']},
+  version = '0.7',
+  description = 'Convenient Wrapper for Beautiful Soup',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/broth',
+  download_url = 'https://github.com/DanielJDufour/broth/tarball/download',
+  keywords = ['extraction', 'BeautifulSoup','python'],
+  classifiers = [],
+  install_requires=["beautifulsoup4"]
+)
