6eabe53b6f3b333870fbd2c9beb787b7d1acaa1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:27 +0000

    Add package simNum/3.1/simNum-3.1.tar.gz

---
+++ packages/simNum/simNum-3.1.tar.gz/simNum-3.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'simNum',
+  packages = ['simNum'],
+  version = '3.1',
+  description = 'A number simulator package for Normal, Poisson or Binomial distributions.',
+  author = 'Adam Round',
+  author_email = 'adamround@hotmail.com',
+  url = 'https://github.com/Roundy123/simNum.git',
+)
---
+++ packages/simNum/simNum-3.1.tar.gz/simNum-3.1/simNum/__init__.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def simDist(samples,distribution,mean=0,stddev=1,lamb=1,trials=None,probability=None):
+    if distribution == 'normal':
+        return np.random.normal(size=samples,loc=mean,scale=stddev)
+    if distribution == 'poisson':
+        return np.random.poisson(size=samples,lam=lamb)
+    if distribution == 'binomial':
+        if trials == None or probability == None:
+            print('error: please include trials and probability arguments for binomial distribution')
+        else:
+            return np.random.binomial(size=samples,n=trials,p=probability)
