1ca8d05c3f7e57cb491d51c7576d1db956494b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package linotype/0.2.0/linotype-0.2.0.tar.gz

---
+++ packages/linotype/linotype-0.2.0.tar.gz/linotype-0.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="linotype",
+    version="0.2.0",
+    description="Automatically format help messages.",
+    long_description="See GitHub page for more details.",
+    url="https://github.com/lostatc/linotype",
+    author="Garrett Powell",
+    author_email="garrett@gpowell.net",
+    license="GPLv3",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Documentation"],
+    install_requires=["sphinx"],
+    python_requires=">=3.5",
+    tests_require=["pytest"],
+    extras_require={
+        "Windows support": ["colorama"]},
+    packages=["linotype"])
