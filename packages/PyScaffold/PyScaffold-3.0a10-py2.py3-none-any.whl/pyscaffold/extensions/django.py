1c71a9a4ef151954fd04b059b2b72fc08fa4d79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package PyScaffold/3.0a10/PyScaffold-3.0a10-py2.py3-none-any.whl

---
+++ packages/PyScaffold/PyScaffold-3.0a10-py2.py3-none-any.whl/pyscaffold/extensions/django.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Extension that creates a base structure for the project using django-admin.py.
+"""
+from __future__ import absolute_import
+
+from six import raise_from
+
+from .. import shell
+
+
+def augment_cli(parser):
+    """Add an option to parser that enables the Travis extension.
+
+    Args:
+        parser (argparse.ArgumentParser): CLI parser object
+    """
+
+    parser.add_argument(
+        "--django",
+        dest="extensions",
+        action="append_const",
+        const=extend_project,
+        help="generate Django project files")
+
+
+def extend_project(actions, helpers):
+    """Register hooks to generate project using django-admin."""
+
+    # `get_default_options` uses passed options to compute derived ones,
+    # so it is better to prepend actions that modify options.
+    actions = helpers.register(actions, enforce_django_options,
+                               before='get_default_options')
+    # `apply_update_rules` uses CWD information,
+    # so it is better to prepend actions that modify it.
+    actions = helpers.register(actions, create_django_proj,
+                               before='apply_update_rules')
+
+    return actions
+
+
+def enforce_django_options(struct, opts):
+    """Make sure options reflect the django usage."""
+    opts['package'] = opts['project']  # required by Django
+    opts['force'] = True
+    opts.setdefault('requirements', []).append('django')
+
+    return struct, opts
+
+
+def create_django_proj(struct, opts):
+    """Creates a standard Django project with django-admin.py
+
+    Args:
+        scaffold (pyscaffold.api.Scaffold): representation of all the actions
+        that can be performed by PyScaffold.
+
+    Raises:
+        :obj:`RuntimeError`: raised if django-admin.py is not installed
+    """
+    try:
+        shell.django_admin('--version')
+    except Exception as e:
+        raise raise_from(DjangoAdminNotInstalled, e)
+
+    shell.django_admin('startproject', opts['project'],
+                       log=True, pretend=opts.get('pretend'))
+
+    return struct, opts
+
+
+class DjangoAdminNotInstalled(RuntimeError):
+    """This extension depends on the ``django-admin.py`` cli script."""
+
+    DEFAULT_MESSAGE = ("django-admin.py is not installed, "
+                       "run: pip install django")
+
+    def __init__(self, message=DEFAULT_MESSAGE, *args, **kwargs):
+        super(DjangoAdminNotInstalled, self).__init__(message, *args, **kwargs)
---
+++ packages/PyScaffold/PyScaffold-3.0a10-py2.py3-none-any.whl/pyscaffold/extensions/pre_commit.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Extension that generates configuration files for Yelp `pre-commit`_.
+
+.. _pre-commit: http://pre-commit.com
+"""
+from __future__ import absolute_import
+
+from ..templates import pre_commit_config
+
+
+def augment_cli(parser):
+    """Add an option to parser that enables the `pre-commit`_ extension.
+
+    Args:
+        parser (argparse.ArgumentParser): CLI parser object
+
+    .. _pre-commit: http://pre-commit.com
+    """
+
+    parser.add_argument(
+        "--pre-commit",
+        dest="extensions",
+        action="append_const",
+        const=extend_project,
+        help="generate pre-commit configuration file")
+
+
+def extend_project(actions, helpers):
+    """Register an action responsible for adding specific files to project."""
+
+    def add_files(struct, opts):
+        """Add pre-commit configuration files to the project structure."""
+
+        files = {
+            '.pre-commit-config.yaml': (
+                pre_commit_config(opts), helpers.NO_OVERWRITE
+            ),
+        }
+
+        return helpers.merge(struct, {opts['project']: files}), opts
+
+    return helpers.register(actions, add_files)
