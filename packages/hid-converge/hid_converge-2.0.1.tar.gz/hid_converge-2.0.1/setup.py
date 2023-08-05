5a5a1502c40afa104e1ce8f3485fc556771147c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:43 +0000

    Add package hid-converge/2.0.1/hid_converge-2.0.1.tar.gz

---
+++ packages/hid-converge/hid_converge-2.0.1.tar.gz/hid_converge-2.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+	name='hid_converge',
+	version='2.0.1',
+	author='HID',
+	license='Thermofisher Scientific',
+	packages=['authentication',
+			'Queue','QueueModule'],
+	install_requires=['xlsxwriter','requests','simplejson'],
+	dependency_links=['https://pypi.python.org/pypi/XlsxWriter',
+					'https://pypi.python.org/pypi/simplejson',
+					'https://pypi.python.org/pypi/requests'],
+	include_package_data=True,
+	zip_safe=False
+)
