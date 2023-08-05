cc84e2856c4f9bf7a408e1e70b858ffefa55f504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:07 +0000

    Add package odoo10-addon-medical/10.0.1.1.0.99.dev1/odoo10_addon_medical-10.0.1.1.0.99.dev1-py2-none-any.whl

---
+++ packages/odoo10-addon-medical/odoo10_addon_medical-10.0.1.1.0.99.dev1-py2-none-any.whl/odoo/addons/medical/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import medical_abstract_entity
+from . import medical_patient
+from . import res_partner
