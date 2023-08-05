e47c6e0ec1ad0bf1e74e1135cf6994360624b1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:00 +0000

    Add package badwolf/0.12.7/badwolf-0.12.7.tar.gz

---
+++ packages/badwolf/badwolf-0.12.7.tar.gz/badwolf-0.12.7/badwolf/context.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import os
+import uuid
+
+from flask import current_app
+
+from badwolf.utils import to_text
+
+
+class Context(object):
+    """Badwolf build/lint context"""
+    def __init__(self, repository, actor, type, message, source, target=None,
+                 rebuild=False, pr_id=None, nocache=False, clone_depth=50,
+                 skip_lint=False):
+        self.task_id = to_text(uuid.uuid4())
+        self.repository = repository
+        self.repo_owner, self.repo_name = repository.split('/')
+        self.actor = actor
+        self.type = type
+        self.message = message
+        self.source = source
+        self.target = target
+        self.rebuild = rebuild
+        self.pr_id = pr_id
+        # Don't use cache when build Docker image
+        self.nocache = nocache
+        self.clone_depth = clone_depth
+        self.skip_lint = skip_lint
+
+        if 'repository' not in self.source:
+            self.source['repository'] = {'full_name': repository}
+
+        self.clone_path = os.path.join(
+            current_app.config['BADWOLF_REPO_DIR'],
+            self.repo_name,
+            self.task_id,
+        )
