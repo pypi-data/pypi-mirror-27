d5749911b46a3207a58490f3f0bc7b2273dfceb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:21 +0000

    Add package pymaf/0.2/pymaf-0.2.tar.gz

---
+++ packages/pymaf/pymaf-0.2.tar.gz/pymaf-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pymaf',
+      version='0.2',
+      description='Finding Maximum autocorrelation factors',
+      url='http://github.com/mahaugen/pymaf',
+      download_url = 'http://github.com/mahaugen/pymaf/archive/0.2.tar.gz',
+      author='Matz Haugen',
+      author_email='no@thanks.com',
+      license='MIT',
+      packages=['pymaf'],
+      zip_safe=False)
