57bc9d68fcc8fe7cf20cfba15e4a8dc75e2cea7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:10 +0000

    Add package Jitbit-Helpdesk/0.1/Jitbit-Helpdesk-0.1.tar.gz

---
+++ packages/Jitbit-Helpdesk/Jitbit-Helpdesk-0.1.tar.gz/Jitbit-Helpdesk-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Jitbit-Helpdesk',
+    version='0.1',
+    packages=['jitbit-helpdesk',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+    author="Jitbit LP",
+    author_email="info@jitbit.com",
+    url="http://pypi.python.org/pypi/JitbitHelpdesk"
+)
