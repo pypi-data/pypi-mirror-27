27193272c22904224ef2d9c62d45aeb934886395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:13 +0000

    Add package kcmApp/1.5/kcmApp-1.5.tar.gz

---
+++ packages/kcmApp/kcmApp-1.5.tar.gz/kcmApp-1.5/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = 'kcmApp',
+    packages = ['kcmApp'],
+    version = '1.5',
+    description = 'A django App for kcm',
+    author = 'davidtnfsh',
+    author_email = 'davidtnfsh@gmail.com',
+    url = 'https://github.com/udicatnchu/kcmApp',
+    download_url = 'https://github.com/udicatnchu/kcmApp/archive/v1.5.tar.gz',
+    keywords = ['kcmApp'],
+    classifiers = [],
+    license='GPL3.0',
+    install_requires=[
+    ],
+    zip_safe=True,
+)
