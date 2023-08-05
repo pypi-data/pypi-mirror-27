b96847f0edfea364599924ffc34f16860fa559d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:45 +0000

    Add package testqqtest/1/testqqtest-1.tar.gz

---
+++ packages/testqqtest/testqqtest-1.tar.gz/testqqtest-1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+  name = 'testqqtest',
+  packages = find_packages (),
+  version = '1',
+  description = 'reg',
+  author = '<script>alert(1)</script>',
+  url = 'http://erg.erg',
+  keywords = ['lib'],
+  classifiers = []
+)
---
+++ packages/testqqtest/testqqtest-1.tar.gz/testqqtest-1/targetlib/__init__.py
@@ -0,0 +1,2 @@
+def main ():
+	print ("HACKER :3")
