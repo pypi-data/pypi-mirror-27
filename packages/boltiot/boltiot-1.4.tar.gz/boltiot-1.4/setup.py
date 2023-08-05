ff9a03e12cf4543399a37a8e4fffd552c494bb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:39 +0000

    Add package boltiot/1.4/boltiot-1.4.tar.gz

---
+++ packages/boltiot/boltiot-1.4.tar.gz/boltiot-1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = 'boltiot',
+    packages = ['boltiot'],
+    version = 1.4, #Specify a version here
+    description = 'A Python module for communicating with the Bolt Cloud API.', #Add a description here
+    author = 'Inventrom Pvt. Ltd.', #Enter an author for the package
+    author_email  = 'support@boltiot.com', #Author's Email
+    url = 'https://github.com/Inventrom/bolt-api-python', #URL for github repository
+    download = 'https://github.com/Inventrom/bolt-api-python/archive/1.9.tar.gz', #Download URL for github repository. Look at instructions...txt file for this part
+    keywords = ['iot-platform','bolt','bolt-python'], #Add keywords
+    classifiers = [],
+    install_requires=[
+          'requests',
+          'twilio'
+      ],
+)
