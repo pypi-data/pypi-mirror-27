9a4b62417e9bee3749efdbb699b83bb2610cf90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:06 +0000

    Add package confluence-tool/0.0.14/confluence-tool-0.0.14.tar.gz

---
+++ packages/confluence-tool/confluence-tool-0.0.14.tar.gz/confluence-tool-0.0.14/confluence_tool/cli/config.py
@@ -0,0 +1,19 @@
+from .cli import command, arg
+
+@command('config',
+    arg('-p', '--show-password', action="store_true", help="unless specified, password will not be shown")
+    )
+def config(config):
+    """Get or set configuration.
+
+    For configuring the default base
+
+    """
+    if config.get('baseurl'):
+        config.setConfig()
+    else:
+        cfg = config.getConfig()
+        if cfg.get('password') and not config.get('show-password'):
+            cfg['password'] = '******'
+
+        pyaml.p(cfg)
