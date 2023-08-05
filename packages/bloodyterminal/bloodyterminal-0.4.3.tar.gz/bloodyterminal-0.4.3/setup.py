a3e69d4c93541a4e863e72541f6965aaec0484e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:53 +0000

    Add package bloodyterminal/0.4.3/bloodyterminal-0.4.3.tar.gz

---
+++ packages/bloodyterminal/bloodyterminal-0.4.3.tar.gz/bloodyterminal-0.4.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='bloodyterminal',
+    packages=['bloodyterminal'],
+    version='0.4.3',
+    description='A little helper to structure your terminal output',
+    author='TheBloodyScreen',
+    author_email='jaydee@thebloodyscreen.com',
+    license='MIT',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.2',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6'
+    ],
+    install_requires=['colorama'],
+    url='https://github.com/TheBloodyScreen/bloodyterminal/'
+)
