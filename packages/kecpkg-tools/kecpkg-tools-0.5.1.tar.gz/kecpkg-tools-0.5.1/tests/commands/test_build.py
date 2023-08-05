47055c2e69080091ca3ecccfc45a63e1e975d83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:07 +0000

    Add package kecpkg-tools/0.5.1/kecpkg-tools-0.5.1.tar.gz

---
+++ packages/kecpkg-tools/kecpkg-tools-0.5.1.tar.gz/kecpkg-tools-0.5.1/tests/commands/test_build.py
@@ -0,0 +1,26 @@
+import os
+
+from click.testing import CliRunner
+
+from kecpkg.cli import kecpkg
+from kecpkg.utils import get_package_dir
+from tests.utils import temp_chdir, BaseTestCase
+
+
+class TestCommandPurge(BaseTestCase):
+    def test_new_non_interactive(self):
+        pkgname = 'new_pkg'
+
+        with temp_chdir() as d:
+            runner = CliRunner()
+            result = runner.invoke(kecpkg, ['new', pkgname, '--no-venv'])
+            package_dir = get_package_dir(pkgname)
+            self.assertTrue(os.path.exists(package_dir))
+
+            os.chdir(package_dir)
+
+            result = runner.invoke(kecpkg, ['build', pkgname])
+            self.assertEqual(result.exit_code, 0)
+            self.assertExists(os.path.join(package_dir, 'dist'))
+
+
