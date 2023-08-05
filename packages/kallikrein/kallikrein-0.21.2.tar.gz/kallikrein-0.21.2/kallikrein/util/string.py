0db32e0b668f17e7dd94a06897a3095227cbf3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package kallikrein/0.21.2/kallikrein-0.21.2.tar.gz

---
+++ packages/kallikrein/kallikrein-0.21.2.tar.gz/kallikrein-0.21.2/kallikrein/util/string.py
@@ -0,0 +1,40 @@
+from typing import Any
+
+from hues import huestr
+
+from amino import __
+
+
+def green(msg: Any) -> str:
+    return huestr(str(msg)).green.colorized
+
+
+def red(msg: Any) -> str:
+    return huestr(str(msg)).red.colorized
+
+
+def yellow(msg: Any) -> str:
+    return huestr(str(msg)).yellow.colorized
+
+
+def blue(msg: Any) -> str:
+    return huestr(str(msg)).blue.colorized
+
+
+def magenta(msg: Any) -> str:
+    return huestr(str(msg)).magenta.colorized
+
+indent = __.map(' {}'.format)
+
+green_check = green('✓')
+
+red_cross = red('✗')
+
+yellow_clock = yellow('⌚')
+
+green_plus = green('+')
+
+red_minus = red('-')
+
+__all__ = ('indent', 'green_check', 'red_cross', 'green', 'red', 'yellow_clock', 'green_plus', 'red_minus', 'blue',
+           'magenta')
---
+++ packages/kallikrein/kallikrein-0.21.2.tar.gz/kallikrein-0.21.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version_parts = (0, 21, 2)
+version = '.'.join(map(str, version_parts))
+
+setup(
+    name='kallikrein',
+    description='spec framework',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/kallikrein',
+    packages=find_packages(exclude=['unit', 'unit.*']),
+    install_requires=[
+        'golgi~=1.10.0',
+        'hues',
+    ],
+    entry_points={
+        'console_scripts': [
+            'klk = kallikrein.run.cli:klk',
+        ],
+    },
+)
