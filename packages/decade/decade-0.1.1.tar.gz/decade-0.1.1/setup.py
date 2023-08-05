fcddb3872884fe0fe6d607eb88f20544c3f86530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:06 +0000

    Add package decade/0.1.1/decade-0.1.1.tar.gz

---
+++ packages/decade/decade-0.1.1.tar.gz/decade-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "decade",
+    version = "0.1.1",
+    keywords = ("pip", "decade", "rzhu"),
+    description = "debug configurator sdk",
+    long_description = "Pycharm remote debug auto configurator sdk",
+    license = "Splunk Licence",
+
+    url = "https://github.com/CarolineZhu/decade",
+    author = "rzhu",
+    author_email = "rzhu@splunk.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["paramiko"],
+    entry_points = {
+        'console_scripts': ['decade = decade.localentry:main']
+    }
+)
