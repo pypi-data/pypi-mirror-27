23ba08da26ecb22533f67171e83f61fa00e1d2d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:28 +0000

    Add package mortasoft/0.12/mortasoft-0.12.tar.gz

---
+++ packages/mortasoft/mortasoft-0.12.tar.gz/mortasoft-0.12/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-11-28 10:01:39
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+from setuptools import setup
+
+setup(name='mortasoft',
+      version='0.12',
+      description='Librerias de Mortasoft.xyz',
+      url='http://github.com/mortasoft',
+      author='Mortasoft',
+      author_email='mortasoft@gmail.com',
+      license='MIT',
+      packages=['mortasoft'],
+      install_requires=[
+          'glob',
+      ],
+      zip_safe=False)
