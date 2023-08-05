b82a1abc5ffd0160e5c4b1bf05fc4a8b5444c586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:01 +0000

    Add package nester-lkx/1.3.1/nester-lkx-1.3.1.tar.gz

---
+++ packages/nester-lkx/nester-lkx-1.3.1.tar.gz/nester-lkx-1.3.1/nester.py
@@ -0,0 +1,15 @@
+"""这是“nester.py”模块，提供了一个名为print_lol()的函数用来打印列表，其中包含或不包含嵌套列表。"""
+
+
+def print_lol(the_list, indent=False, level=0):
+    """第一个参数，名为“the_list”，这可以是任何Python列表（包含或不包含嵌套列表），所提供列表中的各个数据项会（递归地）打印到屏幕上，而且各占一行。
+    第二个参数，名为“indent”，用来决定是否缩进，默认不使用。
+    第三个参数，名为“level”，用来决定遇到嵌套列表时插入制表符的个数，默认插入0个。"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level + 1)
+        else:
+            if indent == True:
+                for tab_stop in range(level):
+                    print("\t", end="")
+            print(each_item)
---
+++ packages/nester-lkx/nester-lkx-1.3.1.tar.gz/nester-lkx-1.3.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="nester-lkx",
+    version="1.3.1",
+    py_modules=["nester"],
+    author="Liu KangXin",
+    author_email="robinliu97@outlook.com",
+    url="http://www.sjtu.edu.cn",
+    description="A simple printer of nested lists"
+)
