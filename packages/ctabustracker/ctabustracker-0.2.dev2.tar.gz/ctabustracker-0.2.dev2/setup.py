a46179ff508a9c62fdb13d2af1401d79bef590c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:21 +0000

    Add package ctabustracker/0.2.dev2/ctabustracker-0.2.dev2.tar.gz

---
+++ packages/ctabustracker/ctabustracker-0.2.dev2.tar.gz/ctabustracker-0.2.dev2/setup.py
@@ -0,0 +1,22 @@
+#!/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ctabustracker',
+    version='0.2dev2',
+    description=("A python wrapper for the Chicago Transit Authority's"
+                 ' Bustracker API.'),
+    long_description='',
+    url='https://github.com/tsaylor/ctabustracker',
+    author='Tim Saylor',
+    author_email='tim.saylor@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'lxml'
+    ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4'
+)
