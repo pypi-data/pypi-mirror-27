abcad2dadb6d68af9d2c48dbd7fe78fbea6249e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:43 +0000

    Add package eth-keys/0.1.0b1/eth-keys-0.1.0b1.tar.gz

---
+++ packages/eth-keys/eth-keys-0.1.0b1.tar.gz/eth-keys-0.1.0b1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+setup(
+    name='eth-keys',
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version='0.1.0-beta.1',
+    description="""Common API for Ethereum key operations.""",
+    long_description_markdown_filename='README.md',
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/ethereum/eth-keys',
+    include_package_data=True,
+    setup_requires=['setuptools-markdown'],
+    install_requires=[
+        "eth-utils>=0.5.0",
+        "cytoolz>=0.8.2",
+    ],
+    py_modules=['eth_keys'],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
