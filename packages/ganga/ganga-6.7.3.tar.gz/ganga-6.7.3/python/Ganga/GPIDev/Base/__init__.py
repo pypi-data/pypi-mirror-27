a7776395b772048a3c866bcdb9e820e0acbdcf9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:50 +0000

    Add package ganga/6.7.3/ganga-6.7.3.tar.gz

---
+++ packages/ganga/ganga-6.7.3.tar.gz/ganga-6.7.3/python/Ganga/GPIDev/Base/__init__.py
@@ -0,0 +1,7 @@
+"""
+Definition of GPI base objects and proxies.
+"""
+
+from Ganga.GPIDev.Base.Objects import GangaObject
+from Ganga.Core.exceptions import GangaException
+from Ganga.GPIDev.Base.Proxy import GangaAttributeError, ProtectedAttributeError, ReadOnlyObjectError
