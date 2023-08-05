c421c8ef111f179752c5f42bc42a527e9e28ea33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:19 +0000

    Add package vignesh_first/0.2/vignesh_first-0.2.tar.gz

---
+++ packages/vignesh_first/vignesh_first-0.2.tar.gz/vignesh_first-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='vignesh_first',    
+    version='0.2',	
+	author = 'Vignesh Ravichandran',
+	author_email = 'vickykct@gmail.com',
+    scripts=['vignesh_first']         
+)
+
