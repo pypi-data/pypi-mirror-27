e81ec914af4fa3aa08b25283a73e5c56d28d2302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:38 +0000

    Add package ucanSystec/0.2.1/ucanSystec-0.2.1.tar.gz

---
+++ packages/ucanSystec/ucanSystec-0.2.1.tar.gz/ucanSystec-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ucanSystec',
+    packages=['ucanSystec'],
+    version='0.2.1',
+    url='https://github.com/SMFSW/python-ucanSystec',
+    license='MIT',
+    author='SMFSW',
+    author_email='xgarmanboziax@gmail.com',
+    description='Systec usb/can dll bindings',
+    keywords=['Communication', 'CAN', 'Systec', 'DLL']
+)
