f54bb98db2f4bc0e37ac4582b8655208b03a55f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:55 +0000

    Add package Chenzhuonester/1.1.0/Chenzhuonester-1.1.0.tar.gz

---
+++ packages/Chenzhuonester/Chenzhuonester-1.1.0.tar.gz/Chenzhuonester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name            ='Chenzhuonester',
+    version         ='1.1.0',
+    py_modules      =['chenzhuonest'],
+    author          ='chenzhuo',
+    author_email    ='761300102@qq.com',
+    url             ='http://www.headfirstlabs.com',
+    description     ='A simple printer of nested lists',
+    )
