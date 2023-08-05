554f9be2601d9662b5a227e8dc3d220b60ad1050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:04 +0000

    Add package utopy/0.0.5/utopy-0.0.5.tar.gz

---
+++ packages/utopy/utopy-0.0.5.tar.gz/utopy-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='utopy',
+    version='0.0.5',
+    py_modules=['utopy', ],
+    url='http://github.com/emre/utopy',
+    license='MIT',
+    author='Emre Yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='Utopian curation bot',
+    entry_points={
+        'console_scripts': [
+            'utopy = utopy:main',
+        ],
+    },
+    install_requires=["dataset", "steem==0.18.103", "pymysql", "requests"]
+)
