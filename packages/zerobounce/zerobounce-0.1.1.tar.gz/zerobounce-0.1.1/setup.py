0cdb585c33f45e9f516c53de7b9f8f2ae45b3197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:23 +0000

    Add package zerobounce/0.1.1/zerobounce-0.1.1.tar.gz

---
+++ packages/zerobounce/zerobounce-0.1.1.tar.gz/zerobounce-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'zerobounce',
+  packages = ['zerobounce'],
+  version = '0.1.1',
+  description = 'ZeroBounce Python API',
+  author = 'Tudor Aursulesei',
+  author_email = 'tudor@zerobounce.net',
+  url = 'https://github.com/zerobounce/zerobounce-python-api',
+  download_url = 'https://github.com/zerobounce/zerobounce-python-api/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['email', 'validation'], # arbitrary keywords
+  classifiers = [],
+)
