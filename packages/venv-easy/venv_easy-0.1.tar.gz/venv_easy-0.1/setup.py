f594b7214f96f686a8e2069541f4d8b3d06eb9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:46 +0000

    Add package venv-easy/0.1/venv_easy-0.1.tar.gz

---
+++ packages/venv-easy/venv_easy-0.1.tar.gz/venv_easy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='venv_easy',
+      version='0.1',
+      description='Easily automating virtual environment creation and implementation from within your Python3 Application.',
+      url='https://github.com/AndrewNeudegg/venv_easy',
+      author='Andrew Neudegg',
+      author_email='andrew.neudegg@gmail.com',
+      license='MIT',
+      packages=['venv_easy'],
+      zip_safe=False)
