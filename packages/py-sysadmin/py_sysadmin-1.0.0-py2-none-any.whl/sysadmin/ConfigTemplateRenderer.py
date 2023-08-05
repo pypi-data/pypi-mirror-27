16f9c4faf2bb26ec2a86e90fcd8730c5ba22aeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:13 +0000

    Add package py-sysadmin/1.0.0/py_sysadmin-1.0.0-py2-none-any.whl

---
+++ packages/py-sysadmin/py_sysadmin-1.0.0-py2-none-any.whl/sysadmin/ConfigTemplateRenderer.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+from jinja2 import Environment
+from jinja2 import FileSystemBytecodeCache
+from jinja2 import FileSystemLoader
+
+from sysadmin import helpers
+
+
+CACHE_DIR = "/tmp/sysadmin_jinja_cache"
+
+
+class ConfigTemplateRenderer:
+    def __init__(self, templatefile):
+        self._has_newline = False
+        self.templateFile = templatefile
+        if not os.path.isdir(CACHE_DIR):
+            os.mkdir(CACHE_DIR)
+        cache = FileSystemBytecodeCache(CACHE_DIR, "%s.cache")
+        templateName = os.path.basename(templatefile)
+        loader = FileSystemLoader(os.path.dirname(templatefile))
+        self.env = Environment(trim_blocks=True,
+                               lstrip_blocks=True,
+                               loader=loader,
+                               bytecode_cache=cache,
+                               keep_trailing_newline=True)
+        self.template = self.env.get_template(templateName)
+
+    def renderTemplate(self, args):
+        rendered = self.template.render(__helpers=helpers,
+                                        json=json,
+                                        map=map,
+                                        **args)
+        if self._has_newline:
+            rendered += '\n'
+        return rendered
