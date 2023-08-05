7a22f195b0201c7ab6d8e2e662737b9acff1b76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:20 +0000

    Add package carreralib/0.6.0/carreralib-0.6.0.tar.gz

---
+++ packages/carreralib/carreralib-0.6.0.tar.gz/carreralib-0.6.0/docs/conf.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+
+def get_version(filename):
+    from re import findall
+    with open(filename) as f:
+        metadata = dict(findall(r"__([a-z]+)__ = '([^']+)'", f.read()))
+    return metadata['version']
+
+
+project = 'carreralib'
+copyright = '2015-2017 Thomas Kemmer'
+version = get_version(b'../carreralib/__init__.py')
+release = version
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo'
+]
+exclude_patterns = ['_build']
+master_doc = 'index'
+html_theme = 'default'
