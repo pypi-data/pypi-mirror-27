04ad6b26372037301640dd84562079930c9c9f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:54 +0000

    Add package mrivis/0.1/mrivis-0.1.tar.gz

---
+++ packages/mrivis/mrivis-0.1.tar.gz/mrivis-0.1/mrivis/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for mrivis."""
+
+__author__ = """Pradeep Reddy Raamana"""
+__email__ = 'raamana@gmail.com'
+__version__ = '0.1.0'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
