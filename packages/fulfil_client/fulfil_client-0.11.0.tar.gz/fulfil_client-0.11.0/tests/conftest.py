f59a409725a5cd3faafe755b26ac78c6eb6e8824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:37 +0000

    Add package fulfil_client/0.11.0/fulfil_client-0.11.0.tar.gz

---
+++ packages/fulfil_client/fulfil_client-0.11.0.tar.gz/fulfil_client-0.11.0/tests/conftest.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Defines fixtures available to all tests."""
+import os
+
+import pytest
+
+from fulfil_client import Client
+from fulfil_client.model import model_base
+
+
+@pytest.fixture
+def client():
+    return Client('demo', os.environ['FULFIL_API_KEY'])
+
+
+@pytest.fixture
+def Model(client):
+    return model_base(client)
