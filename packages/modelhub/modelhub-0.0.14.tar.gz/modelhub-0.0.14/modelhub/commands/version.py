e0806d312ceb43f291d2026df70289da0de6f7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package modelhub/0.0.14/modelhub-0.0.14.tar.gz

---
+++ packages/modelhub/modelhub-0.0.14.tar.gz/modelhub-0.0.14/modelhub/commands/version.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from modelhub import __version__
+import sys
+from .base import BaseCommand, argument, option, types, register   # noqa
+
+
+@register('version')
+class Command(BaseCommand):
+
+    def run(self):
+        """Show modelhub version"""
+        self.echo("Python:\n\t" + sys.version)
+        self.echo("\nModelHub:\n\t%s" % __version__)
