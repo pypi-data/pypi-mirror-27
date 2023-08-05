58a81dd6708096f6fb2e5c37c02b9a2ee05d6005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:47 +0000

    Add package gecosistema_lite/0.0.429/gecosistema_lite-0.0.429.zip

---
+++ packages/gecosistema_lite/gecosistema_lite-0.0.429.zip/gecosistema_lite-0.0.429/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='gecosistema_lite',
+      version='0.0.429',
+      description='A simple python package',
+      author='Valerio Luzzi',
+      author_email='valerio.luzzi@gecosistema.it',
+      url='https://github.com/valluzzi/libcore/',
+      license='MIT',
+      packages=['gecosistema_lite'],
+      package_data={
+          "R": ["R/qkrige_v4.r"]
+      },
+      install_requires=['pyproj', 'rarfile', 'xlrd', 'xlwt', 'xlutils', 'jinja2', 'xmljson', 'openpyxl', 'pycrypto',
+                        'pyodbc'],
+      python_requires='>=2.7'
+      )
