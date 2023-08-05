fb31ee774e5b50fbfd396731618608ff2f7099a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package django-enforce-host/1.0.0/django-enforce-host-1.0.0.tar.gz

---
+++ packages/django-enforce-host/django-enforce-host-1.0.0.tar.gz/django-enforce-host-1.0.0/enforce_host/tests/settings.py
@@ -0,0 +1,19 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'enforce_host.tests.urls'
+
+SECRET_KEY = 'abcde12345'
+
+ALLOWED_HOSTS = ['original.com', 'enforced.com', 'enforced2.com']
+
+MIDDLEWARE = [
+    'enforce_host.EnforceHostMiddleware'
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
