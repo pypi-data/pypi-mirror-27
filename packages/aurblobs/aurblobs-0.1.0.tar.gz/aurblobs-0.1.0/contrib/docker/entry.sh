607a7e933eb254b644aeb249e78ec2ca7635f247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:34 +0000

    Add package aurblobs/0.1.0/aurblobs-0.1.0.tar.gz

---
+++ packages/aurblobs/aurblobs-0.1.0.tar.gz/aurblobs-0.1.0/contrib/docker/entry.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+usermod -u $USER_ID build
+
+gpg --import /privkey.gpg
+
+cd /pkg
+makepkg -fs --noconfirm --sign
+
+if [ $? -eq 0 ]; then
+	echo "Build succeded. Update repository db"
+
+	cd /repo
+	mkdir -p pkgs
+	cp /pkg/*.pkg.tar.xz pkgs/
+	cp /pkg/*.pkg.tar.xz.sig pkgs/
+	repo-add --delta --remove $REPO_NAME.db.tar.gz pkgs/*.pkg.tar.xz
+else
+	echo "Build process failed"
+fi
+
+
