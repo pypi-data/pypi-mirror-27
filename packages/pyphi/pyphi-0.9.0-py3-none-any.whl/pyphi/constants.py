578247978272c537d280769a8b0576b797ac2b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:42 +0000

    Add package pyphi/0.9.0/pyphi-0.9.0-py3-none-any.whl

---
+++ packages/pyphi/pyphi-0.9.0-py3-none-any.whl/pyphi/constants.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# constants.py
+
+'''
+Package-wide constants.
+'''
+
+import pickle
+
+import joblib
+
+# Import Direction for backwards compatibility
+from . import config, Direction
+
+#: The threshold below which we consider differences in phi values to be zero.
+EPSILON = 10 ** - config.PRECISION
+
+#: Label for the filesystem cache backend.
+FILESYSTEM = 'fs'
+
+#: Label for the MongoDB cache backend.
+DATABASE = 'db'
+
+#: The protocol used for pickling objects.
+PICKLE_PROTOCOL = pickle.HIGHEST_PROTOCOL
+
+#: The joblib ``Memory`` object for persistent caching without a database.
+joblib_memory = joblib.Memory(cachedir=config.FS_CACHE_DIRECTORY,
+                              verbose=config.FS_CACHE_VERBOSITY)
+
+#: Node states
+OFF = (0,)
+ON = (1,)
