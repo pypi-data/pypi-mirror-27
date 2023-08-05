85d6f4ac1f160a716bac83c127f5329d744d4cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package omas/0.1.4/omas-0.1.4.tar.gz

---
+++ packages/omas/omas-0.1.4.tar.gz/omas-0.1.4/omas/omas_setup.py
@@ -0,0 +1,42 @@
+from __future__ import print_function, division, unicode_literals
+
+# --------------------------------------------
+# external imports
+# --------------------------------------------
+import os
+import sys
+import glob
+import json
+import copy
+from collections import OrderedDict
+import re
+import numpy
+from pprint import pprint
+import weakref
+
+# Python3/2 import differences
+if sys.version_info < (3, 0):
+    import cPickle as pickle
+else:
+    basestring = str
+    unicode = str
+    import pickle
+
+# --------------------------------------------
+# rcparams
+# --------------------------------------------
+omas_rcparams = {
+    'consistency_check': True,
+}
+
+# --------------------------------------------
+# configuration of directories and IMAS infos
+# --------------------------------------------
+imas_json_dir = os.path.abspath(str(os.path.dirname(__file__)) + '/imas_structures/')
+
+separator = '.'
+
+if 'IMAS_VERSION' in os.environ:
+    default_imas_version = os.environ['IMAS_VERSION']
+else:
+    default_imas_version = '3.10.1'
