b7206f7f02b569561eb1fabde31976348ad44fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:56 +0000

    Add package onegov.core/0.53.6/onegov.core-0.53.6.tar.gz

---
+++ packages/onegov.core/onegov.core-0.53.6.tar.gz/onegov.core-0.53.6/onegov/core/custom_json.py
@@ -0,0 +1,93 @@
+""" Sigh. Here we go again, *another* json implementation with support for:
+
+- date
+- datetime
+- time
+
+Because nobody else does all of these. And if they do (like standardjson), they
+don't support decoding...
+
+"""
+
+import datetime
+import isodate
+import json
+import types
+
+from decimal import Decimal
+from onegov.core import utils
+
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            return '__datetime__@' + isodate.datetime_isoformat(o)
+        elif isinstance(o, datetime.date):
+            return '__date__@' + isodate.date_isoformat(o)
+        elif isinstance(o, Decimal):
+            return '__decimal__@' + str(o)
+        elif isinstance(o, datetime.time):
+            return '__time__@' + isodate.time_isoformat(o)
+        elif isinstance(o, types.GeneratorType):
+            return list(o)
+        else:
+            if isinstance(o, str):
+                # make sure the reserved words can't be added as a string by
+                # some smartass that wants to screw with us
+                o = utils.lchop(o, '__date__@')
+                o = utils.lchop(o, '__datetime__@')
+                o = utils.lchop(o, '__decimal__@')
+                o = utils.lchop(o, '__time__@')
+
+            return super().default(o)
+
+
+def custom_json_decoder(value):
+    if not isinstance(value, str):
+        return value
+
+    if value.startswith('__date__@'):
+        return isodate.parse_date(value[9:])
+
+    if value.startswith('__datetime__@'):
+        return isodate.parse_datetime(value[13:])
+
+    if value.startswith('__decimal__@'):
+        return Decimal(value[12:])
+
+    if value.startswith('__time__@'):
+        return isodate.parse_time(value[9:])
+
+    return value
+
+
+def json_loads_object_hook(dictionary):
+    for key, value in dictionary.items():
+
+        if isinstance(value, str):
+            dictionary[key] = custom_json_decoder(value)
+
+        elif isinstance(value, (list, tuple)):
+            dictionary[key] = list(map(custom_json_decoder, value))
+
+    return dictionary
+
+
+def encode_decimal(o):
+    """ Encodes a Python decimal.Decimal object as an ECMA-262 compliant
+    decimal string."""
+    return str(o)
+
+
+def dumps(obj):
+    if obj is not None:
+        return json.dumps(obj, cls=CustomJSONEncoder)
+    else:
+        return ''
+
+
+def loads(value):
+    if value is not None:
+        return json.loads(value, object_hook=json_loads_object_hook)
+    else:
+        return {}
---
+++ packages/onegov.core/onegov.core-0.53.6.tar.gz/onegov.core-0.53.6/onegov/core/html.py
@@ -0,0 +1,75 @@
+from bleach.sanitizer import Cleaner
+
+
+# html tags allowed by bleach
+SANE_HTML_TAGS = [
+    'a',
+    'abbr',
+    'b',
+    'br',
+    'blockquote',
+    'code',
+    'del',
+    'div',
+    'em',
+    'i',
+    'img',
+    'hr',
+    'li',
+    'ol',
+    'p',
+    'pre',
+    'strong',
+    'sup',
+    'span',
+    'ul',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6'
+]
+
+# html attributes allowed by bleach
+SANE_HTML_ATTRS = {
+    'a': ['href', 'title'],
+    'abbr': ['title', ],
+    'acronym': ['title', ],
+    'img': ['src', 'alt', 'title']
+}
+
+
+cleaner = Cleaner(
+    tags=SANE_HTML_TAGS,
+    attributes=SANE_HTML_ATTRS
+)
+
+
+def sanitize_html(html):
+    """ Takes the given html and strips all but a whitelisted number of tags
+    from it.
+
+    """
+
+    return cleaner.clean(html)
+
+
+def sanitize_svg(svg):
+    """ I couldn't find a good svg sanitiser function yet, so for now
+    this function will be a no-op, though it will try to detect
+    svg files which are harmful.
+
+    I tried to go with bleach/html5lib, but the lack of xml namespace support
+    makes those options a no go.
+
+    In the future we want a proper SVG sanitiser here!
+
+    """
+
+    assert 'javascript:' not in svg
+    assert 'CDATA' not in svg
+    assert '<script>' not in svg
+    assert 'Set-Cookie' not in svg
+
+    return svg
