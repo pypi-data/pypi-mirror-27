d8d32c7d9324381b81b8627832ed56fc018ab675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package aepsicro/0.0.6/aepsicro-0.0.6.tar.gz

---
+++ packages/aepsicro/aepsicro-0.0.6.tar.gz/aepsicro-0.0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'aepsicro',
+	version = '0.0.6',
+	author = 'Antonio Eduardo Téllez Santos',
+	author_email = 'aironmail@gmail.com',
+	description = 'Psicrometría / Psychrometrics',
+	long_description = open('README.rst').read(),
+	license = 'GNU GPLv3',
+	url = 'https://pypi.python.org/pypi/aepsicro',
+	packages = find_packages(),
+    keywords =['psychrometrics', 'psicrometría', ],
+	install_requires=['numpy', 'scipy', 'matplotlib', ],
+	test_suite = 'tests',
+	classifiers = [
+    	'Development Status :: 3 - Alpha',
+    	'Intended Audience :: Science/Research',
+    	'Intended Audience :: Developers',
+    	'Programming Language :: Python',
+    	'Programming Language :: Python :: 3.3',
+    	'Programming Language :: Python :: 3.4',
+    	'Programming Language :: Python :: 3.5',
+    	'Topic :: Scientific/Engineering',
+    	'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    	]
+	)
