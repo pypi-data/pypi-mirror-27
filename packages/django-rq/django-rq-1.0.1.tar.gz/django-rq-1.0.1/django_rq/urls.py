8e2707fafc568e467dfa185614a2eb374bde4140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:32 +0000

    Add package django-rq/1.0.1/django-rq-1.0.1.tar.gz

---
+++ packages/django-rq/django-rq-1.0.1.tar.gz/django-rq-1.0.1/django_rq/urls.py
@@ -0,0 +1,34 @@
+from django.conf.urls import url
+
+from django_rq import views
+
+urlpatterns = [
+    url(r'^$',
+        views.stats, name='rq_home'),
+    url(r'^stats.json/(?P<token>[\w]+)?/?$',
+        views.stats_json, name='rq_home_json'),
+    url(r'^queues/(?P<queue_index>[\d]+)/$',
+        views.jobs, name='rq_jobs'),
+    url(r'^workers/(?P<queue_index>[\d]+)/$',
+        views.workers, name='rq_workers'),
+    url(r'^workers/(?P<queue_index>[\d]+)/(?P<key>[-\w\.\:]+)/$',
+        views.worker_details, name='rq_worker_details'),
+    url(r'^queues/(?P<queue_index>[\d]+)/finished/$',
+        views.finished_jobs, name='rq_finished_jobs'),
+    url(r'^queues/(?P<queue_index>[\d]+)/started/$',
+        views.started_jobs, name='rq_started_jobs'),
+    url(r'^queues/(?P<queue_index>[\d]+)/deferred/$',
+        views.deferred_jobs, name='rq_deferred_jobs'),
+    url(r'^queues/(?P<queue_index>[\d]+)/empty/$',
+        views.clear_queue, name='rq_clear'),
+    url(r'^queues/(?P<queue_index>[\d]+)/requeue-all/$',
+        views.requeue_all, name='rq_requeue_all'),
+    url(r'^queues/(?P<queue_index>[\d]+)/(?P<job_id>[-\w]+)/$',
+        views.job_detail, name='rq_job_detail'),
+    url(r'^queues/(?P<queue_index>[\d]+)/(?P<job_id>[-\w]+)/delete/$',
+        views.delete_job, name='rq_delete_job'),
+    url(r'^queues/actions/(?P<queue_index>[\d]+)/$',
+        views.actions, name='rq_actions'),
+    url(r'^queues/(?P<queue_index>[\d]+)/(?P<job_id>[-\w]+)/requeue/$',
+        views.requeue_job_view, name='rq_requeue_job'),
+]
