bb40230fef56b30cdc9fd529e62b39a5a9a40de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:26 +0000

    Add package antfin/0.0.1/antfin-0.0.1.tar.gz

---
+++ packages/antfin/antfin-0.0.1.tar.gz/antfin-0.0.1/antfin/antfin.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+def main():
+    print ("My test API for antfin")
+
+if __name__ == '__main__':
+    main()
---
+++ packages/antfin/antfin-0.0.1.tar.gz/antfin-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env Python  
+#coding=utf-8
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = "antfin",
+    version = "0.0.1",
+    description = "MyTest API",
+    author = "Derek",
+    author_email = "dingo0927@126.com"
+)
