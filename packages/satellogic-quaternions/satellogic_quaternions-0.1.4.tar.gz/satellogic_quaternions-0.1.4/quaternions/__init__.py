7f37769249a8691c70815e0bb20ceec7f95ce1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package satellogic-quaternions/0.1.4/satellogic_quaternions-0.1.4.tar.gz

---
+++ packages/satellogic-quaternions/satellogic_quaternions-0.1.4.tar.gz/satellogic_quaternions-0.1.4/quaternions/__init__.py
@@ -0,0 +1,3 @@
+from quaternions.quaternion import Quaternion  # NOQA
+from quaternions.general_quaternion import GeneralQuaternion, QuaternionError  # NOQA
+from quaternions.version import __version__  # NOQA
