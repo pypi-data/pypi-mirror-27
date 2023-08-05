8a9a36acee7e79bbc8a2f7df5e1d2c467c2be8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:22 +0000

    Add package buildlib/1.0.20/buildlib-1.0.20-py3-none-any.whl

---
+++ packages/buildlib/buildlib-1.0.20-py3-none-any.whl/buildlib/cmds/docker/__init__.py
@@ -0,0 +1,131 @@
+from typing import Optional, List
+from processy import run
+from cmdinter import CmdFuncResult, Status
+from functools import reduce
+
+
+def _image_exists(image) -> bool:
+    result = run(
+        cmd=['docker', 'inspect', '--type=image', image],
+        verbose=False,
+        return_stdout=True
+    )
+
+    return 'Error: No such image' not in result.stdout
+
+
+def run_container(
+    image: str,
+    port: int
+) -> CmdFuncResult:
+    """
+    Run Docker container locally.
+    """
+    title = 'Run Docker Container.'
+
+    p = run(['docker', 'run', '-d', '-p', f'127.0.0.1:{port}:{port}', image])
+
+    if p.returncode == 0:
+        status: str = Status.ok
+    else:
+        status: str = Status.error
+
+    return CmdFuncResult(
+        returncode=p.returncode,
+        returnvalue=None,
+        summary=f'{status} {title}',
+    )
+
+
+def stop_container(
+    by_port: int,
+) -> CmdFuncResult:
+    title = 'Stop Docker Container'
+
+    if by_port:
+        cmd = ['docker', 'ps', '-q', '--filter', f'expose={by_port}',
+               '--format="{{.ID}}"']
+
+    ids = run(
+        cmd=cmd,
+        return_stdout=True,
+    ).stdout.split('\n')
+
+    ps = [
+        run(['docker', 'stop', id_.replace('"', '')])
+        for id_
+        in ids
+        if id_
+    ]
+
+    returncode = max([p.returncode for p in ps]) if ps else 0
+
+    if returncode == 0:
+        status: str = Status.ok
+    else:
+        status: str = Status.error
+
+    return CmdFuncResult(
+        returncode=returncode,
+        returnvalue=None,
+        summary=f'{status} {title}',
+    )
+
+
+def remove_image(
+    image: str
+) -> CmdFuncResult:
+    """"""
+    title = 'Remove Docker Image.'
+
+    cmd = ['docker', 'rmi', image, '--force']
+
+    if _image_exists(image):
+        p = run(cmd)
+        returncode = p.returncode
+    else:
+        returncode = 0
+
+    if returncode == 0:
+        status: str = Status.ok
+    else:
+        status: str = Status.error
+
+    return CmdFuncResult(
+        returncode=returncode,
+        returnvalue=None,
+        summary=f'{status} {title}',
+    )
+
+
+def build_image(
+    tag: List[str],
+    build_arg: List[str],
+) -> CmdFuncResult:
+    """
+    """
+    title = 'Build Docker Image.'
+
+    if build_arg:
+        nested = [['--build-arg', arg] for arg in build_arg]
+        build_arg = reduce(lambda x, y: x + y, nested)
+    else:
+        build_arg = []
+
+    cmd = ['docker', 'build', '.', '--pull', '-f', 'Dockerfile'] + build_arg
+
+    for t in tag:
+        cmd.extend(['-t', t])
+
+    p = run(cmd)
+
+    if p.returncode == 0:
+        status: str = Status.ok
+    else:
+        status: str = Status.error
+
+    return CmdFuncResult(
+        returncode=p.returncode,
+        returnvalue=None,
+        summary=f'{status} {title}',
+    )
