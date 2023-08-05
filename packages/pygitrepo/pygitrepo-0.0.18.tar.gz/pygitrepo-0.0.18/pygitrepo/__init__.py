dd0545e3bfb1089469a7e483b876bd8bfdaead49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:12 +0000

    Add package pygitrepo/0.0.18/pygitrepo-0.0.18.tar.gz

---
+++ packages/pygitrepo/pygitrepo-0.0.18.tar.gz/pygitrepo-0.0.18/pygitrepo/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__version__ = "0.0.18"
+__short_description__ = (
+    "Allow dummies develop Python project like a Pro - "
+    "Quickly initiate a python project from scratch."
+)
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__maintainer__ = "Sanhe Hu"
+__maintainer_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
+
+
+try:
+    from .cli import initiate_project as init
+except ImportError:  # pragma: no cover
+    pass
