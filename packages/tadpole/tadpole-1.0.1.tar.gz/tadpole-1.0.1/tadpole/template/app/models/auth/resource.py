f1e1bae089d9af4d840819c7cb75b4da0a53f93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:42 +0000

    Add package tadpole/1.0.1/tadpole-1.0.1.tar.gz

---
+++ packages/tadpole/tadpole-1.0.1.tar.gz/tadpole-1.0.1/tadpole/template/app/models/auth/resource.py
@@ -0,0 +1,20 @@
+# !/usr/bin/python
+# coding: utf-8
+
+"""
+    create at 2017/11/19 by allen
+"""
+
+from sqlalchemy import Index
+from app.lib.database import db, Column
+from app.lib.database import MysqlModel as Model
+
+
+class Resource(Model):
+    rtype = Column(db.String(32), nullable=False, default='-')
+    name = Column(db.String(128), nullable=False, default='-')
+    operation = Column(db.String(32), nullable=False, default='-')
+    description = Column(db.String(128), nullable=False, default='-')
+
+
+Index('name_operation_uc', Resource.name, Resource.operation, unique=True)
