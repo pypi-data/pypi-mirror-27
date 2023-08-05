e6db80bd320c2705a964e6f2c0cd1a3732424a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:23 +0000

    Add package nengolib/0.4.1/nengolib-0.4.1-py3-none-any.whl

---
+++ packages/nengolib/nengolib-0.4.1-py3-none-any.whl/nengolib/__init__.py
@@ -0,0 +1,38 @@
+"""
+Main (:mod:`nengolib`)
+======================
+
+.. currentmodule:: nengolib
+
+Extensions
+----------
+
+.. autosummary::
+   :toctree:
+
+   Network
+   Connection
+
+Processes
+---------
+
+.. autosummary::
+   :toctree:
+
+   processes.Callable
+"""
+
+from .version import version as __version__
+
+from .connection import Connection
+from .monkeypatch import patch, unpatch
+from .network import Network
+from .neurons import PerfectLIF
+
+from . import compat
+from . import networks
+from . import processes  # this is a file, not a module
+from . import signal
+from . import stats
+from . import synapses
+from .synapses import Lowpass, Alpha, DoubleExp
