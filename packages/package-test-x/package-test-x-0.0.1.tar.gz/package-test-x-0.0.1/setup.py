3f99f4d5d84cf7efd2f35c73615abc03623c6432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:20 +0000

    Add package package-test-x/0.0.1/package-test-x-0.0.1.tar.gz

---
+++ packages/package-test-x/package-test-x-0.0.1.tar.gz/package-test-x-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name='package-test-x',
+	packages=['package-test-x'],
+	description='hello world test',
+	version='0.0.1',
+	scripts=['package-test-x/hi.py']
+)
