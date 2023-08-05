b0c2e1c3cb41248d516d778e641d4488fc7af376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:44 +0000

    Add package venmo/0.11.0/venmo-0.11.0-py2.py3-none-any.whl

---
+++ packages/venmo/venmo-0.11.0-py2.py3-none-any.whl/venmo/user.py
@@ -0,0 +1,39 @@
+'''
+User module.
+'''
+import json
+
+import requests
+
+import venmo
+
+
+def id_from_username(username):
+    for u in search(username):
+        if u['username'].lower() == username.lower():
+            return u['id']
+    return None
+
+
+def print_search(query):
+    print(json.dumps(search(query), indent=4))
+
+
+def search(query):
+    response = requests.get(
+        venmo.settings.USERS_URL,
+        params={
+            'limit': 5,
+            'query': query,
+        }
+    )
+    users = response.json()['data']
+    results = []
+    for u in users:
+        results.append({
+            'id': u['id'],
+            'username': u['username'],
+            'display_name': u['display_name'],
+            'profile_picture_url': u['profile_picture_url'],
+        })
+    return results
