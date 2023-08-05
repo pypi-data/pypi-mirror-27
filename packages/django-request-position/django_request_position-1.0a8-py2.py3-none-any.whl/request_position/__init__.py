45c79427ca2d43d6eabad9cfee399896dc216ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:49 +0000

    Add package django-request-position/1.0a8/django_request_position-1.0a8-py2.py3-none-any.whl

---
+++ packages/django-request-position/django_request_position-1.0a8-py2.py3-none-any.whl/request_position/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, division, absolute_import
+
+from django import get_version
+
+
+VERSION = (1, 0, 0, 'alpha', 8)
+
+__version__ = get_version(VERSION)
