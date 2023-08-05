9abfc6f41b684e5d5b1d64c358f393dcb090486d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:51 +0000

    Add package fulfil_client/0.11.1/fulfil_client-0.11.1.tar.gz

---
+++ packages/fulfil_client/fulfil_client-0.11.1.tar.gz/fulfil_client-0.11.1/tests/conftest.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Defines fixtures available to all tests."""
+import os
+
+import pytest
+import redis
+
+from fulfil_client import Client
+from fulfil_client.model import model_base
+
+
+@pytest.fixture
+def client():
+    return Client('demo', os.environ['FULFIL_API_KEY'])
+
+
+@pytest.fixture
+def Model(client):
+    return model_base(client)
+
+
+@pytest.fixture
+def ModelWithCache(client):
+    return model_base(
+        client,
+        cache_backend=redis.StrictRedis(host='localhost', port=6379, db=0)
+    )
