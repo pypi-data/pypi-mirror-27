e59a35ce8c155e94cbaa45bc19e6022323313f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:01 +0000

    Add package logbind/2017.11.7/logbind-2017.11.7-py2.py3-none-any.whl

---
+++ packages/logbind/logbind-2017.11.7-py2.py3-none-any.whl/logbind.py
@@ -0,0 +1,28 @@
+"""
+logbind
+=======
+
+Much, much easier interface for ``LoggerAdapter``: bind new fields to
+loggers.
+
+"""
+
+import logging
+
+
+__version__ = '2017.11.7'
+
+
+def bind(logger, **kwargs):
+    # type: (logging.Logger, ...) -> logging.LoggerAdapter
+
+    extra = {}
+
+    # If given a LoggerAdapter instance, extract the extra
+    # fields and find the original logging.Logger instance.
+    if isinstance(logger, logging.LoggerAdapter):
+        extra.update(logger.extra)  # Use update to force a copy
+        logger = logger.logger
+
+    extra.update(kwargs)
+    return logging.LoggerAdapter(logger, extra)
