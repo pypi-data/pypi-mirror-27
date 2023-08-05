5b5546c51d2d6c116fb1140550e7df5fdee6c754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:37 +0000

    Add package tap-marketo/0.4.21/tap-marketo-0.4.21.tar.gz

---
+++ packages/tap-marketo/tap-marketo-0.4.21.tar.gz/tap-marketo-0.4.21/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-marketo',
+      version='0.4.21',
+      description='Singer.io tap for extracting data from the Marketo API',
+      author='Stitch',
+      url='http://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_marketo'],
+      install_requires=[
+          'singer-python==1.5.2',
+          'requests==2.12.4',
+          'backoff==1.3.2',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-marketo=tap_marketo:main
+      ''',
+      packages=['tap_marketo'],
+      package_data = {
+          'tap_marketo/schemas': [
+              "activities.json",
+              "activity_types.json",
+              "lists.json",
+          ]
+      },
+      include_package_data=True,
+)
