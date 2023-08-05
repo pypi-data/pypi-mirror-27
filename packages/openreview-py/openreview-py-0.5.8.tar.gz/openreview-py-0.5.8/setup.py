e77c14711cd4ad168de81f504c31b6c2852f65df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:46 +0000

    Add package openreview-py/0.5.8/openreview-py-0.5.8.tar.gz

---
+++ packages/openreview-py/openreview-py-0.5.8.tar.gz/openreview-py-0.5.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='openreview-py',
+      version='0.5.8',
+      description='OpenReview client library',
+      url='https://github.com/iesl/openreview-py',
+      author='Michael Spector, Melisa Bok',
+      author_email='spector@cs.umass.edu, mbok@cs.umass.edu',
+      license='MIT',
+      packages=[
+          'openreview'
+      ],
+      install_requires=[
+          'pycrypto',
+          'requests'
+      ],
+      zip_safe=False)
