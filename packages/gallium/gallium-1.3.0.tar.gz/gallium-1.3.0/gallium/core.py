51b4dc8c68780e0964f93fbb4cce74b7e9f20308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:55 +0000

    Add package gallium/1.3.0/gallium-1.3.0.tar.gz

---
+++ packages/gallium/gallium-1.3.0.tar.gz/gallium-1.3.0/gallium/core.py
@@ -0,0 +1,70 @@
+"""
+Carbon Core
+###########
+
+:Author: Juti Noppornpitak
+"""
+
+from contextlib import contextmanager
+
+from imagination.assembler.core  import Assembler
+from imagination.meta.container  import Entity, Factorization, Lambda
+from imagination.meta.definition import DataDefinition
+
+
+class Core(object):
+    """ The Core of the Framework
+
+        This relies on Imagination Framework.
+    """
+    def __init__(self, assembler = None):
+        self.assembler = assembler or Assembler()
+
+    @property
+    def _core(self):
+        return self.container
+
+    @property
+    def container(self):
+        return self.assembler.core
+
+    def get(self, id):
+        """ Get the service container. """
+        return self._core.get(id)
+
+    def load(self, *paths):
+        """ Load service containers from multiple configuration files. """
+        self.assembler.load(*paths)
+
+    def set_entity(self, entity_id, fqcn):
+        """ Register a new entity. """
+        container = Entity(identifier = entity_id, fqcn = fqcn)
+
+        self._core.update_metadata({entity_id: container})
+
+    def set_factorization(self, entity_id, factory_id, factory_method_name):
+        """ Register a new entity with factorization. """
+        container = Factorization(entity_id, factory_id, factory_method_name)
+
+        self._core.update_metadata({entity_id: container})
+
+    def set_lambda(self, entity_id, import_path):
+        container = Lambda(entity_id, import_path)
+
+        self._core.update_metadata({entity_id: container})
+
+    def set_entity_param(self, entity_id, kind, value,
+                         transformation_required = False, name = None):
+        """ Define a constructor parameter for a particular entity. """
+        definition = DataDefinition(value, name, kind, transformation_required)
+        metadata   = self._core.get_metadata(entity_id)
+
+        metadata.params.add(definition, name)
+
+    def inject_entity_dependency(self, entity_id, dependency_id, name = None):
+        """ Inject an entity as a dependency to the specified entity. """
+        self.set_entity_param(entity_id, 'entity', dependency_id, True, name)
+
+    def inject_entity_classinfo(self, entity_id, fqcn, name = None):
+        """ Inject a class/type as a dependency to the specified entity. """
+        self.set_entity_param(entity_id, 'class', fqcn, True, name)
