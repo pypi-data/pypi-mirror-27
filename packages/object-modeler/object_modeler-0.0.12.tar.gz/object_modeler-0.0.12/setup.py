281ac7197b476a2585750182fe4385094c99002d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:32 +0000

    Add package object-modeler/0.0.12/object_modeler-0.0.12.tar.gz

---
+++ packages/object-modeler/object_modeler-0.0.12.tar.gz/object_modeler-0.0.12/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    test_suite='test',
+    name='object_modeler',
+    version='0.0.12',
+    packages=['object_modeler'],
+    url='https://github.com/KeyStorke/ObjectModeler',
+    license='MIT',
+    author='Nickolay Ovdienko',
+    author_email='Nickolay.Ovdienko@emc.com',
+    description='Clearly and simply define objects schemes'
+)
