4c13f309b665fb107c0ab63411342a38a2e3bb3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:31 +0000

    Add package poco1/1.0.5/poco1-1.0.5.tar.gz

---
+++ packages/poco1/poco1-1.0.5.tar.gz/poco1-1.0.5/setup.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt', session=False)
+
+# reqs is a list of requirement
+reqs = [str(ir.req) for ir in install_reqs if ir.req]
+
+setup(
+    name='poco1',
+    version='1.0.5',
+    keywords="poco",
+    description='Poco cross-engine UI automated test framework.',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=reqs,
+    license='Apache License 2.0',
+    author='adolli',
+    author_email='adolli@163.com',
+)
