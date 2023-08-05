dcf82fddae8254e69de102c7368103870e936643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:08 +0000

    Add package mapi/2.1.0/mapi-2.1.0.tar.gz

---
+++ packages/mapi/mapi-2.1.0.tar.gz/mapi-2.1.0/setup.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+from distutils.core import setup
+
+with open('requirements.txt', 'r') as fp:
+    REQUIREMENTS = fp.read().splitlines()
+
+with open('readme.rst', 'r') as fp:
+    LONG_DESCRIPTION = fp.read()
+
+setup(
+    author='Jessy Williams',
+    author_email='jessy@jessywilliams.com',
+    description=(
+        'An API for media database APIs which allows you to search for metadata'
+        'using a simple, common interface'
+    ),
+    license='MIT',
+    long_description=LONG_DESCRIPTION,
+    name='mapi',
+    packages=['mapi'],
+    install_requires=REQUIREMENTS,
+    url='https://github.com/jkwill87/mapi',
+    version='2.1.0'
+)
