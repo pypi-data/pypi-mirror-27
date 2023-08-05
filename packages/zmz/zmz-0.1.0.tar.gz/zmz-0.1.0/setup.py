b7c324822e07036e3eb4e3118b42ef06f826a0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:38 +0000

    Add package zmz/0.1.0/zmz-0.1.0.tar.gz

---
+++ packages/zmz/zmz-0.1.0.tar.gz/zmz-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+__author__ = 'ikarishinjigao'
+
+setup(
+    name='zmz',
+    version='0.1.0',
+    description='Scripts for getting download links from www.zimuzu.tv.',
+    author='ikarishinjigao',
+    author_email='ikarishinjigao@gmail.com',
+    url='https://github.com/ikarishinjigao/Zimuzu-Scraping',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    keywords=['zmz', 'zimuzu', 'ZIMUZU'],
+    license='MIT License',
+    install_requires=[
+        'click',
+        'lxml',
+        'prettytable',
+        'bs4',
+        'requests',
+        'progressbar2',
+    ],
+    entry_points="""
+        [console_scripts]
+        zmz = zimuzu.__main__:main
+    """,
+)
---
+++ packages/zmz/zmz-0.1.0.tar.gz/zmz-0.1.0/zmz/__main__.py
@@ -0,0 +1,26 @@
+import click
+from zmz.article import Article
+from zmz.helpers import Helper
+
+
+@click.group()
+def main():
+    pass
+
+
+@click.command()
+@click.option('-a', '--area', type=str, help='The area you want to show. (jp, us, uk)')
+@click.option('-p', '--pages', default=1, help='Number of pages.')
+def show(area):
+    """Show articles."""
+    articles = Article.get_articles_by(area=area, pages=1)
+    Helper.print_article_by_update_time(articles)
+    Helper.db_save(articles, area=area)
+
+
+# add command to group
+main.add_command(show)
+
+
+if __name__ == "__main__":
+    main()
