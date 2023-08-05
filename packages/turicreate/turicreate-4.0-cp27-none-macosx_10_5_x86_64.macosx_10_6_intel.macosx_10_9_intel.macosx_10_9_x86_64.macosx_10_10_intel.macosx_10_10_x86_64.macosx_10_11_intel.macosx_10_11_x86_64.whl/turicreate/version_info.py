25a4d56ac0eadc5107b1de616f8ba580d3a8c8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:09 +0000

    Add package turicreate/4.0/turicreate-4.0-cp27-none-macosx_10_5_x86_64.macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.macosx_10_11_intel.macosx_10_11_x86_64.whl

---
+++ packages/turicreate/turicreate-4.0-cp27-none-macosx_10_5_x86_64.macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.macosx_10_11_intel.macosx_10_11_x86_64.whl/turicreate/version_info.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2017 Apple Inc. All rights reserved.
+#
+# Use of this source code is governed by a BSD-3-clause license that can
+# be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+'''
+Obtain the version number of the current turicreate create.
+'''
+from __future__ import print_function as _
+from __future__ import division as _
+from __future__ import absolute_import as _
+
+# python egg version
+__version__ = '4.0'#{{VERSION_STRING}}
+version = __version__
+build_number = '24148'#{{BUILD_NUMBER}}
+git_sha = '2065e4ea8387694488ace56b71203bed8e3a1abb'#{{GIT_SHA}}
