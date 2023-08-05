b61c3ff2c60b46257ea67f4727c05d20eea48ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:49 +0000

    Add package aiohttp-json-api/0.33.1/aiohttp_json_api-0.33.1.tar.gz

---
+++ packages/aiohttp-json-api/aiohttp_json_api-0.33.1.tar.gz/aiohttp_json_api-0.33.1/aiohttp_json_api/__init__.py
@@ -0,0 +1,227 @@
+"""JSON API implementation for aiohttp."""
+
+import inspect
+from collections import MutableMapping, Sequence
+
+__author__ = """Vladimir Bolshakov"""
+__email__ = 'vovanbo@gmail.com'
+__version__ = '0.33.1'
+
+
+def setup_app_registry(app, registry_class, schemas):
+    """Set up JSON API application registry."""
+    from .common import logger
+    from .registry import Registry
+    from .schema.abc.schema import SchemaABC
+
+    if registry_class is not None:
+        if not issubclass(registry_class, Registry):
+            raise TypeError('Subclass of Registry is required. '
+                            'Got: {}'.format(registry_class))
+    else:
+        registry_class = Registry
+
+    app_registry = registry_class()
+
+    for schema_cls in schemas:
+        if not inspect.isclass(schema_cls):
+            raise TypeError('Class (not instance) of schema is required.')
+
+        if not issubclass(schema_cls, SchemaABC):
+            raise TypeError('Subclass of SchemaABC is required. '
+                            'Got: {}'.format(schema_cls))
+
+        schema = schema_cls(app)
+        if not isinstance(schema.type, str):
+            raise TypeError('Schema type property must be a string.')
+
+        app_registry[schema.type] = schema
+        if schema.resource_class is None:
+            logger.warning('The schema "%s" is not bound to a resource class.',
+                           schema.type)
+        else:
+            if not inspect.isclass(schema.resource_class):
+                raise TypeError('Class (not instance) of resource '
+                                'is required.')
+            app_registry[schema.resource_class] = schema
+
+        logger.debug('Registered %s with resource class %s and type "%s"',
+                     schema_cls.__name__,
+                     schema.resource_class.__name__,
+                     schema.type)
+
+    return app_registry
+
+
+def setup_custom_handlers(custom_handlers):
+    """Set up default and custom handlers for JSON API application."""
+    from . import handlers as default_handlers
+    from .common import logger
+
+    handlers = {
+        name: handler
+        for name, handler in inspect.getmembers(default_handlers,
+                                                inspect.iscoroutinefunction)
+        if name in default_handlers.__all__
+    }
+    if custom_handlers is not None:
+        if isinstance(custom_handlers, MutableMapping):
+            custom_handlers_iter = custom_handlers.items()
+        elif isinstance(custom_handlers, Sequence):
+            custom_handlers_iter = ((c.__name__, c) for c in custom_handlers)
+        else:
+            raise TypeError('Wrong type of "custom_handlers" parameter. '
+                            'Mapping or Sequence is expected.')
+
+        for name, custom_handler in custom_handlers_iter:
+            handler_name = custom_handler.__name__
+            if name not in handlers:
+                logger.warning('Custom handler %s is ignored.', name)
+                continue
+            if not inspect.iscoroutinefunction(custom_handler):
+                logger.error('"%s" is not a co-routine function (ignored).',
+                             handler_name)
+                continue
+
+            handlers[name] = custom_handler
+            logger.debug('Default handler "%s" is replaced '
+                         'with co-routine "%s" (%s)',
+                         name, handler_name, inspect.getmodule(custom_handler))
+    return handlers
+
+
+def setup_resources(app, base_path, handlers, routes_namespace):
+    """Set up JSON API application resources."""
+    from .common import ALLOWED_MEMBER_NAME_RULE
+
+    type_part = '{type:' + ALLOWED_MEMBER_NAME_RULE + '}'
+    relation_part = '{relation:' + ALLOWED_MEMBER_NAME_RULE + '}'
+    collection_resource = app.router.add_resource(
+        '{base}/{type}'.format(base=base_path, type=type_part),
+        name='{}.collection'.format(routes_namespace)
+    )
+    resource_resource = app.router.add_resource(
+        '{base}/{type}/{{id}}'.format(base=base_path, type=type_part),
+        name='{}.resource'.format(routes_namespace)
+    )
+    relationships_resource = app.router.add_resource(
+        '{base}/{type}/{{id}}/relationships/{relation}'.format(
+            base=base_path, type=type_part, relation=relation_part
+        ),
+        name='{}.relationships'.format(routes_namespace)
+    )
+    related_resource = app.router.add_resource(
+        '{base}/{type}/{{id}}/{relation}'.format(
+            base=base_path, type=type_part, relation=relation_part
+        ),
+        name='{}.related'.format(routes_namespace)
+    )
+    collection_resource.add_route('GET', handlers['get_collection'])
+    collection_resource.add_route('POST', handlers['post_resource'])
+    resource_resource.add_route('GET', handlers['get_resource'])
+    resource_resource.add_route('PATCH', handlers['patch_resource'])
+    resource_resource.add_route('DELETE', handlers['delete_resource'])
+    relationships_resource.add_route('GET', handlers['get_relationship'])
+    relationships_resource.add_route('POST', handlers['post_relationship'])
+    relationships_resource.add_route('PATCH', handlers['patch_relationship'])
+    relationships_resource.add_route('DELETE', handlers['delete_relationship'])
+    related_resource.add_route('GET', handlers['get_related'])
+
+
+def setup_jsonapi(app, schemas, *, base_path='/api', version='1.0',
+                  meta=None, context_class=None, registry_class=None,
+                  custom_handlers=None, log_errors=True,
+                  routes_namespace=None):
+    """
+    Set up JSON API in aiohttp application.
+
+    This function will setup resources, handlers and middleware.
+
+    :param ~aiohttp.web.Application app:
+        Application instance
+    :param ~typing.Sequence[BaseSchema] schemas:
+        List of schema classes to register in JSON API
+    :param str base_path:
+        Prefix of JSON API routes paths
+    :param str version:
+        JSON API version (used in ``jsonapi`` key of document)
+    :param dict meta:
+        Meta information will added to response (``meta`` key of document)
+    :param context_class:
+        Override of RequestContext class
+        (must be subclass of :class:`~aiohttp_json_api.context.RequestContext`)
+    :param registry_class:
+        Override of Registry class
+        (must be subclass of :class:`~aiohttp_json_api.registry.Registry`)
+    :param custom_handlers:
+        Sequence or mapping with overrides of default JSON API handlers.
+
+        If your custom handlers named in conform with convention
+        of this application, then pass it as sequence::
+
+            custom_handlers=(get_collection, patch_resource)
+
+        If you have custom name of these handlers, then pass it as mapping::
+
+            custom_handlers={
+                'get_collection': some_handler_for_get_collection,
+                'patch_resource': another_handler_to_patch_resource
+            }
+
+    :param bool log_errors:
+        Log errors handled by
+        :func:`~aiohttp_json_api.middleware.jsonapi_middleware`
+    :param str routes_namespace:
+        Namespace of JSON API application routes
+    :return:
+        aiohttp Application instance with configured JSON API
+    :rtype: ~aiohttp.web.Application
+    """
+    from .common import JSONAPI, logger
+    from .context import RequestContext
+    from .middleware import jsonapi_middleware
+
+    if JSONAPI in app:
+        logger.warning('JSON API application is initialized already. '
+                       'Please check your aiohttp.web.Application instance '
+                       'does not have a "%s" dictionary key.', JSONAPI)
+        logger.error('Initialization of JSON API application is FAILED.')
+        return app
+
+    routes_namespace = routes_namespace \
+        if routes_namespace and isinstance(routes_namespace, str) \
+        else JSONAPI
+
+    if context_class is not None:
+        if not issubclass(context_class, RequestContext):
+            raise TypeError('Subclass of RequestContext is required. '
+                            'Got: {}'.format(context_class))
+    else:
+        context_class = RequestContext
+
+    app_registry = setup_app_registry(app, registry_class, schemas)
+
+    app[JSONAPI] = {
+        'context_class': context_class,
+        'meta': meta,
+        'jsonapi': {
+            'version': version,
+        },
+        'registry': app_registry,
+        'log_errors': log_errors,
+        'routes_namespace': routes_namespace
+    }
+
+    handlers = setup_custom_handlers(custom_handlers)
+
+    setup_resources(app, base_path, handlers, routes_namespace)
+
+    logger.debug('Registered JSON API resources list:')
+    for resource in filter(lambda r: r.name.startswith(routes_namespace),
+                           app.router.resources()):
+        logger.debug('%s -> %s',
+                     [r.method for r in resource], resource.get_info())
+
+    app.middlewares.append(jsonapi_middleware)
+
+    return app
---
+++ packages/aiohttp-json-api/aiohttp_json_api-0.33.1.tar.gz/aiohttp_json_api-0.33.1/aiohttp_json_api/handlers.py
@@ -0,0 +1,334 @@
+"""Handlers."""
+
+import collections
+from http import HTTPStatus
+
+from aiohttp import hdrs, web
+
+from .common import JSONAPI_CONTENT_TYPE, Relation
+from .decorators import jsonapi_handler
+from .errors import InvalidType
+from .helpers import get_router_resource
+from .jsonpointer import JSONPointer
+from .utils import (get_compound_documents, jsonapi_response, render_document,
+                    validate_uri_resource_id)
+
+__all__ = (
+    'get_collection',
+    'post_resource',
+    'get_resource',
+    'patch_resource',
+    'delete_resource',
+    'get_relationship',
+    'post_relationship',
+    'patch_relationship',
+    'delete_relationship',
+    'get_related'
+)
+
+
+@jsonapi_handler
+async def get_collection(request: web.Request, context, schema):
+    """
+    Fetch resources collection, render JSON API document and return response.
+
+    Uses the :meth:`~aiohttp_json_api.schema.BaseSchema.query_collection`
+    method of the schema to query the resources in the collection.
+
+    :seealso: http://jsonapi.org/format/#fetching
+    """
+    resources = await schema.query_collection(context=context)
+
+    compound_documents = None
+    if context.include and resources:
+        compound_documents, relationships = \
+            await get_compound_documents(resources, context)
+
+    result = await render_document(resources, compound_documents, context)
+
+    return jsonapi_response(result)
+
+
+@jsonapi_handler(content_type=JSONAPI_CONTENT_TYPE)
+async def post_resource(request: web.Request, context, schema):
+    """
+    Create resource, render JSON API document and return response.
+
+    Uses the :meth:`~aiohttp_json_api.schema.BaseSchema.create_resource`
+    method of the schema to create a new resource.
+
+    :seealso: http://jsonapi.org/format/#crud-creating
+    """
+    data = await request.json()
+    if not isinstance(data, collections.Mapping):
+        detail = 'Must be an object.'
+        raise InvalidType(detail=detail, source_pointer='')
+
+    resource = await schema.create_resource(
+        data=data.get('data', {}),
+        sp=JSONPointer('/data'),
+        context=context
+    )
+
+    result = await render_document(resource, None, context)
+
+    location = request.url.join(
+        get_router_resource(request.app, 'resource').url_for(
+            **schema.registry.ensure_identifier(resource, asdict=True)
+        )
+    )
+
+    return jsonapi_response(result, status=HTTPStatus.CREATED,
+                            headers={hdrs.LOCATION: str(location)})
+
+
+@jsonapi_handler
+async def get_resource(request: web.Request, context, schema):
+    """
+    Get single resource, render JSON API document and return response.
+
+    Uses the :meth:`~aiohttp_json_api.schema.BaseSchema.query_resource`
+    method of the schema to query the requested resource.
+
+    :seealso: http://jsonapi.org/format/#fetching-resources
+    """
+    resource_id = request.match_info.get('id')
+    validate_uri_resource_id(schema, resource_id, context)
+
+    resource = await schema.query_resource(resource_id, context)
+
+    compound_documents = None
+    if context.include and resource:
+        compound_documents, relationships = \
+            await get_compound_documents(resource, context)
+
+    result = await render_document(resource, compound_documents, context)
+
+    return jsonapi_response(result)
+
+
+@jsonapi_handler(content_type=JSONAPI_CONTENT_TYPE)
+async def patch_resource(request: web.Request, context, schema):
+    """
+    Update resource (via PATCH), render JSON API document and return response.
+
+    Uses the :meth:`~aiohttp_json_api.schema.BaseSchema.update_resource`
+    method of the schema to update a resource.
+
+    :seealso: http://jsonapi.org/format/#crud-updating
+    """
+    resource_id = request.match_info.get('id')
+    validate_uri_resource_id(schema, resource_id, context)
+
+    data = await request.json()
+    if not isinstance(data, collections.Mapping):
+        detail = 'Must be an object.'
+        raise InvalidType(detail=detail, source_pointer='')
+
+    old_resource, new_resource = await schema.update_resource(
+        resource_id, data.get('data', {}), JSONPointer('/data'),
+        context=context
+    )
+
+    if old_resource == new_resource:
+        return web.HTTPNoContent()
+
+    result = await render_document(new_resource, None, context)
+    return jsonapi_response(result)
+
+
+@jsonapi_handler
+async def delete_resource(request: web.Request, context, schema):
+    """
+    Remove resource.
+
+    Uses the :meth:`~aiohttp_json_api.schema.BaseSchema.delete_resource`
+    method of the schema to delete a resource.
+
+    :seealso: http://jsonapi.org/format/#crud-deleting
+    """
+    resource_id = request.match_info.get('id')
+    validate_uri_resource_id(schema, resource_id, context)
+
+    await schema.delete_resource(resource_id, context)
+    return web.HTTPNoContent()
+
+
+@jsonapi_handler
+async def get_relationship(request: web.Request, context, schema):
+    """
+    Get relationships of resource.
+
+    :param request: Request instance
+    :param context: Request context instance
+    :param schema: Schema instance
+    :return: Response
+    """
+    relation_name = request.match_info['relation']
+    relation_field = schema.get_relationship_field(
+        relation_name, source_parameter='URI'
+    )
+
+    resource_id = request.match_info.get('id')
+    validate_uri_resource_id(schema, resource_id, context)
+
+    pagination = None
+    if relation_field.relation is Relation.TO_MANY:
+        pagination_type = relation_field.pagination
+        if pagination_type:
+            pagination = pagination_type(request)
+
+    resource = await schema.query_resource(resource_id, context)
+    result = schema.serialize_relationship(relation_name, resource,
+                                           pagination=pagination)
+    return jsonapi_response(result)
+
+
+@jsonapi_handler(content_type=JSONAPI_CONTENT_TYPE)
+async def post_relationship(request: web.Request, context, schema):
+    """
+    Create relationships of resource.
+
+    Uses the :meth:`~aiohttp_json_api.schema.BaseSchema.add_relationship`
+    method of the schemato add new relationships.
+
+    :seealso: http://jsonapi.org/format/#crud-updating-relationships
+    """
+    relation_name = request.match_info['relation']
+    relation_field = schema.get_relationship_field(
+        relation_name, source_parameter='URI'
+    )
+    pagination = None
+
+    resource_id = request.match_info.get('id')
+    validate_uri_resource_id(schema, resource_id, context)
+
+    if relation_field.relation is Relation.TO_MANY:
+        pagination_type = relation_field.pagination
+        if pagination_type:
+            pagination = pagination_type(request)
+
+    data = await request.json()
+
+    old_resource, new_resource = await schema.add_relationship(
+        relation_name, resource_id, data, JSONPointer(''), context
+    )
+
+    if old_resource == new_resource:
+        return web.HTTPNoContent()
+
+    result = schema.serialize_relationship(relation_name, new_resource,
+                                           pagination=pagination)
+    return jsonapi_response(result)
+
+
+@jsonapi_handler(content_type=JSONAPI_CONTENT_TYPE)
+async def patch_relationship(request: web.Request, context, schema):
+    """
+    Update relationships of resource.
+
+    Uses the :meth:`~aiohttp_json_api.schema.BaseSchema.update_relationship`
+    method of the schema to update the relationship.
+
+    :seealso: http://jsonapi.org/format/#crud-updating-relationships
+    """
+    relation_name = request.match_info['relation']
+    relation_field = schema.get_relationship_field(
+        relation_name, source_parameter='URI'
+    )
+
+    resource_id = request.match_info.get('id')
+    validate_uri_resource_id(schema, resource_id, context)
+
+    pagination = None
+    if relation_field.relation is Relation.TO_MANY:
+        pagination_type = relation_field.pagination
+        if pagination_type:
+            pagination = pagination_type(request)
+
+    data = await request.json()
+    old_resource, new_resource = await schema.update_relationship(
+        relation_name, resource_id, data, JSONPointer(''), context
+    )
+
+    if old_resource == new_resource:
+        return web.HTTPNoContent()
+
+    result = schema.serialize_relationship(relation_name, new_resource,
+                                           pagination=pagination)
+    return jsonapi_response(result)
+
+
+@jsonapi_handler(content_type=JSONAPI_CONTENT_TYPE)
+async def delete_relationship(request: web.Request, context, schema):
+    """
+    Remove relationships of resource.
+
+    Uses the :meth:`~aiohttp_json_api.schema.BaseSchema.delete_relationship`
+    method of the schema to update the relationship.
+
+    :seealso: http://jsonapi.org/format/#crud-updating-relationships
+    """
+    relation_name = request.match_info['relation']
+    relation_field = schema.get_relationship_field(
+        relation_name, source_parameter='URI'
+    )
+
+    resource_id = request.match_info.get('id')
+    validate_uri_resource_id(schema, resource_id, context)
+
+    pagination = None
+    if relation_field.relation is Relation.TO_MANY:
+        pagination_type = relation_field.pagination
+        if pagination_type:
+            pagination = pagination_type(request)
+
+    data = await request.json()
+    old_resource, new_resource = await schema.remove_relationship(
+        relation_name, resource_id, data, JSONPointer(''), context
+    )
+
+    if old_resource == new_resource:
+        return web.HTTPNoContent()
+
+    result = schema.serialize_relationship(relation_name, new_resource,
+                                           pagination=pagination)
+    return jsonapi_response(result)
+
+
+@jsonapi_handler
+async def get_related(request: web.Request, context, schema):
+    """
+    Get related resources.
+
+    Uses the :meth:`~aiohttp_json_api.schema.BaseSchema.query_relative`
+    method of the schema to query the related resource.
+
+    :seealso: http://jsonapi.org/format/#fetching
+    """
+    relation_name = request.match_info['relation']
+    relation_field = schema.get_relationship_field(
+        relation_name, source_parameter='URI'
+    )
+    compound_documents = None
+    pagination = None
+
+    resource_id = request.match_info.get('id')
+    validate_uri_resource_id(schema, resource_id, context)
+
+    if relation_field.relation is Relation.TO_MANY:
+        pagination_type = relation_field.pagination
+        if pagination_type:
+            pagination = pagination_type(request)
+
+    relatives = await schema.query_relatives(relation_name, resource_id,
+                                             context)
+
+    if context.include and relatives:
+        compound_documents, relationships = \
+            await get_compound_documents(relatives, context)
+
+    result = await render_document(relatives, compound_documents, context,
+                                   pagination=pagination)
+
+    return jsonapi_response(result)
