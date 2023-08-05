481d3e06010a6a2efd325373f9bcb30e9d74421e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:08 +0000

    Add package myTestPypiTing/1.0.0/myTestPypiTing-1.0.0.tar.gz

---
+++ packages/myTestPypiTing/myTestPypiTing-1.0.0.tar.gz/myTestPypiTing-1.0.0/myTestPypiTing.py
@@ -0,0 +1,15 @@
+import sys
+import time
+
+
+class MyTestModle:
+    def __init__(self):
+        self.name = 'testname'
+
+    def run(self):
+        print('start running ..')
+
+
+if __name__ == '__main__':
+    m = MyTestModle
+    m.run()
---
+++ packages/myTestPypiTing/myTestPypiTing-1.0.0.tar.gz/myTestPypiTing-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='myTestPypiTing',
+    username='liujun',
+    version='1.0.0',
+    py_modules=['myTestPypiTing'],
+    author='liujun',
+    author_email='jun.mr@qq.com',
+    url='http://docs.51pub.cn',
+    description='one test tool',
+    license='MIT'
+)
