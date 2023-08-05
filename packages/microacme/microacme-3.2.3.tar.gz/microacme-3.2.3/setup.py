2b51e0deb6bd5d1bb8f5778c907b14179ea9846b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:12 +0000

    Add package microacme/3.2.3/microacme-3.2.3.tar.gz

---
+++ packages/microacme/microacme-3.2.3.tar.gz/microacme-3.2.3/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+setup_pth = os.path.dirname(__file__)
+readme_pth = os.path.join(setup_pth, 'README.md')
+reqs_pth = os.path.join(setup_pth, 'requirements.txt')
+reqs = list(filter(bool, open(reqs_pth).read().splitlines()))
+
+setup(
+    name='microacme',
+    version="3.2.3",
+    url="https://gitlab.mirus.io/domains/roadrunner/microacme/",
+    author="Don Spaulding II",
+    author_email="don@mirusresearch.com",
+    long_description=open(readme_pth).read(),
+    py_modules=["microacme"],
+    zip_safe=True,
+    install_requires=reqs,
+)
