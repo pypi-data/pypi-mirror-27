f42d43497dea29690628004bd55a04ae3f0180b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:58 +0000

    Add package ml1/0.1/ml1-0.1.tar.gz

---
+++ packages/ml1/ml1-0.1.tar.gz/ml1-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ml1',    
+    version='0.1',	
+	author = 'Vignesh Ravichandran',
+	author_email = 'vickykct@gmail.com',
+    scripts=['ml1']         
+)
+
