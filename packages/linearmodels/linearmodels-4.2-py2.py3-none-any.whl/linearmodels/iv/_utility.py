e7f8076a1d9cf7ca33ab8b9d22844f1161d47f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:00 +0000

    Add package linearmodels/4.2/linearmodels-4.2-py2.py3-none-any.whl

---
+++ packages/linearmodels/linearmodels-4.2-py2.py3-none-any.whl/linearmodels/iv/_utility.py
@@ -0,0 +1,102 @@
+import numpy as np
+from patsy.highlevel import dmatrices, dmatrix
+from patsy.missing import NAAction
+
+PARSING_ERROR = """
+Conversion of formula blocks to DataFrames using patsy failed.
+The formula blocks used for conversion were:
+
+dependent: {0}
+exogenous: {1}
+endogenous: {2}
+instruments: {3}
+
+The original Patsy error was:
+"""
+
+
+def proj(y, x):
+    """
+    Projection of y on x from y
+
+    Parameters
+    ----------
+    y : ndarray
+        Array to project (nobs by nseries)
+    x : ndarray
+        Array to project onto (nobs by nvar)
+
+    Returns
+    -------
+    yhat : ndarray
+        Projected values of y (nobs by nseries)
+    """
+    return x @ np.linalg.pinv(x) @ y
+
+
+def annihilate(y, x):
+    """
+    Remove projection of y on x from y
+
+    Parameters
+    ----------
+    y : ndarray
+        Array to project (nobs by nseries)
+    x : ndarray
+        Array to project onto (nobs by nvar)
+
+    Returns
+    -------
+    eps : ndarray
+        Residuals values of y minus y projected on x (nobs by nseries)
+    """
+    return y - proj(y, x)
+
+
+def parse_formula(formula, data):
+    """Parse an IV model formula"""
+    na_action = NAAction(on_NA='raise', NA_types=[])
+    if formula.count('~') == 1:
+        dep, exog = dmatrices(formula, data, return_type='dataframe', NA_action=na_action)
+        endog = instr = None
+        return dep, exog, endog, instr
+
+    elif formula.count('~') > 2:
+        raise ValueError('formula not understood.  Must have 1 or 2 '
+                         'occurrences of ~')
+
+    blocks = [bl.strip() for bl in formula.strip().split('~')]
+    if '[' not in blocks[1] or ']' not in blocks[2]:
+        raise ValueError('formula not understood. Endogenous variables and '
+                         'instruments must be segregated in a block that '
+                         'starts with [ and ends with ].')
+
+    dep = blocks[0].strip()
+    exog, endog = [bl.strip() for bl in blocks[1].split('[')]
+    instr, exog2 = [bl.strip() for bl in blocks[2].split(']')]
+    if endog[0] == '+' or endog[1] == '+':
+        raise ValueError(
+            'endogenous block must not start or end with +. This block was: {0}'.format(endog))
+    if instr[0] == '+' or instr[1] == '+':
+        raise ValueError(
+            'instrument block must not start or end with +. This block was: {0}'.format(instr))
+    if exog2:
+        exog += exog2
+
+    if exog:
+        exog = exog[:-1].strip() if exog[-1] == '+' else exog
+
+    try:
+        dep = dmatrix('0 + ' + dep, data, eval_env=2,
+                      return_type='dataframe', NA_action=na_action)
+        exog = '0 + ' + exog if exog else '0'
+        exog = dmatrix(exog, data, eval_env=2,
+                       return_type='dataframe', NA_action=na_action)
+        endog = dmatrix('0 + ' + endog, data, eval_env=2,
+                        return_type='dataframe', NA_action=na_action)
+        instr = dmatrix('0 + ' + instr, data, eval_env=2,
+                        return_type='dataframe', NA_action=na_action)
+    except Exception as e:
+        raise type(e)(PARSING_ERROR.format(dep, exog, endog, instr) + e.msg, e.args[1])
+
+    return dep, exog, endog, instr
---
+++ packages/linearmodels/linearmodels-4.2-py2.py3-none-any.whl/linearmodels/tests/panel/test_formula.py
@@ -0,0 +1,144 @@
+from itertools import product
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from linearmodels.formula import (between_ols, first_difference_ols, panel_ols,
+                                  pooled_ols, random_effects)
+from linearmodels.panel.model import (BetweenOLS, FirstDifferenceOLS, PanelOLS,
+                                      PooledOLS, RandomEffects)
+from linearmodels.tests.panel._utility import generate_data, datatypes
+
+pytestmark = pytest.mark.filterwarnings('ignore::linearmodels.utility.MissingValueWarning')
+
+PERC_MISSING = [0, 0.02, 0.10, 0.33]
+TYPES = datatypes
+
+
+@pytest.fixture(params=list(product(PERC_MISSING, TYPES)),
+                ids=list(map(lambda x: str(int(100 * x[0])) + '-' + str(x[1]),
+                             product(PERC_MISSING, TYPES))))
+def data(request):
+    missing, datatype = request.param
+    return generate_data(missing, datatype, ntk=(91, 7, 5))
+
+
+@pytest.fixture(params=['y ~ x1 + x2',
+                        'y ~ x0 + x1 + x2 + x3 + x4 '],
+                scope='module')
+def formula(request):
+    return request.param
+
+
+classes = [PooledOLS, BetweenOLS, FirstDifferenceOLS, RandomEffects]
+funcs = [pooled_ols, between_ols, first_difference_ols, random_effects]
+
+
+@pytest.fixture(params=list(zip(classes, funcs)))
+def models(request):
+    return request.param
+
+
+def test_basic_formulas(data, models, formula):
+    if not isinstance(data.y, pd.DataFrame):
+        return
+    joined = data.x
+    joined['y'] = data.y
+    model, formula_func = models
+    mod = model.from_formula(formula, joined)
+    res = mod.fit()
+    wmod = model.from_formula(formula, joined, weights=data.w)
+    wres = wmod.fit()
+
+    mod2 = formula_func(formula, joined)
+    res2 = mod2.fit()
+    np.testing.assert_allclose(res.params, res2.params)
+
+    parts = formula.split('~')
+    vars = parts[1].replace(' 1 ', ' const ').split('+')
+    vars = list(map(lambda s: s.strip(), vars))
+    x = data.x
+    res2 = model(data.y, x[vars]).fit()
+    wres2 = model(data.y, x[vars], weights=data.w).fit()
+    np.testing.assert_allclose(res.params, res2.params)
+    np.testing.assert_allclose(wres.params, wres2.params)
+    assert isinstance(mod, model)
+    assert mod.formula == formula
+
+    if model is FirstDifferenceOLS:
+        return
+
+    formula = formula.split('~')
+    formula[1] = ' 1 + ' + formula[1]
+    formula = '~'.join(formula)
+    mod = model.from_formula(formula, joined)
+    res = mod.fit()
+
+    mod2 = formula_func(formula, joined)
+    res2 = mod2.fit()
+    np.testing.assert_allclose(res.params, res2.params)
+
+    x['Intercept'] = 1.0
+    vars = ['Intercept'] + vars
+    mod2 = model(data.y, x[vars])
+    res2 = mod2.fit()
+    np.testing.assert_allclose(res.params, res2.params)
+    assert mod.formula == formula
+
+
+def test_basic_formulas_math_op(data, models, formula):
+    if not isinstance(data.y, pd.DataFrame):
+        return
+    joined = data.x
+    joined['y'] = data.y
+    formula = formula.replace('x0', 'np.exp(x0)')
+    formula = formula.replace('x1', 'np.arctan(x1)')
+    model, formula_func = models
+    model.from_formula(formula, joined).fit()
+
+
+def test_panel_ols_formulas_math_op(data):
+    if not isinstance(data.y, pd.DataFrame):
+        return
+    joined = data.x
+    joined['y'] = data.y
+    formula = 'y ~ x1 + np.exp(x2)'
+    mod = PanelOLS.from_formula(formula, joined)
+    mod.fit()
+
+
+def test_panel_ols_formula(data):
+    if not isinstance(data.y, pd.DataFrame):
+        return
+    joined = data.x
+    joined['y'] = data.y
+    formula = 'y ~ x1 + x2'
+    mod = PanelOLS.from_formula(formula, joined)
+    assert mod.formula == formula
+
+    formula = 'y ~ x1 + x2 + EntityEffects'
+    mod = PanelOLS.from_formula(formula, joined)
+    assert mod.formula == formula
+    assert mod.entity_effects is True
+    assert mod.time_effects is False
+
+    formula = 'y ~ x1 + x2 + TimeEffects'
+    mod = PanelOLS.from_formula(formula, joined)
+    assert mod.formula == formula
+    assert mod.time_effects is True
+    assert mod.entity_effects is False
+
+    formula = 'y ~ x1 + EntityEffects + TimeEffects + x2 '
+    mod = PanelOLS.from_formula(formula, joined)
+    assert mod.formula == formula
+    assert mod.entity_effects is True
+    assert mod.time_effects is True
+    mod2 = panel_ols(formula, joined)
+    res = mod.fit()
+    res2 = mod2.fit()
+    np.testing.assert_allclose(res.params, res2.params)
+
+    formula = 'y ~ x1 + EntityEffects + FixedEffects + x2 '
+    with pytest.raises(ValueError):
+        PanelOLS.from_formula(formula, joined)
