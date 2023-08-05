2ba3eaa97a344d7d4f073774195042d03623dd73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:20 +0000

    Add package vqsr_cnn/0.0.9/vqsr_cnn-0.0.9.tar.gz

---
+++ packages/vqsr_cnn/vqsr_cnn-0.0.9.tar.gz/vqsr_cnn-0.0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='vqsr_cnn',
+      version='0.0.9',
+      description='Variant quality score recalibration with Convolutional Neural Networks',
+      url='https://github.com/broadinstitute/dsde-deep-learning',
+      author='Sam Friedman',
+      author_email='sam@broadinstitute.org',
+      license='MIT',
+      packages=['vqsr_cnn'],
+      zip_safe=False)
