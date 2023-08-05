3d03516e5472c37ce72b29775f40cff7f3388e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:47 +0000

    Add package collate-cs/0.31/collate_cs-0.31.tar.gz

---
+++ packages/collate-cs/collate_cs-0.31.tar.gz/collate_cs-0.31/collate_cs/is_complete_line.py
@@ -0,0 +1,14 @@
+def _(line):
+    string = ''.join(line).strip()
+    if "class" in string:
+        return False
+    if string == '':
+        return False
+    if string[0] == '[':
+        return False
+    if string[0] == '(':
+        return False
+    if string[0:2] == '//':
+        return False
+    # filter for more open than closed brackets
+    return True
