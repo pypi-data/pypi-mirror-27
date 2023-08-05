9f3059affe25f12b652634babeaca9d0cbeea196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:42 +0000

    Add package editorium/0.0.0.9/editorium-0.0.0.9.tar.gz

---
+++ packages/editorium/editorium-0.0.0.9.tar.gz/editorium-0.0.0.9/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup( name = "editorium",
+       url = "https://bitbucket.org/mjr129/editorium",
+       version = "0.0.0.9",
+       description = "Creates a Qt Editor for arbitrary Python Objects using Reflection.",
+       author = "Martin Rusilowicz",
+       license = "GNU AGPLv3",
+       packages = ["editorium"],
+       python_requires = ">=3.6",
+       install_requires = ["py-flags",
+                           "sip",
+                           "PyQt5"]
+       )
