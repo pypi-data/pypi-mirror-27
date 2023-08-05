1ed9fc8272d62901fecf9520b66886554492cf77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package deeper-cxc/1.3.0/deeper_cxc-1.3.0.tar.gz

---
+++ packages/deeper-cxc/deeper_cxc-1.3.0.tar.gz/deeper_cxc-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name         = 'deeper_cxc',
+        version      = '1.3.0',
+        py_modules   = ['deeper_cxc'],
+        author       = 'joanna_cxc',
+        author_email = 'joanna_cxc@126.com',
+        description  = 'A simple printer of nested lists',
+     )
