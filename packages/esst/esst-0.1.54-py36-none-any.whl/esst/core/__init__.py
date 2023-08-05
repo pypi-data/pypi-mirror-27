cb454573db37f86f28efc8ce1fe57a4f29806354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:48 +0000

    Add package esst/0.1.54/esst-0.1.54-py36-none-any.whl

---
+++ packages/esst/esst-0.1.54-py36-none-any.whl/esst/core/__init__.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+"""
+Globals
+"""
+import everett
+
+# from .config import Config
+from .new_config import ESSTConfig
+from .context import Context
+from .logger import setup_logging
+from .status import Status, ServerStatus
+from .version import __version__
+
+CTX = Context()
+
+try:
+    CFG = ESSTConfig()
+    MAIN_LOGGER = setup_logging(CFG.debug, CFG.saved_games_dir)
+except everett.InvalidValueError as exception:
+    KEY = exception.key
+    if exception.namespace:
+        KEY = f'{exception.namespace}_{KEY}'
+    print(f'Invalid value for key: {KEY}')
+    exit(1)
+except everett.ConfigurationMissingError as exception:
+    KEY = exception.key
+    if exception.namespace:
+        KEY = f'{exception.namespace}_{KEY}'
+    print(f'Missing configuration for key: {KEY}')
+    exit(1)
