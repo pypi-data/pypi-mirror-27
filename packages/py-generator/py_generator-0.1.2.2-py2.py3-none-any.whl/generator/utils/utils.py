f5fd76f65cb56c9c29cef95a237614a99fce84bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:08 +0000

    Add package py-generator/0.1.2.2/py_generator-0.1.2.2-py2.py3-none-any.whl

---
+++ packages/py-generator/py_generator-0.1.2.2-py2.py3-none-any.whl/generator/utils/utils.py
@@ -0,0 +1,39 @@
+import ConfigParser
+import os
+import sys
+
+
+def get_config_dict(**kwargs):
+
+    if not kwargs.get('config_file'):
+        config_file = '%s/%s' % (os.getcwd(), 'generator.cfg')
+    else:
+        file_path = os.path.dirname(os.path.realpath(__file__))
+        config_file = '%s/%s' % (file_path, kwargs.get('config_file'))
+
+    config = ConfigParser.RawConfigParser()
+
+    if not os.path.isfile(config_file):
+        sys.exit("Expected to find configuration file with the setup")
+
+    config.readfp(open(config_file))
+
+    if not kwargs.get('section_key') and not kwargs.get('config_file'):
+        dirs_config_hash_map = config.items('dirs')
+    else:
+        dirs_config_hash_map = config.items(kwargs.get('section_key'))
+
+    dirs_config_dict = {}
+
+    for k, v in dirs_config_hash_map:
+        dirs_config_dict[k] = v
+
+    return dirs_config_dict
+
+
+def get_constants_dict():
+    return get_config_dict(config_file='constants.cfg', section_key='constants')
+
+
+def get_wildcards_dict():
+    return get_config_dict(config_file='constants.cfg', section_key='wildcards')
