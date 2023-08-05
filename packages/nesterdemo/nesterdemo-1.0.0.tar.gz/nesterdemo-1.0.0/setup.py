f9a17d43b895e02d2e41943e0221261783968a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:59 +0000

    Add package nesterdemo/1.0.0/nesterdemo-1.0.0.tar.gz

---
+++ packages/nesterdemo/nesterdemo-1.0.0.tar.gz/nesterdemo-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nesterdemo',
+    version='1.0.0',
+    py_modules=['nesterdemo'],
+    author='yueguanghai',
+    author_email='407221436@qq.com',
+    url='http://www.baidu.com',
+    description='A simple printer of nested lists',
+)
