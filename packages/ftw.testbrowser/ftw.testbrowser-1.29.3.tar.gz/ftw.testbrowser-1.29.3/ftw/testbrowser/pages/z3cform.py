3185ece9bfcd8bf1f40d6cc569a343b25fd9d4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:37 +0000

    Add package ftw.testbrowser/1.29.3/ftw.testbrowser-1.29.3.tar.gz

---
+++ packages/ftw.testbrowser/ftw.testbrowser-1.29.3.tar.gz/ftw.testbrowser-1.29.3/ftw/testbrowser/pages/z3cform.py
@@ -0,0 +1,30 @@
+from ftw.testbrowser import browser as default_browser
+from ftw.testbrowser.utils import normalize_spaces
+
+
+def erroneous_fields(form, browser=default_browser):
+    """Returns a mapping of erroneous fields (key is label or name of
+    the field) to a list of error messages for the fields on the form
+    passed as argument.
+
+    :param form: The form node to check for errors.
+    :type form: :py:class:`ftw.testbrowser.form.Form`
+    :param browser: A browser instance. (Default: global browser)
+    :type browser: :py:class:`ftw.testbrowser.core.Browser`
+    :returns: A dict of erroneous fields with error messages.
+    :rtype: dict
+    """
+
+    result = {}
+    for input in form.inputs:
+        if not input.parent('.field.error'):
+            continue
+
+        if input.label is not None:
+            label = input.label.text_content()
+        if not label:
+            label = input.name
+
+        errors = input.parent('.field').css('.fieldErrorBox').normalized_text()
+        result[normalize_spaces(label)] = errors
+    return result
