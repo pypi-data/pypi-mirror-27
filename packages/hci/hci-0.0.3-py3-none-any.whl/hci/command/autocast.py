16651571a097cae62fdcc78d0507f13afc155522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:20 +0000

    Add package hci/0.0.3/hci-0.0.3-py3-none-any.whl

---
+++ packages/hci/hci-0.0.3-py3-none-any.whl/hci/command/autocast.py
@@ -0,0 +1,25 @@
+from .opcode import OpCode
+from .commands import *
+
+
+def _autocast(pkt):
+    _opcode_to_class = {
+        OpCode.ATT_EXCHANGE_MTU_REQUEST: ATT_ExchangeMTUReq,
+        OpCode.GAP_DEVICE_DISCOVERY_REQUEST: GAP_DeviceDiscoveryRequest,
+        OpCode.GAP_DEVICE_INITIALIZATION: GAP_DeviceInit,
+        OpCode.GAP_ESTABLISH_LINK_REQUEST: GAP_EstablishLinkReq,
+        OpCode.GAP_GET_PARAMETER: GAP_GetParam,
+        OpCode.GAP_SET_PARAMETER: GAP_SetParam,
+        OpCode.GAP_TERMINATE_LINK_REQUEST: GAP_TerminateLinkReq,
+        OpCode.GATT_WRITE: GATT_WriteCharValue,
+        OpCode.HCI_RESET: HCI_Reset,
+        OpCode.UTIL_BUILD_REVISION: UTIL_BuildRevision,
+        OpCode.UTIL_FORCE_BOOT: UTIL_ForceBoot,
+    }
+
+    try:
+        pkt.__class__ = _opcode_to_class[pkt.opcode]
+    except KeyError:
+        pass
+
+    return pkt
