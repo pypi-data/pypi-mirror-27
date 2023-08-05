b67fff6b9964ff76951d9870a61a20f02e4a5fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:07 +0000

    Add package django-chemtrails/0.0.30/django-chemtrails-0.0.30.tar.gz

---
+++ packages/django-chemtrails/django-chemtrails-0.0.30.tar.gz/django-chemtrails-0.0.30/chemtrails/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+default_app_config = 'chemtrails.apps.ChemtrailsConfig'
+
+__title__ = 'django-chemtrails'
+__version__ = '0.0.30'
+__author__ = 'Rolf Haavard Blindheim'
+__license__ = 'MIT License'
+
+VERSION = __version__
+
+__all__ = [
+    'settings'
+]
