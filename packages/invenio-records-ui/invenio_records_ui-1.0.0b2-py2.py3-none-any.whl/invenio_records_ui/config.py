5ebdad5d3a0d1f7029c5db99f1e1a73f49b24539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:48 +0000

    Add package invenio-records-ui/1.0.0b2/invenio_records_ui-1.0.0b2-py2.py3-none-any.whl

---
+++ packages/invenio-records-ui/invenio_records_ui-1.0.0b2-py2.py3-none-any.whl/invenio_records_ui/config.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2016 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Flask extension for Invenio-Records-UI."""
+
+from __future__ import absolute_import, print_function
+
+RECORDS_UI_TOMBSTONE_TEMPLATE = "invenio_records_ui/tombstone.html"
+"""Configure the tombstone template."""
+
+RECORDS_UI_DEFAULT_PERMISSION_FACTORY = None
+"""Configure the default permission factory."""
+
+RECORDS_UI_LOGIN_ENDPOINT = "security.login"
+"""Endpoint where redirect the user if login is required."""
+
+RECORDS_UI_ENDPOINTS = {
+    "recid": {
+        "pid_type": "recid",
+        "route": "/records/<pid_value>",
+        "template": "invenio_records_ui/detail.html",
+    },
+    "recid_export": {
+        "pid_type": "recid",
+        "route": "/records/<pid_value>/export/<format>",
+        "view_imp": "invenio_records_ui.views.export",
+        "template": "invenio_records_ui/export.html",
+    }
+}
+"""Default UI endpoints.
+
+This option can be overwritten to describe the endpoints of the different
+record types.
+
+Each element on the dictionary represent a independent endpoint.
+
+The structure of the dictionary is as follows:
+
+.. code-block:: python
+
+    def my_view(pid, record, template=None):
+        return render_template(template, pid=pid, record=record)
+
+
+    def my_permission_factory(record, *args, **kwargs):
+        def can(self):
+            rec = Record.get_record(record.id)
+            return rec.get('access', '') == 'open'
+        return type('MyPermissionChecker', (), {'can': can})()
+
+
+    RECORDS_UI_ENDPOINTS = {
+        "<endpoint-name>": {
+            "pid_type": "<record-pid-type>",
+            "route": "/records/<pid_value>",
+            "template": "invenio_records_ui/detail.html",
+            "permission_factory_imp": "my_permission_factory",
+            "view_imp": my_view,
+            "record_class": "invenio_records.api:Record",
+            "methods": ["GET", "POST", "PUT", "DELETE"],
+        },
+        ...
+    }
+
+:param pid_type: Persistent identifier type for endpoint. Required.
+
+:param route: URL route (must include ``<pid_value>`` pattern). Required.
+
+:param template: Template to render.
+    (Default: ``invenio_records_ui/detail.html``)
+
+:param permission_factory_imp: Import path to factory that creates a
+        permission object for a given record. If the value is ``None``, then
+        no access control is done. (Default: ``None``)
+
+:param view_imp: Import path to view function. (Default: ``None``)
+
+:param record_class: Import path of record class.
+    (Default: ``invenio_records.api:Record``)
+
+:param methods: List of methods supported. (Default: ``['GET']``)
+"""
+
+RECORDS_UI_EXPORT_FORMATS = {}
+"""Defaut record serialization views.
+
+The structure of the dictionary is as follows:
+
+.. code-block:: python
+
+    RECORDS_UI_EXPORT_FORMATS = {
+        {"<pid-type>": {
+            "<format-slug>": {
+                "title": "<export format title>",
+                "serializer": "<object or import path to record serializer>",
+                "order": 1,
+            },
+            ...
+        },
+        ...
+    }
+"""
---
+++ packages/invenio-records-ui/invenio_records_ui-1.0.0b2-py2.py3-none-any.whl/invenio_records_ui/ext.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015, 2016 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Flask extension for Invenio-Records-UI."""
+
+from __future__ import absolute_import, print_function
+
+from . import config
+from .utils import obj_or_import_string
+from .views import create_blueprint
+
+
+class _RecordUIState(object):
+    """Record UI state."""
+
+    def __init__(self, app):
+        """Initialize state.
+
+        :param app: The Flask application.
+        """
+        self.app = app
+        self._permission_factory = None
+        self._export_formats = {}
+
+    def export_formats(self, pid_type):
+        """List of export formats."""
+        if pid_type not in self._export_formats:
+            fmts = self.app.config.get('RECORDS_UI_EXPORT_FORMATS', {}).get(
+                pid_type, {})
+            self._export_formats[pid_type] = sorted(
+                [(k, v) for k, v in fmts.items() if v],
+                key=lambda x: x[1]['order'],
+            )
+        return self._export_formats[pid_type]
+
+    @property
+    def permission_factory(self):
+        """Load default permission factory."""
+        if self._permission_factory is None:
+            imp = self.app.config['RECORDS_UI_DEFAULT_PERMISSION_FACTORY']
+            self._permission_factory = obj_or_import_string(imp)
+        return self._permission_factory
+
+
+class InvenioRecordsUI(object):
+    """Invenio-Records-UI extension.
+
+    The extension takes care of setting default configuration and registering
+    a blueprint with URL routes for the endpoints.
+    """
+
+    def __init__(self, app=None):
+        """Extension initialization.
+
+        :param app: The Flask application. (Default: ``None``)
+        """
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Flask application initialization.
+
+        :param app: The Flask application.
+        """
+        self.init_config(app)
+
+        # Register records blueprints
+        app.register_blueprint(
+            create_blueprint(app.config['RECORDS_UI_ENDPOINTS']))
+
+        app.extensions['invenio-records-ui'] = _RecordUIState(app)
+
+    def init_config(self, app):
+        """Initialize configuration on application.
+
+        :param app: The Flask application.
+        """
+        app.config.setdefault(
+            'RECORDS_UI_BASE_TEMPLATE',
+            app.config.get('BASE_TEMPLATE', 'invenio_records_ui/base.html')
+        )
+
+        for k in dir(config):
+            if k.startswith('RECORDS_UI_'):
+                app.config.setdefault(k, getattr(config, k))
