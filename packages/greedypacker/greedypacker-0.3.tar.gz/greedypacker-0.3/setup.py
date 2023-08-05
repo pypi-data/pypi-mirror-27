655aeafe97c478d876457358ed3291ffee05deb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package greedypacker/0.3/greedypacker-0.3.tar.gz

---
+++ packages/greedypacker/greedypacker-0.3.tar.gz/greedypacker-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+  name = 'greedypacker',
+  packages = ['greedypacker'],
+  version = '0.3',
+  description = 'A two dimensional binpacking library',
+  author = 'Solomon Bothwell',
+  author_email = 'ssbothwell@gmail.com',
+  license = 'Apache2',
+  url = 'https://github.com/ssbothwell/BinPack',
+  download_url = 'https://github.com/ssbothwell/BinPack/archive/0.3.tar.gz',
+  keywords = ['binpacking', 'algorithm', 'greedy', 'library'],
+  classifiers = [],
+  python_requires='>=3',
+)
