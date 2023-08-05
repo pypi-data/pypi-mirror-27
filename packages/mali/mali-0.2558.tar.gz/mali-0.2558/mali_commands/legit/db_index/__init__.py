86461afa56cf3df2c6794e896a477d502d0f0ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:16 +0000

    Add package mali/0.2558/mali-0.2558.tar.gz

---
+++ packages/mali/mali-0.2558.tar.gz/mali-0.2558/mali_commands/legit/db_index/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf8 -*-
+from .sqlite_db_index import SqliteMLIndex
+from .mysql_db_index import MySqlMLIndex
+from .spanner_db_index import SpannerMLIndex
+from .bigquery_db_index import BigQueryMLIndex
+from .backend_db_index import BackendMLIndex
