95de3af3125af73e0845147fab09aaecfc7a3f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:15 +0000

    Add package jss-resource-tools/1.15/jss_resource_tools-1.15.tar.gz

---
+++ packages/jss-resource-tools/jss_resource_tools-1.15.tar.gz/jss_resource_tools-1.15/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='jss_resource_tools',
+    description='A command line utility that utilises the Jamf Pro JSS API' \
+                ' in order to import, export and update JSS resources en-masse.',
+    version='1.15',
+    url='https://github.com/beyondthebox/JSS-Resource-Tools',
+    author='Beyond the Box Labs',
+    author_email='labs@beyondthebox.com.au',
+    scripts=['jss_resource_tools'],
+    license='MIT',
+    keywords='jamf jss casper beyondthebox'
+)
