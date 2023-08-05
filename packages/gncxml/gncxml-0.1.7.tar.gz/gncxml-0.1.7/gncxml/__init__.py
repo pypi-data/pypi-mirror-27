e843ecf4381bb2c0044695ecb19f0f15585802c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package gncxml/0.1.7/gncxml-0.1.7.tar.gz

---
+++ packages/gncxml/gncxml-0.1.7.tar.gz/gncxml-0.1.7/gncxml/__init__.py
@@ -0,0 +1,4 @@
+# vim: set fileencoding=utf-8 :
+"""extract entries in GnuCash data file as pandas.DataFrame"""
+
+from .book import Book
---
+++ packages/gncxml/gncxml-0.1.7.tar.gz/gncxml-0.1.7/setup.py
@@ -0,0 +1,23 @@
+# vim: set fileencoding=utf-8 :
+
+from setuptools import setup
+
+setup(
+        name="gncxml",
+        version="0.1.7",
+        description="Extract entries in GnuCash data file as pandas.DataFrame.",
+        long_description="Extract entries in GnuCash data file as pandas.DataFrame.",
+        author="LiosK",
+        author_email="contact@mail.liosk.net",
+        url="https://github.com/LiosK/gncxml",
+        scripts=["scripts/gncxml"],
+        packages=["gncxml"],
+        install_requires=["pandas"],
+        license="MIT License",
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Office/Business :: Financial",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            ],
+        )
