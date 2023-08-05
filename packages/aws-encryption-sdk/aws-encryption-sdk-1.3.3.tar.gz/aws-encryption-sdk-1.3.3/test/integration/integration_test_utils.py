3ffe0856372fd5c9d51a4ba05636a24a11e8fc20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:19 +0000

    Add package aws-encryption-sdk/1.3.3/aws-encryption-sdk-1.3.3.tar.gz

---
+++ packages/aws-encryption-sdk/aws-encryption-sdk-1.3.3.tar.gz/aws-encryption-sdk-1.3.3/test/integration/integration_test_utils.py
@@ -0,0 +1,44 @@
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""Utility functions to handle configuration, credentials setup, and test skip
+decision making for integration tests."""
+import os
+
+from aws_encryption_sdk.key_providers.kms import KMSMasterKeyProvider
+
+SKIP_MESSAGE = (
+    'Required environment variables not found. Skipping integration tests.'
+    ' See integration tests README.rst for more information.'
+)
+TEST_CONTROL = 'AWS_ENCRYPTION_SDK_PYTHON_INTEGRATION_TEST_CONTROL'
+AWS_KMS_KEY_ID = 'AWS_ENCRYPTION_SDK_PYTHON_INTEGRATION_TEST_AWS_KMS_KEY_ID'
+
+
+def skip_tests():
+    """Only run tests if both required environment variables are found."""
+    test_control = os.environ.get(TEST_CONTROL, None)
+    key_id = os.environ.get(AWS_KMS_KEY_ID, None)
+    return not (test_control == 'RUN' and key_id is not None)
+
+
+def get_cmk_arn():
+    """Retrieves the target CMK ARN from environment variable."""
+    return os.environ.get(AWS_KMS_KEY_ID)
+
+
+def setup_kms_master_key_provider():
+    """Reads the test_values config file and builds the requested KMS Master Key Provider."""
+    cmk_arn = get_cmk_arn()
+    kms_master_key_provider = KMSMasterKeyProvider()
+    kms_master_key_provider.add_master_key(cmk_arn)
+    return kms_master_key_provider
