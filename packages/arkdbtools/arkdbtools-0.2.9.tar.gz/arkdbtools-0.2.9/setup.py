ecbc44ad1908cf39d1060c0009f0318aea416e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:53 +0000

    Add package arkdbtools/0.2.9/arkdbtools-0.2.9.tar.gz

---
+++ packages/arkdbtools/arkdbtools-0.2.9.tar.gz/arkdbtools-0.2.9/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='arkdbtools',
+    version='0.2.9',
+    packages=['arkdbtools', 'arkdbtools.tests'],
+    url='https://github.com/Nijmegen-Consultancy-Group/arkdbtools',
+    license='MIT',
+    author='Charles',
+    author_email='k.l.kubat@gmail.com',
+    description='Database tool for interacting with the Ark blockchain'
+)
