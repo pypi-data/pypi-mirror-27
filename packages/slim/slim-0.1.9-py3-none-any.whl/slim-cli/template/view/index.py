41e0e93b1da2e4b056d4759eef4ec1c72b328110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package slim/0.1.9/slim-0.1.9-py3-none-any.whl

---
+++ packages/slim/slim-0.1.9-py3-none-any.whl/slim-cli/template/view/index.py
@@ -0,0 +1,16 @@
+from slim.base.view import BaseView
+from slim.retcode import RETCODE
+from view import route
+
+
+@route('/misc')
+class TestBaseView(BaseView):
+    @classmethod
+    def interface(cls):
+        cls.use('info', 'GET')
+
+    def info(self):
+        self.finish(RETCODE.SUCCESS, {
+            'retcode': list(RETCODE.items()),
+            #'retinfo': RETCODE.txt,
+        })
