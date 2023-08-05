2896da2fd56eeb09c9fd6a15d463c9d7e3750824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:44 +0000

    Add package motty/1.0.2/motty-1.0.2.tar.gz

---
+++ packages/motty/motty-1.0.2.tar.gz/motty-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = 'motty',
+  packages = ['motty'],
+  version = '1.0.2',
+  description = 'Simpler way to build mocking http server. Do not code yourself for mocking!',
+  author = 'David Lee',
+  author_email = 'scalalang2@gmail.com',
+  include_package_data = True,
+  scripts=['motty/runmotty.py'],
+  entry_points={'console_scripts': [
+    'runmotty = runmotty:run_motty',
+  ]},
+  install_requires=['django', 'djangorestframework', 'django-libsass', 'libsass', 'django-compressor',
+    'django-sass-processor', 'tornado'],
+  url = 'https://github.com/scalalang2/motty', # use the URL to the github repo
+  keywords = ['mocking', 'motty', 'http'], # arbitrary keywords
+  classifiers=[ 
+  ],
+)
