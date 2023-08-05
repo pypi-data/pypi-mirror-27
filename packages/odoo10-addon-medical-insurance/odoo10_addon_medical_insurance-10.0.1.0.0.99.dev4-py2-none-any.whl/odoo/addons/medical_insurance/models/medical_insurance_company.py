d3f683a3097ff39cfa8c6e86152cabb8b3bab0bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:07 +0000

    Add package odoo10-addon-medical-insurance/10.0.1.0.0.99.dev4/odoo10_addon_medical_insurance-10.0.1.0.0.99.dev4-py2-none-any.whl

---
+++ packages/odoo10-addon-medical-insurance/odoo10_addon_medical_insurance-10.0.1.0.0.99.dev4-py2-none-any.whl/odoo/addons/medical_insurance/models/medical_insurance_company.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015-TODAY LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, models
+
+
+class MedicalInsuranceCompany(models.Model):
+    _name = 'medical.insurance.company'
+    _description = 'Medical Insurance Providers'
+    _inherit = 'medical.abstract.entity'
+
+    @api.model
+    def _create_vals(self, vals):
+        vals.update({
+            'is_company': True,
+        })
+        return super(MedicalInsuranceCompany, self)._create_vals(vals)
