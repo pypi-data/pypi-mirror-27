07f279ca0f6fd67a04cfdf9610560351112d0771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:57 +0000

    Add package iceman/0.2.4/iceman-0.2.4-py2-none-any.whl

---
+++ packages/iceman/iceman-0.2.4-py2-none-any.whl/tests/test_traffic.py
@@ -0,0 +1,42 @@
+from iceman.configuration import Configuration
+from iceman.traffic import Traffic
+
+def configuration():
+    config = Configuration()
+    config.cipher_suite = config.initialize_cipher_suite('fernet')
+    return config
+
+
+class TestInstance(object):
+    def test_instance(self):
+        protocol = 'tcp'
+        port = 5000
+        queue = 9001
+        config = configuration()
+
+        subject = Traffic(protocol,
+                          port,
+                          queue,
+                          config.cipher_suite,
+                          config.encryption_enabled)
+        assert subject.__class__.__name__ == 'Traffic'
+        assert subject.port == port
+        assert subject.queue == queue
+
+class TestTransform(object):
+    def test_transform(self, mocker):
+
+        # mocker.patch('nfqueue.queue.set_verdict_modified')
+
+        protocol = 'tcp'
+        queue = 9001
+        config = configuration()
+
+        subject = Traffic(protocol,
+                          queue,
+                          config.cipher_suite,
+                          config.encryption_enabled)
+        # mocker.patch('nfqueue.set_verdict_modified')
+        # obj.ingress()
+        # assert Interceptor.intercept.call_count == 1
+
