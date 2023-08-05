9de9ed512591c69a35bf9ad3332fcdde2be5e2cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:44 +0000

    Add package melange/3.0.0/melange-3.0.0.tar.gz

---
+++ packages/melange/melange-3.0.0.tar.gz/melange-3.0.0/melange/messaging/__init__.py
@@ -0,0 +1,6 @@
+from .driver_manager import *
+from .event_message import *
+from .event_serializer import *
+from .exchange_listener import *
+from .messaging_driver import *
+from .threaded_exchange_message_consumer import *
---
+++ packages/melange/melange-3.0.0.tar.gz/melange-3.0.0/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+class install(_install):
+    def pre_install_script(self):
+        pass
+
+    def post_install_script(self):
+        pass
+
+    def run(self):
+        self.pre_install_script()
+
+        _install.run(self)
+
+        self.post_install_script()
+
+if __name__ == '__main__':
+    setup(
+        name = 'melange',
+        version = '3.0.0',
+        description = '',
+        long_description = '',
+        author = '',
+        author_email = '',
+        license = '',
+        url = '',
+        scripts = [],
+        packages = [
+            'melange',
+            'melange.domain_event_bus',
+            'melange.drivers',
+            'melange.messaging',
+            'melange.infrastructure',
+            'melange.drivers.aws'
+        ],
+        namespace_packages = [],
+        py_modules = ['__init__'],
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python'
+        ],
+        entry_points = {},
+        data_files = [],
+        package_data = {},
+        install_requires = [
+            'boto3',
+            'marshmallow',
+            'pyopenssl'
+        ],
+        dependency_links = ['git+https://github.com/Rydra/redis-simple-cache.git/@master#egg=redis-simple-cache-0'],
+        zip_safe = True,
+        cmdclass = {'install': install},
+        keywords = '',
+        python_requires = '',
+        obsoletes = [],
+    )
