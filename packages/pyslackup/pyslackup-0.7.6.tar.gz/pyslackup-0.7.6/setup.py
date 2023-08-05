49dff3cdba56316a3e47a699578b813732921343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:38 +0000

    Add package pyslackup/0.7.6/pyslackup-0.7.6.tar.gz

---
+++ packages/pyslackup/pyslackup-0.7.6.tar.gz/pyslackup-0.7.6/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyslackup',
+      version='0.7.6',
+      license='ISC',
+      description='Wrapper for SlackClient',
+      long_description=open('README.rst', 'r').read(),
+      author='Christian Bryn',
+      author_email='chr.bryn@gmail.com',
+      url='https://github.com/epleterte/slackup',
+      download_url='https://github.com/epleterte/slackup/archive/0.7.6.tar.gz',
+      platform='Linux',
+      packages=['slackup'],
+      py_modules=['slackup'],
+      keywords=['slack']
+     )
