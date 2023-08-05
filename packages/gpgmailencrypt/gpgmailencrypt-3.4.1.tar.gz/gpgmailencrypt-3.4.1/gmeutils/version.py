acf9ae8f93712ebcc201a1ac824aebf95602c880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:24 +0000

    Add package gpgmailencrypt/3.4.1/gpgmailencrypt-3.4.1.tar.gz

---
+++ packages/gpgmailencrypt/gpgmailencrypt-3.4.1.tar.gz/gpgmailencrypt-3.4.1/gmeutils/version.py
@@ -0,0 +1,23 @@
+#License GPL v3
+#Author Horst Knorr <gpgmailencrypt@gmx.de>
+VERSION="3.4.1"
+DATE="30.11.2017"
+COPYRIGHTYEAR="2017"
+unicodeerror="replace"
+#341 bugfix: doesn't add headers multiple times
+#340 pgpinline put attachments in zip container and add pdf content attachment
+#332
+#331 securitylevel=script and pdfpasswordmode=script, starting windows support 
+#330 mail decryption added
+#320 use additional keys for encryption
+#passwords user better hashing algorithms via passlib
+#sercurelevels may|redirect|bounce
+#database improvements
+#create tables
+#add pdf password storage in database
+#working mysql, sqlite3 and postgres.
+#MSSQL remains untested
+#Admin console supports createtable command
+#Admin console supports resetmessages command
+#gpgclass can extract public keys
+
