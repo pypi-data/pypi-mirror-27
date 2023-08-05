d5e50f497f38b5c93423e9e05b923e7316032826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:14 +0000

    Add package callspectpy/0.1.2/callspectpy-0.1.2.tar.gz

---
+++ packages/callspectpy/callspectpy-0.1.2.tar.gz/callspectpy-0.1.2/setup.py
@@ -0,0 +1,34 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.md') as ptr:
+    long_description = ptr.read()
+
+
+setup(
+    name="callspectpy",
+    description="Collects execution data from python script",
+    long_description=long_description,
+    license="MIT",
+    version="0.1.2",
+    author="xliiv",
+    author_email="tymoteusz.jankowski@gmail.com",
+    maintainer="xliiv",
+    maintainer_email="tymoteusz.jankowski@gmail.com",
+    url="https://github.com/xliiv/callspect",
+    python_requires='>=3.5',
+    packages=['callspectpy', 'examples'],
+    py_modules=["callspectpy_cli"],
+    entry_points={
+        'console_scripts': [
+            'callspectpy = callspectpy_cli:run',
+        ],
+    },
+    include_package_data=True,
+    classifiers=[
+      'Programming Language :: Python :: 3',
+    ]
+)
