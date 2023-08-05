dd857a12e7f04222967f9f565b2d925a63102ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:24 +0000

    Add package importable/0.2.2/importable-0.2.2.tar.gz

---
+++ packages/importable/importable-0.2.2.tar.gz/importable-0.2.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+
+cwd = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(cwd, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(cwd, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().split()
+
+setup(
+    name='importable',
+    version='0.2.2',
+
+    description=(
+        'Allows to import zip-compressed Python package by URL (http, hdfs).'),
+    long_description=long_description,
+    url='https://github.com/malexer/importable',
+
+    author='Alex (Oleksii) Markov',
+    author_email='alex@markovs.me',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords=('import url http hdfs zip importable python_path'),
+
+    packages=['importable'],
+    install_requires=requirements,
+)
