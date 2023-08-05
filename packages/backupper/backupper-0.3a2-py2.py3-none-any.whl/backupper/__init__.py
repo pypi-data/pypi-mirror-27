a5ac8108f135b5a3e755b1d495440e282871f017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package backupper/0.3a2/backupper-0.3a2-py2.py3-none-any.whl

---
+++ packages/backupper/backupper-0.3a2-py2.py3-none-any.whl/backupper/__init__.py
@@ -0,0 +1,7 @@
+from . import utils
+#from . import connect
+
+__version__ = "0.3a2"
+
+__all__ = ["utils"]
+#__all__ = ["utils", "connect"]
---
+++ packages/backupper/backupper-0.3a2-py2.py3-none-any.whl/backupper/connect/__init__.py
@@ -0,0 +1,3 @@
+from .utils import *
+from .dummystorage import DummyStorage
+from .ftpstorage import FTPStorage
