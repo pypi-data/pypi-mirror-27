a5745f786e5b33b607797c67d55042d190b76853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:36 +0000

    Add package trafaret/1.0.1/trafaret-1.0.1-py3-none-any.whl

---
+++ packages/trafaret/trafaret-1.0.1-py3-none-any.whl/trafaret/contrib/rfc_3339.py
@@ -0,0 +1,25 @@
+from dateutil.parser import parse
+from datetime import datetime
+from .. import Trafaret, str_types
+
+
+class DateTime(Trafaret):
+    """ Class for support parsing date im RFC3339 formats
+        via dateutil.parse helper
+    """
+    convertable = str_types + (datetime,)
+    value_type = datetime
+
+    def __init__(self, allow_blank=False):
+        self.allow_blank = allow_blank
+
+    def __repr__(self):
+        return "<Date(blank)>" if self.allow_blank else "<Date>"
+
+    def check_and_return(self, value):
+        if isinstance(value, datetime):
+            return value
+        try:
+            return parse(value)
+        except (ValueError, TypeError) as e:
+            self._failure(str(e))
