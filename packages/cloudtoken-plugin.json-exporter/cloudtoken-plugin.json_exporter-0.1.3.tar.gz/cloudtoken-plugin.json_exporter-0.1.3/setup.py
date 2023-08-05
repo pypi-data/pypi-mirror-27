ed2bf0810fa6d465e621d7aec9621055af0cf0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:41 +0000

    Add package cloudtoken-plugin.json-exporter/0.1.3/cloudtoken-plugin.json_exporter-0.1.3.tar.gz

---
+++ packages/cloudtoken-plugin.json-exporter/cloudtoken-plugin.json_exporter-0.1.3.tar.gz/cloudtoken-plugin.json_exporter-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import os
+
+version = os.environ['VERSION']
+
+setup(
+    name='cloudtoken-plugin.json_exporter',
+    version=version,
+    description='JSON exporter for cloudtoken.',
+    url='https://bitbucket.org/atlassian/cloudtoken',
+    author='Atlassian Cloud Engineering',
+    author_email='cloud-team@atlassian.com',
+    license='Apache',
+    py_modules=['cloudtoken_plugin.export_credentials_json'],
+    zip_safe=False,
+    install_requires=[],
+)
