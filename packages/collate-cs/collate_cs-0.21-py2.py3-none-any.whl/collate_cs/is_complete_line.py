2654b4ad70106a755e6dca53897348f27cf3d1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:15 +0000

    Add package collate-cs/0.21/collate_cs-0.21-py2.py3-none-any.whl

---
+++ packages/collate-cs/collate_cs-0.21-py2.py3-none-any.whl/collate_cs/is_complete_line.py
@@ -0,0 +1,22 @@
+def _(line):
+    string = ''.join(line).strip()
+    if string[0:4] == 'for ':
+        return False
+    if string[0:2] == 'if':
+        return False
+    if 'public class' in string:
+        return False
+    if 'public' in string and ')' in string:
+        return False
+    if string == '':
+        return False
+    if string[0] == '[':
+        return False
+    if string[0] == '(':
+        return False
+    if string[0:2] == '//':
+        return False
+    if string == 'get':
+        return False
+    # filter for more open than closed brackets
+    return True
