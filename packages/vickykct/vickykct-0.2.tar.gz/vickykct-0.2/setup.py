b8a8aebac9b9e40adf371d371a0f2e96dbc39df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:45 +0000

    Add package vickykct/0.2/vickykct-0.2.tar.gz

---
+++ packages/vickykct/vickykct-0.2.tar.gz/vickykct-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='vickykct',    
+    version='0.2',	
+	author = 'Vignesh Ravichandran',
+	author_email = 'vickykct@gmail.com',
+    scripts=['vickykct']         
+)
+
