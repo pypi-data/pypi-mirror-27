fba45d139669f4177fa3d4af9a6549928b442300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:27 +0000

    Add package phenum/1.5.4/phenum-1.5.4-py2.7.egg

---
+++ packages/phenum/phenum-1.5.4-py2.7.egg/phenum/io_utils.py
@@ -0,0 +1,326 @@
+"""Methods for reading and writing enumeration and polya-counting results."""
+def read_group(fname):
+    """Reads the symmetry group in from the 'rot_perms' styled group
+    output by enum.x.
+
+    :arg fname: path to the file to read the group from.
+    """
+    i=0
+    groupi = []
+    with open(fname) as f:
+        for line in f:
+            if i > 5:
+                if ('Perm #:') in line:
+                    groupi.append(map(int, line.split()[4::]))
+                else:
+                    groupi[-1] += map(int, line.split())
+            i += 1
+    from numpy import array
+    return(map(list, array(groupi)-1))
+
+def read_lattice(filename='lattice.in', verbose=False):
+    """Reads the lattice.in file; returns a dictionary with the following fields:
+      "sizes": the range of cell sizes,
+      "lat_vecs": lattice vectors of the parent cell,
+      "nspecies": the number of atomic species in the enumeration,
+      "basis_vecs": basis vectors of the parent cell,
+      "is_crestricted": logical that indicates if the concentrations will be restricted,
+      "arrows": logical that indicates if arrows are present,
+      "concs": array of the concentrations in format [1,3].
+    """
+    from msg import info
+    with open(filename,'r') as f:
+        lines = [l for l in f.readlines() if l.strip()[0] != '#']
+        
+    sizes = [int(el) for el in lines[0].split(' ')]
+    bulk = lines[1].strip()[0].lower() == 'b'
+    lat_vecs = [map(float, l.split()) for l in lines[2:5]]
+    nspecies = int(lines[5])
+    nbas = int(lines[6])
+    bas_vecs = [map(float, l.split()) for l in lines[7:7+nbas]]
+
+    arrows = False
+    res_conc = False
+    if lines[7+nbas].strip() == 'F':
+        info('No concentration restrictions given.')
+        concs = []
+    else:
+        res_conc = True
+        info('Concentrations are being restrictied.')
+        arrows = lines[7+nbas+1].strip()
+        if arrows == 'F':
+            arrows = []
+            info('No displacement directions are included')
+        else:
+            arrows = True
+        concs = [map(float, l.split()) for l in lines[7+nbas+2:]]
+
+    result = {
+        "bulk": bulk,
+        "sizes": sizes,
+        "lat_vecs": lat_vecs,
+        "nspecies": nspecies,
+        "basis_vecs": bas_vecs,
+        "is_crestricted": res_conc,
+        "arrows": arrows,
+        "concs": concs
+    }
+    return result
+
+# This method reads in the enum.in file.
+def read_enum(filename="enum.in"):
+    """Reads the list of structures and the number of random candidates to enumerate
+    from the 'enum.in' styled file. It should contain only integers and have:
+
+       HNF concs num_wanted
+    where,
+
+    HNF: the list of 6 independent entries in the HNF matrix;
+    concs: list of integer species concentrations;
+    num_wanted: number of random structures to draw from the enumerated list.
+    """    
+    from numpy import loadtxt
+    raw = loadtxt(filename, int, ndmin=2)
+    systems = []
+    for i in range(len(raw)):
+        HNF = raw[i,0:6]
+        sys_conc = raw[i,6:-1]
+        num_wanted = raw[i,-1]
+        systems.append((HNF, sys_conc, num_wanted))
+    return systems
+
+def write_enum(params, outfile="enum.out"):
+    """Writes a 'struct_enum.out' style preamble to the specified output file
+    using the enumeration parameters gleaned from a 'lattice.in' styled file.
+
+    :arg params: values returned from method:read_lattice().
+    :arg outfile: path to desired output file.
+    """
+    from datetime import datetime
+    lines = []
+    lines.append("Random structure enumeration: %s" % (str(datetime.now())))
+    lines.append("bulk" if params["bulk"] else "surface")
+    lines.extend([" {0:.7f}       {1:.7f}       {2:.7f}".format(*l) for l in params["lat_vecs"]])
+    lines.append("    {0:d}".format(len(params["basis_vecs"])))
+    lines.extend(["  {0:.7f}       {1:.7f}       {2:.7f}".format(*l) for l in params["basis_vecs"]])
+    lines.append(" {0:d}-nary case".format(params["nspecies"]))
+    lines.append("   " + '   '.join(map(str, params["sizes"])))
+    lines.append(" 1e-7")
+    lines.append("Concentration check:")
+    lines.append("    T" if params["is_crestricted"] else "    F")
+    lines.extend(["full list of labelings (including incomplete labelings) is used ",
+                  "Equivalency list: 1 #Not used in the random enumeration algorithm ",
+                  "start   #tot      HNF     Hdegn   labdegn   Totdegn   #size idx    "
+                  "pg    SNF             HNF                 Left "
+                  "transform                          labeling                 "
+                  "directions",
+                  ""])
+        
+    with open(outfile, 'w') as f:
+        f.write('\n'.join(lines))
+
+def write_struct_enum(params):
+    """Writes a 'struct_enum.in' file for the executable enum.x.
+
+    :arg params: values returned from method:read_lattice().
+    """
+
+    with open("struct_enum.in","w+") as struct_file:
+        struct_file.write("System \n")
+
+        if params["bulk"] == True:
+            struct_file.write("bulk \n")
+        else:
+            struct_file.write("surface \n")
+
+        latt = ""
+        i = 0
+        for vec in params["lat_vecs"]:
+            for point in vec:
+                latt += str(point) + " "
+            if i < 2:
+                latt += "\n"
+            i += 1
+
+        struct_file.write(latt + " \n")
+
+        struct_file.write(str(params["nspecies"]) + " \n")
+
+        occupancy = ""
+        for i in range(params["nspecies"]):
+            if i < params["nspecies"]-1:
+                occupancy += str(i) +"/"
+            else:
+                occupancy += str(i)
+    
+        basis = ""
+        for vec in params["basis_vecs"]:
+            basis += "{}  {} \n".format(" ".join([str(point) for point in vec]),occupancy)
+        
+        struct_file.write(str(len(params["basis_vecs"])) + " \n")
+        struct_file.write(basis + " \n")
+
+        size_range = ""
+        i = 0
+        for point in params["sizes"]:
+            size_range += str(point) + " "
+
+        struct_file.write(size_range + " \n")
+        struct_file.write("0.00001 \n")
+        struct_file.write("full \n")
+
+def which(program):
+    """Determines where if an executable exists on the users path.
+    This code was contributed by Jay at http://stackoverflow.com/a/377028
+    :args program: The name, or path for the program
+    """
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+def create_labeling(config):
+    """This routine takes a string of atomic locations as a vector and the
+    atomic concentrations and returns a unique labeling.
+
+    :arg config: list of integers describing the arrangement of atoms on
+    the lattice.
+    """
+    label = ''
+    arrow =  ''
+    if isinstance(config[0],list):
+        for i in config:
+            label += str(i[1]-1)
+            arrow += str(i[0]+1)
+    else:
+        for i in config:
+            label += str(i-1)
+            arrow += '0'
+    
+    return(label,arrow)
+
+def read_enum_out(args):
+    """Reads the enum.out file and builds a dictionary with the needed
+    information to construct a POSCAR.
+
+    :arg args: The makeStr.py input arguments
+    """
+
+    # which structures are wanted
+    if args["structures"] == None:
+        with open(args["input"],"r") as f:
+            for count, l in enumerate(f):
+                pass
+        structures = range(1,count-13)
+    else:
+        structures = args["structures"]
+    # open the enum.out style file.
+    structf = open(args["input"],"r")
+
+    # we'll build a dictionary of the system data and a list of
+    # dictionaries for the structures that are wanted.
+    structure_data = []
+    system = {}
+    system["plattice"] = []
+    system["dvecs"] = []
+    line_count = 1
+    system["nD"] = 0
+    for line in structf:
+        temp = line.rstrip()
+        if not temp.startswith("#"):
+            if line_count == 1:
+                system["title"] = temp
+            if line_count == 2:
+                system["bulksurf"] = temp
+            if line_count in [3,4,5]:
+                vec = [float(v) for v in temp.split()]
+                system["plattice"].append(vec)
+            if line_count == 6:
+                system["nD"] = int(temp.rstrip())
+            if system["nD"] != 0 and line_count in range(7,7+system["nD"]):
+                vec = [float(v) for v in temp.split()]
+                system["dvecs"].append(vec)
+            if line_count == 7+system["nD"]:
+                system["k"] = int(temp.split('-')[0].strip())
+            if line_count == 9 + system["nD"]:
+                system["eps"] = float(temp.strip())
+            if line_count - (14 + system["nD"]) in structures:
+                data = temp.split()
+                this_struct = {}
+                this_struct["strN"] = int(data[0])
+                this_struct["hnfN"] = int(data[1])
+                this_struct["hnf_degen"] = int(data[2])
+                this_struct["lab_degen"] = int(data[3])
+                this_struct["tot_degen"] = int(data[4])
+                this_struct["sizeN"] = int(data[5])
+                this_struct["n"] = int(data[6])
+                this_struct["pgOps"] = int(data[7])
+                this_struct["diag"] = [int(data[8]),int(data[9]),int(data[10])]
+                this_struct["HNF"] = [[int(data[11]),0,0],[int(data[12]),int(data[13]),0],
+                                      [int(data[14]),int(data[15]),int(data[16])]]
+                this_struct["L"] = [[int(data[17]),int(data[18]),int(data[19])],
+                                    [int(data[20]),int(data[21]),int(data[22])],
+                                      [int(data[23]),int(data[24]),int(data[25])]]
+                this_struct["labeling"] = data[26]
+                this_struct["directions"] = data[27]                
+                structure_data.append(this_struct)
+        line_count += 1
+
+    return (system, structure_data)
+
+def write_POSCAR(system_data,space_data,structure_data,displacement):
+    """Writes a vasp POSCAR style file for the input structure and system
+    data.
+
+    :arg system_data: a dictionary of the system_data
+    :arg space_data: a dictionary containing the spacial data
+    :arg structure_data: a dictionary of the data for this structure
+    """
+
+    from numpy import array
+    
+    filename = "vasp.{}".format(str(structure_data["strN"]))
+
+    labeling = structure_data["labeling"]            
+    gIndx = space_data["gIndx"]
+    arrows = structure_data["directions"]
+
+    arrow_directions = [[0,0,0],[0,0,-1],[0,-1,0],[-1,0,0],[1,0,0],[0,1,0],[0,0,1]]
+    directions = []
+    for arrow in arrows:
+        directions.append(array(arrow_directions[int(arrow)]))
+    
+    with open(filename,"w+") as poscar:
+        poscar.write("{} str #: {}\n".format(str(system_data["title"]),str(structure_data["strN"])))
+        poscar.write("1.00\n")
+        for i in range(3):
+            poscar.write(" {}\n".format(" ".join(
+                ["{0: .8f}".format(j) for j in space_data["sLV"][i]])))
+        poscar.write(" ")
+        for i in range(system_data["k"]):
+            ic = 0
+            for iAt in range(structure_data["n"]*system_data["nD"]):
+                if labeling[gIndx[iAt]] == str(i):
+                    ic += 1
+            poscar.write("{}   ".format(str(ic)))
+        poscar.write("\n")
+        poscar.write("D\n")
+        for ilab in range(system_data["k"]):
+            for iAt in range(structure_data["n"]*system_data["nD"]):
+                if labeling[gIndx[iAt]] == str(ilab):
+                    out_array = array(space_data["aBas"][iAt]) + directions[iAt]*displacement
+                    poscar.write(" {}\n".format(
+                        "  ".join(["{0: .8f}".format(i) for i in out_array.tolist()])))
+        
---
+++ packages/phenum/phenum-1.5.4-py2.7.egg/phenum/phonons.py
@@ -0,0 +1,339 @@
+"""Methods needed for the enumeration of arrows."""
+
+#import neede modules
+from copy import deepcopy
+
+#Find concentration takes a 1D array, col, and returns a 1D array, 
+#Concs, containing the number of times each element in col appears 
+#in the array.
+def _find_concentrations(col):
+    """Finds the concentration of a given array of colors.
+
+    :arg col: an integer array of the labeling
+    """
+    Concs=[]
+
+    tcol=deepcopy(col)
+    for i in range(len(tcol)):
+        if tcol[i] != 0:
+            Concs.append(tcol.count(tcol[i]))
+            for j in range(len(tcol)):
+                if tcol[j] == tcol[i] and i != j:
+                    tcol[j] =0
+            tcol[i] =0
+    return(Concs)
+
+#Takes an array of colors and arrows and tells us how many different
+#colors have arrows on them.
+def how_many_arrows(tcol):
+    """Determines the number of colors that have arrows on them.
+
+    :arg tcol: a 2D array of the labeling that contains the colors
+    and arrows for each site.
+    """
+    arrows = 0
+    
+    species = []
+    for i in tcol:
+        if i[0] >= 0:
+            arrows += 1
+            if i[1] not in species:
+                species.append(i[1])
+
+    aconcs = _find_concentrations(tcol)
+    return(arrows,len(species),aconcs)
+
+#An algorithm for sorting the col array so that colors without arraws
+#appear first in the list by concentration followed by the colors with
+#arraws that are also sorted by concentration.
+def _col_sort(col_list):
+    """Sorts the labeling so that colors with arrows appear last in the
+    list and so that the arrowed and non-arrowed colors are sorted
+    from lowest to highest concentration.
+
+    :arg col_list: a 2D integer array of the full labeling of the
+    system
+    """
+
+    col1 = []
+    col2 = []
+    colt=[]
+
+    # seperate the arrays into colors with arrrows and colors without
+    for i in col_list:
+        if i[0] < 0:
+            col1.append(i)
+        elif i[0] >= 0:
+            col2.append(i)
+
+    # sort each array by concentration
+    col1 = sorted(col1, key = col1.count, reverse=True)
+    col2 = sorted(col2, key = col2.count, reverse=True)
+
+    # put them back together again
+    for i in col1:
+        colt.append(i)
+    for i in col2:
+        colt.append(i)
+    
+    return(colt)
+
+#this method takes a configuration and finds the unique ways of
+#placing the arrows on that configuration.
+#col is the initial configuration.
+#agroup is the group operations with their effects on the arrows.
+#dim is the number of arrow directions that are possible for this system.
+def add_arrows(col,agroup,dim):
+    """Finds the unique arrangements of arrows for a given configuration.
+
+    :arg col: a 2D integer array of the initial labeling
+    :arg agroup: the stabilizers for the colors only with the arrow
+    permutations
+    :arg dim: the number of directions the arrows can point
+    """
+    #survivors is the array that contains the end result of the permutations
+    survivor = []
+
+    #Find out how many arrows there are in the col array
+    (narrows,arrow_types,conc_w_arrows) = how_many_arrows(col)
+    #largest_arrow is largest value any arrow could have.
+    largest_arrow = [dim-1]*narrows
+    #this is an array for storing the unique arrangements.
+    arsurvivors = []
+
+    #this loop runs over every possible configuration of arrows
+    #staring from the one with the smallest hash number and ending
+    #with the one with the largest hash number to see if they are
+    #unique. Each hash will be stored in orighash (original hash) then
+    #compared to the permuted arrows hash (permhash) at the end.
+    for orighash in range(_ahash(largest_arrow,dim)+1):
+        arrow_config = _ainvhash(orighash,narrows,dim)
+        unique = True
+        temp_coloring_with_arroms = [0]*len(col)
+        #we need to cycle through each of the operations in the
+        #permutation group that got passed in to see if they will turn
+        #the arrow configuration into one we've already seen.
+        for perm in agroup:
+            l = 0
+            #this loop constucts the coloring with the correct arrows
+            #for the permutation group to be applied to.
+            for z in range(len(col)):
+                if col[z][0] < 0:
+                    temp_coloring_with_arroms[z] = deepcopy(col[z])
+                elif col[z][0] >= 0:
+                    temp_coloring_with_arroms[z] = deepcopy(col[z])
+                    temp_coloring_with_arroms[z][0] = arrow_config[l]
+                    l += 1
+
+            new_arrow_config=[]
+            arrow_perm = perm[1]
+            color_perm = perm[0]
+            new_coloring_with_arrows = []
+            #here we use the permutations of the colors to permute the
+            #colors for our configuration.
+            for j in range(len(temp_coloring_with_arroms)):
+                new_coloring_with_arrows.append(temp_coloring_with_arroms[color_perm[j]])
+            #if there is an arrow on any lattice site it needs to
+            #be updated. Otherwise just permute the colors.
+            for site in new_coloring_with_arrows:
+                if site[0] >= 0:
+                    site[0] = arrow_perm[site[0]]
+                    new_arrow_config.append(site[0])
+            #if the new configuration has a smaller hash number than
+            #the current configuration then we have seen it before and
+            #it is not unique.
+            permhash = _ahash(new_arrow_config,dim)
+            if permhash < orighash:
+                unique = False
+                break
+        #if none of the permutations have had a smaller hash number
+        #than the current configuration then the configuration is
+        #unique and we need to add it to the list.
+        if unique == True:
+            #this loop make the coloring with the arrows that will be
+            #added to the list of unique configurations.
+            coloring_with_arrows = [0]*len(col)
+            i = 0
+            for z in range(len(col)):
+                if col[z][0] < 0:
+                    coloring_with_arrows[z] = deepcopy(col[z])
+                elif col[z][0] >= 0:
+                    coloring_with_arrows[z] = deepcopy(col[z])
+                    coloring_with_arrows[z][0] = arrow_config[i]
+                    i += 1
+                    
+            arsurvivors.append(coloring_with_arrows)
+
+    return(arsurvivors)
+
+# arrow_concs is a method that returns the concentration string
+# including the arrows
+def arrow_concs(cList,aconcs):
+    """Uses the concentrations of the atoms and the arrows to make a
+    labeling for the system. It then sorts them to be in the correct
+    order for the code.
+
+    :arg cListr: an integer array the concentration of the colors 
+    :arg aconcs: an array of floats of the fractional number of arrows
+    for each color
+    """
+
+    aconcs = [int(cList[i]*aconcs[i]) for i in range(len(cList))]
+    
+    species = 1
+    conc_w_arrows = []
+    for i in range(len(aconcs)):
+        na = aconcs[i]
+        ns = cList[i]
+        if ns >= na:
+            while na > 0:
+                conc_w_arrows.append([1,species])
+                na -= 1
+                ns -= 1
+            while ns > 0:
+                conc_w_arrows.append([-1,species])
+                ns -= 1
+        else:
+            while ns > 0:
+                conc_w_arrows.append([-1,species])
+                ns -= 1
+        species += 1
+
+    conc_w_arrows = _col_sort(conc_w_arrows)
+
+    return(conc_w_arrows)
+
+def get_arrow_concs(params):
+    """If the concentrations are being restricted then find the correct 
+    arrow for each species included.
+
+    :arg params: the lattice.in parameters.
+    """
+    a_concs = []
+    if params["is_crestricted"]:
+        for c in params["concs"]:
+            if (len(c) == 4 and params["arrows"]):
+                a_concs.append(c[3])
+            else:
+                a_concs.append(0)
+    else:
+        for i in range(params["nspecies"]):
+            a_concs.append(0)
+    return a_concs
+
+def enum_sys(groupfile, concs, a_concs, num_wanted, HNF, params):
+    """Enumerates a random subset of the unique structures that have the shape
+    defined by the symmetry group and the specified concentration.
+
+    :arg groupfile: path to the file containing the symmetry group.
+    :arg concs: list of integer concentrations for each species.
+    :arg a_concs: list of integer *arrow* concentrations for each species.
+    :arg num_wanted: the number of structures to pick randomly from the enumerated
+      list.
+    :args HNF: the HNF for the system we're currently enumerating
+    :args params: the dictionary of parameters read in from lattice.in
+    """
+
+    from phenum.grouptheory import get_full_HNF, get_sym_group
+    from phenum.polyaburnside import polya
+    from phenum.tree import brancher
+    import phenum.io_utils as io
+
+    decorations = arrow_concs(concs, a_concs)
+    # get the symmetry group for this HNF. Assumes the group can be
+    # found in the file labeled by (this_HNF)_sym_group.out
+    if groupfile == None:
+        sym_g = get_sym_group(params["lat_vecs"],params["basis_vecs"],get_full_HNF(HNF),3)
+        agroup = []
+        for i in range(len(sym_g.perm.site_perm)):
+            agroup.append([sym_g.perm.site_perm[i],sym_g.perm.arrow_perm[i]])
+    else:
+        group = io.read_group(groupfile)
+        # get symgroup from HNF and lat_vecs
+        # add [0] to each element of the symmetry group
+        agroup = [[g,[0]] for g in group]
+
+    # we need to know the concentrations of the species with and
+    # without arrows, we also need to know the number of arrows and
+    # their species so we can undo the previous step later
+    (n_arrows, arrow_types,concs_w_arrows) = how_many_arrows(decorations)
+
+    # now find the number of unique arrangements using
+    # polya
+    if arrow_types != 0:
+        # Since enumlib doesn't write the arrow group out we have to
+        # recompute the group actions paired with their effects on the
+        # arrows
+        total = polya(concs_w_arrows, agroup, arrowings=arrow_types)
+    else:
+        total = polya(concs, agroup)
+
+    # generate the random subset to be used
+    if num_wanted < total:
+        from random import shuffle
+        subset = range(1, total+1) 
+        shuffle(subset)
+        subset = subset[0:num_wanted]
+    else:
+        from phenum.msg import warn
+        warn("number of configurations requested exceeds the number of "
+            "unique configurations available.")
+        subset = []
+
+    n_stabs = []
+    # if we're doing a purely arrow enumeration then we don't need to
+    # do the tree search but instead perform the final step of the
+    # algorithm to find the possible unique displacements of the atoms
+    if len(concs) == 1 and all(decorations) >=0:
+        configs = []
+        a_configs = add_arrows(decorations, agroup, 6)
+        count = 1
+        for config in a_configs:
+            if count in subset:
+                configs.append(config)
+            count += 1
+    else:
+        if arrow_types != 0:
+            configs = brancher(concs_w_arrows, agroup, decorations, 6, subset)
+        else:
+            configs = brancher(concs, agroup, decorations, 6, subset)
+
+    if len(configs) != num_wanted:
+        from msg import err
+        err("Warning the enumeration code returned {} structures when {} were asked for."
+            " This should not happen. Please submit a bug report on "
+            "https://github.com/wsmorgan/phonon-enumeration including your input files so "
+            "that this error may be corrected.".format(str(len(configs)),str(num_wanted)))
+
+    return configs
+
+def _ahash(coloring,dim):
+    """Produces a unique number for each possible configuration of
+    arrows. This number is used to compare the order that the arrows
+    occure in.
+
+    :arg coloring: is a 2D integer array of the full coloring with colors and arrows	    
+    :arg dim: the number of directions the arrows can point
+    """
+	
+    narrow = 0
+    for i in range(len(coloring)):
+	narrow = narrow + coloring[i]*dim**i
+    return(narrow)
+
+#anum is a unique number that is associated with an array of arrows.
+#num_of_arrows in the number of arrows that are in the array.
+#dim is the number of directions the arrows can point.
+def _ainvhash(anum,num_of_arrrows,dim):
+    """Turns an arrow hash back into the array of arrow directions.
+
+    :arg anum: the arrow hash number
+    :arg num_of_arrows: the number of arrows in the system
+    :arg dim: the number of directions the arrows can point
+    """
+    arrows = [0]*num_of_arrrows
+    for i in range(num_of_arrrows):
+	base = dim**(num_of_arrrows-1-i)
+	arrows[num_of_arrrows-1-i] = anum//base
+	anum = anum -base*(anum//base)
+    return(arrows)
