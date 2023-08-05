7f443317d41892c1423d8d6dd5a5b2a38770e8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:10 +0000

    Add package logging-mv-integrations/0.5.0/logging-mv-integrations-0.5.0.tar.gz

---
+++ packages/logging-mv-integrations/logging-mv-integrations-0.5.0.tar.gz/logging-mv-integrations-0.5.0/logging_mv_integrations/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  @copyright 2017 TUNE, Inc. (http://www.tune.com)
+#  @namespace logging_mv_integrations
+
+__title__ = 'logging-mv-integrations'
+__version__ = '0.5.0'
+__build__ = 0x000500
+__version_info__ = tuple(__version__.split('.'))
+
+__author__ = 'jefft@tune.com'
+__license__ = 'MIT License'
+
+__python_required_version__ = (3, 0)
+
+from .logger_json_lexer import (LoggerJsonLexer)
+from .logging_format import (LoggingFormat)
+from .logging_output import (LoggingOutput)
+from .logging_json_formatter import (LoggingJsonFormatter)
+from .logging_levels import (get_logging_level)
+from .logging import (get_logger)
