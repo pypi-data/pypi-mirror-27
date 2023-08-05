5694799aa7c3592c38b3d993189a0c1a91977dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:27 +0000

    Add package bra-scraper/0.1.7/bra_scraper-0.1.7.tar.gz

---
+++ packages/bra-scraper/bra_scraper-0.1.7.tar.gz/bra_scraper-0.1.7/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+from setuptools import setup
+
+
+
+setup(name='bra_scraper',
+      version='0.1.7',
+      description=u'An interface for fetching statistical data from BRÅ. This is not an official service!',
+      url='https://github.com/jensfinnas/bra_scraper',
+      author='Jens Finnäs',
+      author_email='jens.finnas@gmail.com',
+      license='MIT',
+      packages=['bra_scraper'],
+      include_package_data=True,
+      install_requires=[
+            "requests==2.11.1",
+            "wsgiref==0.1.2"
+      ],
+      zip_safe=False)
