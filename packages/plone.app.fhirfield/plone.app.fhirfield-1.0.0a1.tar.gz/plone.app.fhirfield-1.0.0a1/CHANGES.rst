36e67b10932bb70aaf50d845be16cc5761fa6ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:25 +0000

    Add package plone.app.fhirfield/1.0.0a1/plone.app.fhirfield-1.0.0a1.tar.gz

---
+++ packages/plone.app.fhirfield/plone.app.fhirfield-1.0.0a1.tar.gz/plone.app.fhirfield-1.0.0a1/CHANGES.rst
@@ -0,0 +1,9 @@
+Changelog
+=========
+
+
+1.0.0a1 (2017-12-05)
+--------------------
+
+- Initial release.
+  [nazrulworld]
---
+++ packages/plone.app.fhirfield/plone.app.fhirfield-1.0.0a1.tar.gz/plone.app.fhirfield-1.0.0a1/src/plone/app/fhirfield/schemaeditor.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+from plone.app.fhirfield import field
+from plone.app.fhirfield import interfaces
+from plone.app.fhirfield.compat import _
+from plone.schemaeditor.fields import FieldFactory
+
+
+__author__ = 'Md Nazrul Islam<email2nazrul@gmail.com>'
+
+
+class IFhirResource(interfaces.IFhirResource):
+    """ """
+
+
+FhirResourceFieldFactory = FieldFactory(field.FhirResource, _(u'FHIR Resource Field'))
