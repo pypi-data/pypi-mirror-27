1170ad70363e9cbf4e4e745a0b3fab7d92ff630a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:20 +0000

    Add package link/1.2.7/link-1.2.7.tar.gz

---
+++ packages/link/link-1.2.7.tar.gz/link-1.2.7/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from setuptools import setup
+import os
+import link
+
+dir = os.path.split(os.path.abspath(__file__))[0]
+
+
+DESCRIPTION = "Easy and consistent access to the objects you care about"
+LONG_DESCRIPTION = "Easy and consistent access to the objects you care about"
+URL = ''
+DOWNLOAD_URL = ''
+CLASSIFIERS = ['Development Status :: 4 - Beta',     
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 2',
+               'Programming Language :: Python :: 3'
+              ]
+EMAIL = ''
+SETUP_ARGS = {}
+DATA_FILES = [('link/configs', ['link/configs/link.config'])]
+REQUIRES = ['requests>=2.0.0', 'requests_oauthlib>=0.4.0', 'pandas', 'xmltodict' ]
+
+# write out the version file so we can keep track on what version the built
+# package is
+
+# call setup so it can build the package
+setup(name=link.__title__,
+      version=link.__version__,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      license=link.__license__,
+      maintainer_email=EMAIL,
+      maintainer=link.__author__,
+      url=URL,
+      packages=['link', 'link.wrappers', 'link.configs'],
+      #package_data = {'link.configs': ['link.configs/*config']},
+      install_requires=REQUIRES,
+      #data_files = DATA_FILES,
+      classifiers=CLASSIFIERS,
+      **SETUP_ARGS)
