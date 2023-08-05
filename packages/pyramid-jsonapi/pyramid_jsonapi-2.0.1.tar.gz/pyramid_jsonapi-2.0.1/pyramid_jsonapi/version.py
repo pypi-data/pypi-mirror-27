bd01529d749d51b02478a0b72b209198edfbcba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:53 +0000

    Add package pyramid-jsonapi/2.0.1/pyramid_jsonapi-2.0.1.tar.gz

---
+++ packages/pyramid-jsonapi/pyramid_jsonapi-2.0.1.tar.gz/pyramid_jsonapi-2.0.1/pyramid_jsonapi/version.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""Generate version information from git tags, archive info, or PKG-INFO.
+
+Based on: https://github.com/Changaco/version.py
+
+"""
+
+from os.path import dirname, isdir, join
+import re
+from subprocess import CalledProcessError, check_output
+
+
+def get_version():
+    """Get version in any way possible."""
+
+    prefix = ''
+    tag_re = re.compile(r'\btag: %s([0-9][^,]*)\b' % prefix)
+    version_re = re.compile('^Version: (.+)$', re.M)
+
+    # Return the version if it has been injected into the file by git-archive
+    version = tag_re.search('$Format:%D$')
+    if version:
+        return version.group(1)
+
+    project_dir = dirname(__file__)
+
+    if isdir(join(project_dir, '../.git')):
+        # Get the version using "git describe".
+        cmd = 'git -C %s describe --tags --match %s[0-9]* --dirty' % (project_dir, prefix)
+        try:
+            version = check_output(cmd.split()).decode().strip()[len(prefix):]
+        except CalledProcessError:
+            raise RuntimeError('Unable to get version number from git tags')
+
+        # PEP 440 compatibility
+        if '-' in version:
+            version = '.dev'.join(version.split('-')[:2])
+
+    else:
+        # Extract the version from the PKG-INFO file.
+        with open(join(project_dir, '../PKG-INFO')) as info_f:
+            version = version_re.search(info_f.read()).group(1)
+
+    return version
+
+
+if __name__ == '__main__':
+    print(get_version())
