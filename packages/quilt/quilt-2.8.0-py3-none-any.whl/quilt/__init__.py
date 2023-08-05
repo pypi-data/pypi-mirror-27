5f7ab1506d3157aad03bdc369c1cf79b44e662e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:23 +0000

    Add package quilt/2.8.0/quilt-2.8.0-py3-none-any.whl

---
+++ packages/quilt/quilt-2.8.0-py3-none-any.whl/quilt/__init__.py
@@ -0,0 +1,26 @@
+"""
+Makes functions in .tools.command accessible directly from quilt.
+"""
+
+from .tools.command import (
+    access_add,
+    access_list,
+    access_remove,
+    build,
+    check,
+    config,
+    inspect,
+    install,
+    log,
+    login,
+    login_with_token,
+    logout,
+    ls,
+    delete,
+    push,
+    tag_add,
+    tag_list,
+    tag_remove,
+    version_add,
+    version_list,
+)
