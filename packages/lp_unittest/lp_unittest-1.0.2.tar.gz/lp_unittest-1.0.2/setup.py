2df664f625c9b9a551051e95a42eba8a3d09e525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:46 +0000

    Add package lp_unittest/1.0.2/lp_unittest-1.0.2.tar.gz

---
+++ packages/lp_unittest/lp_unittest-1.0.2.tar.gz/lp_unittest-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+  name='lp_unittest',
+  packages=['lp_unittest'],
+  package_data={'lp_unittest': ['migrations/*']},
+  version='1.0.2',
+  description='REST Unit Test Framework',
+  author='Jim Simon',
+  author_email='hello@launchpeer.com',
+  url='https://github.com/Launchpeer/django-rest-unit-tests',
+  download_url='https://github.com/Launchpeer/django-rest-unit-tests/archive/master.tar.gz',
+  keywords=[],
+  classifiers=[],
+  install_requires=[
+    'django-rest-framework',
+    'django-rest-framework-social-oauth2',
+    'oauth2_provider',
+  ]
+)
