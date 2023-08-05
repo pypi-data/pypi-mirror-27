224e6f87f900fdf2de023670fdb150baef4b3a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:53 +0000

    Add package cnmarvinx/2.1.0/cnmarvinx-2.1.0.tar.gz

---
+++ packages/cnmarvinx/cnmarvinx-2.1.0.tar.gz/cnmarvinx-2.1.0/cnmarvinx.py
@@ -0,0 +1,10 @@
+''' This is my first python '''
+def print_lol(the_list,level):
+      '''定义递归函数'''
+      for each_item in the_list:
+            if isinstance(each_item, list):
+                  print_lol(each_item,level+1)
+            else:
+                  for tab_stop in range(level):
+                        print("\t",end='')
+                  print(each_item)
---
+++ packages/cnmarvinx/cnmarvinx-2.1.0.tar.gz/cnmarvinx-2.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name = 'cnmarvinx',
+      version = '2.1.0',
+      py_modules = ['cnmarvinx'],
+      author = 'MarvinX',
+      author_email = '767428771@qq.com',
+      url = 'http://www.headfirstlabs.com',
+      description = 'A simple printer of nested lists',
+      )
