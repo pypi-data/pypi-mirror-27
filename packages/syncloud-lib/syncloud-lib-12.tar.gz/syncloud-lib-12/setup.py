89074f388f0891ec552e061711325a4ee5d16357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:30 +0000

    Add package syncloud-lib/12/syncloud-lib-12.tar.gz

---
+++ packages/syncloud-lib/syncloud-lib-12.tar.gz/syncloud-lib-12/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from os.path import join, dirname
+
+requirements = [
+    'jsonpickle==0.7.1',
+    'requests==2.11.1'
+]
+
+version = open(join(dirname(__file__), 'version')).read().strip()
+
+setup(
+    name='syncloud-lib',
+    version=version,
+    install_requires = requirements,
+    description='Syncloud common library',
+    packages=['syncloud_app', 'syncloudlib', 'syncloudlib.integration'],
+    py_modules = ['convertible'],
+    license='GPLv3',
+    author='Syncloud',
+    author_email='syncloud@syncloud.it',
+    url='https://github.com/syncloud/lib')
