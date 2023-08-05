afa3bf69e4fe29fd7f191633c3d77a68c8841c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:03 +0000

    Add package upnp-map/0.1.0/upnp_map-0.1.0.tar.gz

---
+++ packages/upnp-map/upnp_map-0.1.0.tar.gz/upnp_map-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+   name='upnp_map',
+   version='0.1.0',
+   description='Minimal `upnp-map` tool. Useful to allow TCP conections on port X from the internet to this machine',
+   author='bbfc',
+   author_email='barrabin.fc@gmail.com',
+   url='https://github.com/barrabinfc/upnp-map',
+    # download_url = 'https://github.com/barrabinfc/upnp-ma/archive/0.1.tar.gz', # I'll explain this in a second
+   packages=['upnp_map'],  
+   install_requires=['miniupnpc'], 
+   scripts=[
+       'bin/upnp-map'
+   ],
+   keywords=['upnp','port mapping','firewall-hole','nat-hole','nat-traversal']
+)
