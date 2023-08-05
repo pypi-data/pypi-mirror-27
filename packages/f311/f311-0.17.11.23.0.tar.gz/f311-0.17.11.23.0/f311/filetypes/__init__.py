931fd3f586dfc1173e0abd9f397ed1af47bc2958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package f311/0.17.11.23.0/f311-0.17.11.23.0.tar.gz

---
+++ packages/f311/f311-0.17.11.23.0.tar.gz/f311-0.17.11.23.0/f311/filetypes/__init__.py
@@ -0,0 +1,11 @@
+"""
+Data representations.
+"""
+
+from .basic import *
+from .molconsts import *
+from .ft_basic import *
+from .ft_pyfant import *
+from .ft_aosss import *
+from .ft_uroboros import *
+from .util import *
