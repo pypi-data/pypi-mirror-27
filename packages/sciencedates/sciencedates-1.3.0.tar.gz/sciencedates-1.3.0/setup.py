20c45694029f4148c8d404fb986f055f3e14f1f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:29 +0000

    Add package sciencedates/1.3.0/sciencedates-1.3.0.tar.gz

---
+++ packages/sciencedates/sciencedates-1.3.0.tar.gz/sciencedates-1.3.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+install_requires = ['numpy','pytz','python-dateutil']
+tests_require=['nose','coveralls']
+# %%
+from setuptools import setup,find_packages
+
+setup(name='sciencedates',
+      packages=find_packages(),
+      version = '1.3.0',
+      description='Date conversions used in the sciences.',
+      author = 'Michael Hirsch, Ph.D.',
+      url = 'https://github.com/scivision/sciencedates',
+      classifiers=[
+      'Intended Audience :: Science/Research',
+      'Development Status :: 5 - Production/Stable',
+      'License :: OSI Approved :: MIT License',
+      'Topic :: Scientific/Engineering :: GIS',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      ],
+      install_requires=install_requires,
+      python_requires='>=2.7',
+      tests_require=tests_require,
+      extras_require={'tests':tests_require,
+                      'plot':['xarray','matplotlib']},
+	  )
+
