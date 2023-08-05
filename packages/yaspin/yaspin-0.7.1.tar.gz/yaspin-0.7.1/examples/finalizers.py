c5b20df33eed869a8e8f1ab7a24db9197bd1e09f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:13 +0000

    Add package yaspin/0.7.1/yaspin-0.7.1.tar.gz

---
+++ packages/yaspin/yaspin-0.7.1.tar.gz/yaspin-0.7.1/examples/finalizers.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+examples.finalizers
+~~~~~~~~~~~~~~~~~~~
+
+Success and Failure finalizers.
+"""
+
+import time
+
+from yaspin import yaspin
+
+
+def default_finalizers():
+    with yaspin(text="Downloading...") as sp:
+        time.sleep(2)
+        sp.ok()
+
+    with yaspin(text="Downloading...") as sp:
+        time.sleep(2)
+        sp.fail()
+
+
+def custom_finalizers():
+    with yaspin(text="Processing...") as sp:
+        time.sleep(2)
+        sp.ok('☀️')
+
+    with yaspin(text="Processing...") as sp:
+        time.sleep(2)
+        sp.fail('💥')
+
+
+def main():
+    default_finalizers()
+    custom_finalizers()
+
+
+if __name__ == '__main__':
+    main()
