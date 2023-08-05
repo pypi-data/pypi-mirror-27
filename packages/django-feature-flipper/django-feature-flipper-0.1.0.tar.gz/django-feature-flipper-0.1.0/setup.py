b2612d9bff813f59eae45d16921408f3df8ce1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:32 +0000

    Add package django-feature-flipper/0.1.0/django-feature-flipper-0.1.0.tar.gz

---
+++ packages/django-feature-flipper/django-feature-flipper-0.1.0.tar.gz/django-feature-flipper-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from os import path
+from setuptools import find_packages, setup
+
+README_PATH = path.join(path.dirname(path.abspath(__file__)), 'README.md')
+
+try:
+    import m2r
+    LONG_DESCRIPTION = m2r.parse_from_file(README_PATH)
+except (ImportError, IOError, FileNotFoundError):
+    # m2r not installed or file does not exist
+    LONG_DESCRIPTION = ''
+
+setup(
+    name='django-feature-flipper',
+    author='Pebble',
+    author_email='scott.walton@mypebble.co.uk',
+    description='A simple, customisable, feature flipper',
+    url='https://github.com/mypebble/django-feature-flipper.git',
+    version='0.1.0',
+    packages=find_packages(),
+    license='MIT',
+    long_description=LONG_DESCRIPTION,
+    install_requires=[
+        'django',
+    ]
+)
