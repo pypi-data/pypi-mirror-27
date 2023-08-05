b0532ed43ea2790b52e8cee9b2f4421b71291397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:33 +0000

    Add package django-distill/1.2/django_distill-1.2-py2-none-any.whl

---
+++ packages/django-distill/django_distill-1.2-py2-none-any.whl/django_distill/distill.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+from django.conf.urls import url
+
+
+from django_distill.errors import (DistillError, DistillWarning)
+
+
+urls_to_distill = []
+
+
+def distill_url(*a, **k):
+    distill_func = k.get('distill_func')
+    distill_file = k.get('distill_file')
+    if distill_file:
+        del k['distill_file']
+    if distill_func:
+        del k['distill_func']
+        name = k.get('name')
+        if not name:
+            raise DistillError('Distill function provided with no name')
+        if not callable(distill_func):
+            err = 'Distill function not callable: {}'
+            raise DistillError(err.format(distill_func))
+        urls_to_distill.append((distill_func, distill_file, name, a, k))
+    else:
+        e = 'URL registered with distill_url but no distill function supplied'
+        raise DistillWarning(e)
+    return url(*a, **k)
+
+
+# eof
