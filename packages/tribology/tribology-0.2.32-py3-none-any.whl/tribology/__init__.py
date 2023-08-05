be36245cf13b291d2469042c1f9c0eeb43c23b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:18 +0000

    Add package tribology/0.2.32/tribology-0.2.32-py3-none-any.whl

---
+++ packages/tribology/tribology-0.2.32-py3-none-any.whl/tribology/__init__.py
@@ -0,0 +1,19 @@
+"""
+Methods related to general tribology
+"""
+
+__all__ = ['tribology',
+           'boundary_element',
+           'constants',
+           'data_import',
+           'dowson_hamrock',
+           'hertz',
+           'lubrication']
+
+from tribology.tribology import *
+from tribology.boundary_element import *
+from tribology.constants import *
+from tribology.data_import import *
+from tribology.dowson_hamrock import *
+from tribology.hertz import *
+from tribology.lubrication import *
