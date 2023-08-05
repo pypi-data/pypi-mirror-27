79edaeed20909bdbf3f628fd9519e1c666f751b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:36 +0000

    Add package infi.logs_collector/0.1.15/infi.logs_collector-0.1.15-py2-none-any.whl

---
+++ packages/infi.logs_collector/infi.logs_collector-0.1.15-py2-none-any.whl/infi/logs_collector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.15"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.15)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '91b6640b31336421d353d342dc807d5079abc054'
+__git_head_subject__ = u'Finished Release v0.1.15'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
