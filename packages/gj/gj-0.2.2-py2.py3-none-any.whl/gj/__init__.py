a3cb5aa030e9d43e9dec28bc97fbbd34e7519fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:43 +0000

    Add package gj/0.2.2/gj-0.2.2-py2.py3-none-any.whl

---
+++ packages/gj/gj-0.2.2-py2.py3-none-any.whl/gj/__init__.py
@@ -0,0 +1,10 @@
+"Grant Jenks tools."
+
+from .packages import release, upload_docs
+
+__title__ = 'gj'
+__version__ = '0.2.2'
+__build__ = 0x000202
+__author__ = 'Grant Jenks'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2016 Grant Jenks'
