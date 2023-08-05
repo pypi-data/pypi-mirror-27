ac013327fd9cdc089d95faf79f8a964a07d781f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:38 +0000

    Add package trip/0.0.6/trip-0.0.6.tar.gz

---
+++ packages/trip/trip-0.0.6.tar.gz/trip-0.0.6/trip/exceptions.py
@@ -0,0 +1,13 @@
+from requests.exceptions import RequestException
+
+from tornado.httpclient import HTTPError
+
+
+class TripException(RequestException, HTTPError):
+    """There was an ambiguous exception that occurred while handling your
+    request.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize TripException with `request` and `response` objects."""
+        RequestException.__init__(*args, **kwargs)
