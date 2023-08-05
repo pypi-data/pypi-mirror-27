a48dbd1066d112dc4b9e5eae0f48b69a912d3d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:54 +0000

    Add package gapipy/2.9.3/gapipy-2.9.3.tar.gz

---
+++ packages/gapipy/gapipy-2.9.3.tar.gz/gapipy-2.9.3/gapipy/models/price_promotion.py
@@ -0,0 +1,26 @@
+from ..resources.base import Resource
+from ..utils import get_resource_class_from_resource_name
+
+
+class PricePromotion(Resource):
+    """
+    The `promotions` referenced in a `Price` are not valid, as they include an
+    `amount` field that is not part of the Promotion object.
+
+    Thus, this intermediatary model must be used to represent a pseudo-Resource.
+    """
+    def __init__(self, data, **kwargs):
+        self._resource_name = 'promotions'
+
+        klass = get_resource_class_from_resource_name('promotions')
+        # Python 2 and 3
+        # inefficient on Python 2 to list items()
+        for k, v in list(klass.__dict__.items()):
+            if 'fields' in k and isinstance(v, list):
+                setattr(self, k, getattr(klass, k))
+
+        super(PricePromotion, self).__init__(data, **kwargs)
+
+        # Add the "fake" amount field.
+        self._price_fields.append('amount')
+        setattr(self, 'amount', data['amount'])
---
+++ packages/gapipy/gapipy-2.9.3.tar.gz/gapipy-2.9.3/gapipy/resources/booking/booking.py
@@ -0,0 +1,42 @@
+# Python 2 and 3
+from __future__ import unicode_literals
+
+from ..base import Resource
+
+from .transaction import Payment, Refund
+from .document import Invoice, Document
+from .override import Override
+from .service import Service
+from gapipy.resources.checkin import Checkin
+
+
+class Booking(Resource):
+    _resource_name = 'bookings'
+    _is_parent_resource = True
+
+    _as_is_fields = ['id', 'href', 'external_id', 'currency']
+    _price_fields = [
+        'amount_paid', 'amount_owing', 'commission', 'tax_on_commission',
+    ]
+    _date_fields = [
+        'date_closed', 'date_of_first_travel', 'date_of_last_travel',
+        'balance_due_date',
+    ]
+    _date_time_fields_utc = ['date_created', ]
+    _resource_fields = [
+        ('agent', 'Agent'),
+        ('agency', 'Agency'),
+        ('associated_agency', 'Agency'),
+    ]
+
+    @property
+    def _resource_collection_fields(self):
+        return [
+            ('services', Service),
+            ('invoices', Invoice),
+            ('payments', Payment),
+            ('refunds', Refund),
+            ('documents', Document),
+            ('overrides', Override),
+            ('checkins', Checkin),
+        ]
