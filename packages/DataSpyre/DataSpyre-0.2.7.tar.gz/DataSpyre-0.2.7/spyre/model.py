996b042c323039fa90ea2376527692ed050c8699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:45 +0000

    Add package DataSpyre/0.2.7/DataSpyre-0.2.7.tar.gz

---
+++ packages/DataSpyre/DataSpyre-0.2.7.tar.gz/DataSpyre-0.2.7/spyre/model.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib
+# from StringIO import StringIO
+import io
+
+
+class Plot:
+    def getPlotPath(self, plt_obj):
+        buffer = io.BytesIO()
+        if isinstance(plt_obj, plt.Figure):
+            plt_obj.savefig(buffer, format='png', bbox_inches='tight')
+        if isinstance(plt_obj, matplotlib.axes.Axes):
+            plt_obj.get_figure().savefig(buffer, format='png', bbox_inches='tight')
+        else:
+            print("Error: getPlot method must return an pyplot figure or matplotlib Axes object")
+        plt.close('all')
+        return(buffer)
+
+
+class Image:
+    def getImagePath(self, img_obj):
+        buffer = io.BytesIO()
+        mpimg.imsave(buffer, img_obj)
+        try:
+            mpimg.imsave(buffer, img_obj)
+        except Exception:
+            print("Error: getImage method must return an image object")
+        return(buffer)
