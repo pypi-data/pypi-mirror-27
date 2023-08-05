1e4bdde3264dd1c7b0ea938eea368a00499a6788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:51 +0000

    Add package fenics-fiat/2017.2.0/fenics-fiat-2017.2.0.tar.gz

---
+++ packages/fenics-fiat/fenics-fiat-2017.2.0.tar.gz/fenics-fiat-2017.2.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, print_function, division
+
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.version_info < (2, 7):
+    print("Python 2.7 or higher required, please upgrade.")
+    sys.exit(1)
+
+version = "2017.2.0"
+
+url = "https://bitbucket.org/fenics-project/fiat/"
+tarball = None
+if 'dev' not in version:
+    tarball = url + "downloads/fenics-fiat-%s.tar.gz" % version
+
+setup(name="fenics-fiat",
+      description="FInite element Automatic Tabulator",
+      version=version,
+      author="Robert C. Kirby et al.",
+      author_email="fenics-dev@googlegroups.com",
+      url=url,
+      download_url=tarball,
+      license="LGPL v3 or later",
+      packages=["FIAT"],
+      install_requires=["numpy", "sympy", "six"])
