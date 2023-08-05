96aa48ad1b860d97249c5f35b78601d62f8a59e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package markdowntable/5.0.0.3/markdowntable-5.0.0.3.tar.gz

---
+++ packages/markdowntable/markdowntable-5.0.0.3.tar.gz/markdowntable-5.0.0.3/markdowntable/exceptions.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+class MarkdownTableException(Exception):
+    def __init__(self, deubg):
+        Exception.__init__(self, 'There is an error with your Markdown Table. Printing debug information. \n {debug}'.format(debug = debug))
+
+class SuppressedError(MarkdownTableException):
+    """docstring for SuppressedError."""
+
+    def __init__(self, error, message, debug):
+        """
+        :rtype: None
+        """
+        Exception.__init__(self, '''An error has occured. Printing error. information:
+        Error Name: {n}
+        Error Message: {m}
+        Debug Info: {d}'''.format(n=error,m=message,d=debug))
+
+class TooManyValues(MarkdownTableException):
+    def __init__(self, columns):
+        Exception.__init__(self, 'You entered in too many row values. Please only enter {} row names.'.format(str(columns)))
