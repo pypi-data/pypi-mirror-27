4c039b4385eb0483567fea4ea884cabc306e9fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:19 +0000

    Add package tcex/0.7.4/tcex-0.7.4.tar.gz

---
+++ packages/tcex/tcex-0.7.4.tar.gz/tcex-0.7.4/tcex/tcex_key_value.py
@@ -0,0 +1,41 @@
+""" standard """
+import urllib
+""" third-party """
+""" custom """
+
+
+class TcExKeyValue(object):
+    """Update Redis via ThreatConnect API Wrapper"""
+
+    def __init__(self, tcex, rhash):
+        """ """
+        self._tcex = tcex
+        self._hash = rhash
+        self._r = tcex.request_tc()
+
+    def create(self, key, value):
+        """Create key/value pair in Redis"""
+        key = urllib.quote(key, safe='~')
+        self._r.body = value
+        self._r.content_type = 'application/octet-stream'
+        self._r.http_method = 'PUT'
+        self._r.url = '{}/internal/playbooks/keyValue/{}'.format(
+            self._tcex.default_args.tc_api_path, key)
+        response = self._r.send()
+        return response.content
+
+    def delete(self, key):
+        """Delete is not supported in API Wrapper"""
+        return None
+
+    def read(self, key):
+        """Read data from Redis for the provided key"""
+        key = urllib.quote(key, safe='~')
+        self._r.url = '{}/internal/playbooks/keyValue/{}'.format(
+            self._tcex.default_args.tc_api_path, key)
+        response = self._r.send()
+
+        data = response.content
+        if data is not None and not isinstance(data, unicode):  # 2to3 converts unicode to str
+            data = unicode(response.content, 'utf-8')  # 2to3 converts unicode to str
+        return data
