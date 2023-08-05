efdda72f7c0d067d04776158958384d660faf40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:19 +0000

    Add package nesterlist2017test/1.0.0/nesterlist2017test-1.0.0.tar.gz

---
+++ packages/nesterlist2017test/nesterlist2017test-1.0.0.tar.gz/nesterlist2017test-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+        name         = 'nesterlist2017test',
+        version      = '1.0.0',
+        py_modules   = ['nesterlist2017test'],
+        author       = 'johnson',
+        author_email = '453713729@qq.com',
+        url          = '',
+        description  = 'A simple printer of nesterlist2017test',
+    )
+
