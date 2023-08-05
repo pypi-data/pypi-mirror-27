2f7c506b0b7ff2f7495d7efaa7ee2ad95a89636f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:00 +0000

    Add package rampante/0.0.4/rampante-0.0.4.tar.gz

---
+++ packages/rampante/rampante-0.0.4.tar.gz/rampante-0.0.4/rampante/__init__.py
@@ -0,0 +1,16 @@
+"""
+Rampante Library.
+~~~~~~~~~~~~~~~~~~~~~
+
+A fancy and opinionated nanoframework for microservices.
+
+:copyright: (c) 2017 by Christian barra.
+:license: Apache 2.0, see LICENSE for more details.
+"""
+
+from .decorator import subscribe_on
+from .scheduler import scheduler
+from .connector import streaming
+
+
+__all__ = ("subscribe_on", "scheduler", "streaming")
