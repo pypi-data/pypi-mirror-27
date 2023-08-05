020a43e2db45dc889b403efc9fe816b12cd7f3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:04 +0000

    Add package hue-sensors/1.2/hue-sensors-1.2.tar.gz

---
+++ packages/hue-sensors/hue-sensors-1.2.tar.gz/hue-sensors-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='hue-sensors',
+    version='1.2',
+    py_modules=['hue_sensors'],
+    url='https://github.com/robmarkcole/Hue-sensors',
+    keywords='phillips hue',
+    author='Robin Cole',
+    author_email='robmarkcole@gmail.com',
+    description='Tools for reading the state of Philips Hue sensors',
+    install_requires=['requests'],
+    license='MIT',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"]
+)
