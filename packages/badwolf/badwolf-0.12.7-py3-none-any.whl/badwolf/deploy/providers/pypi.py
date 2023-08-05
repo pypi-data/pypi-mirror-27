1ea4037349d48f85485d30cc79a5b68e3ab12681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:00 +0000

    Add package badwolf/0.12.7/badwolf-0.12.7-py3-none-any.whl

---
+++ packages/badwolf/badwolf-0.12.7-py3-none-any.whl/badwolf/deploy/providers/pypi.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from badwolf.utils import run_command
+from badwolf.deploy.providers import Provider
+
+
+logger = logging.getLogger(__name__)
+
+
+class PypiProvider(Provider):
+    name = 'pypi'
+
+    def deploy(self):
+        username = self.config['username']
+        password = self.config['password']
+        repository = self.config['repository']
+        command = [
+            'twine',
+            'upload',
+            '--repository',
+            repository,
+            '--repository-url',
+            repository,
+            '--username',
+            username,
+            '--password',
+            password,
+            '--skip-existing',
+            self.config['distributions']
+        ]
+        exit_code, output = run_command(command, include_errors=True, cwd=self.working_dir)
+        return exit_code == 0, output
+
+    def url(self):
+        pkg_name = self.config.get('package', self.context.repo_name)
+        return '{}/pypi/{}'.format(self.config['repository'], pkg_name)
