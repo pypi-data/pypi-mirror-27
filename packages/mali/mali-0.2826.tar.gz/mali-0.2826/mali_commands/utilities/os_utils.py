f41c3ac7f9dedfeb5692571b8a6d25de840208e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:16 +0000

    Add package mali/0.2826/mali-0.2826.tar.gz

---
+++ packages/mali/mali-0.2826.tar.gz/mali-0.2826/mali_commands/utilities/os_utils.py
@@ -0,0 +1,35 @@
+# -*- coding: utf8 -*-
+import os
+import errno
+import shutil
+
+
+def create_dir(dirname):
+    if not dirname or os.path.exists(dirname):
+        return
+
+    try:
+        os.makedirs(dirname)
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise
+
+
+def remove_dir(dirname):
+    try:
+        shutil.rmtree(dirname)
+    except (OSError, IOError):
+        pass
+
+
+def flatten_dir(root_dir):
+    root_walk = os.walk(root_dir)
+    _, top_level_dirs, _ = next(root_walk)
+
+    for dirpath, _, filenames in root_walk:
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            shutil.move(filepath, root_dir)
+
+    for top_level_dir in top_level_dirs:
+        remove_dir(os.path.join(root_dir, top_level_dir))
