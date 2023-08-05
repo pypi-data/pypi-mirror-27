52273b19e71b8fe0912e86209e903dfc76f520a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:07 +0000

    Add package nsepy/0.7/nsepy-0.7.tar.gz

---
+++ packages/nsepy/nsepy-0.7.tar.gz/nsepy-0.7/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from distutils.core import setup
+setup(
+  name = 'nsepy',
+  packages = ['nsepy', 'nsepy.derivatives', 'nsepy.indices', 'nsepy.debt'], # this must be the same as the name above
+  version = '0.7',
+  description = 'Library to download financial data in pandas dataframe',
+  author = 'Swapnil Jariwala',
+  author_email = 'sjerry4u@gmail.com',
+  url = 'https://github.com/swapniljariwala/nsepy', # use the URL to the github repo
+  entry_points='''
+    [console_scripts]
+    nsecli=nsepy.cli:cli
+  ''',
+  download_url = 'https://github.com/swapniljariwala/nsepy/archive/v0.7.zip', 
+  install_requires = ['beautifulsoup4', 'requests', 'numpy', 'pandas', 'six', 'click'],
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
