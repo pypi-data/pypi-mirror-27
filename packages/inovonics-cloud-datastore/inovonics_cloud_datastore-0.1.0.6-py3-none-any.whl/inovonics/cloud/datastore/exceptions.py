742a48b16600780a66b13d45edca8c3ef1453705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:24 +0000

    Add package inovonics-cloud-datastore/0.1.0.6/inovonics_cloud_datastore-0.1.0.6-py3-none-any.whl

---
+++ packages/inovonics-cloud-datastore/inovonics_cloud_datastore-0.1.0.6-py3-none-any.whl/inovonics/cloud/datastore/exceptions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# === IMPORTS ===
+
+# === GLOBALS ===
+
+# === FUNCTIONS ===
+
+# === CLASSES ===
+class DuplicateException(Exception):
+    pass
+
+class ExistsException(Exception):
+    pass
+
+class InvalidDataException(Exception):
+    pass
+
+class NotExistsException(Exception):
+    pass
+
+# === MAIN ===
