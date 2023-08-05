2fcf4704d7ab8e681f7cd32bae9bc1280158a7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:57 +0000

    Add package ripiu.cmsplugin-columns/0.1.0/ripiu.cmsplugin_columns-0.1.0-py3-none-any.whl

---
+++ packages/ripiu.cmsplugin-columns/ripiu.cmsplugin_columns-0.1.0-py3-none-any.whl/ripiu.cmsplugin_columns-0.1.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+cmsplugin_columns
+===================
+
+Liquid columns
+
+
---
+++ packages/ripiu.cmsplugin-columns/ripiu.cmsplugin_columns-0.1.0-py3-none-any.whl/ripiu/cmsplugin_columns/models.py
@@ -0,0 +1,37 @@
+from cms.models import CMSPlugin
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class LiquidColumnsPluginModel(CMSPlugin):
+    """
+    Liquid columns
+    """
+
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    COLUMNS_CHOICES = (
+        (TWO, _('Two')),
+        (THREE, _('Three')),
+        (FOUR, _('Four')),
+        (FIVE, _('Five')),
+        (SIX, _('Six')),
+    )
+
+    num_columns = models.IntegerField(
+        _('columns'),
+        choices=COLUMNS_CHOICES,
+        default=TWO,
+        help_text=_('How many columns for this section?')
+    )
+
+    def __str__(self):
+        return str(self.num_columns)
+
+    class Meta:
+        verbose_name = _('Liquid columns')
+        verbose_name_plural = _('Liquid columns')
