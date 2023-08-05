10cfea157c90edc04e39be0ab655156b0de6525a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:02 +0000

    Add package learning1/0.1/learning1-0.1.tar.gz

---
+++ packages/learning1/learning1-0.1.tar.gz/learning1-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='learning1',    
+    version='0.1',	
+	author = 'Vignesh Ravichandran',
+	author_email = 'vickykct@gmail.com',
+    scripts=['learning1']         
+)
+
