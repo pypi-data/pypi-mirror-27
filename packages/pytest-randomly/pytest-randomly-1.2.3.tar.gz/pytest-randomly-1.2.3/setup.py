77e2a204ad2c0fd145333211a55e043bcb0769e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:23 +0000

    Add package pytest-randomly/1.2.3/pytest-randomly-1.2.3.tar.gz

---
+++ packages/pytest-randomly/pytest-randomly-1.2.3.tar.gz/pytest-randomly-1.2.3/setup.py
@@ -0,0 +1,60 @@
+# -*- encoding:utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import codecs
+import re
+
+from setuptools import setup
+
+
+def get_version(filename):
+    with codecs.open(filename, 'r', 'utf-8') as fp:
+        contents = fp.read()
+    return re.search(r"__version__ = ['\"]([^'\"]+)['\"]", contents).group(1)
+
+
+version = get_version('pytest_randomly.py')
+
+
+with codecs.open('README.rst', 'r', 'utf-8') as readme_file:
+    readme = readme_file.read()
+
+with codecs.open('HISTORY.rst', 'r', 'utf-8') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    'pytest',
+]
+
+setup(
+    name='pytest-randomly',
+    version=version,
+    description="Pytest plugin to randomly order tests and control "
+                "random.seed.",
+    long_description=readme + '\n\n' + history,
+    author="Adam Johnson",
+    author_email='me@adamj.eu',
+    url='https://github.com/adamchainz/pytest-randomly',
+    py_modules=['pytest_randomly'],
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='pytest, random, randomize, randomise, randomly',
+    entry_points={
+        'pytest11': ['randomly = pytest_randomly'],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
