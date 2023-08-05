cdebf224ebc83e5dfa2a9f7530d4526fad37d563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:03 +0000

    Add package data_preprocessing/0.0.1/data_preprocessing-0.0.1.tar.gz

---
+++ packages/data_preprocessing/data_preprocessing-0.0.1.tar.gz/data_preprocessing-0.0.1/data_preprocessing/__init__.py
@@ -0,0 +1,7 @@
+from .data_preprocessing import feature_filter
+from .data_preprocessing import null_dealing
+from .data_preprocessing import sample
+
+feature_filter = feature_filter()
+null_dealing = null_dealing()
+sample = sample()
---
+++ packages/data_preprocessing/data_preprocessing-0.0.1.tar.gz/data_preprocessing-0.0.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+from setuptools import setup
+
+setup(
+    name='data_preprocessing',
+    version='0.0.1',
+    description=(
+        'data preprocessing'
+    ),
+    author='sladesal',
+    author_email='sharalion@gmail.com',
+    maintainer='sladesal',
+    maintainer_email='sharalion@gmail.com',
+    license='BSD License',
+    packages=['data_preprocessing'],
+    platforms=["all"],
+    url='https://github.com/sladesha',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
