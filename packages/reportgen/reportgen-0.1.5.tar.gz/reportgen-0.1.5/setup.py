0d775f750127c53e17f74cb32e031943304d9b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package reportgen/0.1.5/reportgen-0.1.5.tar.gz

---
+++ packages/reportgen/reportgen-0.1.5.tar.gz/reportgen-0.1.5/setup.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='reportgen',
+    version='0.1.5',
+    description=(
+        'reportgen is a Python library for creating and updating analysis report.'
+    ),
+    long_description=open('README.rst').read(),
+    author='JSong',
+    author_email='gasongjian@126.com',
+    maintainer='JSong',
+    maintainer_email='gasongjian@126.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/gasongjian/reportgen',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+	install_requires=[
+        'pandas',
+        'numpy',
+        'python-pptx',
+        'Pillow'
+    ]
+)
