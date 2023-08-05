7874f25cd4aa36a1dd318d467b554d9a70230112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:36 +0000

    Add package py-incomepropertyevaluatorkit/0.0.1/py_incomepropertyevaluatorkit-0.0.1-py3-none-any.whl

---
+++ packages/py-incomepropertyevaluatorkit/py_incomepropertyevaluatorkit-0.0.1-py3-none-any.whl/incomepropertyevaluatorkit/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from incomepropertyevaluatorkit.foundation.constants import *
+from incomepropertyevaluatorkit.foundation.utils import *
+from incomepropertyevaluatorkit.calculator.analyzer import *
+__all__ = ["analyzer", "FinancialAnalyzer"]
