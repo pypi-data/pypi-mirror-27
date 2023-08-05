9114275895b48ae19caaa174f78e4b50cf1e6cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:27 +0000

    Add package MarkovTextGenerator/1.6.4/MarkovTextGenerator-1.6.4.tar.gz

---
+++ packages/MarkovTextGenerator/MarkovTextGenerator-1.6.4.tar.gz/MarkovTextGenerator-1.6.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import MarkovTextGenerator
+
+setup(
+    name="MarkovTextGenerator",
+    version=MarkovTextGenerator.__version__,
+    author=MarkovTextGenerator.__author__,
+    url="https://github.com/NyashniyVladya/MarkovTextGenerator",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3.6"
+    ],
+    install_requires=[
+        "shutil"
+    ],
+    keywords=(
+        "vladya markovgenerator markov_generator markov_chain "
+        "MarkovTextGenerator"
+    ),
+    description="Generator of pseudo-random text, using Markov chains.",
+    long_description=(
+        "Генератор псевдорандомного текста, посредством цепей Маркова."
+    )
+)
