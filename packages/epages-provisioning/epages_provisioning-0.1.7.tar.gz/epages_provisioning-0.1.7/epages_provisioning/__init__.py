ead98d4808d20856f66893fdfd919c46ff14898a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:27 +0000

    Add package epages-provisioning/0.1.7/epages_provisioning-0.1.7.tar.gz

---
+++ packages/epages-provisioning/epages_provisioning-0.1.7.tar.gz/epages_provisioning-0.1.7/epages_provisioning/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for ePages provisioning."""
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+# from .provisioning import *
+
+__author__ = """Tatu Wikman"""
+__email__ = 'tatu.wikman@gmail.com'
+__version__ = '0.1.7'
