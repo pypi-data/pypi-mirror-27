a076ac55a7cc5031322bbdc24ec0386eceb27961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:24 +0000

    Add package smores/0.0.7/smores-0.0.7-py2.py3-none-any.whl

---
+++ packages/smores/smores-0.0.7-py2.py3-none-any.whl/smores/__init__.py
@@ -0,0 +1,2 @@
+from .smores import Smores, AutocompleteResponse, TemplateString, TemplateFile, Schema, Nested
+__all__ = ['Smores', 'AutocompleteResponse', 'TemplateString', 'TemplateFile', 'Schema', 'Nested']
