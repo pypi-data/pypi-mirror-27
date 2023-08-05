1234c65ab65e6b50a8adcfce6fdcc5729c37bccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:03 +0000

    Add package tapioca-cloudez/1.0.0a3/tapioca_cloudez-1.0.0a3-py2-none-any.whl

---
+++ packages/tapioca-cloudez/tapioca_cloudez-1.0.0a3-py2-none-any.whl/tapioca_cloudez/resource_mapping/migration.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+MIGRATION_MAPPING = {
+    'configr_migration': {
+        'resource': 'configr-migration/',
+        'docs': '',
+        'methods': ['POST'],
+    },
+    'sync_list': {
+        'resource': 'sync/',
+        'docs': '',
+        'methods': ['GET'],
+    },
+    'sync_get': {
+        'resource': 'sync/{id}/',
+        'docs': '',
+        'methods': ['GET'],
+    },
+    'sync_create': {
+        'resource': 'sync/',
+        'docs': '',
+        'methods': ['POST'],
+    },
+}
