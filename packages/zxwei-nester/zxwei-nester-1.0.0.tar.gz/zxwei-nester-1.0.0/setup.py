aa271fa9ee8768b8b63285236926e78cda60d434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:24 +0000

    Add package zxwei-nester/1.0.0/zxwei-nester-1.0.0.tar.gz

---
+++ packages/zxwei-nester/zxwei-nester-1.0.0.tar.gz/zxwei-nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+	name		='zxwei-nester',
+	version		='1.0.0',
+	py_modules	=['zxwei-nester'],
+	author		='xiaowei',
+	author_email='1649201921@qq.com',
+	url			='https://zxwei.cc',
+	description ='A simple printer of nested lists',
+
+)
