66b1078b58696f525808352a1a07507cee56ba6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:52 +0000

    Add package molo.core/5.22.5/molo.core-5.22.5-py3-none-any.whl

---
+++ packages/molo.core/molo.core-5.22.5-py3-none-any.whl/molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/urls.py
@@ -0,0 +1,67 @@
+import os
+
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.views.generic.base import TemplateView
+
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailcore import urls as wagtail_urls
+
+from molo.core.views import upload_file, download_file
+
+# Path to a custom template that will be used by the admin
+# site main index view.
+admin.site.index_template = 'django_admin/index.html'
+admin.autodiscover()
+
+# implement CAS URLs in a production setting
+if settings.ENABLE_SSO:
+    urlpatterns = patterns(
+        '',
+        url(r'^admin/login/', 'django_cas_ng.views.login'),
+        url(r'^admin/logout/', 'django_cas_ng.views.logout'),
+        url(r'^admin/callback/', 'django_cas_ng.views.callback'),
+    )
+else:
+    urlpatterns = patterns('', )
+
+urlpatterns += patterns(
+    '',
+    url(r'^django-admin/upload_media/', upload_file,
+        name='molo_upload_media'),
+    url(r'^django-admin/download_media/', download_file,
+        name='molo_download_media'),
+    url(r'^django-admin/', include(admin.site.urls)),
+    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^documents/', include(wagtaildocs_urls)),
+    url(r'^robots\.txt$', TemplateView.as_view(
+        template_name='robots.txt', content_type='text/plain')),
+    url(r'^sitemap\.xml$', 'wagtail.contrib.wagtailsitemaps.views.sitemap'),
+
+{% for app_name, regex in cookiecutter.include %}
+    url(r'{{regex}}',
+        include('{{app_name}}.urls',
+                namespace='{{app_name}}',
+                app_name='{{app_name}}')),
+{% endfor %}
+    url(r"^mote/", include("mote.urls", namespace="mote")),
+    url(r'', include('molo.core.urls')),
+    url(r'^profiles/', include(
+        'molo.profiles.urls',
+        namespace='molo.profiles', app_name='molo.profiles')),
+    url('^', include('django.contrib.auth.urls')),
+    url(r'', include(wagtail_urls)),
+)
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(
+        settings.MEDIA_URL + 'images/',
+        document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
