6972c2daf287ebbf9a91ab27baf9f3dc358600a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:06 +0000

    Add package emoji-extractor/1.0.12/emoji_extractor-1.0.12.tar.gz

---
+++ packages/emoji-extractor/emoji_extractor-1.0.12.tar.gz/emoji_extractor-1.0.12/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(include_package_data=True,
+    name = 'emoji_extractor',
+    packages = ['emoji_extractor'],
+    version = '1.0.12',
+    description = 'Extract, detect and count emoji',
+    author='Alexander Robertson',
+    author_email='alexander.robertson@ed.ac.uk',
+    url='https://github.com/alexanderrobertson/emoji-extractor',
+    classifiers=['Programming Language :: Python :: 3',
+    			'Topic :: Text Processing :: General',
+    			'License :: OSI Approved :: MIT License',
+
+
+    ]
+)
