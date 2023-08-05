29be31bf73bc9eccc5d8e4295a6ab40144fd0923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:17 +0000

    Add package googledrivedownloader/0.1/googledrivedownloader-0.1.tar.gz

---
+++ packages/googledrivedownloader/googledrivedownloader-0.1.tar.gz/googledrivedownloader-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='googledrivedownloader',
+    version='0.1',
+    packages=['google_drive_downloader'],
+    url='https://github.com/ndrplz/google-drive-downloader',
+    download_url='https://github.com/ndrplz/google-drive-downloader/archive/0.2.tar.gz',
+    license='MIT',
+    author='Davide Abati and Andrea Palazzi',
+    author_email='ndrplz@gmail.com',
+    description='Minimal class to download shared files from Google Drive.'
+)
