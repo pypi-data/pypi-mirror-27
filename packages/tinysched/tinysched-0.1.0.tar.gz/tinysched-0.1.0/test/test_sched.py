1f5146474ba7f15599a93d0292ac343d11976a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:45 +0000

    Add package tinysched/0.1.0/tinysched-0.1.0.tar.gz

---
+++ packages/tinysched/tinysched-0.1.0.tar.gz/tinysched-0.1.0/test/test_sched.py
@@ -0,0 +1,30 @@
+import unittest
+import time
+from datetime import timedelta
+
+from tinysched import scheduler
+
+
+class TestSched(unittest.TestCase):
+    def setUp(self):
+        self.count = 0
+
+    def test_repeat(self):
+        def foo():
+            self.count += 1
+
+        repeats = 3
+        scheduler.repeat(self.inc_counter, timedelta(seconds=0), max_repeats=repeats)
+        time.sleep(0.1)
+        self.assertEqual(self.count, repeats)
+
+    def test_interval(self):
+        repeats = 3
+        delay = 0.01
+        scheduler.repeat(self.inc_counter, timedelta(seconds=delay), max_repeats=repeats)
+        for r in range(repeats):
+            self.assertLessEqual(self.count, r + 1)
+            time.sleep(delay)
+
+    def inc_counter(self):
+        self.count += 1
---
+++ packages/tinysched/tinysched-0.1.0.tar.gz/tinysched-0.1.0/tinysched/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['tinysched']
+__version__ = '0.1.0'
+from .scheduler import *
