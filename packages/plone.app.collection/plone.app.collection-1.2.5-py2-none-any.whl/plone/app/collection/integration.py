727c0e8ee95de0141cb3642be72b7baae8b42e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package plone.app.collection/1.2.5/plone.app.collection-1.2.5-py2-none-any.whl

---
+++ packages/plone.app.collection/plone.app.collection-1.2.5-py2-none-any.whl/plone/app/collection/integration.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from Products.CMFPlone.interfaces import INonInstallable \
+    as IPloneFactoryNonInstallable
+from Products.CMFQuickInstallerTool.interfaces import INonInstallable \
+    as IQuickInstallerNonInstallable
+from zope.interface import implementer
+
+
+@implementer(IQuickInstallerNonInstallable, IPloneFactoryNonInstallable)
+class HiddenProfiles(object):
+
+    def getNonInstallableProfiles(self):
+        """
+        Prevents profiles dependencies from showing up in the profile list
+        when creating a Plone site.
+        """
+        return [
+            'plone.app.collection:uninstall',
+            'plone.app.querystring:default',
+        ]
+
+    def getNonInstallableProducts(self):
+        """
+        Prevents our dependencies from showing up in the quick
+        installer's list of installable products.
+        """
+        return [
+            'plone.app.querystring',
+        ]
