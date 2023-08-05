f9f200f253d5c473c229f3218984923a7d2508e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:55 +0000

    Add package MotionClouds/20171206/MotionClouds-20171206.tar.gz

---
+++ packages/MotionClouds/MotionClouds-20171206.tar.gz/MotionClouds-20171206/MotionClouds/__init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, absolute_import
+__author__ = "Laurent Perrinet INT - CNRS"
+__version__ = '20171206'
+__licence__ = 'GPLv2'
+from .MotionClouds import *
