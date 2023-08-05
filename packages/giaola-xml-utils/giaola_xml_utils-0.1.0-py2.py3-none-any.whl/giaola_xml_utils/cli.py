049a2f5c9b66803b26d469d1ac4ca64b89c58d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:02 +0000

    Add package giaola-xml-utils/0.1.0/giaola_xml_utils-0.1.0-py2.py3-none-any.whl

---
+++ packages/giaola-xml-utils/giaola_xml_utils-0.1.0-py2.py3-none-any.whl/giaola_xml_utils/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for giaola_xml_utils."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for giaola_xml_utils."""
+    click.echo("Replace this message by putting your code into "
+               "giaola_xml_utils.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
