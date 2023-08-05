666584f13d34b875494cf49a8c0b8e8b5dd121b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:23 +0000

    Add package asyncbattlerite/0.2/asyncbattlerite-0.2.zip

---
+++ packages/asyncbattlerite/asyncbattlerite-0.2.zip/asyncbattlerite-0.2/AsyncBattlerite/errors.py
@@ -0,0 +1,21 @@
+class BRRequestException(Exception):
+    def __init__(self, response, data):
+        self.reason = response.reason
+        self.status = response.status
+        self.error = data.get("errors")
+        if self.error is not None:
+            self.error = self.error[0]['title']
+        super().__init__("{0.status}: {0.reason} - {0.error}".format(self))
+
+
+class NotFoundException(BRRequestException):
+    pass
+
+
+class BRServerException(BRRequestException):
+    pass
+
+
+class BRFilterException(Exception):
+    def __init__(self, error):
+        super().__init__(error)
