c601dee40d71e3675dda1eda7d0105b8d84c6ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:44 +0000

    Add package pymonet/0.7.1/pymonet-0.7.1.tar.gz

---
+++ packages/pymonet/pymonet-0.7.1.tar.gz/pymonet-0.7.1/pymonet/task.py
@@ -0,0 +1,57 @@
+class Task:
+
+    def __init__(self, fork):
+        """
+        :param fork: function to call during fork
+        :type fork: (reject, resolve) -> any
+        """
+        self.fork = fork
+
+    @classmethod
+    def of(cls, value):
+        """
+        :param value
+        :type: any
+        instant rejected Task
+        :returns: Task<_, resolve>
+        """
+        return lambda _, resolve: resolve(value)
+
+    @classmethod
+    def reject(cls, value):
+        """
+        :param value
+        :type: any
+        instant rejected Task
+        :returns: Task<reject, _>
+        """
+        return lambda reject, _: reject(value)
+
+    def map(self, fn):
+        """
+        :param fn: mapper function
+        :type fn: value -> mapped_value
+        :returns: Task<reject -> mapped_value>
+        """
+        def result(reject, resolve):
+            return self.fork(
+                lambda arg: reject(arg),
+                lambda arg: resolve(fn(arg))
+            )
+
+        return Task(result)
+
+    def fold(self, fn):
+        """
+        also know as flatmap
+        :param fn: mapper function
+        :type fn: value -> Task<reject, mapped_value>
+        :returns: Task<reject, mapped_value>
+        """
+        def result(reject, resolve):
+            return self.fork(
+                lambda arg: reject(arg),
+                lambda arg: fn(arg).fork(reject, resolve)
+            )
+
+        return Task(result)
