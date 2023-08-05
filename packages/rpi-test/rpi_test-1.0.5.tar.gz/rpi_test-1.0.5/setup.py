0ae167b64bc35972fa1557df30e78fd97d9dc147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:59 +0000

    Add package rpi-test/1.0.5/rpi_test-1.0.5.tar.gz

---
+++ packages/rpi-test/rpi_test-1.0.5.tar.gz/rpi_test-1.0.5/setup.py
@@ -0,0 +1,35 @@
+# Copyright 2015-2017 The MathWorks, Inc.
+
+from setuptools import setup
+from distutils.command.clean import clean
+from distutils.command.install import install
+
+class InstallRuntime(install):
+    # Calls the default run command, then deletes the build area 
+    # (equivalent to "setup clean --all").
+    def run(self):
+        install.run(self)
+        c = clean(self.distribution)
+        c.all = True
+        c.finalize_options()
+        c.run()
+
+if __name__ == '__main__':
+
+    setup(
+        name="rpi_test",
+        version="1.0.5",
+        description='A module to call RPI MATLAB primitives from Python3.6',
+        author='Keyi Liu',
+
+        platforms=['Linux', 'Windows', 'MacOS'],
+        packages=[
+            'rpi_test'
+        ],
+        package_data={'rpi_test': ['*.ctf']},
+        # Executes the custom code above in order to delete the build area.
+        cmdclass={'install': InstallRuntime}
+    )
+
+
+
