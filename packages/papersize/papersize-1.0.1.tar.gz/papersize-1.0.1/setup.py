2a058e7c9ee886dc62f593e9ca2361a808155056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:13 +0000

    Add package papersize/1.0.1/papersize-1.0.1.tar.gz

---
+++ packages/papersize/papersize-1.0.1.tar.gz/papersize-1.0.1/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin python
+# -*- coding: utf8 -*-
+
+# Copyright 2017 Louis Paternault
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Installer"""
+
+from setuptools import setup, find_packages
+import codecs
+import os
+
+def readme():
+    """Return content of the `README.rst` file."""
+    directory = os.path.dirname(os.path.join(
+        os.getcwd(),
+        __file__,
+        ))
+    with codecs.open(
+        os.path.join(directory, "README.rst"),
+        encoding="utf8",
+        mode="r",
+        errors="replace",
+        ) as file:
+        return file.read()
+
+setup(
+    name='papersize',
+    version="1.0.1",
+    packages=find_packages(exclude=["test*"]),
+    setup_requires=["hgtools"],
+    install_requires=[
+        ],
+    include_package_data=True,
+    author='Louis Paternault',
+    author_email='spalax+python@gresille.org',
+    description='Paper size related tools',
+    url='http://git.framasoft.org/spalax/papersize',
+    license="GPLv3 or any later version",
+    test_suite="test.suite",
+    keywords = "paper size standard",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Printing",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=readme(),
+    zip_safe = True,
+)
