5325b8951b5259d26d4501682ef5a7f03075720d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:39 +0000

    Add package jnrbase/0.8.1/jnrbase-0.8.1.tar.gz

---
+++ packages/jnrbase/jnrbase-0.8.1.tar.gz/jnrbase-0.8.1/doc/api/json_datetime.rst
@@ -0,0 +1,56 @@
+.. module:: jnrbase.json_datetime
+   :synopsis: :abbr:`JSON (JavaScript Object Notation)` datetime support
+
+json_datetime
+=============
+
+Classes
+-------
+
+.. class:: DatetimeEncoder
+
+    Custom JSON encoding for supporting ``datetime`` objects.
+
+    See also: :class:`json.JSONEncoder`
+
+.. automethod:: DatetimeEncoder.default
+
+Functions
+---------
+
+.. autofunction:: json_to_datetime
+
+.. function:: dump(...)
+
+    Simple :func:`json.dump` wrapper using :class:`DatetimeEncoder`.
+
+.. function:: dumps(...)
+
+    Simple :func:`json.dumps` wrapper using :class:`DatetimeEncoder`.
+
+.. function:: load(...)
+
+    Simple :func:`json.load` wrapper using :func:`json_to_datetime`.
+
+.. function:: loads(...)
+
+    Simple :func:`json.loads` wrapper using :func:`json_to_datetime`.
+
+.. _json_datetime-examples:
+
+Examples
+--------
+
+.. testsetup::
+
+    from datetime import datetime, timezone
+
+    from jnrbase.json_datetime import dumps, loads
+
+.. doctest::
+
+    >>> data = {'test': datetime(2016, 11, 30, 18, 35, tzinfo=timezone.utc)}
+    >>> dumps(data, indent=None)
+    '{"test": "2016-11-30T18:35:00Z"}'
+    >>> loads(dumps(data, indent=None)) == data
+    True
---
+++ packages/jnrbase/jnrbase-0.8.1.tar.gz/jnrbase-0.8.1/setup.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python
+"""setup.py - Setuptools tasks and config for jnrbase."""
+# Copyright © 2014-2017  James Rowe <jnrowe@gmail.com>
+#
+# This file is part of jnrbase.
+#
+# jnrbase is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# jnrbase is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# jnrbase.  If not, see <http://www.gnu.org/licenses/>.
+
+from configparser import ConfigParser
+from glob import glob
+from importlib.util import module_from_spec, spec_from_file_location
+from os import path
+
+from setuptools import setup
+from setuptools.command.test import test
+
+
+class PytestTest(test):
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = ['tests/', ]
+        self.test_suite = True
+
+    def run_tests(self):
+        from sys import exit
+        from pytest import main
+        exit(main(self.test_args))
+
+
+def import_file(package, fname):
+    """Import file directly.
+
+    This is a hack to import files from packages without importing
+    <package>/__init__.py, its purpose is to allow import without requiring
+    all the dependencies at this point.
+
+    Args:
+        package (str): Package to import from
+        fname (str): File to import
+    Returns:
+        types.ModuleType: Imported module
+    """
+    mod_name = fname.rstrip('.py')
+    spec = spec_from_file_location(mod_name, '{}/{}'.format(package, fname))
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+
+def make_list(s):
+    return s.strip().splitlines()
+
+
+conf = ConfigParser()
+conf.read('setup.cfg')
+metadata = dict(conf['metadata'])
+
+pip_support = import_file(metadata['name'], 'pip_support.py')
+
+extras_require = {}
+for file in glob('extra/requirements-*.txt'):
+    suffix = path.splitext(file)[0].split('-')[1]
+    if suffix not in ['doc', 'test']:
+        extras_require[suffix] = pip_support.parse_requires(file)
+
+tests_require = pip_support.parse_requires('extra/requirements-test.txt')
+
+metadata = dict(conf['metadata'])
+for k in ['classifiers', 'packages', 'py_modules']:
+    if k in metadata:
+        metadata[k] = make_list(metadata[k])
+
+with open('README.rst') as readme:
+    metadata['long_description'] = readme.read()
+
+_version = import_file(metadata['name'], '_version.py')
+
+setup(
+    version=_version.dotted,
+    extras_require=extras_require,
+    tests_require=tests_require,
+    cmdclass={'test': PytestTest},
+    zip_safe=False,
+    **metadata,
+)
