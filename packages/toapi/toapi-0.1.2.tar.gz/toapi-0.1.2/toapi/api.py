b8c3d0f48206ebd3c03fd5a0e2ce0419dfc56830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:02 +0000

    Add package toapi/0.1.2/toapi-0.1.2.tar.gz

---
+++ packages/toapi/toapi-0.1.2.tar.gz/toapi-0.1.2/toapi/api.py
@@ -0,0 +1,61 @@
+import re
+
+import requests
+
+
+class Api:
+    """Api handle the routes dispatch"""
+
+    def __init__(self, base_url, *args, **kwargs):
+        self.base_url = base_url
+        self.items = []
+
+    def parse(self, url):
+        """Parse items from a url"""
+        items = []
+        for index, item in enumerate(self.items):
+            if re.match(item['regex'], url):
+                items.append(item['item'])
+        if len(items) > 0:
+            html = self._fetch_page_source(self.base_url + url)
+            return self._parse_items(html, *items)
+        else:
+            return None
+
+    def register(self, item):
+        """Register route"""
+        self.items.append({
+            'regex': item.Meta.route,
+            'item': item
+        })
+
+    def serve(self, ip='0.0.0.0', post='5000'):
+        """Todo: Serve as an api server powered by flask"""
+        from flask import Flask, jsonify, request
+        app = Flask(__name__)
+
+        @app.route("/")
+        def index():
+            try:
+                return jsonify(self.parse(request.path))
+            except Exception as e:
+                return str(e)
+
+        app.run(ip, post)
+
+    def _fetch_page_source(self, url):
+        """Fetch the html of given url"""
+        return requests.get(url).text
+
+    def _parse_item(self, html, item):
+        """Parse a single item from html"""
+        result = {}
+        result[item.name] = item.parse(html)
+        return result
+
+    def _parse_items(self, html, *items):
+        """Parse kinds of items from html"""
+        results = {}
+        for item in items:
+            results.update(self._parse_item(html, item))
+        return results
