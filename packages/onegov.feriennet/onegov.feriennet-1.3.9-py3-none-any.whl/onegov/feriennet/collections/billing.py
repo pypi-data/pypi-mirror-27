39ca75288cc39b1ba3f6514efa45920d9fe6eeac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:02 +0000

    Add package onegov.feriennet/1.3.9/onegov.feriennet-1.3.9-py3-none-any.whl

---
+++ packages/onegov.feriennet/onegov.feriennet-1.3.9-py3-none-any.whl/onegov/feriennet/collections/billing.py
@@ -0,0 +1,193 @@
+import hashlib
+
+from collections import OrderedDict
+from decimal import Decimal
+from itertools import groupby
+from onegov.activity import Activity, Attendee, Booking, Occasion, InvoiceItem
+from onegov.activity import BookingCollection, InvoiceItemCollection
+from onegov.core.utils import normalize_for_url
+from onegov.pay import Price
+from onegov.user import User
+from sortedcontainers import SortedDict
+
+
+class BillingDetails(object):
+
+    __slots__ = (
+        'id', 'items', 'paid', 'total', 'title', 'outstanding', 'first',
+        'discourage_changes', 'disable_changes'
+    )
+
+    def __init__(self, title, items):
+        self.title = title
+        self.total = Decimal()
+        self.outstanding = Decimal()
+        self.paid = True
+        self.first = None
+        self.discourage_changes = False
+        self.disable_changes = False
+
+        def tally(item):
+            self.total += item.amount
+
+            if not item.paid:
+                self.paid = False
+                self.outstanding += item.amount
+
+            if item.discourage_changes:
+                self.discourage_changes = True
+
+            if item.disable_changes:
+                self.disable_changes = True
+
+            if not self.first:
+                self.first = item
+
+            return item
+
+        self.items = {
+            group: tuple(groupitems) for group, groupitems
+            in groupby((tally(i) for i in items), lambda i: i.group)
+        }
+
+        self.id = self.item_id(self.first)
+
+    @property
+    def price(self):
+        return Price(self.outstanding, 'CHF')
+
+    @staticmethod
+    def item_id(item):
+        components = (item.invoice, item.username)
+        return hashlib.md5('/'.join(components).encode('utf-8')).hexdigest()
+
+
+class BillingCollection(object):
+
+    def __init__(self, session, period, username=None, expand=False):
+        self.session = session
+        self.period = period
+        self.username = username
+        self.expand = expand
+
+        self.invoice_items = InvoiceItemCollection(
+            session=session,
+            username=username,
+            invoice=self.period.id.hex
+        )
+
+    @property
+    def period_id(self):
+        return self.period.id
+
+    def for_period(self, period):
+        return self.__class__(self.session, period, self.username, self.expand)
+
+    def for_username(self, username):
+        return self.__class__(self.session, self.period, username, self.expand)
+
+    def for_expand(self, expand):
+        return self.__class__(self.session, self.period, self.username, expand)
+
+    @property
+    def bills(self):
+        q = self.invoice_items.query()
+        q = q.order_by(
+            InvoiceItem.username,
+            InvoiceItem.group,
+            InvoiceItem.text
+        )
+
+        titles = OrderedDict(
+            (user.username, user.realname or user.username) for user
+            in self.session.query(User.username, User.realname).order_by(
+                User.title
+            )
+        )
+
+        bills = SortedDict(
+            lambda username: normalize_for_url(titles[username]))
+
+        for user, items in groupby(q, lambda i: i.username):
+            bills[user] = BillingDetails(titles[user], items)
+
+        return bills
+
+    @property
+    def total(self):
+        return self.invoice_items.total or Decimal("0.00")
+
+    @property
+    def outstanding(self):
+        return self.invoice_items.outstanding or Decimal("0.00")
+
+    def create_invoices(self, all_inclusive_booking_text=None):
+        assert not self.period.finalized
+
+        if self.period.all_inclusive and self.period.booking_cost:
+            assert all_inclusive_booking_text
+
+        # delete all existing invoice items
+        invoice = self.period.id.hex
+        session = self.session
+        period = self.period
+
+        for item in self.invoice_items.query():
+            assert item.invoice == invoice
+            session.delete(item)
+
+        # preload data to avoid more expensive joins
+        activities = {
+            r.id: r.title
+            for r in session.query(Occasion.id, Activity.title).join(Activity)
+        }
+
+        attendees = {
+            a.id: (a.name, a.username)
+            for a in session.query(
+                Attendee.id,
+                Attendee.name,
+                Attendee.username
+            )
+        }
+
+        # regenerate the bookings
+        bookings = BookingCollection(session, period_id=period.id)
+
+        q = bookings.query().with_entities(
+            Booking.username,
+            Booking.cost,
+            Booking.occasion_id,
+            Booking.attendee_id,
+        )
+        q = q.filter(Booking.state == 'accepted')
+
+        # keep track of the attendees which have at least one booking (even
+        # if said booking is free)
+        actual_attendees = set()
+
+        for booking in q:
+            actual_attendees.add(booking.attendee_id)
+
+            if booking.cost:
+                session.add(InvoiceItem(
+                    username=booking.username,
+                    invoice=invoice,
+                    group=attendees[booking.attendee_id][0],
+                    text=activities[booking.occasion_id],
+                    unit=booking.cost,
+                    quantity=1
+                ))
+
+        # add the all inclusive booking costs if necessary
+        if period.all_inclusive and period.booking_cost:
+            for id, (attendee, username) in attendees.items():
+                if id in actual_attendees:
+                    session.add(InvoiceItem(
+                        username=username,
+                        invoice=invoice,
+                        group=attendee,
+                        text=all_inclusive_booking_text,
+                        unit=period.booking_cost,
+                        quantity=1
+                    ))
---
+++ packages/onegov.feriennet/onegov.feriennet-1.3.9-py3-none-any.whl/onegov/feriennet/views/match.py
@@ -0,0 +1,220 @@
+from onegov.core.cache import lru_cache
+from onegov.activity import Attendee
+from onegov.activity import Booking, BookingCollection, Occasion
+from onegov.activity.matching import deferred_acceptance_from_database
+from onegov.core.security import Secret
+from onegov.feriennet import _, FeriennetApp
+from onegov.feriennet.collections import MatchCollection
+from onegov.feriennet.forms import MatchForm
+from onegov.feriennet.layout import MatchCollectionLayout
+from onegov.feriennet.models import PeriodMessage
+from onegov.org.new_elements import Block
+from onegov.org.new_elements import Confirm
+from onegov.org.new_elements import Intercooler
+from onegov.org.new_elements import Link
+from onegov.user import User, UserCollection
+
+
+@FeriennetApp.form(
+    model=MatchCollection,
+    form=MatchForm,
+    template='matches.pt',
+    permission=Secret)
+def handle_matches(self, request, form):
+
+    layout = MatchCollectionLayout(self, request)
+
+    users = UserCollection(request.app.session()).query()
+    users = users.with_entities(User.username, User.id)
+    users = {u.username: u.id.hex for u in users}
+
+    if form.submitted(request):
+        assert self.period.active and not self.period.confirmed
+
+        deferred_acceptance_from_database(
+            session=request.app.session(),
+            period_id=self.period_id,
+            score_function=form.scoring(request.app.session()))
+
+        self.period.scoring = form.scoring(request.app.session())
+
+        if form.confirm_period:
+            self.period.confirm()
+            PeriodMessage.create(self.period, request, 'confirmed')
+            request.success(_("The matching was confirmed successfully"))
+        else:
+            request.success(_("The matching run executed successfully"))
+
+    elif not request.POST:
+        form.process_scoring(self.period.scoring)
+
+    def activity_link(oid):
+        return request.class_link(Occasion, {'id': oid})
+
+    def occasion_links(oid):
+        if self.period.finalized:
+            yield Link(
+                text=_("Signup Attendee"),
+                traits=(
+                    Block(_(
+                        "The period has already been finalized. No new "
+                        "attendees may be added."
+                    ))
+                )
+            )
+        else:
+            yield Link(
+                text=_("Signup Attendee"),
+                url=request.return_here(
+                    request.class_link(Occasion, {'id': oid}, 'anmelden')
+                )
+            )
+
+    wishlist_phase = self.period.wishlist_phase
+    booking_phase = self.period.booking_phase
+
+    phase_title = wishlist_phase and _("Wishlist") or _("Bookings")
+
+    @lru_cache(maxsize=128)
+    def bookings_link(username):
+        return request.class_link(
+            BookingCollection, {
+                'period_id': self.period.id,
+                'username': username
+            }
+        )
+
+    @lru_cache(maxsize=128)
+    def user_link(username):
+        return request.return_here(
+            request.class_link(
+                User, {'id': users[username]}
+            )
+        )
+
+    @lru_cache(maxsize=128)
+    def attendee_link(attendee_id):
+        return request.return_here(
+            request.class_link(
+                Attendee, {'id': attendee_id}
+            )
+        )
+
+    def record_links(record):
+        yield Link(_("User"), user_link(record.attendee_username))
+        yield Link(_("Attendee"), attendee_link(record.attendee_id))
+        yield Link(phase_title, bookings_link(record.attendee_username))
+
+        if wishlist_phase:
+            yield Link(
+                text=_("Remove Wish"),
+                url=layout.csrf_protected_url(
+                    request.class_link(Booking, {'id': record.booking_id})
+                ),
+                traits=(
+                    Confirm(_(
+                        "Do you really want to remove ${attendee}'s wish?",
+                        mapping={
+                            'attendee': record.attendee_name
+                        }
+                    ), yes=_("Remove Wish")),
+                    Intercooler(
+                        request_method='DELETE',
+                        target='#{}'.format(record.booking_id)
+                    )
+                )
+            )
+
+        elif booking_phase and record.booking_state != 'accepted':
+            yield Link(
+                text=_("Remove Booking"),
+                url=layout.csrf_protected_url(
+                    request.class_link(Booking, {'id': record.booking_id})
+                ),
+                traits=(
+                    Confirm(_(
+                        "Do you really want to delete ${attendee}'s booking?",
+                        mapping={
+                            'attendee': record.attendee_name
+                        }
+                    ), yes=_("Remove Booking")),
+                    Intercooler(
+                        request_method='DELETE',
+                        target='#{}'.format(record.booking_id)
+                    )
+                )
+            )
+        elif booking_phase and record.booking_state == 'accepted':
+            yield Link(
+                text=_("Cancel Booking"),
+                url=layout.csrf_protected_url(
+                    request.class_link(
+                        Booking, {'id': record.booking_id}, 'cancel'
+                    )
+                ),
+                traits=(
+                    Confirm(_(
+                        "Do you really want to cancel ${attendee}'s booking?",
+                        mapping={
+                            'attendee': record.attendee_name
+                        }
+                    ), _("This cannot be undone."), yes=_("Cancel Booking")),
+                    Intercooler(
+                        request_method='POST',
+                    )
+                )
+            )
+
+    filters = {}
+    filters['states'] = tuple(
+        Link(
+            text=request.translate(text),
+            active=state in self.states,
+            url=request.link(self.for_filter(state=state))
+        ) for text, state in (
+            (_("Too many attendees"), 'overfull'),
+            (_("Fully occupied"), 'full'),
+            (_("Enough attendees"), 'operable'),
+            (_("Not enough attendees"), 'unoperable'),
+            (_("No attendees"), 'empty'),
+            (_("Rescinded"), 'cancelled')
+        )
+    )
+
+    return {
+        'layout': layout,
+        'title': _("Matches for ${title}", mapping={
+            'title': self.period.title
+        }),
+        'occasions': self.occasions,
+        'activity_link': activity_link,
+        'happiness': '{}%'.format(round(self.happiness * 100)),
+        'operability': '{}%'.format(round(self.operability * 100)),
+        'period': self.period,
+        'periods': request.app.periods,
+        'form': form,
+        'button_text': _("Run Matching"),
+        'model': self,
+        'filters': filters,
+        'record_links': record_links,
+        'occasion_links': occasion_links,
+        'booking_link': lambda record, name=None: request.class_link(
+            Booking, {'id': record.booking_id}, name
+        )
+    }
+
+
+@FeriennetApp.view(
+    model=MatchCollection,
+    name='reset',
+    permission=Secret,
+    request_method="POST")
+def reset_matching(self, request):
+    assert self.period.active and not self.period.confirmed
+
+    bookings = BookingCollection(request.app.session(), self.period_id)
+
+    for booking in bookings.query().filter(Booking.state != 'cancelled'):
+        booking.state = 'open'
+
+    request.success(_("The period was successfully reset"))
