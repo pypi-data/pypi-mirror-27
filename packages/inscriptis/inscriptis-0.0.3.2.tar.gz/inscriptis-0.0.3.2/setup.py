08d74c80b331ad465648864a6f19733da580fc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:14 +0000

    Add package inscriptis/0.0.3.2/inscriptis-0.0.3.2.tar.gz

---
+++ packages/inscriptis/inscriptis-0.0.3.2.tar.gz/inscriptis-0.0.3.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+      ###########################################
+      ## Metadata
+      name="inscriptis",
+      version="0.0.3.2",
+      description='inscriptis - HTML to text converter.',
+      author='Albert Weichselbraun, Fabian Odoni',
+      author_email='albert.weichselbraun@htwchur.ch, fabian.odoni@htwchur.ch',
+      url='http://github.com/weblyzard/inscriptis',
+      license="GPL2",
+      package_dir={'': 'src'},
+
+      ###########################################
+      ## Package List
+      packages=find_packages('src'),
+
+      ###########################################
+      ## scripts
+      scripts = [
+        'scripts/inscript.py'
+      ]
+
+)
