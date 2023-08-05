e9c3074dfc319bf299a7548be2bc79cd2fc78903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:13 +0000

    Add package django-talos/0.17.1846/django_talos-0.17.1846-py3-none-any.whl

---
+++ packages/django-talos/django_talos-0.17.1846-py3-none-any.whl/talos/compatibility/auth.py
@@ -0,0 +1,43 @@
+class AuthBackend(object):
+    def __init__(self, *args, **kwargs):
+        from ..models import BasicCredentialDirectory
+        from ..models import BasicIdentityDirectory
+
+        self.identity_directory = BasicIdentityDirectory.get_auth_directory()
+        self.credential_directory = BasicCredentialDirectory.get_auth_directory()
+        self.evidences = list(self.credential_directory.provided_evidences.filter(code__in=(
+            'authenticated',
+            'knowledge_factor',
+            'knowledge_factor_password')))
+
+    def authenticate(self, username=None, password=None):
+        from ..models import BasicIdentity
+        from ..models import Principal
+
+        if self.identity_directory.is_active:
+            try:
+                basic_identity = self.identity_directory.identities.get(username=username)
+                principal = basic_identity.principal
+            except BasicIdentity.DoesNotExist:
+                try:
+                    principal = Principal.objects.get(email=username)
+                except Principal.DoesNotExist:
+                    principal = None
+
+        if principal and principal.check_password(password):
+            principal._load_authentication_context(self.evidences)
+
+            return principal
+
+        return None
+
+    def get_user(self, user_id):
+        from ..models import Principal
+
+        try:
+            principal = Principal.objects.get(id=user_id)
+            principal._load_authentication_context(self.evidences)
+
+            return principal
+        except Principal.DoesNotExist:
+            return None
---
+++ packages/django-talos/django_talos-0.17.1846-py3-none-any.whl/talos/urls.py
@@ -0,0 +1,46 @@
+from django.urls import path
+
+from .views import BasicLoginView
+from .views import BasicPasswordChangeConfirmDoneView
+from .views import BasicPasswordChangeConfirmEditView
+from .views import BasicPasswordResetConfirmDoneView
+from .views import BasicPasswordResetConfirmEditView
+from .views import BasicPasswordResetRequestDoneView
+from .views import BasicPasswordResetRequestEditView
+from .views import EmailChangeConfirmDoneView
+from .views import EmailChangeConfirmEditView
+from .views import EmailChangeRequestDoneView
+from .views import EmailChangeRequestEditView
+from .views import IndexView
+from .views import LogoutView
+from .views import PrincipalRegistrationConfirmDoneView
+from .views import PrincipalRegistrationConfirmEditView
+from .views import PrincipalRegistrationRequestDoneView
+from .views import PrincipalRegistrationRequestEditView
+from .views import TokenLoginView
+
+
+auth_url_patterns = [
+    path('', IndexView.as_view(), name='talos-index'),
+    path('principal-registration-request-edit/', PrincipalRegistrationRequestEditView.as_view(), name='talos-principal-registration-request-edit'),
+    path('principal-registration-request-done/', PrincipalRegistrationRequestDoneView.as_view(), name='talos-principal-registration-request-done'),
+    path('principal-registration-confirm-edit/<slug:secret>', PrincipalRegistrationConfirmEditView.as_view(), name='talos-principal-registration-confirm-edit'),
+    path('principal-registration-confirm-done/', PrincipalRegistrationConfirmDoneView.as_view(), name='talos-principal-registration-confirm-done'),
+
+    path('email-change-request-edit/', EmailChangeRequestEditView.as_view(), name='talos-email-change-request-edit'),
+    path('email-change-request-done/', EmailChangeRequestDoneView.as_view(), name='talos-email-change-request-done'),
+    path('email-change-confirm-edit/<slug:secret>', EmailChangeConfirmEditView.as_view(), name='talos-email-change-confirm-edit'),
+    path('email-change-confirm-done/', EmailChangeConfirmDoneView.as_view(), name='talos-email-change-confirm-done'),
+
+    path('logout/', LogoutView.as_view(), name='talos-logout'),
+    path('basic-login/', BasicLoginView.as_view(), name='talos-basic-login'),
+    path('token-login/', TokenLoginView.as_view(), name='talos-token-login'),
+
+    path('basic-password-change-edit/', BasicPasswordChangeConfirmEditView.as_view(), name='talos-basic-password-change-edit'),
+    path('basic-password-change-done/', BasicPasswordChangeConfirmDoneView.as_view(), name='talos-basic-password-change-done'),
+
+    path('basic-password-reset-request-edit/', BasicPasswordResetRequestEditView.as_view(), name='talos-basic-password-reset-request-edit'),
+    path('basic-password-reset-request-done/', BasicPasswordResetRequestDoneView.as_view(), name='talos-basic-password-reset-request-done'),
+    path('basic-password-reset-confirm-edit/<slug:secret>', BasicPasswordResetConfirmEditView.as_view(), name='talos-basic-password-reset-token-edit'),
+    path('basic-password-reset-confirm-done/', BasicPasswordResetConfirmDoneView.as_view(), name='talos-basic-password-reset-confirm-done'),
+]
