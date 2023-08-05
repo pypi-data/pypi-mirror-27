a25297e9899841076193075d3b3d7b67083b3979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:24 +0000

    Add package flopy/3.2.7/flopy-3.2.7.r5.zip

---
+++ packages/flopy/flopy-3.2.7.r5.zip/flopy-3.2.7/flopy/version.py
@@ -0,0 +1,12 @@
+# flopy version file automatically created using...pre-commit.py
+# created on...December 01, 2017 15:39:27
+
+major = 3
+minor = 2
+micro = 7
+build = 0
+commit = 2477
+
+__version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
+__build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
+__git_commit__ = '{:d}'.format(commit)
