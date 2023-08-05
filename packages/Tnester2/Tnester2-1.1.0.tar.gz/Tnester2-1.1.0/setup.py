9cade5771a469297a9c36415b0451fcb96f6cf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:51 +0000

    Add package Tnester2/1.1.0/Tnester2-1.1.0.tar.gz

---
+++ packages/Tnester2/Tnester2-1.1.0.tar.gz/Tnester2-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='Tnester2',
+	version='1.1.0',
+	py_modules=['Tnester2'],
+	author='echo',
+	author_email='2026336228@qq.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nested lists',
+	)
