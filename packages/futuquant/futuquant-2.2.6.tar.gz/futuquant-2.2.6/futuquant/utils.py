648634cf50e81e85c17f874038817fbad4ba3b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:48 +0000

    Add package futuquant/2.2.6/futuquant-2.2.6.tar.gz

---
+++ packages/futuquant/futuquant-2.2.6.tar.gz/futuquant-2.2.6/futuquant/utils.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def is_str(obj):
+    if sys.version_info.major == 3:
+        return isinstance(obj, str) or isinstance(obj, bytes)
+    else:
+        return isinstance(obj, basestring)
+
+
+def price_to_str_int1000(price):
+    return str(int(round(float(price) * 1000, 0))) if str(price) is not '' else ''
+
+
+# 1000*int price to float val
+def int1000_price_to_float(price):
+    return round(float(price) / 1000.0, 3) if str(price) is not '' else float(0)
+
+
+# 10^9 int price to float val
+def int10_9_price_to_float(price):
+    return round(float(price) / float(10**9), 3) if str(price) is not '' else float(0)
+
+
