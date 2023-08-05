5b64b82b2d301e5b4f8969519cd44a3b86e0e5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:22 +0000

    Add package django-secrets/0.2.5/django-secrets-0.2.5.tar.gz

---
+++ packages/django-secrets/django-secrets-0.2.5.tar.gz/django-secrets-0.2.5/setup.py
@@ -0,0 +1,38 @@
+# @copyright: Andy Grabow (c) 2017. All rights reserved.
+from setuptools import setup, find_packages
+
+setup(
+    name='django-secrets',
+    description='The easy way of handling Django secrets.',
+    version='0.2.5',
+    url='https://github.com/kakulukia/django-secrets',
+    download_url='https://github.com/kakulukia/django-secrets/tarball/0.2.5',
+    author='Andy Grabow',
+    author_email='andy@freilandkiwis.de',
+    license='MIT',
+    install_requires=[
+      'Django>=1.11',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    keywords=['Django', 'secrets', 'deployment'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Security',
+    ],
+)
