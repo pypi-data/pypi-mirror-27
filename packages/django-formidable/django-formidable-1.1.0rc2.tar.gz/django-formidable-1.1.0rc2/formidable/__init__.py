a4abfe48e6c40da428b794d02f8a69fe7d1065b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:50 +0000

    Add package django-formidable/1.1.0rc2/django-formidable-1.1.0rc2.tar.gz

---
+++ packages/django-formidable/django-formidable-1.1.0rc2.tar.gz/django-formidable-1.1.0rc2/formidable/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from .json_migrations import latest_version
+
+
+default_app_config = 'formidable.app.FormidableConfig'
+version = '1.1.0.rc2'
+json_version = latest_version
