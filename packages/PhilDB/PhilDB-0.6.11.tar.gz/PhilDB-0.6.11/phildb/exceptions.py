72013a43cd3b520b46a8c7397b178475603f265b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:27 +0000

    Add package PhilDB/0.6.11/PhilDB-0.6.11.tar.gz

---
+++ packages/PhilDB/PhilDB-0.6.11.tar.gz/PhilDB-0.6.11/phildb/exceptions.py
@@ -0,0 +1,14 @@
+class AlreadyExistsError(Exception):
+    pass
+
+class DataError(Exception):
+    pass
+
+class DuplicateError(Exception):
+    pass
+
+class MissingAttributeError(Exception):
+    pass
+
+class MissingDataError(Exception):
+    pass
