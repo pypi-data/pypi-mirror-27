2ed2a08dee706c730317641218e430a3c232f854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:08 +0000

    Add package namesgenerator/0.3/namesgenerator-0.3.tar.gz

---
+++ packages/namesgenerator/namesgenerator-0.3.tar.gz/namesgenerator-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='namesgenerator',
+    version='0.3',
+    scripts=['namesgenerator.py'],
+    py_modules=['namesgenerator'],
+    url='https://github.com/shamrin/namesgenerator',
+    maintainer='jznf',
+    maintainer_email='bofh@zvsx.net',
+)
