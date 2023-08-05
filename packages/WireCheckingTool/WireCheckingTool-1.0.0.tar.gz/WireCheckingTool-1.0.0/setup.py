b9ac3664c73b69866af38f36a7245b3fe9a3f99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:57 +0000

    Add package WireCheckingTool/1.0.0/WireCheckingTool-1.0.0.tar.gz

---
+++ packages/WireCheckingTool/WireCheckingTool-1.0.0.tar.gz/WireCheckingTool-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='WireCheckingTool',
+      version='1.0.0',
+      description='This tool is used for checking wires with Raspberry Pi',
+      author='Acronics Systems Inc.',
+      author_email='huy.l.ho@acronicssystems.com',
+      license='Copyright (c) 2017 Acronics. All Rights Reserved',
+      packages=['WireCheckingTool'],
+      zip_safe=False)
