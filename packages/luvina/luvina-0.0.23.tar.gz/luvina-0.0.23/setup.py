339697b82b9836c949111c599313df6d86b4d2bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:12 +0000

    Add package luvina/0.0.23/luvina-0.0.23.tar.gz

---
+++ packages/luvina/luvina-0.0.23.tar.gz/luvina-0.0.23/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='luvina',
+      version='0.0.23',
+      description='High-level API for Natural Language Processing in NLTK',
+      author='Octavio Arriaga',
+      author_email='arriaga.camargo@gmail.com',
+      url='https://github.com/oarriaga/luvina',
+      license='MIT',
+      install_requires=['nltk', 'pyenchant', 'spacy'],
+      packages=find_packages()
+      )
