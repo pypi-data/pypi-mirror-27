68a09d17a19abd19fdf8018538a5a6566c671e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:51 +0000

    Add package tribology/0.2.18/tribology-0.2.18.tar.gz

---
+++ packages/tribology/tribology-0.2.18.tar.gz/tribology-0.2.18/tribology/__init__.py
@@ -0,0 +1,17 @@
+"""
+Methods related to general tribology
+"""
+
+__all__ = ['tribology',
+           'boundary_element',
+           'constants',
+           'dowson_hamrock',
+           'hertz',
+           'lubrication']
+
+from tribology.tribology import *
+from tribology.boundary_element import *
+from tribology.constants import *
+from tribology.dowson_hamrock import *
+from tribology.hertz import *
+from tribology.lubrication import *
