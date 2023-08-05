9764d7483f05913a3765f86e9e06d7099017af5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:42 +0000

    Add package rqalpha-data/0.0.1/rqalpha-data-0.0.1.tar.gz

---
+++ packages/rqalpha-data/rqalpha-data-0.0.1.tar.gz/rqalpha-data-0.0.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+from os.path import dirname, join
+from pip.req import parse_requirements
+
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+
+with open(join(dirname(__file__), 'rqalpha_data/VERSION.txt'), 'rb') as f:
+    version = f.read().decode('ascii').strip()
+
+setup(
+    name='rqalpha-data',
+    version=version,
+    description='A utility for RQAlpha to directly use data',
+    packages=find_packages(exclude=[]),
+    author='sixquant',
+    author_email='public@sixquant.com',
+    license='Apache License v2',
+    package_data={'': ['*.*']},
+    url='https://github.com/sixquant/rqalpha-data',
+    install_requires=[str(ir.req) for ir in parse_requirements("requirements.txt", session=False)],
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
