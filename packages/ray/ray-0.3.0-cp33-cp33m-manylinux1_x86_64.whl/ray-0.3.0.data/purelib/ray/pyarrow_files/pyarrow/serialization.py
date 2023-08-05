36e7bf9d766e0509c68c0c48ff7897d777144a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:45 +0000

    Add package ray/0.3.0/ray-0.3.0-cp33-cp33m-manylinux1_x86_64.whl

---
+++ packages/ray/ray-0.3.0-cp33-cp33m-manylinux1_x86_64.whl/ray-0.3.0.data/purelib/ray/pyarrow_files/pyarrow/serialization.py
@@ -0,0 +1,146 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from collections import OrderedDict, defaultdict
+import sys
+
+import numpy as np
+
+from pyarrow import serialize_pandas, deserialize_pandas
+from pyarrow.lib import _default_serialization_context
+
+
+def register_default_serialization_handlers(serialization_context):
+
+    # ----------------------------------------------------------------------
+    # Set up serialization for primitive datatypes
+
+    # TODO(pcm): This is currently a workaround until arrow supports
+    # arbitrary precision integers. This is only called on long integers,
+    # see the associated case in the append method in python_to_arrow.cc
+    serialization_context.register_type(
+        int, "int",
+        custom_serializer=lambda obj: str(obj),
+        custom_deserializer=lambda data: int(data))
+
+    if (sys.version_info < (3, 0)):
+        serialization_context.register_type(
+            long, "long",  # noqa: F821
+            custom_serializer=lambda obj: str(obj),
+            custom_deserializer=lambda data: long(data))  # noqa: F821
+
+    def _serialize_ordered_dict(obj):
+        return list(obj.keys()), list(obj.values())
+
+    def _deserialize_ordered_dict(data):
+        return OrderedDict(zip(data[0], data[1]))
+
+    serialization_context.register_type(
+        OrderedDict, "OrderedDict",
+        custom_serializer=_serialize_ordered_dict,
+        custom_deserializer=_deserialize_ordered_dict)
+
+    def _serialize_default_dict(obj):
+        return list(obj.keys()), list(obj.values()), obj.default_factory
+
+    def _deserialize_default_dict(data):
+        return defaultdict(data[2], zip(data[0], data[1]))
+
+    serialization_context.register_type(
+        defaultdict, "defaultdict",
+        custom_serializer=_serialize_default_dict,
+        custom_deserializer=_deserialize_default_dict)
+
+    serialization_context.register_type(
+        type(lambda: 0), "function",
+        pickle=True)
+
+    serialization_context.register_type(type, "type", pickle=True)
+
+    # ----------------------------------------------------------------------
+    # Set up serialization for numpy with dtype object (primitive types are
+    # handled efficiently with Arrow's Tensor facilities, see
+    # python_to_arrow.cc)
+
+    def _serialize_numpy_array(obj):
+        return obj.tolist(), obj.dtype.str
+
+    def _deserialize_numpy_array(data):
+        return np.array(data[0], dtype=np.dtype(data[1]))
+
+    serialization_context.register_type(
+        np.ndarray, 'np.array',
+        custom_serializer=_serialize_numpy_array,
+        custom_deserializer=_deserialize_numpy_array)
+
+    # ----------------------------------------------------------------------
+    # Set up serialization for pandas Series and DataFrame
+
+    try:
+        import pandas as pd
+
+        def _serialize_pandas_series(obj):
+            return serialize_pandas(pd.DataFrame({obj.name: obj}))
+
+        def _deserialize_pandas_series(data):
+            deserialized = deserialize_pandas(data)
+            return deserialized[deserialized.columns[0]]
+
+        def _serialize_pandas_dataframe(obj):
+            return serialize_pandas(obj)
+
+        def _deserialize_pandas_dataframe(data):
+            return deserialize_pandas(data)
+
+        serialization_context.register_type(
+            pd.Series, 'pd.Series',
+            custom_serializer=_serialize_pandas_series,
+            custom_deserializer=_deserialize_pandas_series)
+
+        serialization_context.register_type(
+            pd.DataFrame, 'pd.DataFrame',
+            custom_serializer=_serialize_pandas_dataframe,
+            custom_deserializer=_deserialize_pandas_dataframe)
+    except ImportError:
+        # no pandas
+        pass
+
+    # ----------------------------------------------------------------------
+    # Set up serialization for pytorch tensors
+
+    try:
+        import torch
+
+        def _serialize_torch_tensor(obj):
+            return obj.numpy()
+
+        def _deserialize_torch_tensor(data):
+            return torch.from_numpy(data)
+
+        for t in [torch.FloatTensor, torch.DoubleTensor, torch.HalfTensor,
+                  torch.ByteTensor, torch.CharTensor, torch.ShortTensor,
+                  torch.IntTensor, torch.LongTensor]:
+            serialization_context.register_type(
+                t, "torch." + t.__name__,
+                custom_serializer=_serialize_torch_tensor,
+                custom_deserializer=_deserialize_torch_tensor)
+    except ImportError:
+        # no torch
+        pass
+
+
+register_default_serialization_handlers(_default_serialization_context)
---
+++ packages/ray/ray-0.3.0-cp33-cp33m-manylinux1_x86_64.whl/ray-0.3.0.data/purelib/ray/rllib/ppo/ppo.py
@@ -0,0 +1,271 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import time
+
+import numpy as np
+import pickle
+import tensorflow as tf
+from tensorflow.python import debug as tf_debug
+
+import ray
+from ray.rllib.agent import Agent
+from ray.tune.result import TrainingResult
+from ray.rllib.ppo.runner import Runner, RemoteRunner
+from ray.rllib.ppo.rollout import collect_samples
+from ray.rllib.ppo.utils import shuffle
+
+
+DEFAULT_CONFIG = {
+    # Discount factor of the MDP
+    "gamma": 0.995,
+    # Number of steps after which the rollout gets cut
+    "horizon": 2000,
+    # If true, use the Generalized Advantage Estimator (GAE)
+    # with a value function, see https://arxiv.org/pdf/1506.02438.pdf.
+    "use_gae": True,
+    # GAE(lambda) parameter
+    "lambda": 1.0,
+    # Initial coefficient for KL divergence
+    "kl_coeff": 0.2,
+    # Number of SGD iterations in each outer loop
+    "num_sgd_iter": 30,
+    # Stepsize of SGD
+    "sgd_stepsize": 5e-5,
+    # TODO(pcm): Expose the choice between gpus and cpus
+    # as a command line argument.
+    "devices": ["/cpu:%d" % i for i in range(4)],
+    "tf_session_args": {
+        "device_count": {"CPU": 4},
+        "log_device_placement": False,
+        "allow_soft_placement": True,
+    },
+    # Batch size for policy evaluations for rollouts
+    "rollout_batchsize": 1,
+    # Total SGD batch size across all devices for SGD
+    "sgd_batchsize": 128,
+    # Coefficient of the value function loss
+    "vf_loss_coeff": 1.0,
+    # Coefficient of the entropy regularizer
+    "entropy_coeff": 0.0,
+    # PPO clip parameter
+    "clip_param": 0.3,
+    # Target value for KL divergence
+    "kl_target": 0.01,
+    # Config params to pass to the model
+    "model": {"free_log_std": False},
+    # Which observation filter to apply to the observation
+    "observation_filter": "MeanStdFilter",
+    # If >1, adds frameskip
+    "extra_frameskip": 1,
+    # Number of timesteps collected in each outer loop
+    "timesteps_per_batch": 4000,
+    # Each tasks performs rollouts until at least this
+    # number of steps is obtained
+    "min_steps_per_task": 1000,
+    # Number of actors used to collect the rollouts
+    "num_workers": 5,
+    # Dump TensorFlow timeline after this many SGD minibatches
+    "full_trace_nth_sgd_batch": -1,
+    # Whether to profile data loading
+    "full_trace_data_load": False,
+    # Outer loop iteration index when we drop into the TensorFlow debugger
+    "tf_debug_iteration": -1,
+    # If this is True, the TensorFlow debugger is invoked if an Inf or NaN
+    # is detected
+    "tf_debug_inf_or_nan": False,
+    # If True, we write tensorflow logs and checkpoints
+    "write_logs": True
+}
+
+
+class PPOAgent(Agent):
+    _agent_name = "PPO"
+    _default_config = DEFAULT_CONFIG
+
+    def _init(self):
+        self.global_step = 0
+        self.kl_coeff = self.config["kl_coeff"]
+        self.model = Runner(
+            self.env_creator, 1, self.config, self.logdir, False)
+        self.agents = [
+            RemoteRunner.remote(
+                self.env_creator, 1, self.config, self.logdir, True)
+            for _ in range(self.config["num_workers"])]
+        self.start_time = time.time()
+        if self.config["write_logs"]:
+            self.file_writer = tf.summary.FileWriter(
+                self.logdir, self.model.sess.graph)
+        else:
+            self.file_writer = None
+        self.saver = tf.train.Saver(max_to_keep=None)
+
+    def _train(self):
+        agents = self.agents
+        config = self.config
+        model = self.model
+
+        print("===> iteration", self.iteration)
+
+        iter_start = time.time()
+        weights = ray.put(model.get_weights())
+        [a.load_weights.remote(weights) for a in agents]
+        trajectory, total_reward, traj_len_mean = collect_samples(
+            agents, config, self.model.observation_filter,
+            self.model.reward_filter)
+        print("total reward is ", total_reward)
+        print("trajectory length mean is ", traj_len_mean)
+        print("timesteps:", trajectory["dones"].shape[0])
+        if self.file_writer:
+            traj_stats = tf.Summary(value=[
+                tf.Summary.Value(
+                    tag="ppo/rollouts/mean_reward",
+                    simple_value=total_reward),
+                tf.Summary.Value(
+                    tag="ppo/rollouts/traj_len_mean",
+                    simple_value=traj_len_mean)])
+            self.file_writer.add_summary(traj_stats, self.global_step)
+        self.global_step += 1
+
+        def standardized(value):
+            # Divide by the maximum of value.std() and 1e-4
+            # to guard against the case where all values are equal
+            return (value - value.mean()) / max(1e-4, value.std())
+
+        if config["use_gae"]:
+            trajectory["advantages"] = standardized(trajectory["advantages"])
+        else:
+            trajectory["returns"] = standardized(trajectory["returns"])
+
+        rollouts_end = time.time()
+        print("Computing policy (iterations=" + str(config["num_sgd_iter"]) +
+              ", stepsize=" + str(config["sgd_stepsize"]) + "):")
+        names = [
+            "iter", "total loss", "policy loss", "vf loss", "kl", "entropy"]
+        print(("{:>15}" * len(names)).format(*names))
+        trajectory = shuffle(trajectory)
+        shuffle_end = time.time()
+        tuples_per_device = model.load_data(
+            trajectory, self.iteration == 0 and config["full_trace_data_load"])
+        load_end = time.time()
+        rollouts_time = rollouts_end - iter_start
+        shuffle_time = shuffle_end - rollouts_end
+        load_time = load_end - shuffle_end
+        sgd_time = 0
+        for i in range(config["num_sgd_iter"]):
+            sgd_start = time.time()
+            batch_index = 0
+            num_batches = (
+                int(tuples_per_device) // int(model.per_device_batch_size))
+            loss, policy_loss, vf_loss, kl, entropy = [], [], [], [], []
+            permutation = np.random.permutation(num_batches)
+            # Prepare to drop into the debugger
+            if self.iteration == config["tf_debug_iteration"]:
+                model.sess = tf_debug.LocalCLIDebugWrapperSession(model.sess)
+            while batch_index < num_batches:
+                full_trace = (
+                    i == 0 and self.iteration == 0 and
+                    batch_index == config["full_trace_nth_sgd_batch"])
+                batch_loss, batch_policy_loss, batch_vf_loss, batch_kl, \
+                    batch_entropy = model.run_sgd_minibatch(
+                        permutation[batch_index] * model.per_device_batch_size,
+                        self.kl_coeff, full_trace,
+                        self.file_writer)
+                loss.append(batch_loss)
+                policy_loss.append(batch_policy_loss)
+                vf_loss.append(batch_vf_loss)
+                kl.append(batch_kl)
+                entropy.append(batch_entropy)
+                batch_index += 1
+            loss = np.mean(loss)
+            policy_loss = np.mean(policy_loss)
+            vf_loss = np.mean(vf_loss)
+            kl = np.mean(kl)
+            entropy = np.mean(entropy)
+            sgd_end = time.time()
+            print(
+                "{:>15}{:15.5e}{:15.5e}{:15.5e}{:15.5e}{:15.5e}".format(
+                    i, loss, policy_loss, vf_loss, kl, entropy))
+
+            values = []
+            if i == config["num_sgd_iter"] - 1:
+                metric_prefix = "ppo/sgd/final_iter/"
+                values.append(tf.Summary.Value(
+                    tag=metric_prefix + "kl_coeff",
+                    simple_value=self.kl_coeff))
+                values.extend([
+                    tf.Summary.Value(
+                        tag=metric_prefix + "mean_entropy",
+                        simple_value=entropy),
+                    tf.Summary.Value(
+                        tag=metric_prefix + "mean_loss",
+                        simple_value=loss),
+                    tf.Summary.Value(
+                        tag=metric_prefix + "mean_kl",
+                        simple_value=kl)])
+                if self.file_writer:
+                    sgd_stats = tf.Summary(value=values)
+                    self.file_writer.add_summary(sgd_stats, self.global_step)
+            self.global_step += 1
+            sgd_time += sgd_end - sgd_start
+        if kl > 2.0 * config["kl_target"]:
+            self.kl_coeff *= 1.5
+        elif kl < 0.5 * config["kl_target"]:
+            self.kl_coeff *= 0.5
+
+        info = {
+            "kl_divergence": kl,
+            "kl_coefficient": self.kl_coeff,
+            "rollouts_time": rollouts_time,
+            "shuffle_time": shuffle_time,
+            "load_time": load_time,
+            "sgd_time": sgd_time,
+            "sample_throughput": len(trajectory["observations"]) / sgd_time
+        }
+
+        print("kl div:", kl)
+        print("kl coeff:", self.kl_coeff)
+        print("rollouts time:", rollouts_time)
+        print("shuffle time:", shuffle_time)
+        print("load time:", load_time)
+        print("sgd time:", sgd_time)
+        print("sgd examples/s:", len(trajectory["observations"]) / sgd_time)
+        print("total time so far:", time.time() - self.start_time)
+
+        result = TrainingResult(
+            episode_reward_mean=total_reward,
+            episode_len_mean=traj_len_mean,
+            timesteps_this_iter=trajectory["dones"].shape[0],
+            info=info)
+
+        return result
+
+    def _save(self):
+        checkpoint_path = self.saver.save(
+            self.model.sess,
+            os.path.join(self.logdir, "checkpoint"),
+            global_step=self.iteration)
+        agent_state = ray.get([a.save.remote() for a in self.agents])
+        extra_data = [
+            self.model.save(),
+            self.global_step,
+            self.kl_coeff,
+            agent_state]
+        pickle.dump(extra_data, open(checkpoint_path + ".extra_data", "wb"))
+        return checkpoint_path
+
+    def _restore(self, checkpoint_path):
+        self.saver.restore(self.model.sess, checkpoint_path)
+        extra_data = pickle.load(open(checkpoint_path + ".extra_data", "rb"))
+        self.model.restore(extra_data[0])
+        self.global_step = extra_data[1]
+        self.kl_coeff = extra_data[2]
+        ray.get([
+            a.restore.remote(o)
+                for (a, o) in zip(self.agents, extra_data[3])])
+
+    def compute_action(self, observation):
+        observation = self.model.observation_filter(observation, update=False)
+        return self.model.common_policy.compute([observation])[0][0]
