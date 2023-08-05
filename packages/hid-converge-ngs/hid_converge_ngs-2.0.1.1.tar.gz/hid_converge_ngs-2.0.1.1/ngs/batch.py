1390cfa4694274a51dd0d97b221796f554c9d0c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:31 +0000

    Add package hid-converge-ngs/2.0.1.1/hid_converge_ngs-2.0.1.1.tar.gz

---
+++ packages/hid-converge-ngs/hid_converge_ngs-2.0.1.1.tar.gz/hid_converge_ngs-2.0.1.1/ngs/batch.py
@@ -0,0 +1,59 @@
+import configparser
+import logging
+import getpass
+
+from authentication.authentication import Authentication
+from service.batchService import BatchService
+import sys,os
+import json
+
+class Batch:
+	def __init__(self,baseUrl,ngsDir,userName,password):
+		self.baseUrl = baseUrl
+		self.ngsDir = ngsDir
+		self.userName = userName
+		self.password = password
+		self.config = configparser.RawConfigParser()
+		self.package_dir = os.path.dirname(os.path.abspath(__file__))
+		#self.readCfg()
+		if not self.login():
+			raise Exception("LoginError - %s and %s" % (self.userName,self.password))
+		
+
+	def readCfg(self):
+		self.config.read(os.path.join(self.package_dir,'init.cfg'))
+		self.baseUrl = self.config.get('urls','base_dir')
+		self.ngsDir = self.config.get('ngs','base_uri')
+
+	def login(self):
+		self.auth = Authentication()
+		return self.auth.performLogin(self.baseUrl,self.userName,self.password)
+
+	def logOut(self):
+		self.auth.performLogOut(self.baseUrl,self.userName)
+
+	def getAll(self):
+		batchDashborad = BatchService.getAll(self.baseUrl)
+		return batchDashborad
+
+	def get(self,batchId):
+		batch = BatchService.get(self.baseUrl,batchId)
+		return batch
+
+	def getResultsCardData(self,batchId):
+		return BatchService.getResultsCardData(self.baseUrl,self.ngsDir,batchId)
+
+	def getTaskReview(self,taskId):
+		return BatchService.getTaskReview(self.baseUrl,self.ngsDir,taskId)
+
+	
+
+
+
+
+
+
+
+
+
+
