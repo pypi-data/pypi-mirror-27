fd117eae7b00f220da8dc2977f7aa3cfd9e00118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:39 +0000

    Add package txttk/0.10.1/txttk-0.10.1.tar.gz

---
+++ packages/txttk/txttk-0.10.1.tar.gz/txttk-0.10.1/txttk/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+import os
+from txttk.corpus import *
+from txttk.feature import *
+from txttk.nlptools import *
+from txttk.report import *
+from txttk.retools import *
+
+__author__ = 'Chia-Jung, Yang'
+__email__ = 'jeroyang@gmail.com'
+__version__ = '0.10.1'
