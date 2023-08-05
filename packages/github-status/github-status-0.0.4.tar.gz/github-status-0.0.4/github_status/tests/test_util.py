6e23de014adc79c63544e07e9bd1b484c823761b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:53 +0000

    Add package github-status/0.0.4/github-status-0.0.4.tar.gz

---
+++ packages/github-status/github-status-0.0.4.tar.gz/github-status-0.0.4/github_status/tests/test_util.py
@@ -0,0 +1,39 @@
+import os
+from github_status import util
+
+import pytest
+
+
+@pytest.fixture
+def p_env(monkeypatch):
+    def patch(environ):
+        monkeypatch.setattr(os, 'environ', environ)
+        monkeypatch.setattr(os, 'getenv', lambda x: environ.get(x))
+    return patch
+
+
+class TestBuildIsTriggered(object):
+
+    def setup(self):
+        self.ghprb_env_vars = [
+            'ghprbActualCommit', 'ghprbTriggerAuthor', 'ghprbTargetBranch',
+            'ghprbTriggerAuthorLogin', 'ghprbCredentialsId', 'ghprbGhRepository',
+        ]
+
+    def test_is_triggered(self, p_env):
+        p_env(dict((k, '1') for k in self.ghprb_env_vars))
+        assert util.build_is_triggered() is True
+
+    def test_is_not_triggered(self, p_env):
+        assert util.build_is_triggered() is False
+
+class TestConstructURl(object):
+
+    def test_repo_gets_stripped(self, p_env):
+        p_env({'GITHUB_REPOSITORY': 'ceph/ceph/', 'GITHUB_SHA': 'asdf'})
+        assert util.construct_url() == 'https://api.github.com/repos/ceph/ceph/statuses/asdf'
+
+    def test_repo_removes_quotes(self, p_env):
+        p_env({'GITHUB_REPOSITORY': '"ceph/ceph/"', 'GITHUB_SHA': 'asdf'})
+        assert util.construct_url() == 'https://api.github.com/repos/ceph/ceph/statuses/asdf'
+
