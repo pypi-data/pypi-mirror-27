6c4791760429b90303c3cec70eb022d300c9f477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:29 +0000

    Add package fpr-infra/0.1.11/fpr_infra-0.1.11-py2.py3-none-any.whl

---
+++ packages/fpr-infra/fpr_infra-0.1.11-py2.py3-none-any.whl/fpr_infra/pyinfra.py
@@ -0,0 +1,90 @@
+# coding: utf-8
+
+import os
+from pathlib import Path
+from pyinfra.api import FactBase, operation
+from pyinfra.api.exceptions import OperationError
+from pyinfra.modules import files, python, server
+
+
+class NginxVersion(FactBase):
+    '''
+    Returns the nginx version installed.
+    '''
+
+    def command(self):
+        return "nginx -v 2>&1"
+
+    def process(self, output):
+        return str(output[0].split('/')[1].split()[0]).strip()
+
+
+class NodeVersion(FactBase):
+    """get odoo version"""
+
+    def command(self):
+        return "node --version"
+
+    def process(self, output):
+        return output[0].strip()
+
+
+class PythonVersion(FactBase):
+    """get python version"""
+
+    def command(self):
+        return 'cat /usr/local/lib/pyenv/_pyenv/version'
+
+    def process(self, output):
+        return output[0].strip()
+
+
+class UwsgiVersion(FactBase):
+    '''
+    Returns the uwsgi version installed.
+    '''
+
+    def command(self):
+        return "uwsgi --version"
+
+    def process(self, output):
+        return str(output[0]).strip()
+
+
+@operation
+def move(state, host, src, dest, add_deploy_dir=True, **kwargs):
+    """Move a file."""
+
+    # no source or destination
+    if src is None:
+        raise OperationError('source not defined')
+    if dest is None:
+        raise OperationError('destination not defined')
+
+    src_path = Path(state.deploy_dir, src) if add_deploy_dir and state.deploy_dir else Path(src)
+
+    kwargs.pop('sudo', None)
+    cmd = 'mv'
+    server.shell(
+        state, host,
+        {f"{cmd.capitalize()} {src_path.as_posix()} file to {dest}"},
+        f"{cmd} {src_path.as_posix()} {dest}",
+        sudo=True
+    )
+
+    if src_path.is_dir():
+        files.directory(
+            state, host,
+            {f"Change file {dest} mode"},
+            dest,
+            **kwargs,
+            sudo=True
+        )
+    else:
+        files.file(
+            state, host,
+            {f"Change file {dest} mode"},
+            dest,
+            **kwargs,
+            sudo=True
+        )
