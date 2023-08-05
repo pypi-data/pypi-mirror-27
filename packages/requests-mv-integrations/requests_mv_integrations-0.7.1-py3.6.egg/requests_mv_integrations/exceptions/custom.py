82cc7c5febfc35607c9ccec717141fced6bc6d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:24 +0000

    Add package requests-mv-integrations/0.7.1/requests_mv_integrations-0.7.1-py3.6.egg

---
+++ packages/requests-mv-integrations/requests_mv_integrations-0.7.1-py3.6.egg/requests_mv_integrations/exceptions/custom.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  @copyright 2017 TUNE, Inc. (http://www.tune.com)
+
+from requests_mv_integrations.errors.error_codes import TuneRequestErrorCodes
+from requests_mv_integrations.exceptions.base import (TuneRequestBaseError)
+
+
+class TuneRequestError(TuneRequestBaseError):
+    pass
+
+
+class TuneRequestClientError(TuneRequestBaseError):
+    pass
+
+
+class TuneRequestServiceError(TuneRequestBaseError):
+    pass
+
+
+class TuneRequestModuleError(TuneRequestBaseError):
+    pass
+
+
+class TuneRequestClientGoneError(TuneRequestModuleError):
+    """Request Mv Integration: Value error"""
+
+    def __init__(self, **kwargs):
+        error_code = kwargs.pop('error_code', None) or \
+            TuneRequestErrorCodes.GONE
+        super(TuneRequestClientGoneError, self).__init__(error_code=error_code, **kwargs)
+
+
+class TuneRequestValueError(TuneRequestModuleError):
+    """Request Mv Integration: Value error"""
+
+    def __init__(self, **kwargs):
+        error_code = kwargs.pop('error_code', None) or \
+            TuneRequestErrorCodes.REQ_ERR_ARGUMENT
+        super(TuneRequestValueError, self).__init__(error_code=error_code, **kwargs)
+
+
+class TuneRequestAuthenticationError(TuneRequestModuleError):
+    """Request Mv Integration: Authentication error"""
+
+    def __init__(self, **kwargs):
+        error_code = kwargs.pop('error_code', None) or \
+            TuneRequestErrorCodes.REQ_ERR_AUTH_ERROR
+        super(TuneRequestAuthenticationError, self).__init__(error_code=error_code, **kwargs)
