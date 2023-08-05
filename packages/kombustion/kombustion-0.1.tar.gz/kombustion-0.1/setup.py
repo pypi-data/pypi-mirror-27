8c8d59fde0584b0ef094318b64c41339ebaf51d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:48 +0000

    Add package kombustion/0.1/kombustion-0.1.tar.gz

---
+++ packages/kombustion/kombustion-0.1.tar.gz/kombustion-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kombustion',
+      version='0.1',
+      description='Eat a dict',
+      url='https://github.com/eL0ck/Kombustion',
+      author='eL0ck',
+      author_email='tpj800@gmail.com',
+      license='MIT',
+      packages=['kombustion'],
+      zip_safe=False)
