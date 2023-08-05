6d0c0a490f4090ba8fa0413b599030f7f98b79a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:57 +0000

    Add package release-notes-generator/1.0.7/release_notes_generator-1.0.7.tar.gz

---
+++ packages/release-notes-generator/release_notes_generator-1.0.7.tar.gz/release_notes_generator-1.0.7/release_notes_generator/cli.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+from release_notes_generator.release_note_generator import ReleaseNoteGenerator
+from release_notes_generator.services import ReleaseNotesFormatter
+
+def main():
+    token = os.getenv('GITHUB_API_TOKEN')
+    if not token:
+        print('ERROR: GITHUB_API_TOKEN must be set in the environment.')
+    else:
+        # repos of interest:
+        # wet_arms
+        # auto_pipeline
+        # wetlab-ops
+        repository_name = "auto_pipeline"
+
+        notes = ReleaseNoteGenerator(token, repository_name=repository_name).release_notes()
+        ReleaseNotesFormatter.output(notes)
