08550ff35cc795e140adca1cfe4f0174f7d1ff29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:52 +0000

    Add package jun-nester/1.3.0/jun_nester-1.3.0.tar.gz

---
+++ packages/jun-nester/jun_nester-1.3.0.tar.gz/jun_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'jun_nester',
+    version      = '1.3.0',
+    py_modules   = ['jun_nester'],
+    author       = 'goblinconan',
+    author_email = 'meguanjun@hotmail.com',
+    url          = 'http://www.headfirstlabs.com',
+    description  = 'A simple printer of jun_nester lists',
+    )
