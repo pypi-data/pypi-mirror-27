274b0e2e7909db9e98ee60b88eb2a3e66497d7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:20 +0000

    Add package cryptocurrency/0.4/cryptocurrency-0.4.tar.gz

---
+++ packages/cryptocurrency/cryptocurrency-0.4.tar.gz/cryptocurrency-0.4/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+
+setup(
+     name='cryptocurrency',
+     version='0.4',
+     license='MIT',
+     keywords='cryptocurrency bitcoin ethereum litecoin crypto',
+     author='Jake Meyer',
+     author_email='jakewmeyer@gmail.com',
+     url='https://github.com/jakewmeyer/Crypto'
+     )
+     
