aed2972242a6c4974805551f0004f6b386593899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:00 +0000

    Add package PyTwitchInteract/1.0.1/PyTwitchInteract-1.0.1.tar.gz

---
+++ packages/PyTwitchInteract/PyTwitchInteract-1.0.1.tar.gz/PyTwitchInteract-1.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='PyTwitchInteract',
+      version='1.0.1',
+      description='A collection of tools and utilities for easier interaction with Twitch.',
+      author='Vilsol',
+      author_email='me@vil.so',
+      url='https://github.com/Vilsol/PyTwitchInteract',
+      packages=[
+          "pytwitchinteract"
+      ],
+      )
