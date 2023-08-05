35cf0487782a9f20e6bb2739b93a26da1e74921e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:57 +0000

    Add package stylelens-product/1.0.10/stylelens-product-1.0.10.tar.gz

---
+++ packages/stylelens-product/stylelens-product-1.0.10.tar.gz/stylelens-product-1.0.10/stylelens_product/apis/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .image_api import ImageApi
+from .object_api import ObjectApi
+from .product_api import ProductApi
+from .version_api import VersionApi
