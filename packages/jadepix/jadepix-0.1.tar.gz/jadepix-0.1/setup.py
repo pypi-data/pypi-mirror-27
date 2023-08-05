d9a56c1d1714116b217afd41077a6489246b75e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:06 +0000

    Add package jadepix/0.1/jadepix-0.1.tar.gz

---
+++ packages/jadepix/jadepix-0.1.tar.gz/jadepix-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='jadepix',    # This is the name of your PyPI-package.
+    version='0.1',     # Update the version number for new releases
+    scripts=['jadepix-run']   # The name of your scipt, and also the command you'll be using for calling it
+)
+
+
+
