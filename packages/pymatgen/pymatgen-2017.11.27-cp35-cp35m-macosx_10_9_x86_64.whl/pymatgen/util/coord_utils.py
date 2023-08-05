548e1dea2bc42dfe69fda3d7931d0ed69509a0db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:03 +0000

    Add package pymatgen/2017.11.27/pymatgen-2017.11.27-cp35-cp35m-macosx_10_9_x86_64.whl

---
+++ packages/pymatgen/pymatgen-2017.11.27-cp35-cp35m-macosx_10_9_x86_64.whl/pymatgen/util/coord_utils.py
@@ -0,0 +1,6 @@
+import warnings
+
+warnings.warn("pymatgen.util.coord_utils has been renamed "
+              "pymatgen.util.coord. This stub will be removed in PMG 2018.1.1.")
+
+from pymatgen.util.coord import *
