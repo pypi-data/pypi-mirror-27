55ec7d7e72c500d1e9976544760d29f87b602c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:20 +0000

    Add package task-list-dev/1.0.4/task-list-dev-1.0.4.tar.gz

---
+++ packages/task-list-dev/task-list-dev-1.0.4.tar.gz/task-list-dev-1.0.4/docs/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to task-list-dev\u2019s documentation!"],titleterms:{dev:0,document:0,indic:0,list:0,tabl:0,task:0,welcom:0}})
