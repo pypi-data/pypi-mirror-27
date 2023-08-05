ec19d5a6be271783563f23fd593faab5e28d0f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:17 +0000

    Add package hitbtc/1.0.4.dev0/hitbtc-1.0.4.dev0.tar.gz

---
+++ packages/hitbtc/hitbtc-1.0.4.dev0.tar.gz/hitbtc-1.0.4.dev0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+VERSION = '1.0.4dev0'
+
+
+setup(name='hitbtc',
+      version=VERSION,
+      description='HitBTC Websocket API Client',
+      author='Nils Diefenbach',
+      author_email='23okrs20+gitlab@mykolab.com',
+      packages=['hitbtc'],
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      install_requires=['websocket-client'],
+      package_data={'': ['*.md', '*.rst']})
+
