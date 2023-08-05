fe0962d2d2d7c3946b2a883fac33b6324d1f3940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:15 +0000

    Add package check-tls-certs/0.10.0/check-tls-certs-0.10.0.tar.gz

---
+++ packages/check-tls-certs/check-tls-certs-0.10.0.tar.gz/check-tls-certs-0.10.0/test_check_tls_certs.py
@@ -0,0 +1,54 @@
+from click.testing import CliRunner
+import pytest
+
+
+@pytest.yield_fixture(scope="session", autouse=True)
+def event_loop_closing():
+    import check_tls_certs
+    loop = set()
+
+    def close_event_loop(_loop):
+        loop.add(_loop)
+
+    check_tls_certs.close_event_loop = close_event_loop
+    yield
+    for l in loop:
+        l.close()
+
+
+def test_arg(monkeypatch):
+    from check_tls_certs import main
+    l = []
+    monkeypatch.setattr(
+        "check_tls_certs.check",
+        lambda x: l.append(x) or ([], None))
+    runner = CliRunner()
+    result = runner.invoke(main, ['-vv', 'example.com/www.example.com'])
+    assert result.exit_code == 0
+    assert [[d for d, c in x] for x in l] == [
+        ['example.com', 'www.example.com']]
+    assert result.output == (
+        'example.com, www.example.com\n'
+        '0 error(s), 0 warning(s)\n')
+
+
+def test_file(monkeypatch, tmpdir):
+    from check_tls_certs import main
+    l = []
+    monkeypatch.setattr(
+        "check_tls_certs.check",
+        lambda x: l.append(x) or ([], None))
+    f = tmpdir.join("domains.txt")
+    f.write_binary(b"example.com/www.example.com\nfoo.com\nbar.com/\n    #none.bar.com\n    www.bar.com")
+    runner = CliRunner()
+    result = runner.invoke(main, ['-vv', '-f', f.strpath])
+    assert result.exit_code == 0
+    assert [[d for d, c in x] for x in l] == [
+        ['example.com', 'www.example.com'],
+        ['foo.com'],
+        ['bar.com', 'www.bar.com']]
+    assert result.output == (
+        'example.com, www.example.com\n'
+        'foo.com\n'
+        'bar.com, www.bar.com\n'
+        '0 error(s), 0 warning(s)\n')
