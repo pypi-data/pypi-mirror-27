c87ea5923e1bb6c37384d6f05bb9f7e95416e807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:07 +0000

    Add package django-opt-out/0.1.8/django_opt_out-0.1.8-py2.py3-none-any.whl

---
+++ packages/django-opt-out/django_opt_out-0.1.8-py2.py3-none-any.whl/django_opt_out/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+from . import views as v
+
+app_name = 'django_opt_out'
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="django_opt_out/base.html")),
+    url(r'^opt-out$', v.OptOutConfirm.as_view(), name='OptOutConfirm'),
+    url(r'^opt-out/(?P<pk>[\d]+)/(?P<secret>[\w]+)/(?P<email>[^/]+)$', v.OptOutSuccess.as_view(), name='OptOutSuccess'),
+    url(r'^opt-out/(?P<pk>[\d]+)/(?P<secret>[\w]+)/(?P<email>[^/]+)/update$', v.OptOutUpdate.as_view(), name='OptOutUpdate'),
+    url(r'^opt-out/removed$', v.OptOutRemoved.as_view(), name='OptOutRemoved'),
+]
