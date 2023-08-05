3217effe70d627d10e75bbb1e8d2f00fcc6724bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:07 +0000

    Add package pycoinone/2017.12.5a0.dev0/pycoinone-2017.12.5a0.dev0.tar.gz

---
+++ packages/pycoinone/pycoinone-2017.12.5a0.dev0.tar.gz/pycoinone-2017.12.5a0.dev0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='pycoinone',
+    version='2017.12.05a0.dev0',
+    packages=[
+        'coinone',
+        'coinone.api',
+        'coinone.api.common',
+        'coinone.api.v1',
+        'coinone.api.v2'
+    ],
+    url='http://github.com/gwangyi/pycoinone',
+    license='MIT',
+    author='Sungkwang Lee',
+    author_email='gwangyi.kr@gmail.com',
+    description='Python Coinone API wrapper',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6'
+    ],
+    install_requires=['requests']
+)
