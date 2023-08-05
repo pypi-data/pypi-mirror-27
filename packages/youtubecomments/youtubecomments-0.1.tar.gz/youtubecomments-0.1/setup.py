17b45f753c57bc6f77f9e347d967fe50a01e280d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:47 +0000

    Add package youtubecomments/0.1/youtubecomments-0.1.tar.gz

---
+++ packages/youtubecomments/youtubecomments-0.1.tar.gz/youtubecomments-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'youtubecomments',
+  packages = ['youtubecomments'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Get youtube public video comments',
+  author = 'Akash kumar',
+  author_email = 'akashkumar0824@gmail.com',
+  url = 'https://github.com/akash0824/youtubecomments', # use the URL to the github repo
+  #download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['youtube', 'comments', 'python', 'googleapi'], # arbitrary keywords
+  classifiers = [],
+)
