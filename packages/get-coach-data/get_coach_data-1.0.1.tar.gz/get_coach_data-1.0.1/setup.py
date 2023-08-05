dddd83d94008f99c7dcbdcf24d8a5315d368f270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:52 +0000

    Add package get-coach-data/1.0.1/get_coach_data-1.0.1.tar.gz

---
+++ packages/get-coach-data/get_coach_data-1.0.1.tar.gz/get_coach_data-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="get_coach_data",
+    version="1.0.1",
+    py_modules=["get_coach_data"],
+    author="Liu KangXin",
+    author_email="robinliu97@outlook.com",
+    url="http://www.sjtu.edu.cn",
+    description="A programme compiling with Chapter 5 of Head First Python"
+)
