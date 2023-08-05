a59e092b5e4a1c1eadd3f3e556a1738407ea7de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:52 +0000

    Add package django-react-templatetags/4.0.0/django_react_templatetags-4.0.0.tar.gz

---
+++ packages/django-react-templatetags/django_react_templatetags-4.0.0.tar.gz/django_react_templatetags-4.0.0/setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import sys
+import re
+import pip
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+# Handle requirements
+requires = parse_requirements("requirements/install.txt",
+                              session=pip.download.PipSession())
+install_requires = [str(ir.req) for ir in requires]
+
+requires = parse_requirements("requirements/tests.txt",
+                              session=pip.download.PipSession())
+tests_require = [str(ir.req) for ir in requires]
+
+# Convert markdown to rst
+try:
+    from pypandoc import convert
+    long_description = convert("README.md", "rst")
+except:
+    long_description = ""
+
+version = ''
+with io.open('django_react_templatetags/__init__.py', 'r', encoding='utf8') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name="django_react_templatetags",
+    version=version,
+    description=("This django library allows you to add React components into your django templates."),  # NOQA
+    long_description=long_description,
+    author="Fröjd",
+    author_email="martin@marteinn.se",
+    url="https://github.com/frojd/django-react-templatetags",
+    packages=find_packages(exclude=('tests*',)),
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        'ssr': ['requests'],
+    },
+    tests_require=tests_require,
+    license="MIT",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Environment :: Web Environment',
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2',
+        "Programming Language :: Python :: 2.7",
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Topic :: Utilities',
+    ],
+)
