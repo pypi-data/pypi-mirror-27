fb0063bbcf4ec5a5883ab70e18451d7803f90a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:57 +0000

    Add package tensorci/0.0.4/tensorci-0.0.4-py2.py3-none-any.whl

---
+++ packages/tensorci/tensorci-0.0.4-py2.py3-none-any.whl/tensorci/commands/__init__.py
@@ -0,0 +1,21 @@
+from login import login
+from logout import logout
+from create import create
+from init import init
+from push import push
+from use_team import use_team
+from get import get
+from current_team import current_team
+from version import version
+
+all_cmds = [
+  login,
+  logout,
+  create,
+  init,
+  push,
+  use_team,
+  get,
+  current_team,
+  version
+]
