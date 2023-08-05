446e8072d68a09ef43b1f757802517256f74fd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:15 +0000

    Add package biocode_fims/0.2.3/biocode_fims-0.2.3.tar.gz

---
+++ packages/biocode_fims/biocode_fims-0.2.3.tar.gz/biocode_fims-0.2.3/biocode_fims/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Biocode FIMS."""
+
+__author__ = """Mike Trizna"""
+__email__ = 'mike.trizna@gmail.com'
+__version__ = '0.2.3'
+
+from .biocode_fims import *
