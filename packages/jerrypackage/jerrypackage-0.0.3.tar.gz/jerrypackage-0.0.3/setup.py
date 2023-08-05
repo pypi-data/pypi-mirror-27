09761d2eb22f24b4c89e98efc41037e60381153e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:55 +0000

    Add package jerrypackage/0.0.3/jerrypackage-0.0.3.tar.gz

---
+++ packages/jerrypackage/jerrypackage-0.0.3.tar.gz/jerrypackage-0.0.3/setup.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='jerrypackage',
+    version='0.0.3',
+    author='jerrychy',
+    author_email='jerrychen@ustc.edu',
+    url='https://www.zhihu.com/people/jerrychen-57/activities',
+    description='Tools for daily use',
+    packages=['jerrypackage'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+
+        ]
+    }
+)
