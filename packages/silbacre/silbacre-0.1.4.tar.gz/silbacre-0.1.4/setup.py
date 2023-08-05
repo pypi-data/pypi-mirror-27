22fd51a71ea2a37b3c3e31f29233f180939727c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:06 +0000

    Add package silbacre/0.1.4/silbacre-0.1.4.tar.gz

---
+++ packages/silbacre/silbacre-0.1.4.tar.gz/silbacre-0.1.4/setup.py
@@ -0,0 +1,15 @@
+import os
+
+from setuptools import setup
+config_path = os.path.join(os.path.expanduser("~"), ".loquitor")
+py_modules = ['bot', 'skeleton']
+setup(name='silbacre',
+      version='0.1.4',
+      description='Silence (SMSSecure) backup file API',
+      author='Ralph Embree',
+      author_email='ralph.embree@brominator.org',
+      url='https://gitlab.com/ralphembree/silbacre',
+      #packages=['silbacre'],
+      install_requires=['bs4', 'phonenumbers', 'strudel'],
+      scripts=['silbacre'],
+)
