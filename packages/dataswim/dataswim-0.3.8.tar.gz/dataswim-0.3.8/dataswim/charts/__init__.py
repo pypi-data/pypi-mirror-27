88534a485d08bf343633c8f3e38d5054a017aab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:26 +0000

    Add package dataswim/0.3.8/dataswim-0.3.8.tar.gz

---
+++ packages/dataswim/dataswim-0.3.8.tar.gz/dataswim-0.3.8/dataswim/charts/__init__.py
@@ -0,0 +1,191 @@
+from .bokeh import Bokeh
+from .altair import Altair
+from .colors import Colors
+
+
+class Plot(Bokeh, Altair, Colors):
+    """
+    Class to handle charts
+    """
+
+    def __init__(self, df=None):
+        """
+        Initialize
+        """
+        self.df = df
+        self.x = None
+        self.err = None
+        self.y = None
+        self.chart_obj = None
+        self.chart_opts = dict(width=940)
+        self.chart_style = None
+        self.label = None
+        self.engine = "bokeh"
+
+    def chart(self, x=None, y=None, chart_type="line", opts=None, style=None, label=None, **kwargs):
+        """
+        Get a chart
+        """
+        try:
+            self.chart_obj = self._chart(
+                x, y, chart_type, opts, style, label, **kwargs)
+        except Exception as e:
+            self.err(e, self.chart, "Can not create chart")
+
+    def chart_(self, x=None, y=None, chart_type="line", opts=None, style=None, label=None, **kwargs):
+        """
+        Get a chart
+        """
+        try:
+            return self._chart(x, y, chart_type, opts, style, label, **kwargs)
+        except Exception as e:
+            self.err(e, self.chart, "Can not create chart")
+
+    def _chart(self, x=None, y=None, chart_type="line", opts=None, style=None, label=None, **kwargs):
+        """
+        Initialize chart options
+        """
+        if opts is not None:
+            self.chart_opts = opts
+        if style is not None:
+            self.chart_style = style
+        if label is not None:
+            self.label = label
+        self.x = x
+        self.y = y
+        try:
+            chart_obj = self._get_chart(chart_type, x,
+                                        y, style=style, opts=opts, label=label, **kwargs)
+            return chart_obj
+        except Exception as e:
+            self.err(e)
+
+    def bar_(self, label=None, style=None, opts=None):
+        """
+        Get a bar chart
+        """
+        try:
+            return self._get_chart("bar", style=style, opts=opts, label=label)
+        except Exception as e:
+            self.err(e, self.chart, "Can draw bar chart")
+
+    def line_(self, label=None, style=None, opts=None):
+        """
+        Get a line chart
+        """
+        try:
+            return self._get_chart("line", style=style, opts=opts, label=label)
+        except Exception as e:
+            self.err(e, self.line_, "Can draw line chart")
+
+    def area_(self, label=None, style=None, opts=None):
+        """
+        Get an area chart
+        """
+        try:
+            return self._get_chart("area", style=style, opts=opts, label=label)
+        except Exception as e:
+            self.err(e, self.area_, "Can draw area chart")
+
+    def hist_(self, label=None, style=None, opts=None):
+        """
+        Get an historiogram chart
+        """
+        try:
+            return self._get_chart("hist", style=style, opts=opts, label=label)
+        except Exception as e:
+            self.err(e, self.hist_, "Can draw historiogram")
+
+    def errorbar_(self, label=None, style=None, opts=None):
+        """
+        Get a point chart
+        """
+        try:
+            return self._get_chart("err", style=style, opts=opts, label=label)
+        except Exception as e:
+            self.err(e, self.errorbar_, "Can draw errorbar chart")
+
+    def point_(self, label=None, style=None, opts=None):
+        """
+        Get a point chart
+        """
+        try:
+            return self._get_chart("point", style=style, opts=opts, label=label)
+        except Exception as e:
+            self.err(e, self.point_, "Can draw point chart")
+
+    def heatmap_(self, label=None, style=None, opts=None):
+        """
+        Get a heatmap chart
+        """
+        try:
+            return self._get_chart("point", style=style, opts=opts, label=label)
+        except Exception as e:
+            self.err(e, self.heatmap_, "Can draw heatmap")
+
+    def line_point_(self, label=None, style=None, opts=None, colors={"line": "yellow", "point": "navy"}):
+        """
+        Get a line and point chart
+        """
+        try:
+            if style is None:
+                style = self.chart_style
+            style["color"] = colors["line"]
+            l = self._get_chart("line", style=style, opts=opts, label=label)
+            style["color"] = colors["point"]
+            p = self._get_chart("point", style=style, opts=opts, label=label)
+            return l * p
+        except Exception as e:
+            self.err(e, self.line_point_, "Can draw line_point chart")
+
+    def opts(self, dictobj):
+        """
+        Add or update an option value to defaults
+        """
+        for k in dictobj:
+            self.chart_opts[k] = dictobj[k]
+
+    def style(self, dictobj):
+        """
+        Add or update a style value to defaults
+        """
+        for k in dictobj:
+            self.chart_style[k] = dictobj[k]
+
+    def _get_chart(self, chart_type, x=None, y=None, style=None, opts=None, label=None, **kwargs):
+        """
+        Get a full chart object
+        """
+        if x is None:
+            if self.x is None:
+                self.err(
+                    self._get_chart, "X field is not set: please specify a parameter")
+                return
+            x = self.x
+        if y is None:
+            if self.y is None:
+                self.err(
+                    self._get_chart, "Y field is not set: please specify a parameter")
+                return
+            y = self.y
+        if opts is None:
+            opts = self.chart_opts
+        if style is None:
+            style = self.chart_style
+        if x is None:
+            x = self.x
+        if y is None:
+            y = self.y
+        if self.engine == "bokeh":
+            func = self._get_bokeh_chart
+        elif self.engine == "altair":
+            func = self._get_altair_chart
+        else:
+            self.err(self._get_chart, "Engine " + self.engine + " unknown")
+            return
+        try:
+            chart = func(
+                x, y, chart_type, label, opts, style, **kwargs)
+            return chart
+        except Exception as e:
+            self.err(e)
---
+++ packages/dataswim/dataswim-0.3.8.tar.gz/dataswim-0.3.8/dataswim/charts/bokeh.py
@@ -0,0 +1,99 @@
+import holoviews as hv
+from bokeh.embed import components
+from holoviews.core.data.interface import DataError
+
+
+bokeh_renderer = hv.renderer('bokeh')
+
+
+class Bokeh():
+    """
+    A class to handle charts with the Bokeh library
+    """
+
+    def __init__(self, df=None, db=None):
+        """
+        Initialize
+        """
+        self.df = df
+        self.x_field = None
+        self.y_field = None
+        self.chart_obj = None
+        self.chart_opts = dict(width=940)
+        self.chart_style = None
+        self.label = None
+        self.engine = "bokeh"
+
+    def bokeh_header_(self):
+        """
+        Returns html script tags for Bokeh
+        """
+        header = """
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bokeh/0.12.10/bokeh.min.js"></script>         
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bokeh/0.12.10/bokeh.min.js.map"></script>
+        <script type="text/javascript">
+            Bokeh.set_log_level("info");
+        </script>
+        """
+        return header
+
+    def _get_bokeh_chart(self, x_field, y_field, chart_type, label, opts, style, **kwargs):
+        """
+        Get a Bokeh chart object
+        """
+        if type(x_field) == list:
+            kdims = x_field
+        else:
+            kdims = [x_field]
+        if type(y_field) == list:
+            vdims = y_field
+        else:
+            vdims = [y_field]
+        args = kwargs
+        args["data"] = self.df
+        args["kdims"] = kdims
+        args["vdims"] = vdims
+        if label is not None:
+            args["label"] = label
+        else:
+            if self.label is not None:
+                args["label"] = self.label
+        chart = None
+        try:
+            if chart_type == "line":
+                chart = hv.Curve(**args)
+            elif chart_type == "point":
+                chart = hv.Scatter(**args)
+            elif chart_type == "area":
+                chart = hv.Area(**args)
+            elif chart_type == "bar":
+                chart = hv.Bars(**args)
+            elif chart_type == "hist":
+                chart = hv.Histogram(**args)
+            elif chart_type == "err":
+                chart = hv.ErrorBars(**args)
+            endchart = chart(plot=opts, style=style)
+            return endchart
+        except DataError as e:
+            msg = "Column not found in " + x_field + " and " + y_field
+            self.err(e, self._get_bokeh_chart, msg)
+        except Exception as e:
+            self.err(e)
+        if chart is None:
+            self.err("Chart type " + chart_type +
+                     " unknown", self._get_bokeh_chart)
+
+    def _get_bokeh_html(self, chart_obj):
+        """
+        Get the html for a Bokeh chart
+        """
+        global bokeh_renderer
+        try:
+            renderer = bokeh_renderer
+            p = renderer.get_plot(chart_obj).state
+            script, div = components(p)
+            return script + "\n" + div
+
+        except Exception as e:
+            self.err(e, self._get_bokeh_html,
+                     "Can not get html from the Bokeh rendering engine")
