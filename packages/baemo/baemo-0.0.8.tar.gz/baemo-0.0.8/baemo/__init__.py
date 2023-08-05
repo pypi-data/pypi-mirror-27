95f0c871b352dbc0cdf28f0a869147722509677b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:11 +0000

    Add package baemo/0.0.8/baemo-0.0.8.tar.gz

---
+++ packages/baemo/baemo-0.0.8.tar.gz/baemo-0.0.8/baemo/__init__.py
@@ -0,0 +1,11 @@
+#     __
+#    / /_  ____  ___  ____ ___  ____
+#   / __ \/ __ `/ _ \/ __ `__ \/ __ \
+#  / /_/ / /_/ /  __/ / / / / / /_/ /
+# /_.___/\__,_/\___/_/ /_/ /_/\____/
+# baemo
+
+__title__ = "baemo"
+__author__ = "Christopher Antonellis"
+__license__ = "MIT"
+__copyright__ = "Copyright 2017 Christopher Antonellis"
