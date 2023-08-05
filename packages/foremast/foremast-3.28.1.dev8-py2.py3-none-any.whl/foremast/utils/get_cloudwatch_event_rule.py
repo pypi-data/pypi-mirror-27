9720313894a0cb34318408e4ebe062cc283ceecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:14 +0000

    Add package foremast/3.28.1.dev8/foremast-3.28.1.dev8-py2.py3-none-any.whl

---
+++ packages/foremast/foremast-3.28.1.dev8-py2.py3-none-any.whl/foremast/utils/get_cloudwatch_event_rule.py
@@ -0,0 +1,22 @@
+"""CloudWatch Event functions."""
+import logging
+
+import boto3
+
+from ..utils.awslambda import get_lambda_alias_arn
+
+LOG = logging.getLogger(__name__)
+
+
+def get_cloudwatch_event_rule(app_name, account, region):
+    """Get CloudWatch Event rule names."""
+    session = boto3.Session(profile_name=account, region_name=region)
+    cloudwatch_client = session.client('events')
+
+    lambda_alias_arn = get_lambda_alias_arn(app=app_name, account=account, region=region)
+    rule_names = cloudwatch_client.list_rule_names_by_target(TargetArn=lambda_alias_arn)
+
+    if rule_names['RuleNames']:
+        return rule_names['RuleNames']
+    else:
+        LOG.debug("No event rules found")
