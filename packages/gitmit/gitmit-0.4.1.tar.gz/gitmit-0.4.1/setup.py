ff7a0c0446fa8a89cb597547c60df98e13ae18c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:45 +0000

    Add package gitmit/0.4.1/gitmit-0.4.1.tar.gz

---
+++ packages/gitmit/gitmit-0.4.1.tar.gz/gitmit-0.4.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from gitmit import VERSION
+
+setup(
+      name = "gitmit"
+    , version = VERSION
+    , packages = ['gitmit'] + ['gitmit.%s' % pkg for pkg in find_packages('gitmit')]
+    , include_package_data = True
+
+    , install_requires =
+      [ "pygit2==0.26.1"
+      ]
+
+    , extras_require =
+      { "tests":
+        [ "noseOfYeti>=1.5.0"
+        , "nose"
+        , "mock==1.0.1"
+        , "tox"
+        ]
+      }
+
+    , entry_points =
+      { 'console_scripts' :
+        [ 'gitmit = gitmit.executor:main'
+        ]
+      }
+
+    # metadata for upload to PyPI
+    , url = "https://github.com/delfick/gitmit"
+    , author = "Stephen Moore"
+    , author_email = "delfick755@gmail.com"
+    , description = "Python library to discover commit times of all files under a git repository"
+    , keywords = "git,commit,mtime"
+    )
+
