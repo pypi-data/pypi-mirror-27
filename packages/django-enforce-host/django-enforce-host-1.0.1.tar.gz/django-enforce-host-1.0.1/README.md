fdfd57673f5b2044d311f1ca197ce9497001a1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package django-enforce-host/1.0.1/django-enforce-host-1.0.1.tar.gz

---
+++ packages/django-enforce-host/django-enforce-host-1.0.1.tar.gz/django-enforce-host-1.0.1/README.md
@@ -0,0 +1,30 @@
+django-enforce-host
+===================
+
+[![Build Status](https://travis-ci.org/dabapps/django-enforce-host.svg)](https://travis-ci.org/dabapps/django-enforce-host)
+[![pypi release](https://img.shields.io/pypi/v/django-enforce-host.svg)](https://pypi.python.org/pypi/django-enforce-host)
+
+Sometimes, it's unavoidable that multiple URLs point at the same app - for example, on Heroku, all apps get a `.herokuapp.com` address, as well as any custom domains that are pointed at them.
+
+This is a simple Django middleware that redirects all traffic from hosts other than the one(s) you specify to your canonical URL.
+
+Tested against Django 1.8, 1.9, 1.10, 1.11 on Python 2.7, 3.4 and 3.6
+
+### Installation
+
+Install from PIP
+
+    pip install django-enforce-host
+
+In your `MIDDLEWARE` setting, add the following at the top:
+
+    MIDDLEWARE = [
+        'enforce_host.EnforceHostMiddleware',
+        ... other middleware
+    ]
+
+Set the following setting to be either a single allowed host, or a list of allowed hosts:
+
+    ENFORCE_HOST = 'yourapp.com'
+
+That's it!
---
+++ packages/django-enforce-host/django-enforce-host-1.0.1.tar.gz/django-enforce-host-1.0.1/enforce_host/__init__.py
@@ -0,0 +1,42 @@
+__version__ = '1.0.1'
+
+from django.conf import settings
+from django.core.exceptions import MiddlewareNotUsed
+from django.http import HttpResponsePermanentRedirect
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    MiddlewareMixin = object
+
+
+try:
+  string_type = basestring
+except NameError:
+  string_type = str
+
+
+class EnforceHostMiddleware(MiddlewareMixin):
+
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+        setting_value = getattr(settings, 'ENFORCE_HOST', None)
+
+        if setting_value is None:
+            raise MiddlewareNotUsed()
+
+        if isinstance(setting_value, string_type):
+            setting_value = [setting_value]
+
+        self.allowed_hosts = setting_value
+
+    def process_request(self, request):
+        host = request.get_host()
+
+        if host in self.allowed_hosts:
+            return
+
+        new_url = "%s://%s%s" % (
+            'https' if request.is_secure() else 'http',
+            self.allowed_hosts[0], request.get_full_path())
+
+        return HttpResponsePermanentRedirect(new_url)
