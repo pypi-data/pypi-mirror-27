e383c8a57c17234e64577fd10f3c9a8106fa839b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:08 +0000

    Add package odoo10-addon-medical-practitioner/10.0.1.2.0.99.dev2/odoo10_addon_medical_practitioner-10.0.1.2.0.99.dev2-py2-none-any.whl

---
+++ packages/odoo10-addon-medical-practitioner/odoo10_addon_medical_practitioner-10.0.1.2.0.99.dev2-py2-none-any.whl/odoo/addons/medical_practitioner/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import medical_practitioner
+from . import medical_role
+from . import medical_specialty
+from . import res_partner
