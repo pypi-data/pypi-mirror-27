985d62c1bb51246179831b621f42268faa00e0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:05 +0000

    Add package grr/0.7.1/grr-0.7.1.tar.gz

---
+++ packages/grr/grr-0.7.1.tar.gz/grr-0.7.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='grr',
+    version='0.7.1',
+    author='Kunal Mehta',
+    author_email='legoktm@gmail.com',
+    url='https://github.com/legoktm/grr/',
+    license='GPL-3.0+',
+    description='A command-line utility to work with Gerrit',
+    long_description=open('README.rst').read(),
+    packages=['grr'],
+    install_requires=[],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    entry_points={
+        'console_scripts': [
+            'grr = grr:main'
+        ],
+    },
+    include_package_data=True,
+)
