19ea6f4ffd7caa4e368b03388b9177b05471252b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:43 +0000

    Add package checkspot/1.0.0/checkspot-1.0.0.tar.gz

---
+++ packages/checkspot/checkspot-1.0.0.tar.gz/checkspot-1.0.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+
+import re
+from setuptools import setup
+
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('checkspot/checkspot.py').read(),
+    re.M
+    ).group(1)
+
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+
+setup(
+    name = "checkspot",
+    packages = ["checkspot"],
+    entry_points = {
+        "console_scripts": ['checkspot = checkspot.checkspot:main']
+        },
+    version = version,
+    description = "A simple CLI tool to check the spot prices of AWS instances. ",
+    long_description = long_descr,
+    author = "Joey Sham",
+    author_email = "sham.joey@gmail.com",
+    url = "http://www.joeyism.com",
+    )
