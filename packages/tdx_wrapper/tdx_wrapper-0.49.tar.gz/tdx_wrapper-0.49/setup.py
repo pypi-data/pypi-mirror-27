c08c038f82471b271112d5964778d0c07d654e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:06 +0000

    Add package tdx_wrapper/0.49/tdx_wrapper-0.49.tar.gz

---
+++ packages/tdx_wrapper/tdx_wrapper-0.49.tar.gz/tdx_wrapper-0.49/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+
+setup(
+    name='tdx_wrapper',
+    version='0.49',
+    description='A Python wrapper of pytdx',
+    long_description=long_description,
+    author='Jie Wang',
+    author_email='790930856@qq.com',
+    url='https://github.com/JaysonAlbert/tdx',
+    packages=find_packages(),
+    install_requires=[
+        'pytdx',
+        'toolz'
+    ],
+
+    package_data = {'tdx': ['data/*','data/blocknew/*']},
+
+)
