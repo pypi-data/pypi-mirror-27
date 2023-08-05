6b77da7cb28f116bdc0b303ac45a3fe8fb2b3d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:52 +0000

    Add package specguiutils/0.6.1/specguiutils-0.6.1.tar.gz

---
+++ packages/specguiutils/specguiutils-0.6.1.tar.gz/specguiutils-0.6.1/specguiutils/test/testsuite.py
@@ -0,0 +1,21 @@
+'''
+Created on Aug 21, 2017
+
+@author: hammonds
+'''
+import unittest
+from specguiutils.test.testScanBrowser import TestScanBrowserTest
+from specguiutils.test.testCounterSelectorModel import CounterSelectorTableModelTest
+from specguiutils.test import testCounterSelectorView
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestScanBrowserTest())
+    suite.addTest(CounterSelectorTableModelTest())
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    test_suite = suite()
+    runner.run(test_suite)
