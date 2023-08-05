1ff909a7f6fe640fe4d95a40409e67438156277d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package django-logging-json/1.15/django-logging-json-1.15.tar.gz

---
+++ packages/django-logging-json/django-logging-json-1.15.tar.gz/django-logging-json-1.15/setup.py
@@ -0,0 +1,56 @@
+import os
+import re
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+        return readme.read()
+
+
+def version():
+    pattern = re.compile(r'__version__ = \'([\d\.]+)\'')
+    with open(os.path.join('django_logging', '__init__.py')) as f:
+        data = f.read()
+        return re.search(pattern, data).group(1)
+
+
+setup(
+    name='django-logging-json',
+    version=version(),
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD',
+    description='A simple Django app to log requests/responses in various formats, such as JSON.',
+    long_description=readme(),
+    url='https://github.com/cipriantarta/django-logging',
+    author='Ciprian Tarta',
+    author_email='me@cipriantarta.ro',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Framework :: Django",
+        "Framework :: Django :: 1.4",
+        "Framework :: Django :: 1.5",
+        "Framework :: Django :: 1.6",
+        "Framework :: Django :: 1.7",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.10",
+    ],
+    keywords='django logging json',
+    install_requires=[
+        'django>=1.4',
+        'six',
+        'elasticsearch>=2.0.0',
+        'certifi'
+    ]
+)
