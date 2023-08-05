ce4312ebd511468d77b40473d3400605f26cdf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:27 +0000

    Add package pypeman/0.3.1/pypeman-0.3.1.tar.gz

---
+++ packages/pypeman/pypeman-0.3.1.tar.gz/pypeman-0.3.1/pypeman/endpoints.py
@@ -0,0 +1,20 @@
+import asyncio
+
+all = []
+
+class BaseEndpoint:
+
+    def __init__(self):
+        all.append(self)
+
+    async def start(self):
+        pass
+
+from pypeman.helpers import lazyload
+
+wrap = lazyload.Wrapper(__name__)
+
+wrap.add_lazy('pypeman.contrib.http', 'HTTPEndpoint', ['aiohttp'])
+wrap.add_lazy('pypeman.contrib.hl7', 'MLLPEndpoint', ['hl7'])
+
+
