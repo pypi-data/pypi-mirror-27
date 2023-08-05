bbeaf3479a8b5893ba55c3dde7829ebf917096bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:50 +0000

    Add package windirs/1.1.1/windirs-1.1.1.tar.gz

---
+++ packages/windirs/windirs-1.1.1.tar.gz/windirs-1.1.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name='windirs',
+      version='1.1.1',
+      description='Windows path wrapper',
+      author='Isaac Robinson',
+      author_email='isaac@rirobinson.com',
+      packages=find_packages(),
+      url=''
+      )
