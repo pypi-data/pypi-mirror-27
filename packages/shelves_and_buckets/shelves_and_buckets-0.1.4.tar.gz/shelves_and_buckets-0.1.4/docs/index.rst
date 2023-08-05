dbaa8a8cc348f46b370677c6bd25e67f9218798a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:18 +0000

    Add package shelves_and_buckets/0.1.4/shelves_and_buckets-0.1.4.tar.gz

---
+++ packages/shelves_and_buckets/shelves_and_buckets-0.1.4.tar.gz/shelves_and_buckets-0.1.4/docs/index.rst
@@ -0,0 +1,22 @@
+Welcome to shelves-and-buckets's documentation!
+======================================
+
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   readme
+   installation
+   modules
+   contributing
+   authors
+   history
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/shelves_and_buckets/shelves_and_buckets-0.1.4.tar.gz/shelves_and_buckets-0.1.4/shelves_and_buckets/__init__.py
@@ -0,0 +1,13 @@
+"""Top-level package for shelves-and-buckets."""
+
+from .shelves_and_buckets import AbstractShelf, IntervalShelf, IntIntervalShelf, NamedShelf, FloatIntervalShelf
+
+
+__author__ = """Ilian Stefkov Iliev"""
+__email__ = 'ilian@ilian.io'
+__version__ = '0.1.4'
+
+
+__all__ = [
+    'AbstractShelf', 'IntervalShelf', 'IntIntervalShelf', 'NamedShelf', 'FloatIntervalShelf'
+]
