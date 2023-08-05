8ea1b7ba27f56444bca7fab85ac25c588a915e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:35 +0000

    Add package sctools/0.1.5/sctools-0.1.5.tar.gz

---
+++ packages/sctools/sctools-0.1.5.tar.gz/sctools-0.1.5/src/sctools/platform.py
@@ -0,0 +1,135 @@
+from . import fastq, bam
+import argparse
+
+
+class GenericPlatform:
+    """Abstract base class for platform specific metadata"""
+
+    @classmethod
+    def attach_barcodes(cls, args=None):
+        """command line entrypoint for attaching barcodes to a bamfile."""
+        raise NotImplementedError
+
+    @classmethod
+    def split_bam(cls, args=None):
+        """command line entrypoint for splitting a bamfile into subfiles of equal size.
+
+        prints filenames of chunks to stdout
+
+        :param list args: optional arguments list, for testing (see test/test_entrypoints.py for
+          example).
+        :return int:
+        """
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '-b', '--bamfile', required=True,
+            help='input bamfile')
+        parser.add_argument(
+            '-p', '--output-prefix', required=True,
+            help='prefix for output chunks')
+        parser.add_argument(
+            '-s', '--subfile-size', required=False, default=1000, type=float,
+            help='approximate size target for each subfile (in MB)')
+        parser.add_argument('-t', '--tag', required=True, help='tag to split bamfile over')
+        parser.set_defaults(raise_missing=True)
+        parser.add_argument('--drop-missing', action='store_false',
+                            help='drop records without tag specified by -t/--tag (default '
+                                 'behavior is to raise an exception')
+        if args is not None:
+            args = parser.parse_args(args)
+        else:
+            args = parser.parse_args()
+
+        filenames = bam.split(
+            args.bamfile, args.output_prefix, args.tag, args.subfile_size, args.raise_missing)
+
+        print(' '.join(filenames))
+        return 0
+
+
+class TenXV2(GenericPlatform):
+
+    # 10x contains three barcodes embedded within sequencing reads. The below objects define the
+    # start and end points of those barcodes relative to the start of the sequence, and the
+    # GA4GH standard tags that the extracted barcodes should be labeled with in the BAM file.
+    cell_barcode = fastq.EmbeddedBarcode(start=0, end=16, quality_tag='CY', sequence_tag='CR')
+    molecule_barcode = fastq.EmbeddedBarcode(start=16, end=26, quality_tag='UY', sequence_tag='UR')
+    sample_barcode = fastq.EmbeddedBarcode(start=0, end=8, quality_tag='SY', sequence_tag='SR')
+
+    @classmethod
+    def tag_bamfile(cls, input_bamfile_name, output_bamfile_name, tag_generators):
+        """Extracts tags from fastq files_with_tags, attaches them to input_bamfile_name and writes
+        the result to output_bamfile_name
+
+        :param str input_bamfile_name: input bam
+        :param str output_bamfile_name: output bam
+        :param list tag_generators: list of generators of tags
+        """
+        bam_tagger = bam.Tagger(input_bamfile_name)
+        bam_tagger.tag(output_bamfile_name, tag_generators)
+
+    @classmethod
+    def make_tag_generators(cls, r1, i1, whitelist=None):
+        """Create tag generators from fastq files.
+
+        Tag generators are iterators that run over fastq records, they extract and yield all of the
+        barcodes embedded in each fastq record. For 10x, this means extracting the cell, umi, and
+        sample barcode.
+
+        :param str r1: fastq file, read 1
+        :param str i1: fastq file, read 2
+        :param str whitelist: file containing barcodes -- one barcode perline.
+        :return [Generator]: a list of embedded barcodes generators associated with each fastq
+          record
+        """
+        if whitelist is not None:
+            return [
+                fastq.BarcodeGeneratorWithCorrectedCellBarcodes(
+                    fastq_files=r1,
+                    embedded_cell_barcode=cls.cell_barcode,
+                    whitelist=whitelist,
+                    other_embedded_barcodes=[cls.molecule_barcode],
+                ),
+                fastq.EmbeddedBarcodeGenerator(
+                    fastq_files=i1, embedded_barcodes=[cls.sample_barcode])
+            ]
+        else:
+            return [
+                fastq.EmbeddedBarcodeGenerator(
+                    fastq_files=r1, embedded_barcodes=[cls.cell_barcode, cls.molecule_barcode]),
+                fastq.EmbeddedBarcodeGenerator(
+                    fastq_files=i1, embedded_barcodes=[cls.sample_barcode])
+            ]
+
+    @classmethod
+    def attach_barcodes(cls, args=None):
+        """command line entrypoint for attaching barcodes to a bamfile.
+        :param list args: optional arguments list, for testing (see test/test_entrypoints.py for
+          example).
+        :return int:
+        """
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '--r1', required=True,
+            help='read 1 fastq record for a 10x genomics experiment')
+        parser.add_argument(
+            '--i1', required=True, help='i7 fastq record for a 10x genomics experiment')
+        parser.add_argument(
+            '--u2', required=True,
+            help='unaligned read-2 bam containing genomic information. Can be converted'
+                 'using picard FastqToSam')
+        parser.add_argument('-o', '--output-bamfile', required=True,
+                            help='filename for tagged bam')
+        parser.add_argument('-w', '--whitelist', default=None,
+                            help='optional cell barcode whitelist. If provided, corrected barcodes '
+                                 'will also be output when barcodes are observed within 1ED of a '
+                                 'whitelisted barcode')
+        if args is not None:
+            args = parser.parse_args(args)
+        else:
+            args = parser.parse_args()
+
+        tag_generators = cls.make_tag_generators(args.r1, args.i1, args.whitelist)
+        cls.tag_bamfile(args.u2, args.output_bamfile, tag_generators)
+
+        return 0
---
+++ packages/sctools/sctools-0.1.5.tar.gz/sctools-0.1.5/src/sctools/test/test_bam.py
@@ -0,0 +1,120 @@
+import os
+import pytest
+import glob
+from .. import bam, platform
+
+
+data_dir = os.path.split(__file__)[0] + '/data/'
+
+
+# TEST SUBSETALIGNMENTS
+
+@pytest.fixture(scope='module', params=[data_dir + 'test.sam', data_dir + 'test.bam'])
+def sa_object(request):
+    """fixture returns SubsetAlignments objects for testing"""
+    return bam.SubsetAlignments(request.param)
+
+
+@pytest.fixture(scope='module', params=[
+    (data_dir + 'test.sam', 20, 20),
+    (data_dir + 'test.bam', 20, 20)])
+def indices(request):
+    """fixture returns indices from a SubsetAlignments objects for testing"""
+    sa = bam.SubsetAlignments(request.param[0])
+    ind_specific, ind_nonspecific = sa.indices_by_chromosome(
+        request.param[1], '19', include_other=request.param[2])
+    return ind_specific, ind_nonspecific
+
+
+@pytest.fixture(scope='session')
+def n_nonspecific():
+    """the number of non-specific records to extract"""
+    return 20
+
+
+@pytest.fixture(scope='session')
+def n_specific():
+    """the number of specific records to extract"""
+    return 20
+
+
+def test_incorrect_extension_does_not_raise_when_open_mode_is_specified():
+    sa = bam.SubsetAlignments(data_dir + '/is_actually_a_samfile.wrong_extensions', 'r')
+    assert isinstance(sa, bam.SubsetAlignments)
+
+
+def test_incorrect_extension_without_open_mode_raises_value_error():
+    with pytest.raises(ValueError):
+        _ = bam.SubsetAlignments(data_dir + '/is_actually_a_samfile.wrong_extensions')
+
+
+def test_str_and_int_chromosomes_both_function(sa_object):
+    _ = sa_object.indices_by_chromosome(10, '19', include_other=10)
+    _ = sa_object.indices_by_chromosome(10, 19, include_other=10)
+
+
+def test_correct_number_of_indices_are_extracted(sa_object, n_specific, n_nonspecific):
+    ind_specific, ind_nonspecific = sa_object.indices_by_chromosome(
+        n_specific, '19', include_other=n_nonspecific)
+    assert len(ind_specific) == n_specific
+    assert len(ind_nonspecific) == n_nonspecific
+
+
+def test_indices_are_all_greater_than_zero(sa_object, n_specific, n_nonspecific):
+    ind_specific, ind_nonspecific = sa_object.indices_by_chromosome(
+        n_specific, '19', include_other=n_nonspecific)
+    assert min(ind_nonspecific + ind_nonspecific) > 0
+
+
+def test_chromosome_19_comes_before_21(indices):
+    """chromosome 19 comes before 21 in the test file, this should be replicated in the output"""
+    assert max(indices[0]) < min(indices[1])
+
+
+# TAGGER TESTED IN INTEGRATION TESTS ONLY (see test_entrypoints.py)
+
+# TEST SPLIT
+
+@pytest.fixture(scope='module', params=[data_dir + 'test.sam', data_dir + 'test.bam'])
+def bamfile(request):
+    return request.param
+
+
+def test_split_bam_raises_value_error_when_passed_bam_without_barcodes(bamfile):
+    split_size = 0.02  # our test data is very small, 0.01mb = ~10kb, which should yield 5 files.
+    with pytest.raises(RuntimeError):
+        bam.split(bamfile, 'test_output', 'CB', split_size)
+
+
+@pytest.fixture
+def tagged_bam():
+    args = [
+        '--r1', data_dir + 'test_r1.fastq',
+        '--i1', data_dir + 'test_i7.fastq',
+        '--u2', data_dir + 'test_r2.bam',
+        '--output-bamfile', 'test_tagged_bam.bam',
+        '--whitelist', data_dir + '1k-august-2016.txt']
+    platform.TenXV2.attach_barcodes(args)
+    return 'test_tagged_bam.bam'
+
+
+def test_split_on_tagged_bam(tagged_bam):
+    split_size = 0.005  # our test data is very small, this value should yield 3 files
+    outputs = bam.split(tagged_bam, 'test_output', 'CB', split_size)
+    assert len(outputs) == 3
+
+    # cleanup
+    os.remove(tagged_bam)  # clean up
+    for f in glob.glob('test_output_*'):
+        os.remove(f)
+
+
+def test_split_with_large_chunk_size_generates_one_file(tagged_bam):
+    split_size = 1024  # our test data is very small, this value should yield 1 file
+    outputs = bam.split(tagged_bam, 'test_output', 'CB', split_size)
+    assert len(outputs) == 1
+
+    # cleanup
+    os.remove(tagged_bam)  # clean up
+    for f in glob.glob('test_output_*'):
+        os.remove(f)
