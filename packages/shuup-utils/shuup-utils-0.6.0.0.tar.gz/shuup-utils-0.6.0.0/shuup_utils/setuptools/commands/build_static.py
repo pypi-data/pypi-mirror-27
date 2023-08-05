9bb9c9467574c7eefec4eba8069b4cba758d35bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:07 +0000

    Add package shuup-utils/0.6.0.0/shuup-utils-0.6.0.0.tar.gz

---
+++ packages/shuup-utils/shuup-utils-0.6.0.0.tar.gz/shuup-utils-0.6.0.0/shuup_utils/setuptools/commands/build_static.py
@@ -0,0 +1,37 @@
+import os
+import importlib
+import subprocess
+
+from setuptools import Command
+
+
+# noinspection PyAttributeOutsideInit
+class BuildStaticCommand(Command):
+    description = 'Runs `yarn install` and `yarn run compile` for the static files.'
+    user_options = [(
+        'yarn-dir-path=',
+        None,
+        'The directory containing yarn, relative to the project root.',
+    )]
+
+    def initialize_options(self):
+        self.yarn_dir_path = get_default_yarn_dir_path(command=self)
+    
+    def run(self):
+        build_static(self.yarn_dir_path)
+
+
+def build_static(yarn_dir_path: str = None):
+    os.chdir(yarn_dir_path)
+    subprocess.run(['yarn', 'install', '--pure-lockfile'], check=True)
+    subprocess.run(['yarn', 'run', 'compile'], check=True)
+
+
+def get_default_yarn_dir_path(command: Command) -> str:
+    package_name = command.distribution.packages[0] # type: str
+    package_module = importlib.import_module(package_name) # type: object
+    package_path = os.path.dirname(package_module.__file__) # type: str
+    return '{package_path}/static/{app_name}/'.format(
+        package_path=package_path,
+        app_name=package_name,
+    )
