0430640fe6def982b7aedcc47acc0516ee480675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package abilian-sbe/0.3.19/abilian-sbe-0.3.19.tar.gz

---
+++ packages/abilian-sbe/abilian-sbe-0.3.19.tar.gz/abilian-sbe-0.3.19/abilian/sbe/apps/documents/views/views.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Document management blueprint.
+"""
+from __future__ import absolute_import, print_function
+
+from flask import g
+
+from abilian.i18n import _l
+from abilian.sbe.apps.communities.blueprint import Blueprint
+from abilian.web.action import Endpoint
+from abilian.web.nav import BreadcrumbItem
+
+from ..actions import register_actions
+
+__all__ = ['blueprint']
+
+blueprint = Blueprint(
+    "documents", __name__, url_prefix="/docs", template_folder="../templates")
+route = blueprint.route
+blueprint.record_once(register_actions)
+
+
+@blueprint.url_value_preprocessor
+def init_document_values(endpoint, values):
+    g.current_tab = 'documents'
+
+    g.breadcrumb.append(
+        BreadcrumbItem(
+            label=_l(u'Documents'),
+            url=Endpoint('documents.index', community_id=g.community.slug)))
