79afadd09a6dd196a677212863d8f5982cfe4506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:59 +0000

    Add package build_score_logit_knn/0.2/build_score_logit_knn-0.2.tar.gz

---
+++ packages/build_score_logit_knn/build_score_logit_knn-0.2.tar.gz/build_score_logit_knn-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='build_score_logit_knn',    
+    version='0.2',	
+	author = 'Vignesh Ravichandran',
+	author_email = 'vickykct@gmail.com',
+    scripts=['build_score_logit_knn']         
+)
+
