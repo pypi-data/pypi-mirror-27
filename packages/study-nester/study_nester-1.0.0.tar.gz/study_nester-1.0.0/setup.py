65e910d746c62aae484e94cf2905e0ed2d87dbc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:11 +0000

    Add package study-nester/1.0.0/study_nester-1.0.0.tar.gz

---
+++ packages/study-nester/study_nester-1.0.0.tar.gz/study_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         = 'study_nester',
+    version      = '1.0.0',
+    py_modules   = ['study_nester'],
+    author       = 'akdada',
+    author_email = '2104349964@qq.com',
+    description  = 'A simple printer of nested lists',
+    )
