83564f94a2c10d3d3323dea0b46024be31522ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:23 +0000

    Add package cryptocurrency/1.0/cryptocurrency-1.0.tar.gz

---
+++ packages/cryptocurrency/cryptocurrency-1.0.tar.gz/cryptocurrency-1.0/README.md
@@ -0,0 +1,14 @@
+## Install / Setup
+
+```shell
+pip install cryptocurrency
+```
+
+## Usage
+```python
+import cryptocurrency
+
+cryptocurrency.get_price("btc", "usd")
+
+>>> 9881.26429271
+```
---
+++ packages/cryptocurrency/cryptocurrency-1.0.tar.gz/cryptocurrency-1.0/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+
+setup(
+     name='cryptocurrency',
+     version='1.0',
+     license='MIT',
+     keywords='cryptocurrency bitcoin ethereum litecoin crypto',
+     author='Jake Meyer',
+     author_email='jakewmeyer@gmail.com',
+     url='https://github.com/jakewmeyer/Crypto'
+     )
+     
