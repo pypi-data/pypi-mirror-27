bcef943d78b6a70c90882855f87fc860d7e0a335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:02 +0000

    Add package django-modern-rpc/0.10.0/django_modern_rpc-0.10.0-py2.py3-none-any.whl

---
+++ packages/django-modern-rpc/django_modern_rpc-0.10.0-py2.py3-none-any.whl/modernrpc/conf/default_settings.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+# List of python modules containing RPC methods.
+MODERNRPC_METHODS_MODULES = []
+
+# Decoder class used to convert python data to JSON
+MODERNRPC_JSON_DECODER = 'json.decoder.JSONDecoder'
+# Encoder class used to convert JSON data to python values
+MODERNRPC_JSON_ENCODER = 'django.core.serializers.json.DjangoJSONEncoder'
+
+
+# Control how builtin types are handled by XML-RPC serializer and deserializer
+MODERNRPC_XMLRPC_USE_BUILTIN_TYPES = True
+# Control how XML-RPC serializer will handle None values. If set to True, None values will be converted to <nil>
+MODERNRPC_XMLRPC_ALLOW_NONE = True
+# Configure the default encoding used by XML encoder
+MODERNRPC_XMLRPC_DEFAULT_ENCODING = None
+
+# List of handler classes used by default in any ``RPCEntryPoint`` instance
+MODERNRPC_HANDLERS = [
+    'modernrpc.handlers.JSONRPCHandler',
+    'modernrpc.handlers.XMLRPCHandler',
+]
+
+# Default name associated with anonymous ``RPCEntryPoint``
+MODERNRPC_DEFAULT_ENTRYPOINT_NAME = '__default_entry_point__'
+
+# Configure the format of the docstring used to document your RPC methods.
+# Possible values are: '', 'rst' or 'md'
+MODERNRPC_DOC_FORMAT = ''
+
+MODERNRPC_PY2_STR_TYPE = None
+MODERNRPC_PY2_STR_ENCODING = 'UTF-8'
+
+# Set to False if you need to disable exception logging
+MODERNRPC_LOG_EXCEPTIONS = True
