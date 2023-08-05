f714638bba1410c05c8ad7099dd88fc6a37133d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:17 +0000

    Add package PyScaffold/3.0b2/PyScaffold-3.0b2-py2.py3-none-any.whl

---
+++ packages/PyScaffold/PyScaffold-3.0b2-py2.py3-none-any.whl/pyscaffold/extensions/tox.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Extension that generates configuration files for the Tox test automation tool.
+"""
+from __future__ import absolute_import
+
+from ..templates import tox as tox_ini
+
+
+def augment_cli(parser):
+    """Add an option to parser that enables the Travis extension.
+
+    Args:
+        parser (argparse.ArgumentParser): CLI parser object
+    """
+
+    parser.add_argument(
+        "--tox",
+        dest="extensions",
+        action="append_const",
+        const=extend_project,
+        help="generate Tox configuration file")
+
+
+def extend_project(actions, helpers):
+    """Register an action responsible for adding tox files to project."""
+
+    def add_files(struct, opts):
+        """Add Tox specific files to the project structure."""
+
+        files = {
+            'tox.ini': (tox_ini(opts), helpers.NO_OVERWRITE)
+        }
+
+        return helpers.merge(struct, {opts['project']: files}), opts
+
+    return helpers.register(actions, add_files)
---
+++ packages/PyScaffold/PyScaffold-3.0b2-py2.py3-none-any.whl/pyscaffold/extensions/travis.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Extension that generates configuration and script files for Travis CI.
+"""
+from __future__ import absolute_import
+
+from ..templates import travis, travis_install
+
+
+def augment_cli(parser):
+    """Add an option to parser that enables the Travis extension.
+
+    Args:
+        parser (argparse.ArgumentParser): CLI parser object
+    """
+
+    parser.add_argument(
+        "--travis",
+        dest="extensions",
+        action="append_const",
+        const=extend_project,
+        help="generate Travis configuration files")
+
+
+def extend_project(actions, helpers):
+    """Register an action responsible for adding travis files to project."""
+
+    def add_files(struct, opts):
+        """Add Travis specific files to the project structure."""
+
+        files = {
+            '.travis.yml': (travis(opts), helpers.NO_OVERWRITE),
+            'tests': {
+                'travis_install.sh': (travis_install(opts),
+                                      helpers.NO_OVERWRITE)
+            }
+        }
+
+        return helpers.merge(struct, {opts['project']: files}), opts
+
+    return helpers.register(actions, add_files)
