5a9a5f311bfef4fea5f23f6bd1b74bef59f40f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:19 +0000

    Add package Hello-World-Package/0.1.3/Hello-World-Package-0.1.3.tar.gz

---
+++ packages/Hello-World-Package/Hello-World-Package-0.1.3.tar.gz/Hello-World-Package-0.1.3/README.rst
@@ -0,0 +1,32 @@
+===============================
+Hello World Package
+===============================
+
+|travis status|
+|pypi status|
+|docs status|
+|coverage status|
+
+Hello World test package
+
+* Free software: MIT license
+* Documentation: (COMING SOON!) https://hello-world-package-example.readthedocs.org.
+
+Features
+--------
+
+* TODO
+
+
+.. |travis status| image:: https://travis-ci.org/grantwilliams/hello-world-package-example.svg?branch=master
+    :target: https://travis-ci.org/grantwilliams/hello-world-package-example
+
+.. |pypi status| image:: https://img.shields.io/pypi/v/Hello-World-Package.svg
+        :target: https://pypi.python.org/pypi/Hello-World-Package
+
+.. |docs status| image:: https://readthedocs.org/projects/hello-world-package-example/badge/?version=latest
+        :target: http://hello-world-package-example.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. |coverage status| image:: https://coveralls.io/repos/github/grantwilliams/hello-world-package-example/badge.svg?branch=master
+        :target: https://coveralls.io/github/grantwilliams/hello-world-package-example?branch=master
---
+++ packages/Hello-World-Package/Hello-World-Package-0.1.3.tar.gz/Hello-World-Package-0.1.3/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+import versioneer
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+with open(path.join(here, 'HISTORY.rst'), encoding='utf-8') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    # TODO: put package requirements here
+    'click',
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='Hello-World-Package',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Hello World test package",
+    long_description=readme + '\n\n' + history,
+    author="Grant Williams",
+    author_email='grant.williams2986@gmail.com',
+    url='https://github.com/granwilliams/hello-world',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    entry_points={
+        'console_scripts':[
+            'hello-world=hello_world.cli:say_hello',
+            ],
+        },
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+)
