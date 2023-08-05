7326dab09b0acb2d5fc67acc612ec31b836a53da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:40 +0000

    Add package cloudtoken-plugin.adfs/0.1.3/cloudtoken-plugin.adfs-0.1.3.tar.gz

---
+++ packages/cloudtoken-plugin.adfs/cloudtoken-plugin.adfs-0.1.3.tar.gz/cloudtoken-plugin.adfs-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import os
+
+version = os.environ['VERSION']
+
+setup(
+    name='cloudtoken-plugin.adfs',
+    version=version,
+    description='Microsoft ADFS plugin for cloudtoken.',
+    url='https://bitbucket.org/atlassian/cloudtoken',
+    author='Atlassian Cloud Engineering',
+    author_email='cloud-team@atlassian.com',
+    license='Apache',
+    py_modules=['cloudtoken_plugin.adfs'],
+    zip_safe=False,
+    install_requires=[
+        "cloudtoken>=0.1.0",
+        ],
+)
