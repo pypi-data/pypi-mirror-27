a2afb78f1e5a4610edc935a778b4dde8295ccd06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:32 +0000

    Add package pygmsh/4.0.5/pygmsh-4.0.5-py2.py3-none-any.whl

---
+++ packages/pygmsh/pygmsh-4.0.5-py2.py3-none-any.whl/pygmsh/__about__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+
+__version__ = '4.0.5'
+__author__ = u'Nico Schlömer'
+__author_email__ = 'nico.schloemer@gmail.com'
+__website__ = 'https://github.com/nschloe/pygmsh'
+__license__ = 'License :: OSI Approved :: MIT License'
+__status__ = 'Development Status :: 5 - Production/Stable'
