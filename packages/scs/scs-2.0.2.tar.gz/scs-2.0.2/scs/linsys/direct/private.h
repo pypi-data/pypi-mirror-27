ec4ebf4eb73b0fbbafd9c3cd39841f29e15c5434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:12 +0000

    Add package scs/2.0.2/scs-2.0.2.tar.gz

---
+++ packages/scs/scs-2.0.2.tar.gz/scs-2.0.2/scs/linsys/direct/private.h
@@ -0,0 +1,27 @@
+#ifndef PRIV_H_GUARD
+#define PRIV_H_GUARD
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "glbopts.h"
+#include "scs.h"
+#include "cs.h"
+#include "external/amd.h"
+#include "external/ldl.h"
+#include "common.h"
+
+struct SCS_LIN_SYS_WORK {
+  cs *L;         /* KKT, and factorization matrix L resp. */
+  scs_float *D;  /* diagonal matrix of factorization */
+  scs_int *P;    /* permutation of KKT matrix for factorization */
+  scs_float *bp; /* workspace memory for solves */
+  /* reporting */
+  scs_float total_solve_time;
+};
+
+#ifdef __cplusplus
+}
+#endif
+#endif
