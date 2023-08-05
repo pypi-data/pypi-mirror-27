32908315f22da28e52f6f3abb994d5e7bbbf868c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:55 +0000

    Add package requests-facebook/0.4.2/requests-facebook-0.4.2.tar.gz

---
+++ packages/requests-facebook/requests-facebook-0.4.2.tar.gz/requests-facebook-0.4.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setuptools
+
+setup(
+    name='requests-facebook',
+    version='0.4.2',
+    install_requires=['requests>=2.18.4'],
+    author='Mike Helmick',
+    author_email='me@michaelhelmick.com',
+    license='BSD',
+    url='https://github.com/michaelhelmick/requests-facebook/',
+    keywords='python facebook requests graph oauth oauth2 api',
+    description='A Python Library to interface with Facebook Graph API',
+    long_description=open('README.rst').read(),
+    download_url='https://github.com/michaelhelmick/requests-facebook/zipball/master',
+    py_modules=['facebook'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
