6432434ba89ffe720638cfb020d5c37b5fabcef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:19 +0000

    Add package django-framework/0.1.39/django_framework-0.1.39.tar.gz

---
+++ packages/django-framework/django_framework-0.1.39.tar.gz/django_framework-0.1.39/django_framework/django_helpers/settings_helper/variables/__init__.py
@@ -0,0 +1,17 @@
+from project_name import ProjectName
+from secret_key import SecretKey
+
+from debug_configuration import DebugConfiguration
+
+from triple_des_token import TripleDESToken
+
+from redis_configuration import RedisConfiguration
+from kafka_configuration import KafkaConfiguration
+
+from job_configuration import JobConfiguration
+
+from authentication_configuration import AuthenticationConfiguration
+
+from database_configuration import DatabaseConfiguration
+
+from encryption_configuration import EncryptionConfiguration
