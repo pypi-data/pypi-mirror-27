7f8497a1ad4751969ce1827254aed5e719374c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:17 +0000

    Add package Clarissa/1.1.1/Clarissa-1.1.1.tar.gz

---
+++ packages/Clarissa/Clarissa-1.1.1.tar.gz/Clarissa-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name='Clarissa',
+  packages=['ClarissaPackages', 'ClarissaPackages3'],
+  version='1.1.1',
+  description='Talk to me, Clarissa Campbell, your favorite bot',
+  author='Indie Dev',
+  author_email='bremo.lincolin101@gmail.com',
+  url='https://github.com/Clarissa-Bot/Setup/archive/master.zip',
+  keywords=['Clarissa', 'Python bot', 'tree person'],
+  classifiers = [],
+  )
