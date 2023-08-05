f636b128367b551350d2fff52e23a335ff228cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:38 +0000

    Add package mw-api-client/2.0.0/mw-api-client-2.0.0.tar.gz

---
+++ packages/mw-api-client/mw-api-client-2.0.0.tar.gz/mw-api-client-2.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+    longdesc = f.read()
+
+setup(
+    name="mw-api-client",
+    version="2.0.0",
+    description="A simple MediaWiki client.",
+    long_description=longdesc,
+    url="https://github.com/Kenny2github/mw-api-client",
+    author="Ken Hilton",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Wiki',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='mediawiki api requests',
+    py_modules=['mw_api_client'],
+    install_requires='requests',
+    python_requires='>=2.7',
+)
