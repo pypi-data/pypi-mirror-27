6f9c54d3ebee7be6eb6df01d5e42aa762e3eadf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:21 +0000

    Add package gitviewerrx/0.1.2/gitviewerrx-0.1.2.tar.gz

---
+++ packages/gitviewerrx/gitviewerrx-0.1.2.tar.gz/gitviewerrx-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gitviewerrx',
+      version='0.1.2',
+      description='view git commit history.',
+      author='Rui Xu',
+      author_email='hippolyte_xu@hotmail.com',
+      license='MIT',
+      packages=['gitviewerrx'],
+      zip_safe=False)
