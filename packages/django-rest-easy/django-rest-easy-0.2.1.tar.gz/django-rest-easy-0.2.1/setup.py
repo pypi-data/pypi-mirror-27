0d73fde247b2af3c36411630bb8aa6f334fd89f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:53 +0000

    Add package django-rest-easy/0.2.1/django-rest-easy-0.2.1.tar.gz

---
+++ packages/django-rest-easy/django-rest-easy-0.2.1.tar.gz/django-rest-easy-0.2.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='django-rest-easy',
+    packages=['rest_easy'],
+    version='0.2.1',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    install_requires=[
+        'django>=1.8.0',
+        'djangorestframework>=3.0.0',
+        'setuptools>=36.0.1',
+        'six',
+    ],
+    description='django-rest-easy is an extension to DRF providing QOL improvements to serializers and views.',
+    long_description='django-rest-easy enables:\n'
+                     ' * versioning serializers by model and schema,\n'
+                     ' * creating views and viewsets using model and schema,\n'
+                     ' * serializer override for a particular DRF verb, like create or update,\n'
+                     ' * scoping views\' querysets and viewsets by url kwargs or request object parameters.',
+    author='SMARTPAGER SYSTEMS INC. / Krzysztof Bujniewicz',
+    author_email='racech@gmail.com',
+    url='https://github.com/TelmedIQ/django-rest-easy',
+    keywords=['django', 'DRF', 'rest framework', 'serializers', 'viewsets'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
