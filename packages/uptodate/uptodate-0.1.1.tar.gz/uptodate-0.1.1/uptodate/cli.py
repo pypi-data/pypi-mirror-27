23934f3c3d6781304641f563766ff01258d590b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:48 +0000

    Add package uptodate/0.1.1/uptodate-0.1.1.tar.gz

---
+++ packages/uptodate/uptodate-0.1.1.tar.gz/uptodate-0.1.1/uptodate/cli.py
@@ -0,0 +1,15 @@
+import click
+
+from uptodate.core import UpToDate
+
+
+@click.command()
+@click.argument('file_path', nargs=-1, type=click.Path(exists=True))
+def up_to_date(file_path):
+    """Scan requirements.txt file for dependences which are not up to date"""
+    uptodate = UpToDate(file_path[0])
+    uptodate.check()
+
+
+if __name__ == '__main__':
+    up_to_date()
