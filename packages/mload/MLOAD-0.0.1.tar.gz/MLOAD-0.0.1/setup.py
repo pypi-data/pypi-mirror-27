b76c97d375789ace6612134e0e60bfb9e7b3d2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:47 +0000

    Add package mload/0.0.1/MLOAD-0.0.1.tar.gz

---
+++ packages/mload/MLOAD-0.0.1.tar.gz/MLOAD-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MLOAD',
+      version='0.0.1',
+      description='Machine Learning for Outlier and Anomaly Detection.',
+      url='http://github.com/chaosimpler/',
+      author='Wang Chao',
+      author_email='chaosimpler@gmail.com',
+      license='MIT',
+      packages=['MLOAD'],
+      zip_safe=False)   
