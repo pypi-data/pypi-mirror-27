5da58ec8ed5a57a8e25adeaa9008c73ee3c47f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:48 +0000

    Add package sqlalchemy_json_api/0.4.6/SQLAlchemy-JSON-API-0.4.6.tar.gz

---
+++ packages/sqlalchemy_json_api/SQLAlchemy-JSON-API-0.4.6.tar.gz/SQLAlchemy-JSON-API-0.4.6/sqlalchemy_json_api/__init__.py
@@ -0,0 +1,11 @@
+from .exc import (  # noqa
+    IdPropertyNotFound,
+    InvalidField,
+    UnknownField,
+    UnknownFieldKey,
+    UnknownModel
+)
+from .query_builder import QueryBuilder, RESERVED_KEYWORDS  # noqa
+from .utils import assert_json_document  # noqa
+
+__version__ = '0.4.6'
