fd0f875e7ae0d6c6d2a9e0b3272ed0467d84ace1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:32 +0000

    Add package flask_json_content_type_validator/0.1.1/flask_json_content_type_validator-0.1.1.tar.gz

---
+++ packages/flask_json_content_type_validator/flask_json_content_type_validator-0.1.1.tar.gz/flask_json_content_type_validator-0.1.1/flask_json_content_type_validator/json_content_type_validator.py
@@ -0,0 +1,18 @@
+import flask
+import functools
+
+
+def validator(exception):
+    """Validates that the request has a Content-Type header with application/json
+    @param: exception A custom exception instance that will be raised if the validation fails
+    """
+    def _validator(f):
+        @functools.wraps(f)
+        def __validator(*args, **kwargs):
+            content_type = flask.request.headers.get('Content-Type',
+                                                     '').lower()
+            if 'application/json' not in content_type:
+                raise exception
+            return f(*args, **kwargs)
+        return __validator
+    return _validator
