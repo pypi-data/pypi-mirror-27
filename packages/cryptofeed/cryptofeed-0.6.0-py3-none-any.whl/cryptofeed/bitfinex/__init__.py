7be189a248d3742b09ea49fe262de5c82079e5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:19 +0000

    Add package cryptofeed/0.6.0/cryptofeed-0.6.0-py3-none-any.whl

---
+++ packages/cryptofeed/cryptofeed-0.6.0-py3-none-any.whl/cryptofeed/bitfinex/__init__.py
@@ -0,0 +1,8 @@
+'''
+Copyright (C) 2017-2018  Bryant Moscon - bmoscon@gmail.com
+
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
+from .bitfinex import Bitfinex
+from .pairs import bitfinex_trading_pairs
---
+++ packages/cryptofeed/cryptofeed-0.6.0-py3-none-any.whl/cryptofeed/feedhandler.py
@@ -0,0 +1,44 @@
+'''
+Copyright (C) 2017-2018  Bryant Moscon - bmoscon@gmail.com
+
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
+import asyncio
+
+import websockets
+
+from .nbbo import NBBO
+
+
+class FeedHandler(object):
+    def __init__(self):
+        self.feeds = []
+    
+    def add_feed(self, feed):
+        self.feeds.append(feed)
+
+    def add_nbbo(self, feeds, pairs, callback):
+        for pair in pairs:
+            cb = NBBO(callback, pair)
+            for feed in feeds:
+                self.add_feed(feed(channels=['ticker'], pairs=pairs, callbacks={'ticker': cb}))
+
+    def run(self):
+        if len(self.feeds) == 0:
+            raise ValueError("No feeds specified")
+        asyncio.get_event_loop().run_until_complete(self._run())
+
+    def _run(self):
+        loop = asyncio.get_event_loop()
+        feeds = [asyncio.ensure_future(self._connect(feed)) for feed in self.feeds]
+        done, pending = yield from asyncio.wait(feeds)
+    
+    async def _connect(self, feed):
+        async with websockets.connect(feed.address) as websocket:
+            await feed.subscribe(websocket)
+            await self._handler(websocket, feed.message_handler)
+
+    async def _handler(self, websocket, handler):
+        async for message in websocket:
+            await handler(message)
