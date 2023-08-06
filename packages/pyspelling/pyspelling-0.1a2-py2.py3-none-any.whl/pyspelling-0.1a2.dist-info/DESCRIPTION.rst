5dfa4f14edeb5f1254f198ab634b48ad12882d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:02 +0000

    Add package pyspelling/0.1a2/pyspelling-0.1a2-py2.py3-none-any.whl

---
+++ packages/pyspelling/pyspelling-0.1a2-py2.py3-none-any.whl/pyspelling-0.1a2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,16 @@
+
+PySpelling is a spell checker that wraps around Aspell.
+You can check out the list of available extensions and learn more about them by `reading the docs`_.
+
+.. _`reading the docs`: http://facelessuser.github.io/pyspelling/
+
+Support
+=======
+
+Help and support is available here at the repositories `bug tracker`_.
+Please read about `support and contributing`_ before creating issues.
+
+.. _`bug tracker`: https://github.com/facelessuser/pyspelling/issues
+.. _`support and contributing`: http://facelessuser.github.io/pyspelling/contributing/
+
+
---
+++ packages/pyspelling/pyspelling-0.1a2-py2.py3-none-any.whl/pyspelling/__main__.py
@@ -0,0 +1,48 @@
+"""Main."""
+from __future__ import unicode_literals
+import sys
+import argparse
+from pyspelling import settings
+from pyspelling import Aspell
+from pyspelling import __version__
+
+
+def run():
+    """Main."""
+
+    parser = argparse.ArgumentParser(prog='spellcheck', description='Spell checking tool.')
+    # Flag arguments
+    parser.add_argument('--version', action='version', version=('%(prog)s ' + __version__.version))
+    parser.add_argument('--verbose', '-v', action='count', default=0, help="Verbosity level.")
+    parser.add_argument('--name', '-n', action='store', default='', help="Specific spelling task by name to run.")
+    parser.add_argument('--config', '-c', action='store', default='.spelling.yml', help="Spelling config.")
+    args = parser.parse_args()
+
+    fail = False
+    config = settings.read_config(args.config)
+    spelling = Aspell(config, name=args.name, verbose=args.verbose)
+    for results in spelling.check():
+        if results.error:
+            fail = True
+            print('ERROR: Could not read %s -- %s' % (results.context, results.error))
+        elif results.words:
+            fail = True
+            print('Misspelled words:\n<%s> %s' % (results.category, results.context))
+            print('-' * 80)
+            for word in results.words:
+                print(word)
+            print('-' * 80)
+            print('')
+        elif args.verbose:
+            print('CHECK: %s' % results.context)
+    return fail
+
+
+def main():
+    """Main entry point."""
+
+    sys.exit(run())
+
+
+if __name__ == "__main__":
+    main()
