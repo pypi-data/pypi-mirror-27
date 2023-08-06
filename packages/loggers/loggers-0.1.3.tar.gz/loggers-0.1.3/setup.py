1ad52521daf30323a7faafaba9c49a9c4bb3f374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:30 +0000

    Add package loggers/0.1.3/loggers-0.1.3.tar.gz

---
+++ packages/loggers/loggers-0.1.3.tar.gz/loggers-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='loggers',
+    version='0.1.3',
+    author='jonDel',
+    author_email='jdellagostin@gmail.com',
+    url='https://github.com/jonDel/loggers',
+    packages=['loggers'],
+    license='GPLv3',
+    classifiers=[
+     'Development Status :: 3 - Alpha',
+     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+     'Programming Language :: Python :: 2.6',
+     'Programming Language :: Python :: 2.7',
+     'Topic :: System :: Logging',
+    ],
+    description='Usefull wrapper methods for logging native package',
+    long_description=open('README.rst').read(),
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+)
