3d5212023b7595beb330bb7da801dbd088ed0810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:47 +0000

    Add package testPy/0.3/testPy-0.3.tar.gz

---
+++ packages/testPy/testPy-0.3.tar.gz/testPy-0.3/setup.py
@@ -0,0 +1,15 @@
+__author__ = 'tangxuewei'
+__date__ = '2017/12/28'
+
+
+from setuptools import setup
+
+setup(name='testPy',
+      version=0.3,
+      description='this a test for testPy',
+      url='http://www.wecash.net',
+      author='Joshua Tang',
+      author_email='txw@outlook.com',
+      license='MIT',
+      packages=['testPy'],
+      zip_safe=False)
