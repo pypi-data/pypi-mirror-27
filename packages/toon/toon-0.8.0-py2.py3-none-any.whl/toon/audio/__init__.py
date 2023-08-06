124d5eca26b043a7d1986de339201bea49e7d95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:03 +0000

    Add package toon/0.8.0/toon-0.8.0-py2.py3-none-any.whl

---
+++ packages/toon/toon-0.8.0-py2.py3-none-any.whl/toon/audio/__init__.py
@@ -0,0 +1,11 @@
+"""Tools to generate audio stimuli.
+.. moduleauthor:: Alexander Forrence <aforren1@jhu.edu>
+"""
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+from future import standard_library
+standard_library.install_aliases()
+from .beep_sequence import beep, beep_sequence
