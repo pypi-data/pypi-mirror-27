376436ad4ad4175cacfe046a1c12eacced50a271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:36 +0000

    Add package wkhtmltopdf-pack/0.12.3.0.post1/wkhtmltopdf-pack-0.12.3.0.post1.tar.gz

---
+++ packages/wkhtmltopdf-pack/wkhtmltopdf-pack-0.12.3.0.post1.tar.gz/wkhtmltopdf-pack-0.12.3.0.post1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='wkhtmltopdf-pack',
+    version='0.12.3.0-rev1',
+    url='https://github.com/illarra/wkhtmltopdf-pack',
+    author='illarra',
+    author_email='team@illarra.com',
+    license='MIT',
+    data_files=[('bin', ['bin/wkhtmltopdf-pack'])],
+    description='wkhtmltopdf 0.12.3.0 for Python on Heroku/cloudControl buildpacks',
+    long_description=open('README.rst').read(),
+    keywords='pdf wkhtmltopdf paas heroku cloudcontrol buildpack',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    zip_safe=False
+)
