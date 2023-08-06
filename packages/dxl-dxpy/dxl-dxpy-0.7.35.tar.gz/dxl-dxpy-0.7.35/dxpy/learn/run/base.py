e366baf27b89f3c5fd97f39d9dc3e784de0a721c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:01 +0000

    Add package dxl-dxpy/0.7.35/dxl-dxpy-0.7.35.tar.gz

---
+++ packages/dxl-dxpy/dxl-dxpy-0.7.35.tar.gz/dxl-dxpy-0.7.35/dxpy/learn/run/base.py
@@ -0,0 +1,13 @@
+class DxlnRunEnvrionment:
+    def __init__(self, config_file_name='dxln.yml'):
+        self._config = config_file_name
+        pass
+
+    def __enter__(self):
+        from dxpy.learn.utils.general import pre_work, load_yaml_config
+        load_yaml_config(self._config)
+        pre_work()
+        return self
+
+    def __exit__(self, type, value, trackback):
+        pass
