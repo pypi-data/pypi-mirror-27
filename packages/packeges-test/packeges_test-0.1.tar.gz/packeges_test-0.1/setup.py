85528c37638eb6e6ab7e302d5f35c8f015cd23fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:56 +0000

    Add package packeges-test/0.1/packeges_test-0.1.tar.gz

---
+++ packages/packeges-test/packeges_test-0.1.tar.gz/packeges_test-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='packeges_test',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/ddi-pc-57/packeges_test',
+      author='Flying Circus',
+      author_email='packeges_test@example.com',
+      license='MIT',
+      packages=['packeges_test'],
+      zip_safe=False)
