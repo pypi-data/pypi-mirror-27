8f6648b170464263031c4e666b996b302780686d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:21 +0000

    Add package django-crosswalk-client/0.0.1/django-crosswalk-client-0.0.1.tar.gz

---
+++ packages/django-crosswalk-client/django-crosswalk-client-0.0.1.tar.gz/django-crosswalk-client-0.0.1/crosswalk_client/__init__.py
@@ -0,0 +1,21 @@
+from .methods import ClientMethods
+
+
+class Client(ClientMethods):
+    def __init__(
+        self,
+        token,
+        service_address,
+        domain=None,
+        threshold=80,
+        scorer='fuzzywuzzy.default_process',
+    ):
+        self.token = token
+        self.service_address = service_address
+        self.headers = {
+            'Authorization': 'TOKEN {}'.format(self.token)
+        }
+        self.domain = domain
+        self.threshold = threshold
+        self.scorer = scorer
+        self.client_check()
