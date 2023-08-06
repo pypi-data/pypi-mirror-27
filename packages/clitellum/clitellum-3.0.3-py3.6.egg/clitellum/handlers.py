834b1716dfcc4556978254ecdfc89f75ee688945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:17 +0000

    Add package clitellum/3.0.3/clitellum-3.0.3-py3.6.egg

---
+++ packages/clitellum/clitellum-3.0.3-py3.6.egg/clitellum/handlers.py
@@ -0,0 +1,51 @@
+
+from clitellum.core import serialization
+
+__author__ = 'Sergio'
+
+
+class HandlerManager:
+
+    def __init__(self):
+        self.__handlers = dict()
+
+    def handler(self, key, **options):
+        def decorator(f):
+            self.add_handler(key, f, options)
+            return f
+        return decorator
+
+    def add_handler(self, key, class_type, options):
+        self.__handlers[key] = class_type
+
+    def get_handler(self, key):
+        return self.__handlers[key]()
+
+
+class HandlerBase:
+
+    def __init__(self):
+        self.__bus = None
+        self.__context = None
+
+    def initialize(self, bus, context):
+        self.__bus = bus
+        self.__context = context
+
+    @property
+    def bus(self):
+        """
+        :rtype: SenderGateway
+        :return:
+        """
+        return self.__bus
+
+    @property
+    def context(self):
+        return self.__context
+
+    def handle_message(self, message):
+        pass
+
+    def _send(self, message, key):
+        self.bus.send(message, key=key, context=self.context)
---
+++ packages/clitellum/clitellum-3.0.3-py3.6.egg/clitellum/publishers.py
@@ -0,0 +1,35 @@
+from clitellum.core import serialization
+from clitellum.core.bus import MessageBus
+from clitellum.endpoints import gateways
+from clitellum.server import Identification
+
+__author__ = 'Sergio'
+
+
+def create_agent_from_config(cfg):
+    identification = Identification(cfg['identification']['id'], cfg['identification']['type'])
+    sg = gateways.CreateSenderFromConfig(cfg["sender_gateway"])
+    return Publisher(identification, sg)
+
+
+class Publisher:
+
+    def __init__(self, identification, sender_gateway):
+        self._identification = identification
+        self._senderGateway = sender_gateway
+        self._senderGateway.connect()
+
+    @property
+    def identification(self):
+        return self._identification
+
+    def publish(self, message, key, context=None):
+        message_bus = MessageBus.create(message, key, self.identification.id, self.identification.type)
+        if context is not None:
+            message_bus['Header']['CallContext'] = context
+
+        message_serialized = serialization.dumps(message_bus)
+        self._senderGateway.send(message_serialized, key)
+
+    def __del__(self):
+        self._senderGateway.close()
