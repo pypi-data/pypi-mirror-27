b63414a30199418cfdbba9e196a19df878e4769c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:02 +0000

    Add package nester-xfb/1.1.0/nester_xfb-1.1.0.tar.gz

---
+++ packages/nester-xfb/nester_xfb-1.1.0.tar.gz/nester_xfb-1.1.0/nester_xfb.py
@@ -0,0 +1,12 @@
+'这是一个模块，名字是nester_xfb'
+'提供了一个名为print_lol()的函数'
+def print_lol(the_list,level):
+    """这是一个可递归输出列表的函数
+参数是the_list"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item,level+1)
+        else:
+            for tap_stop in range(level):
+                print('\t',end='')
+            print(each_item)
