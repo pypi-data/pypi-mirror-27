56f681553115c98552093c501e83f23cc76282b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:02 +0000

    Add package sqlbrowser/1.0.0/sqlbrowser-1.0.0.tar.gz

---
+++ packages/sqlbrowser/sqlbrowser-1.0.0.tar.gz/sqlbrowser-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="sqlbrowser",
+    version="1.0.0",
+    packages=["sqlbrowser"],
+    author="Matthew Swain",
+    author_email="matthewswain@gmail.com",
+)
