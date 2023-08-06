f4d7e12d2eca7c69c5578b6182c77a790baf4fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:53 +0000

    Add package itk-segmentation/4.13.0/itk_segmentation-4.13.0-cp27-cp27m-win_amd64.whl

---
+++ packages/itk-segmentation/itk_segmentation-4.13.0-cp27-cp27m-win_amd64.whl/itk/Configuration/ITKVoronoiConfig.py
@@ -0,0 +1,4 @@
+depends = ('ITKPyBase', 'ITKMesh', 'ITKImageFilterBase', )
+templates = (
+  ('VoronoiSegmentationImageFilter', 'itk::VoronoiSegmentationImageFilter', 'itkVoronoiSegmentationImageFilterIUC2IUC2IUC2', True, 'itk::Image< unsigned char,2 >, itk::Image< unsigned char,2 >, itk::Image< unsigned char,2 >'),
+)
