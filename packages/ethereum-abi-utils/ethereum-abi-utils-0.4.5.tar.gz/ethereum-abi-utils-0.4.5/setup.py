2feffed689ed4c74166fb321a01d48c7bcecdcb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:44 +0000

    Add package ethereum-abi-utils/0.4.5/ethereum-abi-utils-0.4.5.tar.gz

---
+++ packages/ethereum-abi-utils/ethereum-abi-utils-0.4.5.tar.gz/ethereum-abi-utils-0.4.5/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+setup(
+    name='ethereum-abi-utils',
+    version='0.4.5',
+    description="""Common utility functions for ethereum codebases.""",
+    long_description_markdown_filename='README.md',
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/ethereum/ethereum-abi-utils',
+    include_package_data=True,
+    install_requires=[],
+    setup_requires=['setuptools-markdown'],
+    py_modules=['eth_abi'],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum',
+    packages=find_packages(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
