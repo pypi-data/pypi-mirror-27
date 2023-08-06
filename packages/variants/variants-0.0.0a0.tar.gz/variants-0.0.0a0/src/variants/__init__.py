4ae02e49e4eedf409cf8baacb43a9da525eaf565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:25 +0000

    Add package variants/0.0.0a0/variants-0.0.0a0.tar.gz

---
+++ packages/variants/variants-0.0.0a0.tar.gz/variants-0.0.0a0/src/variants/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for variants."""
+
+from ._variants import primary
+
+__author__ = """Paul Ganssle"""
+__version__ = '0.1.0'
+
+__all__ = ['primary']
