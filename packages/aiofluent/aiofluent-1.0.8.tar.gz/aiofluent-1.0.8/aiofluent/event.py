719978eea5c0b80d2319c53c288f538198c9ecc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:06 +0000

    Add package aiofluent/1.0.8/aiofluent-1.0.8.tar.gz

---
+++ packages/aiofluent/aiofluent-1.0.8.tar.gz/aiofluent-1.0.8/aiofluent/event.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from aiofluent import sender
+
+import time
+
+
+class Event(object):
+    def __init__(self, label, data, **kwargs):
+        assert isinstance(data, dict), 'data must be a dict'
+        sender_ = kwargs.get('sender', sender.get_global_sender())
+        timestamp = kwargs.get('time', int(time.time()))
+        sender_.emit_with_time(label, timestamp, data)
+
+
+class AsyncEvent(object):
+    def __init__(self, label, data, **kwargs):
+        assert isinstance(data, dict), 'data must be a dict'
+        self.label = label
+        self.data = data
+        self.sender_ = kwargs.get('sender', sender.get_global_sender())
+        self.timestamp = kwargs.get('time', int(time.time()))
+
+    async def __call__(self):
+        await self.sender_.async_emit_with_time(
+            self.label, self.timestamp, self.data)
