80242f49c15b4bf6ed2988729e8e55a9f9892845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:11 +0000

    Add package pyseqlib/0.1.0/pyseqlib-0.1.0.tar.gz

---
+++ packages/pyseqlib/pyseqlib-0.1.0.tar.gz/pyseqlib-0.1.0/pyseqlib/__init__.py
@@ -0,0 +1,11 @@
+# Copyright(c) 2017, Yuanhua Huang
+# Licensed under the Apache License 2.0 at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+__version__ = "0.1.0"
+
+import pyximport; pyximport.install()
+from .utils.gtf import *
+from .utils.fasta import *
+
+
