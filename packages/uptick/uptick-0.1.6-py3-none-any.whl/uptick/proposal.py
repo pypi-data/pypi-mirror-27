ea20c2a20c36bec10b81af4aa56ca019e9da321f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:46 +0000

    Add package uptick/0.1.6/uptick-0.1.6-py3-none-any.whl

---
+++ packages/uptick/uptick-0.1.6-py3-none-any.whl/uptick/proposal.py
@@ -0,0 +1,95 @@
+import json
+import click
+from pprint import pprint
+from bitshares.storage import configStorage as config
+from bitshares.proposal import Proposals
+from prettytable import PrettyTable
+from .decorators import (
+    onlineChain,
+    unlockWallet
+)
+from .main import main
+
+
+@main.command()
+@click.pass_context
+@onlineChain
+@click.argument(
+    'proposal',
+    nargs=-1)
+@click.option(
+    "--account",
+    help="Account that takes this action",
+    default=config["default_account"],
+    type=str)
+@unlockWallet
+def disapproveproposal(ctx, proposal, account):
+    """ Disapprove a proposal
+    """
+    pprint(ctx.bitshares.disapproveproposal(
+        proposal,
+        account=account
+    ))
+
+
+@main.command()
+@click.pass_context
+@onlineChain
+@click.argument(
+    'proposal',
+    nargs=-1)
+@click.option(
+    "--account",
+    help="Account that takes this action",
+    default=config["default_account"],
+    type=str)
+@unlockWallet
+def approveproposal(ctx, proposal, account):
+    """ Approve a proposal
+    """
+    pprint(ctx.bitshares.approveproposal(
+        proposal,
+        account=account
+    ))
+
+
+@main.command()
+@click.pass_context
+@onlineChain
+@click.argument(
+    "account",
+    default=config["default_account"],
+    type=str,
+    required=False
+)
+def proposals(ctx, account):
+    """ List proposals
+    """
+    proposals = Proposals(account)
+    t = PrettyTable([
+        "id",
+        "expiration",
+        "required approvals",
+        "available approvals",
+        "review period time",
+        "proposal",
+    ])
+    t.align = 'l'
+    for proposal in proposals:
+        t.add_row([
+            proposal["id"],
+            proposal["expiration_time"],
+            json.dumps(
+                proposal["required_active_approvals"] +
+                proposal["required_owner_approvals"],
+                indent=1),
+            json.dumps(
+                proposal["available_active_approvals"] +
+                proposal["available_key_approvals"] +
+                proposal["available_owner_approvals"],
+                indent=1),
+            proposal.get("review_period_time", None),
+            json.dumps(proposal["proposed_transaction"], indent=4),
+        ])
+
+    click.echo(str(t))
