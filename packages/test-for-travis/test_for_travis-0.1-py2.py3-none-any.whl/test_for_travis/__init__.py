185de6d5cff68cb7ea1c675a9c194e80afd5bb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:01 +0000

    Add package test-for-travis/0.1/test_for_travis-0.1-py2.py3-none-any.whl

---
+++ packages/test-for-travis/test_for_travis-0.1-py2.py3-none-any.whl/test_for_travis/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+c = 'j'
+def uploadpy():
+	b = 23
+	if b is 23:
+		print 'upload test'
+	else:
+		return None
+
+def pypy():
+	a = 13
+	print 'this test is ok!'
+	return a
