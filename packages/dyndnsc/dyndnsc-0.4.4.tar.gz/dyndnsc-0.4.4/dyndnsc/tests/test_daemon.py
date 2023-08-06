659e551c3fc8a4eb0a2ced355e4bc4a751b90626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:30 +0000

    Add package dyndnsc/0.4.4/dyndnsc-0.4.4.tar.gz

---
+++ packages/dyndnsc/dyndnsc-0.4.4.tar.gz/dyndnsc-0.4.4/dyndnsc/tests/test_daemon.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from dyndnsc import daemon
+
+
+class TestDaemon(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test_daemonize(self):
+        # TODO
+        pass
