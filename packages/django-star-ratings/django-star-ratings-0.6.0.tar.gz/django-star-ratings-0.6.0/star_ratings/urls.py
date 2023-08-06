0adfe5db5651b55ee514fb59c1e4c00e8a462334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:19 +0000

    Add package django-star-ratings/0.6.0/django-star-ratings-0.6.0.tar.gz

---
+++ packages/django-star-ratings/django-star-ratings-0.6.0.tar.gz/django-star-ratings-0.6.0/star_ratings/urls.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+from .views import Rate
+
+
+urlpatterns = [
+    url(r'(?P<content_type_id>\d+)/(?P<object_id>\d+)/', Rate.as_view(), name='rate'),
+]
+
+app_name = 'star_ratings'
