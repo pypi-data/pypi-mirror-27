65808080c86cef46ea2200b8c3056dd87590e813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:13 +0000

    Add package nester-apple830418/1.0.0/nester_apple830418-1.0.0.tar.gz

---
+++ packages/nester-apple830418/nester_apple830418-1.0.0.tar.gz/nester_apple830418-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester_apple830418',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='Apple830418',
+    author_email='su2002a5@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='Apple A simple printer of nested lists',
+)
