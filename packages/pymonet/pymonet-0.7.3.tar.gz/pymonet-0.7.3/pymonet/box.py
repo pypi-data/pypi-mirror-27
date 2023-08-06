08fd41ef631ac846e3cfddb44f047a7615317b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:31 +0000

    Add package pymonet/0.7.3/pymonet-0.7.3.tar.gz

---
+++ packages/pymonet/pymonet-0.7.3.tar.gz/pymonet-0.7.3/pymonet/box.py
@@ -0,0 +1,84 @@
+class Box:
+    """
+    Data type for storage any type of data
+    """
+
+    def __init__(self, value):
+        """
+        :param value: value to store in Box
+        :type value: Any
+        """
+        self.value = value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def map(self, mapper):
+        """
+        Take function (A) -> b and applied this function on current box value and returns new box with mapped value.
+
+        :param mapper: mapper function
+        :type mapper: Function(A) -> B
+        :returns: new box with mapped value
+        :rtype: Box<B>
+        """
+        return Box(mapper(self.value))
+
+    def bind(self, mapper):
+        """
+        Take function and applied this function on current box value and returns mapped value.
+
+        :param mapper: mapper function
+        :type mapper: Function(A) -> B
+        :returns: new box with mapped value
+        :rtype: B
+        """
+        return mapper(self.value)
+
+    def ap(self, monad):
+        """
+        It takes as a parameter another Box type which contains a function,
+        and then applies that function to the value contained in the calling Box.
+
+        :param monad: monad contains function
+        :type monad: Box<Function(A) -> B>
+        :returns: new Box with result of contains function
+        :rtype: Box<B>
+        """
+        return self.map(monad.value)
+
+    def to_maybe(self):
+        """
+        :returns: non empty Maybe monad with previous value
+        :rtype: Maybe<A>
+        """
+        from pymonet.maybe import Maybe
+
+        return Maybe.just(self.value)
+
+    def to_either(self):
+        """
+        :returns: right Either monad with previous value
+        :rtype: Right<A>
+        """
+        from pymonet.either import Right
+
+        return Right(self.value)
+
+    def to_lazy(self):
+        """
+        :returns: not folded Lazy monad with function returning previous value
+        :rtype: Lazy<Function(() -> A)
+        """
+        from pymonet.lazy import Lazy
+
+        return Lazy(lambda: self.value)
+
+    def to_try(self):
+        """
+        :returns: successfully Try monad with previous value
+        :rtype: Try<A>
+        """
+        from pymonet.monad_try import Try
+
+        return Try(self.value, is_success=True)
---
+++ packages/pymonet/pymonet-0.7.3.tar.gz/pymonet-0.7.3/pymonet/test_either.py
@@ -0,0 +1,132 @@
+from pymonet.either import Left, Right
+from pymonet.box import Box
+from pymonet.maybe import Maybe
+from pymonet.monad_try import Try
+from pymonet.monad_law_tester import get_associativity_test, get_left_unit_test, get_right_unit_data
+from pymonet.utils import increase, identity
+
+from hypothesis import given
+from hypothesis.strategies import integers
+
+
+class EitherSpy:
+
+    def error_handler(value, *args):
+        pass
+
+    def success_handler(value, *args):
+        pass
+
+
+def test_either_eq_operator_should_compare_values():
+
+    assert Right(42) == Right(42)
+    assert Right(42) != Right(43)
+
+    assert Left(42) == Left(42)
+    assert Left(42) != Left(43)
+
+    assert Right(42) != Left(42)
+
+
+def test_mapper_should_be_applied_only_on_current_value():
+
+    assert Left(42).map(increase) == Left(42)
+    assert Right(42).map(increase) == Right(43)
+
+
+def test_ap_method_should_be_call_on_only_right():
+
+    assert Left(42).ap(Left(increase)) == Left(42)
+    assert Right(42).ap(Left(increase)) == Right(43)
+    assert Left(42).ap(Right(increase)) == Left(42)
+    assert Right(42).ap(Right(increase)) == Right(43)
+
+
+def test_is_right_should_return_suitable_value():
+
+    assert Right(42).is_right()
+    assert not Left(42).is_right()
+
+
+def test_is_left_should_return_suitable_value():
+    assert Left(42).is_left()
+    assert not Right(42).is_left()
+
+
+def test_bind_should_be_applied_only_on_current_value_and_return_value():
+    assert Left(42).bind(lambda value: Right(value + 1)).value == 42
+    assert Right(42).bind(lambda value: Right(value + 1)).value == 43
+    assert Right(42).bind(lambda value: Left(value + 1)).value == 43
+
+
+def test_case_method_should_call_proper_handler(mocker):
+    either_spy = EitherSpy()
+    mocker.spy(either_spy, 'error_handler')
+    mocker.spy(either_spy, 'success_handler')
+
+    Left(42).case(
+        success=either_spy.success_handler,
+        error=either_spy.error_handler
+    )
+
+    assert either_spy.error_handler.call_count == 1
+    assert either_spy.success_handler.call_count == 0
+
+    Right(42).case(
+        success=either_spy.success_handler,
+        error=either_spy.error_handler
+    )
+
+    assert either_spy.error_handler.call_count == 1
+    assert either_spy.success_handler.call_count == 1
+
+
+def test_maybe_associativity_law():
+    get_associativity_test(
+        monadic_value=Right(42),
+        mapper1=lambda value: Right(value + 1),
+        mapper2=lambda value: Right(value + 2),
+    )()
+    get_associativity_test(
+        monadic_value=Left(0),
+        mapper1=lambda value: Right(value + 1),
+        mapper2=lambda value: Right(value + 2),
+    )()
+
+
+def test_maybe_left_unit_law():
+    get_left_unit_test(
+        monad=Right,
+        monad_value=42,
+        mapper=lambda value: Right(value + 1)
+    )()
+
+
+def test_maybe_right_unit_data_law():
+    get_right_unit_data(Right, 42)()
+    get_right_unit_data(Left, 42)()
+
+
+@given(integers())
+def test_transform_to_box_should_return_box(integer):
+    assert Right(integer).to_box() == Box(integer)
+    assert Left(integer).to_box() == Box(integer)
+
+
+@given(integers())
+def test_transform_to_maybe_should_return_maybe(integer):
+    assert Right(integer).to_maybe() == Maybe.just(integer)
+    assert Left(integer).to_maybe() == Maybe.nothing()
+
+
+@given(integers())
+def test_transform_to_lazy_should_return_lazy(integer):
+    assert Right(integer).to_lazy().fold(identity) == integer
+    assert Left(integer).to_lazy().fold(identity) == integer
+
+
+@given(integers())
+def test_transform_to_try_should_return_try(integer):
+    assert Right(integer).to_try() == Try(integer, is_success=True)
+    assert Left(integer).to_try() == Try(integer, is_success=False)
