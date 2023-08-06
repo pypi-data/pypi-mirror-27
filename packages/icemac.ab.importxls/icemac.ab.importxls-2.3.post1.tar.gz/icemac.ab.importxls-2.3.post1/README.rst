2799506cad5d2ffa7fc4f84b2565b561bd72a3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:58 +0000

    Add package icemac.ab.importxls/2.3.post1/icemac.ab.importxls-2.3.post1.tar.gz

---
+++ packages/icemac.ab.importxls/icemac.ab.importxls-2.3.post1.tar.gz/icemac.ab.importxls-2.3.post1/README.rst
@@ -0,0 +1,39 @@
+=====================
+ icemac.ab.importxls
+=====================
+
+This package enables `icemac.addressbook`_ to import XLS (Excel) files.
+
+.. _`icemac.addressbook` : https://pypi.org/project/icemac.addressbook
+
+Copyright (c) 2009-2017 Michael Howitz
+
+All Rights Reserved.
+
+This software is subject to the provisions of the Zope Public License,
+Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+FOR A PARTICULAR PURPOSE.
+
+Hacking
+=======
+
+Source code
+-----------
+
+Get the source code::
+
+   $ hg clone https://bitbucket.org/icemac/icemac.ab.importxls
+
+or fork me on: https://bitbucket.org/icemac/icemac.ab.importxls
+
+Running Tests
+-------------
+
+To run the tests yourself call::
+
+  $ python2.7 bootstrap.py
+  $ bin/buildout
+  $ bin/py.test
---
+++ packages/icemac.ab.importxls/icemac.ab.importxls-2.3.post1.tar.gz/icemac.ab.importxls-2.3.post1/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import os.path
+import setuptools
+
+
+def read(*path_elements):
+    """Read a file."""
+    return "\n\n" + file(os.path.join(*path_elements)).read()
+
+version = '2.3.post1'
+name = 'icemac.ab.importxls'
+
+setuptools.setup(
+    name=name,
+    version=version,
+    description="Import XLS files into icemac.addressbook.",
+    long_description=(
+        read('README.rst') +
+        read('CHANGES.rst')
+    ),
+    keywords='icemac.addressbook xls excel import',
+    author='Michael Howitz',
+    author_email='icemac@gmx.net',
+    url='https://pypi.org/project/%s' % name,
+    license='ZPL 2.1',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Paste',
+        'Framework :: Zope3',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: Zope Public License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+    ],
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['icemac', 'icemac.ab'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'icemac.ab.importer >= 1.0',
+        'setuptools',
+        'xlrd',
+    ],
+    extras_require=dict(
+        test=['zope.testing >= 3.8.0',
+              ]),
+)
