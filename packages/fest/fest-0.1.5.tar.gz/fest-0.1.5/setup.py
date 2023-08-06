165917592b6e01ff14ac37d29728034076825708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:32 +0000

    Add package fest/0.1.5/fest-0.1.5.tar.gz

---
+++ packages/fest/fest-0.1.5.tar.gz/fest-0.1.5/setup.py
@@ -0,0 +1,19 @@
+import textwrap
+from setuptools import setup
+
+
+setup(name='fest',
+      version='0.1.5',
+      author='amancevice',
+      author_email='smallweirdnum@gmail.com',
+      packages=['fest'],
+      url='https://github.com/amancevice/fest',
+      description='Sync Facebook events to other services',
+      long_description=textwrap.dedent(
+          '''See GitHub_ for documentation.
+          .. _GitHub: https://github.com/amancevice/fest'''),
+      install_requires=['click >= 6.7.0, < 6.8',
+                        'facebook-sdk >= 2.0.0, < 3',
+                        'google >= 1.9.3, < 2',
+                        'google-api-python-client >= 1.6.4, < 2'],
+      entry_points={'console_scripts': ['fest=fest.main:fest']})
