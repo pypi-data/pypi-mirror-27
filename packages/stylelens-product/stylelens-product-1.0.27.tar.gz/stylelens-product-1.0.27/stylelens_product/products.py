d16debcbd6f0b9aadb5d209a435980faa5611110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:41 +0000

    Add package stylelens-product/1.0.27/stylelens-product-1.0.27.tar.gz

---
+++ packages/stylelens-product/stylelens-product-1.0.27.tar.gz/stylelens-product-1.0.27/stylelens_product/products.py
@@ -0,0 +1,54 @@
+from bson.objectid import ObjectId
+from stylelens_product.database import DataBase
+
+class Products(DataBase):
+  def __init__(self):
+    super(Products, self).__init__()
+    self.products = self.db.products
+
+  def add_product(self, product):
+    object_id = None
+    query = {"host_code": product['host_code'], "product_no": product["product_no"]}
+    try:
+      r = self.products.update_one(query,
+                                  {"$set": product},
+                                  upsert=True)
+    except Exception as e:
+      print(e)
+
+    if 'upserted' in r.raw_result:
+      product_id = str(r.raw_result['upserted'])
+
+    return product_id
+
+  def get_products_by_host_code_and_version_id(self,
+                                              host_code, version_id,
+                                              offset=0, limit=100):
+    try:
+      r = self.products.find({"host_code": host_code,
+                              "version_id": version_id})\
+                            .skip(offset).limit(limit)
+    except Exception as e:
+      print(e)
+
+    return list(r)
+
+  def update_product_by_id(self, product_id, product):
+    try:
+      r = self.products.update_one({"_id": ObjectId(product_id)},
+                                  {"$set": product})
+    except Exception as e:
+      print(e)
+
+    return r.modified_count
+
+  def update_product_by_hostcode_and_productno(self, product):
+    query = {"host_code": product['host_code'], "product_no": product['product_no']}
+    try:
+      r = self.products.update_one(query,
+                                  {"$set": product},
+                                  upsert=True)
+    except Exception as e:
+      print(e)
+
+    return r.modified_count
