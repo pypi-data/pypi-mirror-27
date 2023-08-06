a98394a7e2efdb22d2cc382723dab7c1260d8cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:51 +0000

    Add package stacklesslib/1.2.0/stacklesslib-1.2.0.tar.gz

---
+++ packages/stacklesslib/stacklesslib-1.2.0.tar.gz/stacklesslib-1.2.0/setup.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+setup(
+    name = "stacklesslib",
+    version = "1.2.0",
+    packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = "Kristján Valur Jónsson",
+    author_email = "sweskman@gmail.com",
+    description = "A set of essential utilities for Stackless",
+    license = "PSF",
+    keywords = "stackless stacklesssocket",
+    url = "https://bitbucket.org/stackless-dev/stacklesslib",
+    test_suite = "stacklesslib.test.run.load_tests",
+    use_2to3 = True,
+    use_2to3_exclude_fixers = ['lib2to3.fixes.fix_throw'],
+)
---
+++ packages/stacklesslib/stacklesslib-1.2.0.tar.gz/stacklesslib-1.2.0/stacklesslib/test/support.py
@@ -0,0 +1,67 @@
+#support.py
+# unittest support functions
+
+from time import clock
+import sys
+import unittest
+import functools
+import contextlib
+import cStringIO
+
+import stackless
+import stacklesslib.errors
+from stacklesslib.util import timeout
+from stacklesslib import app, main
+
+# This is the total timeout for running tests.  Adjust as needed.
+TIMEOUT = 10
+
+def timesafe(t=1.0):
+    """Decorate a unittest with this to make it error after 't' seconds"""
+    def helper(func):
+        @functools.wraps(func)
+        def testmethod(self):
+            try:
+                with timeout(t):
+                    func(self)
+            except stacklesslib.errors.TimeoutError:
+                self.fail("test case timed out")
+        return testmethod
+    return helper
+
+
+@contextlib.contextmanager
+def captured_stderr():
+    old = sys.stderr
+    sys.stderr = cStringIO.StringIO()
+    try:
+        yield sys.stderr
+    finally:
+        sys.stderr = old
+
+class StacklessTestSuite(unittest.TestSuite):
+    def run(self, results):
+        err = []
+        def tasklet_run():
+            try:
+                unittest.TestSuite.run(self, results)
+            except:
+                err.append(sys.exc_info())
+        app.install_stackless()
+        tasklet = stackless.tasklet(tasklet_run)()
+        deadline = clock() + TIMEOUT
+        while tasklet.alive:
+            main.mainloop.loop()
+            if clock() > deadline:
+                raise stacklesslib.errors.TimeoutError("unittest took too long.  Deadlock?")
+        if err:
+            try:
+                raise err[0][0], err[0][1], err[0][2]
+            finally:
+                err = None
+
+
+def load_tests(loader, tests, pattern): # test loader protocol
+    suite = StacklessTestSuite()
+    suite.addTests(tests)
+    return suite
