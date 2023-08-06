f3c522f13141f21b31ab8169a379056de2e1436b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:40 +0000

    Add package robolearn-python/0.1.1/robolearn-python-0.1.1.tar.gz

---
+++ packages/robolearn-python/robolearn-python-0.1.1.tar.gz/robolearn-python-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='robolearn-python',
+    description='SDK for Robolearn',
+    license='MIT',
+    url='https://github.com/NoUseFreak/robolearn-python',
+    version='0.1.1',
+    author='Dries De Peuter',
+    author_email='dries@nousefreak.be',
+    maintainer='Dries De Peuter',
+    maintainer_email='dries@nousefreak.be',
+    long_description='',
+    packages=["robolearn"],
+    zip_safe=False,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7"
+    ],
+    install_requires=['urllib2'],
+)
