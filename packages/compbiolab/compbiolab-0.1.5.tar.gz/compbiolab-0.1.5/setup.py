e6b3ba983cb58e9f5b4595bbbedc2210faf406b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:49 +0000

    Add package compbiolab/0.1.5/compbiolab-0.1.5.tar.gz

---
+++ packages/compbiolab/compbiolab-0.1.5.tar.gz/compbiolab-0.1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='compbiolab',
+      version='0.1.5',
+      description='Common functions for the CompBioLab researchers',
+      url='https://github.com/miqueleg/compbiolab',
+      author='Miquel Estevez',
+      author_email='miquel.estevez@udg.edu',
+      license='MIT',
+      packages=['compbiolab'],
+      zip_safe=False)
