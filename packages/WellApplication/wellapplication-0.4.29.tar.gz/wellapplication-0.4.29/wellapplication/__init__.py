3fce2279265528be2aa164ebe3726f7cca6f9c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:34 +0000

    Add package WellApplication/0.4.29/wellapplication-0.4.29.tar.gz

---
+++ packages/WellApplication/wellapplication-0.4.29.tar.gz/wellapplication-0.4.29/wellapplication/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*- 
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os
+
+from .transport import *
+from .usgs import *
+from .chem import *
+from .mesopy import *
+from .graphs import *
+from .MannKendall import *
+
+__version__ = '0.4.29'
+__author__ = 'Paul Inkenbrandt'
+__name__ = 'wellapplication'
+
+__all__ = ['WQP',
+           'get_response',
+           'get_wqp_stations',
+           'get_wqp_results',
+           'massage_results',
+           'datetimefix',
+           'parnorm',
+           'unitfix',
+           'massage_stations',
+           'piv_chem']
