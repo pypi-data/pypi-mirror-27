69c9a16d42ec6757a3401f0a8fd03a997a0af656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:19 +0000

    Add package zenner-nester/1.0.0/zenner_nester-1.0.0.tar.gz

---
+++ packages/zenner-nester/zenner_nester-1.0.0.tar.gz/zenner_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		=	'zenner_nester',
+	version 	=	'1.0.0',
+	py_modules	=	['zenner_nester'],
+	author		=	'jubingyi',
+	authoremail	=	'juyi006@163.com',
+	url		=	'https://weibo.com/u/1956918893',
+	description	=	'A simple printer of nested lists',
+)
