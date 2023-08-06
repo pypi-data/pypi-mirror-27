5f2f6e8020345cde4f391a19c4a82566dba43fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:53 +0000

    Add package dynamicmultithreadedexecutor/1.0.1/dynamicmultithreadedexecutor-1.0.1.tar.gz

---
+++ packages/dynamicmultithreadedexecutor/dynamicmultithreadedexecutor-1.0.1.tar.gz/dynamicmultithreadedexecutor-1.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='dynamicmultithreadedexecutor',
+      version='1.0.1',
+      description='Dynamic Multi-threaded Executor',
+      author='Kevin McCabe',
+      author_email='csmp@hotmail.com',
+      url='https://github.com/gumpcraca/dynamicmultithreadedexecutor',
+      download_url="https://github.com/gumpcraca/dynamicmultithreadedexecutor/archive/1.1.zip",
+      keywords = [],
+      packages=find_packages('.'),
+      install_requires=['six'],
+      classifiers = [],
+     )
