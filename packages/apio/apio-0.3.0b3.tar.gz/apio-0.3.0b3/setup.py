2a2607d5ede0d71704e8e7687390e2a7841e9620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:14 +0000

    Add package apio/0.3.0b3/apio-0.3.0b3.tar.gz

---
+++ packages/apio/apio-0.3.0b3.tar.gz/apio-0.3.0b3/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+from apio import (__author__, __description__, __email__, __license__,
+                  __title__, __url__, __version__)
+
+
+setup(
+    name=__title__,
+    version=__version__,
+    description=__description__,
+    author=__author__,
+    author_email=__email__,
+    url=__url__,
+    license=__license__,
+    packages=['apio'],
+    package_data={
+        'apio': ['commands/*.py',
+                 'managers/*.py',
+                 'resources/*']
+    },
+    install_requires=[
+        'click>=5,<7',
+        'semantic_version>=2.5.0',
+        'requests>=2.4.0,<3',
+        'pyjwt>=1.5.3,<2',
+        'colorama',
+        'pyserial>=3,<4',
+        'scons==3.0.1'
+    ],
+    entry_points={
+        'console_scripts': ['apio=apio.__main__:cli']
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python']
+)
