bc13f79a631a00438950a6342859fd45cdde97a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:54 +0000

    Add package multimethod/0.7.1/multimethod-0.7.1.tar.gz

---
+++ packages/multimethod/multimethod-0.7.1.tar.gz/multimethod-0.7.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import multimethod
+
+setup(
+    name='multimethod',
+    version=multimethod.__version__,
+    description='Multiple argument dispatching.',
+    long_description=open('README.rst').read(),
+    author='Aric Coady',
+    author_email='aric.coady@gmail.com',
+    url='https://bitbucket.org/coady/multimethod',
+    license='Apache Software License',
+    py_modules=['multimethod'],
+    python_requires='>=2.7',
+    tests_require=['pytest-cov'],
+    keywords='multiple dispatch multidispatch generic functions methods overload',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
