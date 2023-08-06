f228e997dfc7197c9d8f3c8627e819bf46a25844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:53 +0000

    Add package colorqrxy/0.0.3/colorqrxy-0.0.3.tar.gz

---
+++ packages/colorqrxy/colorqrxy-0.0.3.tar.gz/colorqrxy-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'colorqrxy',
+  packages = ['colorqrxy'],
+  version = '0.0.3',
+  description = 'Get All Cars Brand',
+  long_description='Get All Cars Brand From juhe API',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'tel', 'python3','python','telphone'],
+  license='MIT',
+)
