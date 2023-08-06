e86054f3d4830bd8249fdbf359a0d1a4c95b9ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:49 +0000

    Add package schizophrenia/0.2.2/schizophrenia-0.2.2.tar.gz

---
+++ packages/schizophrenia/schizophrenia-0.2.2.tar.gz/schizophrenia-0.2.2/schizophrenia/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from schizophrenia import manager
+from schizophrenia import result
+from schizophrenia import task
+
+from schizophrenia.manager import *
+from schizophrenia.result import *
+from schizophrenia.task import *
+
+__all__ = manager.__all__ + result.__all__ + task.__all__
