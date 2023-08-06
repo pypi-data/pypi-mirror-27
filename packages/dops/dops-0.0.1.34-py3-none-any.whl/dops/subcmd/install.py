49bd46fdc35b669d8659b3cb66ddc4347b05b7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:36 +0000

    Add package dops/0.0.1.34/dops-0.0.1.34-py3-none-any.whl

---
+++ packages/dops/dops-0.0.1.34-py3-none-any.whl/dops/subcmd/install.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+@version:0.1
+@author: ysicing
+@file: dops/install.py 
+@time: 2018/1/3 23:43
+"""
+
+from . import BaseFlag, SubCommand
+from dops.plugins.instance import GeneralInstall
+
+
+def _create_instance(args):
+    m = GeneralInstall()
+    return m.install_instance(args.name, args.reinstall)
+
+InstallCommand = SubCommand(
+    name="install", help="install instance", metavar="<resource>",
+    flags=[],
+    subcmds=[
+        SubCommand(
+            name="instance",
+            usage="install an instance",
+            flags=[
+                BaseFlag('name', metavar='instance_id'),
+                BaseFlag('-r', '--reinstall', action='store_true')
+            ],
+            func=_create_instance
+        ),
+    ]
+)
---
+++ packages/dops/dops-0.0.1.34-py3-none-any.whl/dops/version.py
@@ -0,0 +1,7 @@
+VERSION_INFO = (
+                ("program","dops"),
+                ("version","dev-0.0.1"),
+                ("pypi","0.0.1.34"),
+                ("commit","34.g3234c87"),
+                ("buildTime","2018-01-03 16:16:30"),
+)
