172f8c1acac1c117bbb15737ec32e6d977df30c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:00 +0000

    Add package pretalx/0.2.2/pretalx-0.2.2.tar.gz

---
+++ packages/pretalx/pretalx-0.2.2.tar.gz/pretalx-0.2.2/pretalx/person/models/profile.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils.functional import cached_property
+from django.utils.translation import ugettext_lazy as _
+
+from pretalx.common.mixins import LogMixin
+from pretalx.common.urls import EventUrls
+
+
+class SpeakerProfile(LogMixin, models.Model):
+    biography = models.TextField(
+        verbose_name=_('Biography'),
+        help_text=_('You can use markdown here.'),
+        null=True, blank=True,
+    )
+    user = models.ForeignKey(
+        to='person.User',
+        related_name='profiles',
+        on_delete=models.CASCADE,
+        null=True, blank=True,
+    )
+    event = models.ForeignKey(
+        to='event.Event',
+        related_name='+',
+        on_delete=models.CASCADE,
+    )
+
+    class urls(EventUrls):
+        public = '{self.event.urls.base}/speaker/{self.user.code}'
+
+    @cached_property
+    def code(self):
+        return self.user.code
+
+    @cached_property
+    def submissions(self):
+        return self.user.submissions.filter(event=self.event)
+
+    @cached_property
+    def talks(self):
+        from pretalx.submission.models import SubmissionStates
+        return self.submissions.filter(state__in=[SubmissionStates.ACCEPTED, SubmissionStates.CONFIRMED])
