fdfd5d8d848679d4380746b67405104084ab3086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:59 +0000

    Add package pyperseus-treebank/0.0.1/pyperseus_treebank-0.0.1.tar.gz

---
+++ packages/pyperseus-treebank/pyperseus_treebank-0.0.1.tar.gz/pyperseus_treebank-0.0.1/pyperseus_treebank/base.py
@@ -0,0 +1,143 @@
+import glob
+
+
+class Token:
+    """ Token (single element in a sentence : a word or a punctuation sign)
+
+    :param index: Index of the token in the sentence
+    :param form: Form of the token in the sentence
+    :param lemma: Lemma that is attributed to the form
+    :param parent: Head word of the current token in the index, aka. token this word is dependant of
+    :param features: Features of the Token
+    :param pos: Part of Speech tag of the current Token
+    :param rel: Relation kind to the head word.
+
+    .. note:: Token can be compared (Token() == Token())
+    """
+    def __init__(self, index, form, lemma, parent=0, features="", pos=None, rel="ROOT"):
+        self.index = index
+        self.form = form
+        self.lemma = lemma
+        self.parent = parent
+        self.pos = pos
+        self.rel = rel
+        self.features = self.parse_features(features.replace("_", "-"))
+
+    def __eq__(self, other):
+        return self.index == other.index and \
+                self.form == other.form and \
+                self.lemma == other.lemma and \
+                self.parent == other.parent and \
+                self.pos == other.pos and \
+                self.rel == other.rel and \
+                self.features == other.features
+
+    def parse_features(self, features):
+        """ Parse features from the POSTAG of Perseus Latin XML
+
+        .. example :: self.parse_features("n-p---na-")
+
+        :param features: A string containing morphological informations
+        :type features: str
+        :return: Parsed features
+        :rtype: dict
+        """
+        raise NotImplementedError("Parse Features has not been implemented for this class")
+
+
+class Sentence:
+    """ A sentence of treebanked data
+
+    :param words: Words of the sentence
+    :type words: [Token]
+
+    .. note:: str(Corpus()) will release in a conllu export
+
+    .. note:: len(Sentence) tells how many words there is in the sentence
+
+    .. note:: Sentence accepts comparison
+
+    .. note:: You can iter over a sentence directly [token for token in Sentenc(..)]
+    """
+    def __init__(self, words):
+        self.__words__ = words
+
+    @property
+    def words(self):
+        """ Words of the sentence
+
+        :return: List of sentence's token
+        :rtype: [Token]
+        """
+        return self.__words__
+
+    def __eq__(self, other):
+        return len(other) == len(self) and \
+                False not in [mine == its for mine, its in zip(self, other)]
+
+    def __len__(self):
+        return len(self.words)
+
+    def __iter__(self):
+        for w in self.words:
+            yield w
+
+    @staticmethod
+    def str_features(features):
+        return "|".join("{}={}".format(cle, features[cle]) for cle in sorted(features.keys()))
+
+    def __getitem__(self, item):
+        return self.words[item]
+
+    def __str__(self):
+        """ String representation is Conllu format
+
+        :rtype: str
+        """
+        return "\n".join(
+            "\t".join([
+                token.index, token.form, token.lemma, token.pos, token.pos,
+                self.str_features(token.features), token.parent, token.rel, "_", "_"
+            ])
+            for token in self
+        ) or "_"
+
+
+class Corpus:
+    """ Corpus is a set of sentences which can be exported to Conllu
+
+    :param glob_path: Path to files that need to be parsed (Can be in the form \
+    of a glob : ./path/to/**/*.xml
+    :type glob_path: str
+
+    :attr sentences: List of sentences
+    :type sentences : [Sentence]
+
+    .. note:: str(Corpus()) will release in a conllu export
+    """
+    def __init__(self, glob_path):
+        self.files = glob.glob(glob_path)
+        self.__sentences__ = list(self.parse(self.files))
+
+    @property
+    def sentences(self):
+        return self.__sentences__
+
+    @staticmethod
+    def parse(files):
+        """ Parse a set of files
+
+        :param files: List of file's paths that needs to be parsed
+        :type files: str
+
+        :yields: Sentence thar were parsed
+        :ytype: Sentence
+        """
+        raise NotImplementedError()
+
+    def __str__(self):
+        """ String representation is Conllu format
+
+        :rtype: str
+        """
+        return "\n\n".join([str(s) for s in self.sentences])
---
+++ packages/pyperseus-treebank/pyperseus_treebank-0.0.1.tar.gz/pyperseus_treebank-0.0.1/pyperseus_treebank/latin.py
@@ -0,0 +1,96 @@
+"""
+
+..source :: https://github.com/perseids-project/perseids_treebanking/blob/ae0305138dacc4a89c5fe6b0f086a4b3b1efdc92/transformations/aldt-util.xsl
+"""
+from .base import Token, Corpus, Sentence
+import lxml.etree as etree
+
+
+_NUMBER = {"s": "Sing", "p": "Plur"}
+_TENSE = {"p": "Pres", "f": "Fut", "r": "Perf", "l": "PQP", "i": "Imp"}
+_MOOD = {"i": "Ind", "s": "Sub", "m": "Imp", "g": "Ger", "p": "Part", "u": "Sup", "n": "Inf"}
+_VOICE = {"a": "Act", "p": "Pass", "d": "Dep"}
+_GENDER = {"f": "Fem", "m": "Masc", "n": "Neut", "c": "Com"}
+_CASE = {"g": "Gen", "d": "Dat", "a": "Acc", "v": "Voc", "n": "Nom", "b": "Abl", "i": "Ins", "l": "Loc"}
+_DEGREE = {"p": "Pos", "c": "Comp", "s": "Sup"}
+
+
+class LatinToken(Token):
+
+    def parse_features(self, features):
+        """ Parse features from the POSTAG of Perseus Latin XML
+
+        .. example :: self.parse_features("n-p---na-")
+
+        :param features: A string containing morphological informations
+        :type features: str
+        :return: Parsed features
+        :rtype: dict
+        """
+
+        feats = {}
+
+        self.pos = features[0]
+
+        # Person handling : 3 possibilities
+        if features[1] != "-":
+            feats["Person"] = features[1]
+
+        # Number handling : two possibilities
+        if features[2] != "-":
+            feats["Number"] = _NUMBER[features[2]]
+
+        # Tense
+        if features[3] != "-":
+            feats["Tense"] = _TENSE[features[3]]
+
+        # Mood
+        if features[4] != "-":
+            feats["Mood"] = _MOOD[features[4]]
+
+        # Voice
+        if features[5] != "-":
+            feats["Voice"] = _VOICE[features[5]]
+
+        # Tense
+        if features[6] != "-":
+            feats["Gender"] = _GENDER[features[6]]
+
+        # Tense
+        if features[7] != "-":
+            feats["Case"] = _CASE[features[7]]
+
+        # Degree
+        if features[8] != "-":
+            feats["Degree"] = _DEGREE[features[8]]
+
+        return feats
+
+
+class LatinCorpus(Corpus):
+    @staticmethod
+    def parse(files):
+        for file in files:
+            with open(file) as f:
+                xml = etree.parse(f)
+            for sentence in xml.xpath("//sentence"):
+                sent = []
+                for word in sentence.xpath(".//word[not(@artificial)]"):
+                    postag = word.get("postag")
+                    if not postag:
+                        if word.get("lemma") == "punc1":
+                            postag = "u--------"
+                        elif word.get("form") in ",!“":
+                            postag = "u--------"
+                        else:
+                            print(etree.tostring(word))
+                            raise ValueError("postag is empty")
+                    sent.append(LatinToken(
+                        index=word.get("id"),
+                        form=word.get("form"),
+                        lemma=word.get("lemma"),
+                        features=postag,
+                        rel=word.get("relation"),
+                        parent=word.get("head")
+                    ))
+                yield Sentence(sent)
