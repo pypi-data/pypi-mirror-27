c971a1caebde5d25041e460feb591af358e2ee7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:33 +0000

    Add package auto-extract/0.0.2/auto_extract-0.0.2.tar.gz

---
+++ packages/auto-extract/auto_extract-0.0.2.tar.gz/auto_extract-0.0.2/auto_extract/crawling/medium.py
@@ -0,0 +1,21 @@
+from auto_extract.crawling.crawler import Crawler
+from auto_extract.crawling.regexes import re_count
+
+
+def remove_after_last_dash(article_url):
+    return "-".join(article_url.split("-")[:-1])
+
+
+class MediumCrawler(Crawler):
+
+    def __init__(self, page, *args, **kwargs):
+        base_url = "https://medium.com/{}/".format(page)
+        seed_url = base_url + "latest"
+        super().__init__(base_url=base_url, seed_url=seed_url,
+                         name="medium_{}".format(page),
+                         any_exclude_regexes=["/about", "/archive",
+                                              "/welcome", "/tagged", "/trending", "/@"],
+                         all_required_regexes=[re_count("/", 4, 4)],
+                         get_id=remove_after_last_dash,
+                         *args, **kwargs)
+        self.page = page
