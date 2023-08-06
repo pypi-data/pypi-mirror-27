d7f7155a7a664d17ba8e5d4aaf71a822dc0dd566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:10 +0000

    Add package django-x509/0.3.4/django-x509-0.3.4.tar.gz

---
+++ packages/django-x509/django-x509-0.3.4.tar.gz/django-x509-0.3.4/django_x509/static/django-x509/js/switcher.js
@@ -0,0 +1,35 @@
+django.jQuery(function ($) {
+    'use strict';
+    var operationType = $('.field-operation_type select');
+    // enable switcher only in add forms
+    if (!operationType.length || $('form .deletelink-box').length > 0) {
+        $('.field-operation_type').hide();
+        return;
+    }
+    // function for operation_type switcher
+    var showFields = function () {
+        // define fields for each operation
+        var importFields = $('.form-row:not(.field-certificate, .field-operation_type, ' +
+                          '.field-private_key, .field-name, .field-ca)'),
+            newFields = $('.form-row:not(.field-certificate, .field-private_key)'),
+            defaultFields = $('.form-row:not(.field-operation_type)'),
+            allFields = $('.form-row'),
+            value = operationType.val();
+        if (!value) {
+            allFields.show();
+            defaultFields.hide();
+        }
+        if (value === 'new') {
+            allFields.hide();
+            newFields.show();
+        }
+        if (value === 'import') {
+            allFields.show();
+            importFields.hide();
+        }
+    };
+    showFields();
+    operationType.on('change', function (e) {
+        showFields();
+    });
+});
