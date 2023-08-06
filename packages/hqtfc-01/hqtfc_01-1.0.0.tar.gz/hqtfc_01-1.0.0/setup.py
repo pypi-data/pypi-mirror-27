35e26fa49eb644678b7ba7c82470f16f773a0215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:44 +0000

    Add package hqtfc-01/1.0.0/hqtfc_01-1.0.0.tar.gz

---
+++ packages/hqtfc-01/hqtfc_01-1.0.0.tar.gz/hqtfc_01-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup  
+  
+setup(  
+    name = "hqtfc_01",  
+    version = "1.0.0",  
+    py_modules = ["hqtfc_01"],  
+    author = "hequan1230",  
+    author_email = "627956257@qq.com",  
+    url = "http://tfc",  
+    description = "just a sample of distribution",  
+    )  
