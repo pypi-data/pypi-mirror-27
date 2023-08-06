2126643e8b72ba871fc872e3a7f9e5b73c03af4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:50 +0000

    Add package drosolf/0.1.0/drosolf-0.1.0.tar.gz

---
+++ packages/drosolf/drosolf-0.1.0.tar.gz/drosolf-0.1.0/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup
+
+setup(
+    name='drosolf',
+    version='0.1.0',
+    packages=['drosolf'],
+    scripts=[],
+    install_requires=['numpy', 'pandas'],
+    # TODO where is it relative to exactly? '.'?
+    package_data={
+        'drosolf': ['Hallem_Carlson_2006.txt']
+    },
+    author="Tom O'Connell",
+    author_email='toconnel@caltech.edu',
+    license='GPLv3',
+    keywords='neuroscience olfaction population coding drosophila',
+    url='https://github.com/tom-f-oconnell/drosolf',
+    description='Responses of 1st , 2nd, and soon 3rd order Drosophila olfactory neurons',
+    long_description=open('README.rst').read(),
+)
+
