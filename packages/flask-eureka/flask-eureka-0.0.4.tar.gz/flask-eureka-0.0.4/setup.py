b207b695cdf92e25815a9b25c5c7f204fde52243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:25 +0000

    Add package flask-eureka/0.0.4/flask-eureka-0.0.4.tar.gz

---
+++ packages/flask-eureka/flask-eureka-0.0.4.tar.gz/flask-eureka-0.0.4/setup.py
@@ -0,0 +1,23 @@
+"""
+flask-eureka
+-------------
+flask extension that provides an interface to eureka via a flask.app
+"""
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-eureka',
+    version='0.0.4',
+    packages=find_packages(exclude=['tests*','examples*']),
+    include_package_data=True,
+    install_requires=['Flask','dnspython'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
