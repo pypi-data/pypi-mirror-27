3570e46e66f7f70cbcff3efb87b3adb4a3dea0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:58 +0000

    Add package e13tools/0.4.0a3/e13tools-0.4.0a3.tar.gz

---
+++ packages/e13tools/e13tools-0.4.0a3.tar.gz/e13tools-0.4.0a3/e13tools/sampling/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+Sampling
+======
+Provides a collection of functions and techniques useful in sampling problems.
+Recommended usage::
+
+    import e13tools.sampling as e13spl
+
+Available submodules
+--------------------
+lhcs
+    Provides a Latin Hypercube Sampling method.
+
+"""
+
+
+# %% IMPORTS
+from __future__ import division, absolute_import, print_function
+
+from . import lhcs
+from .lhcs import *
+
+__all__ = []
+__all__.extend(lhcs.__all__)
