7e45cacfabec3db71ac0c1cbd6914ef459f46adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:59 +0000

    Add package ratsoup/0.1/ratsoup-0.1.tar.gz

---
+++ packages/ratsoup/ratsoup-0.1.tar.gz/ratsoup-0.1/setup.py
@@ -0,0 +1,16 @@
+"""Rat soup package setup."""
+from setuptools import setup
+
+
+setup(
+    name='ratsoup',
+    packages=['ratsoup'],
+    version='0.1',
+    description='Make soup for the rats',
+    author='Paul Fablanod',
+    author_email='p.fablanod@gmail.com',
+    url='https://github.com/gabrielx52/ratsoup.git',
+    download_url='https://github.com/gabrielx52/ratsoup/archive/0.1.tar.gz',
+    keywords=['rat', 'rats', 'soup'],
+    classifiers=[],
+)
