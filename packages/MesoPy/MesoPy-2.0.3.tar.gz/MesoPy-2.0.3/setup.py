1de195b4743bf4738df41a300a15a290d4a97223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:03 +0000

    Add package MesoPy/2.0.3/MesoPy-2.0.3.tar.gz

---
+++ packages/MesoPy/MesoPy-2.0.3.tar.gz/MesoPy-2.0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='MesoPy',
+    py_modules=['MesoPy'],
+    version='2.0.3',
+    description='A Python wrapper for the MesoWest environmental data',
+    author='Mesowest/SynopticLabs',
+    author_email='help@synopticlabs.org',
+    url='https://github.com/mesowx/MesoPy',
+    download_url='https://github.com/mesowx/MesoPy/archive/2.0.3.tar.gz',
+    keywords=['weather API climate MesoWest meteorology'],
+    classifiers=[],
+    requires=[],
+)
