5d7c6214bbb34c136870a59c651326c8876b2282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:10 +0000

    Add package cmf-nester/1.1.2/cmf_nester-1.1.2.zip

---
+++ packages/cmf-nester/cmf_nester-1.1.2.zip/cmf_nester-1.1.2/cmf_nester.py
@@ -0,0 +1,12 @@
+""" This module in compounded by functiona developend on the second chapter of the book head first - Phyton"""
+
+def print_lol(the_list,level):
+        """ This function searches for elements in a nested list and print them
+		a second argumente is used to pusha tab whenever a nested list is found"""
+        for element in the_list:
+                if isinstance(element,list):
+                        print_lol(element,level+1)
+                else:
+                        for tab_stop in range(level):
+							print("\t",end='')
+						print(element)
