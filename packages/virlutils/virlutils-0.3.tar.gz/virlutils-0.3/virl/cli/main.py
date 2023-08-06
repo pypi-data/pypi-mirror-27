531b5d27c65ac853a512f9440ad623a33b59771a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:04 +0000

    Add package virlutils/0.3/virlutils-0.3.tar.gz

---
+++ packages/virlutils/virlutils-0.3.tar.gz/virlutils-0.3/virl/cli/main.py
@@ -0,0 +1,39 @@
+import click
+import os
+from subprocess import call
+from virl.api import VIRLServer
+
+from .console.commands import console
+from .nodes.commands import nodes
+from .logs.commands import logs
+from .up.commands import up
+from .down.commands import down
+from .ls.commands import ls
+from .save.commands import save
+from .telnet.commands import telnet
+from .generate import generate
+
+@click.group()
+def virl():
+    pass
+
+virl.add_command(console)
+virl.add_command(nodes)
+virl.add_command(logs)
+virl.add_command(up)
+virl.add_command(down)
+virl.add_command(ls)
+virl.add_command(save)
+virl.add_command(telnet)
+virl.add_command(generate)
+
+
+if __name__ == '__main__':
+    simengine_host = os.getenv("VIRL_HOST")
+    virl_user = os.getenv("VIRL_USER")
+    virl_password = os.getenv("VIRL_PASSWORD")
+
+    if not all([simengine_host, virl_user, virl_password]):
+        simengine_host = raw_input("Enter VIRL hostname/IP: ")
+
+    cli()
