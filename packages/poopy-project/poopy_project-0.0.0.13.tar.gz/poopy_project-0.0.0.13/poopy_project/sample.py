46a778648bb110e96013789718c7b6d64052325a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:47 +0000

    Add package poopy-project/0.0.0.13/poopy_project-0.0.0.13.tar.gz

---
+++ packages/poopy-project/poopy_project-0.0.0.13.tar.gz/poopy_project-0.0.0.13/poopy_project/sample.py
@@ -0,0 +1,7 @@
+"""mission critical application"""
+
+def count_the_poops(num):
+    """for the documenting of fecal matter"""
+    for i in range(num):
+        print "%s poops!  Ah! Ah! Ah!" % i
+    return True
---
+++ packages/poopy-project/poopy_project-0.0.0.13.tar.gz/poopy_project-0.0.0.13/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+setup(
+  name = 'poopy_project',
+  packages = ['poopy_project'],
+  version = '0.0.0.13',
+  description = 'I make poopy',
+  author = 'Winnie The Poo',
+  author_email = 'poo-delivery@hotmail.com',
+  keywords = ["poo", "slushy", "relief"],
+  classifiers = []
+)
