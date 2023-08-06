0e5f33d28007795f7c67bf967213979f49132425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:44 +0000

    Add package wcxf/0.1.2/wcxf-0.1.2.tar.gz

---
+++ packages/wcxf/wcxf-0.1.2.tar.gz/wcxf-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='wcxf',
+      version='0.1.2',
+      author='David M. Straub, Jason Aebischer',
+      author_email='david.straub@tum.de, jason.aebischer@tum.de',
+      license='MIT',
+      packages=['wcxf'],
+      package_data={
+      'wcxf':['data/*.yml']
+      },
+      install_requires=['pyyaml', 'ckmutil>=0.3', 'smeftrunner>=1.9'],
+      entry_points={
+        'console_scripts': [
+            'wcxf = wcxf.cli:wcxf_cli',
+            'wcxf2eos = wcxf.cli:eos',
+        ]
+      },
+    )
