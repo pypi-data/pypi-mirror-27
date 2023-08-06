98c903c88e7aefbf72f072fadabd36327d632d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:33 +0000

    Add package polyfil/0.2/polyfil-0.2.tar.gz

---
+++ packages/polyfil/polyfil-0.2.tar.gz/polyfil-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+  name = 'polyfil',
+  packages = ['polyfil'], # this must be the same as the name above
+  version = '0.2',
+  description = 'A random test lib',
+  author = 'laformarua',
+  author_email = 'lcrua@utp.edu.co',
+  url = 'https://github.com/laformarua/polyfill', # use the URL to the github repo
+  download_url = 'https://github.com/laformarua/polyfill/archive/tag/0.2.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+  install_requires=[
+   'opencv-python'
+  ]
+)
