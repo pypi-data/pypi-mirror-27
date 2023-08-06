ddd2413e8b59d29a023005aeb37684357067a955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:00 +0000

    Add package rapidpro_controller/0.4.1/rapidpro_controller-0.4.1.tar.gz

---
+++ packages/rapidpro_controller/rapidpro_controller-0.4.1.tar.gz/rapidpro_controller-0.4.1/rapidpro_controller/sms.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+import requests
+
+from rapidpro_controller import (SEND_SMS_URL, get_proxies)
+
+
+def send_sms_to(message, recipient):
+    payload = {
+        'from': '85855',
+        'to': recipient,
+        'text': message,
+    }
+    try:
+        req = requests.post(SEND_SMS_URL,
+                            params=payload,
+                            proxies=get_proxies())
+        return req.status_code == 200
+    except:
+        return False
