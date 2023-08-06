8e433439b5e6fbd5610df9609b99aca3f9d0377a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:51 +0000

    Add package skt7/0.2.1/skt7-0.2.1.tar.gz

---
+++ packages/skt7/skt7-0.2.1.tar.gz/skt7-0.2.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 17:13:44 2017
+
+@author: SKT
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='skt7',
+      version='0.2.1',
+      description='Library for api of all my python codes',
+      url='https://github.com/skt7/python-skt',
+      author='SKT',
+      author_email='developerskt@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['requests', 'matplotlib', 'numpy', 'opencv-python'])
