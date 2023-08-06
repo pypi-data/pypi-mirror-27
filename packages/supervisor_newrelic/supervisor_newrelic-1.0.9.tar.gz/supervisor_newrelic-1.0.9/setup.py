ce37b29a24ddb5602bfb6450623af8c394e53a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:47 +0000

    Add package supervisor_newrelic/1.0.9/supervisor_newrelic-1.0.9.tar.gz

---
+++ packages/supervisor_newrelic/supervisor_newrelic-1.0.9.tar.gz/supervisor_newrelic-1.0.9/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+version = '1.0.9'
+
+setup(
+    name = 'supervisor_newrelic',
+    version = version,
+    description = 'Collection of Supervisor plugins to provide metrics and monitoring within New Relic',
+    author = 'Sportlobster',
+    author_email = 'info@sportlobster.com',
+    license = 'MIT',
+    url = 'https://github.com/sportlobster/supervisor-newrelic',
+    download_url = 'https://github.com/sportlobster/supervisor-newrelic/tarball/%s' % version,
+    long_description = open('README.rst').read(),
+    keywords = ['supervisor', 'supervisord', 'newrelic', 'monitoring'],
+    classifiers = [],
+
+    install_requires = [
+        'requests>=2.4.2',
+        'setuptools>=38.0.0',
+        'supervisor>=3.3.3',
+    ],
+    tests_require = ['nose', 'mock'],
+    test_suite = 'nose.collector',
+
+    packages = ['supervisor_newrelic'],
+    entry_points = {
+        'console_scripts': {
+            'supervisor_newrelic_status = supervisor_newrelic.status:main',
+        },
+    },
+)
