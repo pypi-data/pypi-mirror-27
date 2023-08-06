920e6d5d2a5efcc1696078e1b4b00d8839df9466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:11 +0000

    Add package py-mysql/1.0/py_mysql-1.0.tar.gz

---
+++ packages/py-mysql/py_mysql-1.0.tar.gz/py_mysql-1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+requires = ['mysql-connector-python']
+
+setup(
+    name='py_mysql',
+    version='1.0',
+    description='Operating Mysql for Python.',
+    long_description=readme,
+    author='Takeki Shikano',
+    author_email='shikano.takeki@nexon.co.jp',
+    url=None,
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=requires,
+    entry_points={
+        'console_scripts': ['pysql=py_mysql.execute_queries:global_entry_point'
+        ],
+    },
+)
+
