43ac1c0c497b4b48976ce15a9f5698482c81949a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:51 +0000

    Add package EPAB/0.1.36/EPAB-0.1.36-py36-none-any.whl

---
+++ packages/EPAB/EPAB-0.1.36-py36-none-any.whl/epab/cmd/changelog.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+"""
+Updates CHANGELOG.rst with the latest commits
+"""
+
+import os
+import re
+
+import click
+
+from epab.utils import _info, do, ensure_exe, repo_commit, run_once
+
+
+@run_once
+def _write_changelog(ctx, auto_commit):
+    if ctx.obj['CONFIG'].get('disabled_changelog'):
+        _info('Skipping changelog update')
+    else:
+        ensure_exe('git')
+        ensure_exe('gitchangelog')
+        _info('Writing changelog')
+        changelog = do(ctx, ['gitchangelog'], mute_stdout=True)
+        with open('CHANGELOG.rst', mode='w') as stream:
+            stream.write(re.sub(r'(\s*\r\n){2,}', '\r\n', changelog))
+        if auto_commit:
+            files_to_add = ['CHANGELOG.rst']
+            msg = 'chg: dev: update changelog [auto]'
+            if os.getenv('APPVEYOR_REPO_BRANCH'):
+                msg = f'{msg} [skip ci]'
+            repo_commit(ctx, msg, files_to_add=files_to_add)
+
+
+@click.command()
+@click.pass_context
+@click.option('-c', '--auto-commit', is_flag=True, help='Commit the changes')
+def chglog(ctx, auto_commit):
+    """
+    Writes the changelog
+    """
+    _write_changelog(ctx, auto_commit)
