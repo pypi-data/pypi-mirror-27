c5940986e611da9e11a298b5a50bd5201f55a2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:29 +0000

    Add package lintpy/0.1/lintpy-0.1.tar.gz

---
+++ packages/lintpy/lintpy-0.1.tar.gz/lintpy-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='lintpy',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/abhinavralhan/lintpy',
+      author='abhinavralhan',
+      author_email='abhinav.ralhan1@gmail.com',
+      license='MIT',
+      packages=['lintpy'],
+      zip_safe=False)
