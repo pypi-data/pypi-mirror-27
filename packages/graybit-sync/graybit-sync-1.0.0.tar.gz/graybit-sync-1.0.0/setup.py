41ee77a666d9179b6cd9c8c724fe79a5c30bbfdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:18 +0000

    Add package graybit-sync/1.0.0/graybit-sync-1.0.0.tar.gz

---
+++ packages/graybit-sync/graybit-sync-1.0.0.tar.gz/graybit-sync-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='graybit-sync',
+      version='1.0.0',
+      description='Graybit blockchain sync module',
+      url='http://github.com/graybit/graybit-sync',
+      author='Graybit',
+      author_email='hash@graybit.org',
+      license='MIT',
+      packages=['graybit_sync'],
+      zip_safe=False)
