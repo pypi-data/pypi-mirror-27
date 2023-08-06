b3e9e0c8e41ef6fc79c76b1be602b2477aafb130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:50 +0000

    Add package udicOpenData/0.4/udicOpenData-0.4.tar.gz

---
+++ packages/udicOpenData/udicOpenData-0.4.tar.gz/udicOpenData-0.4/udicOpenData/stopwords/__init__.py
@@ -0,0 +1,17 @@
+from udicOpenData.dictionary import *
+import json, os, re
+DIR_NAME = os.path.dirname(os.path.abspath(__file__))
+
+STOPWORD_JSON = json.load(open(os.path.join(DIR_NAME, 'stopwords.json'), 'r'))
+
+def rmsw(doc):
+    def is_chinese(keyword):
+        for uchar in keyword:
+            if '\u4e00' <= uchar<='\u9fff':
+                continue
+            else:
+                return False
+        return True
+    doc = re.sub(r"[http|www]\S+", "", doc)
+    doc = doc.strip()
+    return (i for i in jieba.cut(doc) if i not in STOPWORD_JSON and is_chinese(i) and i not in ['\xa0', '\xc2'])
