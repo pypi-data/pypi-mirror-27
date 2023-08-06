ebab39b718dad283e5f3d896da2963f5abf0978d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:07 +0000

    Add package gastropy/0.0dev/gastropy-0.0dev.tar.gz

---
+++ packages/gastropy/gastropy-0.0dev.tar.gz/gastropy-0.0dev/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gastropy',
+      version='0.0dev',
+      description='(g)astronomy',
+      author='Brigitta Sipocz',
+      )
