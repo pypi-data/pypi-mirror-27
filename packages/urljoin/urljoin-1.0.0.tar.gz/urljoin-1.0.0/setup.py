30d8d1d492dfcac19cdb5393dbca53a958246859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:12 +0000

    Add package urljoin/1.0.0/urljoin-1.0.0.tar.gz

---
+++ packages/urljoin/urljoin-1.0.0.tar.gz/urljoin-1.0.0/setup.py
@@ -0,0 +1,33 @@
+#  -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='urljoin',
+    version='1.0.0',
+    description='join URL paths',
+    url='https://github.com/datasets-org/urljoin',
+    author='Vit Listik',
+    author_email='tivvit@seznam.cz',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    keywords="url path join",
+    packages=["urljoin"],
+)
