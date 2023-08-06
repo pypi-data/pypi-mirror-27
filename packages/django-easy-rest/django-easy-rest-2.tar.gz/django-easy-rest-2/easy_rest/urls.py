3fba25380470b8b070334597f1d47592dcb9c722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:36 +0000

    Add package django-easy-rest/2/django-easy-rest-2.tar.gz

---
+++ packages/django-easy-rest/django-easy-rest-2.tar.gz/django-easy-rest-2/easy_rest/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url
+from django.contrib.staticfiles.views import serve
+
+
+class ValidateLoading(object):
+    dependencies = [
+        'rest_framework',
+    ]
+
+    def validate_dependencies(self):
+        uninstalled = []
+        for dependency in self.dependencies:
+            try:
+                __import__(dependency)
+            except ImportError:
+                uninstalled += [dependency]
+        if uninstalled:
+            return ('\n!!!!! error required dependencies for easy_rest not installed\n'
+                    '{}, consider installing them by: '
+                    'pip\n').format(','.join(uninstalled))
+
+    def validate(self):
+        error = self.validate_dependencies()
+        if error:
+            print(error)
+
+
+ValidateLoading().validate()
+
+urlpatterns = [
+    url(r'^static/(?P<path>.*)$', serve),
+]
