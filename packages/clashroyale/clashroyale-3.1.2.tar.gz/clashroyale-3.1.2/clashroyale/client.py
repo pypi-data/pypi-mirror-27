b9eccc36608a3ff63d6d50c8284b1ee6d0de1ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:00 +0000

    Add package clashroyale/3.1.2/clashroyale-3.1.2.tar.gz

---
+++ packages/clashroyale/clashroyale-3.1.2.tar.gz/clashroyale-3.1.2/clashroyale/client.py
@@ -0,0 +1,225 @@
+'''
+MIT License
+
+Copyright (c) 2017 kyb3r
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+import asyncio
+import json
+from urllib.parse import urlencode
+from datetime import datetime
+
+import aiohttp
+import requests
+
+from .models import Player, Clan, PlayerInfo, ClanInfo, Constants, Tournament
+from .errors import NotFoundError, ServerError, NotResponding, Unauthorized
+from .utils import Endpoints, typecasted, crtag, clansearch, SqliteDict
+
+
+
+class Client:
+    '''Represents an (a)sync client connection to cr-api.com
+
+    Parameters
+    ----------
+    token: str
+        The api authorization token to be used for requests.
+    is_async: bool
+        Toggle for asynchronous/synchronous usage of the client.
+        Defaults to False
+    session: (requests.Session, aiohttp.ClientSession)
+        The http session to be used for requests
+    timeout: int
+        A timeout for requests to the API, defaults to 10 seconds.
+    camel_case: bool
+        Whether or not to access keys in snake_case or camelCase
+    '''
+
+    def __init__(self, token, session=None, is_async=False, **options):
+        self.token = token
+        self.is_async = is_async
+        self.timeout = options.get('timeout', 10)
+        self.session = session or (aiohttp.ClientSession() if is_async else requests.Session())
+        self.camel_case = options.get('camel_case', False)
+        self.headers = {
+            'auth': token,
+            'user-agent': 'python-clashroyale (kyb3r)'
+            }
+        self.cache_fp = options.get('cache_fp')
+        self.using_cache = bool(self.cache_fp)
+        self.cache_reset = options.get('cache_expires', 300)
+        if self.using_cache:
+            table = options.get('table_name')
+            self.cache = SqliteDict(self.cache_fp, table)
+
+    def _resolve_cache(self, url, **params):
+        bucket = url + (('?' + urlencode(params)) if params else '')
+        cached_data = self.cache.get(bucket)
+        if not cached_data:
+            return None
+        prev = datetime.fromtimestamp(cached_data['timestamp'])
+        if (datetime.utcnow() - prev).total_seconds() < self.cache_reset:
+            if self.is_async:
+                return self._do_nothing(cached_data['data'])
+            return cached_data['data']
+        return None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.session.close()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        self.session.close()
+    
+    def __repr__(self):
+        return f'<ClashRoyaleClient async={self.is_async}>'
+
+    def close(self):
+        self.session.close()
+    
+    def _raise_for_status(self, resp, text):
+        try:
+            data = json.loads(text)
+        except:
+            data = text
+        code = getattr(resp, 'status', None) or getattr(resp, 'status_code')
+        if 300 > code >= 200: # Request was successful
+            if self.using_cache:
+                cached_data = {
+                    'timestamp': datetime.utcnow().timestamp(),
+                    'data': data
+                }
+                self.cache[str(resp.url)] = cached_data
+            return data
+        if code == 401: # Unauthorized request - Invalid token
+            raise Unauthorized(resp, data) 
+        if code == 404: # Tag not found
+            raise NotFoundError(resp, data)
+        if code > 500: # Something wrong with the api servers :(
+            raise ServerError(resp, data)
+
+    async def _arequest(self, url, **params):
+        try:
+            async with self.session.get(url, timeout=self.timeout, headers=self.headers, params=params) as resp:
+                return self._raise_for_status(resp, await resp.text())
+        except asyncio.TimeoutError:
+            raise NotResponding()
+    
+    async def _do_nothing(self, arg):
+        return arg
+    
+    def request(self, url, **params):
+        if self.using_cache:
+            cache = self._resolve_cache(url, **params)
+            if cache is not None:
+                return cache
+        if self.is_async:
+            return self._arequest(url, **params)
+        try:
+            resp = self.session.get(url, timeout=self.timeout, headers=self.headers, params=params)
+            return self._raise_for_status(resp, resp.text)
+        except requests.Timeout:
+            raise NotResponding()
+
+    async def _aget_model(self, url, model, **params):
+        try:
+            data = await self.request(url, **params)
+        except Exception as e:
+            if self.using_cache:
+                data = self._resolve_cache(url, **params)
+            if 'data' not in locals():
+                raise e
+
+        if isinstance(data, list):
+            return [model(self, c) for c in data]
+        else:
+            return model(self, data)
+
+    def _get_model(self, url, model, **params):
+        if self.is_async:
+            return self._aget_model(url, model, **params)
+
+        try:
+            data = self.request(url, **params)
+        except Exception as e:
+            if self.using_cache:
+                data = self._resolve_cache(url, **params)
+            if 'data' not in locals():
+                raise e
+
+        if isinstance(data, list):
+            return [model(self, c) for c in data]
+        else:
+            return model(self, data)
+
+    @typecasted()
+    def get_tournament(self, tag: crtag):
+        url = Endpoints.TOURNAMENT + '/' + tag
+        return self._get_model(url, Tournament)
+    
+    @typecasted()
+    def get_player(self, *tags: crtag):
+        url = Endpoints.PLAYER + '/' + ','.join(tags)
+        return self._get_model(url, Player)
+
+    get_players = get_player
+    
+    @typecasted()
+    def get_clan(self, *tags: crtag):
+        url = Endpoints.CLAN + '/' + ','.join(tags)
+        return self._get_model(url, Clan)
+
+    get_clans = get_clan
+
+    @typecasted()
+    def search_clans(self, **params: clansearch):
+        return self._get_model(Endpoints.SEARCH, ClanInfo, **params)
+
+    def get_constants(self):
+        return self._get_model(Endpoints.CONSTANTS, Constants)
+
+    def get_version(self):
+        return self.request(Endpoints.VERSION)
+
+    def get_endpoints(self):
+        return self.request(Endpoints.ENDPOINTS)
+
+    def get_top_clans(self, country_key=None):
+        url = Endpoints.TOP + '/clans/' + (country_key or '')
+        return self._get_model(url, ClanInfo)
+
+    def get_top_players(self, country_key=None):
+        url = Endpoints.TOP + '/players/' + (country_key or '')
+        return self._get_model(url, PlayerInfo)
+
+    def get_popular_clans(self):
+        url = Endpoints.POPULAR + '/clans'
+        return self._get_model(url, Clan)
+
+    def get_popular_players(self):
+        url = Endpoints.POPULAR + '/players'
+        return self._get_model(url, PlayerInfo)
---
+++ packages/clashroyale/clashroyale-3.1.2.tar.gz/clashroyale-3.1.2/clashroyale/models.py
@@ -0,0 +1,133 @@
+'''
+MIT License
+
+Copyright (c) 2017 kyb3r
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+from box import Box, BoxList
+from .utils import _to_snake_case, Endpoints
+
+class BaseAttrDict:
+    '''Uses a python-box for data storage, this class 
+    is used as a base class so we can easily add extra 
+    methods without interfering with the box itself.
+    '''
+    def __init__(self, client, data):
+        self.client = client
+        self.from_data(data)
+        
+    def from_data(self, data):
+        self.raw_data = data
+        self._boxed_data = Box(
+            data, camel_killer_box=not self.client.camel_case
+            )
+        return self
+
+    def __getattr__(self, attr):
+        try:
+            return getattr(self._boxed_data, attr)
+        except AttributeError:
+            return super().__getattr__(attr)
+
+    def __repr__(self):
+        _type = self.__class__.__name__
+        return f'<{_type}: {self}>'
+
+    def __str__(self):
+        try:
+            return f'{self.name} (#{self.tag})'
+        except AttributeError:
+            return super().__str__()
+    
+class FullClan:
+    def get_clan(self):
+        '''(a)sync function to return clan.'''
+        if not self.clan.get('tag'):
+            raise ValueError('This player does not have a clan.')
+        return self.client.get_clan(self.clan.tag)
+
+class FullPlayer:
+    def get_profile(self):
+        return self.client.get_player(self.tag)
+
+    get_player = get_profile # consistancy
+
+class Refreshable:
+    '''Mixin class for re requesting data from 
+    the api for the specific model.
+    '''
+    def refresh(self):
+        '''(a)sync refresh the data.'''
+        client = self.client
+        if client.is_async:
+            return self._arefresh()
+        else:
+            data = client.request(self.url)
+            return self.from_data(data)
+
+    async def _arefresh(self):
+        client = self.client
+        data = await client.request(self.url)
+        return self.from_data(data)
+
+    @property
+    def url(self):
+        endpoint = self.__class__.__name__.lower()
+        return f'{Endpoints.BASE}/{endpoint}/{self.tag}'
+
+class Player(BaseAttrDict, Refreshable, FullClan):
+    '''A clash royale player model.'''
+    pass
+
+class Member(BaseAttrDict, FullPlayer):
+    '''A clan member model, 
+    keeps a reference to the clan object it came from.
+    '''
+    def __init__(self, clan, data):
+        self.clan = clan
+        super().__init__(clan.client, data)
+
+class PlayerInfo(BaseAttrDict, FullClan, FullPlayer):
+    '''Brief player model, 
+    does not contain full data, non refreshable.
+    '''
+    pass
+
+class ClanInfo(BaseAttrDict, FullClan):
+    '''Brief clan model, 
+    does not contain full data, non refreshable.
+    '''
+    pass
+
+class Clan(BaseAttrDict, Refreshable):
+    '''A clash royale clan model, full data + refreshable.
+    '''
+    def from_data(self, data):
+        super().from_data(data)
+        self.members = [Member(self, m) for m in data['members']]
+
+class Constants(BaseAttrDict, Refreshable):
+    '''Clash Royale constants storage'''
+    pass
+
+class Tournament(BaseAttrDict, Refreshable):
+    '''Represents a clash royale tournament.'''
+    pass
