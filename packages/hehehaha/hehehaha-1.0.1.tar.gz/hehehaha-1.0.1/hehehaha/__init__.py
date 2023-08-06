ece47af9323ce17c832a08fc3899bce8227a2339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:58 +0000

    Add package hehehaha/1.0.1/hehehaha-1.0.1.tar.gz

---
+++ packages/hehehaha/hehehaha-1.0.1.tar.gz/hehehaha-1.0.1/hehehaha/__init__.py
@@ -0,0 +1,6 @@
+from .print_cwd import print_cwd
+from .print_now import print_now
+
+__version__ = '1.0.1'
+
+__all_ = ['print_cwd', 'print_now']
---
+++ packages/hehehaha/hehehaha-1.0.1.tar.gz/hehehaha-1.0.1/hehehaha/print_now.py
@@ -0,0 +1,5 @@
+import time
+
+
+def print_now():
+    print(time.time())
