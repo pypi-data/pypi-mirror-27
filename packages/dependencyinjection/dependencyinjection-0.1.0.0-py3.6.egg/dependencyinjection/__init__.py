121e25f4855e695f21c0fda1080d3c3074c42108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:51 +0000

    Add package dependencyinjection/0.1.0.0/dependencyinjection-0.1.0.0-py3.6.egg

---
+++ packages/dependencyinjection/dependencyinjection-0.1.0.0-py3.6.egg/dependencyinjection/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from .internal.services import Services
+
+
+__all__ = [
+    'Services'
+]
