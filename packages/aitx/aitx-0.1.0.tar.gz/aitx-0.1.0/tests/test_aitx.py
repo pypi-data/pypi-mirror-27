1cab6580c867ffe5b4b5a03a510d1a15f4160b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:26 +0000

    Add package aitx/0.1.0/aitx-0.1.0.tar.gz

---
+++ packages/aitx/aitx-0.1.0.tar.gz/aitx-0.1.0/tests/test_aitx.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+"""
+test_aitx
+----------------------------------
+
+Tests for `aitx` module.
+"""
+
+import unittest
+
+from aitx import aitx
+
+
+class TestAitx(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
