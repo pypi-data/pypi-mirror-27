d5dae810336533cd214a0c6a707a80919777424a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:27 +0000

    Add package asyncorm/0.3.5/asyncorm-0.3.5.tar.gz

---
+++ packages/asyncorm/asyncorm-0.3.5.tar.gz/asyncorm-0.3.5/asyncorm/exceptions.py
@@ -0,0 +1,59 @@
+__all__ = (
+    'AsyncormException', 'FieldError', 'ModelDoesNotExist',
+    'ModelError', 'ModuleError', 'MultipleObjectsReturned', 'QuerysetError',
+    'SerializerError', 'ConfigError', 'CommandError', 'MigrationError'
+)
+
+
+class AsyncormException(Exception):
+    pass
+
+
+class CommandError(AsyncormException):
+    '''Exceptions to be raised when command errors'''
+    pass
+
+
+class FieldError(AsyncormException):
+    '''to be raised when there are field errors detected'''
+    pass
+
+
+class ModelError(AsyncormException):
+    '''to be raised when there are model errors detected'''
+    pass
+
+
+class QuerysetError(AsyncormException):
+    '''to be raised when there are queryset errors detected'''
+    pass
+
+
+class ConfigError(AsyncormException):
+    '''to be raised when there are configuration errors detected'''
+    pass
+
+
+class MigrationError(AsyncormException):
+    '''to be raised when there are configuration errors detected'''
+    pass
+
+
+class ModelDoesNotExist(AsyncormException):
+    '''to be raised when there are model errors detected'''
+    pass
+
+
+class MultipleObjectsReturned(AsyncormException):
+    '''to be raised when there are model errors detected'''
+    pass
+
+
+class SerializerError(AsyncormException):
+    '''to be raised when there are model errors detected'''
+    pass
+
+
+class ModuleError(AsyncormException):
+    '''to be raised when there are model module or config errors detected'''
+    pass
