1c808f5e6d88d1b9e00c7329001745447e8290dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:08 +0000

    Add package sample-sheet/0.0.2/sample_sheet-0.0.2.tar.gz

---
+++ packages/sample-sheet/sample_sheet-0.0.2.tar.gz/sample_sheet-0.0.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+setup(
+    name='sample_sheet',
+    packages=['sample_sheet'],
+    version='0.0.2',
+    description='An Illumina Sample Sheet parsing utility.',
+    long_description=open('README.rst').read().strip(),
+    author='clintval',
+    author_email='valentine.clint@gmail.com',
+    url='https://github.com/clintval/sample-sheet',
+    download_url='https://github.com/clintval/sample-sheet/archive/v0.0.2.tar.gz',  # noqa
+    py_modules=['sample_sheet'],
+    install_requires=[
+        'click',
+        'smart_open',
+        'tabulate',
+        'terminaltables',
+    ],
+    extras_require={
+        'test': ['nose', 'nose-progressive', 'coverage'],
+    },
+    scripts=[
+        'scripts/csv-whitespace-strip',
+        'scripts/sample-sheet-summary',
+    ],
+    license='MIT',
+    zip_safe=True,
+    keywords='illumina samplesheet sample sheet parser bioinformatics',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
