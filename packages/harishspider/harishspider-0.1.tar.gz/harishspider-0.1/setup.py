00d0e2618f5a6dca2645076f76cdf7d738416b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:51 +0000

    Add package harishspider/0.1/harishspider-0.1.tar.gz

---
+++ packages/harishspider/harishspider-0.1.tar.gz/harishspider-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='harishspider',
+	version='0.1',
+	description="""Harish Spider will enter your domain and scan every page of your website, extracting all emails in that page.""",
+	url='http://github.com/harish121436',
+	author='Vadde Harish',
+	author_email='mahadevendra7@gmail.com',
+	license='HarishVadde',
+	packages=['harishspider'],
+	zip_safe=False)
