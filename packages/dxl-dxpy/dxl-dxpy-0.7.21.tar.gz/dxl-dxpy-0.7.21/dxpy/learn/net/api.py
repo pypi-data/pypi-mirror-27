d4843c5c6dbc5c4673212df44dc572e734dd9e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:04 +0000

    Add package dxl-dxpy/0.7.21/dxl-dxpy-0.7.21.tar.gz

---
+++ packages/dxl-dxpy/dxl-dxpy-0.7.21.tar.gz/dxl-dxpy-0.7.21/dxpy/learn/net/api.py
@@ -0,0 +1,29 @@
+from dxpy.configs import configurable
+from dxpy.learn.config import config
+from dxpy.core.path import Path
+
+
+@configurable(config, with_name=True)
+def get_network(name, dataset, network_cls_name, network_name=None, **kw):
+    from .base import NodeKeys, Graph
+    if network_name is None:
+        network_name = name
+    if network_cls_name == 'srms':
+        from .zoo.srms import SRMultiScale
+        if not isinstance(dataset, (dict, Graph)):
+            raise TypeError('srms network required dataset, got {}.'.format(dataset))
+        # valid_keys = ['input/image{}'.format(2**i) for i in range(10)]
+        # valid_keys += ['label/image{}'.format(2**i) for i in range(10)]
+        # inputs = dict()
+        inputs = dataset
+        # for k in valid_keys:
+        # if k in dataset.nodes:
+        # inputs[k] = dataset[k]
+        return SRMultiScale(name=network_name, inputs=inputs, **kw)
+    elif network_cls_name == 'sin':
+        from .zoo.sin import SinNet
+        return SinNet(name=name, inputs={NodeKeys.INPUT: dataset['x'], NodeKeys.LABEL: dataset['y']}, **kw)
+
+    else:
+        raise ValueError(
+            'Unknown network name (class name) {}.'.format(dataset_cls_name))
---
+++ packages/dxl-dxpy/dxl-dxpy-0.7.21.tar.gz/dxl-dxpy-0.7.21/dxpy/learn/run/train.py
@@ -0,0 +1,54 @@
+from pprint import pprint
+
+import numpy as np
+import tensorflow as tf
+from dxpy.learn.session import Session
+from dxpy.learn.train.summary import SummaryWriter
+from dxpy.learn.utils.general import pre_work
+from tqdm import tqdm
+from dxpy.learn.config import config
+import yaml
+from dxpy.configs import configurable, ConfigsView
+
+
+@configurable(ConfigsView(config).get('train'))
+def get_train_configs(summary_freq=1000, save_freq=10000, steps=100000000):
+    return {'summary_freq': summary_freq,
+            'save_freq': save_freq,
+            'steps': steps}
+
+
+def train(definition_func):
+    with open('dxln.yml') as fin:
+        ycfg = yaml.load(fin)
+    config.update(ycfg)
+    pre_work()
+    train_cfgs = get_train_configs()
+    steps = train_cfgs['steps']
+    summary_freq = train_cfgs['summary_freq']
+    save_freq = train_cfgs['save_freq']
+    network, summary = definition_func(ycfg)
+    session = Session()
+    with session.as_default():
+        network.post_session_created()
+        summary.post_session_created()
+        session.post_session_created()
+
+    with session.as_default():
+        network.load()
+        for i in tqdm(range(steps)):
+            network.train()
+            if i % summary_freq == 0 and i > 0:
+                summary.summary()
+                summary.flush()
+            if i % save_freq == 0 and i > 0:
+                network.save()
+
+    with session.as_default():
+        network.save()
+        
+
+def train_dist(definition_func):
+    from dxpy.learn.utils.general import load_yaml_config
+    load_yaml_config('dxln.yml')
+    
