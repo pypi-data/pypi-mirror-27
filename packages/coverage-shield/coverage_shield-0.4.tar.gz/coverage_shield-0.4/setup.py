9844d7a3dede95833e27913a2d59f437811f2958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:01 +0000

    Add package coverage-shield/0.4/coverage_shield-0.4.tar.gz

---
+++ packages/coverage-shield/coverage_shield-0.4.tar.gz/coverage_shield-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='coverage_shield',
+    packages=['coverage_shield'],
+    version='0.4',
+    description='Uploads total coverage for displaying badge',
+    install_requires=['coverage'],
+    author='Samuel Carlsson',
+    author_email='samuel.carlsson@volumental.com',
+    url='https://github.com/Volumental/badges',
+    keywords=['coverage', 'badge', 'shields.io'],
+)
