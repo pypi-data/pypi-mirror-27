a001866d3c94f7dc80db1749d31821f279efc5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:30 +0000

    Add package aollio/0.0.1/aollio-0.0.1.tar.gz

---
+++ packages/aollio/aollio-0.0.1.tar.gz/aollio-0.0.1/aollio/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+__author__ = 'Aollio Hou'
+__email__ = 'aollio-project@outlook.com'
+
+from aollio.fileinput import FileInput
+from aollio.fileinput import input
+
+
+def hello():
+    print('Hello, This is aollio-project Python module')
