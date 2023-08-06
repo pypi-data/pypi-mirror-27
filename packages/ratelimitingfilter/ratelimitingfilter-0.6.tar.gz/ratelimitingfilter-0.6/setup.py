03f5a7ccd6dbbc057ea5e5d90933e369a9704609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:41 +0000

    Add package ratelimitingfilter/0.6/ratelimitingfilter-0.6.tar.gz

---
+++ packages/ratelimitingfilter/ratelimitingfilter-0.6.tar.gz/ratelimitingfilter-0.6/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='ratelimitingfilter',
+      version='0.6',
+      description='A rate limiting filter for the Python logging system',
+      long_description=open('README.rst').read(),
+      url='https://github.com/wkeeling/ratelimitingfilter',
+      download_url='https://github.com/wkeeling/ratelimitingfilter/archive/0.6.tar.gz',
+      author='Will Keeling',
+      author_email='will@zifferent.com',
+      maintainer='Will Keeling',
+      maintainer_email='will@zifferent.com',
+      license='MIT',
+      packages=['ratelimitingfilter'],
+      keywords='logging filter SMTPHandler ratelimit throughput',
+      tests_require=['mock==2.0'])
