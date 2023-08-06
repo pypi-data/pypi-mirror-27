f8498d40f4231e1b75f48702dbe1dc9ab7050260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:01 +0000

    Add package beautifullist/1.0.2/beautifullist-1.0.2.tar.gz

---
+++ packages/beautifullist/beautifullist-1.0.2.tar.gz/beautifullist-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='beautifullist',
+    version='1.0.2',
+    py_modules=['prettylist'],
+    author='lovetocode',
+    author_email='yogesh.chandra.iitbhu@gmail.com',
+    description='pretty_list() function print the nested list in beautiful pattern',
+)
