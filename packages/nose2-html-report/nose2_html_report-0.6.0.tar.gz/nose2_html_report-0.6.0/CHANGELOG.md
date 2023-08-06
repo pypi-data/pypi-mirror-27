10fd879e6e568cd5a8d60e96b1e3db6755187f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:02 +0000

    Add package nose2-html-report/0.6.0/nose2_html_report-0.6.0.tar.gz

---
+++ packages/nose2-html-report/nose2_html_report-0.6.0.tar.gz/nose2_html_report-0.6.0/CHANGELOG.md
@@ -0,0 +1,20 @@
+# Change Log
+
+## [0.6.0] - 2017-12-16
+- [#5](https://github.com/mgrijalva/nose2-html-report/pull/5) add metadata to template context ([artragis](https://github.com/artragis))
+
+## [0.5.0] - 2017-08-05
+- Fix typo in documentation. `report_path` should have been `path`.
+
+## [0.4.0] - 2017-05-11
+- Fixed issue where tracebacks for error'ed tests were not captured
+
+## [0.3.0] - 2017-02-03
+- Updated packaging
+
+## [0.2.0] - 2017-01-30
+- Include Jinja2 template in distribution
+
+## [0.1.0] - 2017-01-30
+- Initial release
+
---
+++ packages/nose2-html-report/nose2_html_report-0.6.0.tar.gz/nose2_html_report-0.6.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='nose2_html_report',
+    packages=['nose2_html_report'],
+    version='0.6.0',
+    description='Generate an HTML report for your unit tests',
+    long_description=open('README.md', 'r').read(),
+    author='Michael Grijalva',
+    author_email='michaelgrij@gmail.com',
+    license='MIT',
+    install_requires=['jinja2'],
+    package_data={
+        'nose2_html_report': ['templates/report.html']
+    },
+    url='https://github.com/mgrijalva/nose2-html-report',
+    download_url='',
+    keywords=['nose2', 'testing', 'reporting'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
