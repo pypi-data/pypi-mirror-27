edf7a6827596aee538890c865782d9344e3cdfd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:32 +0000

    Add package password-strength-check-bate0.1/0.2/password_strength_check_bate0.1-0.2.tar.gz

---
+++ packages/password-strength-check-bate0.1/password_strength_check_bate0.1-0.2.tar.gz/password_strength_check_bate0.1-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+        name         = 'password_strength_check_bate0.1',
+        version      = '0.2',
+        py_modules   = ['password_strength_check'],
+        author       = 'johnson',
+        author_email = '453713729@qq.com',
+        url          = 'https://github.com/johnson2017github/Sec/tree/master',
+        description  = 'password_strength_check',
+    )
+
