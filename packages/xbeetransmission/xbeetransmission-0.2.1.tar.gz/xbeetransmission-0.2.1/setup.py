c3242767cee7918c80af650c64c2e838be3bee38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:12 +0000

    Add package xbeetransmission/0.2.1/xbeetransmission-0.2.1.tar.gz

---
+++ packages/xbeetransmission/xbeetransmission-0.2.1.tar.gz/xbeetransmission-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from os import path
+
+f = open('README', 'r')
+long_description = f.read()
+
+setup(
+    name='xbeetransmission',
+    version='0.2.1',
+    packages=['xbeetransmission'],
+    description='xbeetransmission is a python module is designed to reliably and securely send messages over Xbee modules in out-of-the-box configuration.',
+    long_description=long_description,
+    url='https://github.com/KyleEvers/xbeetransmission',
+    author='Kyle Evers',
+    author_email='kyleevers14@yahoo.com',
+    license='MIT',
+    keywords='xbee'
+)
