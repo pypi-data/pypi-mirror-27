808a75d4381b783f137982b3523097f8573d91ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:24 +0000

    Add package simMachines/1.0.1/simMachines-1.0.1.tar.gz

---
+++ packages/simMachines/simMachines-1.0.1.tar.gz/simMachines-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+version = '1.0.1'
+
+setup(name='simMachines',
+      scripts = ['simMachines.py'],
+      version=version,
+      description="A library to use simMachines, Inc. API",
+      python_requires='>=3',
+      author = 'Rick Palmer',
+      author_email = 'rickpalmer@simmachines.com'
+      )
