9b8c91cecc66477f772eaea72a63ca1c0374e5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:24 +0000

    Add package jh-stackformation/0.1.3/jh-stackformation-0.1.3.tar.gz

---
+++ packages/jh-stackformation/jh-stackformation-0.1.3.tar.gz/jh-stackformation-0.1.3/README.rst
@@ -0,0 +1,43 @@
+==============
+StackFormation
+==============
+
+
+.. image:: https://img.shields.io/pypi/v/jh-stackformation.svg
+        :target: https://pypi.python.org/pypi/jh-stackformation
+
+.. image:: https://img.shields.io/travis/ibejohn818/stackformation.svg
+        :target: https://travis-ci.org/ibejohn818/stackformation
+
+.. image:: https://readthedocs.org/projects/stackformation/badge/?version=latest
+        :target: https://stackformation.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/ibejohn818/stackformation/shield.svg
+     :target: https://pyup.io/repos/github/ibejohn818/stackformation/
+     :alt: Updates
+
+
+Stackfromation is an AWS CloudFormation framework that allows you to enforce "Infrastructure-as-code".
+It uses a convention to allow you to manage your stack-resources as python objects.
+It does not bind stacks together using Import and allows for more predictable updates to existing stacks
+and quick prototyping of your infrastructure to different stages IE: DEV, STAGING & PRODUCTION.
+
+Optionally, it also enforces an AMI workflow that separates the construction and updating of your AMI's
+using ansible and packer.
+
+
+Installation
+============
+
+pip install jh-stackformation
+
+* Free software: MIT license
+* Documentation: https://stackformation.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
---
+++ packages/jh-stackformation/jh-stackformation-0.1.3.tar.gz/jh-stackformation-0.1.3/stackformation/aws/ami.py
@@ -0,0 +1,142 @@
+from stackformation import (StackComponent, PackerImage)
+import stackformation
+import json
+import os
+import subprocess
+import yaml
+
+
+CWD = os.path.realpath(os.path.dirname(stackformation.__file__)).strip('/')
+
+AMI_INFO={
+    'ubuntu': {
+        'username': 'ubuntu',
+        'ami_filters': [
+            {
+                'Name': 'name',
+                'Values': ['ubuntu*16*']
+            },
+        ]
+    },
+    'aws': {
+        'username': 'ec2-user',
+        'ami_filters': [
+            {
+                'Name': 'name',
+                'Values': ['amzn-ami*x86_64-gp2']
+            },
+            {
+                'Name': 'description',
+                'Values': ["*Linux*"]
+            },
+        ]
+    }
+}
+
+class BaseAmi(StackComponent):
+
+    def __init__(self, name, os_type='aws'):
+
+        super(BaseAmi, self).__init__(name)
+        self.boto_session = None
+        self.os_type = os_type
+
+
+
+
+
+class Ami(PackerImage):
+
+    def __init__(self, name, os_type='aws'):
+
+        super(Ami, self).__init__(name)
+        self.os_type = os_type
+
+
+
+    def get_base_ami(self):
+        """
+        Get the latest AWS Linux AMI based on
+        the creation date
+
+        Args:
+            botoKwargs (dict): the boto3 client kwargs
+
+        Returns:
+            amiid (str): The AWS Linux AmiID
+        """
+
+        filters = [
+                {
+                    'Name': 'architecture',
+                    'Values': ["x86_64"]
+                },
+                {
+                    'Name': 'root-device-type',
+                    'Values': ["ebs"]
+                },
+                {
+                    'Name': 'virtualization-type',
+                    'Values': ["hvm"]
+                },
+                {
+                    'Name': 'state',
+                    'Values': ['available']
+                },
+                {
+                    'Name': 'ena-support',
+                    'Values': ['true']
+                },
+                {
+                    'Name': 'image-type',
+                    'Values': ['machine']
+                },
+                {
+                    'Name': 'is-public',
+                    'Values': ['true']
+                },
+            ]
+
+        filters.extend(AMI_INFO[self.os_type]['ami_filters'])
+
+        amis_query = self.boto_session.client("ec2").describe_images(Filters=filters)
+
+        # sort by CreationDate
+        amis_query['Images'].sort(
+            key=lambda item: item['CreationDate'],
+            reverse=True)
+
+        return amis_query['Images'][0]['ImageId']
+
+    def build(self):
+
+        d = [
+            {
+                'roles':[
+                    json.dumps({'Role': 'php-56', 'var':'var', 'var1':'var1'}),
+                    json.dumps({'Role': 'php-56', 'var':'var', 'var1':'var1'})
+                ]
+            }
+        ]
+        print(d)
+
+
+        with open("test.yaml", "w") as yml:
+            yaml.dump(d, yml, default_flow_style=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
