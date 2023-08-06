4d0113d0719998ec55339b4b7a760e2d76ca3eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:09 +0000

    Add package Facebook_PyBot/0.8b4/Facebook_PyBot-0.8b4.tar.gz

---
+++ packages/Facebook_PyBot/Facebook_PyBot-0.8b4.tar.gz/Facebook_PyBot-0.8b4/readme.md
@@ -0,0 +1,37 @@
+Readme:
+
+# Project Title
+
+Facebook_PyBot
+
+## Description
+This is an Unofficial Facebook bot API in python. Facebook Bots can be build using this library.
+
+
+## Installing
+Using pip:
+
+
+	pip install Facebook_PyBot
+
+Or install from the source with:
+
+
+	$ git clone https://github.com/hundredeir/Facebook_PyBot
+	$ cd Facebook_Pybot
+	$ python setup.py install
+
+## Wiki
+https://github.com/hundredeir/Facebook_PyBot/wiki/Getting-Started
+
+### Docs
+
+https://facebook-pybot.readthedocs.io/en/latest/
+
+
+Note: 
+1. This wrapper is still in beta. there are yet features missing and bugs may occur. Please report any issues or features requests 
+2. It is higly recommended to fist read the documentation here https://developers.facebook.com/docs/messenger-platform
+
+A wiki is in work in progress
+
---
+++ packages/Facebook_PyBot/Facebook_PyBot-0.8b4.tar.gz/Facebook_PyBot-0.8b4/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+
+def requirements():
+	req_list=[]
+	#List of requirements for this project
+	with open("requirements.txt") as req:
+		for each in req:
+			req_list.append(each.strip())
+
+	return req_list
+
+
+setup(
+    name='Facebook_PyBot',
+    version='0.8b4',
+    author='hundredeir',
+    author_email='hundredeir@protonmail.com',
+    packages=['Facebook'],
+    scripts=['examples/echo.py','examples/userInfo.py'],
+    url='https://github.com/hundredeir/Facebook_PyBot',
+    license='GPL3',
+    description='This is an Unofficial Facebook bot API in python. Facebook Bots can be build using this library.',
+    long_description=open('readme.md').read(),
+    install_requires=requirements(),
+    extras_require={'UltraJSON':["ujson"]},
+    keywords="Facebook Bot API Wrapper Python",
+    classifiers=[
+              'Development Status :: 4 - Beta',
+              'Intended Audience :: Developers',
+              'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+              'Operating System :: OS Independent',
+              'Topic :: Software Development :: Libraries :: Python Modules',
+              'Topic :: Communications :: Chat',
+              'Topic :: Internet',
+              'Programming Language :: Python',
+          ],
+)
