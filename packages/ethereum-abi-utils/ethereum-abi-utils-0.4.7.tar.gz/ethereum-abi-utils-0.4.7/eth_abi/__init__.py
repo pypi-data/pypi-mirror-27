805c98994488e7de2c2104b16c297cbb7dab1357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:57 +0000

    Add package ethereum-abi-utils/0.4.7/ethereum-abi-utils-0.4.7.tar.gz

---
+++ packages/ethereum-abi-utils/ethereum-abi-utils-0.4.7.tar.gz/ethereum-abi-utils-0.4.7/eth_abi/__init__.py
@@ -0,0 +1,5 @@
+raise DeprecationWarning(
+    "This library has been renamed to `eth-abi`.  The `ethereum-abi-utils` "
+    "package will no longer recieve updates.  Please update your dependencies "
+    "accordingly."
+)
