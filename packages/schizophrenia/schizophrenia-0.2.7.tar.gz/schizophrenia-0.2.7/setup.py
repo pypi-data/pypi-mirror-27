b1bc6ed463962b50bff7339fd0b6cf57ff05cf6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:38 +0000

    Add package schizophrenia/0.2.7/schizophrenia-0.2.7.tar.gz

---
+++ packages/schizophrenia/schizophrenia-0.2.7.tar.gz/schizophrenia-0.2.7/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'schizophrenia'
+    ,version = '0.2.7'
+    ,author = 'frank2'
+    ,author_email = 'frank2@dc949.org'
+    ,description = 'A task-based threading library based on Python threads.'
+    ,license = 'GPLv3'
+    ,keywords = 'threading'
+    ,url = 'https://github.com/frank2/schizophrenia'
+    ,package_dir = {'schizophrenia': 'schizophrenia'}
+    ,packages = ['schizophrenia']
+    ,install_requires = []
+    ,python_requires = '>=3'
+    ,classifiers = [
+        'Development Status :: 4 - Beta'
+        ,'Topic :: Software Development :: Libraries'
+        ,'License :: OSI Approved :: GNU General Public License v3 (GPLv3)']
+)
