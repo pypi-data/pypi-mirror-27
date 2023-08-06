05d2d36e741d5ca8a3d019727ecf07669e97090f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:30 +0000

    Add package pyjst/0.1.6/pyjst-0.1.6.tar.gz

---
+++ packages/pyjst/pyjst-0.1.6.tar.gz/pyjst-0.1.6/pyjst/__init__.py
@@ -0,0 +1,28 @@
+#
+# Potap module by GuillaumeDesforges
+#
+
+from os.path import normcase, join, isabs
+from os import getcwd
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import json
+
+class Baker:
+    def __init__(self, templates_folder_path=None):
+        if templates_folder_path == None:
+            templates_folder_path = join(getcwd(), 'templates')
+        elif not isabs(templates_folder_path):
+            templates_folder_path = join(getcwd(), templates_folder_path)
+        self.env = Environment(
+            loader = FileSystemLoader(templates_folder_path)
+        )
+
+    def bake_file(self, data_file_path, template_name):
+        template = self.env.get_template(template_name + '.tmpl')
+        with open(data_file_path, 'r') as f:
+            data_dict = json.loads(f.read())
+        return template.render(data_dict)
+
+    def bake(self, name):
+        data_file_path = join(getcwd(), 'datas', name + '.json')
+        return self.bake_file(data_file_path, name)
