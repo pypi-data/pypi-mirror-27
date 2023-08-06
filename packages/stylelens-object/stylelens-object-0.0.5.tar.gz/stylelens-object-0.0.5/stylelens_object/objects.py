779d7472e7b4a2a8b498cd8e119dd179b7e27ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:50 +0000

    Add package stylelens-object/0.0.5/stylelens-object-0.0.5.tar.gz

---
+++ packages/stylelens-object/stylelens-object-0.0.5.tar.gz/stylelens-object-0.0.5/stylelens_object/objects.py
@@ -0,0 +1,34 @@
+from stylelens_object.database import DataBase
+
+class Objects(DataBase):
+  def __init__(self):
+    super().__init__()
+    self.objects = self.db.objects
+
+  def get_objects_with_null_index(self, offset=0, limit=50):
+    try:
+      r = self.objects.find({"index":None})
+    except Exception as e:
+      print(e)
+
+    return list(r)
+
+  def add_object(self, object):
+    try:
+      r = self.objects.update_one({"name": object['name']},
+                                  {"$set": object},
+                                  upsert=True)
+    except Exception as e:
+      print(e)
+
+    return r.raw_result
+
+  def update_object(self, object):
+    try:
+      r = self.objects.update_one({"name": object['name']},
+                                  {"$set": object})
+    except Exception as e:
+      print(e)
+      return None
+
+    return r.raw_result
