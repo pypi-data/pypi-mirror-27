99d7e4ca736a407c2c83d412788a366f35394f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:43 +0000

    Add package toapi/0.2.11/toapi-0.2.11.tar.gz

---
+++ packages/toapi/toapi-0.2.11.tar.gz/toapi-0.2.11/toapi/__init__.py
@@ -0,0 +1,7 @@
+from toapi.api import Api
+from toapi.item import Item
+from toapi.log import logger
+from toapi.selector import XPath, Regex, Css
+from toapi.settings import Settings
+
+__version__ = '0.2.11'
