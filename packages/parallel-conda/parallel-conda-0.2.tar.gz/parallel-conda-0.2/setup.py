1ba43a502cefbba1d9427faf64a747a9d22bd2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package parallel-conda/0.2/parallel-conda-0.2.tar.gz

---
+++ packages/parallel-conda/parallel-conda-0.2.tar.gz/parallel-conda-0.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='parallel-conda',
+    version='0.2',
+    description='Drop-in parallel replacement for "conda install ..." procedures.',
+    url='https://github.com/milesgranger/parallel-conda',
+    maintainer='Miles Granger',
+    maintainer_email='miles.r.granger@gmail.com',
+    license='MIT',
+    install_requires=['click>=6.6'],
+    packages=['pconda'],
+    entry_points={
+        'console_scripts': [
+            'pconda = pconda.pconda:pconda'
+        ]
+    },
+    zip_safe=True
+)
