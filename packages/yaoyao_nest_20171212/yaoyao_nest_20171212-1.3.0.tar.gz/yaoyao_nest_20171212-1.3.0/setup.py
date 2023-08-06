c034f5c74af3eda78f78e9b0b4d2c782382978a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:35 +0000

    Add package yaoyao_nest_20171212/1.3.0/yaoyao_nest_20171212-1.3.0.tar.gz

---
+++ packages/yaoyao_nest_20171212/yaoyao_nest_20171212-1.3.0.tar.gz/yaoyao_nest_20171212-1.3.0/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+		name = 'yaoyao_nest_20171212',
+		version = '1.3.0',
+		py_modules = ['yaoyao_nest_20171212'],
+		author = 'fushenmomo',
+		author_email = '1021051650@qq.com',
+		url = 'https://pypi.python.org/pypi',
+		description = 'A simple printer of nested lists',
+	)
