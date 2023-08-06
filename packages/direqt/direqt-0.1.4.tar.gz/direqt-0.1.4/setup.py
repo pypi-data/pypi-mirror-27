4e9054e9a6fdf0f464eb544b4febef0aa8a1e0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:15 +0000

    Add package direqt/0.1.4/direqt-0.1.4.tar.gz

---
+++ packages/direqt/direqt-0.1.4.tar.gz/direqt-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(name='direqt',
+      version='0.1.4',
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python :: 2'
+      ],
+      description='Client library for Direqt Ads API',
+      url='https://github.com/direqt/direqt-sdk-python',
+      author='Myk Willis',
+      author_email='myk@direqt.io',
+      license='Apache License 2.0',
+      packages=['direqt'],
+      install_requires=[
+            'google-auth',
+            'requests'
+      ],
+      platforms='any',
+      setup_requires=[
+            'pytest-runner'
+      ],
+      tests_requires=[
+            'pytest',
+            'requests-mock'
+      ],
+      zip_safe=False)
