88dc701ea06ab5f6a036a51b20e44a52b5a8223b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:42 +0000

    Add package face_replace/0.1.6/face_replace-0.1.6.tar.gz

---
+++ packages/face_replace/face_replace-0.1.6.tar.gz/face_replace-0.1.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='face_replace',
+    version='0.1.6',
+    description='Simple facial recognition and face replacement',
+    author='Matt Lewis',
+    author_email='domattthings@gmail.com',
+    license='MIT',
+    url='https://github.com/mattmatters/face_replace',
+    download_url='https://github.com/mattmatters/face_replace/archive/0.1.6.tar.gz',
+    py_modules=['face_replace'],
+    packages=['face_replace'],
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'pillow',
+    ],
+)
