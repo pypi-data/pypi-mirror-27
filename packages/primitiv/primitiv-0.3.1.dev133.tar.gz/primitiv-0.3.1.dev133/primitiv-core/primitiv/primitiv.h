fbd424b3f9af34441bac3c5bcb9698e009acf65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:43 +0000

    Add package primitiv/0.3.1.dev133/primitiv-0.3.1.dev133.tar.gz

---
+++ packages/primitiv/primitiv-0.3.1.dev133.tar.gz/primitiv-0.3.1.dev133/primitiv-core/primitiv/primitiv.h
@@ -0,0 +1,17 @@
+#ifndef PRIMITIV_PRIMITIV_H_
+#define PRIMITIV_PRIMITIV_H_
+
+// This header file describes some include directives and may help users to use
+// the primitiv library.
+#include <primitiv/error.h>
+#include <primitiv/functions.h>
+#include <primitiv/graph.h>
+#include <primitiv/initializer_impl.h>
+#include <primitiv/model.h>
+#include <primitiv/naive_device.h>
+#include <primitiv/parameter.h>
+#include <primitiv/shape.h>
+#include <primitiv/tensor.h>
+#include <primitiv/optimizer_impl.h>
+
+#endif  // PRIMITIV_PRIMITIV_H_
