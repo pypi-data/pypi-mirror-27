351f9d0f426f4cd02c3e056c5ee1d1edb20e745b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:02 +0000

    Add package goprocam/1.9.8/goprocam-1.9.8.tar.gz

---
+++ packages/goprocam/goprocam-1.9.8.tar.gz/goprocam-1.9.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='goprocam',
+      version='1.9.8',
+      description='GoPro WiFi API Wrapper for Python - Compatible with all GoPro cameras with WiFi.',
+      url='http://github.com/konradit/gopro-py-api',
+      author='Konrad Iturbe',
+      author_email='mail@chernowii.com',
+      license='MIT',
+      packages=['goprocam'],
+      zip_safe=False)
