258db6111bdc0a7ba21ab6f93fe492577a2d9c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:53 +0000

    Add package pyutd/0.1/pyutd-0.1.tar.gz

---
+++ packages/pyutd/pyutd-0.1.tar.gz/pyutd-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyutd',
+    version='0.1',
+    description='python package for Modern Uniform Theory of diffraction',
+    url='https://github.com/bazingaedward/pyUTD',
+    author='Edward Qiu',
+    author_email='bazingaedward@gmail.com',
+    license='MIT',
+    packages=['pyutd'],
+    zip_safe=False
+)
