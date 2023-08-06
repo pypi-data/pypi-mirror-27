aa012136a4233374c0b80a5533ff6644ae278592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:18 +0000

    Add package ananke_sdk/1.1.2/ananke_sdk-1.1.2.tar.gz

---
+++ packages/ananke_sdk/ananke_sdk-1.1.2.tar.gz/ananke_sdk-1.1.2/ananke_sdk/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ananke_sdk',
+      version='1.1.1',
+      description='The funniest joke in the world',
+      url='https://github.com/ferrumaeternum/ananke_sdk',
+      author='Effective Solutions Ltd,Sri Lanka',
+      author_email='www.effectivesolutions.xyz',
+      license='MIT',
+      packages=['ananke_sdk'],
+      zip_safe=False)
