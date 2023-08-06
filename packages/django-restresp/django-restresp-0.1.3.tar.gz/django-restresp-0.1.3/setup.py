f28a2acdd33354937667264fce7edb359e8883dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:26 +0000

    Add package django-restresp/0.1.3/django-restresp-0.1.3.tar.gz

---
+++ packages/django-restresp/django-restresp-0.1.3.tar.gz/django-restresp-0.1.3/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+setup(
+    name='django-restresp',
+    version='0.1.3',
+    packages=['restresp'],
+    description='Pseudo-standard responders for django DRF',
+    long_description=README,
+    author='Andrea Carmisciano',
+    author_email='andrea.carmisciano@gmail.com',
+    url='https://github.com/acarmisc/django-restresp/',
+    license='MIT',
+    install_requires=[
+        'Django>=1.9',
+	'djangorestframework',
+    ]
+)
