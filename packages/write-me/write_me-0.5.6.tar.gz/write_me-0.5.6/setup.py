ef89cbee3516634ea9abdaf6d49f47fe915b755f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:34 +0000

    Add package write-me/0.5.6/write_me-0.5.6.tar.gz

---
+++ packages/write-me/write_me-0.5.6.tar.gz/write_me-0.5.6/setup.py
@@ -0,0 +1,26 @@
+"""Setup script for write-me Python package."""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='write_me',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['genreadme=readme_generator.make_scaffold:main'],
+    },
+    version='0.5.6',
+    description='Python package to assist developers with constructing README as project evolves.',
+    author=['Chelsea Dole',
+            'Matt Favoino',
+            'Darren Haynes',
+            'Chris Closser',
+            'Gabriel Meringolo'],
+    author_email='chelseadole@gmail.com',
+    url='https://github.com/chelseadole/write-me',
+    download_url='https://github.com/chelseadole/write-me/archive/0.5.6.tar.gz',
+    keywords=['Python', 'README', 'PyPi', 'pip'],
+    classifiers=[],
+    install_requires=[
+        "markdown_generator",
+    ],
+)
