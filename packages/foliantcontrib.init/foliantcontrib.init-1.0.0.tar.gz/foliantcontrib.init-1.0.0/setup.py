9bff76c5e7d9328e344128a5d8c97b7fd011aacb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:29 +0000

    Add package foliantcontrib.init/1.0.0/foliantcontrib.init-1.0.0.tar.gz

---
+++ packages/foliantcontrib.init/foliantcontrib.init-1.0.0.tar.gz/foliantcontrib.init-1.0.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+
+SHORT_DESCRIPTION = 'Create Foliant projects from templates.'
+
+try:
+    with open('README.md', encoding='utf8') as readme:
+        LONG_DESCRIPTION = readme.read()
+
+except FileNotFoundError:
+    LONG_DESCRIPTION = SHORT_DESCRIPTION
+
+
+setup(
+    name='foliantcontrib.init',
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    version='1.0.0',
+    author='Konstantin Molchanov',
+    author_email='moigagoo@live.com',
+    url='https://github.com/foliant-docs/foliantcontrib',
+    packages=['foliant.cli'],
+    license='MIT',
+    platforms='any',
+    install_requires=[
+        'foliant>=1.0.0',
+        'python-slugify'
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
+    ]
+)
