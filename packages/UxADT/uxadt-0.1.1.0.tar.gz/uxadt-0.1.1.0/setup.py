a875f3fc6e8dd01e36f695e0d97a224b934c3acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:34 +0000

    Add package UxADT/0.1.1.0/uxadt-0.1.1.0.tar.gz

---
+++ packages/UxADT/uxadt-0.1.1.0.tar.gz/uxadt-0.1.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name             = 'uxadt',
+    version          = '0.1.1.0',
+    packages         = ['uxadt',],
+    install_requires = [],
+    license          = 'MIT',
+	url              = 'http://uxadt.org',
+	author           = 'Andrei Lapets',
+	author_email     = 'a@lapets.io',
+    description      = 'Cross-platform embedded representation for algebraic data types, values, and common operations.',
+    long_description = open('README.rst').read(),
+    test_suite       = 'nose.collector',
+    tests_require    = ['nose'],
+)
