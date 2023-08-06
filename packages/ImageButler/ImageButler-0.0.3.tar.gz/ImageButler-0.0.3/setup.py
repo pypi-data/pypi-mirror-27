b8cf829fec28c92bee61752154c1f9f16a88a2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:25 +0000

    Add package ImageButler/0.0.3/ImageButler-0.0.3.tar.gz

---
+++ packages/ImageButler/ImageButler-0.0.3.tar.gz/ImageButler-0.0.3/setup.py
@@ -0,0 +1,55 @@
+import io
+import re
+from setuptools import setup
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('imagebutler/__init__.py', 'rb') as f:
+    version = str(_version_re.search(
+        f.read().decode('utf-8')
+    ).group(1)).replace('\'', '')
+with io.open('README.rst', encoding='utf-8') as f:
+    description = f.read()
+with io.open('HISTORY.rst', encoding='utf-8') as f:
+    description += "\n\n%s" % f.read()
+
+
+setup(
+    name='ImageButler',
+    version=version,
+    author='Trong-Nghia Nguyen',
+    author_email='nghia@viisix.space',
+    url='https://viisix.space/projects/image-butler/',
+    description='Simple images serving service,',
+    long_description=description,
+    packages=['imagebutler', 'imagebutler.apis'],
+    entry_points="""
+        [flask.commands]
+        user=imagebutler.commands:user
+        image=imagebutler.commands:image 
+    """,
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+        'Flask-Cors',
+        'Flask-Restful',
+        'Flask-SQLAlchemy',
+        'Flask-Migrate',
+        'Flask-Redislite',
+        'Flask-Login',
+        'pycrypto',
+        'Pillow',
+        'progressbar2'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
