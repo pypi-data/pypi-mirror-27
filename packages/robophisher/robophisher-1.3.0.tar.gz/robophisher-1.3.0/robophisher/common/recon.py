9b388ed84e3346b81c511e73a834c4186ddd3e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:25 +0000

    Add package robophisher/1.3.0/robophisher-1.3.0.tar.gz

---
+++ packages/robophisher/robophisher-1.3.0.tar.gz/robophisher-1.3.0/robophisher/common/recon.py
@@ -0,0 +1,53 @@
+"""
+This module handles all the operations regarding locating all the
+available access points
+"""
+
+import collections
+import scapy.layers.dot11 as dot11
+import robophisher.common.constants as constants
+
+
+def is_packet_valid(packet):
+    """
+    Return whether the packet is valid using the following merits:
+        1. Is a Dot11Beacon frame
+        2. Is not malformed
+        3. The channel is in range of 1..13
+
+    :param packet: A scapy packet
+    :type packet: scapy.layers.RadioTap
+    :return: True if packet is valid and False otherwise
+    :rtype: bool
+    """
+    return (packet.haslayer(dot11.Dot11Beacon) and hasattr(packet.payload, "info") and packet.info
+            and not packet.info.startswith('\x00') and len(packet[dot11.Dot11Elt:3].info) == 1
+            and ord(packet[dot11.Dot11Elt:3].info) in constants.ALL_2G_CHANNELS)
+
+
+def get_new_ap(interface_name):
+    """
+    Return a tuple containing the information for an access point
+
+    :param interface_name: Name of an interface to sniff
+    :type interface_name: str
+    :return: AccessPoint tuple containing information
+    :rtype: AccessPoint(name, channel, mac_address, is_encrypted)
+    :Example:
+
+        >>> interface = "wlan0"
+        >>> get_new_ap()
+        AccessPoint("NEW AP", 2, 00:11:22:33:44:55, True)
+
+    .. Note: This function has the possibility of blocking if it can't
+        find a valid packet. However in practice this never happens.
+    """
+    access_point = collections.namedtuple("AccessPoint", "name channel mac_address is_encrypted")
+    packet = dot11.sniff(iface=interface_name, count=1, lfilter=is_packet_valid)[0]
+
+    name = packet.info
+    channel = ord(packet[dot11.Dot11Elt:3].info)
+    mac_address = packet.addr3
+    is_encrypted = "privacy" in packet.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}")
+
+    return access_point(name, channel, mac_address, is_encrypted)
---
+++ packages/robophisher/robophisher-1.3.0.tar.gz/robophisher-1.3.0/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+"""
+This module handles the setup for package distribution
+"""
+
+from __future__ import print_function
+import re
+from setuptools import setup, find_packages
+
+with open('robophisher/__init__.py', 'r') as fd:
+    VERSION = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(),
+                        re.MULTILINE).group(1)
+
+
+def read_long_description():
+    """
+    Return the long description of the project
+    """
+    try:
+        with open('README.rst') as readme:
+            return readme.read()
+    except IOError:
+        return None
+
+
+# setup settings
+NAME = "robophisher"
+AUTHOR = "blackHatMonkey"
+AUTHOR_EMAIL = "brian.smith@riseup.net"
+URL = "https://github.com/blackHatMonkey/RoboPhisher"
+DESCRIPTION = "Automated phishing attacks against Wi-Fi networks"
+LICENSE = "GPL"
+KEYWORDS = ["robophisher", "evil", "twin", "phishing"]
+PACKAGES = find_packages(exclude=["docs", "tests"])
+INCLUDE_PACKAGE_DATA = True
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+    "Natural Language :: English", "Operating System :: Unix",
+    "Programming Language :: Python :: 2", "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 2 :: Only", "Topic :: Security",
+    "Topic :: System :: Networking", "Intended Audience :: End Users/Desktop",
+    "Intended Audience :: System Administrators", "Intended Audience :: Information Technology"
+]
+ENTRY_POINTS = {"console_scripts": ["robophisher= robophisher.pyrobophisher:run"]}
+INSTALL_REQUIRES = ["PyRIC", "tornado", "pbkdf2", "roguehostapd"]
+
+# run setup
+setup(
+    name=NAME,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=read_long_description(),
+    license=LICENSE,
+    keywords=KEYWORDS,
+    packages=PACKAGES,
+    include_package_data=INCLUDE_PACKAGE_DATA,
+    version=VERSION,
+    entry_points=ENTRY_POINTS,
+    install_requires=INSTALL_REQUIRES,
+    classifiers=CLASSIFIERS,
+    url=URL)
