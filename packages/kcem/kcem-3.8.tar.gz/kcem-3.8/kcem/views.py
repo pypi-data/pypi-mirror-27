d9161c8b63b570bbfaff3ebcb9a6994da1f6a85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:39 +0000

    Add package kcem/3.8/kcem-3.8.tar.gz

---
+++ packages/kcem/kcem-3.8.tar.gz/kcem-3.8/kcem/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.http import JsonResponse
+from djangoApiDec.djangoApiDec import queryString_required
+from kcem import WikiKCEM
+from udic_nlp_API.settings_database import uri
+
+k = WikiKCEM(uri)
+# Create your views here.
+@queryString_required(['keyword'])
+def kcem(request):
+    """Generate list of term data source files
+    Returns:
+        if contains invalid queryString key, it will raise exception.
+    """
+    keyword = request.GET['keyword']
+    return JsonResponse(k.findParent(keyword), safe=False)
