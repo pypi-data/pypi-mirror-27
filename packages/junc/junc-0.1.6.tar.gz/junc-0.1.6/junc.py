b359e56a1ce1368a2dcb62ae04d45e383148110a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:52 +0000

    Add package junc/0.1.6/junc-0.1.6.tar.gz

---
+++ packages/junc/junc-0.1.6.tar.gz/junc-0.1.6/junc.py
@@ -0,0 +1,73 @@
+"""
+Usage:
+    junc connect <name>
+    junc list
+    junc add (<name> <username> <ip>) [<location>]
+    junc remove <name>
+    junc backup [<file>]
+
+Options:
+    -h --help     Show this screen.
+    --version     Show version.
+
+Notes:
+    Data is stored in ~/.junc.json
+    Default backup location is ~/.junc.json.bak
+"""
+
+VERSION = "0.1.6"
+
+import os
+import sys
+
+from docopt import docopt
+
+from storage import Storage
+from server import *
+
+def cli(args):
+    storage = Storage()
+    try:
+        server_list = storage.get_servers()
+        server_table = storage.get_server_table()
+    except PermissionError:
+        print("Error: Permission denied. Try again with sudo or change permissions on " + storage.file_path + " (Recommended)")
+        sys.exit(1)
+
+    if args['list']:
+        print(server_table)
+
+    if args['add']:
+        server_list.append(new_server(args))
+        storage.write(server_list)
+
+    if args['connect']:
+        connection = ""
+        for server in server_list:
+            if server['name'] == args['<name>']:
+                connection = server['username'] + "@" + server['ip']
+        if not connection:
+            print("Couldn't find that server...")
+            sys.exit(1)
+        print("Connecting...")
+        os.system('ssh ' + connection)
+
+    if args['remove']:
+        for i in range(len(server_list)):
+            if server_list[i]['name'] == args['<name>']:
+                del server_list[i]
+                break
+            if i == len(server_list) - 1:
+                print("Couldn't find that server...")
+        storage.write(server_list)
+
+    if args['backup']:
+        storage.backup(args['<file>'])
+        print("Done.")
+
+def main():
+    arguments = docopt(__doc__, version="Junc v" + VERSION)
+    cli(arguments)
+
+if __name__ == '__main__':
+    main()
---
+++ packages/junc/junc-0.1.6.tar.gz/junc-0.1.6/storage.py
@@ -0,0 +1,44 @@
+import json
+from shutil import copy2
+import os
+
+from terminaltables import AsciiTable
+
+class Storage():
+    def __init__(self):
+        self.file_path = os.path.expanduser("~") + "/.junc.json"
+        self.create_file(self.file_path)
+
+    def create_file(self, file_path):
+        try:
+            open(file_path, 'a')
+        except PermissionError:
+            print("Error: Permission denied. Change permissions on " + file_path)
+        return True
+
+    def write(self, server_dict):
+        with open(self.file_path, 'w') as f:
+            json.dump(server_dict, f, indent=4)
+        return True
+
+    def get_servers(self):
+        try:
+            return json.loads(open(self.file_path, 'r').read())
+        except json.decoder.JSONDecodeError:
+            return []
+
+    def get_server_table(self):
+        server_list = self.get_servers()
+        if not server_list:
+            return "No Servers :("
+        table_data = [
+            ['Name', "Address", "Location"],
+        ]
+        for server in server_list:
+            table_data.append([server['name'], server['username'] + "@" + server['ip'], server['location']])
+        return AsciiTable(table_data).table
+
+    def backup(self, location=None):
+        if not location:
+            location = self.file_path + '.bak'
+        copy2(self.file_path, location)
