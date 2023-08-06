8656579b6e08af215a2b6d29c2adfbb06f5f76c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:04 +0000

    Add package cutecare-py/0.15/cutecare-py-0.15.tar.gz

---
+++ packages/cutecare-py/cutecare-py-0.15.tar.gz/cutecare-py-0.15/cutecare/gpio.py
@@ -0,0 +1,21 @@
+from cutecare.backends import BluetoothInterface
+import logging
+
+_HANDLE_GPIO_DATA = 0x07
+_LOGGER = logging.getLogger(__name__)
+
+class CuteCareGPIOJDY8(object):
+
+    def __init__(self, mac, backend, adapter='hci0'):
+        self._mac = mac
+        self._bt_interface = BluetoothInterface(backend, adapter)
+
+    def name(self):
+        return 'CuteCare GPIO'
+
+    def set_gpio1(self, value):
+        with self._bt_interface.connect(self._mac) as connection:
+            connection.write_handle(_HANDLE_GPIO_DATA, "E7F100" if value else "E7F101" )
+            _LOGGER.debug('Received result for handle %s: %s', \
+                          _HANDLE_GPIO_DATA, self._format_bytes(raw_data))
+            return None
---
+++ packages/cutecare-py/cutecare-py-0.15.tar.gz/cutecare-py-0.15/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='cutecare-py',
+      version='0.15',
+      description='Library to read data from CuteCare Bluetooth LE DIY sensor',
+      url='https://github.com/cutecare/cutecare-py',
+      author='Evgeny Savitsky',
+      author_email='evgeny.savitsky@gmail.com',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: System :: Hardware :: Hardware Drivers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5'
+      ],
+      packages=['cutecare','cutecare.backends'],
+      keywords='home assistant sensor bluetooth low-energy ble',
+      zip_safe=False,
+      extras_require={'testing': ['pyest']}
+      )
