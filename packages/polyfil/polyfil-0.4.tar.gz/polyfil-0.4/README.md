a8aadc2869d899f20300e382de1a4e31854a82f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:37 +0000

    Add package polyfil/0.4/polyfil-0.4.tar.gz

---
+++ packages/polyfil/polyfil-0.4.tar.gz/polyfil-0.4/README.md
@@ -0,0 +1,5 @@
+If you look for a python algorithm to find the pixels coordinates between n points and
+then fill it, this is just what you need.
+To install this python statechart library:
+
+pip install polyfill
---
+++ packages/polyfil/polyfil-0.4.tar.gz/polyfil-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+ dependency_links=['https://download.lfd.uci.edu/pythonlibs/gjr6o2id/opencv_python-3.4.0-cp37-cp37m-win_amd64.whl'],
+  name = 'polyfil',
+  packages = ['polyfil'], # this must be the same as the name above
+  version = '0.4',
+  description = 'A random test lib',
+  author = 'laformarua',
+  author_email = 'lcrua@utp.edu.co',
+  url = 'https://github.com/laformarua/polyfill', # use the URL to the github repo
+  download_url = 'https://github.com/laformarua/polyfill/archive/tag/0.2.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+  install_requires=[
+   'opencv_python-3.4.0-cp37-cp37m-win_amd64.whl'
+  ]
+
+)
