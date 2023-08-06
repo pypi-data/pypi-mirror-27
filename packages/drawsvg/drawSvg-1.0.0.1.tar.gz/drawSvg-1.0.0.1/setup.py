4ab3cfbcfc120aa0beccff25d2612793156cf794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:43 +0000

    Add package drawsvg/1.0.0.1/drawSvg-1.0.0.1.tar.gz

---
+++ packages/drawsvg/drawSvg-1.0.0.1.tar.gz/drawSvg-1.0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'drawSvg',
+    packages = ['drawSvg'],
+    version = '1.0.0.1',
+    description = 'This is a Python 3 library for programmatically generating SVG images (vector drawings) and rendering them or displaying them in an iPython notebook.',
+    author = 'Casey Duckering',
+    #author_email = '',
+    url = 'https://github.com/cduck/drawSvg',
+    download_url = 'https://github.com/cduck/drawSvg/archive/1.0.0.tar.gz',
+    keywords = ['SVG', 'draw', 'graphics', 'iPython', 'Jupyter'],
+    classifiers = [],
+)
+
