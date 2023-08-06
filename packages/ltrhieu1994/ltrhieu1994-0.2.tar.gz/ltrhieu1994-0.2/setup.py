f140cbf43cd5c7f2792560c6bc53825486192847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:45 +0000

    Add package ltrhieu1994/0.2/ltrhieu1994-0.2.tar.gz

---
+++ packages/ltrhieu1994/ltrhieu1994-0.2.tar.gz/ltrhieu1994-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+ 
+setup(name='ltrhieu1994',
+      version='0.2',
+      description='Hello World',
+      url='',
+      author='Hieu Le',
+      author_email='ltrhieu1994@gmail.com',
+      license='BKU',
+      packages=['leedsx'],
+      zip_safe=False)
