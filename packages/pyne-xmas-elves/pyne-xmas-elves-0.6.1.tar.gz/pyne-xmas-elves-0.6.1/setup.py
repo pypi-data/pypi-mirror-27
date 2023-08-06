e3d9a6ed0abb03d5691b557a7c690a764d37c771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:23 +0000

    Add package pyne-xmas-elves/0.6.1/pyne-xmas-elves-0.6.1.tar.gz

---
+++ packages/pyne-xmas-elves/pyne-xmas-elves-0.6.1.tar.gz/pyne-xmas-elves-0.6.1/setup.py
@@ -0,0 +1,28 @@
+from os import path
+from setuptools import setup
+import m2r
+
+with open('requirements.txt') as f:
+    requirements = [l.split(' ')[0] for l in f.readlines()]
+
+readme_file = path.join(path.dirname(path.abspath(__file__)),
+                        'README.md')
+try:
+    DESCRIPTION = m2r.parse_from_file(readme_file)
+except (IOError, FileNotFoundError):
+    DESCRIPTION = ''
+
+setup(name='pyne-xmas-elves',
+      version='0.6.1',
+      description='The Great Elf Game adapted for Python North East - client '
+      '& server',
+      url='http://github.com/pythonnortheast/xmas-elves',
+      author='Scott Walton',
+      author_email='scott@pythonnortheast.com',
+      license='MIT',
+      long_description=DESCRIPTION,
+      packages=['pyne_xmas_elves.client', 'pyne_xmas_elves.server'],
+      python_requires='>=3.5',
+      install_requires=requirements,
+      scripts=['bin/elves.py'],
+      zip_safe=False)
