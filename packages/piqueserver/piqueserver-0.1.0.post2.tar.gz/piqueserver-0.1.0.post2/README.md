287022c79495cd976ec75a628bb59d40f62ca15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:59 +0000

    Add package piqueserver/0.1.0.post2/piqueserver-0.1.0.post2.tar.gz

---
+++ packages/piqueserver/piqueserver-0.1.0.post2.tar.gz/piqueserver-0.1.0.post2/README.md
@@ -0,0 +1,112 @@
+
+<img src="doc/logo.png" align="right" width="200px" alt="logo">
+
+# piqueserver [![Build Status](https://travis-ci.org/piqueserver/piqueserver.svg?branch=master)](https://travis-ci.org/piqueserver/piqueserver) [![Join the chat at https://gitter.im/piqueserver/piqueserver](https://badges.gitter.im/piqueserver/piqueserver.svg)](https://gitter.im/piqueserver/piqueserver?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+An Ace of Spades 0.75 server based on [PySnip](https://github.com/NateShoffner/PySnip).
+
+## :rocket: Installation
+
+### pip (stable version)
+
+```bash
+pip install piqueserver
+```
+
+to install with the optional ssh server
+
+```bash
+pip install piqueserver[ssh]
+```
+
+### git (bleeding edge)
+
+```bash
+git clone https://github.com/piqueserver/piqueserver
+virtualenv2 venv
+source venv/bin/activate
+pip install -r requirements.txt
+python setup.py install
+```
+
+### Archlinux
+
+An [AUR package](https://aur.archlinux.org/packages/piqueserver-git/) (git master) is available. Install manually or
+with your favourite AUR helper:
+
+```bash
+pacaur -y piqueserver-git
+```
+
+## :rocket: Running
+
+Then copy the default configuration as a base to work off
+
+```bash
+piqueserver --copy-config
+```
+
+A-a-and lift off!
+
+```bash
+piqueserver
+```
+
+### Custom config location
+
+If you wish to use a different location to `~/.config/piqueserver/` for config files, specify a directory with the `-d`
+flag:
+
+```bash
+piqueserver --copy-config -d custom_dir
+piqueserver -d custom_dir
+```
+
+
+## :speech_balloon: FAQ
+
+#### What's the purpose?
+
+
+ The purpose of this repo is to be a continuation of PySnip.
+
+#### What if PySnip development returns?
+
+Then they would merge our changes and development would be continued
+there, I guess. The important thing is to keep AoS servers alive.
+
+#### Why should I use piqueserver instead of PySnip/PySpades?
+
+ * Multi config installation
+ * Docker support
+ * Bug fixes
+ * Improvements
+ * Better anti-hacking
+ * New scripts
+
+#### What about 0.76 support
+
+ Working with multiple versions is a pain. 0.76 will be suported in the
+ future only.
+
+#### Is that everything?
+
+ Please see also the [original README](https://github.com/piqueserver/piqueserver/blob/master/OLD_README.md) from PySnip and
+ the [Wiki](https://github.com/piqueserver/piqueserver/wiki) for more information.
+
+## :blush: Contribute
+
+Don't be shy and submit us a PR or an issue! Help is always appreciated
+
+## :wrench: Development
+
+Use `pip` and `virtualenv` to setup the development environment:
+
+```bash
+$ virtualenv -p python2 venv && . ./venv/bin/activate
+(venv) $ pip install -r requirements.txt
+(venv) $ ./setup.py install
+(venv) $ deactivate # Deactivate virtualenv
+```
+-------
+Brought to you with :heart: by the [piqueserver team](https://github.com/orgs/piqueserver/people).
---
+++ packages/piqueserver/piqueserver-0.1.0.post2.tar.gz/piqueserver-0.1.0.post2/setup.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+
+# ### Virtualenv autoload ###
+# # Not really sure whether it's worth it... Looks like reinveting virtualenv .
+# # Consider making users to load virtualenv on their own.
+# from os import path as __p
+# venv_dirs = [__p.join(__p.dirname(__p.realpath(__file__)), i) for i in ('venv', '.venv')]
+# activated_venv = None
+# for venv_dir in venv_dirs:
+#     try:
+#         activate_this = __p.join(venv_dir, 'bin', 'activate_this.py')
+#         execfile(activate_this, dict(__file__=activate_this))
+#         activated_venv = venv_dir
+#     except IOError:
+#         pass
+#     else:
+#         break
+# print("Using virtualenv %s" % activated_venv)
+
+# import sys
+# import os
+
+# if activated_venv is not None:
+#     sys.argv[0] = __p.join(activated_venv, 'bin', 'python2')
+#     sys.executable = sys.argv[0]
+#     os.environ['_'] = sys.argv[0]
+#     sys.prefix = activated_venv
+#     sys.exec_prefix = activated_venv
+# ### Virtualenv autoload end ###
+
+
+import sys
+import os
+
+PKG_NAME="piqueserver"
+PKG_URL="https://github.com/piqueserver/piqueserver"
+PKG_DOWNLOAD_URL="https://github.com/piqueserver/piqueserver/archive/0.1.0.zip"
+
+extra_args = sys.argv[2:]
+
+import subprocess
+import shutil
+from setuptools import setup, find_packages, Extension, dist
+from distutils.core import run_setup
+
+try:
+   import pypandoc
+   import re
+   long_description = pypandoc.convert_text(
+        re.sub(r'[^\x00-\x7F]+',' ',
+            pypandoc.convert('README.md', 'markdown', format="markdown_github")), 'rst', format="markdown")
+except (IOError, ImportError):
+    long_description = ''
+
+
+ext_modules = []
+
+names = [
+    'pyspades.vxl',
+    'pyspades.bytes',
+    'pyspades.packet',
+    'pyspades.contained',
+    'pyspades.common',
+    'pyspades.world',
+    'pyspades.loaders',
+    'pyspades.mapmaker'
+]
+
+static = os.environ.get('STDCPP_STATIC') == "1"
+
+if static:
+    print("Linking the build statically.")
+
+for name in names:
+    if static:
+        extra = {'extra_link_args' : ['-static-libstdc++', '-static-libgcc']}
+    else:
+        extra = {}
+
+    if name in ['pyspades.vxl', 'pyspades.world', 'pyspades.mapmaker']:
+        extra["extra_compile_args"] = ['-std=c++11']
+
+    ext_modules.append(Extension(name, ['./%s.pyx' % name.replace('.', '/')],
+        language = 'c++', include_dirs=['./pyspades'], **extra))
+
+
+from distutils.command.build_ext import build_ext as _build_ext
+class build_ext(_build_ext):
+    def run(self):
+
+        from Cython.Build import cythonize
+        self.extensions = cythonize(self.extensions)
+
+        _build_ext.run(self)
+
+        run_setup(os.path.join(os.getcwd(), "setup.py"), ['build_py'] + extra_args)
+
+setup(
+    name = PKG_NAME,
+    packages = [PKG_NAME, '%s.web' % PKG_NAME, 'pyspades'],
+    version = '0.1.0.post2',
+    description = 'Open-Source server implementation for Ace of Spades',
+    author = 'Originally MatPow2 and PySnip contributors, now, StackOverflow and piqueserver authors',
+    author_email = 'nate.shoffner@gmail.com',
+    maintainer = 'noway421',
+    maintainer_email = 'noway@2ch.hk',
+    license = 'GNU General Public License v3',
+    long_description = long_description,
+    url = PKG_URL,
+    download_url = PKG_DOWNLOAD_URL,
+    keywords = ['ace of spades', 'aos', 'server', 'pyspades', 'pysnip', 'piqueserver'],
+    classifiers = [
+        'Intended Audience :: System Administrators',
+        'Development Status :: 3 - Alpha',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 2 :: Only',
+        'Framework :: Twisted',
+    ],
+    platforms = "Darwin, Unix",
+    setup_requires = ['Cython>=0,<1'], # at least for now when we have to cythonize enet
+    install_requires = [
+        'Cython>=0,<1',
+        'Twisted>=17',
+        'Jinja2>=2,<3', # status server is part of our 'vanila' package
+        'Pillow>=3,<5',
+        'pyenet',
+        'ipaddr'
+    ],
+    extras_require = {
+        'from': ['pygeoip>=0.3.2,<0.4'],
+        # 'statusserver': ['Jinja2>=2.8,<2.9', 'Pillow>=3.4.2,<3.5'],
+        'ssh': ['pycrypto>=2.6.1,<2.7', 'cryptography>=2.0.0,<3.0', 'pyasn1>=0.1.9,<0.2']
+    },
+    entry_points = {
+        'console_scripts': [
+            '%s=%s.run:main' % (PKG_NAME, PKG_NAME)
+        ],
+    },
+    package_dir = {PKG_NAME: 'piqueserver', '%s.web' % PKG_NAME: 'piqueserver/web', 'pyspades': 'pyspades'}, # some kind of find_packages?
+    package_data = {"%s.web" % PKG_NAME: ["templates/status.html"]},
+    include_package_data=True,
+
+    ext_modules = ext_modules,
+    cmdclass = { 'build_ext': build_ext },
+)
