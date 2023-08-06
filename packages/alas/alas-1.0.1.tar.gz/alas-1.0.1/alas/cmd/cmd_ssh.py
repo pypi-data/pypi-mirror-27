d0e4a0071deb7d8799c674c236dff54f07d96d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:08 +0000

    Add package alas/1.0.1/alas-1.0.1.tar.gz

---
+++ packages/alas/alas-1.0.1.tar.gz/alas-1.0.1/alas/cmd/cmd_ssh.py
@@ -0,0 +1,85 @@
+import os
+import sys
+import click
+
+from alas.cli import pass_context
+from alas.utils import ssh_config
+
+
+def _get_conf_obj(host, port, user, id_file, props):
+    config = {}
+    if host:
+        config['hostname'] = host
+    if port:
+        config['port'] = port
+    if user:
+        config['user'] = user
+    if id_file:
+        config['identityfile'] = id_file
+    if props:
+        for prop in props:
+            tmp = prop.split(' ', 1)
+            config[tmp[0]] = tmp[1]
+    return config
+
+
+@click.command('ssh', short_help='Manage SSH host alias.')
+@click.option('-c', '--config-file', type=click.Path(),
+              default='~/.ssh/config',
+              help='SSH config file to manage host aliases; default to '
+                   '~/.ssh/config.')
+@click.option('-pr', '--prop', type=click.STRING, multiple=True,
+              help='Multiple additional property for SSH alias, must '
+                   'be in format "Attribute value".')
+@click.option('-i', '--id-file', type=click.Path(),
+              help='Identity file used for perform authz in SSH session.')
+@click.option('-h', '--host',
+              help='Target host to SSH.')
+@click.option('-p', '--port', default=22,
+              help='Port for perform SSH in target host; default is 22.')
+@click.option('-u', '--user',
+              help='Username for authz with target host.')
+@click.option('-a', '--alias', type=click.STRING,
+              help='Alias name to add/update/remove or view.')
+@click.argument('action', type=click.Choice(['add', 'update', 'rm', 'list',
+                                             'view']))
+@pass_context
+def cli(ctx, action, alias, user, port, host, id_file, prop, config_file):
+    if not os.path.exists(os.path.expanduser(config_file)):
+        ctx.log('Config file not existed!\n')
+        sys.exit(1)
+    else:
+        config = ssh_config.SSHConfig(os.path.expanduser(config_file))
+        conf = _get_conf_obj(host, port, user, id_file, prop)
+    if alias:
+        curr_config = config.lookup(alias)
+    else:
+        curr_config = None
+    if action == 'add':
+        if curr_config:
+            ctx.log('Alias already exists or missing alias name!\n')
+            sys.exit(1)
+        config.write(alias, conf)
+        ctx.log('Done!')
+    elif action == 'update':
+        if not curr_config:
+            ctx.log('Alias does not exist or missing alias name!\n')
+            sys.exit(2)
+        for k, v in curr_config.items():
+            if k not in conf:
+                conf[k] = v
+        config.write(alias, conf)
+        ctx.log('Done!')
+    elif action == 'rm':
+        if not curr_config:
+            ctx.log('Alias does not exist or missing alias name!\n')
+            sys.exit(3)
+        config.delete(alias)
+        ctx.log('Done!')
+    elif action == 'list':
+        ctx.log(str(config))
+    elif action == 'view':
+        if curr_config:
+            ctx.log("alias: {0}".format(alias))
+            for k, v in curr_config.items():
+                ctx.log("  {0}: {1}".format(k, v))
---
+++ packages/alas/alas-1.0.1.tar.gz/alas-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='alas',
+    version='1.0.1',
+    description='All Linux aliases and auto-completion manager.',
+    author='Hieu LE',
+    author_email='sudo@rm-rf.cloud',
+    license='Apache-2.0',
+    url='https://github.com/hieulq/alas',
+    packages=['alas', 'alas.cmd', 'alas.utils'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'prettytable',
+        'GitPython'
+    ],
+    entry_points='''
+        [console_scripts]
+        alas=alas.cli:cli
+    ''',
+)
