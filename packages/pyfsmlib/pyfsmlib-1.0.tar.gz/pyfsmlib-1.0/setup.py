780c782e7bed50ee1004c9bb90aeff8aa0c8168d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:46 +0000

    Add package pyfsmlib/1.0/pyfsmlib-1.0.tar.gz

---
+++ packages/pyfsmlib/pyfsmlib-1.0.tar.gz/pyfsmlib-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+    name = 'pyfsmlib',
+    version='1.0',
+    packages = ['pyfsmlib'],
+    license = 'AGPL v3',
+    description = 'FSM for event driven applications',
+    author = "Javier Barbadillo",
+    author_email = "javier.barbadillo@gmail.com",
+    url = 'https://github.com/jbarbadillo/pyFSM',
+    download_url = 'https://github.com/jbarbadillo/pyFSM/archive/1.0.tar.gz',
+    keywords = ['fsm', 'state-machine'],
+    classifiers = ['Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6'],
+)
