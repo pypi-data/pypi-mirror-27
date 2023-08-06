98dba12a716e70579e63baead1f4ebcf0b9690e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:55 +0000

    Add package pyz/0.4.2/pyz-0.4.2.tar.gz

---
+++ packages/pyz/pyz-0.4.2.tar.gz/pyz-0.4.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = "0.4.2"
+
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
+setup(
+    name="pyz",
+    version=version,
+    description="Packages all source code and dependencies into a single Python zip application.",
+    long_description=readme(),
+    author="Bradley Odell",
+    author_email="btodell@hotmail.com",
+    url="https://github.com/BTOdell/pyz",
+    download_url="https://github.com/BTOdell/pyz/archive/{0}.tar.gz".format(version),
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Build Tools"
+    ],
+    license="GPLv3",
+    keywords="pyz zip app zipapp bundle dependencies library"
+)
