7a9ee58f97c8f106020cb6c756432f3359565029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:26 +0000

    Add package django-fobi/0.12.8/django_fobi-0.12.8-py2.py3-none-any.whl

---
+++ packages/django-fobi/django_fobi-0.12.8-py2.py3-none-any.whl/fobi/contrib/themes/simple/apps.py
@@ -0,0 +1,17 @@
+__title__ = 'fobi.contrib.themes.simple.apps'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2017 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('Config',)
+
+try:
+    from django.apps import AppConfig
+
+    class Config(AppConfig):
+        """Config."""
+
+        name = 'fobi.contrib.themes.simple'
+        label = 'fobi_contrib_themes_simple'
+
+except ImportError:
+    pass
