87816c8aab84f9b887197f1d48774fb92f45e823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:36 +0000

    Add package plumlightpad/0.0.7/plumlightpad-0.0.7-py3-none-any.whl

---
+++ packages/plumlightpad/plumlightpad-0.0.7-py3-none-any.whl/plumlightpad/test.py
@@ -0,0 +1,8 @@
+import plumlightpad
+
+plum = PlumLightpad("heath@heath-bar.com", "lightupmynight")
+# x.turn_on("aaddbd34-1fd1-40c7-8725-5381e90b7508")
+# time.sleep(2)
+# x.turn_off("aaddbd34-1fd1-40c7-8725-5381e90b7508")
+# time.sleep(2)
+# x.set_level("aaddbd34-1fd1-40c7-8725-5381e90b7508", 127)
