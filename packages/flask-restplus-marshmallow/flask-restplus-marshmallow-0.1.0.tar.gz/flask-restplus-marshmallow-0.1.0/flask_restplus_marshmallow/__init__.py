7b293884a132c5962087abd190d7bb7e5dad6e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:20 +0000

    Add package flask-restplus-marshmallow/0.1.0/flask-restplus-marshmallow-0.1.0.tar.gz

---
+++ packages/flask-restplus-marshmallow/flask-restplus-marshmallow-0.1.0.tar.gz/flask-restplus-marshmallow-0.1.0/flask_restplus_marshmallow/__init__.py
@@ -0,0 +1,7 @@
+from flask_restplus import *
+from .api import Api
+from .model import Schema, ModelSchema, DefaultHTTPErrorSchema
+from .namespace import Namespace
+from .parameters import Parameters, PostJSONParameters
+from .swagger import Swagger
+from .resource import Resource
