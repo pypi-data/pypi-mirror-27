c7dbba88197248e5dc54fde929b88ce0bb46c0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:39 +0000

    Add package gocomics-downloader/1.0/gocomics_downloader-1.0.tar.gz

---
+++ packages/gocomics-downloader/gocomics_downloader-1.0.tar.gz/gocomics_downloader-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='gocomics_downloader',
+      version='1.0',
+      description='Conveniently download the comic images from GoComics comic files. ',
+      url='https://github.com/PokestarFan/GoComics-Downloader',
+      author='PokestarFan',
+      author_email='pokestarfan@yahoo.com',
+      license='MIT',
+      packages=['gocomics-downloader'],
+      zip_safe=True)
