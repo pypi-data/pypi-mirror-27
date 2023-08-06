6e7dee0b34c884f0292390bdc9bfdae807e72b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:00 +0000

    Add package jikkensol/0.0.8/jikkensol-0.0.8-py3-none-any.whl

---
+++ packages/jikkensol/jikkensol-0.0.8-py3-none-any.whl/jikkensol-0.0.8.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+jikken package
+
+
---
+++ packages/jikkensol/jikkensol-0.0.8-py3-none-any.whl/jikkensol/hello.py
@@ -0,0 +1,26 @@
+import sys, os
+
+class Hello:
+    def __init__(self, name):
+        self._name = name
+
+    def say(self):
+        print('{}さん、こんにちは。'.format(self._name))
+
+    def song(self):
+        print('バージョン 0.0.8')
+
+        song_file = os.path.dirname(os.path.abspath(__file__)) + os.sep +\
+                'files' + os.sep + 'songs.txt'
+        with open(song_file, 'rt') as f:
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                print(line, end='')
+
+
+if __name__ == '__main__':
+    h = Hello('太郎')
+    h.say()
+    h.song()
