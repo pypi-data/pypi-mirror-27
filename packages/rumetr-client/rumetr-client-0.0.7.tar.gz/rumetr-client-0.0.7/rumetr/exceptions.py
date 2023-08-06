10917fecdef7e345acfb1c0f89fad23ee44ff135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:44 +0000

    Add package rumetr-client/0.0.7/rumetr-client-0.0.7.tar.gz

---
+++ packages/rumetr-client/rumetr-client-0.0.7.tar.gz/rumetr-client-0.0.7/rumetr/exceptions.py
@@ -0,0 +1,30 @@
+class RumetrException (BaseException):
+    pass
+
+
+class RumetrDeveloperNotFound(RumetrException):
+    pass
+
+
+class RumetrComplexNotFound(RumetrException):
+    pass
+
+
+class RumetrHouseNotFound(RumetrException):
+    pass
+
+
+class RumetrApptNotFound(RumetrException):
+    pass
+
+
+class Rumetr404Exception(RumetrException):
+    pass
+
+
+class Rumetr403Exception(RumetrException):
+    pass
+
+
+class RumetrBadServerResponseException(RumetrException):
+    pass
