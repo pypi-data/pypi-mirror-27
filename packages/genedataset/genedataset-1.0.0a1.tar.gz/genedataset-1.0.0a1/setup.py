167160dfe8bc62863ea9c76770b579810b0f7472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:16 +0000

    Add package genedataset/1.0.0a1/genedataset-1.0.0a1.tar.gz

---
+++ packages/genedataset/genedataset-1.0.0a1.tar.gz/genedataset-1.0.0a1/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+requires = [
+    'pandas',
+    ]
+
+setup(name='genedataset',
+      version='1.0.0a1',
+      description='Store and access gene expression datasets and gene definitions.',
+      long_description=README,
+      classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        ],
+      author='Jarny Choi',
+      author_email='jarnyc@unimelb.edu.au',
+      url='https://github.com/jarny/genedataset',
+      keywords='gene expression dataset rna-seq',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      )
