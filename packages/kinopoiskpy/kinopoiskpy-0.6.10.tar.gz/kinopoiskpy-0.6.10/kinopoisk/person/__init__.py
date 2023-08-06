f34e02d3c98becfae969d5fe44447ec8c5826b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:36 +0000

    Add package kinopoiskpy/0.6.10/kinopoiskpy-0.6.10.tar.gz

---
+++ packages/kinopoiskpy/kinopoiskpy-0.6.10.tar.gz/kinopoiskpy-0.6.10/kinopoisk/person/__init__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from future.utils import python_2_unicode_compatible
+
+from .sources import PersonLink, PersonMainPage, PersonPhotosPage
+from ..utils import KinopoiskObject, Manager
+
+
+@python_2_unicode_compatible
+class Person(KinopoiskObject):
+    """
+    Person Class
+    """
+    def set_defaults(self):
+        self.name = ''
+        self.name_original = ''
+        self.information = ''
+
+        self.year_birth = None
+
+        self.photos = []
+
+    def __init__(self, *args, **kwargs):
+        super(Person, self).__init__(*args, **kwargs)
+
+        self.register_source('link', PersonLink)
+        self.register_source('main_page', PersonMainPage)
+        self.register_source('photos', PersonPhotosPage)
+
+        self.set_url('info', '/handler_info.php?token={token}&obj_type={type}&obj_id={id}')
+
+    def __str__(self):
+        return '%s (%s), %s' % (self.name, self.name_original, self.year_birth or '-')
+
+
+class PersonManager(Manager):
+    """
+    Person manager
+    """
+    kinopoisk_object = Person
+
+    def get_url_with_params(self, query):
+        # http://www.kinopoisk.ru/index.php?level=7&from=forma&result=adv&m_act[from]=forma&m_act[what]=content&m_act[find]=pulp+fiction
+        # http://www.kinopoisk.ru/index.php?level=7&from=forma&result=adv&m_act[from]=forma&m_act[what]=actor&m_act[find]=malkovich
+        return ('http://www.kinopoisk.ru/index.php', {
+            'level': 7,
+            'from': 'forma',
+            'result': 'adv',
+            'm_act[from]': 'forma',
+            'm_act[what]': 'actor',
+            'm_act[find]': query,
+        })
+
+
+Person.objects = PersonManager()
