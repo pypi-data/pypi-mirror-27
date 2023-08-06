5868041d1e78e298ccbda62c3643cb5f59c16de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:48 +0000

    Add package emailgen/0.1.1/emailgen-0.1.1.tar.gz

---
+++ packages/emailgen/emailgen-0.1.1.tar.gz/emailgen-0.1.1/emailgen/__init__.py
@@ -0,0 +1,15 @@
+
+from .email import Email, Button, Table
+from .generator import Generator
+from .themes import DEFAULT_THEME
+
+__all__ = [
+    "Button",
+    "Email",
+    "Generator",
+    "Table",
+
+    "DEFAULT_THEME",
+]
+
+__version__ = "0.1.1"
