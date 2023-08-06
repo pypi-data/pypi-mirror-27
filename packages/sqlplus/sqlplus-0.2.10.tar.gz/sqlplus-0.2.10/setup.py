cf8e4ec75200403089ef49e634e1cfa61aeb35fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:02 +0000

    Add package sqlplus/0.2.10/sqlplus-0.2.10.tar.gz

---
+++ packages/sqlplus/sqlplus-0.2.10.tar.gz/sqlplus-0.2.10/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='sqlplus',
+      version='0.2.10',
+      description='data work tools',
+      url='https://github.com/nalssee/sqlplus.git',
+      author='nalssee',
+      author_email='kenjin@sdf.org',
+      license='MIT',
+      packages=['sqlplus'],
+      # Install statsmodels manually with conda install
+      install_requires=[
+          'pypred',
+          'sas7bdat',
+          'xlrd'
+      ],
+      scripts=['bin/prepend', 'bin/fnguide'],
+      zip_safe=False)
