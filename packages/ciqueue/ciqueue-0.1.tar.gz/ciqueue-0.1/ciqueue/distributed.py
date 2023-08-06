3c873d78118d21a391ff1de8becce7efed7a1703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:59 +0000

    Add package ciqueue/0.1/ciqueue-0.1.tar.gz

---
+++ packages/ciqueue/ciqueue-0.1.tar.gz/ciqueue-0.1/ciqueue/distributed.py
@@ -0,0 +1,221 @@
+import os
+import time
+import math
+import redis
+
+from past.builtins import xrange  # pylint: disable=redefined-builtin,import-modules-only
+
+from ciqueue import static
+
+
+class LostMaster(Exception):
+    pass
+
+
+class Base(object):
+
+    def __init__(self, redis, build_id):
+        self.redis = redis
+        self.build_id = str(build_id)
+        self.is_master = False
+        self.total = None
+        self._scripts = {}
+
+    def key(self, *args):
+        return ':'.join(['build', self.build_id] + [str(i) for i in args])
+
+    def wait_for_master(self, timeout=10):
+        if self.is_master:
+            return True
+
+        for _ in xrange(timeout * 10 + 1):
+            master_status = self._master_status()
+            if master_status in ['ready', 'finished']:
+                return True
+            time.sleep(0.1)
+
+        raise LostMaster(
+            "The master worker is still `" +
+            repr(master_status) +
+            "` after {} seconds waiting.".format(timeout))
+
+    def _master_status(self):
+        raw = self.redis.get(self.key('master-status'))
+        return raw.decode() if raw else None
+
+    def __len__(self):
+        transaction = self.redis.pipeline(transaction=True)
+        transaction.llen(self.key('queue'))
+        transaction.zcard(self.key('running'))
+        return sum(transaction.execute())
+
+    @property
+    def progress(self):
+        return self.total - len(self)
+
+
+class Worker(Base):
+    distributed = True
+
+    def __init__(self, tests, worker_id, redis, build_id,  # pylint: disable=too-many-arguments
+                 timeout, max_requeues=0, requeue_tolerance=0):
+        super(Worker, self).__init__(redis=redis, build_id=build_id)
+        self.timeout = timeout
+        self.total = len(tests)
+        self.max_requeues = max_requeues
+        self.global_max_requeues = math.ceil(len(tests) * requeue_tolerance)
+        self.worker_id = worker_id
+        self.shutdown_required = False
+        self._push(tests)
+
+    def __iter__(self):
+        def poll():
+            while not self.shutdown_required and len(self):  # pylint: disable=len-as-condition
+                test = self._reserve()
+                if test:
+                    yield test.decode()
+                else:
+                    time.sleep(0.05)
+
+        try:
+            self.wait_for_master()
+            for i in poll():
+                yield i
+        except redis.ConnectionError:
+            pass
+
+    def shutdown(self):
+        self.shutdown_required = True
+
+    def acknowledge(self, test):
+        return self._eval_script(
+            'acknowledge',
+            keys=[self.key('running'), self.key('processed')],
+            args=[test],
+        ) == 1
+
+    def requeue(self, test, offset=42):
+        if not (self.max_requeues > 0 and self.global_max_requeues > 0.0):
+            return False
+
+        return self._eval_script(
+            'requeue',
+            keys=[
+                self.key('processed'),
+                self.key('requeues-count'),
+                self.key('queue'),
+                self.key('running')],
+            args=[self.max_requeues, self.global_max_requeues, test, offset],
+        ) == 1
+
+    def retry_queue(self):
+        tests = [v.decode() for v in self.redis.lrange(
+            self.key('worker', self.worker_id, 'queue'), 0, -1)]
+        tests.reverse()
+        return Retry(
+            tests,
+            redis=self.redis,
+            build_id=self.build_id,
+        )
+
+    def _push(self, tests):
+        def push(tests):
+            transaction = self.redis.pipeline(transaction=True)
+            transaction.lpush(self.key('queue'), *tests)
+            transaction.set(self.key('total'), self.total)
+            transaction.set(self.key('master-status'), 'ready')
+            transaction.execute()
+
+        try:
+            self.is_master = self.redis.setnx(
+                self.key('master-status'),
+                'setup'
+            )
+            if self.is_master:
+                push(tests)
+
+            self._register()
+        except redis.ConnectionError:
+            if self.is_master:
+                raise
+
+    def _register(self):
+        self.redis.sadd(self.key('workers'), self.worker_id)
+
+    def _reserve(self):
+        return self._try_to_reserve_lost_test() or self._try_to_reserve_test()
+
+    def _try_to_reserve_lost_test(self):
+        if self.timeout:
+            return self._eval_script(
+                'reserve_lost',
+                keys=[
+                    self.key('running'),
+                    self.key('completed'),
+                    self.key(
+                        'worker',
+                        self.worker_id,
+                        'queue')],
+                args=[time.time(), self.timeout],
+            )
+
+    def _try_to_reserve_test(self):
+        return self._eval_script(
+            'reserve',
+            keys=[
+                self.key('queue'),
+                self.key('running'),
+                self.key('processed'),
+                self.key('worker', self.worker_id, 'queue'),
+            ],
+            args=[
+                time.time(),
+            ],
+        )
+
+    def _eval_script(self, script_name, keys=None, args=None):
+        keys = keys or []
+        args = args or []
+        if script_name not in self._scripts:
+            filename = 'redis/' + script_name + '.lua'
+
+            path = os.path.join(os.path.dirname(__file__), '../../', filename)
+            if not os.path.exists(path):
+                path = os.path.join(os.path.dirname(__file__), filename)
+
+            with open(path) as script_file:
+                self._scripts[script_name] = self.redis.register_script(
+                    script_file.read())
+
+        script = self._scripts[script_name]
+        return script(keys=keys, args=args)
+
+
+class Supervisor(Base):
+
+    def __init__(self, redis, build_id, *args, **kwargs):  # pylint: disable=unused-argument
+        super(Supervisor, self).__init__(redis=redis, build_id=build_id)
+
+    def _push(self, tests):
+        pass
+
+    def wait_for_workers(self, master_timeout=None):
+        if not self.wait_for_master(timeout=master_timeout):
+            return False
+
+        while len(self):  # pylint: disable=len-as-condition
+            time.sleep(0.1)
+
+        return True
+
+
+class Retry(static.Static):
+    distributed = True
+
+    def __init__(self, tests, redis, build_id):
+        super(Retry, self).__init__(tests)
+        self.redis = redis
+        self.build_id = build_id
+
+    def key(self, *args):
+        return ':'.join(['build', self.build_id] + map(str, args))
---
+++ packages/ciqueue/ciqueue-0.1.tar.gz/ciqueue-0.1/ciqueue/pytest.py
@@ -0,0 +1,181 @@
+"""
+This is the pytest plugin for running distributed tests.
+Example usage (run on each node):
+py.test -p ciqueue.pytest --queue redis://<host>:6379?worker=<worker_id>&build=<build_id>&retry=<n>
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+import zlib
+from ciqueue._pytest import test_queue
+from ciqueue._pytest import outcomes
+import dill
+import pytest
+from _pytest import terminal
+
+# pylint: disable=too-few-public-methods
+
+
+def pytest_addoption(parser):
+    """Add command line options to py.test command."""
+    parser.addoption('--queue', metavar='queue_url',
+                     type=str, help='The queue url',
+                     required=True)
+
+
+class ItemIndex(object):
+
+    def __init__(self, items):
+        self.index = dict((test_queue.key_item(i), i) for i in items)
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, key):
+        return self.index[key]
+
+    def __iter__(self):
+        return iter(self.index)
+
+    def keys(self):
+        return self.index.keys()
+
+
+class ItemList(object):
+
+    def __init__(self, index, queue):
+        self.index = index
+        self.queue = queue
+
+    def __getitem__(self, index):
+        return None
+
+    def __iter__(self):
+        for test in self.queue:
+            yield self.index[test]
+
+
+class RedisReporter(object):
+
+    def __init__(self, config, queue):
+        self.config = config
+        self.queue = queue
+        self.redis = queue.redis
+        self.errors_key = queue.key('error-reports')
+        self.terminalreporter = config.pluginmanager.get_plugin('terminalreporter')
+        if hasattr(self.terminalreporter, '_get_progress_information_message'):
+            self.__replace_progress_message()
+        self.terminalwriter = config.get_terminal_writer()
+        self.logxml = config._xml if hasattr(config, '_xml') else None  # pylint: disable=protected-access
+
+    def __replace_progress_message(self):  # pylint: disable=no-self-use
+        def _get_progress(self):  # pylint: disable=unused-argument
+            return ''
+
+        terminal.TerminalReporter._get_progress_information_message = _get_progress  # pylint: disable=protected-access
+
+    def record(self, item):
+        # if the test passed, we remove it from the errors queue
+        # otherwise we add it
+        if hasattr(item, 'error_reports'):
+            self.redis.hset(
+                self.errors_key,
+                test_queue.key_item(item),
+                zlib.compress(dill.dumps(item.error_reports)))
+        else:
+            self.redis.hdel(self.errors_key, test_queue.key_item(item))
+
+    def mark_as_skipped(self, call, item, msg):
+        assert call.when == 'teardown'
+
+        stats = self.terminalreporter.stats
+
+        def clear_out_stats(key):
+            if key in stats:
+                new_stats = []
+                for i in stats[key]:
+                    if i.nodeid != item.nodeid:
+                        new_stats.append(i)
+                    elif self.logxml:
+                        xmlkey = 'failure' if key == 'failed' else key
+                        self.logxml.stats[xmlkey] -= 1
+                stats[key] = new_stats
+                if not stats[key]:
+                    del stats[key]
+
+        # remove the failure/error from logxml
+        if self.logxml:
+            self.logxml.node_reporters_ordered[-1].nodes = []
+
+        # the call is converted to a skip
+        call.excinfo = outcomes.skipped_excinfo(item, msg)
+
+        # clear out the stats like the test never happened
+        for key in ('passed', 'error', 'failed'):
+            clear_out_stats(key)
+
+        # rollback the testsfailed number like it never happened
+        item.session.testsfailed -= len([v for k, v in item.error_reports.items()
+                                         if not issubclass(v['excinfo'].type, outcomes.Skipped) and k != 'teardown'])
+
+        # and clear out any state on the item like it never happened
+        if hasattr(item, 'error_reports'):
+            del item.error_reports
+
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_runtest_makereport(self, item, call):
+        """This function hooks into pytest's reporting of test results, and pushes a failed test's error report
+        onto the redis queue. A test can fail in any of the 3 call stages: setup, test, or teardown.
+        This is captured by pushing a dict of {call_state: error} for each failed test."""
+        if call.excinfo:
+            payload = call.__dict__.copy()
+            payload['excinfo'] = outcomes.swap_in_serializable(payload['excinfo'])
+
+            if not hasattr(item, 'error_reports'):
+                item.error_reports = {call.when: payload}
+            else:
+                item.error_reports[call.when] = payload
+
+        if call.when == 'teardown':
+            test_name = test_queue.key_item(item)
+            test_failed = outcomes.failed(item)
+
+            # Only attempt to requeue if the test failed.
+            # The method will return `False` if the test couldn't be requeued
+            if test_failed and self.queue.requeue(test_name):
+                self.mark_as_skipped(call, item, "WILL_RETRY")
+                self.terminalwriter.write(' WILL_RETRY ', green=True)
+
+            # If the test was already acknowledged by another worker (we timed out)
+            # Then we only record it if it was successful.
+            elif self.queue.acknowledge(test_name) or not test_failed:
+                self.record(item)
+
+            # The test timed out and failed, mark it as skipped so that it doesn't
+            # fail the build
+            else:
+                self.mark_as_skipped(call, item, "TIMED OUT")
+                self.terminalwriter.write(' TIMED OUT ', green=True)
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_runtestloop(session):
+    if (session.testsfailed and
+            not session.config.option.continue_on_collection_errors):
+        raise session.Interrupted(
+            "%d errors during collection" % session.testsfailed)
+
+    if session.config.option.collectonly:
+        return True
+
+    config = session.config
+    tests_index = ItemIndex(session.items)
+    queue = test_queue.build_queue(config.getoption('queue'), tests_index)
+    if queue.distributed:
+        config.pluginmanager.register(RedisReporter(config, queue))
+    session.items = ItemList(tests_index, queue)
+
+    for item in session.items:
+        item.config.hook.pytest_runtest_protocol(item=item, nextitem=None)
+        if session.shouldstop:
+            raise session.Interrupted(session.shouldstop)
+    return True
