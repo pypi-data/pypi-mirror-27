9a1040b8cfc394fa686df51351745b290df8470a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:07 +0000

    Add package marius/0.1.0/marius-0.1.0.tar.gz

---
+++ packages/marius/marius-0.1.0.tar.gz/marius-0.1.0/README.md
@@ -0,0 +1,20 @@
+## Marius
+--------
+
+Light weight job scheduling tool with more performance and scalability.
+
+
+### Installation
+
+```shell
+pip install sisyphus
+```
+
+### Usage Example
+
+```python
+
+```
+
+### License
+MIT
---
+++ packages/marius/marius-0.1.0.tar.gz/marius-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='marius',
+    packages=['marius.core', 'marius.helper'],
+    version='0.1.0',
+    description='Light weight job scheduling tool with more performance and scalability.',
+    url='https://github.com/Jack-Kingdom/marius.git',
+    license='MIT',
+    author='Jack King',
+    author_email='email@qiaohong.org',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Natural Language :: English',
+    ],
+)
