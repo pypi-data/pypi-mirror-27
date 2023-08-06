b726b19a1c8ca3dfa2df72c209f30a6b8b79c89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:01 +0000

    Add package enCompress/0.6/enCompress-0.6.tar.gz

---
+++ packages/enCompress/enCompress-0.6.tar.gz/enCompress-0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+     name='enCompress',    # This is the name of your PyPI-package.
+     version='0.6',                          # Update the version number for new releases
+     description='Creates a password protected compressed file',
+     install_requires=['pyminizip'],
+     python_requires='>=3',
+
+ )
+
