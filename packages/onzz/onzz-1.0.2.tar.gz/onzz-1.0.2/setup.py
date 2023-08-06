f3de48c3005aa994d96451960e1105b982a5ea41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package onzz/1.0.2/onzz-1.0.2.tar.gz

---
+++ packages/onzz/onzz-1.0.2.tar.gz/onzz-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name='onzz',
+		version='1.0.2',
+		py_modules=['onzz'],
+		author='onzz',
+		author_email='lipan@kaytune.com',
+		url='',
+		description='a onzz simple demo'
+)
