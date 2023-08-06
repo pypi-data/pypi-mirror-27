3f753f6ff66e750c088ece6864256d413e70de52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:30 +0000

    Add package django-core-waili/0.1.3/django-core-waili-0.1.3.tar.gz

---
+++ packages/django-core-waili/django-core-waili-0.1.3.tar.gz/django-core-waili-0.1.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='django-core-waili',
+    version='0.1.3',
+    description='django core utils',
+    author='linxuedong',
+    author_email='xuedonglin1994@gmail.com',
+    url='https://github.com/linxuedong/django-core',
+    py_modules=['django-core-waili'],
+)
