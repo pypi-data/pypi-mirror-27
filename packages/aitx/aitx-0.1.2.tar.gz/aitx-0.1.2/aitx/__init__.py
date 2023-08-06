77f5a30e09a94d3ac545b337ae34af00b437c4e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:26 +0000

    Add package aitx/0.1.2/aitx-0.1.2.tar.gz

---
+++ packages/aitx/aitx-0.1.2.tar.gz/aitx-0.1.2/aitx/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from aitx.imagebox import *
+
+import os
+__author__ = 'Chia-Jung, Yang'
+__email__ = 'jeroyang@gmail.com'
+__version__ = '0.1.2'
---
+++ packages/aitx/aitx-0.1.2.tar.gz/aitx-0.1.2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import sys
+import os
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+setup(
+    name='aitx',
+    version='0.1.2',
+    description="The toolbox for general ML projects",
+    long_description=readme,
+    author="Chia-Jung, Yang",
+    author_email='jeroyang@gmail.com',
+    url='https://github.com/jeroyang/aitx',
+    packages=[
+        'aitx',
+    ],
+    package_dir={'aitx':
+                 'aitx'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='aitx',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+)
