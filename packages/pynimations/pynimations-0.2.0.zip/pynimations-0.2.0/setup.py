6861d37d95519e22ea7f17d644bb50452c5d0a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:26 +0000

    Add package pynimations/0.2.0/pynimations-0.2.0.zip

---
+++ packages/pynimations/pynimations-0.2.0.zip/pynimations-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(name="pynimations",
+      platforms="Cross Platform",
+      packages=["pynimations"],
+      version="0.2.0",
+      description="All-in-one python library for program loading animations",
+      long_description="Visit https://tbhaxor.github.io/pynimations/",
+      author_email="tbhaxor@gmail.com",
+      author="Gurkirat Singh",
+      url="https://tbhaxor.github.io/pynimations/",
+      download_url="https://github.com/tbhaxor/pynimations/archive/pynimations-v0.2.0.tar.gz",
+      keywords="pynimations gurkirat_singh tbhaxor",
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6'],
+      license="GPL-3.0",
+      python_requires='>=3',
+
+      )
