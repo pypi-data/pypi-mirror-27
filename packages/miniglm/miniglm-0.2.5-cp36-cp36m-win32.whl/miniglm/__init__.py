56d152bbd75e82a5b222d68d480a105a2d3e02de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package miniglm/0.2.5/miniglm-0.2.5-cp36-cp36m-win32.whl

---
+++ packages/miniglm/miniglm-0.2.5-cp36-cp36m-win32.whl/miniglm/__init__.py
@@ -0,0 +1,42 @@
+'''
+    Minimalist pythonic matrix, vector, quaternion math.
+
+    Matrices are stored in column-major order.
+
+    This module is using glm.
+'''
+
+import os
+import sys
+
+if os.environ.get('READTHEDOCS') == 'True' or sys.argv[0] == 'completion.py':
+    from .mock import (
+        Vec2, Vec3, Vec4, Mat2, Mat3, Mat4, Quat,
+        mat4_perspective, mat4_ortho, mat4_look_at,
+        radians, degrees,
+    )
+
+else:
+    from .glm import (
+        Vec2, Vec3, Vec4, Mat2, Mat3, Mat4, Quat,
+        Vec2Array,
+        mat4_perspective, mat4_ortho, mat4_look_at,
+    )
+
+
+    def radians(deg):
+        return deg / 57.2957795131
+
+
+    def degrees(rad):
+        return rad * 57.2957795131
+
+
+__all__ = [
+    'Vec2', 'Vec3', 'Vec4', 'Mat2', 'Mat3', 'Mat4', 'Quat',
+    'Vec2Array',
+    'mat4_perspective', 'mat4_ortho', 'mat4_look_at',
+    'radians', 'degrees',
+]
+
+__version__ = '0.2.5'
