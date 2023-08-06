b4c2960760fdd24125696d2767f87bb5cc4bd72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:43 +0000

    Add package nester_hhg/1.0.1/nester_hhg-1.0.1.tar.gz

---
+++ packages/nester_hhg/nester_hhg-1.0.1.tar.gz/nester_hhg-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name            = 'nester_hhg',
+        version         = '1.0.1',
+        py_modules      = ['nester_hhg'],
+        author          = 'hhg',
+        author_email    = 'hh@dd.com',
+        url             = 'http://www.com',
+        description     = ' A simple',
+    )
