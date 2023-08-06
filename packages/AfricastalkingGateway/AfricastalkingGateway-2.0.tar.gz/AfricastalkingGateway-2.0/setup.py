82ca2bda90cb02db266e8ba092ec2cbb8a8b033c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:21 +0000

    Add package AfricastalkingGateway/2.0/AfricastalkingGateway-2.0.tar.gz

---
+++ packages/AfricastalkingGateway/AfricastalkingGateway-2.0.tar.gz/AfricastalkingGateway-2.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import sys
+import os
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='AfricastalkingGateway',
+    version='2.0',
+    packages=['africastalking'],
+    description='An Official Python library for communicating with the AfricasTalking REST API',
+    author='Ian Juma',
+    install_requires=[
+        'requests>=2.11.0'
+    ],
+    author_email='ijuma@africastalking.com',
+    url='https://github.com/AfricasTalkingLtd/africastalking-python',
+    download_url='https://codeload.github.com/AfricasTalkingLtd/africastalking-python/tar.gz/2.0',
+    keywords=['ussd', 'voice', 'sms', 'mpesa', 'payments', 'africastalking'],
+    classifiers=[],
+)
