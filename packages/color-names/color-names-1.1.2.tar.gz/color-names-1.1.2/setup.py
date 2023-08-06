73a46bf2645e2f8ba06385f3944cc45d39271b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:30 +0000

    Add package color-names/1.1.2/color-names-1.1.2.tar.gz

---
+++ packages/color-names/color-names-1.1.2.tar.gz/color-names-1.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='color-names',
+    version='1.1.2',
+    url='http://github.com/bpeterso2000/color-names',
+    keywords=[
+        'color', 'colour', 'colors' 'colours', 'names', 'hex', 'css',
+        'html', 'themes', 'styles', 'standards', 'ansi', '256', 'truecolor',
+    ],
+
+    author='Brian Peterson',
+    author_email='bpeterso2000@yahoo.com',
+
+    description='A curated collection of color names to RGB hex code mappings',
+
+    packages=find_packages(include=['color-names']),
+    include_package_data=True,
+    zipsafe=False,
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: Public Domain',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+
+    test_requires=['pytest'],
+)
