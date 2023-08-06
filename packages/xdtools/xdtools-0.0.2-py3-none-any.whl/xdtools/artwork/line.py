97a7ade56af536c2b47be9dcd8dcfb24317f41f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:31 +0000

    Add package xdtools/0.0.2/xdtools-0.0.2-py3-none-any.whl

---
+++ packages/xdtools/xdtools-0.0.2-py3-none-any.whl/xdtools/artwork/line.py
@@ -0,0 +1,39 @@
+"""
+Contains the definition of Line.
+"""
+
+from xdtools.utils.point import Point
+from xdtools.artwork.artwork import Artwork
+
+
+class Line(Artwork):
+    """
+    A Line.
+
+    === Attributes ===
+    name - The name of this Line as it appears in the Layers panel.
+	uid - The unique id of this Line.
+    position - The position of this Line.
+    start - The starting Point of this Line.
+    end - The ending Point of this Line.
+
+	=== Operations ===
+    """
+
+    def __init__(self, uid, start_x, start_y, end_x, end_y, name='Line', x=0, y=0):
+        """
+        Instantiate a new Line.
+        """
+        self.uid = uid
+        self.name = name
+        self.position = Point(x, y)
+        self.start = Point(start_x, start_y)
+        self.end = Point(end_x, end_y)
+
+    def __repr__(self):
+        """
+        Return a constructor-style representation of this Line.
+        """
+        return str.format(
+            "Line(uid=\'{}\', name=\'{}\', position={}, start={}, end={})",
+            self.uid, self.name, self.position, self.start, self.end)
