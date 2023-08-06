4dfbb6dddcd5c0c85f93c88605c5e18a9790b3f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:34 +0000

    Add package fuzzyfuzzer/0.0.3/fuzzyfuzzer-0.0.3.tar.gz

---
+++ packages/fuzzyfuzzer/fuzzyfuzzer-0.0.3.tar.gz/fuzzyfuzzer-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "fuzzyfuzzer",
+    packages=find_packages(where='fuzzy'),
+    package_dir={'fuzzy': 'fuzzy'},
+    entry_points={
+        'console_scripts': [
+            'fuzzy = fuzzy.__main__:main',
+        ]
+    },
+    version = '0.0.3',
+    description = "Fuzzy - An other web fuzzer",
+    long_description = "It is used to test website URLs with a wordlist.",
+    author = "SakiiR SakiiR (@SakiiR)",
+    url = "https://github.com/SakiiR/fuzzy",
+    install_requires=[
+        'aiohttp',
+        'colored',
+    ],
+)
