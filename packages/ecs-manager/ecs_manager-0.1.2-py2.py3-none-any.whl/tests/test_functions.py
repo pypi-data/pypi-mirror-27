10b941148c778b488c68e1f01007a31c652b733f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:52 +0000

    Add package ecs-manager/0.1.2/ecs_manager-0.1.2-py2.py3-none-any.whl

---
+++ packages/ecs-manager/ecs_manager-0.1.2-py2.py3-none-any.whl/tests/test_functions.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+from ecs_manager.functions import set_variables, merge_environ
+
+
+def test_set_variables():
+    d = {
+        'int': 123,
+        'str': '{{env}}',
+        'dict': {'nested_str': '{{ env }}'}
+    }
+    variables = {'env': 'test'}
+    assert set_variables(d, variables) == {
+        'int': 123,
+        'str': 'test',
+        'dict': {'nested_str': 'test'}
+    }
+
+
+def test_merge_environ():
+    definition = [
+        {
+            'environment': [
+                {'name': 'hoge', 'value': 'hoge'},
+                {'name': 'fuga', 'value': 'fuga'},
+            ]
+        }, {
+        }
+    ]
+
+    merge_environ(definition, {'hoge': 'hogehoge', 'piyo': 'piyopiyo'})
+
+    assert definition[0]['environment'][0] == {'name': 'fuga', 'value': 'fuga'}
+    assert definition[0]['environment'][1] == {'name': 'hoge', 'value': 'hogehoge'}
+    assert definition[0]['environment'][2] == {'name': 'piyo', 'value': 'piyopiyo'}
+    assert definition[1]['environment'][0] == {'name': 'hoge', 'value': 'hogehoge'}
+    assert definition[1]['environment'][1] == {'name': 'piyo', 'value': 'piyopiyo'}
+
