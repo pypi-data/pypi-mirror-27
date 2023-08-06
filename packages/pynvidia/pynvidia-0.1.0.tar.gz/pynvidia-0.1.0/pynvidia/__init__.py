067cf83d6cbd62363d734fd0dc13676678e5dabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:11 +0000

    Add package pynvidia/0.1.0/pynvidia-0.1.0.tar.gz

---
+++ packages/pynvidia/pynvidia-0.1.0.tar.gz/pynvidia-0.1.0/pynvidia/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from . import memory
+from .memory import *
+
+__version__ = '0.1.0'
---
+++ packages/pynvidia/pynvidia-0.1.0.tar.gz/pynvidia-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(name='pynvidia',
+      version='0.1.0',
+      description='NVIDIA GPU utilities for Python',
+      author='Jackson Huang',
+      author_email='jackson_huang@live.com',
+      url='https://github.com/jaxony/pynvidia',
+      license='MIT',
+      packages=['pynvidia'],
+      keywords=[
+          'Utilities',
+          'Deep Learning',
+          'NVIDIA',
+          'GPU'
+      ])
