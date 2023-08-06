dd5f4e695fff11840450b4d8ed343788c302945b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:47 +0000

    Add package parrotnester/1.0.0/parrotnester-1.0.0.tar.gz

---
+++ packages/parrotnester/parrotnester-1.0.0.tar.gz/parrotnester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'parrotnester',
+    version = '1.0.0',
+    py_modules = ['parrotnester'],
+    author = 'parrot',
+    author_email = 'wanghao8888@outlook.com',
+    url = 'http://www.abc.com',
+    description = 'A simple printer of nested lists',
+    )
