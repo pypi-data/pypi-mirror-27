82eb0311032f855ffa130ecfddd75f7c2fdaacf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:55 +0000

    Add package mbstrdecoder/0.2.3/mbstrdecoder-0.2.3.tar.gz

---
+++ packages/mbstrdecoder/mbstrdecoder-0.2.3.tar.gz/mbstrdecoder-0.2.3/mbstrdecoder/__init__.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+from ._mbstrdecoder import MultiByteStrDecoder
