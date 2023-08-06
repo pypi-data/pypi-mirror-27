ec3d141835c7c3929b89de01afced3e1420b717f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:46 +0000

    Add package badminton-queuing-system/0.12/badminton_queuing_system-0.12.tar.gz

---
+++ packages/badminton-queuing-system/badminton_queuing_system-0.12.tar.gz/badminton_queuing_system-0.12/setup.py
@@ -0,0 +1,14 @@
+# copyright 2017 Yi Wang
+# don't distribute without author's explicit authorization
+
+from distutils.core import setup
+
+setup(
+    name='badminton_queuing_system',
+    packages=['badminton_queuing_system'],
+    version='0.12',
+    description='Badminton Queuing system tailored for CBA',
+    url='http://github.com/yiw2000/badminton_queuing_system',
+    author='Yi Wang',
+    author_email='yiw2000@gmail.com',
+)
