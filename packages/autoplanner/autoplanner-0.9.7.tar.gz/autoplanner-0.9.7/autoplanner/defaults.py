8c3d7495a5fd332b075080dbdfa7121dafc983a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:57 +0000

    Add package autoplanner/0.9.7/autoplanner-0.9.7.tar.gz

---
+++ packages/autoplanner/autoplanner-0.9.7.tar.gz/autoplanner-0.9.7/autoplanner/defaults.py
@@ -0,0 +1,11 @@
+DF_JS = ['admin/js/core.js',
+         'admin/js/calendar.js',
+         'admin/js/admin/DateTimeShortcuts.js',
+         'autoplanner/js/autoplanner.js']
+DF_CSS = ['autoplanner/css/autoplanner.css']
+DF_INDEX_VIEW = 'autoplanner.views.index'
+DF_PROJECT_NAME = 'AutoPlanner'
+DF_SITE_SEARCH_VIEW = None
+# Make this unique, and don't share it with anybody.
+LP_SOLVE_PATH = 'lp_solve'
+REFRESH_DURATION = '1H'
