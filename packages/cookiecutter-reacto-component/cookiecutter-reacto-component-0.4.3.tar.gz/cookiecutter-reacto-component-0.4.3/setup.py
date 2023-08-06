4954805762c4d187042b9d3199c1cc1472c4868a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:13 +0000

    Add package cookiecutter-reacto-component/0.4.3/cookiecutter-reacto-component-0.4.3.tar.gz

---
+++ packages/cookiecutter-reacto-component/cookiecutter-reacto-component-0.4.3.tar.gz/cookiecutter-reacto-component-0.4.3/setup.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+from distutils.core import setup
+
+
+setup(
+	name='cookiecutter-reacto-component',
+	packages=[],
+	version='0.4.3',
+	description='Cookiecutter template for reacto component',
+	author='Artem Rudenko',
+	author_email='gvidon@ottofeller.com',
+	url='https://github.com/gvidon/cookiecutter-reacto-component',
+	keywords=['cookiecutter', 'template', 'package', 'react', 'js', ],
+)
