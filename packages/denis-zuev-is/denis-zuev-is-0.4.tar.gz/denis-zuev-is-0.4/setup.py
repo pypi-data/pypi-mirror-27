d7c822dabb385efcefb23dfe63815b587c3c5f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:15 +0000

    Add package denis-zuev-is/0.4/denis-zuev-is-0.4.tar.gz

---
+++ packages/denis-zuev-is/denis-zuev-is-0.4.tar.gz/denis-zuev-is-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+     name='denis-zuev-is',    
+     description='This program will tell you few things about Denis Zuev',
+     url='http://dzuev.pro',
+     author='Denis Zuev',
+     author_email='root@dzuev.pro',
+     license='MIT',
+     version='0.4',
+     scripts=['dzuev-is, dzuev-story']
+)
+
