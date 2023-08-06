52c3decd1e964924917eae45d058124958c7cf29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:04 +0000

    Add package sheepdoge/0.1.6/sheepdoge-0.1.6.tar.gz

---
+++ packages/sheepdoge/sheepdoge-0.1.6.tar.gz/sheepdoge-0.1.6/setup.py
@@ -0,0 +1,48 @@
+"""Configuration for the `sheepdoge` pip package."""
+
+import ast
+import re
+
+from setuptools import setup, find_packages
+
+GITHUB_URL = 'https://github.com/sheepdoge/sheepdoge'
+
+with open('sheepdoge/__version__.py', 'rb') as version_file:
+    file_contents = version_file.read().decode('utf-8')
+    version_re = re.compile(r'__version__\s+=\s+(.*)')
+    version = str(ast.literal_eval(version_re.search(file_contents).group(1)))
+
+setup(
+    name='sheepdoge',
+    version=version,
+    description='Manage your personal Unix machine(s) with Ansible.',
+    url=GITHUB_URL,
+    author='Matt McNaughton',
+    author_email='mattjmcnaughton@gmail.com',
+    license='Apache',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords=['provisioning', 'automation'],
+    packages=find_packages(where='.', exclude=['tests*']),
+    install_requires=[
+        'ansible>=2.0,<3.0',
+        'configparser>=3.0',
+        'click>=6.0,<7.0',
+        'PyYaml>=3.0,<4.0',
+        'six>=1.0,<2.0',
+        'futures>=3.0,<4.0; python_version == "2.7"'
+    ],
+    entry_points={
+        'console_scripts': ['sheepdoge = sheepdoge.cli:main']
+    }
+)
