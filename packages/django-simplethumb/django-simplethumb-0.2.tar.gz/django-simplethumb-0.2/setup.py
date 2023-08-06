de36692531d7220791d01865cd33e6a6b13b8c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:36 +0000

    Add package django-simplethumb/0.2/django-simplethumb-0.2.tar.gz

---
+++ packages/django-simplethumb/django-simplethumb-0.2.tar.gz/django-simplethumb-0.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-simplethumb',
+    version='0.2',
+    description='Django template tag to do some basic image processing, and cache the resulting image',
+    long_description=open('README.md').read(),
+    author='Jake Scaltreto',
+    author_email='jscaltreto@gmail.com',
+    url='http://github.com/jscaltreto/django-simplethumb',
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['django', 'six', 'django-appconf', 'Pillow', ],
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+        'Framework :: Django',
+    ]
+)
