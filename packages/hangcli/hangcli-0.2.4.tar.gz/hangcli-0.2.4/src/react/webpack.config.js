ab0c6ec6e1ad2f01e13af62a7fdc1d380950cca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:55 +0000

    Add package hangcli/0.2.4/hangcli-0.2.4.tar.gz

---
+++ packages/hangcli/hangcli-0.2.4.tar.gz/hangcli-0.2.4/src/react/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/, // 这里面的不解析
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader" // compiles Less to CSS
+        }]
+      }
+    ]
+  },
+};
