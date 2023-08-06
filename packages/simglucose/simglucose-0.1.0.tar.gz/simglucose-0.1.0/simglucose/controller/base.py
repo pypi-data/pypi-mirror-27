1c979c46b0d56dd63f834877c83980b1a7c43c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:39 +0000

    Add package simglucose/0.1.0/simglucose-0.1.0.tar.gz

---
+++ packages/simglucose/simglucose-0.1.0.tar.gz/simglucose-0.1.0/simglucose/controller/base.py
@@ -0,0 +1,29 @@
+from collections import namedtuple
+
+Action = namedtuple('ctrller_action', ['basal', 'bolus'])
+
+
+class Controller(object):
+    def __init__(self, init_state):
+        self.state = init_state
+
+    def policy(self, observation, reward, done, **info):
+        '''
+        Every controller must have this implementation!
+        ----
+        Inputs:
+        observation - a namedtuple defined in simglucose.simulation.env. It has
+                      CHO and CGM two entries.
+        reward      - current reward returned by environment
+        done        - True, game over. False, game continues
+        info        - additional information as key word arguments,
+                      simglucose.simulation.env.T1DSimEnv returns patient_name
+                      and sample_time
+        ----
+        Output:
+        action - a namedtuple defined at the beginning of this file. The
+                 controller action contains two entries: basal, bolus
+        '''
+        self.state = observation
+        action = Action(basal=0, bolus=0)
+        return action
