32005ab1489c4fe0c7bbcb12696be20c98995e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:04 +0000

    Add package wdwapp/0.0.6/wdwapp-0.0.6-py3-none-any.whl

---
+++ packages/wdwapp/wdwapp-0.0.6-py3-none-any.whl/wdwapp/__init__.py
@@ -0,0 +1,36 @@
+__version__ = '0.0.6'
+
+def main(global_config, **settings):
+
+    from pyramid.config import Configurator
+    from sqlalchemy import engine_from_config
+    from pyramid.session import SignedCookieSessionFactory
+    from .models import DBSession, Base, Root
+
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+
+    my_session_factory = SignedCookieSessionFactory(
+        settings['session.secret'])
+
+    config = Configurator(settings=settings,
+                    session_factory=my_session_factory,
+                    root_factory='wdwapp.models.Root')
+
+    config.include('pyramid_chameleon')
+
+    # Routes for web site
+    config.add_route('overview', '/')
+    config.add_route('wikipage_add', '/add')
+    config.add_route('wikipage_view', '/{uid}')
+    config.add_route('wikipage_edit', '/{uid}/edit')
+    config.add_route('home', '/howdy/{first}/{last}')
+
+    # Static things
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('deform_static', 'deform:static/')
+
+    # Let's go
+    config.scan('.views')
+    return config.make_wsgi_app()
