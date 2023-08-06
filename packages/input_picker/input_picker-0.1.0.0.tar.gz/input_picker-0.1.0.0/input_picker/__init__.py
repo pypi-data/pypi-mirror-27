aa74902a61fee5533b4b88d8399e3b001ec0863c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:01 +0000

    Add package input_picker/0.1.0.0/input_picker-0.1.0.0.tar.gz

---
+++ packages/input_picker/input_picker-0.1.0.0.tar.gz/input_picker-0.1.0.0/input_picker/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from .common import Stop, Help
+from .bool import pick_bool
+from .list import pick_item
