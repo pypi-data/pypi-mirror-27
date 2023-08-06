be8ff35a71f4d124296d3dfad62cdaaf75160c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:53 +0000

    Add package oyou/0.2/oyou-0.2.tar.gz

---
+++ packages/oyou/oyou-0.2.tar.gz/oyou-0.2/README.md
@@ -0,0 +1,23 @@
+# oYou
+Build low level tensorflow model with pleasure and flexibility
+
+## Why?
+Low level tensorflow api is powerful and flexible, but lacks code reuse.
+High level tensorflow api is simple and elegant, but lacks flexibility.
+
+oYou balance benefits and deficits of the high level and low level frameworks.
+Using oYou
+
+
+## Get started with oYou
+
+### install
+```terminal
+pip install oyou
+```
+
+### import
+```python
+import oyou
+```
+
---
+++ packages/oyou/oyou-0.2.tar.gz/oyou-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='oyou',
+      version='0.2',
+      description='Build and make use of tensorflow model with pleasure and flexibility',
+      url='http://github.com/sepmein/oyou',
+      author='Spencer Zhang',
+      author_email='sepmein@hotmail.com',
+      license='MIT',
+      packages=['oyou'],
+      zip_safe=False,
+      install_requires=[
+          'tensorflow',
+          'numpy'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'])
