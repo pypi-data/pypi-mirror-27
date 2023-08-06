810e9cf5ad00183d9542de9cebb7626e7096a15f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:47 +0000

    Add package consumeraffairs-django-experiments/1.4.0rc1/consumeraffairs_django_experiments-1.4.0rc1-py2.py3-none-any.whl

---
+++ packages/consumeraffairs-django-experiments/consumeraffairs_django_experiments-1.4.0rc1-py2.py3-none-any.whl/experiments/tests/test_lock.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+from test import lock_tests
+from unittest import skipIf
+
+from django.conf import settings
+
+from experiments.models import DbLock
+
+
+#@skipIf(not hasattr(LockTests, 'locktype'), 'test.lock_tests not found')
+@skipIf('sqlite' in settings.DATABASES['default']['ENGINE'],
+        'Not testing DbLock with SQLite')
+class DbLockTestCase(lock_tests.LockTests):
+
+    def locktype(self):
+        return DbLock('foo')
+
+    def test_extend(self):
+        lock = self.locktype()
+        lock.acquire()
+        self.assertTrue(lock.extend())
+        lock.release()
+        self.assertFalse(lock.extend())
+        del lock
+
+    def test_extend_expired(self):
+        lock = self.locktype()
+        lock.acquire()
+        self.assertTrue(lock.extend(0))
+        self.assertFalse(lock.extend())
+        del lock
