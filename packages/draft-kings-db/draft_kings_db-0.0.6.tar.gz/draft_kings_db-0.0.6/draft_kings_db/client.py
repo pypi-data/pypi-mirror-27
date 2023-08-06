4c3a8a8c070911017f7bf68188a1f93c0085eda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:17 +0000

    Add package draft-kings-db/0.0.6/draft_kings_db-0.0.6.tar.gz

---
+++ packages/draft-kings-db/draft_kings_db-0.0.6.tar.gz/draft_kings_db-0.0.6/draft_kings_db/client.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from draft_kings_db import models, db_data
+
+
+class DraftKingsHistory(object):
+    def __init__(self, db_url='sqlite:///:memory:', verbose=False):
+        self.most_recent_data = None
+        if verbose:
+            self.verbose = True
+        else:
+            self.verbose = False
+
+        engine = create_engine(db_url)
+        models.Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+    def __del__(self):
+        self.session.close()
+
+    def initialize_nba(self):
+        self.most_recent_data = db_data.retrieve_data(self.session, self.verbose)
+        print('Retrieved data from {} to {}'.format(
+            db_data.DB_START,
+            self.most_recent_data,
+        ))
+
+    def lookup_nba_performances(self, name, limit=5):
+        return (
+            self.session
+                .query(models.NBAPerformance)
+                .filter(models.NBAPerformance.name == name)
+                .limit(limit)
+                .all()
+        )
