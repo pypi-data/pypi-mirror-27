7a0eb74e8fddf75d8f7e8162d9d9f227f2ee487a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:38 +0000

    Add package languageflow/1.1.6a4/languageflow-1.1.6a4.tar.gz

---
+++ packages/languageflow/languageflow-1.1.6a4.tar.gz/languageflow-1.1.6a4/languageflow/model/__init__.py
@@ -0,0 +1,16 @@
+
+
+
+class Model:
+    def __init__(self, estimator, name):
+        self.estimator = estimator
+        self.name = name
+
+    def fit(self, X, y, filename=None):
+        pass
+
+    def predict(self, X):
+        pass
+
+
+
---
+++ packages/languageflow/languageflow-1.1.6a4.tar.gz/languageflow-1.1.6a4/languageflow/model/crf.py
@@ -0,0 +1,37 @@
+import pycrfsuite
+
+
+class CRF:
+    def __init__(self, params={'c1':0.1, 'c2':0.01, 'feature.minfreq':0}):
+        self.estimator = None
+        self.params = params
+
+    def fit(self, X, y):
+        """Fit CRF according to X, y
+
+        Parameters
+        ----------
+        X : list of text
+            each item is a text
+        y: list
+           each item is either a label (in multi class problem) or list of
+           labels (in multi label problem)
+        """
+        trainer = pycrfsuite.Trainer(verbose=True)
+        for xseq, yseq in zip(X, y):
+            trainer.append(xseq, yseq)
+
+        trainer.set_params(self.params)
+        self.estimator = trainer
+
+    def predict(self, X):
+        """Predict class labels for samples in X.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape = [n_samples, n_features]
+            Samples.
+        """
+        if isinstance(X[0], list):
+            return [self.estimator.tag(x) for x in X]
+        return self.estimator.tag(X)
