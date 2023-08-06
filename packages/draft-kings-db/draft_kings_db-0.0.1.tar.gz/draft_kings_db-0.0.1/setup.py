35cfc2c50547ea4cb64020ea8e0ae07c721664b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:30 +0000

    Add package draft-kings-db/0.0.1/draft_kings_db-0.0.1.tar.gz

---
+++ packages/draft-kings-db/draft_kings_db-0.0.1.tar.gz/draft_kings_db-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='draft_kings_db',
+    version='0.0.1',
+    packages=['draft_kings_db'],
+    url='',
+    license='MIT',
+    author='Ben Brostoff',
+    author_email='ben.brostoff@gmail.com',
+    description='A tool for retrieving DraftKings data.'
+)
