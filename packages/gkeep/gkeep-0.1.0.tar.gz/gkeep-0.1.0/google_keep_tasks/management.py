fae01ac88a46edf89e4fbca38e1b82e6be39160f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:40 +0000

    Add package gkeep/0.1.0/gkeep-0.1.0.tar.gz

---
+++ packages/gkeep/gkeep-0.1.0.tar.gz/gkeep-0.1.0/google_keep_tasks/management.py
@@ -0,0 +1,20 @@
+import click
+import os
+
+import gkeepapi
+from click import ClickException, FileError
+
+from google_keep_tasks.auth import get_auth
+
+
+@click.group()
+@click.option('--debug/--no-debug', default=None)
+@click.option('--auth', default=None)
+@click.pass_context
+def cli(ctx, debug, auth):
+    keep = gkeepapi.Keep()
+    success = keep.login(*get_auth())
+    ctx.obj = {'keep': keep}
+
+
+import google_keep_tasks.note
