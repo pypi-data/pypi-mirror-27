49db343ba3ea7eda48e303fb0614655b031d2ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:45 +0000

    Add package sklearn-xarray/0.1.2/sklearn_xarray-0.1.2-py2.py3-none-any.whl

---
+++ packages/sklearn-xarray/sklearn_xarray-0.1.2-py2.py3-none-any.whl/sklearn_xarray/__init__.py
@@ -0,0 +1,21 @@
+""" ``sklearn_xarray`` """
+
+from sklearn_xarray.common.wrappers import (
+    wrap, ClassifierWrapper, RegressorWrapper, TransformerWrapper)
+
+from sklearn_xarray.target import Target
+
+from sklearn_xarray.version import __version__, __release__
+
+import os
+
+
+__all__ = [
+    'wrap',
+    'ClassifierWrapper',
+    'RegressorWrapper',
+    'TransformerWrapper',
+    'Target'
+]
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
