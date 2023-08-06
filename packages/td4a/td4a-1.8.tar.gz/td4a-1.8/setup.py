b632522ad27c7756e797984f125d692ddf16434a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:30 +0000

    Add package td4a/1.8/td4a-1.8.tar.gz

---
+++ packages/td4a/td4a-1.8.tar.gz/td4a-1.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='td4a',
+      version='1.8',
+      description='A browser based jinja template renderer',
+      url='http://github.com/cidrblock/td4a',
+      author='Bradley A. Thornton',
+      author_email='brad@thethorntons.net',
+      license='MIT',
+      include_package_data=True,
+      packages=[
+          'td4a'
+      ],
+      scripts=['td4a-server'],
+      install_requires=[
+          'ansible==2.4.1.0',
+          'Flask==0.12.2',
+          'netaddr==0.7.19',
+          'Twisted==17.9.0',
+          'requests==2.18.4',
+          'ruamel.yaml==0.15.35',
+          'genson==0.2.3',
+          'jsonschema==2.6.0'
+      ],
+      zip_safe=False)
