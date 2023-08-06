853c73dc6583adcf2a6ef354f28515b014623140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:09 +0000

    Add package arkdbtools/0.2.14/arkdbtools-0.2.14.tar.gz

---
+++ packages/arkdbtools/arkdbtools-0.2.14.tar.gz/arkdbtools-0.2.14/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='arkdbtools',
+    version='0.2.14',
+    packages=['arkdbtools', 'arkdbtools.tests'],
+    url='https://github.com/BlockHub/arkdbtools',
+    license='MIT',
+    author='Charles',
+    author_email='k.l.kubat@gmail.com',
+    description=''
+)
