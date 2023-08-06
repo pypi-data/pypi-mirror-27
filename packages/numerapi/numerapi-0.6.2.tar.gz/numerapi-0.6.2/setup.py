326c5dfb5209103f392afc0f29dde836cf279266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:38 +0000

    Add package numerapi/0.6.2/numerapi-0.6.2.tar.gz

---
+++ packages/numerapi/numerapi-0.6.2.tar.gz/numerapi-0.6.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+numerapi_version = '0.6.2'
+
+
+setup(
+    name="numerapi",
+    version=numerapi_version,
+    maintainer="uuazed",
+    maintainer_email="uuazed@gmail.com",
+    description="Automatically download and upload data for the Numerai machine learning competition",
+    url='https://github.com/uuazed/numerapi',
+    platforms="OS Independent",
+    license='MIT License',
+    package_data={'numerai': ['LICENSE', 'README.md']},
+    packages=find_packages(exclude=['tests']),
+    install_requires=["requests"]
+)
