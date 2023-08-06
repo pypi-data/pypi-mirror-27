a061390ddc0f7bd061d932f69b7af6a04eeddd2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:49 +0000

    Add package istype/0.1.0.0/istype-0.1.0.0.tar.gz

---
+++ packages/istype/istype-0.1.0.0.tar.gz/istype-0.1.0.0/istype/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from .common import ISA, IS
+
+# for init all module:
+from .any import typing
+from .dict import typing
+from .typevar import typing
+from .tuplemeta import typing
+from .type import typing
+from .union import typing
+
+__all__ = ['ISA', 'IS']
