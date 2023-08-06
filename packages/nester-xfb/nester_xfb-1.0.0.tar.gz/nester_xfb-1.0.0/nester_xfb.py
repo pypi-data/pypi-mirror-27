1eb1206665aba4c0e17ab917e0fe31c30e21dfed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:16 +0000

    Add package nester-xfb/1.0.0/nester_xfb-1.0.0.tar.gz

---
+++ packages/nester-xfb/nester_xfb-1.0.0.tar.gz/nester_xfb-1.0.0/nester_xfb.py
@@ -0,0 +1,10 @@
+'这是一个模块，名字是nester_xfb'
+'提供了一个名为print_lol()的函数'
+def print_lol(the_list):
+    """这是一个可递归输出列表的函数
+参数是the_list"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
---
+++ packages/nester-xfb/nester_xfb-1.0.0.tar.gz/nester_xfb-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+            name                ='nester_xfb',
+            version             ='1.0.0',
+            py_modules      =['nester_xfb'],
+            author              ='fabien_xu',
+            author_email    ='xfb0729@163.com',
+            url                    =' ',
+            description       ='a module contains a function to print a list recursively',
+        )
+            
