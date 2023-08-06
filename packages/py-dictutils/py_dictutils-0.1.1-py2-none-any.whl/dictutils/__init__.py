629ba7a3c5f07611419adf7955bf422f22df4a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:56 +0000

    Add package py-dictutils/0.1.1/py_dictutils-0.1.1-py2-none-any.whl

---
+++ packages/py-dictutils/py_dictutils-0.1.1-py2-none-any.whl/dictutils/__init__.py
@@ -0,0 +1,6 @@
+from .attr_dict import AttrDict
+from .ordered_attr_dict import OrderedAttrDict
+from .utils import deep_getattr
+from .xml_dict import xml_to_dict
+
+__all__ = ['AttrDict', 'OrderedAttrDict', 'deep_getattr', 'xml_to_dict']
