c18fb66a80c20f9a290cb1c3c0e6d03ff5b52f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:13 +0000

    Add package pkglts/2.2.1/pkglts-2.2.1-py2.py3-none-any.whl

---
+++ packages/pkglts/pkglts-2.2.1-py2.py3-none-any.whl/pkglts/option/travis/handlers.py
@@ -0,0 +1,20 @@
+from pkglts.option.doc import fmt_badge
+
+
+def environment_extensions(cfg):
+    """Add more functionality to an environment.
+
+    Args:
+        cfg (Config):  current package configuration
+
+    Returns:
+        dict of str: any
+    """
+    owner = cfg['github']['owner']
+    project = cfg['github']['project']
+
+    url = "travis-ci.org/%s/%s" % (owner, project)
+    img = url + ".svg?branch=master"
+    badge = fmt_badge(img, url, "Travis build status", cfg['doc']['fmt'])
+
+    return {"badge": badge}
