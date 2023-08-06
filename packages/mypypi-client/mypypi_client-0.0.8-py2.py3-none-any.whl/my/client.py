6673547708adf863334f0242a067e1284b8f51f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:17 +0000

    Add package mypypi-client/0.0.8/mypypi_client-0.0.8-py2.py3-none-any.whl

---
+++ packages/mypypi-client/mypypi_client-0.0.8-py2.py3-none-any.whl/my/client.py
@@ -0,0 +1,12 @@
+
+def sum(a, b):
+    print('Adding a and b...')
+    return a + b
+
+def sum2(a, b):
+    print 'Hello py 2!', a, b
+    return a + b
+
+def sum3(a, b):
+    print('Hello py 3!', a, b)
+    return a + b
