cff7078bba98800c59016db1532d87cd5235f333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:22 +0000

    Add package seaborn-table/1.1.5/seaborn_table-1.1.5.tar.gz

---
+++ packages/seaborn-table/seaborn_table-1.1.5.tar.gz/seaborn_table-1.1.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='seaborn_table',
+    version='1.1.5',
+    description='SeabornTable reads and writes tables in '
+                'csv and md and acts like a list and dict."',
+    long_description='',
+    author='Ben Christenson',
+    author_email='Python@BenChristenson.com',
+    url='https://github.com/christensonb/SeabornTable',
+    download_url='https://github.com/christensonb/SeabornTable/archive/0.1.tar.gz',
+    install_requires=[],
+    extras_require={
+        'test': ['test-chain'],
+    },
+    py_modules=['seaborn.seaborn_table'],
+    license='MIT License',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: Other/Proprietary License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'),
+    entry_points='''
+        [console_scripts]
+        seaborn_table=seaborn.seaborn_table:main
+    ''',
+)
