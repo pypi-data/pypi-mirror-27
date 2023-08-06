863a1ee7c0a66576a974d2ed25d9252525d240ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:38 +0000

    Add package hangcli/0.2.7/hangcli-0.2.7.tar.gz

---
+++ packages/hangcli/hangcli-0.2.7.tar.gz/hangcli-0.2.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='hangcli',
+    version='0.2.7',
+    py_modules=['hangcli'],
+    url = "http://blog.coderhelper.cn",
+    author = "limhang",
+    author_email = "lm_hang@163.com",
+    data_files=[('hangcli/react', ['src/react/package.json']),('hangcli/react', ['src/react/.babelrc']), ('hangcli/react', ['src/react/index.html']) ,('hangcli/react', ['src/react/index.js']), ('hangcli/react', ['src/react/webpack.config.js'])],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        hangcli=hangcli:router
+    ''',
+)
