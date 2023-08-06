0d1d70d2398cc7cc1b984b847e6b0b87f8eb73a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:01 +0000

    Add package htmldate/0.3.1/htmldate-0.3.1.tar.gz

---
+++ packages/htmldate/htmldate-0.3.1.tar.gz/htmldate-0.3.1/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Seamlessly extract the date of web pages based on header or body.
+http://github.com/adbar/htmldate
+"""
+
+from codecs import open # python2
+import os
+from setuptools import setup # find_packages,
+
+#try:
+#    from setuptools import setup
+#except ImportError:
+#    from distutils.core import setup
+
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+packages = ['htmldate']
+
+
+def readme():
+    with open(os.path.join(here, 'README.rst'), 'r', 'utf-8') as readmefile:
+        return readmefile.read()
+
+setup(
+    name='htmldate',
+    version='0.3.1',
+    description='Seamlessly extract the creation or modification date of web pages by scraping the HTML code or performing content guesses.',
+    long_description=readme(),
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        #'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 6 - Mature',
+        #'Development Status :: 7 - Inactive',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+    keywords=['metadata-extraction', 'date-parser', 'html-parsing', 'webarchives', 'web-scraping'],
+    url='http://github.com/adbar/htmldate',
+    author='Adrien Barbaresi',
+    author_email='adrien.barbaresi@oeaw.ac.at',
+    license='GPLv3+',
+    packages=packages,
+    include_package_data=True,
+    install_requires=[
+        'dateparser == 0.6.0',
+        'lxml >= 4.0.0',
+        'requests >= 2.18.0',
+    ],
+    # python_requires='>=3',
+    entry_points = {
+        'console_scripts': ['htmldate=htmldate.cli:main'],
+    },
+    # platforms='any',
+    tests_require=['pytest', 'tox'],
+    zip_safe=False,
+)
