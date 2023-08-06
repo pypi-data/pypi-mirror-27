c1c69a4cee356adf42081142cfc79a47be1deeeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:18 +0000

    Add package os_nova_ha_monitor/1.1.15/os_nova_ha_monitor-1.1.15.tar.gz

---
+++ packages/os_nova_ha_monitor/os_nova_ha_monitor-1.1.15.tar.gz/os_nova_ha_monitor-1.1.15/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='os_nova_ha_monitor',
+      version='1.1.15',
+      description='Python Openstack Nova HA monitor package',
+      author='Premysl Kouril',
+      author_email='pkouril@cra.cz',
+      url='http://cra.cz',
+      packages=['nova_ha_monitor'],
+      license='MIT',
+      long_description=open('README.md').read(),
+      scripts=[
+          'scripts/ha_monitor',
+          'scripts/health_aggregate',
+          'scripts/pick_recovery_target',
+          'scripts/runtime_info',
+          'scripts/ssh_check',
+      ],
+      install_requires=[
+          'os_nova_ha_utils',
+          'python-consul',
+          'shade',
+          'pyghmi',
+          'paramiko',
+          'requests',
+          'retrying',
+          'mdstat',
+          'python-novaclient'
+      ],
+      setup_requires=[
+          'os_nova_ha_utils',
+          'python-consul',
+          'shade',
+          'pyghmi',
+          'paramiko',
+          'requests',
+          'retrying',
+          'mdstat',
+          'python-novaclient'
+      ],
+      tests_require=[
+          'pytest',
+          'mock'
+      ],
+     )
