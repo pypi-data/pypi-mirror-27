bc559b9c267ac9bf98f1581f2756d54772bf9128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:22 +0000

    Add package py-vision/0.1.6/py-vision-0.1.6.tar.gz

---
+++ packages/py-vision/py-vision-0.1.6.tar.gz/py-vision-0.1.6/pyvision/filters/lee_filter.py
@@ -0,0 +1,20 @@
+from scipy.ndimage.filters import uniform_filter
+
+def transform(band, window, var_noise = 0.25):
+    """
+    Expects single band from SAR image, already re-shaped
+
+    :param band: single band data from SAR image
+    :param window: descpeckling filter window
+    :param var_noise: noise variance, default = 0.25
+
+    """
+    
+    mean_window = uniform_filter(band, window)
+    mean_sqr_window = uniform_filter(band**2, window)
+    var_window = mean_sqr_window - mean_window**2
+
+    weights = var_window / (var_window + var_noise)
+    band_filtered = mean_window + weights*(band - mean_window)
+
+    return band_filtered
