4ec14147317fa25e500be6ed7d6289fdc0493f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:25 +0000

    Add package benford-py/0.1.0.2/benford_py-0.1.0.2.tar.gz

---
+++ packages/benford-py/benford_py-0.1.0.2.tar.gz/benford_py-0.1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='benford_py',
+      version='0.1.0.2',
+      description='A library for testing data sets with Bendford\'s Law',
+      url='https://github.com/milcent/benford_py',
+      download_url=
+        'https://github.com/milcent/benford_py/archive/v.0.1.0.2.tar.gz',
+      author='Marcel Milcent',
+      author_email='marcelmilcent@gmail.com',
+      license='GPLv3.0',
+      packages=['benford'],
+      install_requires=[
+      	'pandas',
+      	'numpy',
+      	'matplotlib',
+      ],
+      zip_safe=False)
