9aec35ee8318bc5e7b8547cf3956ac04547cf8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:34 +0000

    Add package python-memcached/1.59/python-memcached-1.59.tar.gz

---
+++ packages/python-memcached/python-memcached-1.59.tar.gz/python-memcached-1.59/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools.depends import get_module_constant
+from setuptools import setup  # noqa
+
+
+version = get_module_constant('memcache', '__version__')
+setup(
+    name="python-memcached",
+    version=version,
+    description="Pure python memcached client",
+    long_description=open("README.md").read(),
+    author="Evan Martin",
+    author_email="martine@danga.com",
+    maintainer="Sean Reifschneider",
+    maintainer_email="jafo@tummy.com",
+    url="https://github.com/linsomniac/python-memcached",
+    download_url="https://github.com/linsomniac/python-memcached/releases/download/{0}/python-memcached-{0}.tar.gz".format(version),
+    py_modules=["memcache"],
+    install_requires=open('requirements.txt').read().split(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
