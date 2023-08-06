506af34fe8192d218028c63f9a0540093c6c2b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:56 +0000

    Add package rp/0.1.18/rp-0.1.18.tar.gz

---
+++ packages/rp/rp-0.1.18.tar.gz/rp-0.1.18/rp/r_iterm_comm.py
@@ -0,0 +1,9 @@
+# Getting variables to be shared between modules can be difficult.
+# This class exists to that modules from r.py can talk to modules in the rp_ptpython package.
+# Essentially, this class is th middle-man. The other modules will both read and write to this module.
+globa=[]
+last_assignable_comm=None
+pseudo_terminal_level=0
+realtime_display_string="oijijoijo"
+rp_evaluator=lambda x:None
+current_input_text=""
