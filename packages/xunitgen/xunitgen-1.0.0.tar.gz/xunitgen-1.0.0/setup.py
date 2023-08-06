45333191d9901c68735cbd23037f0f87929d8d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:30 +0000

    Add package xunitgen/1.0.0/xunitgen-1.0.0.tar.gz

---
+++ packages/xunitgen/xunitgen-1.0.0.tar.gz/xunitgen-1.0.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name="xunitgen",
+    version="1.0.0",
+    description="Generate xUnit.xml files",
+    author="Nicolas Léveillé",
+    author_email="nicolas@uucidl.com",
+    url="https://github.com/uucidl/uu.xunitgen",
+    packages=["xunitgen"],
+    license='MIT',
+    long_description=open('README').read(),
+)
