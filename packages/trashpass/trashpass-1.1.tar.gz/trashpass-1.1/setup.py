fa457f1ce3282fe66c243daf2b5ba204a5f1acbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:38 +0000

    Add package trashpass/1.1/trashpass-1.1.tar.gz

---
+++ packages/trashpass/trashpass-1.1.tar.gz/trashpass-1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'trashpass',
+  packages = ['trashpass'], 
+  version = '1.1',
+  description = 'Library that helps bot developer to read disposable email from Trash-Mail',
+  author = 'Herdian Nugraha',
+  author_email = 'herdiannugraha@gmail.com',
+  url = 'https://github.com/hrdn/trashpass',  
+  keywords = ['scraper', 'disposable email'], 
+  classifiers = [],
+)
