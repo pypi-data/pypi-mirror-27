5e6255fd8fd069516f0acab243d0371a47c370ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:49 +0000

    Add package chronoamperometry/0.1.0a1/chronoamperometry-0.1.0a1.tar.gz

---
+++ packages/chronoamperometry/chronoamperometry-0.1.0a1.tar.gz/chronoamperometry-0.1.0a1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='chronoamperometry',
+    version='0.1.0a1',
+    packages=['chronoamperometry'],
+    url='https://github.com/Zer0Credibility/chronoamperometry',
+    license='4-clause BSD',
+    author='Clayton Rabideau',
+    author_email='cmr57@cam.ac.uk',
+    description='A toolset for analyzing chronoamperometric data',
+    install_requires=[
+                       'numpy',
+                       'pandas',
+                       'plotnine',
+                       'tqdm',
+                       'scipy',
+                   ]
+)
