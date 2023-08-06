ea0ef7f1950918e7edb25899b651038d50e25227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:16 +0000

    Add package ghtrending/0.1.3/ghtrending-0.1.3.tar.gz

---
+++ packages/ghtrending/ghtrending-0.1.3.tar.gz/ghtrending-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+requires = [
+    'lxml',
+    'requests',
+]
+
+setup(
+    name='ghtrending',
+    version='0.1.3',
+    license='MIT',
+    description='Github Trending',
+    author='chinsyo',
+    author_email='chinsyo@sina.cn',
+    packages=['ghtrending'],
+    package_data={'': ['LICENSE', 'NOTICE']},
+    package_dir={'ghtrending':'ghtrending'},
+    include_package_data=True,
+    install_requires=requires,
+    zip_safe=False
+)
