ece07bb729b82364c6828be93c410094d7691327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:54 +0000

    Add package pyhacores/0.1.0/pyhacores-0.1.0.tar.gz

---
+++ packages/pyhacores/pyhacores-0.1.0.tar.gz/pyhacores-0.1.0/pyhacores/filter/__init__.py
@@ -0,0 +1,3 @@
+from .moving_average import MovingAverage
+from .dc_removal import DCRemoval
+from .fir import FIR
---
+++ packages/pyhacores/pyhacores-0.1.0.tar.gz/pyhacores-0.1.0/under_construction/fsk_demodulator/model.py
@@ -0,0 +1,41 @@
+import numpy as np
+from pyha.common.hwsim import HW
+from pyha.common.sfix import Sfix, ComplexSfix
+from pyhacores.moving_average.model import MovingAverage
+from pyhacores.under_construction.quadrature_demodulator.model import QuadratureDemodulator
+
+
+class FSKDemodulator(HW):
+    """
+    Takes in complex signal and gives out bits. It uses Quadrature demodulator followed by
+    matched filter (moving average). M&M clock recovery is the last DSP block, it performs timing recovery.
+
+    .. note:: M&M clock recovery is currently not implemented
+    """
+
+    def __init__(self, deviation, fs, sps):
+        self.gain = fs / (2 * np.pi * deviation) / np.pi
+
+        self.demod = QuadratureDemodulator(self.gain)
+        self.match = MovingAverage(sps)
+
+        # constants
+        self._delay = self.demod._delay + self.match._delay
+
+    def main(self, input):
+        """
+        :type  input: ComplexSfix
+        :rtype: Sfix
+        """
+        demod = self.demod.main(input)
+        match = self.match.main(demod)
+
+        return match
+
+    def model_main(self, input_list):
+        demod = self.demod.model_main(input_list)
+        match = self.match.model_main(demod)
+
+        return match
+
+
