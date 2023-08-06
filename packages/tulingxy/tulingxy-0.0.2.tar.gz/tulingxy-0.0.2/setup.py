3baa980c997c3514b9b66bf3511f0b91bc72c445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:44 +0000

    Add package tulingxy/0.0.2/tulingxy-0.0.2.tar.gz

---
+++ packages/tulingxy/tulingxy-0.0.2.tar.gz/tulingxy-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'tulingxy',
+  packages = ['tulingxy'],
+  version = '0.0.2',
+  description = 'Get All Cars Brand',
+  long_description='Get All Cars Brand From juhe API',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'bot', 'python3','python','tuling'],
+  license='MIT',
+)
