7bbe193a9d2246c878054d9a4ece5eb058503999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:00 +0000

    Add package cfgcaddy/0.0.2/cfgcaddy-0.0.2.tar.gz

---
+++ packages/cfgcaddy/cfgcaddy-0.0.2.tar.gz/cfgcaddy-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='cfgcaddy',
+      version='0.0.2',
+      description='Tool for managing configuration files',
+      url='https://github.com/tstapler/cfgcaddy',
+      author='Tyler Stapler',
+      author_email='tystapler@gmail.com',
+      license='MIT',
+      packages=['cfgcaddy'],
+      install_requires=[
+          "click",
+          "enum34",
+          "pathspec",
+          "pyyaml",
+          "whaaaaat",
+      ],
+      entry_points={
+          'console_scripts': [
+              'cfgcaddy = cfgcaddy.__main__:main'
+          ]
+      },
+      zip_safe=True)
