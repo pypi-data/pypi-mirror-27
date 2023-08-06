505d919e0d4765b0834131e9c7cc929016e9f119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:28 +0000

    Add package carbrandxy/0.0.1/carbrandxy-0.0.1.tar.gz

---
+++ packages/carbrandxy/carbrandxy-0.0.1.tar.gz/carbrandxy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'carbrandxy',
+  packages = ['carbrandxy'],
+  version = '0.0.1',
+  description = 'Get All Cars Brand',
+  long_description='Get All Cars Brand From juhe API',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'carbrandxy', 'python3','python'],
+  license='MIT',
+)
