dd268e647b373778750303090681e13e6575ba90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:35 +0000

    Add package shudder/0.2.0/shudder-0.2.0.tar.gz

---
+++ packages/shudder/shudder-0.2.0.tar.gz/shudder-0.2.0/setup.py
@@ -0,0 +1,22 @@
+"""A simple service for capturing autoscaling lifecycle hook actions
+and notifying another service that it needs a graceful shutdown.
+
+"""
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setup(
+    name='shudder',
+    description="Graceful shutdowns using autoscaling lifecycle hooks.",
+    version='0.2.0',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    author='Anthony Grimes',
+    author_email='anthony@scopely.com',
+    url='https://github.com/scopely/shudder',
+    license='Apache 2.0',
+    install_requires=requirements
+)
---
+++ packages/shudder/shudder-0.2.0.tar.gz/shudder-0.2.0/shudder/__main__.py
@@ -0,0 +1,63 @@
+# Copyright 2014 Scopely, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Start polling of SQS and metadata."""
+import shudder.queue as queue
+import shudder.metadata as metadata
+from shudder.config import CONFIG
+
+import time
+import os
+import requests
+import signal
+import subprocess
+import sys
+
+def receive_signal(signum, stack):
+    if signum in [1,2,3,15]:
+        print 'Caught signal %s, exiting.' %(str(signum))
+        sys.exit()
+    else:
+        print 'Caught signal %s, ignoring.' %(str(signum))
+
+if __name__ == '__main__':
+    uncatchable = ['SIG_DFL','SIGSTOP','SIGKILL']
+    for i in [x for x in dir(signal) if x.startswith("SIG")]:
+        if not i in uncatchable:
+            signum = getattr(signal,i)
+            signal.signal(signum,receive_signal)
+
+    sqs_connection, sqs_queue = queue.create_queue()
+    sns_connection, subscription_arn = queue.subscribe_sns(sqs_queue)
+    while True:
+        message = queue.poll_queue(sqs_connection, sqs_queue)
+        if message or metadata.poll_instance_metadata():
+            queue.clean_up_sns(sns_connection, subscription_arn, sqs_queue)
+            if 'endpoint' in CONFIG:
+                requests.get(CONFIG["endpoint"])
+            if 'endpoints' in CONFIG:
+                for endpoint in CONFIG["endpoints"]:
+                    requests.get(endpoint)
+            if 'commands' in CONFIG:
+                for command in CONFIG["commands"]:
+                    print 'Running command: %s' % command
+                    process = subprocess.Popen(command)
+                    while process.poll() is None:
+                        time.sleep(30)
+                        """Send a heart beat to aws"""
+                        queue.record_lifecycle_action_heartbeat(message)
+            """Send a complete lifecycle action"""
+            queue.complete_lifecycle_action(message)
+            sys.exit(0)
+        time.sleep(5)
