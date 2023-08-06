66d08434730d1d7e726fc74c93b9b4a67b220c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:36 +0000

    Add package Rest-Utils/0.1.2/rest-utils-0.1.2.tar.gz

---
+++ packages/Rest-Utils/rest-utils-0.1.2.tar.gz/rest-utils-0.1.2/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2014 windpro
+
+Author  :   windpro
+E-mail  :   windprog@gmail.com
+Date    :   14-12-1
+Desc    :   
+"""
+from pip.req import parse_requirements
+from setuptools import setup
+
+__version__ = "0.1.2"
+__author__ = "windprog"
+__author_email__ = "windprog@gmail.com"
+__description__ = "gunicorn wrapper. support worker"
+__title__ = 'rest-utils'
+__url__ = "https://github.com/windprog/rest-utils"
+
+try:
+    install_reqs = parse_requirements('requirements.txt')
+    INSTALL_REQUIRES = [str(ir.req) for ir in install_reqs]
+except:
+    INSTALL_REQUIRES = []
+
+setup(
+    name=__title__,
+    version=__version__,
+    description=__description__,
+    url=__url__,
+    author=__author__,
+    author_email=__author_email__,
+    packages=['rest_utils', 'rest_utils.worker'],
+    install_requires=INSTALL_REQUIRES
+)
