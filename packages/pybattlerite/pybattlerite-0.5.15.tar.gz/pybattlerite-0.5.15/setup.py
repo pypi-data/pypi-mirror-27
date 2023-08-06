dd85695bdc213a41abf07e1f9db404028b4fdcab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:44 +0000

    Add package pybattlerite/0.5.15/pybattlerite-0.5.15.tar.gz

---
+++ packages/pybattlerite/pybattlerite-0.5.15.tar.gz/pybattlerite-0.5.15/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name = "pybattlerite",
+    packages = ['pybattlerite'],
+    version = "0.5.15",
+    description= "A wrapper for the madglory Battlerite API, complete with synchronous and asynchronous client implementations.",
+    author = "xKynn",
+    author_email = "xkynn@github.com",
+    url = "https://github.com/xKynn/PyBattlerite",
+    download_url = "https://github.com/xKynn/PyBattlerite/archive/0.5.15.tar.gz",
+    keywords = ['battlerite', 'asyncbattlerite', 'async-battlerite', 'async_battlerite'],
+    classifiers = [],
+    install_requires=[
+        "aiohttp",
+        "requests"
+    ]
+)
