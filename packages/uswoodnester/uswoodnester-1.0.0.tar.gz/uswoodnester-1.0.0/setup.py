a3c1ce32c54a1dce4dd33e24bb67b568a7cc259e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:14 +0000

    Add package uswoodnester/1.0.0/uswoodnester-1.0.0.tar.gz

---
+++ packages/uswoodnester/uswoodnester-1.0.0.tar.gz/uswoodnester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name			= 'uswoodnester',
+	version			= '1.0.0',
+	py_modules		= ['nester'],
+	author			= 'Zhangkeyi',
+	author_email	= 'uswood@163.com',
+	description		='A simple printer of nested lists.'
+)
