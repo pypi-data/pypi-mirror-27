f8a7dc97af24873f585a527199f68de23ff3774e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:45 +0000

    Add package nose2-cprofiler-plugin/1.0.0/nose2-cprofiler-plugin-1.0.0.tar.gz

---
+++ packages/nose2-cprofiler-plugin/nose2-cprofiler-plugin-1.0.0.tar.gz/nose2-cprofiler-plugin-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+setup(
+    name='nose2-cprofiler-plugin',
+    version='1.0.0',
+    description='cprofiler plugin for nose2',
+    url='https://github.com/stratoscale/nose2-cprofiler-plugin',
+    author='Noam Chadash',
+    author_email='noam@stratoscale.com',
+    keywords='cprofiler nose2 plugin',
+    packages=find_packages()
+)
