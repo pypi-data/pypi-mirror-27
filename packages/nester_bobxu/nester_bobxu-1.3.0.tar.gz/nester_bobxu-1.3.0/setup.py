0d2c077ca6046c91dea8649bb2220bba0757f363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:56 +0000

    Add package nester_bobxu/1.3.0/nester_bobxu-1.3.0.tar.gz

---
+++ packages/nester_bobxu/nester_bobxu-1.3.0.tar.gz/nester_bobxu-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester_bobxu',
+	version = '1.3.0',
+	py_module = ['nester_bobxu'],
+	author = 'eksbobo',
+	author_email = 'xub6@airproducts.com',
+	url = 'http://www.jianshu.com/u/46b61a5f089d',
+	description = 'A simple printer for nested lists by eksbobo',
+	)
