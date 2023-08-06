f316f864469248bf4b62a10ee44bdbe0061dda74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:48 +0000

    Add package hangcli/0.1.2/hangcli-0.1.2.tar.gz

---
+++ packages/hangcli/hangcli-0.1.2.tar.gz/hangcli-0.1.2/hangcli.py
@@ -0,0 +1,37 @@
+import click
+import os
+import json
+from shutil import copyfile
+
+@click.command()
+@click.option('--project', default='demo', help='input the project name default demo')
+@click.argument('operate')
+def router(operate, project):
+	if operate == "react":
+		react(project)
+	else:
+		print("input the operate")
+
+
+def react(project):
+	reactSrcJsonPath = os.getcwd() + '/' + 'src' + '/' + 'react' + '/' + 'package.json'
+	reactSrcWebpackConfigPath = os.getcwd() + '/' + 'src' + '/' + 'react' + '/' + 'webpack.config.js'
+	print(reactSrcJsonPath)
+	print(reactSrcWebpackConfigPath)
+
+	# 通过输入的project名字，修改package.json中的项目名称
+	with open(reactSrcJsonPath, "r+") as jsonFile:
+	    data = json.load(jsonFile)
+	    data["name"] = project
+	    jsonFile.seek(0)  # rewind
+	    json.dump(data, jsonFile)
+	    jsonFile.truncate()
+	os.mkdir(project)
+	copyfile(reactSrcJsonPath, os.getcwd()+ '/'+ project + '/' + 'package.json')
+	
+
+
+
+
+if __name__ == '__main__':
+    router()
