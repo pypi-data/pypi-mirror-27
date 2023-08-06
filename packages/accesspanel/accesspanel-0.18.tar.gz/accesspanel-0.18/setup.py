0b756c01dd6c69ca0b7ee3f1123b0abb98cb4533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:09 +0000

    Add package accesspanel/0.18/accesspanel-0.18.tar.gz

---
+++ packages/accesspanel/accesspanel-0.18.tar.gz/accesspanel-0.18/setup.py
@@ -0,0 +1,18 @@
+﻿from setuptools import setup, find_packages
+
+DESCRIPTION = """
+AccessPanel is a module to enhance accessibility though a set of
+classes derived from wx.
+""".strip()
+
+setup(
+    name = "accesspanel",
+    version = "0.18",
+    packages = find_packages(),
+    install_requires = [],
+    description = DESCRIPTION,
+    author = 'Vincent Le Goff',
+    author_email = 'vincent.legoff.srs@gmail.com',
+    url = 'https://github.com/vlegoff/accesspanel',
+    keywords = ['accessibility'],
+)
