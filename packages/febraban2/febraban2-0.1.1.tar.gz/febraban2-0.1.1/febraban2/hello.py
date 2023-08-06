0b0d102bc26573dd3813d534e4b754cbd619b05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:14 +0000

    Add package febraban2/0.1.1/febraban2-0.1.1.tar.gz

---
+++ packages/febraban2/febraban2-0.1.1.tar.gz/febraban2-0.1.1/febraban2/hello.py
@@ -0,0 +1,5 @@
+class Hello:
+
+    @staticmethod
+    def message():
+        return "hello world!"
---
+++ packages/febraban2/febraban2-0.1.1.tar.gz/febraban2-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+version = "0.1.1"
+setup(
+    name='febraban2',
+    packages=['febraban2'],
+    version=version,
+    description='A library to generate files that conform to the FEBRABAN formats',
+    author='Hummingbird Product Studio',
+    author_email='deromir.neves@hummingbird.com.br',
+    url='https://github.com/HummingbirdStudio/febraban.git',
+    download_url='https://github.com/HummingbirdStudio/febraban/archive/v%s.tar.gz' % version,
+    keywords=['febraban', 'cnab', 'transfer', 'billing', 'bank']
+)
