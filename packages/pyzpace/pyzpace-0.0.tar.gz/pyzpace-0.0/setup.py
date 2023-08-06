5d5f9c9e2bf5194509c240c7df7b178c39f5e542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:39 +0000

    Add package pyzpace/0.0/pyzpace-0.0.tar.gz

---
+++ packages/pyzpace/pyzpace-0.0.tar.gz/pyzpace-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyzpace',
+      version='0.0',
+      description='Zach Pace\'s astronomy-related python tools',
+      url='http://github.com/zpace/python-personal',
+      author='Zach Pace',
+      author_email='zpace@astro.wisc.edu',
+      license='MIT',
+      packages=['pyzpace'],
+      zip_safe=False)
