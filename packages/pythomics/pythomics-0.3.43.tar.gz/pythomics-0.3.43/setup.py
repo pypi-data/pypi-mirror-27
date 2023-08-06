c883b07d05978fc668c5bcc8127c6f5af111f58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:00 +0000

    Add package pythomics/0.3.43/pythomics-0.3.43.tar.gz

---
+++ packages/pythomics/pythomics-0.3.43.tar.gz/pythomics-0.3.43/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='pythomics',
+      version='0.3.43',
+      description='A multi-omic python package',
+      url='https://github.com/pandeylab/pythomics',
+      author='Chris Mitchell',
+      author_email='chris.mit7@gmail.com',
+      install_requires = ['six'],
+      license='GPL3',
+      packages=['pythomics','pythomics.parsers', 'pythomics.proteomics',
+                'pythomics.templates', 'pythomics.tests', 'pythomics.genomics', 'pythomics.utils'],
+      scripts=['pythomics/scripts/fastadigest.py', 'pythomics/scripts/fastadigeststats.py',
+               'pythomics/scripts/incorporateVCF.py', 'pythomics/scripts/fetchOrfs.py',
+               'pythomics/scripts/incorporateGFF.py', 'pythomics/scripts/proteinInference.py',
+               'pythomics/scripts/featureCollapser.py', 'pythomics/scripts/fastxTrimmer.py',
+               'pythomics/scripts/intersectFiles.py', 'pythomics/scripts/junctionalReads.py',
+               'pythomics/scripts/ptmSummary.py'],
+      zip_safe=False)
