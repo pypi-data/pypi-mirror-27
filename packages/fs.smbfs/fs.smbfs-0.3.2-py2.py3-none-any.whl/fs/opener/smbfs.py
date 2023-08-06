35cf9f140fa37211446e78b55c0ac875f1fd0cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:55 +0000

    Add package fs.smbfs/0.3.2/fs.smbfs-0.3.2-py2.py3-none-any.whl

---
+++ packages/fs.smbfs/fs.smbfs-0.3.2-py2.py3-none-any.whl/fs/opener/smbfs.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+"""`SMBFS` opener definition.
+"""
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+import configparser
+
+from .base import Opener
+from ..subfs import ClosingSubFS
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2017 Martin Larralde"
+__author__ = "Martin Larralde <martin.larralde@ens-cachan.fr>"
+__version__ = 'dev'
+
+
+# Dynamically get the version of the main module
+try:
+    import pkg_resources
+    _name = __name__.replace('.opener', '')
+    __version__ = pkg_resources.get_distribution(_name).version
+except Exception: # pragma: no cover
+    pkg_resources = None
+finally:
+    del pkg_resources
+
+
+class SMBOpener(Opener):
+    """`SMBFS` opener.
+    """
+
+    protocols = ['smb', 'cifs']
+
+    @staticmethod
+    def open_fs(fs_url, parse_result, writeable, create, cwd):  # noqa: D102
+        from ..smbfs import SMBFS
+        smb_host, _, dir_path = parse_result.resource.partition('/')
+        smb_host, _, smb_port = smb_host.partition(':')
+        smb_port = int(smb_port) if smb_port.isdigit() else 445
+
+
+        params = configparser.ConfigParser()
+        params.read_dict({'smbfs':getattr(parse_result, 'params', {})})
+
+        smb_fs = SMBFS(
+            smb_host,
+            username=parse_result.username or 'guest',
+            passwd=parse_result.password or '',
+            port=smb_port,
+            timeout=params.getint('smbfs', 'timeout', fallback=15),
+            name_port=params.getint('smbfs', 'name-port', fallback=137),
+            direct_tcp=params.getboolean('smbfs', 'direct-tcp', fallback=False)
+        )
+
+        if dir_path: # pragma: no cover
+            return smb_fs.opendir(dir_path, factory=ClosingSubFS)
+        else:
+            return smb_fs
