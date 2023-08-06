a1815bdc56fe1b1c04c299fc6eb58ac3fa73a28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:13 +0000

    Add package snapit/0.1/snapit-0.1.tar.gz

---
+++ packages/snapit/snapit-0.1.tar.gz/snapit-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='snapit',
+      version='0.1',
+      description='Spliced Neoantigen Pipeline for Immunotherapy',
+      url='http://github.com/b1tsai/snapit',
+      author='Brandon Liang Tsai',
+      author_email='b1tsai@ucla.com',
+      packages=['snapit'],
+      zip_safe=False)
