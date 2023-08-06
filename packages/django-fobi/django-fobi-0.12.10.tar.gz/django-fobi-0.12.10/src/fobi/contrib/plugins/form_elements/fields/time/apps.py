4507db95b6706370456887bb1c581608ff4c3581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:48 +0000

    Add package django-fobi/0.12.10/django-fobi-0.12.10.tar.gz

---
+++ packages/django-fobi/django-fobi-0.12.10.tar.gz/django-fobi-0.12.10/src/fobi/contrib/plugins/form_elements/fields/time/apps.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+__title__ = 'fobi.contrib.plugins.form_elements.fields.time.apps'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2017 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('Config',)
+
+try:
+    from django.apps import AppConfig
+
+    class Config(AppConfig):
+        """Config."""
+
+        name = 'fobi.contrib.plugins.form_elements.fields.time'
+        label = 'fobi_contrib_plugins_form_elements_fields_time'
+
+except ImportError:
+    pass
