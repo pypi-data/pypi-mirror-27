e3bb64caea285ec9975cc421402e8d0a7bfc01db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:55 +0000

    Add package unrest/0.7.0/unrest-0.7.0.tar.gz

---
+++ packages/unrest/unrest-0.7.0.tar.gz/unrest-0.7.0/unrest/tests/flask/test_put.py
@@ -0,0 +1,50 @@
+from . import idsorted
+from ..model import Tree
+
+
+def test_put_tree_implicitly_unallowed(rest, http):
+    rest(Tree, methods=['GET', 'PUT'])
+    code, json = http.put('/api/tree', json={'id': 1, 'name': 'cedar'})
+    assert code == 406
+
+
+def test_put_tree(rest, http):
+    rest(Tree, methods=['GET', 'PUT'], allow_batch=True)
+    code, json = http.put(
+        '/api/tree',
+        json={
+            'objects': [{
+                'id': 1,
+                'name': 'cedar'
+            }, {
+                'id': 2,
+                'name': 'mango'
+            }]
+        }
+    )
+    assert code == 200
+    assert json['occurences'] == 2
+    assert idsorted(json['objects']) == [
+        {
+            'id': 1,
+            'name': 'cedar'
+        },
+        {
+            'id': 2,
+            'name': 'mango'
+        },
+    ]
+
+    code, json = http.get('/api/tree')
+    assert code == 200
+    assert json['occurences'] == 2
+    assert idsorted(json['objects']) == [
+        {
+            'id': 1,
+            'name': 'cedar'
+        },
+        {
+            'id': 2,
+            'name': 'mango'
+        },
+    ]
