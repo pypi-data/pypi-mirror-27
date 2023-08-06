cb18a07d83e1bb0aba82d81749225cae87e7b9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:06 +0000

    Add package lazyme/0.0.16/lazyme-0.0.16.tar.gz

---
+++ packages/lazyme/lazyme-0.0.16.tar.gz/lazyme-0.0.16/lazyme/__init__.py
@@ -0,0 +1,7 @@
+
+from lazyme.iterate import per_section, per_chunk, per_window
+from lazyme.iterate import zigzag
+
+from lazyme.string import *
+
+from lazyme.wikipedia import iter_paragraph as iter_wiki
