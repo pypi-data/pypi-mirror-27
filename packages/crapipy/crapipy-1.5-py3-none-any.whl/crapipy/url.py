6e5df72787286ec1dee657b6c9834118a55d4207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:02 +0000

    Add package crapipy/1.5/crapipy-1.5-py3-none-any.whl

---
+++ packages/crapipy/crapipy-1.5-py3-none-any.whl/crapipy/url.py
@@ -0,0 +1,9 @@
+class APIURL:
+    """
+    API URL
+    """
+    clan = 'http://api.cr-api.com/clan/{}'
+    player = 'http://api.cr-api.com/player/{}'
+    constants = 'http://api.cr-api.com/constants'
+    top_players = 'http://api.cr-api.com/top/players/{}'
+    top_clans = 'http://api.cr-api.com/top/clans/{}'
