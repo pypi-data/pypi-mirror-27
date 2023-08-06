747ae7106860e7103cb09953f23f1d5c5d1c5712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:06 +0000

    Add package ewtestpypi/0.1/ewtestpypi-0.1.tar.gz

---
+++ packages/ewtestpypi/ewtestpypi-0.1.tar.gz/ewtestpypi-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'ewtestpypi',
+  packages = ['ewtestpypi'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'E W',
+  author_email = 'ewinston@us.ibm.com',
+  url = 'https://ibm.com', # use the URL to the github repo
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
