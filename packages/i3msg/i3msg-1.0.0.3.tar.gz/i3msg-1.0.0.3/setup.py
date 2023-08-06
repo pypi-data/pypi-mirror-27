8649f1a9584f95bbb88753d02491e65f69b34976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:44 +0000

    Add package i3msg/1.0.0.3/i3msg-1.0.0.3.tar.gz

---
+++ packages/i3msg/i3msg-1.0.0.3.tar.gz/i3msg-1.0.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'i3msg',
+    packages = ['i3msg'],
+    version = '1.0.0.3',
+    description = 'A minimal Python interface to the i3wm IPC.',
+    author = 'Ceryn',
+    author_email = 'ceryn@protonmail.com',
+    url = 'https://github.com/Ceryn/i3msg-python',
+    download_url = 'https://github.com/Ceryn/i3msg-python/archive/master.zip',
+    keywords = 'i3msg i3wm ipc i3 i3ipc i3msg-python i3ipc-python python i3-python i3wm-python i3-msg i3-msg-python',
+    license = 'MIT'
+)
