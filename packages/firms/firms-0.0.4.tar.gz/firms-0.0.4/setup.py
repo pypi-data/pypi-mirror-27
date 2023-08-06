9d349894f0d51d29115905e3bc310d42f91601b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:13 +0000

    Add package firms/0.0.4/firms-0.0.4.tar.gz

---
+++ packages/firms/firms-0.0.4.tar.gz/firms-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='firms',
+    version='0.0.4',
+    description='Fuzzy Information Retrieval for Musical Scores',
+    url='https://github.com/axiomabsolute/cs410-information-retrieval',
+    download_url='https://github.com/axiomabsolute/cs410-information-retrieval/archive/0.0.4.tar.gz',
+    author='Devon Terrell',
+    author_email='devon@axioms.me',
+    license='MIT',
+    packages=['firms'],
+    python_requires=">=3",
+    entry_points={
+        'console_scripts': ['firms=firms.command_line:main'],
+    },
+    install_requires=[
+        'music21',
+        'tabulate',
+        'click',
+        'scipy'
+    ])
