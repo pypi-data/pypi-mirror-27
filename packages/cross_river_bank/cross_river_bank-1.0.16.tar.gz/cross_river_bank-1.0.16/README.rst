ab9384099f9a468eea8cadc0abb2b3e47d847471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:41 +0000

    Add package cross_river_bank/1.0.16/cross_river_bank-1.0.16.tar.gz

---
+++ packages/cross_river_bank/cross_river_bank-1.0.16.tar.gz/cross_river_bank-1.0.16/README.rst
@@ -0,0 +1,6 @@
+Cross River Bank Python Client
+
+[![CircleCI](https://circleci.com/gh/finix-payments/crb-python.svg?style=svg&circle-token=d363138a4fb6cf28dee4374ff697eec723bc7449)](https://circleci.com/gh/finix-payments/crb-python)
+
+Getting Started
+`pip install cross_river_bank`
---
+++ packages/cross_river_bank/cross_river_bank-1.0.16.tar.gz/cross_river_bank-1.0.16/setup.py
@@ -0,0 +1,72 @@
+"""
+Cross River Bank Python client library.
+See ``README.md`` for usage advice.
+"""
+import os
+import re
+
+try:
+    import setuptools
+except ImportError:
+    import distutils.core
+
+    setup = distutils.core.setup
+else:
+    setup = setuptools.setup
+
+
+PACKAGE = next((str(s) for s in setuptools.find_packages('.', exclude=("tests", "tests.*"))), None)
+PWD = os.path.abspath(os.path.dirname(__file__))
+VERSION = (
+    re
+        .compile(r".*__version__ = '(.*?)'", re.S)
+        .match(open(os.path.join(PWD, PACKAGE, "__init__.py")).read())
+        .group(1)
+)
+
+with open(os.path.join(PWD, "README.rst")) as f:
+    README = f.read()
+
+dependency_links = [
+]
+
+requires = [
+    "finix>=1.0.17"
+]
+
+extras_require = {
+    "tests": [
+        "coverage==4.0.3"
+    ]
+}
+
+scripts = [
+    # 'bin/citadel'
+]
+
+setup(
+    name=PACKAGE,
+    version=VERSION,
+    url='https://www.crossriverbank.com/',
+    license='MIT License',
+    author='Cross River Bank',
+    author_email='support@crossriverbank.com',
+    description='Cross River Bank API Python client',
+    long_description=README,
+    packages=[PACKAGE],
+    test_suite='nose.collector',
+    tests_require=extras_require['tests'],
+    install_requires=requires,
+    dependency_links=dependency_links,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    scripts=scripts,
+    extras_require=extras_require,
+    setup_requires=['nose>=1.3.7']
+)
