3bac3df018a4d36643388422ba16efe13829e505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:51 +0000

    Add package TFANN/1.0/TFANN-1.0.tar.gz

---
+++ packages/TFANN/TFANN-1.0.tar.gz/TFANN-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='TFANN',
+      version='1.0',
+      description='A neural network module containing implementations of MLP, and CNN networks in TensorFlow.',
+      url='http://github.com/nicholastoddsmith/pythonml',
+      author='Nicholas T. Smith',
+      author_email='nicholastsmithblog@gmail.com',
+      keywords="TensorFlow ANN CNN Neural Network",
+      license='MIT',
+      packages=['TFANN'],
+      zip_safe=False)
