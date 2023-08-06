fdf915f15bec423c3a028330fd9fadd7150e82bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:59 +0000

    Add package wechat-pay-sdk/0.2/wechat-pay-sdk-0.2.tar.gz

---
+++ packages/wechat-pay-sdk/wechat-pay-sdk-0.2.tar.gz/wechat-pay-sdk-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wechat-pay-sdk',
+    packages=['wechatpay'],
+    version='0.2',
+    description='A sdk for wechat pay',
+    author='Zack Ren',
+    license='MIT',
+    include_package_data=True,
+    author_email='m6106918@gmail.com',
+    url='https://github.com/Narcissist1/wechat-pay',
+    download_url='https://github.com/Narcissist1/wechat-pay/archive/0.1.tar.gz',
+    keywords=['wechat', 'pay'],
+    classifiers=[],
+)
