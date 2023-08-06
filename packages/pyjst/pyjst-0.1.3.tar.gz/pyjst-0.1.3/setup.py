30f1651041948fa0077c9a4e1eb99e4440e5fca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:03 +0000

    Add package pyjst/0.1.3/pyjst-0.1.3.tar.gz

---
+++ packages/pyjst/pyjst-0.1.3.tar.gz/pyjst-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='pyjst',
+    version='0.1.3',
+    description='Bake a template from corresponding JSON object',
+    long_description=readme(),
+    keywords='simple static template templating json',
+    url='http://github.com/GuillaumeDesforges/pyjst',
+    author='Guillaume Desforges',
+    author_email='aceus02@gmail.com',
+    license='MIT',
+    packages=['pyjst'],
+    install_requires=[
+      'jinja2',
+    ],
+    scripts=['bin/pyjst'],
+    zip_safe=False
+)
