aa1c5a01ed4523bfc3e612b78b7aaefbd1f56869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:25 +0000

    Add package generamento/1.0/generamento-1.0.tar.gz

---
+++ packages/generamento/generamento-1.0.tar.gz/generamento-1.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name='generamento',
+    version='1.0',
+    author='carzacc',
+    license='GPL-3.0',
+    url='https://github.com/FacciamoUnCasino-TheGame/generamento',
+    description='generamento con indicazione grandezza',
+    package_dir={"": "generamento"},
+    packages=setuptools.find_packages("generamento"),
+    include_package_data=True
+)
