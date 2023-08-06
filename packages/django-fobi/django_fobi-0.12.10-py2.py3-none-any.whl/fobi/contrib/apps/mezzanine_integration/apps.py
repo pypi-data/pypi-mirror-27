27c40e9e93f77cc248a00c32a33aaee745abdc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:48 +0000

    Add package django-fobi/0.12.10/django_fobi-0.12.10-py2.py3-none-any.whl

---
+++ packages/django-fobi/django_fobi-0.12.10-py2.py3-none-any.whl/fobi/contrib/apps/mezzanine_integration/apps.py
@@ -0,0 +1,17 @@
+__title__ = 'fobi.contrib.apps.mezzanine_integration.apps'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2017 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('Config',)
+
+try:
+    from django.apps import AppConfig
+
+    class Config(AppConfig):
+        """Config."""
+
+        name = 'fobi.contrib.apps.mezzanine_integration'
+        label = 'fobi_contrib_apps_mezzanine_integration'
+
+except ImportError:
+    pass
---
+++ packages/django-fobi/django_fobi-0.12.10-py2.py3-none-any.whl/fobi/contrib/themes/djangocms_admin_style_theme/widgets/form_handlers/db_store/apps.py
@@ -0,0 +1,19 @@
+__title__ = 'fobi.contrib.themes.djangocms_admin_style_theme.widgets.' \
+            'form_handlers.db_store.apps'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = 'Copyright (c) 2015 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('Config',)
+
+try:
+    from django.apps import AppConfig
+
+    class Config(AppConfig):
+        """Config."""
+        name = 'fobi.contrib.themes.djangocms_admin_style_theme.widgets.' \
+               'form_handlers.db_store'
+        label = 'fobi_contrib_themes_djangocms_admin_style_theme_widgets_' \
+                'form_handlers_db_store'
+
+except ImportError:
+    pass
