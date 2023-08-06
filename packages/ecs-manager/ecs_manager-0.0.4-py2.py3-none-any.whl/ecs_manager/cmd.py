f7cd9007d0c25fae99c784207b44c434a6a78263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:00 +0000

    Add package ecs-manager/0.0.4/ecs_manager-0.0.4-py2.py3-none-any.whl

---
+++ packages/ecs-manager/ecs_manager-0.0.4-py2.py3-none-any.whl/ecs_manager/cmd.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+
+import json
+
+import click
+import boto3
+
+from ecs_manager.utils.json_util import json_dumps
+from ecs_manager.functions import merge_environ, check_task_status
+
+client = boto3.client('ecs')
+
+
+def json_validator(context, param, value):
+    if value is None:
+        raise click.BadParameter('{} definition is required'.format(param.name))
+    try:
+        return json.loads(value)
+    except json.decoder.JSONDecodeError:
+        raise click.BadParameter('{} definition is invalid json format'.format(param.name))
+
+
+@click.group()
+def cmd():
+    pass
+
+
+@cmd.command()
+@click.argument('name')
+@click.option('--cluster', '-c', required=True)
+@click.option('--task_container_definition', '-t', callback=json_validator)
+@click.option('--service_definition', '-s', callback=json_validator)
+@click.option('--environment', '-e', callback=json_validator, default='{}')
+def deploy_service(name, cluster, task_container_definition, service_definition, environment):
+
+    merge_environ(task_container_definition, environment)
+
+    status = check_task_status(client, name, task_container_definition)
+    
+    if status['changed']:
+        click.echo(status)
+        res = client.register_task_definition(family=name, containerDefinitions=task_container_definition)
+        task_arn = res['taskDefinition']['taskDefinitionArn']
+        click.echo(task_arn)
+    else:
+        click.echo('Task definition is not changed')
+        task_arn = status['arn']
+
+    res = client.describe_services(cluster=cluster, services=[name, ])
+    if not res['services']:
+        res = client.create_service(cluster=cluster, serviceName=name, taskDefinition=task_arn, **service_definition)
+        click.echo('Created service')
+        click.echo(json_dumps(res))
+        return
+    res = client.update_service(cluster=cluster, service=name, taskDefinition=task_arn, **service_definition)
+    click.echo('Updated service')
+    click.echo(json_dumps(res))
+
+
+def main():
+    cmd()
+
+
+if __name__ == '__main__':
+    main()
