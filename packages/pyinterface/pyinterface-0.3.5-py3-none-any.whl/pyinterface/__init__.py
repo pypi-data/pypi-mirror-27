9f4db49fe068cae8d783cede5b2ea7b2da9d11db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:31 +0000

    Add package pyinterface/0.3.5/pyinterface-0.3.5-py3-none-any.whl

---
+++ packages/pyinterface/pyinterface-0.3.5-py3-none-any.whl/pyinterface/__init__.py
@@ -0,0 +1,15 @@
+
+
+__version__ = '0.3.5'
+
+
+from .tools import open
+
+from .core import interface_driver
+
+from . import pci2702
+from . import pci2724
+from . import pci6204
+from . import pci7204
+
+
