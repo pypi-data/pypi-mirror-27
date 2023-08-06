8e7b6696d778990085c7883f54853a9c254b34e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:24 +0000

    Add package nbextension-share-button/0.1.4/nbextension-share-button-0.1.4.tar.gz

---
+++ packages/nbextension-share-button/nbextension-share-button-0.1.4.tar.gz/nbextension-share-button-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+  name = 'nbextension-share-button',
+  version = '0.1.4',
+  description = 'A Jupyter Notebook extension that adds a share button to the toolbar.',
+  author = 'Globo.com',
+  url = 'https://github.com/globocom/nbextension-share-button/',
+  keywords = ['nbextension', 'share', 'notebook'],
+  classifiers = [],
+  packages = ['nbextension_share_button'],
+  package_dir = {'nbextension_share_button': 'nbextension_share_button'}
+)
