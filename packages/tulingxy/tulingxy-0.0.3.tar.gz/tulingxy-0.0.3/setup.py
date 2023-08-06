d1bcb0ab776a745421f300da26aa47d6c9fa42cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:44 +0000

    Add package tulingxy/0.0.3/tulingxy-0.0.3.tar.gz

---
+++ packages/tulingxy/tulingxy-0.0.3.tar.gz/tulingxy-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'tulingxy',
+  packages = ['tulingxy'],
+  version = '0.0.3',
+  description = 'Use Tuling API',
+  long_description='Use Tuling API',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'bot', 'python3','python','tuling'],
+  license='MIT',
+)
