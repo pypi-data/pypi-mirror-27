f2ebc17b1d8c3c7ca3d99156d83f8448d9f5c02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:03 +0000

    Add package parski/2.2.1.49/parski-2.2.1.49.tar.gz

---
+++ packages/parski/parski-2.2.1.49.tar.gz/parski-2.2.1.49/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+setup(
+  name = 'parski',
+  url = 'https://github.build.ge.com/CloudPod/parski',
+  packages = ['parski'],
+  version = '2.2.1.49',
+  description = 'Helper library for filtering json data',
+  author = 'Russell Endicott',
+  author_email = 'rendicott@gmail.com',
+  keywords = ['vpc', 'json', 'parski'],
+  classifiers = []
+)
