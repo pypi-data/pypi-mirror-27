30f7770ab8df7bbd0a388a52b6b3e71bc6bfb0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:36 +0000

    Add package dops/0.0.1.32/dops-0.0.1.32-py3-none-any.whl

---
+++ packages/dops/dops-0.0.1.32-py3-none-any.whl/dops/plugins/instance/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+@version:0.1
+@author: ysicing
+@file: dops/__init__.py 
+@time: 2018/1/3 23:55
+"""
+
+from .general import GeneralInstall
+
+
+__all__ = ('GeneralInstall')
---
+++ packages/dops/dops-0.0.1.32-py3-none-any.whl/dops/version.py
@@ -0,0 +1,7 @@
+VERSION_INFO = (
+                ("program","dops"),
+                ("version","dev-0.0.1"),
+                ("pypi","0.0.1.32"),
+                ("commit","32.ge981ed7"),
+                ("buildTime","2018-01-03 16:06:54"),
+)
