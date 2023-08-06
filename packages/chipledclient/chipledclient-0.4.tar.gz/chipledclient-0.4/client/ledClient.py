b9453a59ca7443b40e0425a7de8cde8d187e5af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:30 +0000

    Add package chipledclient/0.4/chipledclient-0.4.tar.gz

---
+++ packages/chipledclient/chipledclient-0.4.tar.gz/chipledclient-0.4/client/ledClient.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+class LEDStrip:
+    def __init__(self, name, host, port):
+        self.host = host
+        self.port = port
+        self.name = name
+
+    def setColor(self, red, green, blue):
+        url = self.getUrl()
+        data = {
+            'red': red,
+            'green': green,
+            'blue': blue
+        }
+
+
+        response = requests.post(url, json=data)
+        return response.status_code == 200
+
+    def turnOff(self):
+        url = self.getUrl()
+        data = {
+            'red': 0,
+            'green': 0,
+            'blue': 0
+        }
+
+        response = requests.post(url, json=data)
+        return response.status_code == 200
+
+    def getUrl(self):
+        return 'http://{}:{}'.format(self.host, self.port)
+
+    def getName(self):
+        return self.name
