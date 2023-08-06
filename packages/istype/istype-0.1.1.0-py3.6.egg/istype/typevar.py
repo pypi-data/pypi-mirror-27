9945ebcbe9d0bc77f1d36e2bc707c709b7edd937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:52 +0000

    Add package istype/0.1.1.0/istype-0.1.1.0-py3.6.egg

---
+++ packages/istype/istype-0.1.1.0-py3.6.egg/istype/typevar.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+import typing
+from .common import INSTANCECHECK, ISA
+
+def instancecheck(self, obj, **kwargs):
+    args = self.__constraints__
+    if args:
+        return ISA(obj, args)
+    if self.__covariant__:
+        raise NotImplementedError
+    if self.__contravariant__:
+        raise NotImplementedError
+    typevar_table = kwargs.get('typevar_table')
+    if isinstance(typevar_table, dict):
+        typed = typevar_table.get(self.__name__, None)
+        if typed is None:
+            typevar_table[self.__name__] = type(obj)
+            return True
+        else:
+            return ISA(obj, typed)
+    return True
+INSTANCECHECK[typing.TypeVar] = instancecheck
