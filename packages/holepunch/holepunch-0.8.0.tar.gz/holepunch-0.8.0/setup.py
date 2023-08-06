55d450be80859d1f230ba23f7b6d217c3cdf663a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:41 +0000

    Add package holepunch/0.8.0/holepunch-0.8.0.tar.gz

---
+++ packages/holepunch/holepunch-0.8.0.tar.gz/holepunch-0.8.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+# Define __version__
+with open('holepunch/version.py', 'r') as fp:
+    exec(fp.read())
+
+
+setup(
+    name='holepunch',
+    version=__version__,
+    description="Punch holes in your AWS account security",
+    author='Erik Price',
+    url='https://github.com/erik/holepunch',
+    packages=['holepunch'],
+    entry_points={
+        'console_scripts': [
+            'holepunch = holepunch:main',
+        ],
+    },
+    license='MIT',
+    install_requires=[
+        'boto3==1.5.6',
+        'docopt==0.6.2',
+        # Backport for python2.7
+        'ipaddress==1.0.19'
+    ]
+)
