cc46f389f9df0d67edeaac69d44bb157e2e08cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:20 +0000

    Add package autoclass/1.9.1/autoclass-1.9.1.tar.gz

---
+++ packages/autoclass/autoclass-1.9.1.tar.gz/autoclass-1.9.1/autoclass/__init__.py
@@ -0,0 +1,11 @@
+# allow users to do
+#     from autoclass import xxx
+from autoclass.autoargs import *
+from autoclass.autoclass import *
+from autoclass.autoprops import *
+from autoclass.autodict import *
+from autoclass.var_checker import *
+
+# allow users to do
+#     import autoclass as ac
+__all__ = ['autoclass', 'autoargs', 'autoprops', 'autodict', 'var_checker']
