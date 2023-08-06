23fe1d76e22662f77a04b3bba8874f7925e84e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:29 +0000

    Add package pref_timer/0.1.6/pref_timer-0.1.6.tar.gz

---
+++ packages/pref_timer/pref_timer-0.1.6.tar.gz/pref_timer-0.1.6/setup.py
@@ -0,0 +1,19 @@
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+      name='pref_timer',
+      version='0.1.6',
+      description='Used to find the function execution time',
+      url='https://github.com/PraveenJP/pyPerformanceTimer',
+      author='Praveen J',
+      author_email='praveen.josephmasilamani@outlook.com',
+      license='MIT',
+      long_description=read('README'),
+      packages=['pref_timer'],
+      keywords=['performance', 'testing', 'execution'],
+      classifiers = [],
+)
