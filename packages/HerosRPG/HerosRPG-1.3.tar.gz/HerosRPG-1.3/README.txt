36f82a23753333e34c3cfd033ed0284604b7e349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:09 +0000

    Add package HerosRPG/1.3/HerosRPG-1.3.tar.gz

---
+++ packages/HerosRPG/HerosRPG-1.3.tar.gz/HerosRPG-1.3/README.txt
@@ -0,0 +1,10 @@
+===========
+HerosRPG
+===========
+
+HerosrPG is a simple console based RPG I wrote to learn python. Typical usage
+often looks like this::
+
+	python HerorsRPG.py
+
+
---
+++ packages/HerosRPG/HerosRPG-1.3.tar.gz/HerosRPG-1.3/setup.py
@@ -0,0 +1,14 @@
+# Useful doc: http://the-hitchhikers-guide-to-packaging.readthedocs.io/en/latest/creation.html
+from distutils.core import setup
+
+setup(
+    name='HerosRPG',
+    version='1.3',
+    packages=['HerosRPG'],
+    url='',
+    license='LICENSE.txt',
+    author='ianc',
+    author_email='ianc@slalom.com',
+    description='A simple console based  Role Playing Game in Python',
+    long_description=open('README.txt').read()
+)
