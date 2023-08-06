fb27f56473cb8695555ef7b2d751c6e6d943c3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:05 +0000

    Add package reescraper/1.6.0/reescraper-1.6.0.tar.gz

---
+++ packages/reescraper/reescraper-1.6.0.tar.gz/reescraper-1.6.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='reescraper',
+    version='1.6.0',
+    packages=['reescraper', 'reescraper/core', 'reescraper/response', 'reescraper/canary', 'reescraper/balearic'],
+    install_requires=[
+        'arrow',
+        'requests'
+      ],
+    url='https://github.com/blackleg/reescraper',
+    license='MIT',
+    author='blackleg',
+    author_email='hectorespertpardo@gmail.com',
+    description='Red Eléctrica de España data scraper'
+)
