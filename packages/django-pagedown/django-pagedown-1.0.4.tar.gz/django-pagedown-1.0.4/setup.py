46e9c8f1145dee1068e048ff6633794645748e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:14 +0000

    Add package django-pagedown/1.0.4/django-pagedown-1.0.4.tar.gz

---
+++ packages/django-pagedown/django-pagedown-1.0.4.tar.gz/django-pagedown-1.0.4/setup.py
@@ -0,0 +1,44 @@
+from os import path
+from setuptools import setup
+from subprocess import check_call
+from distutils.command.build import build
+from setuptools.command.develop import develop
+
+
+def get_submodules():
+    if path.exists('.git'):
+        check_call(['rm', '-rf', 'pagedown/static/pagedown'])
+        check_call(['rm', '-rf', 'pagedown/static/pagedown-extra'])
+        check_call(['git', 'reset', '--hard'])
+        check_call(['git', 'submodule', 'update', '--init', '--recursive'])
+
+
+class build_with_submodules(build):
+    def run(self):
+        get_submodules()
+        build.run(self)
+
+
+class develop_with_submodules(develop):
+    def run(self):
+        get_submodules()
+        develop.run(self)
+
+
+setup(
+    name="django-pagedown",
+    version="1.0.4",
+    author="Timmy O'Mahony",
+    author_email="hey@timmyomahony.com",
+    url="https://github.com/timmyomahony/django-pagedown",
+    description=("A django app that allows the easy addition of Stack Overflow's 'PageDown' markdown editor to a django form field"),
+    long_description=open('README.md').read(),
+    packages=['pagedown'],
+    include_package_data=True,
+    install_requires=[
+        "Django >= 1.8",
+    ],
+    license='LICENSE.txt',
+    cmdclass={"build": build_with_submodules, "develop": develop_with_submodules},
+    zip_safe=False,
+)
