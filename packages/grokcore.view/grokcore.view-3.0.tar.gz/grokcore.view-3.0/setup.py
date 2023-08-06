5d38acf687c8e564db26ef8b9ba4094a2df57974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:18 +0000

    Add package grokcore.view/3.0/grokcore.view-3.0.tar.gz

---
+++ packages/grokcore.view/grokcore.view-3.0.tar.gz/grokcore.view-3.0/setup.py
@@ -0,0 +1,88 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    )
+
+install_requires = [
+    'grokcore.component >= 2.5',
+    'grokcore.security',
+    'martian >= 0.13',
+    'setuptools',
+    'six',
+    'zope.browserpage',
+    'zope.browserresource >= 3.9.0',
+    'zope.component',
+    'zope.contentprovider',
+    'zope.interface',
+    'zope.pagetemplate',
+    'zope.ptresource >= 3.9.0',
+    'zope.publisher',
+    'zope.security',
+    'zope.traversing',
+    ]
+
+tests_require = [
+    'grok',
+    'zope.app.appsetup',
+    'zope.app.publication',
+    'zope.app.wsgi[test]',
+    'zope.configuration',
+    'zope.container',
+    'zope.login',
+    'zope.password',
+    'zope.principalregistry',
+    'zope.securitypolicy',
+    'zope.site',
+    'zope.testbrowser',
+    'zope.testing',
+    ]
+
+publication_require = [
+    'zope.app.publication'
+    ]
+
+setup(
+    name='grokcore.view',
+    version='3.0',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    download_url='http://pypi.python.org/pypi/grok/',
+    description='Grok-like configuration for Zope browser pages',
+    long_description=long_description,
+    license='ZPL',
+    classifiers=['Environment :: Web Environment',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Zope Public License',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: Implementation',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Framework :: Zope3',
+                 ],
+
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['grokcore'],
+    include_package_data = True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+        'security_publication': publication_require
+        },
+)
