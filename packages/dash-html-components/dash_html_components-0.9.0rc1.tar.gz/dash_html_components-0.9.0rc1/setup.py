3c2779475f7a677ec7ac8a2700c3cfb3fc963350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:16 +0000

    Add package dash-html-components/0.9.0rc1/dash_html_components-0.9.0rc1.tar.gz

---
+++ packages/dash-html-components/dash_html_components-0.9.0rc1.tar.gz/dash_html_components-0.9.0rc1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+exec (open('dash_html_components/version.py').read())
+
+setup(
+    name='dash_html_components',
+    version=__version__,
+    author='Chris Parmer',
+    author_email='chris@plot.ly',
+    packages=['dash_html_components'],
+    include_package_data=True,
+    license='MIT',
+    description='Dash UI HTML component suite',
+    install_requires=['dash']
+)
