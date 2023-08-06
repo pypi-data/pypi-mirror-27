531854e13822ed9dcd4c938024b17c57d7625e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:42 +0000

    Add package infi.rpc/0.2.5/infi.rpc-0.2.5.tar.gz

---
+++ packages/infi.rpc/infi.rpc-0.2.5.tar.gz/infi.rpc-0.2.5/src/infi/rpc/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.2.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '5654960c77f4706219808277242fc340b02a3dc0'
+__git_head_subject__ = u'Finished Release v0.2.5'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
