aa323a546ad98ab812c54010c6e7fa278360ffb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:16 +0000

    Add package simpledb-dev2/0.1/simpledb-dev2-0.1.tar.gz

---
+++ packages/simpledb-dev2/simpledb-dev2-0.1.tar.gz/simpledb-dev2-0.1/simpledbdev2/__init__.py
@@ -0,0 +1,7 @@
+##################################################
+# Copyright (C) 2017, All rights reserved.
+##################################################
+
+__project_name__ = "simpledb-dev2"
+__version__ = "0.1"
+__description__ = "A local SimpleDB server"
