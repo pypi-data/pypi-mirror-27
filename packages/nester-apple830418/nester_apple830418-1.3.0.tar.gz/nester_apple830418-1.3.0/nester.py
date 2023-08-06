ff49f4b9e8366a26fedc82a535f015e59b80ac9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:35 +0000

    Add package nester-apple830418/1.3.0/nester_apple830418-1.3.0.tar.gz

---
+++ packages/nester-apple830418/nester_apple830418-1.3.0.tar.gz/nester_apple830418-1.3.0/nester.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 28 11:44:36 2017
+
+@author: root
+"""
+
+
+def print_lol(the_list,indent=False,leavel=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,leavel+1)
+        else:
+            if indent:
+                for tab_stop in range(leavel):
+                    print("\t",end='')
+            print(each_item)
+
+movies = ["The Holy Grail",1975,"Terry Jones & Terry Gilliam",91,
+          ["Graham Chapman",
+           ["Michael Palin","John Cleese", "Terry Gilliam","Eric Idle","Terry Jones"]]]
+print_lol(movies,0)
+
+
+
