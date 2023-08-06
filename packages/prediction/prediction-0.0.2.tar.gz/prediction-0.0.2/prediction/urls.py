9fe04f4131292c0eabcccf102827bed9a7c30d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:01 +0000

    Add package prediction/0.0.2/prediction-0.0.2.tar.gz

---
+++ packages/prediction/prediction-0.0.2.tar.gz/prediction-0.0.2/prediction/urls.py
@@ -0,0 +1,33 @@
+"""prediction URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from scoring import api
+
+# Register DRF ViewSets using the router
+router = routers.DefaultRouter()
+
+# Register class based Views here
+class_based_views = [
+    path('hello-world/', api.HelloWorld.as_view(), name='hello-world')
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(class_based_views)),
+    path('api/v1/', include(router.urls)),
+]
