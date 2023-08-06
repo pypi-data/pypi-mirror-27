b5afa3e6a7b4bb50f1e88f163c9dbebc5d09d525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:23 +0000

    Add package pyHik/0.1.7/pyHik-0.1.7.tar.gz

---
+++ packages/pyHik/pyHik-0.1.7.tar.gz/pyHik-0.1.7/pyhik/constants.py
@@ -0,0 +1,47 @@
+"""
+pyhik.constants
+~~~~~~~~~~~~~~~~~~~~
+Constants list
+Copyright (c) 2016-2017 John Mihalic <https://github.com/mezz64>
+Licensed under the MIT license.
+"""
+
+MAJOR_VERSION = 0
+MINOR_VERSION = 1
+SUB_MINOR_VERSION = 7
+__version__ = '{}.{}.{}'.format(
+    MAJOR_VERSION, MINOR_VERSION, SUB_MINOR_VERSION)
+
+DEFAULT_PORT = 80
+XML_ENCODING = 'UTF-8'
+XML_NAMESPACE = 'http://www.hikvision.com/ver20/XMLSchema'
+
+DEFAULT_HEADERS = {
+    'Content-Type': "application/xml; charset='UTF-8'",
+    'Accept': "*/*"
+}
+
+SENSOR_MAP = {
+    'VMD': 'Motion',
+    'linedetection': 'Line Crossing',
+    'fielddetection': 'Field Detection',
+    'videoloss': 'Video Loss',
+    'tamperdetection': 'Tamper Detection',
+    'shelteralarm': 'Shelter Alarm',
+    'diskfull': 'Disk Full',
+    'diskerror': 'Disk Error',
+    'nicbroken': 'Net Interface Broken',
+    'ipconflict': 'IP Conflict',
+    'illaccess': 'Illegal Access',
+    'videomismatch': 'Video Mismatch',
+    'badvideo': 'Bad Video',
+    'PIR': 'PIR Alarm',
+    'facedetection': 'Face Detection',
+    'scenechangedetection': 'Scene Change Detection',
+    'IO': 'I/O',
+    'unattendedBaggage': 'Unattended Baggage',
+    'attendedBaggage': 'Attended Baggage',
+}
+
+CAM_DEVICE = 'CAM'
+NVR_DEVICE = 'NVR'
