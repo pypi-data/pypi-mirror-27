002d1e3066591105efa679d394d5b734bcde477e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:15 +0000

    Add package archinfo/7.7.12.16/archinfo-7.7.12.16.tar.gz

---
+++ packages/archinfo/archinfo-7.7.12.16.tar.gz/archinfo-7.7.12.16/setup.py
@@ -0,0 +1,14 @@
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
+
+setup(
+    name='archinfo',
+    version='7.7.12.16',
+    packages=packages
+)
