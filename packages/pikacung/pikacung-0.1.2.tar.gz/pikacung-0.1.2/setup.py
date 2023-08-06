6aadfef5b140d5c02dbd66ca0e4db2a29167bad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:53 +0000

    Add package pikacung/0.1.2/pikacung-0.1.2.tar.gz

---
+++ packages/pikacung/pikacung-0.1.2.tar.gz/pikacung-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'pikacung',
+    packages = ['pikacung'],
+    version = '0.1.2',
+    description = 'Pika wrapper for simple publisher and consumer helper',
+    author = 'M Gilang Januar',
+    author_email = 'mgilangjanuar@gmail.com',
+    url = 'https://github.com/mgilangjanuar/pikacung',
+    download_url = 'https://github.com/mgilangjanuar/pikacung/archive/0.1.2.tar.gz',
+    keywords = ['pika', 'message queue', 'rabbitmq'],
+    classifiers = [],
+    install_requires=['pika>=0.11.2']
+)
