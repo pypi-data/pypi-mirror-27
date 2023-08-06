88d9f979aa4fdf7220c7bac1804b1f5d477afae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:10 +0000

    Add package eviltransform/0.1.1/eviltransform-0.1.1.tar.gz

---
+++ packages/eviltransform/eviltransform-0.1.1.tar.gz/eviltransform-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'eviltransform',
+  packages = [ 'eviltransform' ],
+  version = '0.1.1',
+  description = 'Transform coordinates among GPS (WGS-84), Baidu (BD-02) and Mars in China (GCJ-02).',
+  author = 'googollee, et al.',
+  url = 'https://github.com/googollee/eviltransform',
+  keywords = [ 'gis' ]
+)
+
