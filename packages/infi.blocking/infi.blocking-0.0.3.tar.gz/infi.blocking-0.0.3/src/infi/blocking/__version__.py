1a709844aabfaa7f91736b11cf1eb09d4af7beac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:42 +0000

    Add package infi.blocking/0.0.3/infi.blocking-0.0.3.tar.gz

---
+++ packages/infi.blocking/infi.blocking-0.0.3.tar.gz/infi.blocking-0.0.3/src/infi/blocking/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.3"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.0.3)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '3a8247238b94bc7ade17067ef06b141ba94a00be'
+__git_head_subject__ = u'Finished Release v0.0.3'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-12-27 14:58:52'
