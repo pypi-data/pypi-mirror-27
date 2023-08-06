384f1b20eadd24dc44a3054d48c5e1fd6887307f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:38 +0000

    Add package navio-builder/0.1.20/navio-builder-0.1.20.tar.gz

---
+++ packages/navio-builder/navio-builder-0.1.20.tar.gz/navio-builder-0.1.20/navio/builder/__init__.py
@@ -0,0 +1,10 @@
+"""
+Lightweight Python Build Tool
+"""
+
+from ._nb import task, main, nsh
+import pkgutil
+
+__path__ = pkgutil.extend_path(__path__,__name__)
+
+__all__ = ["task",  "main", "nsh"]
