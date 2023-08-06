4dacd14a5388dfaf3ddb1bc03505357afd3c6a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:04 +0000

    Add package esst/0.1.67/esst-0.1.67-py36-none-any.whl

---
+++ packages/esst/esst-0.1.67-py36-none-any.whl/esst/core/fs_paths.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+"""
+Manages all FileSystem path for ESST
+"""
+
+
+class FS:
+    """
+    Manages all FileSystem path for ESST
+    """
+
+    saved_games_path = None
+    ur_settings_folder = None
+    ur_voice_settings_file = None
+    ur_install_path = None
