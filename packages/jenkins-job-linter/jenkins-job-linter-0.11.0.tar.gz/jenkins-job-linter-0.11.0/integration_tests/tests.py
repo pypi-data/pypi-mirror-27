600c82a0161d9d5b76eee60dff3a5c0bdb27ab6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:01 +0000

    Add package jenkins-job-linter/0.11.0/jenkins-job-linter-0.11.0.tar.gz

---
+++ packages/jenkins-job-linter/jenkins-job-linter-0.11.0.tar.gz/jenkins-job-linter-0.11.0/integration_tests/tests.py
@@ -0,0 +1,144 @@
+# Copyright (C) 2017  Daniel Watkins <daniel@daniel-watkins.co.uk>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import inspect
+import os
+import socket
+import subprocess
+from collections import namedtuple
+from glob import iglob
+
+import pytest
+import yaml
+
+
+def get_available_port():
+    sock = socket.socket()
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    return port
+
+
+JJB_CONFIG = '''\
+[job_builder]
+ignore_cache=True
+
+[jenkins]
+url=http://0.0.0.0:{}/
+user=XXX
+password=XXX
+'''.format(get_available_port())
+
+
+def _direct_runner(tmpdir, config):
+    output_dir = os.path.join(tmpdir, 'output')
+    subprocess.check_call([
+        'jenkins-jobs', 'test', os.path.join(tmpdir), '-o', output_dir])
+    config_args = []
+    if config is not None:
+        conf_file = tmpdir.join('config.ini')
+        conf_file.write(config)
+        config_args = ['--conf', str(conf_file)]
+    success = True
+    try:
+        output = subprocess.check_output(
+            ['jenkins-job-linter', output_dir] + config_args,
+            stderr=subprocess.STDOUT,
+        )
+    except subprocess.CalledProcessError as exc:
+        output = exc.output
+        success = False
+    return success, output.decode('utf-8')
+
+
+def _jjb_subcommand_runner(tmpdir, config):
+    conf_file = tmpdir.join('config.ini')
+    config = '\n'.join([JJB_CONFIG, config or ''])
+    conf_file.write(config)
+    success = True
+    try:
+        output = subprocess.check_output([
+            'jenkins-jobs', '--conf', str(conf_file),
+            'lint', os.path.join(tmpdir)],
+            stderr=subprocess.STDOUT,
+        )
+    except subprocess.CalledProcessError as exc:
+        output = exc.output
+        success = False
+    return success, output.decode('utf-8')
+
+
+@pytest.fixture(params=['direct', 'jjb_subcommand'])
+def runner(request):
+    runner_funcs = {
+        'direct': _direct_runner,
+        'jjb_subcommand': _jjb_subcommand_runner,
+    }
+    return runner_funcs[request.param]
+
+
+def test_integration(runner, tmpdir, integration_testcase):
+    tmpdir.join('jobs.yaml').write(integration_testcase.jobs_yaml)
+    success, output = runner(tmpdir, integration_testcase.config)
+    assert integration_testcase.expected_output == output
+    assert integration_testcase.expect_success == success
+
+
+IntegrationTestcase = namedtuple(
+    'IntegrationTestcase',
+    ['test_name', 'jobs_yaml', 'expected_output', 'expect_success', 'config'])
+
+
+def _get_case_item(key, case_dict, defaults, required=True):
+    value = case_dict.get(key, None)
+    if value is not None:
+        return value
+    if required:
+        return defaults[key]
+    return defaults.get(key, None)
+
+
+def _parse_case(case_dict, defaults):
+    if 'description' not in case_dict:
+        raise Exception('Test {} has no description'.format(case_dict['name']))
+    return IntegrationTestcase(
+        case_dict['name'],
+        _get_case_item('jobs.yaml', case_dict, defaults),
+        _get_case_item('expected_output', case_dict, defaults),
+        _get_case_item('expect_success', case_dict, defaults),
+        _get_case_item('config', case_dict, defaults, required=False),
+    )
+
+
+def _parse_testcases(filenames):
+    names = set()
+    for filename in filenames:
+        with open(filename) as f:
+            data = yaml.safe_load(f)
+        defaults = data.get('defaults', {})
+        for case_dict in data['cases']:
+            testcase = _parse_case(case_dict, defaults)
+            if testcase.test_name in names:
+                raise Exception(
+                    'Duplicate test name: {}'.format(testcase.test_name))
+            names.add(testcase.test_name)
+            yield testcase
+
+
+def pytest_generate_tests(metafunc):
+    test_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+    test_cases = _parse_testcases(iglob(os.path.join(test_dir, 'test_*.yaml')))
+    if 'integration_testcase' in metafunc.fixturenames:
+        metafunc.parametrize('integration_testcase', test_cases,
+                             ids=lambda testcase: testcase.test_name)
---
+++ packages/jenkins-job-linter/jenkins-job-linter-0.11.0.tar.gz/jenkins-job-linter-0.11.0/jenkins_job_linter/__init__.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# Copyright (C) 2017  Daniel Watkins <daniel@daniel-watkins.co.uk>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Run a series of checks against compiled job XML."""
+import os
+import sys
+from configparser import ConfigParser
+from typing import Optional
+from xml.etree import ElementTree
+
+import click
+
+from jenkins_job_linter.config import _filter_config, GetListConfigParser
+from jenkins_job_linter.linters import LINTERS
+from jenkins_job_linter.models import LintContext, RunContext
+
+
+def lint_job_xml(ctx: RunContext, job_name: str, tree: ElementTree.ElementTree,
+                 config: GetListConfigParser) -> bool:
+    """Run all the linters against an XML tree."""
+    success = True
+    for linter_name, linter in LINTERS.items():
+        if linter_name in config.getlist('job_linter', 'disable_linters'):
+            continue
+        only_run = config.getlist('job_linter', 'only_run')
+        if only_run and linter_name not in only_run:
+            continue
+        section = config['job_linter:{}'.format(linter_name)]
+        result, text = linter(LintContext(section, ctx, tree)).check()
+        if not result.value:
+            success = False
+            output = '{}: {}: FAIL'.format(job_name, linter.description)
+            if text is not None:
+                output += ': {}'.format(text)
+            print(output)
+    return success
+
+
+def lint_jobs_from_directory(compiled_job_directory: str,
+                             config: ConfigParser) -> bool:
+    """Load jobs from a directory and run linters against each one."""
+    config = _filter_config(config)
+    success = True
+    filenames = os.listdir(compiled_job_directory)
+    for job_file in filenames:
+        job_path = os.path.join(compiled_job_directory, job_file)
+        result = lint_job_xml(RunContext(filenames), job_file,
+                              ElementTree.parse(job_path), config)
+        success = success and result
+    return success
+
+
+@click.command()
+@click.argument('compiled_job_directory',
+                type=click.Path(exists=True, file_okay=False))
+@click.option('--conf', type=click.Path(exists=True, dir_okay=False))
+def main(compiled_job_directory: str, conf: Optional[str] = None) -> None:
+    """Take a directory of Jenkins job XML and run some checks against it."""
+    config = ConfigParser()
+    if conf is not None:
+        config.read(conf)
+    result = lint_jobs_from_directory(compiled_job_directory, config)
+    if not result:
+        sys.exit(1)
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()  # pragma: nocover
