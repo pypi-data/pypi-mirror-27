6ed9b28d2a50499aebdd006de97f03de79357286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:02 +0000

    Add package auto-pack/1.0.5/auto_pack-1.0.5.tar.gz

---
+++ packages/auto-pack/auto_pack-1.0.5.tar.gz/auto_pack-1.0.5/auto_pack/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .FlavorApkProducer import produceApks
+__version__ = '1.0.5'
+
+def produceApks(autoPackConfig, appName):
+	FlavorApkProducer.produceApks(autoPackConfig, appName)
