1b9760c96092bd9ddadc0886144ef49b29656103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:30 +0000

    Add package lmpscore/0.1/lmpscore-0.1.tar.gz

---
+++ packages/lmpscore/lmpscore-0.1.tar.gz/lmpscore-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'lmpscore',
+  packages = ['lmpscore'],
+  version = '0.1',
+  description = 'LogicMonitor Professional Services - Core - Python',
+  author = 'LogicMonitor Professional Services',
+  author_email = 'ps@logicmonitor.com',
+  url = 'https://github.com/logicmonitor/lmps-core-python',
+  download_url = 'https://github.com/logicmonitor/lmps-core-python/archive/0.1.tar.gz',
+  keywords = ['lm', 'ps', 'sdk'],
+  classifiers = [],
+)
