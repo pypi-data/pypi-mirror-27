dee1c8e5f8ba7172e3ff34bd93c9243989cf493b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:13 +0000

    Add package ipydeps/0.5.1/ipydeps-0.5.1.tar.gz

---
+++ packages/ipydeps/ipydeps-0.5.1.tar.gz/ipydeps-0.5.1/setup.py
@@ -0,0 +1,23 @@
+# vim: expandtab tabstop=4 shiftwidth=4
+
+from setuptools import setup
+
+setup(
+    name='ipydeps',
+    version='0.5.1',
+    author='Bill Allen',
+    author_email='photo.allen@gmail.com',
+    description='A pip interface wrapper for installing packages from within Jupyter notebooks.',
+    license='MIT',
+    keywords='pip install setup jupyter notebook dependencies'.split(),
+    url='https://github.com/nbgallery/ipydeps',
+    download_url='https://github.com/nbgallery/ipydeps/archive/0.5.1.tar.gz',
+    packages=['ipydeps'],
+    install_requires=['pip', 'setuptools'],
+    dependency_links=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
