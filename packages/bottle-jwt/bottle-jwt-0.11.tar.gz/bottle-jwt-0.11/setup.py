22339e2e7ae34305f3ff9601d220759d50366d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:30 +0000

    Add package bottle-jwt/0.11/bottle-jwt-0.11.tar.gz

---
+++ packages/bottle-jwt/bottle-jwt-0.11.tar.gz/bottle-jwt-0.11/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import re
+import ast
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('bottle_jwt/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')
+    ).group(1)))
+
+
+setup(
+    name='bottle-jwt',
+    version=version,
+    packages=['bottle_jwt'],
+    url='',
+    license='GLPv3',
+    author='Papavassiliou Vassilis',
+    author_email='vpapavasil@gmail.com',
+    description='JWT Auth plugin for bottle.py applications',
+    install_requires=['pyjwt']
+)
