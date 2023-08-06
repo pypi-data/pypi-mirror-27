6adaccac93c5e9a304eff19387afa12bce2e4778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:18 +0000

    Add package memaccess/0.1/memaccess-0.1.tar.gz

---
+++ packages/memaccess/memaccess-0.1.tar.gz/memaccess-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='memaccess',
+    version='0.1',
+    packages=find_packages(),
+    author='Mischa Krüger (Makman2)',
+    author_email='makmanx64@gmail.com',
+    description="Python library for Windows giving live access to a program's memory",
+    license='MIT',
+)
