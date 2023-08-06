3359e4554514dff049309129f5c9c08c1487bd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:35 +0000

    Add package cqfirstnester/1.0.0/cqfirstnester-1.0.0.tar.gz

---
+++ packages/cqfirstnester/cqfirstnester-1.0.0.tar.gz/cqfirstnester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'cqfirstnester',
+    version = '1.0.0',
+    py_modules =['cqfirstnester'],
+    author = 'CQing',
+    author_email = '1209843765@qq.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+
+    )
