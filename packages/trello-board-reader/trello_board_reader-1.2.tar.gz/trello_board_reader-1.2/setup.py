853d266a8d9a88063b02f998677907d05c230239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:26 +0000

    Add package trello-board-reader/1.2/trello_board_reader-1.2.tar.gz

---
+++ packages/trello-board-reader/trello_board_reader-1.2.tar.gz/trello_board_reader-1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='trello_board_reader',
+    version='1.2',
+    keywords=['trello', 'trello-board', 'scraper'],
+    description='Small tool to help read from Trello boards',
+
+
+    classifiers=[],
+    packages=['trello_board_reader'],
+    install_requires=['requests'],
+
+    author='Shashank Saxena',
+    author_email='saxena.shashank96@gmail.com',
+    url='https://github.com/Salil999/TrelloBoardReader',
+    download_url='https://github.com/Salil999/TrelloBoardReader/archive/1.0.tar.gz',
+)
