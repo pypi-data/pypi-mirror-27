b918878d95b22f61698112780b102c9af4079aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:04 +0000

    Add package django-geoip2-extras/1.0/django-geoip2-extras-1.0.tar.gz

---
+++ packages/django-geoip2-extras/django-geoip2-extras-1.0.tar.gz/django-geoip2-extras-1.0/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from os import path, pardir, chdir
+
+from setuptools import setup, find_packages
+
+README = open(path.join(path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
+
+setup(
+    name="django-geoip2-extras",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        'Django>=1.10',
+        'geoip2>=2.4'
+    ],
+    include_package_data=True,
+    description='Additional functionality using the GeoIP2 database.',
+    license='MIT',
+    long_description=README,
+    url='https://github.com/yunojuno/django-geoip2-extras',
+    author='YunoJuno',
+    author_email='code@yunojuno.com',
+    maintainer='YunoJuno',
+    maintainer_email='code@yunojuno.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
