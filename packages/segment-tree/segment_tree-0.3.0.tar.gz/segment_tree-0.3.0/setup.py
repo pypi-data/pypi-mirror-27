05da280a8f9d432533ac6181a9b21d216e1826ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:13 +0000

    Add package segment-tree/0.3.0/segment_tree-0.3.0.tar.gz

---
+++ packages/segment-tree/segment_tree-0.3.0.tar.gz/segment_tree-0.3.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+from codecs import open
+from os import path
+
+long_description = open('README').read()
+
+setup(
+    name='segment_tree',
+    version='0.3.0',
+    description='Multidimensional segment tree with ranges updates.',
+    long_description=long_description,
+    url='https://github.com/evgeth/segment_tree',
+    author='Evgeny Yurtaev',
+    author_email='eugene@yurtaev.com',
+    license='MIT',
+    packages=find_packages(exclude=['tests*']),
+    keywords='segment, tree, range, rmq, multidimensional',
+    python_requires='>=3')
