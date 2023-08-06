51ce2071b6152d5d8a461c263c1786a4754d1224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:44 +0000

    Add package BirthdayFb/0.3/BirthdayFb-0.3.tar.gz

---
+++ packages/BirthdayFb/BirthdayFb-0.3.tar.gz/BirthdayFb-0.3/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup,find_packages
+import os
+import subprocess
+
+setup(name='BirthdayFb',
+      version='0.3',
+      description='Schedule your Birthday wishes for your facebook friends and never Miss any birthdays',
+      url='https://github.com/ankitpyc/Autopy/tree/master/Facebook_BirthdayWish_/Fb_Birthday',
+      author='Ankit Mishra',
+      author_email='ankitmishra@gmail.com',
+      license='MIT',	
+      entry_points={
+        'console_scripts': ['WishFb = FbBirthday.Happy:main','FbSettings = FbBirthday.UpdateCred:Update'],
+          },         
+      include_package_data=True,
+      packages=find_packages(),  
+      install_requires=[
+      'python-crontab==2.2.8',
+      'click==6.7',
+      'colorama',
+      'chromedriver-installer==0.0.6',
+      'colorama==0.3.9',
+      'notify2==0.3.1',
+      'pygobject==3.20.0',
+      'requests==2.18.4',
+      'selenium==3.6.0',
+      'subprocess32==3.2.7'
+      ],
+      zip_safe=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
