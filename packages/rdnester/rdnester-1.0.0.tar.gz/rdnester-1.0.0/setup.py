03ac6b2ce98a06f789fa7bd26fe7026e0da486fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:31 +0000

    Add package rdnester/1.0.0/rdnester-1.0.0.tar.gz

---
+++ packages/rdnester/rdnester-1.0.0.tar.gz/rdnester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'rdnester', 
+        version      = '1.0.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['rdnester'],
+        author       = 'rd',
+        author_email = 'muhan95@163.com',
+        url          = 'http://www.mudoli.com',
+        description  = 'print nester list',
+     )
