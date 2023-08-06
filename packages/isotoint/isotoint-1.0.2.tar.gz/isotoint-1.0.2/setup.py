8fcc84f415448b60ad5416267fbd729876b6889d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:25 +0000

    Add package isotoint/1.0.2/isotoint-1.0.2.tar.gz

---
+++ packages/isotoint/isotoint-1.0.2.tar.gz/isotoint-1.0.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from os import path
+from distutils.core import setup
+from Cython.Build import cythonize
+
+def read(relpath):
+	with open(path.join(path.dirname(__file__), relpath)) as f:
+		return f.read()
+
+setup(
+	name = 'isotoint',
+	version = read('version.txt').strip(),
+	description = 'Convert ISO 8601 string to integer in a microsecond.',
+	long_description = read('README.rst'),
+	author = 'Radek Kysely',
+	author_email = 'kyselyradek@gmail.com',
+	url = 'https://github.com/kysely/isotoint',
+	license = 'MIT',
+	ext_modules = cythonize('isotoint/__init__.pyx'),
+	keywords = ['iso', 'iso8601', 'integer', 'cython'],
+	packages = [
+		'isotoint'
+	]
+)
