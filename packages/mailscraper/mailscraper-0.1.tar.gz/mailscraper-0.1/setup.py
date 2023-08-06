ff2848134e3a269b0c7f4e432964b187751bf686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:31 +0000

    Add package mailscraper/0.1/mailscraper-0.1.tar.gz

---
+++ packages/mailscraper/mailscraper-0.1.tar.gz/mailscraper-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mailscraper',
+	version='0.1',
+	description="""Webcrawler will enter your domain and scan every page of your website, extracting page titles, descriptions, keywords, and links etc,.""",
+	url='http://github.com/username/mailscraper',
+	author='JALEEL AKBERUDDIN BASHA',
+	author_email='jaleelbasha0@gmail.com',
+	license='JaleelBasha',
+	packages=['mailscraper'],
+	zip_safe=False)
