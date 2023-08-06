be85782d31e319a1c5741f8dd60ea25b4c1e8f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:34 +0000

    Add package avro-json-serializer/1.0.1/avro_json_serializer-1.0.1.tar.gz

---
+++ packages/avro-json-serializer/avro_json_serializer-1.0.1.tar.gz/avro_json_serializer-1.0.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+# (c) [2014] LinkedIn Corp. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License"); 
+# you may not use this file except in compliance with the License. 
+# You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
+ 
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+import setuptools
+from sys import version_info
+
+# Project uses OrderedDict which is part of Python Standard Library
+# since version 2.7. On older versions, this is provided by simplejson.
+install_requires = ['six']
+if version_info[:2] < (2, 7):
+    install_requires.append('simplejson >= 2.0.9')
+
+if version_info[:2] <= (3, 0):
+    install_requires.append('avro==1.8.2')
+else:
+    install_requires.append('avro-python3==1.8.2')
+
+setuptools.setup(
+        name='avro_json_serializer',
+        version='1.0.1',
+        description='Avro Json Serializer',
+        author='Roman Inozemtsev',
+        author_email='rinozemtsev@linkedin.com',
+        install_requires = install_requires,
+        packages = ['avro_json_serializer'],
+        license = 'Apache 2.0'
+)
