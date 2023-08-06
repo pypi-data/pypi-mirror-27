a80c2c63c4bce71523d4d51492b9ebefe7c00046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:16 +0000

    Add package parski/2.0.0.129/parski-2.0.0.129.tar.gz

---
+++ packages/parski/parski-2.0.0.129.tar.gz/parski-2.0.0.129/parski/__init__.py
@@ -0,0 +1,2 @@
+from filter_data import FilterData as filter_data
+from get_data import GetData as get_data
---
+++ packages/parski/parski-2.0.0.129.tar.gz/parski-2.0.0.129/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+setup(
+  name = 'parski',
+  packages = ['parski'],
+  version = '2.0.0.129',
+  description = 'Helper library for parsing vpc data feed',
+  author = 'Russell Endicott',
+  author_email = 'rendicott@gmail.com',
+  keywords = ['vpc', 'json', 'parski'],
+  classifiers = []
+)
