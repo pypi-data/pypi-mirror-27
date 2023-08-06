2fad85e8d314a18cad5c2bb645a4f5345c8f062d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:57 +0000

    Add package android-utils/1.1.1/android_utils-1.1.1.tar.gz

---
+++ packages/android-utils/android_utils-1.1.1.tar.gz/android_utils-1.1.1/android_utils/CurlDownloadHelper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import shutil
+
+
+def download(url, destDir):
+    if not os.path.exists(destDir):
+        os.makedirs(destDir)
+    fileName = url.split("/", url.count("/"))[url.count("/")]
+    filePath = destDir + "/" + fileName
+    if os.path.exists(filePath):
+        os.remove(filePath)
+    os.system("curl -o %s %s" % (filePath, url))
+    return filePath
