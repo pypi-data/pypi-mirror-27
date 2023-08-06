63da48fd457c4eb11228513ec43d98181caf7fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:59 +0000

    Add package jenks-natural-breaks/0.1.0/jenks_natural_breaks-0.1.0-cp27-cp27m-win_amd64.whl

---
+++ packages/jenks-natural-breaks/jenks_natural_breaks-0.1.0-cp27-cp27m-win_amd64.whl/jenks_natural_breaks/__init__.py
@@ -0,0 +1,43 @@
+__version__ = '0.1.0'
+
+import numpy as np
+
+from ._jenks_matrices import ffi as _ffi
+from ._jenks_matrices import lib as _lib
+
+
+def _jenks_matrices(data, n_classes):
+    lower_class_limits = np.zeros((len(data) + 1, n_classes + 1), dtype=int)
+    variance_combinations = np.zeros((len(data) + 1, n_classes + 1), dtype=float)
+
+    lower_class_limits[1, :] = 1
+    variance_combinations[2:, :] = float("Inf")
+
+    _lib.jenks_matrices(
+        len(data), n_classes,
+        _ffi.cast("double *", data.ctypes.data),
+        _ffi.cast("int *", lower_class_limits.ctypes.data),
+        _ffi.cast("double *", variance_combinations.ctypes.data))
+
+    return lower_class_limits, variance_combinations
+
+
+def _jenks_breaks(data, lower_class_limits, n_classes):
+    k = len(data) - 1
+    class_breaks = [data[-1]] * (n_classes + 1)
+    class_breaks[0] = data[0]
+
+    for i in range(n_classes, 1, -1):
+        idx = lower_class_limits[k, i] - 2
+        class_breaks[i - 1] = data[idx]
+        k = lower_class_limits[k, i] - 1
+
+    return class_breaks
+
+
+def classify(data, n_classes):
+    assert len(data.shape) == 1 and n_classes < data.shape[0]
+    data = np.copy(data)
+    data.sort()
+    lower_class_limits, _ = _jenks_matrices(data, n_classes)
+    return _jenks_breaks(data, lower_class_limits, n_classes)
---
+++ packages/jenks-natural-breaks/jenks_natural_breaks-0.1.0-cp27-cp27m-win_amd64.whl/jenks_natural_breaks/_jenks_matrices.c
@@ -0,0 +1,42 @@
+
+#define idx(row_len, i, j) ((row_len) * (i) + (j))
+
+void jenks_matrices(
+    const int data_len, const int n_classes, double *data,
+    int *lower_class_limits,
+    double *variance_combinations) {
+
+    const int row_len = n_classes + 1;
+    
+    int l, m, j;
+    for (l = 2; l < data_len + 1; l++) {
+        double sum = 0,
+            sum_squares = 0,
+            variance = 0;
+
+        for (m = 1; m < l + 1; m++) {
+
+            int lower_class_limit = l - m + 1;
+            double val = data[lower_class_limit - 1];
+
+            sum += val;
+            sum_squares += val * val;
+
+            variance = sum_squares - (sum * sum) / m;
+
+            if (lower_class_limit != 1) {
+                for (j = 2; j < n_classes + 1; j++) {
+                    const double test_variance = variance
+                        + variance_combinations[idx(row_len, lower_class_limit - 1, j - 1)];
+                    if (variance_combinations[idx(row_len, l, j)] >= test_variance) {
+                        lower_class_limits[idx(row_len, l, j)] = lower_class_limit;
+                        variance_combinations[idx(row_len, l, j)] = test_variance;
+                    }
+                }
+            }
+        }
+
+        lower_class_limits[idx(row_len, l, 1)] = 1;
+        variance_combinations[idx(row_len, l, 1)] = variance;
+    }
+}
