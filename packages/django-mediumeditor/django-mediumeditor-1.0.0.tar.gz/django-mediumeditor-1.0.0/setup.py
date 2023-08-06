15078e39be136a9dc0d59716d2825f02cae288b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:50 +0000

    Add package django-mediumeditor/1.0.0/django-mediumeditor-1.0.0.tar.gz

---
+++ packages/django-mediumeditor/django-mediumeditor-1.0.0.tar.gz/django-mediumeditor-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+long_description = open('README.md').read()
+
+setup(
+    name="django-mediumeditor",
+    version='1.0.0',
+    packages=["mediumeditor"],
+    include_package_data=True,
+    description="Medium Editor widget for Django",
+    url="https://github.com/g3rd/django-mediumeditor",
+    author="Chad Shryock",
+    author_email="chad@aceportfol.io",
+    license='MIT',
+    long_description=long_description,
+    platforms=["any"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=[
+        'django-appconf >= 1.0.2',
+    ],
+)
