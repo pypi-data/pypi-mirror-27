6ac0da16f6b6e7e3d11b34a0312a8f02415e08c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:08 +0000

    Add package dawa-facade/2017.12.1b1/dawa_facade-2017.12.1b1.tar.gz

---
+++ packages/dawa-facade/dawa_facade-2017.12.1b1.tar.gz/dawa_facade-2017.12.1b1/dawa_facade/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""__init__.py
+Copyright 2017 Martin Storgaard Dieu under The MIT License
+
+Written by Martin Storgaard Dieu <martin@storgaarddieu.com>, november 2017
+"""
+from .facade.facade import DawaFacade
+
+__version__ = '2017.12.1.b1'
