4c0be8f04a379ad52985cdc262888f913f0370f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:53 +0000

    Add package cnfaker/0.1.1/cnfaker-0.1.1.zip

---
+++ packages/cnfaker/cnfaker-0.1.1.zip/cnfaker-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'cnfaker',
+    version = '0.1.1',
+    keywords='faker generate chinese words',
+    description = 'a library for chinese words or other things filling ',
+    license = 'MIT License',
+    author = 'hjlarry',
+    author_email = 'hjlarry@163.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
