ea30b19faace62213b3f00dcde5bf22ef0d723d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:15 +0000

    Add package djangocms-helper/1.1.0b1/djangocms_helper-1.1.0b1-py2.py3-none-any.whl

---
+++ packages/djangocms-helper/djangocms_helper-1.1.0b1-py2.py3-none-any.whl/djangocms_helper/test_utils/cms_helper_extra_runner.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from djangocms_helper.test_utils.cms_helper import HELPER_SETTINGS
+
+HELPER_SETTINGS['TEMPLATE_CONTEXT_PROCESSORS'] = 'django.core.context_processors.debug'
+HELPER_SETTINGS['TEST_RUNNER'] = 'runners.CapturedOutputRunner'
