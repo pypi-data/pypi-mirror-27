f7bc4063dd7d408dcff3d421f769403bb2246ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:01 +0000

    Add package hodl/1.0.0.dev3/hodl-1.0.0.dev3.tar.gz

---
+++ packages/hodl/hodl-1.0.0.dev3.tar.gz/hodl-1.0.0.dev3/hodl/__init__.py
@@ -0,0 +1,82 @@
+#!bin/python
+from __future__ import print_function, unicode_literals
+
+try:
+    from urllib.request import urlopen, Request, HTTPError
+    import configparser as cp
+except ImportError:
+    import ConfigParser as cp
+    from urllib2 import urlopen, Request, HTTPError
+import argparse
+import json
+import os
+
+# constants
+description = """
+Your friendly, no-nonsense script to instantaneously check cryptocurrency prices, 
+helping you HODL one day at a time :)
+"""
+
+# read config
+config_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                               'conf', 'config.ini')
+config = cp.ConfigParser()
+config.read(config_filename)
+
+
+def get_price(crypto="BTC", fiat=config.get("currency", "FIAT")):
+    try:
+        url = 'https://api.coinbase.com/v2/prices/{}-{}/spot'.format(crypto, fiat)
+        req = Request(url)
+        r = urlopen(req).read()
+        data = json.loads(r.decode('utf-8'))
+        return "1 {} = {} {}".format(data['data']['base'],
+                                     data['data']['amount'],
+                                     data['data']['currency'])
+    except HTTPError:
+        return "[*] error, check you are using correct crypto and fiat symbols"
+
+
+def get_majors(fiat=config.get("currency", "FIAT")):
+    return [get_price(crypto, fiat) for crypto in
+            ['BTC', 'BCH', 'ETH', 'LTC']]
+
+
+def set_fiat(fiat):
+    try:
+        config.set('currency', 'FIAT', fiat)
+        with open(config_filename, 'w') as configfile:
+            config.write(configfile)
+        return "[*] {} configured as standard fiat".format(fiat)
+    except Exception as e:
+        return "[*] error while configuring fiat, report: ", e
+
+
+def main():
+    parser = argparse.ArgumentParser(description=description)
+    group = parser.add_mutually_exclusive_group()
+    parser.add_argument('-c', '--crypto',
+                        help='the crypto you wish to price check',
+                        choices=['BTC', 'BCH', 'ETH', 'LTC'])
+    group.add_argument('-f', '--fiat',
+                        help='the fiat currency you wish to use for comparison')
+    group.add_argument('-sf', '--set_fiat',
+                        help='set your preferred fiat currency')
+    args = parser.parse_args()
+
+    if args.crypto and args.fiat:
+        print(get_price(args.crypto, args.fiat))
+    elif (args.set_fiat and args.crypto) or (args.crypto and args.set_fiat):
+        print(set_fiat(args.set_fiat))
+        print(get_price(args.crypto))
+    elif args.crypto:
+        print(get_price(args.crypto))
+    elif args.set_fiat:
+        print(set_fiat(args.set_fiat))
+    else:
+        for report in get_majors():
+            print(report)
+
+
+if __name__ == '__main__':
+    main()
