cbaed26985ee3c77010a14f7a61319f50a1eb900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:25 +0000

    Add package stylelens-search-vector/1.0.7/stylelens-search-vector-1.0.7.tar.gz

---
+++ packages/stylelens-search-vector/stylelens-search-vector-1.0.7.tar.gz/stylelens-search-vector-1.0.7/setup.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+"""
+    stylelens-search-vector
+"""
+
+
+from setuptools import setup, find_packages
+
+NAME = "stylelens-search-vector"
+VERSION = "1.0.7"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["numpy", "grpcio", "protobuf"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="stylelens-search-vector",
+    author_email="bluehackmaster@bluehack.net",
+    url="",
+    keywords=["BlueLens", "stylelens-search-vector"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    """
+)
