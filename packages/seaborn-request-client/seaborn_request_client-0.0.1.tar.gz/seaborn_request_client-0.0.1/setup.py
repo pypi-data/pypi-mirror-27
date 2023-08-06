12718216dfeb550f73a0cab5555aa01c93dfb616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:02 +0000

    Add package seaborn-request-client/0.0.1/seaborn_request_client-0.0.1.tar.gz

---
+++ packages/seaborn-request-client/seaborn_request_client-0.0.1.tar.gz/seaborn_request_client-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='seaborn_request_client',
+    version='0.0.1',
+    description='SeabornTimingProfile collects, records, and reports timing'
+                'data on code implementing a number of different execution'
+                'strategies"',
+    long_description='',
+    author='Ben Christenson',
+    author_email='Python@BenChristenson.com',
+    url='https://github.com/SeabornGames/RequestClient',
+    install_requires=[],
+    extras_require={
+    },
+    license='MIT License',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: Other/Proprietary License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5')
+)
