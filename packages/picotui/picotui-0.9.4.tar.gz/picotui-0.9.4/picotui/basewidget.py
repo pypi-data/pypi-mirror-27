0c1869dce7564b461dd65ea2fdd82b1df6bbe61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:03 +0000

    Add package picotui/0.9.4/picotui-0.9.4.tar.gz

---
+++ packages/picotui/picotui-0.9.4.tar.gz/picotui-0.9.4/picotui/basewidget.py
@@ -0,0 +1,94 @@
+import os
+
+from .screen import *
+
+
+# Standard widget result actions (as return from .loop())
+ACTION_OK = 1000
+ACTION_CANCEL = 1001
+ACTION_NEXT = 1002
+ACTION_PREV = 1003
+
+class Widget(Screen):
+
+    focusable = False
+    # If set to non-False, pressing Enter on this widget finishes
+    # dialog, with Dialog.loop() return value being this value.
+    finish_dialog = False
+
+    def __init__(self):
+        self.kbuf = b""
+        self.signals = {}
+
+    def set_xy(self, x, y):
+        self.x = x
+        self.y = y
+
+    def inside(self, x, y):
+        return self.y <= y < self.y + self.h and self.x <= x < self.x + self.w
+
+    def signal(self, sig):
+        if sig in self.signals:
+            self.signals[sig](self)
+
+    def on(self, sig, handler):
+        self.signals[sig] = handler
+
+    @staticmethod
+    def longest(items):
+        if not items:
+            return 0
+        return max((len(t) for t in items))
+
+    def set_cursor(self):
+        # By default, a widget doesn't use text cursor, so disables it
+        self.cursor(False)
+
+    def get_input(self):
+        if self.kbuf:
+            key = self.kbuf[0:1]
+            self.kbuf = self.kbuf[1:]
+        else:
+            key = os.read(0, 32)
+            if key[0] != 0x1b:
+                key = key.decode()
+                self.kbuf = key[1:].encode()
+                key = key[0:1].encode()
+        key = KEYMAP.get(key, key)
+
+        if isinstance(key, bytes) and key.startswith(b"\x1b[M") and len(key) == 6:
+            row = key[5] - 33
+            col = key[4] - 33
+            return [col, row]
+
+        return key
+
+    def handle_input(self, inp):
+        if isinstance(inp, list):
+            res = self.handle_mouse(inp[0], inp[1])
+        else:
+            res = self.handle_key(inp)
+        return res
+
+    def loop(self):
+        self.redraw()
+        while True:
+            key = self.get_input()
+            res = self.handle_input(key)
+
+            if res is not None and res is not True:
+                return res
+
+
+# Widget with few internal selectable items
+class ItemSelWidget(Widget):
+
+    def __init__(self, items):
+        super().__init__()
+        self.items = items
+        self.selected = 0
+
+    def move_sel(self, direction):
+        self.selected = (self.selected + direction) % len(self.items)
+        self.redraw()
+        self.signal("changed")
