a0b12149752f55ee3c646c149a2ee0b8b05bbead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:18 +0000

    Add package elcaminoreal/17.12.1/elcaminoreal-17.12.1.tar.gz

---
+++ packages/elcaminoreal/elcaminoreal-17.12.1.tar.gz/elcaminoreal-17.12.1/src/elcaminoreal/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides elcaminoreal version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update elcaminoreal` to change this file.
+
+from incremental import Version
+
+__version__ = Version('elcaminoreal', 17, 12, 1)
+__all__ = ["__version__"]
