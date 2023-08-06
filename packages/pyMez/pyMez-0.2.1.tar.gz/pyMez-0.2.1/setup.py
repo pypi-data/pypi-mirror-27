6a51999ad1c9f7861c15bbf8d87fdad76e1fb680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:06 +0000

    Add package pyMez/0.2.1/pyMez-0.2.1.tar.gz

---
+++ packages/pyMez/pyMez-0.2.1.tar.gz/pyMez-0.2.1/setup.py
@@ -0,0 +1,48 @@
+#-----------------------------------------------------------------------------
+# Name:        setup
+# Purpose:    
+# Author:      Aric Sanders
+# Created:     12/30/2017
+# License:     MIT License
+#-----------------------------------------------------------------------------
+""" Module for distribution """
+#-----------------------------------------------------------------------------
+# Standard Imports
+from setuptools import setup, find_packages
+
+#-----------------------------------------------------------------------------
+# Third Party Imports
+
+#-----------------------------------------------------------------------------
+# Module Constants
+
+#-----------------------------------------------------------------------------
+# Module Functions
+
+#-----------------------------------------------------------------------------
+# Module Classes
+
+#-----------------------------------------------------------------------------
+# Module Scripts
+
+#-----------------------------------------------------------------------------
+# Module Runner
+if __name__ == '__main__':
+
+
+    setup(
+        name='pyMez',
+		package_dir={'':'pyMez'} ,
+        packages=find_packages('pyMez'),  # this must be the same as the name above
+        version='0.2.1',
+        description='Measurement, Analysis and Data Management Software. To load the API interface use from pyMez import *.',
+        author='Aric Sanders',
+        author_email='aric.sanders@gmail.com',
+        url='https://github.com/aricsanders/pyMez',  # use the URL to the github repo
+        download_url='https://github.com/aricsanders/pyMez.git',  # I'll explain this in a second
+        keywords=['measurement', 'data handling', 'example'],  # arbitrary keywords
+        classifiers=[],
+		license="MIT",
+		install_requires=['markdown','numpy','pandas',"PIL<=4.0.0","pdfkit","pdoc","networkx","visa"],
+    )
+    
