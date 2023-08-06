01a5045079e84a48d72338285d21ef452fde6c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:15 +0000

    Add package fdroidserver/1.0.0/fdroidserver-1.0.0.tar.gz

---
+++ packages/fdroidserver/fdroidserver-1.0.0.tar.gz/fdroidserver-1.0.0/buildserver/config.buildserver.py
@@ -0,0 +1,15 @@
+sdk_path = "/home/vagrant/android-sdk"
+ndk_paths = {
+    'r9b': "/home/vagrant/android-ndk/r9b",
+    'r10e': "/home/vagrant/android-ndk/r10e",
+    'r11c': "/home/vagrant/android-ndk/r11c",
+    'r12b': "/home/vagrant/android-ndk/r12b",
+    'r13b': "/home/vagrant/android-ndk/r13b",
+    'r14b': "/home/vagrant/android-ndk/r14b",
+    'r15c': "/home/vagrant/android-ndk/r15c",
+    'r16': "/home/vagrant/android-ndk/r16",
+}
+qt_sdk_path = "/home/vagrant/qt-sdk/5.7.0/5.7"
+java_paths = {
+    '8': "/usr/lib/jvm/java-8-openjdk-amd64",
+}
