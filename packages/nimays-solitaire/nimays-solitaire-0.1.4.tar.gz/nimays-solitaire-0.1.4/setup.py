b5e477cdb121208169fa272edef1cd71646f031d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:16 +0000

    Add package nimays-solitaire/0.1.4/nimays-solitaire-0.1.4.tar.gz

---
+++ packages/nimays-solitaire/nimays-solitaire-0.1.4.tar.gz/nimays-solitaire-0.1.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='nimays-solitaire',
+    version='0.1.4',
+    python_requires='>=3',
+    scripts=['bin/nimay-solitaire'],
+    packages=['nimay_solitaire'],
+    install_requires=['pygame']
+)
---
+++ packages/nimays-solitaire/nimays-solitaire-0.1.4.tar.gz/nimays-solitaire-0.1.4/test/test_foundation_pile.py
@@ -0,0 +1,63 @@
+from unittest import TestCase
+
+from nimay_solitaire.card import Card
+from nimay_solitaire.foundation_pile import FoundationPile
+
+
+class TestFoundationPile(TestCase):
+    """
+    A class to test the foundation pile.
+    """
+
+    """
+    Test the foundation pile initialization
+    """
+    def test_init(self):
+        fp = FoundationPile(Card.HEART)
+        self.assertEqual(0, len(fp.cards))
+        self.assertEqual(Card.HEART, fp.suit)
+
+    """
+    Test the foundation validity check on a valid case
+    """
+    def test_check_valid(self):
+        fp = FoundationPile(Card.HEART)
+        card = Card(Card.HEART, 0, True)
+        self.assertTrue(fp.check_valid(card))
+        fp.add_card(card, True, True)
+        card2 = Card(Card.HEART, 1, True)
+        self.assertTrue(fp.check_valid(card2))
+
+    """
+    Test the foundation validity check on a invalid case
+    """
+    def test_check_valid_invalid_case(self):
+        fp = FoundationPile(Card.HEART)
+        self.assertFalse(fp.check_valid(None))
+
+    """
+    Test adding a valid card to the foundation pile
+    """
+    def test_add_card(self):
+        fp = FoundationPile(Card.HEART)
+        card = Card(Card.HEART, 0, False)
+        self.assertTrue(fp.add_card(card, True, True))
+        self.assertEqual(1, len(fp.cards))
+
+    """
+    Test adding a invalid card to the foundation pile
+    """
+    def test_add_card_invalid_case(self):
+        fp = FoundationPile(Card.HEART)
+        self.assertFalse(fp.add_card(None, True, True))
+        self.assertEqual(0, len(fp.cards))
+
+    """
+    Test getting the string representation of the foundation pile
+    """
+    def test_str(self):
+        fp = FoundationPile(Card.HEART)
+        card = Card(Card.HEART, 0, False)
+        self.assertTrue(fp.add_card(card, True, True))
+        expected_str = "heart: ace of hearts"
+        self.assertEqual(expected_str, str(fp))
