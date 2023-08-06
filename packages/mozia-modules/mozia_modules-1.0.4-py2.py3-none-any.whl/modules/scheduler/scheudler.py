becdfe2ff7cf7218fd8a23f235f57d4b75adcf01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:37 +0000

    Add package mozia-modules/1.0.4/mozia_modules-1.0.4-py2.py3-none-any.whl

---
+++ packages/mozia-modules/mozia_modules-1.0.4-py2.py3-none-any.whl/modules/scheduler/scheudler.py
@@ -0,0 +1,20 @@
+from rediscluster import StrictRedisCluster
+
+
+class RedisTaskScheduler:
+    def __init__(self, nodes=[{"host": "172.16.8.147", "port": "6379"}]):
+        self.redis = StrictRedisCluster(startup_nodes=nodes, decode_responses=True)
+
+    def push(self, name, task):
+        # print "push task:" + task
+        self.redis.lpush(name, task)
+
+    def pop(self, name):
+        return self.redis.rpop(name)
+
+    def length(self, name):
+        return self.redis.llen(name)
+
+
+if __name__ == "__main__":
+    print ""
