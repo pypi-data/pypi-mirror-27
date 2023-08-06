bdc6c6312a07d88f39d51ab2e900d5a5385f7965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:08 +0000

    Add package nesternits1991/1.0.0/nesternits1991-1.0.0.tar.gz

---
+++ packages/nesternits1991/nesternits1991-1.0.0.tar.gz/nesternits1991-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name= 'nesternits1991',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'hfpython',
+    author_email = 'agg@gmail.com',
+    url = 'http://nitin.com',
+    description = 'a simple list printer',
+)
