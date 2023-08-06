caf69846de8124495a767c2d386205f078df54ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:14 +0000

    Add package revision/0.1.0/revision-0.1.0.tar.gz

---
+++ packages/revision/revision-0.1.0.tar.gz/revision-0.1.0/revision/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    revision
+    ~~~~~~~~
+
+    :copyright: (c) 2017 by SENSY Inc.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import
+
+__version__ = "0.1.0"
+__license__ = "MIT"
