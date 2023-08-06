106db5b38cec511e7159c0f12a55294e83b63f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:30 +0000

    Add package chipledclient/0.3/chipledclient-0.3.tar.gz

---
+++ packages/chipledclient/chipledclient-0.3.tar.gz/chipledclient-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'chipledclient',
+  packages = ['client'], # this must be the same as the name above
+  version = '0.3',
+  description = 'Client library for chip LED controller (see https://github.com/oxygen0211/chip-led-controller)',
+  author = 'Julian Engelhardt',
+  author_email = 'jengelhardt211@gmail.com',
+  url = 'https://github.com/oxygen0211/chip-led-client', # use the URL to the github repo
+  download_url = 'https://github.com/oxygen0211/chip-led-client/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['LED', 'C.H.I.P.', 'NextThing.co', 'GPIO'], # arbitrary keywords
+  classifiers = [],
+)
