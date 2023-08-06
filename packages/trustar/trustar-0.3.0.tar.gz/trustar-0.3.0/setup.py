d3b05a5197fd52fb2acb6e1c4393a8e61adaed11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:14 +0000

    Add package trustar/0.3.0/trustar-0.3.0.tar.gz

---
+++ packages/trustar/trustar-0.3.0.tar.gz/trustar-0.3.0/setup.py
@@ -0,0 +1,29 @@
+# git tag 0.3.0 -m "Bump version" && git push --tags origin master
+# python setup.py sdist
+# twine upload --skip-existing dist/*
+
+from distutils.core import setup
+
+setup(
+    name='trustar',
+    packages=['trustar'],
+    version='0.3.0',
+    author='TruSTAR Technology, Inc.',
+    author_email='support@trustar.co',
+    url='https://github.com/trustar/trustar-python',
+    download_url='https://github.com/trustar/trustar-python/tarball/0.3.0',
+    description='Python SDK for the TruSTAR REST API',
+    license='MIT',
+    install_requires=['future',
+                      'python-dateutil',
+                      'pytz',
+                      'requests',
+                      'configparser',
+                      'unicodecsv',
+                      'tzlocal',
+                      'PyYAML',
+                      'six'
+                      ],
+
+    use_2to3=True
+)
