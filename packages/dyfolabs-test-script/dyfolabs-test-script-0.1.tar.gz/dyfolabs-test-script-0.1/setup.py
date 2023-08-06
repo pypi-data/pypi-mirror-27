6f35eb923f3f876bbb4f86d6c79bcb91eec4b3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:48 +0000

    Add package dyfolabs-test-script/0.1/dyfolabs-test-script-0.1.tar.gz

---
+++ packages/dyfolabs-test-script/dyfolabs-test-script-0.1.tar.gz/dyfolabs-test-script-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='dyfolabs-test-script',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['test_script']                  # The name of your scipt, and also the command you'll be using for calling it
+)
