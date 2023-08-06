d3638bd643f2d9da68ec4fe8005d3d95cd671137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:04 +0000

    Add package gitlab-ci-test/0.8/gitlab-ci-test-0.8.tar.gz

---
+++ packages/gitlab-ci-test/gitlab-ci-test-0.8.tar.gz/gitlab-ci-test-0.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='gitlab-ci-test',    # This is the name of your PyPI-package.
+    version='0.8',                          # Update the version number for new releases
+    scripts=['helloworld.py'],                 # The name of your scipt, and also the command you'll be using for calling it
+	author="Artsiom Kushniarou",
+	author_email="kushnerovartem@gmail.com"
+)
