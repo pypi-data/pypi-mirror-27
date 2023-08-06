634006127c0375e1100dcc1d372399b944e55a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:42 +0000

    Add package logslacker/0.1.dev0/logslacker-0.1.dev0.tar.gz

---
+++ packages/logslacker/logslacker-0.1.dev0.tar.gz/logslacker-0.1.dev0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="logslacker",
+      version="0.1dev",
+      description="Log handler wrapper for the Slack Python API",
+      url="http://github.com/inJeans/LogSlacker",
+      author="Christopher Jon Watkins",
+      author_email="christopher.watkins@me.com",
+      license="MIT",
+      packages=["logslacker"],
+      zip_safe=False)
