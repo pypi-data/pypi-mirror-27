975e56029b4fb2bb61f654334bd19d3b0cc52082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:27 +0000

    Add package ai2thor/0.0.10/ai2thor-0.0.10-py2.py3-none-any.whl

---
+++ packages/ai2thor/ai2thor-0.0.10-py2.py3-none-any.whl/ai2thor/_builds.py
@@ -0,0 +1,5 @@
+# GENERATED FILE - DO NOT EDIT
+BUILDS = {'Darwin': {'sha256': '91fcd1b260cc616d54ea2f775546e5de2560d82e638cbf169e2a978d36041521',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712201400-OSXIntel64.zip'},
+ 'Linux': {'sha256': 'edecaab3e20f2ed2d779a685cc032ec34479216f743d172ebd39a8e2d0d9d9d1',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712201400-Linux64.zip'}}
