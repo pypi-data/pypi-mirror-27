059c17caf848b87451f0c24f7fe613b09958a17d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:15 +0000

    Add package aliyun-python-sdk-emr/2.4.0/aliyun-python-sdk-emr-2.4.0.tar.gz

---
+++ packages/aliyun-python-sdk-emr/aliyun-python-sdk-emr-2.4.0.tar.gz/aliyun-python-sdk-emr-2.4.0/aliyunsdkemr/request/v20160408/ModifyResourcePoolRequest.py
@@ -0,0 +1,70 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class ModifyResourcePoolRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Emr', '2016-04-08', 'ModifyResourcePool')
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_Name(self):
+		return self.get_query_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_query_param('Name',Name)
+
+	def get_Id(self):
+		return self.get_query_params().get('Id')
+
+	def set_Id(self,Id):
+		self.add_query_param('Id',Id)
+
+	def get_Clusterid(self):
+		return self.get_query_params().get('Clusterid')
+
+	def set_Clusterid(self,Clusterid):
+		self.add_query_param('Clusterid',Clusterid)
+
+	def get_Yarnsiteconfig(self):
+		return self.get_query_params().get('Yarnsiteconfig')
+
+	def set_Yarnsiteconfig(self,Yarnsiteconfig):
+		self.add_query_param('Yarnsiteconfig',Yarnsiteconfig)
+
+	def get_Configs(self):
+		return self.get_query_params().get('Configs')
+
+	def set_Configs(self,Configs):
+		for i in range(len(Configs)):	
+			if Configs[i].get('Id') is not None:
+				self.add_query_param('Config.' + bytes(i + 1) + '.Id' , Configs[i].get('Id'))
+			if Configs[i].get('Configkey') is not None:
+				self.add_query_param('Config.' + bytes(i + 1) + '.Configkey' , Configs[i].get('Configkey'))
+			if Configs[i].get('Configvalue') is not None:
+				self.add_query_param('Config.' + bytes(i + 1) + '.Configvalue' , Configs[i].get('Configvalue'))
+			if Configs[i].get('Category') is not None:
+				self.add_query_param('Config.' + bytes(i + 1) + '.Category' , Configs[i].get('Category'))
+			if Configs[i].get('Note') is not None:
+				self.add_query_param('Config.' + bytes(i + 1) + '.Note' , Configs[i].get('Note'))
