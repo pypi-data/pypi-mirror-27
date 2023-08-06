d5b3a4508c3d22c95996ce6ed86a2cb41c4f2763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:33 +0000

    Add package pytorch-monitor/0.1/pytorch-monitor-0.1.tar.gz

---
+++ packages/pytorch-monitor/pytorch-monitor-0.1.tar.gz/pytorch-monitor-0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'pytorch-monitor',
+  packages = ['pytorch-monitor'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Monitor pytorch modules with minimal code.',
+  author = 'Tom Effland',
+  author_email = 'teffland@cs.columbia.edu',
+  url = 'https://github.com/teffland/pytorch-monitor', # use the URL to the github repo
+  download_url = 'https://github.com/teffland/pytorch-monitor/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['pytorch', 'logging'], # arbitrary keywords
+  classifiers = [],
+  install_requires = [
+    'tensorboardX'
+  ]
+)
