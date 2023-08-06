3e2e0cd05a49066137fcb59d327ee0563c159d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:35 +0000

    Add package pynrrd/0.2.3/pynrrd-0.2.3.tar.gz

---
+++ packages/pynrrd/pynrrd-0.2.3.tar.gz/pynrrd-0.2.3/setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='pynrrd',
+      version='0.2.3',
+      description='Pure python module for reading and writing nrrd files.',
+      long_description='Pure python module for reading and writing nrrd files. See the github page for more information.',
+      author='Maarten Everts',
+      author_email='me@nn8.nl',
+      url='http://github.com/mhe/pynrrd',
+      py_modules=['nrrd'],
+      license="MIT License",
+      install_requires=['numpy'],
+      keywords = ["nrrd"],
+      classifiers = [
+          "License :: OSI Approved :: MIT License",
+          "Topic :: Scientific/Engineering",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3"
+      ]
+      )
