713411ecebfdd1908c3b29d30fd00dbf8903fb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:38 +0000

    Add package pylleida/0.2.6/pylleida-0.2.6-py2.py3-none-any.whl

---
+++ packages/pylleida/pylleida-0.2.6-py2.py3-none-any.whl/pylleida/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pylleida."""
+
+__author__ = """NickM. Jaremek"""
+__email__ = 'nick13jaremek@gmail.com'
+__version__ = '0.2.6'
+
+from pylleida.api.mail_cert import MailCertApi
+
+
+class PyLleida(object):
+    def __init__(self, username, password):
+        """
+        Python wrapper for the Lleida.net API.
+
+        There exists only one way to authenticate with the Lleida.net API, which consists on providing a
+        username/password combination.
+
+        :param username: Lleida.net username
+        :param password: Lleida.net password
+        """
+
+        config = dict(
+            username=username,
+            password=password
+        )
+
+        self.mailcert = MailCertApi(config)
