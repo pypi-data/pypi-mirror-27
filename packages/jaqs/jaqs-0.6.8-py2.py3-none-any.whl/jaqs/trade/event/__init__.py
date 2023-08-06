48406a8ec32919962f8dad5896346321e3f93e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:53 +0000

    Add package jaqs/0.6.8/jaqs-0.6.8-py2.py3-none-any.whl

---
+++ packages/jaqs/jaqs-0.6.8-py2.py3-none-any.whl/jaqs/trade/event/__init__.py
@@ -0,0 +1,7 @@
+# encoding: UTF-8
+"""
+Our trade is powered by this event engine that register, generate and process
+events in an efficient way.
+"""
+from .engine import EventEngine, Event
+from .eventtype import EVENT_TYPE
