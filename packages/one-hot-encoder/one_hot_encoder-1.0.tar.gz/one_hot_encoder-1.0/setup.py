1da60e534c2f9fea217694193ca0a80bcb3e33a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:17 +0000

    Add package one-hot-encoder/1.0/one_hot_encoder-1.0.tar.gz

---
+++ packages/one-hot-encoder/one_hot_encoder-1.0.tar.gz/one_hot_encoder-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='one_hot_encoder',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/yassineameur/one_hot_encoder',
+    license='MIT LICENSE',
+    author='Yassine Ameur',
+    author_email='yassine.ameur2013@gmail.com',
+    description='making one hot encoding so easy with pandas for ml practitioners'
+)
