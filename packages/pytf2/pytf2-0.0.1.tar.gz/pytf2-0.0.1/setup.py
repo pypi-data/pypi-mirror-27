8782ae2bbeeea50870cb0763b1be774d47587a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:13 +0000

    Add package pytf2/0.0.1/pytf2-0.0.1.tar.gz

---
+++ packages/pytf2/pytf2-0.0.1.tar.gz/pytf2-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pytf2',
+    version='0.0.1',
+    packages=['pytf2'],
+    url='https://github.com/mninc/pytf',
+    license='MIT',
+    author='manic',
+    author_email='manicminer106@gmail.com',
+    description='An API wrapper for everything TF2-related'
+)
