db498cb60c050dccdb52741c81ac18f77274c536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:31 +0000

    Add package django-oscar-wfrs/0.10.0rc1/django_oscar_wfrs-0.10.0rc1-py3-none-any.whl

---
+++ packages/django-oscar-wfrs/django_oscar_wfrs-0.10.0rc1-py3-none-any.whl/wellsfargo/api/app.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url
+from oscar.core.application import Application
+
+from .views import (
+    SelectCreditAppView,
+    USCreditAppView,
+    USJointCreditAppView,
+    CACreditAppView,
+    CAJointCreditAppView,
+    FinancingPlanView,
+    SubmitAccountInquiryView,
+)
+
+
+class WFRSAPIApplication(Application):
+    def get_urls(self):
+        urlpatterns = [
+            url(r'^apply/$', SelectCreditAppView.as_view(), name='wfrs-api-apply-select'),
+
+            url(r'^apply/us-individual/$', USCreditAppView.as_view(), name='wfrs-api-apply-us-individual'),
+            url(r'^apply/us-joint/$', USJointCreditAppView.as_view(), name='wfrs-api-apply-us-join'),
+            url(r'^apply/ca-individual/$', CACreditAppView.as_view(), name='wfrs-api-apply-ca-individual'),
+            url(r'^apply/ca-joint/$', CAJointCreditAppView.as_view(), name='wfrs-api-apply-ca-joint'),
+
+            url(r'^plans/$', FinancingPlanView.as_view(), name='wfrs-api-plan-list'),
+
+            url(r'^inquiry/$', SubmitAccountInquiryView.as_view(), name='wfrs-api-acct-inquiry'),
+        ]
+        return self.post_process_urls(urlpatterns)
+
+
+application = WFRSAPIApplication()
