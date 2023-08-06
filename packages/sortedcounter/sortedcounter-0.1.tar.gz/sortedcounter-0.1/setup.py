1b330d0caa6cbdffe5b01bfa72a075798028b5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:49 +0000

    Add package sortedcounter/0.1/sortedcounter-0.1.tar.gz

---
+++ packages/sortedcounter/sortedcounter-0.1.tar.gz/sortedcounter-0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+    name='sortedcounter',
+    version='0.1',
+    packages=['sortedcounter'],
+    license='MIT',
+    author='Cammie King',
+    author_email = 'cam@cmking.io',
+    url = 'https://github.com/ckingdev/sortedcounter',
+    download_url = 'https://github.com/ckingdev/sortedcounter/archive/0.1.tar.gz',
+    keywords = ['counter', 'collection', 'sorted', 'dict'],
+    classifiers = [],
+    description='A Counter like the collections class but with sorted keys.',
+)
