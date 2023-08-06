e935cab40710af1f60f4e4a9093fdbfd1ff37fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:11 +0000

    Add package codonOpt/0.21/codonOpt-0.21.tar.gz

---
+++ packages/codonOpt/codonOpt-0.21.tar.gz/codonOpt-0.21/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='codonOpt',
+    version='0.21',
+    packages=['codonOpt'],
+    url='https://github.com/willfinnigan/codonOpt',
+    license='MIT',
+    author='William Finnigan',
+    author_email='wjafinnigan@gmail.com',
+    description='A package for codon optimisation',
+    download_url = 'https://github.com/willfinnigan/codonOpt/archive/0.21.tar.gz',
+    keywords = ['DNA', 'Codon Optimisation', 'Gene Design']
+)
