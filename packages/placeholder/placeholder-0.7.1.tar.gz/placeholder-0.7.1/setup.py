366d784646b59cb55d95f32140ec9e739eca28c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:06 +0000

    Add package placeholder/0.7.1/placeholder-0.7.1.tar.gz

---
+++ packages/placeholder/placeholder-0.7.1.tar.gz/placeholder-0.7.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import placeholder
+
+setup(
+    name='placeholder',
+    version=placeholder.__version__,
+    description='Operator overloading for fast anonymous functions.',
+    long_description=open('README.rst').read(),
+    author='Aric Coady',
+    author_email='aric.coady@gmail.com',
+    url='https://bitbucket.org/coady/placeholder',
+    license='Apache Software License',
+    py_modules=['placeholder'],
+    python_requires='>=2.7',
+    tests_require=['pytest-cov', 'pytest-parametrized'],
+    keywords='functional lambda scala underscore',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
