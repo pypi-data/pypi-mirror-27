aec2ed546870e8d42b342852e510fb5abadf30ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:03 +0000

    Add package run80by24-common/0.1.0/run80by24-common-0.1.0.tar.gz

---
+++ packages/run80by24-common/run80by24-common-0.1.0.tar.gz/run80by24-common-0.1.0/run80by24/common/id_generator/__init__.py
@@ -0,0 +1,27 @@
+import base64
+import hashlib
+import secrets
+import pkg_resources
+import io
+
+stream = io.TextIOWrapper(pkg_resources.resource_stream(__package__,'words.txt'))
+try:
+    all_words = [line.strip() for line in stream.readlines()]
+finally:
+    stream.close()
+
+def randomwords(k=4):
+    return [secrets.choice(all_words) for _ in range(k)]
+
+def hashwords_b64(words):
+    msg = ' '.join(words).encode()
+    digest = hashlib.sha256(msg).digest()
+    b64_bytes = base64.urlsafe_b64encode(digest)
+    return str(b64_bytes,'utf-8').rstrip('=')
+
+def hashwords_hex(words):
+    msg = ' '.join(words).encode()
+    return hashlib.sha256(msg).hexdigest()
+
+def id_hash(words):
+    return hashwords_b64(words)[:8]
