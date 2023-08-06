4a0f17c54ec9380ee06f7304ca5d68d36c7977d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:44 +0000

    Add package ethereum-keys/0.1.0a9/ethereum_keys-0.1.0a9-py3-none-any.whl

---
+++ packages/ethereum-keys/ethereum_keys-0.1.0a9-py3-none-any.whl/eth_keys/__init__.py
@@ -0,0 +1,5 @@
+raise DeprecationWarning(
+    "This library has been renamed to `eth-keys`.  The `ethereum-keys` "
+    "package will no longer recieve updates.  Please update your dependencies "
+    "accordingly."
+)
---
+++ packages/ethereum-keys/ethereum_keys-0.1.0a9-py3-none-any.whl/ethereum_keys-0.1.0a9.dist-info/DESCRIPTION.rst
@@ -0,0 +1,7 @@
+Ethereum Keys
+=============
+
+This library has been renamed to ``eth-keys``. This package will no
+longer recieve updates. Please update your dependencies accordingly.
+
+
