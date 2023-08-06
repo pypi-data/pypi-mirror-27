29fdb75b46bfcc485ef7c533d1e1bf3152f344d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:47 +0000

    Add package japanese-numbers-python/0.1.1/japanese-numbers-python-0.1.1.tar.gz

---
+++ packages/japanese-numbers-python/japanese-numbers-python-0.1.1.tar.gz/japanese-numbers-python-0.1.1/japanese_numbers/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+from __future__ import absolute_import
+
+from japanese_numbers.parser.to_arabic_parser import (  # noqa
+  to_arabic,
+  to_arabic_numbers
+)
+
+__japanese_numbers__ = 'japanese-numbers-python'
+__version__ = '0.1.1'
+__author__ = 'Takumakanari'
+__license__ = 'MIT'
+__description__ = 'A parser for Japanese number (Kanji, arabic) in the natulal language.'  # noqa
+
