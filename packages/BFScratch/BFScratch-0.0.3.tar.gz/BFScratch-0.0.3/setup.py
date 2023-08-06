ad0f0f094b28d49b8345b8d9e0cc3a25fdce08c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:04 +0000

    Add package BFScratch/0.0.3/BFScratch-0.0.3.tar.gz

---
+++ packages/BFScratch/BFScratch-0.0.3.tar.gz/BFScratch-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='BFScratch',
+    version='0.0.3',
+    py_modules=['BFScratch'],
+    url = "http://blog.coderhelper.cn",
+    author = "limhang",
+    author_email = "lm_hang@163.com",
+    install_requires=[
+        'requests','lxml','pymysql'
+    ],
+    entry_points={},
+)
