ea6d8b2552c4f6b3ed304a956d5751f229a5a85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:44 +0000

    Add package libevdev/0.2/libevdev-0.2.tar.gz

---
+++ packages/libevdev/libevdev-0.2.tar.gz/libevdev-0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='libevdev',
+      version='0.2',
+      description='Python wrapper for libevdev',
+      author='Peter Hutterer',
+      author_email='peter.hutterer@who-t.net',
+      url='https://www.github.com/whot/libevdev-python/',
+      packages=['libevdev'],
+      )
