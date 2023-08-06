4db9f36bbe6a989fc9bb05268ae6dab31e2894ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:05 +0000

    Add package partridge/0.4.0/partridge-0.4.0-py2.py3-none-any.whl

---
+++ packages/partridge/partridge-0.4.0-py2.py3-none-any.whl/partridge/__init__.py
@@ -0,0 +1,24 @@
+from partridge.__version__ import __version__  # noqa: F401
+from partridge.gtfs import feed, raw_feed  # noqa: F401
+
+
+__all__ = [
+    '__version__',
+    'feed',
+    'raw_feed',
+    'read_service_ids_by_date',
+    'read_dates_by_service_ids',
+    'read_trip_counts_by_date',
+]
+
+
+def read_service_ids_by_date(path):
+    return feed(path).service_ids_by_date
+
+
+def read_dates_by_service_ids(path):
+    return feed(path).dates_by_service_ids
+
+
+def read_trip_counts_by_date(path):
+    return feed(path).trip_counts_by_date
