1f1d62b86b4c329b7454613f2eff8a70f09e2ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:28 +0000

    Add package IntraPy/0.1.6.1/IntraPy-0.1.6.1.tar.gz

---
+++ packages/IntraPy/IntraPy-0.1.6.1.tar.gz/IntraPy-0.1.6.1/IntraPy/config.py
@@ -0,0 +1,24 @@
+'''
+    A python Library to help make python apps/bots using the 42 API
+    Copyright (C) 2017-2018  Jules LASNE jlasne@student.42.fr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from decouple import config
+
+# 42 API Variables
+APP_UID = config('APP_UID', cast = str, default = None)
+APP_SECRET = config('APP_SECRET', cast = str, default = None)
+TOKEN_FILE = config('TOKEN_FILE', cast = str, default = None)
