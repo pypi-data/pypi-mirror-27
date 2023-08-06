236d0fb6c5b0383a3fedf483c8d074b2307bb04e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:49 +0000

    Add package logomaker/0.1/logomaker-0.1.tar.gz

---
+++ packages/logomaker/logomaker-0.1.tar.gz/logomaker-0.1/logomaker/__init__.py
@@ -0,0 +1,25 @@
+from __future__ import division
+import numpy as np
+import pandas as pd
+import ast
+import inspect
+import re
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties, FontManager
+from matplotlib.transforms import Bbox
+from matplotlib.colors import to_rgba
+import matplotlib as mpl
+import pdb
+
+import validate
+from validate import validate_parameter, validate_mat
+
+from data import load_alignment
+from Logo import Logo
+from make_logo import make_logo
+from character import get_fontnames_dict, get_fontnames
+from make_styled_logo import make_styled_logo
+
+from load_meme import load_meme
