29a9dfe0f710a0cc7dc0400ef0ae23d034872b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:40 +0000

    Add package sqlphile/0.1.3.7/sqlphile-0.1.3.7.tar.gz

---
+++ packages/sqlphile/sqlphile-0.1.3.7.tar.gz/sqlphile-0.1.3.7/sqlphile/utils.py
@@ -0,0 +1,70 @@
+import datetime
+
+class F:
+	def __init__ (self, val):
+		self.val = val
+		
+	def __str__ (self):
+		return self.val
+	
+	def add_percent (self, pos = 0):
+		if pos == 0:
+			self.val = "'%' || " + str (self.val) + " || '%'"
+		elif pos == 1:	
+			self.val = "'%' || " + str (self.val)
+		else:
+			self.val = str (self.val) + " || '%'"	
+
+class D:
+	def __init__ (self, **data):
+		self._feed = data
+		self._columns = list (self._feed.keys ())
+		self._encoded = False
+	
+	def encode (self, engine):
+		if self._encoded:
+			return
+		_data = {}
+		for k, v in self._feed.items ():
+			_data [k] = toval (v, engine)
+		self._feed = _data
+		self._encoded = True
+		
+	@property
+	def columns (self):	
+		return ", ".join (self._columns)
+	
+	@property
+	def values (self):		
+		return ", ".join ([self._feed [c] for c in self._columns])
+	
+	@property
+	def pairs (self):		
+		return ", ".join (["{} = {}".format (c, self._feed [c]) for c in self._columns])
+		
+
+def toval (s, engine = "postgresql"):
+	if isinstance (s, datetime.date):
+		return "timestamp '" + s.strftime ("%Y%m%d %H:%M:%S") + "'"
+	if isinstance (s, (float, int, F)):
+		return str (s)
+	if s is None:
+		return "NULL"	
+	return "'" + s.replace ("'", "''") + "'"
+
+def toD (dict, engine):	
+	d = D (**dict)
+	d.encode (engine)
+	return d
+
+def make_orders (order_by, keyword = "ORDER"):
+	if isinstance (order_by, str):
+		order_by = [order_by]
+	orders = []
+	for f in order_by:
+		if f[0] == "-":
+			orders.append (f[1:] + " DESC")
+		else:
+			orders.append (f)	
+	return "{} BY {}".format (keyword, ", ".join (orders))
+	
