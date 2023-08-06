c2c86a2509a27b07b643cee593f99d53d3b45fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:45 +0000

    Add package django-precise-bbcode/1.2.9/django-precise-bbcode-1.2.9.tar.gz

---
+++ packages/django-precise-bbcode/django-precise-bbcode-1.2.9.tar.gz/django-precise-bbcode-1.2.9/precise_bbcode/core/compat.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+
+def with_metaclass(meta, *bases):
+    """Create a base class with a metaclass."""
+    class metaclass(meta):  # noqa
+        __call__ = type.__call__
+        __init__ = type.__init__
+
+        def __new__(cls, name, this_bases, d):
+            if this_bases is None:
+                return type.__new__(cls, name, (), d)
+            return meta(name, bases, d)
+    return metaclass("NewBase", None, {})
