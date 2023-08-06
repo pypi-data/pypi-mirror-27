3bff8e41249bd56d1d1462e4a20d4f02e9fc0e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:55 +0000

    Add package diffimg/0.1.1/diffimg-0.1.1.tar.gz

---
+++ packages/diffimg/diffimg-0.1.1.tar.gz/diffimg-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'diffimg',
+  packages = ['diffimg'], # this must be the same as the name above
+  version = '0.1.1',
+  description = 'Get the % difference in images + generate a diff image Edit',
+  author = 'Nicolas Hahn',
+  author_email = 'nicolas@stonespring.org',
+  url = 'https://github.com/nicolashahn/python-image-diff',
+  download_url = 'https://github.com/nicolashahn/python-image-diff/archive/0.1.1.tar.gz',
+  keywords = ['diff', 'difference', 'image'],
+  classifiers = [],
+)
