f8588624300ef2b6b6bac7571adb793b6b93b257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:55 +0000

    Add package jinja2-git/0.1.0/jinja2-git-0.1.0.tar.gz

---
+++ packages/jinja2-git/jinja2-git-0.1.0.tar.gz/jinja2-git-0.1.0/setup.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import Command, setup
+
+REQUIRED = [
+    'jinja2>=2.10',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is
+# present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+setup(
+    name='jinja2-git',
+    version='0.1.0',
+    description='Jinja2 extension to handle git-specific things',
+    long_description=long_description,
+    author='Nikita Sobolev',
+    author_email='mail@sobolenv.me',
+    url='https://github.com/sobolevn/jinja2-git',
+
+    py_modules=['jinja2_git'],
+
+    install_requires=REQUIRED,
+    include_package_data=True,
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    keywords='jinja2 jinja2-extension jinja2-plugin git',
+
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
