73de5122a7942799b8a7d9cffc0ce6b5d3437148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:48 +0000

    Add package aollio/0.0.4/aollio-0.0.4-py3-none-any.whl

---
+++ packages/aollio/aollio-0.0.4-py3-none-any.whl/aollio/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+__author__ = 'Aollio Hou'
+__email__ = 'aollio@outlook.com'
+
+from fileinput import FileInput
+from fileinput import input
+import crawl as crawl
+import fileinput as fileinput
+from hello import hello
+import fileutil
