d097fc8493a603c329d99209382f3a28dae54b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:28 +0000

    Add package django-account-keeping/0.3.1/django-account-keeping-0.3.1.tar.gz

---
+++ packages/django-account-keeping/django-account-keeping-0.3.1.tar.gz/django-account-keeping-0.3.1/account_keeping/templatetags/account_keeping_tags.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django import template
+from django.contrib.humanize.templatetags.humanize import intcomma
+
+register = template.Library()
+
+
+@register.filter()
+def currency(value, currency=None):
+    try:
+        currency = currency.iso_code
+    except AttributeError:
+        currency = getattr(settings, 'BASE_CURRENCY', 'EUR')
+    cleaned_value = '{0:.2f}'.format(float(value))
+    return u'{0} {1}'.format(currency, intcomma(cleaned_value))
