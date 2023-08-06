581694639b22d40e5106addfc226785acc9b5bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:15 +0000

    Add package hassmart-home-assistant-frontend/20171218.0/hassmart-home-assistant-frontend-20171218.0.tar.gz

---
+++ packages/hassmart-home-assistant-frontend/hassmart-home-assistant-frontend-20171218.0.tar.gz/hassmart-home-assistant-frontend-20171218.0/hass_frontend_es5/__init__.py
@@ -0,0 +1,27 @@
+"""AUTO-GENERATED. DO NOT MODIFY"""
+import os
+
+
+def where():
+    """Return path to the frontend."""
+    return os.path.dirname(__file__)
+
+VERSION = 'f8765b90c7aee99edd7fd22b58d4ad4256aaf3e7'
+CREATED_AT = 1513517856
+FINGERPRINTS = {
+    "config": "70e4ad9a1c2722ef90730ada1168d8a3",
+    "dev-event": "5336e675a747a97441f6a2e61ea5a1fa",
+    "dev-info": "26219225bcd0cb261b67b5765fb74225",
+    "dev-mqtt": "ebf4d71b0871b23655e68e125d9aca05",
+    "dev-service": "49fd1f75d2867ef9f196cb7363da87de",
+    "dev-state": "e79a4877a08a9c9c8a1cc921a8ecf631",
+    "dev-template": "18743488268ed3378c29f664783b3889",
+    "hassio": "2e835444019113d22cef41047c0a980f",
+    "history": "c6d64596879bad7ea0373eecce651a69",
+    "iframe": "185480826c02fec59d9ad045abee15f4",
+    "kiosk": "4d15ef5a5be516d512bb8a5ba879c3b0",
+    "logbook": "05584fd801df5f5755db3399aac22baf",
+    "mailbox": "bdaf8ed7f889813e8d90569a456116bc",
+    "map": "ce41fd7128bac58b3b705c3f7eec7047",
+    "shopping-list": "38ef810e0a73839d7c5101f296e46165"
+}
