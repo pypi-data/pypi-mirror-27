1832e1745455bbdde73ffc6b090740eb2ff1dfb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:03 +0000

    Add package jqueryquerybuilder/0.1.0/jqueryquerybuilder-0.1.0-py2.py3-none-any.whl

---
+++ packages/jqueryquerybuilder/jqueryquerybuilder-0.1.0-py2.py3-none-any.whl/querybuilder/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for jQuery Query Builder."""
+
+__author__ = """Adam Hitchcock"""
+__email__ = 'adam@northisup.com'
+__version__ = '0.1.0'
+
+
+from .filters import *
+from .rules import *
