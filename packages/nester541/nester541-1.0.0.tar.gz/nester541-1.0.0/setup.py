bb3c8be089d36921e04b76ea16ff6f043762d9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:24 +0000

    Add package nester541/1.0.0/nester541-1.0.0.tar.gz

---
+++ packages/nester541/nester541-1.0.0.tar.gz/nester541-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='nester541',
+	version='1.0.0',
+	py_modules=['nester'],
+	author='hust-laowu',
+	author_email='894754681@qq.com',
+	url='https://www.baidu.com',
+	description='a simple printer of nested list',
+	)
