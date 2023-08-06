0b686f31dddc4ec1fa8417e4c025df4d0dc47dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:01 +0000

    Add package beautifullist/1.0.1/beautifullist-1.0.1.tar.gz

---
+++ packages/beautifullist/beautifullist-1.0.1.tar.gz/beautifullist-1.0.1/prettylist.py
@@ -0,0 +1,9 @@
+def pretty_list(l,ident=False,level=0):
+    for each_list in l:
+        if isinstance(each_list,list):
+            pretty_list(each_list,ident,level+1)
+        else:
+            if ident:
+                for i in range(level):
+                    print('\t',end='')
+            print(each_list)
