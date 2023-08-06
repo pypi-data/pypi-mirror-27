0e1fce34bd5286c53afe87cc033eb23d29700b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:25 +0000

    Add package Redberry/0.0.7.9/Redberry-0.0.7.9-py2.py3-none-any.whl

---
+++ packages/Redberry/Redberry-0.0.7.9-py2.py3-none-any.whl/redberry/static/redberry/js/redberry_admin.js
@@ -0,0 +1,34 @@
+var RedberryAdmin = {
+
+    _init_rich_text: function(){
+        $.getScript( "https://cdn.quilljs.com/1.1.8/quill.js", function( data, textStatus, jqxhr ) {
+
+            $('.rich-editable').each(function(){
+                var quill = new Quill("#" + $(this).attr('id'), {
+                    theme: 'snow'
+                })
+            })
+
+            $('.rich-editable').closest('form').submit(function(){
+
+                $('.rich-editable').each(function(){
+                    var textarea_id = $(this).attr('id')
+                    var edited_text = $(this).find('.ql-editor').html()
+
+                    $('textarea[name=' + textarea_id + ']').text( edited_text )
+
+                })
+                return true
+            })
+
+        });
+    },
+
+    init: function(){
+        RedberryAdmin._init_rich_text()
+    }
+}
+
+$(document).ready(function(){
+    RedberryAdmin.init()
+})
