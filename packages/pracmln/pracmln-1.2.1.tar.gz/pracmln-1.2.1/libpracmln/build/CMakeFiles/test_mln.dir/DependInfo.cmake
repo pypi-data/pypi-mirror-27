ec28a2a9e49a8925186bfa95675ab9f6111005ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:46 +0000

    Add package pracmln/1.2.1/pracmln-1.2.1.tar.gz

---
+++ packages/pracmln/pracmln-1.2.1.tar.gz/pracmln-1.2.1/libpracmln/build/CMakeFiles/test_mln.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nyga/work/python/3.5/lib/python3.5/site-packages/pracmln-1.2.0-py3.5.egg/libpracmln/src/test_mln.cpp" "/home/nyga/work/code/pracmln/libpracmln/build/CMakeFiles/test_mln.dir/src/test_mln.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "PROJECT_SRC_DIR=\"/home/nyga/work/python/3.5/lib/python3.5/site-packages/pracmln-1.2.0-py3.5.egg/libpracmln\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/nyga/work/python/3.5/lib/python3.5/site-packages/pracmln-1.2.0-py3.5.egg/libpracmln/include"
+  "/home/nyga/work/python/3.5/include/python3.5m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nyga/work/code/pracmln/libpracmln/build/CMakeFiles/pracmln.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ packages/pracmln/pracmln-1.2.1.tar.gz/pracmln-1.2.1/libpracmln/build/CMakeFiles/test_mln.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_mln.dir/src/test_mln.cpp.o"
+  "test_mln.pdb"
+  "test_mln"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_mln.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
