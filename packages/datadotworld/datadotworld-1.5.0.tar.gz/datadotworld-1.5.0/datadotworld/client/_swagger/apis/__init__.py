7b58d489c7e16425f834d8e7ada190779c9ba39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:33 +0000

    Add package datadotworld/1.5.0/datadotworld-1.5.0.tar.gz

---
+++ packages/datadotworld/datadotworld-1.5.0.tar.gz/datadotworld-1.5.0/datadotworld/client/_swagger/apis/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .datasets_api import DatasetsApi
+from .download_api import DownloadApi
+from .sparql_api import SparqlApi
+from .sql_api import SqlApi
+from .streams_api import StreamsApi
+from .uploads_api import UploadsApi
+from .user_api import UserApi
