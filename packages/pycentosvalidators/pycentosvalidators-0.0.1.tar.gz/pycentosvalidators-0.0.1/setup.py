b77774730a34becd1439b2359cf79adafddc6a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:53 +0000

    Add package pycentosvalidators/0.0.1/pycentosvalidators-0.0.1.tar.gz

---
+++ packages/pycentosvalidators/pycentosvalidators-0.0.1.tar.gz/pycentosvalidators-0.0.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""setup.py."""
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = [
+    'requests>=2.0',
+    'httplib2>=0.9'
+]
+VERSION = '0.0.1'
+
+setup(
+    name='pycentosvalidators',
+    version=VERSION,
+    description='Common pycentosvalidators component.',
+    author='feiger',
+    author_email='351275698@qq.com',
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT License',
+    url='https://github.com/istommao/pyvalidators',
+    keywords='pycentosvalidators is a common validators component,only testing!'
+)
