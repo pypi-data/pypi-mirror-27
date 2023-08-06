58e9acee5296c5478c41edd63484a82b28f76bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package xshg/1.0.0/xshg-1.0.0.tar.gz

---
+++ packages/xshg/xshg-1.0.0.tar.gz/xshg-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'xshg',
+    version         = '1.0.0',
+    py_modules      = ['xshg'],
+    author          = 'xshg',
+    author_email    = 'xshg@hotmail.com',
+    url             = 'http://www.luckya2z.com',
+    description     = 'samples from xshg',
+    )
---
+++ packages/xshg/xshg-1.0.0.tar.gz/xshg-1.0.0/xshg.py
@@ -0,0 +1,10 @@
+def helloworld():
+    '''
+    This is a sample py file.
+    print helloworld on screen.
+    '''
+    print("Hello, world.")
+
+if __name__ == "__main__":
+    helloworld()
+
