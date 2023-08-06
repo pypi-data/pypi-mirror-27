7f01766efd8587bf55472495f0951df00edaa463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:16 +0000

    Add package json2df/0.1.3.2/json2df-0.1.3.2.tar.gz

---
+++ packages/json2df/json2df-0.1.3.2.tar.gz/json2df-0.1.3.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name = "json2df",
+    version = "0.1.3.2",
+    description = "convert json data to Pandas DataFrame",
+    long_description=open('./README.rst').read(),
+    author = "Shichao(Richard) Ji",
+    author_email = "jshichao@vt.edu",
+    url = "https://github.com/shichaoji/json2df",
+    download_url = "https://github.com/shichaoji/json2df/archive/0.1.1.tar.gz",
+    license = 'MIT',
+    keywords = ['data','json','dataframe','python'],
+    classifiers = [
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 2', 
+	'Programming Language :: Python :: 3',
+	],
+    packages = find_packages(),
+    install_requires=[
+          'pandas',
+      ]
+)
+
