eabb1416478f877ce33be35c0ef2c800fee89ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:01 +0000

    Add package Dijkstar/2.4.0/Dijkstar-2.4.0.tar.gz

---
+++ packages/Dijkstar/Dijkstar-2.4.0.tar.gz/Dijkstar-2.4.0/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+
+with open('README') as fp:
+    long_description = fp.read()
+
+
+setup(
+    name='Dijkstar',
+    version='2.4.0',
+    description='Dijkstra/A*',
+    long_description=long_description,
+    license='MIT',
+    author='Wyatt Baldwin',
+    author_email='self@wyattbaldwin.com',
+    keywords='Dijkstra A* algorithms',
+    url='https://github.com/wylee/Dijkstar',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=['dijkstar']
+)
