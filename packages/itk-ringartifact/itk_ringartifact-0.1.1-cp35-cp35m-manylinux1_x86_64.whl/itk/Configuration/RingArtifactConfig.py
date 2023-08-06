2860f37456a3948140b10906a4f6faeb2d4d440d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:32 +0000

    Add package itk-ringartifact/0.1.1/itk_ringartifact-0.1.1-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/itk-ringartifact/itk_ringartifact-0.1.1-cp35-cp35m-manylinux1_x86_64.whl/itk/Configuration/RingArtifactConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKFFT', 'ITKCommon', )
+templates = (
+  ('FourierStripeArtifactImageFilter', 'itk::FourierStripeArtifactImageFilter', 'itkFourierStripeArtifactImageFilterIF2', True, 'itk::Image< float,2 >'),
+  ('FourierStripeArtifactImageFilter', 'itk::FourierStripeArtifactImageFilter', 'itkFourierStripeArtifactImageFilterIF3', True, 'itk::Image< float,3 >'),
+)
