ae2248301b57233e6be7a39393356d035236afa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:58 +0000

    Add package SimpleFilter/0.11/SimpleFilter-0.11.tar.gz

---
+++ packages/SimpleFilter/SimpleFilter-0.11.tar.gz/SimpleFilter-0.11/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='SimpleFilter',
+      version='0.11',
+      description='SimpleFilter is a module that provides the tools necessary to build a convolutional classification network.',
+      url='https://github.com/dibsonthis/SimpleFilter',
+      author='Adib Attie',
+      author_email='adib.attie33@gmail.com',
+      license='MIT',
+      packages=['SimpleFilter'],
+      install_requires=[
+          'matplotlib', 'Pillow'
+      ],
+      zip_safe=False)
