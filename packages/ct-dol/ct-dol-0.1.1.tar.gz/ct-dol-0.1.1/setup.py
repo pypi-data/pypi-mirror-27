a41f217f52b6660991c525dd511dd55211c87f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:37 +0000

    Add package ct-dol/0.1.1/ct-dol-0.1.1.tar.gz

---
+++ packages/ct-dol/ct-dol-0.1.1.tar.gz/ct-dol-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="ct-dol",
+    version="0.1.1",
+    description="A tool for pulling down ct unemployment as a dataframe",
+    url="https://github.com/jakekara/ct_unemployment",
+    author="Jake Kara",
+    author_email="jake@jakekara.com",
+    license="GPL-3",
+    install_requires=["pandas"],
+    packages=["ct_unemployment"],
+    
+    
+)
+    
