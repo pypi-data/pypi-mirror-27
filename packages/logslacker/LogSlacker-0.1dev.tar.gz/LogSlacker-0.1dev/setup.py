44ec1da18fc4750d9c5483dc70a20e69d658f029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:41 +0000

    Add package logslacker/0.1dev/LogSlacker-0.1dev.tar.gz

---
+++ packages/logslacker/LogSlacker-0.1dev.tar.gz/LogSlacker-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='LogSlacker',
+    version='0.1dev',
+    packages=['logslacker',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
