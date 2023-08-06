3506aca1db20f974645597704ca9f7ff5d151fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:28 +0000

    Add package nestera/1.0.0/nestera-1.0.0.tar.gz

---
+++ packages/nestera/nestera-1.0.0.tar.gz/nestera-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	   name = 'nestera',
+	   version = '1.0.0',
+	   py_modules = ['nestera'],
+	   author = 'Dorothywen96',
+	   author_email = 'name1120@163.com',
+	   url = 'https://github.com/Dorothywen96/dorothy-house.git',
+	   description = 'A learner of data',
+	   )
