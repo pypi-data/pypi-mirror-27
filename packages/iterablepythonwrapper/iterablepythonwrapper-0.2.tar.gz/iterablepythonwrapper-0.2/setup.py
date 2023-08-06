2658337eac012a1c4ef0703d858139d78997717a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:01 +0000

    Add package iterablepythonwrapper/0.2/iterablepythonwrapper-0.2.tar.gz

---
+++ packages/iterablepythonwrapper/iterablepythonwrapper-0.2.tar.gz/iterablepythonwrapper-0.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+
+long_description = '''
+Iterable Python is a Python client that wraps the Iterable Api.
+Originally developed by Carter Hickingbotham.
+'''
+
+setup(
+	name='iterablepythonwrapper',
+	packages=['iterablepythonwrapper'],
+	url='https://github.com/carter-j-h/iterable-python-wrapper',
+	author='Carter Hickingbotham',
+	author_email='carterhickingbotham@gmail.com',
+	license= 'MIT',
+	version='0.2',	
+	install_requires=['peppercorn'],
+	keywords = ['Iterable', 'API', 'Wrapper', 'Client', 'Python'],
+    description='Python Client for the Iterable Api',
+    long_description=long_description
+
+	)
