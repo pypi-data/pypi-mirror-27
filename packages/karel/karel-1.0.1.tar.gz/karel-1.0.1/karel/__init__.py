fc1933f44bac190e445a2362cebc6c0cb7d22cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:03 +0000

    Add package karel/1.0.1/karel-1.0.1.tar.gz

---
+++ packages/karel/karel-1.0.1.tar.gz/karel-1.0.1/karel/__init__.py
@@ -0,0 +1,10 @@
+from .parser import KarelParser
+from .utils import str2bool, makedirs, pprint, beautify
+
+__copyright__ = 'Copyright 2015 - {} by Taehoon Kim'.format(datetime.now().year)
+__version__ = '1.0.1'
+__license__ = 'BSD'
+__author__ = 'Taehoon Kim'
+__email__ = 'carpedm20@gmail.com'
+__source__ = 'https://github.com/carpedm20/karel'
+__description__ = 'Karel dataset for program synthesis and program induction'
