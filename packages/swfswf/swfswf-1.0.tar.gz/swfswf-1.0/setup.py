79d99de1e9d48485e39197aa15df7d93092df708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:45 +0000

    Add package swfswf/1.0/swfswf-1.0.tar.gz

---
+++ packages/swfswf/swfswf-1.0.tar.gz/swfswf-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup  # 从 Python 发布工具导入 setup 函数
+setup(name='swfswf',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='shenwenfang',
+      author_email='1978626782@qq.com',
+      url='https://swenfang.github.io/',
+     )
