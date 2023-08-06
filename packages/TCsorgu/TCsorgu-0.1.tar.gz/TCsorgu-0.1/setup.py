be3ccab5265103ecd2ce18a712ff308542b3abe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:08 +0000

    Add package TCsorgu/0.1/TCsorgu-0.1.tar.gz

---
+++ packages/TCsorgu/TCsorgu-0.1.tar.gz/TCsorgu-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='TCsorgu',
+      version='0.1',
+      description='TC Identity Number Check',
+      url='http://github.com/dorukgezici/TCsorgu',
+      author='Doruk Gezici',
+      author_email='dorukgezici@gmail.com',
+      license='MIT',
+      packages=['TCsorgu'],
+      scripts=['bin/TCsorgu'],
+      zip_safe=False)
