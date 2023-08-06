afff65b3b04c76d55b8bd66b5b71d8bd3cfd7aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:56 +0000

    Add package inoon-lora-packet/0.5.0/inoon_lora_packet-0.5.0.tar.gz

---
+++ packages/inoon-lora-packet/inoon_lora_packet-0.5.0.tar.gz/inoon_lora_packet-0.5.0/inoon_lora_packet/parser.py
@@ -0,0 +1,29 @@
+from inoon_lora_packet.packet import (InvalidPacketError,
+                                      NotSupportedPacketError)
+from inoon_lora_packet.v2.parser import Ver2Parser
+from inoon_lora_packet.v3.parser import Ver3Parser
+
+
+class ParserFactory():
+    @classmethod
+    def get_parser(cls, version):
+
+        if version is 1:
+            return Ver2Parser()
+        elif version is 2:
+            return Ver2Parser()
+        elif version is 3:
+            return Ver3Parser()
+        else:
+            raise NotSupportedPacketError
+
+
+class PacketParser():
+    @classmethod
+    def parse(cls, raw_packet):
+        try:
+            version = int(raw_packet[:2], 16)
+            parser = ParserFactory.get_parser(version=version)
+            return parser.parse(raw_packet=raw_packet)
+        except ValueError:
+            raise InvalidPacketError
---
+++ packages/inoon-lora-packet/inoon_lora_packet-0.5.0.tar.gz/inoon_lora_packet-0.5.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+setup_requires = [
+]
+
+install_requires = [
+]
+
+dependency_links = [
+]
+
+setup(
+    name='inoon_lora_packet',
+    version='0.5.0',
+    description='LoRa packet parser for Ino-on, Inc.',
+    author='Joonkyo Kim',
+    author_email='jkkim@ino-on.com',
+    packages=["inoon_lora_packet"],
+    include_package_data=True,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    dependency_links=dependency_links,
+    # scripts=['manage.py'],
+    entry_points={
+        'console_scripts': [
+        ],
+        "egg_info.writers": [
+            "foo_bar.txt = setuptools.command.egg_info:write_arg",
+        ],
+    },
+)
