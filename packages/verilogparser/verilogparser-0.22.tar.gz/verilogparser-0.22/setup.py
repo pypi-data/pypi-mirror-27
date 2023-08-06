d1ded49abe3ffa74d260fb38eaca457a6b91d165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:23 +0000

    Add package verilogparser/0.22/verilogparser-0.22.tar.gz

---
+++ packages/verilogparser/verilogparser-0.22.tar.gz/verilogparser-0.22/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+  name = 'verilogparser',
+  packages = ['verilogparser'],
+  version = '0.22',
+  description = 'Python Verilog File Parser',
+  long_description='''Python Verilog File Parser''',
+  author = 'Sepand Haghighi',
+  author_email = 'sepand@qpage.ir',
+  url = 'https://github.com/sepandhaghighi/verilogparser',
+  download_url = 'https://github.com/sepandhaghighi/verilogparser/tarball/v0.22',
+  keywords = ["Verilog","python","HDL","ISCAS","script","parser","simulation"],
+  install_requires=[
+      'art',
+	  'codecov',
+      ],
+  classifiers = [],
+  license='MIT',
+)
