d22ecb61755105962de437477e2bca3ac361bcf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:28 +0000

    Add package annotypes/0.3/annotypes-0.3.tar.gz

---
+++ packages/annotypes/annotypes-0.3.tar.gz/annotypes-0.3/annotypes/__init__.py
@@ -0,0 +1,7 @@
+from ._anno import Anno, NO_DEFAULT
+from ._array import Array, to_array
+from ._calltypes import WithCallTypes, add_call_types
+from ._typing import (
+    TYPE_CHECKING, TypeVar, Sequence, Union, Optional, Generic,
+    overload, Mapping, Any
+)
