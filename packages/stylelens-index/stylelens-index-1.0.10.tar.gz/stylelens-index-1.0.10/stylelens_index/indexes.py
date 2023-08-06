8aed27119804b0288b14ce3cf7b2471b30ffce11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:04 +0000

    Add package stylelens-index/1.0.10/stylelens-index-1.0.10.tar.gz

---
+++ packages/stylelens-index/stylelens-index-1.0.10.tar.gz/stylelens-index-1.0.10/stylelens_index/indexes.py
@@ -0,0 +1,52 @@
+from stylelens_index.database import DataBase
+
+class Indexes(DataBase):
+  def __init__(self):
+    super().__init__()
+    self.images = self.db.images
+    self.objects = self.db.objects
+
+  def add_image(self, image):
+    id = None
+    try:
+      query = {"host_code": image['host_code'],
+               "product_no": image['product_no'],
+               "version_id": image['version_id']}
+      r = self.images.update_one(query,
+                                 {"$set":image},
+                                 upsert=True)
+    except Exception as e:
+      print(e)
+
+    if 'upserted' in r.raw_result:
+      id = str(r.raw_result['upserted'])
+
+    return id
+
+  def add_object(self, object):
+    id = None
+    try:
+      query = {"name": object['name'],
+               "version_id": object['version_id']}
+      r = self.objects.update_one(query,
+                                  {"$set":object},
+                                  upsert=True)
+    except Exception as e:
+      print(e)
+
+    if 'upserted' in r.raw_result:
+      id = str(r.raw_result['upserted'])
+
+    return id
+
+
+  # def get_images_by_object(self, object_id, offset=0, limit=100):
+  #   query = {'_id'}
+  #
+  #   try:
+  #     r = self.objects.find({query}).skip(offset).limit(limit)
+  #
+  #   except Exception as e:
+  #     print(e)
+  #
+  #   return list(r)
