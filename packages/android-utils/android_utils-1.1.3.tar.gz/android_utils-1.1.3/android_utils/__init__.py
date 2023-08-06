24ccf45e977486cc809a16fa28a0636ab619a955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:35 +0000

    Add package android-utils/1.1.3/android_utils-1.1.3.tar.gz

---
+++ packages/android-utils/android_utils-1.1.3.tar.gz/android_utils-1.1.3/android_utils/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = '1.1.3'
+
+from .apkinfo import ApkInfo
+from .CurlDownloadHelper import download
+
+def getApkInfo(apkPath):
+    return ApkInfo(apkPath)
+def download(url, destDir, targetFileName):
+	return CurlDownloadHelper.download(url, destDir, targetFileName())
