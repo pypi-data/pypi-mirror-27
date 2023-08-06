00de1685801c5f703ed017f51b60729945fa7a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:32 +0000

    Add package is-prime/1.0.1/is_prime-1.0.1.tar.gz

---
+++ packages/is-prime/is_prime-1.0.1.tar.gz/is_prime-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'is_prime',
+	version = '1.0.1',
+	py_modules = ['is_prime'],
+	author = 'Muhammad Tehami',
+	author_email = 'mohammadtehami@yahoo.com',
+	description = 'Module to find whether a given number is prime number and its nearest prime. A new method game() included in version 1.0.1.'
+)
