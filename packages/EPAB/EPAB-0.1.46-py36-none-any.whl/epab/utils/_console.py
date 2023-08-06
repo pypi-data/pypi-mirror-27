c1d34a6e888048277722023c6b04d2fb670c8696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:47 +0000

    Add package EPAB/0.1.46/EPAB-0.1.46-py36-none-any.whl

---
+++ packages/EPAB/EPAB-0.1.46-py36-none-any.whl/epab/utils/_console.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+"""
+Manages output functions
+"""
+
+import click
+
+
+def _sanitize(input_: str) -> str:
+    input_ = f'EPAB: {input_}'
+    return input_.encode('ascii', 'ignore').decode()
+
+
+def _info(txt: str, **args):
+    txt = _sanitize(txt)
+    click.secho(txt, fg='green', **args)
+
+
+def _error(txt: str, **args):
+    txt = _sanitize(txt)
+    click.secho(txt, fg='red', err=True, **args)
+
+
+def _cmd(txt: str, **args):
+    txt = _sanitize(txt)
+    click.secho(txt, fg='magenta', nl=False, **args)
+
+
+def _cmd_out(txt: str, **args):
+    txt = _sanitize(txt).replace('EPAB: ', '')
+    click.secho(txt, fg='magenta', **args)
+
+
+def _out(txt: str, **args):
+    txt = _sanitize(txt)
+    click.secho(txt, fg='cyan', **args)
