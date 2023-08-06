21b1b1772ceaf570a77deb12ecb0b83e024d3563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:55 +0000

    Add package nesterMax/1.0.0/nesterMax-1.0.0.tar.gz

---
+++ packages/nesterMax/nesterMax-1.0.0.tar.gz/nesterMax-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nesterMax',
+	version 	= '1.0.0',
+	py_modules 	= ['nester'],
+	author		= 'Max',
+	author_email	= 'bart30508@gmail.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'a simple printer of nested lists',
+     )
