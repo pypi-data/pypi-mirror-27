56e648a7bfb215ec1bb0592ef113c3090e820f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:27 +0000

    Add package djangoproton/0.11/djangoproton-0.11.tar.gz

---
+++ packages/djangoproton/djangoproton-0.11.tar.gz/djangoproton-0.11/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='djangoproton',
+  version='0.11',
+  description='Django templating system to get you from zero to production fast',
+  author_email='garfonzo@gmail.com',
+  author='garfonzo',
+  url='https://github.com/garfonzo/djangoproton',
+  python_requires='>=3',
+  scripts=['proton']
+)
