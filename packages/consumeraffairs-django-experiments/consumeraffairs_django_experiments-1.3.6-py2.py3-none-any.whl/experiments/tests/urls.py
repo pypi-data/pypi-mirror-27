9ae5f2fbe88ea7902854df42b05d7debbeb5f9e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:37 +0000

    Add package consumeraffairs-django-experiments/1.3.6/consumeraffairs_django_experiments-1.3.6-py2.py3-none-any.whl

---
+++ packages/consumeraffairs-django-experiments/consumeraffairs_django_experiments-1.3.6-py2.py3-none-any.whl/experiments/tests/urls.py
@@ -0,0 +1,8 @@
+from experiments.urls import urlpatterns
+from django.conf.urls import include, url
+from django.contrib import admin
+
+
+urlpatterns += [
+    url(r'^admin/', include(admin.site.urls)),
+]
