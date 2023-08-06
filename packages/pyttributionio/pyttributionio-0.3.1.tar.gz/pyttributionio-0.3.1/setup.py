bddc9429b727baf4887ecce21f654ef3fd9467ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:51 +0000

    Add package pyttributionio/0.3.1/pyttributionio-0.3.1.tar.gz

---
+++ packages/pyttributionio/pyttributionio-0.3.1.tar.gz/pyttributionio-0.3.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='pyttributionio',
+    packages=['pyttributionio'],
+    version='0.3.1',
+    description='A Python wrapper around the Attribution.io API',
+    author='Jakob Löhnertz',
+    author_email='help@jakob.codes',
+    license='MIT',
+    url='https://github.com/loehnertz/Pyttribution.io',
+    download_url='https://github.com/loehnertz/Pyttribution.io/archive/0.3.1.tar.gz',
+    keywords=['attribution.io', 'api', 'api-wrapper'],
+    classifiers=[],
+)
