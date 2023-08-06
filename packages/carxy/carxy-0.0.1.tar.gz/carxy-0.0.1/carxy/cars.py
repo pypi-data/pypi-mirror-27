700ad2380ca435514fb71e4e86d56a0f9cb0ac35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:03 +0000

    Add package carxy/0.0.1/carxy-0.0.1.tar.gz

---
+++ packages/carxy/carxy-0.0.1.tar.gz/carxy-0.0.1/carxy/cars.py
@@ -0,0 +1,25 @@
+import urllib
+import urllib.request
+import urllib.parse
+import json
+APPKEY='631c5a5b9992bd74'
+APPSECRET='eS0iNTaMdI5mgRew8JkrVqUB7mG1MytS'
+API_URL='http://api.jisuapi.com/car/brand'
+version="1.0"
+
+def carslist():
+    data = {}
+    data['appkey'] = APPKEY
+    url_values = urllib.parse.urlencode(data)
+    url = API_URL + '?' + url_values
+    result = urllib.request.urlopen(url)
+    jsonarr = json.loads(result.read())
+    if jsonarr['status'] != '0':
+        print(jsonarr['msg'])
+        exit()
+    result = jsonarr['result']
+    cars = []
+    for val in result:
+        cars.append(val['name'])
+    return cars
+
