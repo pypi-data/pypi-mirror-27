9142a72db88bf355e9ed0c58ad902ef5880c24c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:42 +0000

    Add package online-monitor/0.3.1/online_monitor-0.3.1.tar.gz

---
+++ packages/online-monitor/online_monitor-0.3.1.tar.gz/online_monitor-0.3.1/online_monitor/testing/test_start_scripts.py
@@ -0,0 +1,148 @@
+''' Script to check the online monitor.
+'''
+
+import sys
+import unittest
+import yaml
+import subprocess
+import time
+import os
+import psutil
+from PyQt5.QtWidgets import QApplication
+
+import online_monitor
+from online_monitor.utils import settings
+
+
+# creates a yaml config
+def create_config_yaml():
+    conf = {}
+    # Add producer
+    devices = {}
+    devices['DAQ0'] = {'backend': 'tcp://127.0.0.1:6500',
+                       'kind': 'example_producer_sim',
+                       'delay': 1}
+    devices['DAQ1'] = {'backend': 'tcp://127.0.0.1:6501',
+                       'kind': 'example_producer_sim',
+                       'delay': 1}
+    conf['producer_sim'] = devices
+    # Add converter
+    devices = {}
+    devices['DUT0'] = {
+        'kind': 'example_converter',
+        'frontend': 'tcp://127.0.0.1:6500',
+        'backend': 'tcp://127.0.0.1:6600',
+        'max_cpu_load': None,
+        'threshold': 8
+    }
+    devices['DUT1'] = {
+        'kind': 'forwarder',
+        'frontend': 'tcp://127.0.0.1:6600',
+        'backend': 'tcp://127.0.0.1:6601',
+        'max_cpu_load': None
+    }
+    conf['converter'] = devices
+    # Add receiver
+    devices = {}
+    devices['DUT0'] = {
+        'kind': 'example_receiver',
+        'frontend': 'tcp://127.0.0.1:6600'
+    }
+    devices['DUT1'] = {
+        'kind': 'example_receiver',
+        'frontend': 'tcp://127.0.0.1:6600'
+    }
+    conf['receiver'] = devices
+    return yaml.dump(conf, default_flow_style=False)
+
+
+# kill process by id, including subprocesses; works for linux and windows
+def kill(proc):
+    process = psutil.Process(proc.pid)
+    for child_proc in process.children(recursive=True):
+        child_proc.kill()
+    process.kill()
+
+
+def run_script_in_shell(script, arguments, command=None):
+    if os.name == 'nt':
+        creationflags = subprocess.CREATE_NEW_PROCESS_GROUP
+    else:
+        creationflags = 0
+    return subprocess.Popen("%s %s %s" % ('python' if not command else command,
+                                          script, arguments), shell=True,
+                            creationflags=creationflags)
+
+
+class TestStartScripts(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Set the config file path to the test folder, otherwise they are
+        # created where nosetests are called
+        cls.config_path = os.path.join(os.path.dirname(__file__),
+                                       'tmp_cfg_2.yml')
+        # Get the absoulte path of the online_monitor installation
+        package_path = os.path.dirname(online_monitor.__file__)
+
+        # Add examples folder to entity search paths
+        settings.add_producer_sim_path(os.path.join(package_path,
+                                                    'examples',
+                                                    'producer_sim'))
+        settings.add_converter_path(os.path.join(package_path,
+                                                 'examples',
+                                                 'converter'))
+        settings.add_receiver_path(os.path.join(package_path,
+                                                'examples',
+                                                'receiver'))
+
+        with open(cls.config_path, 'w') as outfile:
+            config_file = create_config_yaml()
+            outfile.write(config_file)
+        # linux CI travis runs headless, thus virtual x server is needed for
+        # gui testing
+        if os.getenv('TRAVIS', False):
+            from xvfbwrapper import Xvfb
+            cls.vdisplay = Xvfb()
+            cls.vdisplay.start()
+
+    @classmethod
+    def tearDownClass(cls):  # remove created files
+        os.remove(cls.config_path)
+        time.sleep(1)
+
+    def test_start_converter(self):
+        converter_process = run_script_in_shell(
+            '', self.config_path, 'start_converter')
+        time.sleep(0.5)
+        kill(converter_process)
+        time.sleep(0.5)
+        self.assertNotEqual(converter_process.poll(), None)
+
+    def test_start_producer_sim(self):
+        producer_sim_process = run_script_in_shell(
+            '', self.config_path, 'start_producer_sim')
+        time.sleep(0.5)
+        kill(producer_sim_process)
+        time.sleep(0.5)
+        self.assertNotEqual(producer_sim_process.poll(), None)
+
+    def test_start_online_monitor(self):
+        online_monitor_process = run_script_in_shell(
+            '', self.config_path, 'start_online_monitor')
+        time.sleep(1)
+        kill(online_monitor_process)
+        time.sleep(1)
+        self.assertNotEqual(online_monitor_process.poll(), None)
+
+    def test_online_monitor(self):
+        online_monitor_process = run_script_in_shell(
+            '', self.config_path, 'online_monitor')
+        time.sleep(0.5)
+        kill(online_monitor_process)
+        time.sleep(0.5)
+        self.assertNotEqual(online_monitor_process.poll(), None)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestStartScripts)
+    unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ packages/online-monitor/online_monitor-0.3.1.tar.gz/online_monitor-0.3.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+version = '0.3.1'
+author = 'David-Leon Pohl'
+author_email = 'pohl@physik.uni-bonn.de'
+
+# Requirements for core functionality from requirements.txt
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name='online_monitor',
+    version=version,
+    description='A generic online monitor showing real-time plots from '
+                'multiple independent data acquisition systems.',
+    url='https://github.com/SiLab-Bonn/online_monitor',
+    license='MIT License',
+    long_description='',
+    author=author,
+    maintainer=author,
+    author_email=author_email,
+    maintainer_email=author_email,
+    install_requires=install_requires,
+    packages=find_packages(),
+    # Accept all data files and directories matched by MANIFEST.in or found in
+    # source control
+    include_package_data=True,
+    keywords=['online monitor', 'real time', 'plots'],
+    entry_points={
+        'console_scripts': [
+            # starts the online monitor application; blocking until app exit
+            'online_monitor = online_monitor.OnlineMonitor:main',
+            # starts the converters; blocking until CRTL-C
+            'start_converter = online_monitor.start_converter:main',
+            # starts the producer simulatiion; blocking until CRTL-C
+            'start_producer_sim = online_monitor.start_producer_sim:main',
+            # starts the online monitor application + converters + producer
+            # simulation defined on the configuration.yaml
+            'start_online_monitor = online_monitor.start_online_monitor:main',
+        ]
+    },
+    platforms='any'
+)
