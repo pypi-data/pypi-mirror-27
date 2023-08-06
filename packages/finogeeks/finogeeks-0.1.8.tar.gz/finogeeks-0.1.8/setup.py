a48404b897f766504e8a8e6eb2c3951564a4d3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:28 +0000

    Add package finogeeks/0.1.8/finogeeks-0.1.8.tar.gz

---
+++ packages/finogeeks/finogeeks-0.1.8.tar.gz/finogeeks-0.1.8/setup.py
@@ -0,0 +1,13 @@
+"""
+author: created by zhao.guangyao on 11/30/17.
+desc: 
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='finogeeks',
+    version='0.1.8',
+    description='Private purpose package for finogeeks',
+    packages=find_packages(include=['finogeeks']),
+    install_requires=['pymongo', 'kafka'],
+)
