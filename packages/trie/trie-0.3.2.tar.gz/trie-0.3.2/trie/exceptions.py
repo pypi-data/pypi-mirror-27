b62ddac5270526bba48797702cc9969c73d62338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:15 +0000

    Add package trie/0.3.2/trie-0.3.2.tar.gz

---
+++ packages/trie/trie-0.3.2.tar.gz/trie-0.3.2/trie/exceptions.py
@@ -0,0 +1,14 @@
+class InvalidNibbles(Exception):
+    pass
+
+
+class InvalidNode(Exception):
+    pass
+
+
+class ValidationError(Exception):
+    pass
+
+
+class BadTrieProof(Exception):
+    pass
