298599029435f4e251c12aaebc3002a1a20e7936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:25 +0000

    Add package metaparticle_pkg/0.2.0/metaparticle_pkg-0.2.0.tar.gz

---
+++ packages/metaparticle_pkg/metaparticle_pkg-0.2.0.tar.gz/metaparticle_pkg-0.2.0/metaparticle_pkg/metaparticle_runner.py
@@ -0,0 +1,46 @@
+import json
+import os
+import subprocess
+
+class MetaparticleRunner:
+    def cancel(name):
+        subprocess.call(['mp-compiler', '-f', '.metaparticle/spec.json', '--delete'])
+
+    def logs(name):
+        subprocess.call(['mp-compiler', '-f', '.metaparticle/spec.json', '--deploy=false', '--attach=true'])
+
+    def ports(portArray):
+        result = []
+        for port in portArray:
+            result.append({
+                'number': port,
+                'protocol': 'TCP'
+            })
+        return result
+
+    def run(img, name, options):
+        svc =  {
+            "name": name,
+            "guid": 1234567, 
+            "services": [ 
+                {
+                    "name": name,
+                    "replicas": options.get('replicas', None),
+                    "shardSpec": options.get('shardSpec', None),
+                    "containers": [
+                        { "image": img }
+                    ],
+                    "ports": ports(options.get('ports', []))
+                }
+            ],
+            "serve": {
+                "name": name,
+            }
+        }
+        if not os.path.exists('.metaparticle'):
+            os.makedirs('.metaparticle')
+
+        with open('.metaparticle/spec.json', 'w') as out:
+            json.dump(svc, out)
+    
+        subprocess.call(['mp-compiler', '-f', '.metaparticle/spec.json'])
---
+++ packages/metaparticle_pkg/metaparticle_pkg-0.2.0.tar.gz/metaparticle_pkg-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name = 'metaparticle_pkg',
+  packages = ['metaparticle_pkg'],
+  version = '0.2.0',
+  description = 'Metaparticle library for Python',
+  author = 'Metaparticle Authors',
+  author_email = 'metaparticle@metaparticle.io',
+  url = 'https://github.com/metaparticle-io/package',
+  keywords = ['metaparticle', 'containers', 'docker'],
+  classifiers = [],
+)
