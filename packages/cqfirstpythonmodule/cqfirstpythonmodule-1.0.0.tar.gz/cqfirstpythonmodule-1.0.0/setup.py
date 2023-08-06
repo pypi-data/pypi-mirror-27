8c341429847ff777765f3ffe8909ee186a2c3f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:46 +0000

    Add package cqfirstpythonmodule/1.0.0/cqfirstpythonmodule-1.0.0.tar.gz

---
+++ packages/cqfirstpythonmodule/cqfirstpythonmodule-1.0.0.tar.gz/cqfirstpythonmodule-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = "cqfirstpythonmodule",
+        version = "1.0.0",
+        py_modules = ["cqfirstpythonmodule"],
+        author = "ChenQiang",
+        author_email = "1209843765@qq.com",
+        url = "http://www.headfirstlabs.com",
+        description = "A simple printer of nested lists",
+    )
