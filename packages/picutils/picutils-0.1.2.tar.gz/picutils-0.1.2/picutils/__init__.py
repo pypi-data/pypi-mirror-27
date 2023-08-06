2a02473c6d96bd172eff2c5e8ff35cd352021972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:25 +0000

    Add package picutils/0.1.2/picutils-0.1.2.tar.gz

---
+++ packages/picutils/picutils-0.1.2.tar.gz/picutils-0.1.2/picutils/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+from .base import processPics
+
+__version__ = '0.1'
+__license__ = 'MIT'
+
+__all__ = ['processPics']
+
+
+if __name__=='__main__':
+	print('run processPics()')
+	processPics()
