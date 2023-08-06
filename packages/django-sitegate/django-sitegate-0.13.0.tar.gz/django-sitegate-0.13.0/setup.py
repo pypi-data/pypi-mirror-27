1b7825e34a3b135498c87da6174a33ba4433dcf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:47 +0000

    Add package django-sitegate/0.13.0/django-sitegate-0.13.0.tar.gz

---
+++ packages/django-sitegate/django-sitegate-0.13.0.tar.gz/django-sitegate-0.13.0/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup
+from sitegate import VERSION
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='django-sitegate',
+    version='.'.join(map(str, VERSION)),
+    url='http://github.com/idlesign/django-sitegate',
+
+    description='Reusable application for Django to ease sign up & sign in processes',
+    long_description=readme,
+    license='BSD 3-Clause License',
+
+    author='Igor `idle sign` Starikov',
+    author_email='idlesign@yandex.ru',
+
+    packages=['sitegate'],
+    install_requires=['django-etc'],
+    include_package_data=True,
+    zip_safe=False,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
