dd4f5f6140c20e6518e68297db75c22b30a834c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:52 +0000

    Add package wx-sdk/1.0.2/wx_sdk-1.0.2.tar.gz

---
+++ packages/wx-sdk/wx_sdk-1.0.2.tar.gz/wx_sdk-1.0.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+
+try :
+    from setuptools import setup
+except :
+    from distutils.core import setup
+setup(
+    name='wx_sdk',
+    version='1.0.2',
+    description='wangxiang Python SDK',
+    long_description=open('README.txt').read(),
+    author='wanxiang',
+    author_email='wanxiang@jd.com',
+    maintainer='Alex',
+    maintainer_email='wanxiang@jd.com',
+    license='BSD License',
+    packages=['wx_sdk'],
+    url='https://wx.jcloud.com/',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
