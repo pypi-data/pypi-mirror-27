3368db048055c9670a45b94472f63f1aee659a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:49 +0000

    Add package qiskit/0.3.16/qiskit-0.3.16-py3-none-any.whl

---
+++ packages/qiskit/qiskit-0.3.16-py3-none-any.whl/qiskit/__init__.py
@@ -0,0 +1,15 @@
+from ._classicalregister import ClassicalRegister
+from ._quantumregister import QuantumRegister
+from ._quantumcircuit import QuantumCircuit
+from ._gate import Gate
+from ._compositegate import CompositeGate
+from ._instruction import Instruction
+from ._instructionset import InstructionSet
+from ._qiskiterror import QISKitError
+import qiskit.extensions.standard
+from ._quantumprogram import QuantumProgram
+from ._quantumprogram import Result
+#from .simulators import UnitarySimulator
+
+
+__version__ = '0.3.16'
