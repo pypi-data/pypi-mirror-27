1cf7eb051ebb42cc59cae380ac84563d87b9713e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:52 +0000

    Add package kanika/v0.2/kanika-v0.2.tar.gz

---
+++ packages/kanika/kanika-v0.2.tar.gz/kanika-v0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'kanika',
+  packages = ['kanika'], # this must be the same as the name above
+  version = 'v0.2',
+  description = 'A random test lib',
+  author = 'Peter Downs',
+  author_email = 'gkanika@vmware.com',
+  url = 'https://github.com/gkanika/demo_library/kanika', # use the URL to the github repo
+  download_url = 'https://github.com/gkanika/demo_library/kanika/archive/v0.2.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
