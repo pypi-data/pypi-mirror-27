1f8ee7c1b0c7f87172b30c4e915496252800241f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:41 +0000

    Add package ethereum-utils/0.6.1/ethereum_utils-0.6.1-py3-none-any.whl

---
+++ packages/ethereum-utils/ethereum_utils-0.6.1-py3-none-any.whl/eth_utils/__init__.py
@@ -0,0 +1,5 @@
+raise DeprecationWarning(
+    "The `ethereum-utils` library has been renamed to `eth-utils`.  The "
+    "`ethereum-utils` library will no longer recieve updates.  Please update "
+    "your dependencies accordingly."
+)
---
+++ packages/ethereum-utils/ethereum_utils-0.6.1-py3-none-any.whl/ethereum_utils-0.6.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,7 @@
+Ethereum Utils
+==============
+
+This library has been renamed to ``eth-utils``. This package will no
+longer recieve updates. Please update your dependencies accordingly.
+
+
