a67f2984d68473760795801f66c1cede8e0cd2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:33 +0000

    Add package publicdata/0.1.5/publicdata-0.1.5.tar.gz

---
+++ packages/publicdata/publicdata-0.1.5.tar.gz/publicdata-0.1.5/publicdata/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2017 Civic Knowledge. This file is licensed under the terms of the
+# MIT, included in this distribution as LICENSE
+
+""" """
+
+
+from rowgenerators import get_generator, parse_app_url, Downloader
---
+++ packages/publicdata/publicdata-0.1.5.tar.gz/publicdata-0.1.5/setup.py
@@ -0,0 +1,38 @@
+from setuptools import  find_packages, setup
+import sys
+import os
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='publicdata',
+    version='0.1.5',
+    url='https://github.com/CivicKnowledge/publicdata',
+    license='MIT',
+    author='Eric Busboom',
+    author_email='eric@busboom.org',
+    description='Appurl And Rowgenerators for public datasets',
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=[
+        'fs >= 2',
+        'rowgenerators',
+        'pandas',
+        'requests',
+        'geoid'
+        ],
+    entry_points={
+        'appurl.urls': [
+            "censusreporter: = publicdata.censusreporter:CensusReporterURL",
+            "censusreportergeo: = publicdata.censusreporter:CensusReporterShapeURL"
+        ],
+        'rowgenerators': [
+            "CRJSON+ = publicdata.censusreporter:CensusReporterSource"
+
+        ]
+
+    },
+
+)
