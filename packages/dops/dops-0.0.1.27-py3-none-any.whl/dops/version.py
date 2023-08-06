f68b6ac786c9b9dc0b3b86d5639e60c4d552be5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:01 +0000

    Add package dops/0.0.1.27/dops-0.0.1.27-py3-none-any.whl

---
+++ packages/dops/dops-0.0.1.27-py3-none-any.whl/dops/version.py
@@ -0,0 +1,7 @@
+VERSION_INFO = (
+                ("program","dops"),
+                ("version","dev-0.0.1"),
+                ("pypi","0.0.1.27"),
+                ("commit","27.g421ada7"),
+                ("buildTime","2017-12-30 16:02:36"),
+)
