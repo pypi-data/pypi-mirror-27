e47b6e60933eaa6db2ab6122f19fa6add5b4a637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:07 +0000

    Add package yle-dl/2.30/yle-dl-2.30.tar.gz

---
+++ packages/yle-dl/yle-dl-2.30.tar.gz/yle-dl-2.30/yledl/utils.py
@@ -0,0 +1,22 @@
+from __future__ import print_function, absolute_import, unicode_literals
+import sys
+
+
+def print_enc(msg, out=None, linefeed_and_flush=True):
+    if out is None:
+        out = sys.stdout
+
+    if hasattr(out, 'buffer'):
+        bytes_out = out.buffer
+    else:
+        bytes_out = out
+
+    if hasattr(out, 'encoding'):
+        enc = out.encoding or 'UTF-8'
+    else:
+        enc = 'UTF-8'
+
+    bytes_out.write(bytes(msg.encode(enc, 'ignore')))
+    if linefeed_and_flush:
+        bytes_out.write(b'\n')
+        bytes_out.flush()
