f664cdd80bf3f38ef5d23dbe977b7301a1c6ac61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:33 +0000

    Add package django-fakery/1.8.5/django-fakery-1.8.5.tar.gz

---
+++ packages/django-fakery/django-fakery-1.8.5.tar.gz/django-fakery-1.8.5/django_fakery/compat.py
@@ -0,0 +1,12 @@
+try:
+    import psycopg2
+    HAS_PSYCOPG2 = True
+except ImportError:
+    psycopg2 = None
+    HAS_PSYCOPG2 = False
+
+try:
+    from django.contrib.gis.geos.libgeos import geos_version_info
+    HAS_GEOS = geos_version_info()['version'] >= '3.3.0'
+except (ImportError, OSError):
+    HAS_GEOS = False
