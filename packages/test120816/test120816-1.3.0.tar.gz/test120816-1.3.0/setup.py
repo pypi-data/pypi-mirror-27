5879bb25f5cfac7c73262d8ff22e81bb42b0a907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:54 +0000

    Add package test120816/1.3.0/test120816-1.3.0.tar.gz

---
+++ packages/test120816/test120816-1.3.0.tar.gz/test120816-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name = 'test120816',
+       version = '1.3.0',
+       py_modules = ['test120816'],
+       author = 'xiaoguang',
+       author_email = '1259123804@qq.com',
+       url = 'http://blog.useasp.net/',
+       description = 'A simple printer of nested lists',
+    ) 
