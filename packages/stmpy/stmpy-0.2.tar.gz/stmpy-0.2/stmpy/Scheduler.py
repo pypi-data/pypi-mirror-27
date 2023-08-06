86ca745ad1ea8b0db63921eb2cfbec9383ba295d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:49 +0000

    Add package stmpy/0.2/stmpy-0.2.tar.gz

---
+++ packages/stmpy/stmpy-0.2.tar.gz/stmpy-0.2/stmpy/Scheduler.py
@@ -0,0 +1,177 @@
+import time
+import logging
+from threading import Thread
+from threading import Event
+from threading import Condition
+from queue import Queue
+from queue import Empty
+
+
+def _current_time_millis():
+    return int(round(time.time() * 1000))
+
+class IterableQueue():
+    def __init__(self,source_queue):
+            self.source_queue = source_queue
+    def __iter__(self):
+        while True:
+            try:
+               yield self.source_queue.get_nowait()
+            except Empty:
+               return
+
+
+class Scheduler:
+
+    _stms_by_id = {}
+
+    def __init__(self):
+        #logging.setLevel
+        logging.warning('init scheduler!')
+        self._active = False
+        self._event_queue = Queue()
+        self._timer_queue = []
+        self._next_timeout = None
+        # TODO need clarity of this should be a class variable
+        Scheduler._stms_by_id = {}
+
+
+    def print_state(self):
+        print('=== State Machines: ===')
+        for stm_id in Scheduler._stms_by_id:
+            stm = Scheduler._stms_by_id[stm_id]
+            print('    - {} in state {}'.format(stm.id, stm.state))
+        print('=== Events in Queue: ===')
+        #print('Queue: {}'.format(self._event_queue))
+        for event in IterableQueue(self._event_queue):
+            if event is not None:
+                print('    - {} for {} with args:{} kwargs:{}'.format(event['id'], event['stm'].id, event['args'], event['kwargs']))
+        print('=== Active Timers: ===')
+        for timer in self._timer_queue:
+            print('    - {} for {} with timeout {}'.format(timer['id'], timer['stm'].id, timer['timeout']))
+
+    def add_stm(self, stm):
+        """
+        Add the state machine to this scheduler.
+        """
+        print("Add and initialize stm={}".format(stm))
+        #stm._initialize(self)
+        stm._scheduler = self
+        if stm.id is not None:
+            # TODO warning when STM already registered
+            Scheduler._stms_by_id[stm.id] = stm
+            self._event_queue.put({'id': None, 'stm': stm, 'args': [], 'kwargs': {}})
+
+    def start(self, max_transitions=None, keep_active=False, block=True):
+        """
+        max_transitions: execute only this number of transitions, then stop
+        keep_active: When true, keep the scheduler running even when all state machines terminated
+        """
+        self._active = True
+        self._max_transitions = max_transitions
+        self._keep_active = keep_active
+        self.thread = Thread(target = self._start_loop)
+        self.thread.start()
+        if block:
+            self.thread.join()
+
+
+    def step(self, steps=1):
+        self.start(max_transitions=steps, block=True)
+
+    def stop(self):
+        self._active = False
+        self._event_queue.put(None) # wake up the thread
+
+    def wait_until_finished(self):
+        self.thread.join()
+
+
+    def _sort_timer_queue(self):
+        self._timer_queue = sorted(self._timer_queue, key=lambda timer: timer['timeout_abs'])
+
+
+    def _start_timer(self, timer_id, timeout, stm):
+        print("Start timer with id={} from stm={}".format(timer_id, stm))
+        timeout_abs = _current_time_millis() + int(timeout)
+        self._timer_queue.append({'id': timer_id, 'timeout': timeout, 'timeout_abs': timeout_abs, 'stm': stm})
+        self._sort_timer_queue()
+        self._event_queue.put(None) # wake up the thread
+
+    def _stop_timer(self, timer_id, stm):
+        # TODO search through the timer queue and remove timer, sort again
+        #_sort_timer_queue()
+        self._event_queue.put(None) # wake up the thread
+
+    def _check_timers(self):
+        if self._timer_queue:
+            timer = self._timer_queue[0]
+            if timer['timeout_abs'] < _current_time_millis():
+                # the timer is expired
+                self._timer_queue.pop()
+                # put into the event queue
+                # TODO maybe put timer first in queue?
+                self._event_queue.put({'id': timer['id'], 'args': [], 'kwargs': {}, 'stm': timer['stm']})
+                # not necessary to set next timeout, complete check timers will be called again
+            else:
+                self._next_timeout = (timer['timeout_abs'] -  _current_time_millis()) / 1000
+                if self._next_timeout<0:
+                    self._next_timeout = 0
+        else:
+            self._next_timeout = None
+
+
+    def _add_event(self, event_id, args, kwargs, stm):
+        # add the event to the queue
+        self._event_queue.put({'id': event_id, 'args': args, 'kwargs':kwargs, 'stm': stm})
+
+
+    def send_signal(self, signal_id, stm_id, args=[], kwargs={}):
+        """
+        Send a signal to a state machine handled by this scheduler. If you have
+        a reference to the state machine, you can also send it directly to it by
+        using StateMachine.send_signal().
+
+        `stm_id` must be the id of a state machine earlier added to the scheduler.
+        """
+        stm = Scheduler._stms_by_id[stm_id]
+        # TODO error when STM not found
+        self._add_event(signal_id, args, kwargs, stm)
+
+
+    def _terminate_stm(self, stm_id):
+        # remove the state machine
+        logging.warning('terminating stm with frnk id {}'.format(stm_id))
+        Scheduler._stms_by_id.pop(stm_id, None)
+
+
+    def _execute_transition(self, stm, event_id, args, kwargs):
+        stm._execute_transition(event_id, args, kwargs)
+        if self._max_transitions is not None:
+            self._max_transitions = self._max_transitions-1
+            if self._max_transitions == 0:
+                self._active = False
+
+
+    def _start_loop(self):
+
+        while self._active:
+            #print('Scheduler in queue')
+            # check timer queue, if a timer expired
+            self._check_timers()
+            try:
+                #print("Wait for next event, {} sec max.".format(self._next_timeout))
+                event = self._event_queue.get(block=True, timeout=(self._next_timeout))
+                if event is None:
+                    # somebody added a timer, or shutdown the scheduler
+                    pass
+                else:
+                    self._execute_transition(stm=event['stm'], event_id=event['id'], args=event['args'], kwargs=event['kwargs'])
+                    if not Scheduler._stms_by_id and not self._keep_active: # no more state machines
+                        self._active = False
+            except Empty:
+                # timeout has occured
+                print("Event queue interrupted waiting because of timeout")
+                pass
+
+        print('Scheduler finished')
---
+++ packages/stmpy/stmpy-0.2.tar.gz/stmpy-0.2/stmpy/StateMachine.py
@@ -0,0 +1,110 @@
+import ast
+
+def _tid(state_id, event_id):
+    return state_id + '_' + 'event_id'
+
+class StateMachine:
+
+    def _parse_Transitions(self, transitions):
+        for transition_string in transitions:
+            t_dict = transition_string #ast.literal_eval(transition_string)
+            # TODO error handling: string may be written in a wrong way
+            trigger = t_dict['trigger']
+            source = t_dict['source']
+            target = t_dict['target']
+            effect = t_dict['effect']
+            t_id = _tid(source, trigger)
+            transition = _Transition(trigger, source, target, effect)
+            # TODO error handling: what if several transition with same id start from same source state?
+            self._table[t_id] = transition
+
+
+    def __init__(self, first_state, transitions, obj, stm_id, initial_effects=''):
+        self._first_state = first_state
+        self._state = None
+        self._obj = obj
+        self._initial_effects = initial_effects.split()
+        self._id = stm_id
+        self._table = {}
+        self._parse_Transitions(transitions)
+
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def scheduler(self):
+        return self._scheduler
+
+
+    def _run_function(self, obj, function_name, args, kwargs):
+        try:
+            func = getattr(obj, function_name)
+            print(str(func))
+            func(*args, **kwargs)
+        except AttributeError as error:
+            print('Error when running function from state machine: {}'.format(error))
+            #print("function {} not found on {}".format(function_name, obj))
+
+
+    def _initialize(self, scheduler):
+        self._scheduler = scheduler
+        # run initial transition
+
+
+
+    def _execute_transition(self, event_id, args, kwargs):
+        # execute the initial transition
+        if self._state is None:
+            if self._initial_effects is not None:
+                for function in self._initial_effects:
+                    self._run_function(self._obj, function, args=[], kwargs={})
+            self._state = self._first_state
+        else:
+            t_id = _tid(self._state, event_id)
+            if t_id not in self._table:
+                # TODO: error: no transition declared
+                print('Error: State machine is in state {} and received event {}, but no transition with this event is declared! {} '.format(self._state, event_id, self._table))
+            else:
+                transition = self._table[t_id]
+                for function in transition.effect:
+                    self._run_function(self._obj, function, args, kwargs)
+
+                # go into the next state
+                self._state = transition.target
+                print('state --> {}'.format(self._state))
+
+    def start_timer(self, timer_id, timeout):
+        self._scheduler._start_timer(timer_id, timeout, self)
+
+
+    def stop_timer(self, timer_id):
+        self._scheduler._stop_timer(timer_id, self)
+
+
+    def send_signal(self, signal_id, args=[], kwargs={}):
+        """
+        Send a signal to this state machine.
+
+        Note: To send a signal to a machine by its stm_id, use the method in the
+        scheduler.
+        """
+        self._scheduler._add_event(event_id=signal_id, args=args, kwargs=kwargs, stm=self)
+
+
+    def terminate(self):
+        self._scheduler._terminate_stm(self.id)
+
+
+class _Transition:
+
+    def __init__(self, trigger, source, target, effect):
+        self.trigger = trigger
+        self.source = source
+        self.target = target
+        self.effect = effect.split()
