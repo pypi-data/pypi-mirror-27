d4bf36e07ce28aa16457b02c90f17294657110ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:24 +0000

    Add package autoface/0.1/autoface-0.1.tar.gz

---
+++ packages/autoface/autoface-0.1.tar.gz/autoface-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='autoface',
+      version='0.1',
+      maintainer='Ahmed Elmahy',
+      maintainer_email='elmahy2005@gmail.com',
+      license='LGPL',
+      packages=['autoface']
+      )
