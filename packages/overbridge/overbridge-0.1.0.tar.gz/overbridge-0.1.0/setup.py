9fd67617130990937d36b7422ebeaadd16767d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:58 +0000

    Add package overbridge/0.1.0/overbridge-0.1.0.tar.gz

---
+++ packages/overbridge/overbridge-0.1.0.tar.gz/overbridge-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='overbridge',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='meituanqa',
+    author_email='',
+    description='The Overbridge, sometimes called the War Room or Death Star Control Room, was the bridge and main command center of the first Death Star. The Overbridge was located above the battlestation\'s superlaser dish.',
+    install_requires=[
+    ]
+)
