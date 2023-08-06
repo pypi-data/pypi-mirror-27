1a2455cbf0f73b52b016e93195f3d567bff2a209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:35 +0000

    Add package cloudcoverage/2.0/cloudcoverage-2.0.tar.gz

---
+++ packages/cloudcoverage/cloudcoverage-2.0.tar.gz/cloudcoverage-2.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys
+from distutils.core import setup
+
+try:
+    import PIL
+except ImportError:
+    sys.exit("""You need Pillow!
+                install it from https://pypi.org/project/Pillow/
+                or run pip install Pillow.""")
+                
+try:
+    import shapely
+except ImportError:
+    sys.exit("""You need Shapely!
+                install it from https://pypi.org/project/Shapely/
+                or run pip install shapely.""")
+                
+try:
+    import cv2
+except ImportError:
+    sys.exit("""You need opencv!
+                install it from https://pypi.org/project/opencv-python/
+                or run pip install opencv-python.""")
+                
+setup(name='cloudcoverage',
+      version='2.0',
+      description='Used to calculate cloud coverage form an image.',
+      author='Andrew Vadnais, Gage Davidson, Peter Bush',
+      author_email='avadnais@oswego.edu',
+      packages=['cloudcoverage'],
+      url="https://github.com/GDave50/Cloud-Coverage-Calculator",
+      license='MIT',
+      
+     )
+     
