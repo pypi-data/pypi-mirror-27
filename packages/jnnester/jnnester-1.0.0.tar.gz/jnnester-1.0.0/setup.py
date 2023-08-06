2077e069bfdb264aebf3ce952e8d57cf7744939b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package jnnester/1.0.0/jnnester-1.0.0.tar.gz

---
+++ packages/jnnester/jnnester-1.0.0.tar.gz/jnnester-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    #下面都是setup函数的参数名
+    name = 'jnnester',
+    version = '1.0.0',
+    #版本号
+    py_modules = ['jnnester'],
+    author = 'Karrdy',
+    #作者
+    author_email = 'jackaudrey@gmail.com',
+    url = 'http://www.headfristlabs.com',
+    description = 'A simple printer of nester lists'
+    #简介内容
+)
