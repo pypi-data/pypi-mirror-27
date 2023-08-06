cb82a3c95ace56ac295e8d775370862946f4b34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:55 +0000

    Add package django_settings_cmd/0.1/django_settings_cmd-0.1.tar.gz

---
+++ packages/django_settings_cmd/django_settings_cmd-0.1.tar.gz/django_settings_cmd-0.1/django_settings_cmd/command_line.py
@@ -0,0 +1,47 @@
+import sys,os,re
+
+def find_settings():
+    for root, dirs, files in os.walk("."):
+        if "settings.py" in files:
+            return os.path.join(root, "settings.py")
+    return None        
+
+def is_app_installed(content,app):
+    pass
+
+def enable_app():
+    if( len(sys.argv) != 2 ):
+        print("Expecting 1 app argument")
+    else:
+        disable_app();
+        appname = sys.argv[1] 
+        settings = find_settings()
+        if( settings ):
+            with open(settings,"r") as f:
+                content = f.read()
+                newconfig = content.replace( "INSTALLED_APPS = [", "INSTALLED_APPS = [\n    '"+appname+"',")
+                f.close()
+                with open(settings,"w") as g:
+                    g.write( newconfig )
+                print newconfig
+        else:
+            print( "Cannot locate a settings.py file. Make sure that current directory is a django project folder.")
+
+def disable_app():
+    if( len(sys.argv) != 2 ):
+        print("Expecting 1 app argument")
+    else:
+        appname = sys.argv[1] 
+        settings = find_settings()
+        if( settings ):
+            with open(settings,"r") as f:
+                content = f.read()
+                newconfig = re.sub( r"(INSTALLED_APPS \= \[[\s\S^\]]*?)(\s+'"+appname+"',)", r"\1", content )
+                f.close()
+                with open(settings,"w") as g:
+                    g.write( newconfig )
+                print newconfig
+        else:
+            print( "Cannot locate a settings.py file. Make sure that current directory is a django project folder.")
+        
+        pass
---
+++ packages/django_settings_cmd/django_settings_cmd-0.1.tar.gz/django_settings_cmd-0.1/setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from setuptools import setup
+
+setup(
+  name = 'django_settings_cmd',
+  packages = ['django_settings_cmd'], 
+  version = '0.1',
+  description = 'A commmand line tool to manage the django settings file',
+  author = 'Ruben Nørgaard',
+  author_email = 'email@rubennoergaard.dk',
+  url = 'https://github.com/RubenNorgaard/django-settings-cmd', 
+  download_url = 'https://github.com/RubenNorgaard/django-settings-cmd/archive/0.1.tar.gz', 
+  keywords = ['django', 'settings', 'commandline'],
+  classifiers = [],
+  entry_points = {
+      'console_scripts':
+          ['django-settings-enable-app = django_settings_cmd.command_line:enable_app',
+          'django-settings-disable-app = django_settings_cmd.command_line:disable_app'], 
+  }
+)
