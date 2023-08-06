bc37623f6b3c919a7a42728e25e19bf290b67f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:44 +0000

    Add package pyapp/3.3.1/pyApp-3.3.1-py2.py3-none-any.whl

---
+++ packages/pyapp/pyApp-3.3.1-py2.py3-none-any.whl/pyapp/utils.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+
+# Try to load colorama
+try:
+    import colorama
+except ImportError:
+    colorama = None
+    
+
+def is_iterable(obj):
+    """
+    Determine if an object is iterable.
+
+    :rtype: bool
+
+    """
+    try:
+        iter(obj)
+    except TypeError:
+        return False
+    else:
+        return True
+
+
+class cached_property(object):
+    """
+    A property that is only computed once per instance and then replaces
+    itself with an ordinary attribute. Deleting the attribute resets the
+    property. (From bottle)
+    """
+
+    def __init__(self, func):
+        self.__doc__ = getattr(func, '__doc__')
+        self.func = func
+
+    def __get__(self, obj, cls):
+        value = obj.__dict__[self.func.__name__] = self.func(obj)
+        return value
