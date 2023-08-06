ee46d58abf0bb584d05281b4862ecce62672b754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:47 +0000

    Add package urban.py/1.0/urban.py-1.0.tar.gz

---
+++ packages/urban.py/urban.py-1.0.tar.gz/urban.py-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="urban.py",
+    version="1.0",
+    author = 'Derek Acosta',
+    author_email = 'da606@georgetown.edu',
+    url = 'https://github.com/derekacosta/urbanthesaurus', # use the URL to the github repo
+    install_requires=[
+        "requests",
+    ],
+)
