978c9eb3558b0dabc2ab246d7c388ca076b154c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:43 +0000

    Add package chartkra/0.0.4/chartkra-0.0.4.tar.gz

---
+++ packages/chartkra/chartkra-0.0.4.tar.gz/chartkra-0.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='chartkra',
+    version='0.0.04',
+    description='chartkra',
+    url='https://github.com/KazafChen',
+    author='Kazaf Chen',
+    author_email='kazaf.chen@gmail.com',
+)
