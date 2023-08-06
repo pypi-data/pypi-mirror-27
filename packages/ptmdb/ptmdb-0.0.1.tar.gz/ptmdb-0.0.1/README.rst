d4568315fd3c6d5a3b50655d9bbbd12bb63a5b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:30 +0000

    Add package ptmdb/0.0.1/ptmdb-0.0.1.tar.gz

---
+++ packages/ptmdb/ptmdb-0.0.1.tar.gz/ptmdb-0.0.1/README.rst
@@ -0,0 +1,12 @@
+PTMDB Library
+=======================
+
+A small library to allow a pandas data frame to be saved to a MongoDB repository
+
+*Dependancies*::
+	import pandas as pd
+	import pymongo as py
+
+*Usage*::
+	import PTMDB as pmdb
+	
---
+++ packages/ptmdb/ptmdb-0.0.1.tar.gz/ptmdb-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ptmdb',
+    version='0.0.1',
+    description='Project to allow a pandas dataframe to save to MongoDB',
+    long_description=long_description,
+    author='Jimmy Hennessy',
+    author_email='jimmy@woden.ie',
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='sample setuptools development',  
+    packages=find_packages(exclude=['pandas', 'pymongo', 'json']),
+
+)
