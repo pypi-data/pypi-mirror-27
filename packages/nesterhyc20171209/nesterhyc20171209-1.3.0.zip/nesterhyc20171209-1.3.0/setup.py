113547f5c54d7476d5b0e2223622410a6450d095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:39 +0000

    Add package nesterhyc20171209/1.3.0/nesterhyc20171209-1.3.0.zip

---
+++ packages/nesterhyc20171209/nesterhyc20171209-1.3.0.zip/nesterhyc20171209-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nesterhyc20171209',
+    version='1.3.0',
+    py_modules=['nesterhyc20171209'],
+    author='hycpython',
+    author_email='heyichao1@supcon.com',
+    url='http://www.supcon.com',
+    description='A simple printer of nested lists',
+)
