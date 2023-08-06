09255eba7d59b6699e3176edd9b3ea0d83af9f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:40 +0000

    Add package parski-test/2.2.1.70/parski_test-2.2.1.70.tar.gz

---
+++ packages/parski-test/parski_test-2.2.1.70.tar.gz/parski_test-2.2.1.70/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+setup(
+  name = 'parski_test',
+  packages = ['parski'],
+  version = '2.2.1.70',
+  description = 'Helper library for filtering json data',
+  author = 'Russell Endicott',
+  author_email = 'rendicott@gmail.com',
+  keywords = ["vpc", "json", "parski"],
+  classifiers = []
+)
