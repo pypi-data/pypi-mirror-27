cf5f100fef5a825d82b45ce551d35dca40df43e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:18 +0000

    Add package py-vision/0.1.1/py-vision-0.1.1.tar.gz

---
+++ packages/py-vision/py-vision-0.1.1.tar.gz/py-vision-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='py-vision',
+    version='0.1.1',
+    packages=['py-vision'],
+    url='https://github.com/saivarunk/py-vision',
+    license='Apache License 2.0',
+    author='Varun Kruthiventi, Shiva Katepally',
+    author_email='saivarunk@gmail.com, shiva.analytics@gmail.com',
+    description='Image Processing Library for Python',
+    install_requires=[
+        'scipy',
+    ]
+)
