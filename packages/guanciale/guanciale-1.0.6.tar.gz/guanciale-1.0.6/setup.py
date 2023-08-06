25236048454eb1b57e107db18e88f704e47b2088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:53 +0000

    Add package guanciale/1.0.6/guanciale-1.0.6.tar.gz

---
+++ packages/guanciale/guanciale-1.0.6.tar.gz/guanciale-1.0.6/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+__author__ = "Andrea Fioraldi, Luigi Paolo Pileggi"
+__copyright__ = "Copyright 2017, Carbonara Project"
+__license__ = "BSD 2-clause"
+__email__ = "andreafioraldi@gmail.com, rop2bash@gmail.com"
+
+from setuptools import setup
+import os
+import platform
+
+setup(
+    name='guanciale',
+    version="1.0.6",
+    license=__license__,
+    description='Grab information needed by Carbonara',
+    author=__author__,
+    author_email=__email__,
+    url='https://github.com/Carbonara-Project/Guanciale',
+    download_url = 'https://github.com/Carbonara-Project/Guanciale/archive/1.0.1.tar.gz',
+    package_dir={'guanciale': 'guanciale'},
+    packages=['guanciale'],
+    install_requires=[
+            'requests',
+            'python-idb',
+            'archinfo',
+            'carbonara-pyvex',
+            'appdirs',
+            'datasketch'
+    ]
+)
