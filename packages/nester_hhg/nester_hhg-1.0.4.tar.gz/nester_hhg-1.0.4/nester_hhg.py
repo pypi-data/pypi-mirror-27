8989b03dec7fd999217cf3cddaae7df9859e6809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:14 +0000

    Add package nester_hhg/1.0.4/nester_hhg-1.0.4.tar.gz

---
+++ packages/nester_hhg/nester_hhg-1.0.4.tar.gz/nester_hhg-1.0.4/nester_hhg.py
@@ -0,0 +1,9 @@
+def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
+        for i in the_list:
+            if isinstance(i, list):
+                print_lol(i, indent, level+1, fh)
+            else:
+                if indent:
+                    for num in range(level):
+                        print("\t",end='', file=fh)
+                        print(i, file=fh)
