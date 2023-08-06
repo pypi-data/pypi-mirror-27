caa5380f551c237276b06c1f47f98e0d7773b7ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:38 +0000

    Add package aitx/0.3.1/aitx-0.3.1.tar.gz

---
+++ packages/aitx/aitx-0.3.1.tar.gz/aitx-0.3.1/aitx/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from aitx.imagebox import *
+from aitx.history import *
+import os
+__author__ = 'Chia-Jung, Yang'
+__email__ = 'jeroyang@gmail.com'
+__version__ = '0.3.1'
