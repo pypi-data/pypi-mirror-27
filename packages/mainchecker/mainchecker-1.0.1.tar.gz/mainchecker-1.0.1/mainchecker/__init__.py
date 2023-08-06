b33a79e138fb318e0c5092fe480cbc00e4fa26f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:23 +0000

    Add package mainchecker/1.0.1/mainchecker-1.0.1.tar.gz

---
+++ packages/mainchecker/mainchecker-1.0.1.tar.gz/mainchecker-1.0.1/mainchecker/__init__.py
@@ -0,0 +1,6 @@
+def check_for_main(name = __name__):
+    if 'main' == name:
+        raise Exception('Do not launch this program directly, but import it.')
+		
+if __name__ == '__main__':
+	check_for_main()
---
+++ packages/mainchecker/mainchecker-1.0.1.tar.gz/mainchecker-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='mainchecker',
+    version='1.0.1',
+    description='Easy way to implement the \'if __name__ == \'__main__\' \'',
+    author='PokestarFan',
+    author_email='pokestarfan@yahoo.com',
+    license='MIT',
+    packages=['mainchecker'],
+    zip_safe=True)
