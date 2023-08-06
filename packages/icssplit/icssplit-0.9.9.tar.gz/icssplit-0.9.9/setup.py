d2e5f850ac0d7ed1f00c7c16c76f420d3f6c00af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:11 +0000

    Add package icssplit/0.9.9/icssplit-0.9.9.tar.gz

---
+++ packages/icssplit/icssplit-0.9.9.tar.gz/icssplit-0.9.9/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+setuptools.setup(
+    name="icssplit",
+    version="0.9.9",
+    url="https://github.com/beorn/icssplit",
+    download_url = 'https://github.com/beorn/icssplit/archive/0.9.9.tar.gz',
+
+    author="Bjorn Stabell",
+    author_email="bjorn@stabell.org",
+
+    description="Split big .ics/.ical files into smaller ones for easy import into Google Calendar",
+    long_description=open('README.rst').read(),
+    license="MIT",
+
+    #packages=setuptools.find_packages(),
+    #packages=['icssplit'],
+    py_modules=['icssplit',],
+
+    python_requires='~=3.0',
+    install_requires=['docopt',],
+
+    #scripts=['icssplit.py'],
+    entry_points={ 'console_scripts': ['icssplit=icssplit:cli',] },
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'Topic :: Office/Business :: Scheduling',
+    ],
+    keywords='ics calendar ical vcal',
+)
