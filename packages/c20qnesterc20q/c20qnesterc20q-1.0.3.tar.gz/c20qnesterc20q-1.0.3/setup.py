8395579be61e779d01b9c1b7c285e7b27ccd3b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:11 +0000

    Add package c20qnesterc20q/1.0.3/c20qnesterc20q-1.0.3.tar.gz

---
+++ packages/c20qnesterc20q/c20qnesterc20q-1.0.3.tar.gz/c20qnesterc20q-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+ name = 'c20qnesterc20q',
+ version = '1.0.3',
+ py_modules = ['c20qnesterc20q'],
+ author = 'c20q',
+ author_email = 'chenqing151@gmail.com',
+ url = 'https://pypi.python.org/pypi/crazy20q',
+ description = 'A practice of a new learner',
+     )
