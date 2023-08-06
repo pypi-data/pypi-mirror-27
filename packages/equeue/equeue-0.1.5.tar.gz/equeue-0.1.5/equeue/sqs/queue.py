0a28db4131e6e17c88bd9491d9081a17634a0772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:08 +0000

    Add package equeue/0.1.5/equeue-0.1.5.tar.gz

---
+++ packages/equeue/equeue-0.1.5.tar.gz/equeue-0.1.5/equeue/sqs/queue.py
@@ -0,0 +1,19 @@
+" Press ? for help
+
+.. (up a dir)
+</jesuejunior/code/equeue/
+▸ build/
+▸ dist/
+▸ equeue.egg-info/
+▾ equeue/
+  ▸ rabbit/
+  ▸ tests/
+    __init__.py
+  example.py
+  LICENSE
+  main.py
+  MANIFEST.in
+  README.md
+  requirements-dev.txt
+  requirements.txt
+  setup.py
