9ef7a861c0499175f56425938f494254291b9dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:02 +0000

    Add package var_dump/1.2/var_dump-1.2.tar.gz

---
+++ packages/var_dump/var_dump-1.2.tar.gz/var_dump-1.2/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='var_dump',
+    version='1.2',
+    packages=['var_dump'],
+    url='http://github.com/sha256/python-var-dump',
+    license='BSD',
+    author='Shamim Hasnath',
+    author_email='shamim@hasnath.net',
+    description='var_dump for python',
+    download_url='http://github.com/sha256/python-var-dump/tarball',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+)
