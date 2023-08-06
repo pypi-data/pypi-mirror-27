0247c86db2bbcf1968b6b1fbe933e37c52263c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:39 +0000

    Add package flake8-invalid-escape-sequences/0.1.4/flake8_invalid_escape_sequences-0.1.4-py2.py3-none-any.whl

---
+++ packages/flake8-invalid-escape-sequences/flake8_invalid_escape_sequences-0.1.4-py2.py3-none-any.whl/flake8_invalid_escape_sequences/test.py
@@ -0,0 +1,12 @@
+from flake8.main import cli
+
+
+cli.main(["E:/docs/programming/python/wiki/pywikibot-core/user-sandbox.py"])
+# cli.main([
+#     "E:/docs/programming/python/wiki/pywikibot-core/pywikibot/login.py",
+#     '--ignore=H,E,D,N,F'])
+
+# cli.main([
+#     r"E:\docs\programming\python\wiki\pywikibot-core\tests\pwb\__init__.py"
+# ])
+
