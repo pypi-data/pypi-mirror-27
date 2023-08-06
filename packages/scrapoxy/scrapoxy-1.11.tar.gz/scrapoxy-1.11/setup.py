f2063bf8397fc74be9cdc54f2f285d1c1b010d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:33 +0000

    Add package scrapoxy/1.11/scrapoxy-1.11.tar.gz

---
+++ packages/scrapoxy/scrapoxy-1.11.tar.gz/scrapoxy-1.11/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+setup(
+    name='scrapoxy',
+    packages=find_packages(),
+    install_requires=['requests'],
+    version='1.11',
+    description='Use Scrapoxy with Scrapy',
+    author='Fabien Vauchelles',
+    author_email='fabien@vauchelles.com',
+    url='https://github.com/fabienvauchelles/scrapoxy-python-api',
+    download_url='https://github.com/fabienvauchelles/scrapoxy-python-api/tarball/1.11',
+    keywords=['crawler', 'crawling', 'scrapoxy', 'scrapy', 'scraper', 'scraping'],
+    classifiers=[],
+)
