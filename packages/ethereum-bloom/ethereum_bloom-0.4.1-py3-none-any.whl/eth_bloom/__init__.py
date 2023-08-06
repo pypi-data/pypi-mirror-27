5505cedace0cd5bbaefdc532d89c5d80863a8d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:44 +0000

    Add package ethereum-bloom/0.4.1/ethereum_bloom-0.4.1-py3-none-any.whl

---
+++ packages/ethereum-bloom/ethereum_bloom-0.4.1-py3-none-any.whl/eth_bloom/__init__.py
@@ -0,0 +1,5 @@
+raise DeprecationWarning(
+    "This library has been renamed to `eth-bloom`.  The `ethereum-bloom` "
+    "package will no longer recieve updates.  Please update your dependencies "
+    "accordingly."
+)
---
+++ packages/ethereum-bloom/ethereum_bloom-0.4.1-py3-none-any.whl/ethereum_bloom-0.4.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,7 @@
+Ethereum Bloom
+==============
+
+This library has been renamed to ``eth-bloom``. This package will no
+longer recieve updates. Please update your dependencies accordingly.
+
+
