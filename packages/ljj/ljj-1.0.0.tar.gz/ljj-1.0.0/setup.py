3fae7eec25e2154781248754c4e17d309a199f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:38 +0000

    Add package ljj/1.0.0/ljj-1.0.0.tar.gz

---
+++ packages/ljj/ljj-1.0.0.tar.gz/ljj-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name     ='ljj',
+        version  ='1.0.0',
+        py_modules=['nester'],
+        author   ='hfpython',
+        author_email='173925462@qq.com',
+        url ='http://www.guabi.tv',
+        description='a simple printer of nested lists',
+        )
