0626642737689b1e239ae1c1c3b6e9691ceb59a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:47 +0000

    Add package grafana-api/0.1.4/grafana_api-0.1.4-py2.py3-none-any.whl

---
+++ packages/grafana-api/grafana_api-0.1.4-py2.py3-none-any.whl/grafana_api/grafana_api.py
@@ -0,0 +1,36 @@
+import requests
+
+
+class GrafanaAPI:
+    def __init__(self, auth, host="localhost", port=None, url_path_prefix="", protocol="http"):
+        self.auth = auth
+        self.url_host = host
+        self.url_port = port
+        self.url_path_prefix = url_path_prefix
+        self.url_protocol = protocol
+
+        def construct_api_url():
+            params = {
+                "protocol": self.url_protocol,
+                "host": self.url_host,
+                "url_path_prefix": self.url_path_prefix,
+            }
+
+            if self.url_port is None:
+                url_pattern = "{protocol}://{host}/{url_path_prefix}api"
+            else:
+                params["port"] = self.url_port
+                url_pattern = "{protocol}://{host}:{port}/{url_path_prefix}api"
+
+            return url_pattern.format(**params)
+
+        self.url = construct_api_url()
+
+    def __getattr__(self, item):
+        def __requests_run(url, json=None, headers=None):
+            __url = "%s%s" % (self.url, url)
+            runner = getattr(requests, item.lower())
+            r = runner(__url, json=json, headers=headers, auth=self.auth)
+            return r
+
+        return __requests_run
