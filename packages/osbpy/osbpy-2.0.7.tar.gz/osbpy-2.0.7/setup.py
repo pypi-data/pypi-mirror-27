9915a0c26da808e2ef2f1022035215f914a995f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:58 +0000

    Add package osbpy/2.0.7/osbpy-2.0.7.tar.gz

---
+++ packages/osbpy/osbpy-2.0.7.tar.gz/osbpy-2.0.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os.path
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.6",
+]
+
+setup(
+    name="osbpy",
+    version="2.0.7",
+    packages=['osbpy'],
+    include_package_data=True,
+    install_requires=[
+        'matplotlib>=2.1.0',
+        'numpy>=1.13.3+mkl',
+        'scipy>=1.0.0'
+    ],
+    author="Jiri Olszar",
+    author_email="remiliass@gmail.com",
+    description="Simple library for osu! storyboarding",
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), "README.rst")).read(),
+    classifiers=classifiers,
+    license="MIT",
+    keywords="graphics audio rhythm game",
+    url="https://github.com/KawaiiWafu/osbpy",
+    python_requires=">=3",
+)
