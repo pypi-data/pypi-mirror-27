0b82fac1f2125c6d85e4d2e9e59c0db1e6f6fbd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:05 +0000

    Add package udicTfidf/0.6/udicTfidf-0.6.tar.gz

---
+++ packages/udicTfidf/udicTfidf-0.6.tar.gz/udicTfidf-0.6/udicTfidf/views.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django.http import JsonResponse
+from djangoApiDec.djangoApiDec import queryString_required
+from udicTfidf import TFIDF
+from udic_nlp_API.settings_database import uri
+obj = TFIDF(uri=uri)
+
+@queryString_required(['keyword'])
+def idf(request):
+    keyword = request.GET['keyword']
+    return JsonResponse(obj.idf(keyword), safe=False)
+
+def tfidf(request):
+    if request.POST and 'doc' in request.POST:
+        doc = request.POST.dict()['doc']
+        try:
+            return JsonResponse(obj.tfidf(doc), safe=False)
+        except Exception as e:
+            return JsonResponse([], safe=False)
+    return JsonResponse([], safe=False)
