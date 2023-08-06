bc8454bce964bd28c46e7d72b2312b2e2c4758fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:08 +0000

    Add package arjun_nester/1.1.0/arjun_nester-1.1.0.tar.gz

---
+++ packages/arjun_nester/arjun_nester-1.1.0.tar.gz/arjun_nester-1.1.0/arjun_nester.py
@@ -0,0 +1,13 @@
+
+"""This module is defining a recursive function for printing out lists within lists"""
+
+def print_lol(the_list,level):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
+			
+         
---
+++ packages/arjun_nester/arjun_nester-1.1.0.tar.gz/arjun_nester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'arjun_nester',
+    version = '1.1.0',
+    py_modules =['arjun_nester'],
+    author = 'Arjun Prakash',
+    author_email = 'arjuniyer.55@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A Simple printer of nested lists',
+    )
