40ddf9c4ad4a7f4dbc084d0e6b5c3f24334a58df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:36 +0000

    Add package n2w/0.1.3/n2w-0.1.3.tar.gz

---
+++ packages/n2w/n2w-0.1.3.tar.gz/n2w-0.1.3/n2w/n2w.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .converter import Convert
+
+
+class N2w(Convert):
+
+    def __init__(self):
+        super(N2w, self).__init__()
+
+    def convert(self, number=0):
+        if number == 0:
+            return "zero"
+        else:
+            # check for invalid int
+            try:
+                int(number)
+            except ValueError:
+                return "Input not a valid number"
+
+            # check if number bigger than quintillion
+            if (len(str(number)) >= (len(self.greater_names) * 4) - 1):
+                return "Number too big"
+
+            number, prefix = int(number), ""
+            if number < 0:
+                prefix, number = "negative", -number
+
+            result, pointer = "", 0
+            while True:
+                if int(number) % 1000 != 0:
+                    result = "{} {} {}".format(self.less_than_athousand(
+                        number % 1000), self.greater_names.get(pointer - 1), result)
+                pointer += 1
+                number /= 1000
+
+                if int(number) < 1:
+                    break
+            return "{} {}".format(prefix, result).strip().replace("  ", " ")
---
+++ packages/n2w/n2w-0.1.3.tar.gz/n2w-0.1.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="n2w",
+    version="0.1.3",
+    description="Python library to convert numbers to words",
+    long_description="Python library to covnert numbers to words",
+    kewords='library numbers algorithim words convert',
+    author="Collins Abitekaniza",
+    author_email="abtcolns@gmail.com",
+    url="https://github.com/collin5/python-n2w",
+    packages=['n2w'],
+    install_requires=[
+        'markdown'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
