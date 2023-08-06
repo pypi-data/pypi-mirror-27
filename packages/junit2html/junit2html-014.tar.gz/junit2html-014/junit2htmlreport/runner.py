cca61bf48d4fa00675af207c5b1cce675591986f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:11 +0000

    Add package junit2html/014/junit2html-014.tar.gz

---
+++ packages/junit2html/junit2html-014.tar.gz/junit2html-014/junit2htmlreport/runner.py
@@ -0,0 +1,44 @@
+"""
+Small command line tool to generate a html version of a junit report file
+"""
+
+from optparse import OptionParser
+import sys
+from junit2htmlreport import parser
+
+
+USAGE = "usage: %prog JUNIT_XML_REPORT [OUTFILE.html]"
+PARSER = OptionParser(usage=USAGE, prog="junit2html")
+
+
+def run(args):
+    """
+    Run this tool
+    :param args:
+    :return:
+    """
+    (opts, args) = PARSER.parse_args(args) if args else PARSER.parse_args()
+    if not len(args):
+        PARSER.print_usage()
+        sys.exit(1)
+
+    outfilename = args[0] + ".html"
+    if len(args) > 1:
+        outfilename = args[1]
+
+    report = parser.Junit(args[0])
+    html = report.html()
+
+    with open(outfilename, "wb") as outfile:
+        outfile.write(html.encode('utf-8'))
+
+
+def start():
+    """
+    Run using the current sys.argv
+    """
+    run(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    start()
