60e48ab1b1d2fb87fd6794caf1e10f1d69b0b1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package gjtheme/0.1.1/gjtheme-0.1.1.tar.gz

---
+++ packages/gjtheme/gjtheme-0.1.1.tar.gz/gjtheme-0.1.1/gjtheme/__init__.py
@@ -0,0 +1,13 @@
+"""Sphinx GJ Theme
+
+Sphinx GJ Theme is an Apache2 licensed Sphinx theme for projects by Grant
+Jenks.
+
+"""
+
+__title__ = 'gjtheme'
+__version__ = '0.1.1'
+__build__ = 0x000101
+__author__ = 'Grant Jenks'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2017 Grant Jenks'
