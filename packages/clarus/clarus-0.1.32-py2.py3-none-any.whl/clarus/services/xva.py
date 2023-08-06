356b04e5aeb97225b178d626dc6902dba4427512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:23 +0000

    Add package clarus/0.1.32/clarus-0.1.32-py2.py3-none-any.whl

---
+++ packages/clarus/clarus-0.1.32-py2.py3-none-any.whl/clarus/services/xva.py
@@ -0,0 +1,11 @@
+import clarus.services
+
+def fva(output=None, **params):
+    return clarus.services.api_request('XVA', 'FVA', output=output, **params)
+
+def mva(output=None, **params):
+    return clarus.services.api_request('XVA', 'MVA', output=output, **params)
+
+def mvaattribution(output=None, **params):
+    return clarus.services.api_request('XVA', 'MVAAttribution', output=output, **params)
+
