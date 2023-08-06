823a110159cb17496ff827d4e887d642b5572987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:25 +0000

    Add package kiteconnect/3.6.2/kiteconnect-3.6.2.tar.gz

---
+++ packages/kiteconnect/kiteconnect-3.6.2.tar.gz/kiteconnect-3.6.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name="kiteconnect",
+	version="3.6.2",
+	description="The official Python client for the Kite Connect trading API",
+	author="Rainmatter Technology (India)",
+	author_email="talk@rainmatter.com",
+	url="https://kite.trade",
+	packages=["kiteconnect"],
+	download_url="https://github.com/rainmattertech/pykiteconnect",
+	license="MIT",
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Financial and Insurance Industry",
+		"Programming Language :: Python",
+		"Natural Language :: English",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3.3",
+		"Programming Language :: Python :: 3.4",
+		"Programming Language :: Python :: 3.5",
+		"Topic :: Office/Business :: Financial :: Investment",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Software Development :: Libraries"
+	],
+	install_requires=["requests", "six", "websocket-client"]
+)
