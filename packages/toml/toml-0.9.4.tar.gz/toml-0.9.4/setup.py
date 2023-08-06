bff4bebb7ecbdb65d760b2b7ca960bc0a5f0ee6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:43 +0000

    Add package toml/0.9.4/toml-0.9.4.tar.gz

---
+++ packages/toml/toml-0.9.4.tar.gz/toml-0.9.4/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import toml
+
+with open("README.rst") as readme_file:
+    readme_string = readme_file.read()
+
+setup(
+    name="toml",
+    version=toml.__version__,
+    description="Python Library for Tom's Obvious, Minimal Language",
+    author="Uiri Noyb",
+    author_email="uiri@xqz.ca",
+    url="https://github.com/uiri/toml",
+    py_modules=['toml'],
+    license="License :: OSI Approved :: MIT License",
+    long_description=readme_string,
+    classifiers=[
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6']
+)
