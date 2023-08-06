9953ce7b24f13098312345382ef17c9f2da9683d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:52 +0000

    Add package nnmnkwii/0.0.11/nnmnkwii-0.0.11.tar.gz

---
+++ packages/nnmnkwii/nnmnkwii-0.0.11.tar.gz/nnmnkwii-0.0.11/nnmnkwii/preprocessing/__init__.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+from __future__ import division, print_function, absolute_import
+
+from .generic import mulaw, inv_mulaw, mulaw_quantize, inv_mulaw_quantize
+from .generic import preemphasis, inv_preemphasis
+from .generic import delta_features, trim_zeros_frames, remove_zeros_frames
+from .generic import adjast_frame_length, adjast_frame_lengths
+from .generic import scale, inv_scale
+from .generic import minmax_scale_params, minmax_scale, inv_minmax_scale
+from .generic import meanvar, meanstd, minmax
+from .f0 import interp1d
+from .modspec import modspec, modphase, inv_modspec, modspec_smoothing
+
+__all__ = ['mulaw', 'inv_mulaw', 'mulaw_quantize', 'inv_mulaw_quantize',
+           'preemphasis', 'inv_preemphasis', 'delta_features',
+           'trim_zeros_frames', 'remove_zeros_frames',
+           'adjast_frame_length', 'adjast_frame_lengths',
+           'scale', 'inv_scale', 'minmax_scale_params', 'minmax_scale',
+           'inv_minmax_scale', 'meanvar',
+           'meanstd', 'minmax', 'interp1d',
+           'modspec', 'modphase', 'inv_modspec', 'modspec_smoothing']
