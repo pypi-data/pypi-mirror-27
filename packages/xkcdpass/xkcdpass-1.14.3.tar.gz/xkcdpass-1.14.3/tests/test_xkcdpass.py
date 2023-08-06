77796de7566d1925423230d0edda41378645ae8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:31 +0000

    Add package xkcdpass/1.14.3/xkcdpass-1.14.3.tar.gz

---
+++ packages/xkcdpass/xkcdpass-1.14.3.tar.gz/xkcdpass-1.14.3/tests/test_xkcdpass.py
@@ -0,0 +1,54 @@
+import re
+import sys
+import subprocess
+import unittest
+
+from xkcdpass import xkcd_password
+
+
+WORDFILE = 'xkcdpass/static/legacy'
+
+
+class XkcdPasswordTests(unittest.TestCase):
+    def setUp(self):
+        self.wordlist_full = xkcd_password.generate_wordlist(
+            wordfile=WORDFILE,
+            min_length=5,
+            max_length=8,)
+        self.wordlist_small = xkcd_password.generate_wordlist(
+            wordfile='tests/test_list.txt',
+            valid_chars='[a-z]')
+
+    def test_loadwordfile(self):
+        self.assertEqual(len(self.wordlist_full), 29611)
+
+    def test_regex(self):
+        self.assertNotIn("__$$$__", self.wordlist_small)
+
+    def test_acrostic(self):
+        word = "face"
+        result = xkcd_password.generate_xkcdpassword(
+            self.wordlist_small,
+            acrostic=word)
+        self.assertEqual("".join(map(lambda x: x[0], result.split())), word)
+
+    def test_commandlineCount(self):
+        count = 5
+        result = subprocess.check_output([
+            sys.executable, "xkcdpass/xkcd_password.py",
+            "-w", WORDFILE,
+            "-c", str(count),
+        ])
+        self.assertTrue(result.count(b"\n"), count)
+
+    def test_delim(self):
+        tdelim = "_"
+        result = xkcd_password.generate_xkcdpassword(
+            self.wordlist_small,
+            delimiter=tdelim)
+        self.assertIsNotNone(re.match('([a-z]+(_|$))+', result))
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(XkcdPasswordTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
