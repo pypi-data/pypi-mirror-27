15e3c2fdf21f9310ced181ddca7e8288dec8025e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:24 +0000

    Add package NewArcWar/2.0.0/NewArcWar-2.0.0.tar.gz

---
+++ packages/NewArcWar/NewArcWar-2.0.0.tar.gz/NewArcWar-2.0.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+with open('README') as file:
+    readme = file.read()
+
+# NOTE: change the 'name' field below with some unique package name.
+# then update the url field accordingly.
+
+setup(
+    name='NewArcWar',
+    version='2.0.0',
+    packages=['wargame'],
+    url='https://upload.pypi.org/legacy/',
+    license='LICENSE.txt',
+    description='test pkg ignore',
+    long_description=readme,
+    author='Kamal',
+    author_email='infikamal5@gmail.com'
+)
+
