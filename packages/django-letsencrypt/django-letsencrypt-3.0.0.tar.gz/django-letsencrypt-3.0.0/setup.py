06a1e241d65ea51d8d3e61db24913fe80e43eb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:06 +0000

    Add package django-letsencrypt/3.0.0/django-letsencrypt-3.0.0.tar.gz

---
+++ packages/django-letsencrypt/django-letsencrypt-3.0.0.tar.gz/django-letsencrypt-3.0.0/setup.py
@@ -0,0 +1,61 @@
+"""
+Copyright 2016-2017 Peter Urda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-letsencrypt',
+    version='3.0.0',
+    packages=['letsencrypt'],
+    include_package_data=True,
+    license='Apache License, Version 2.0',
+    description="A simple Django app to handle Let's Encrypt ACME challenges.",
+    long_description=README,
+    url='https://github.com/urda/django-letsencrypt',
+    author='Peter Urda',
+    author_email='noreply@urda.cc',
+    install_requires=[
+        "Django>=1.8.18,!=1.9.*,!=1.10.*",
+        "pytz>=2017.3",
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
