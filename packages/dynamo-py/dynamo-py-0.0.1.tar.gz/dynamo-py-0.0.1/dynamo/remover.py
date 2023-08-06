94227a302c5714af569796f11fc5d2853182916f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:13 +0000

    Add package dynamo-py/0.0.1/dynamo-py-0.0.1.tar.gz

---
+++ packages/dynamo-py/dynamo-py-0.0.1.tar.gz/dynamo-py-0.0.1/dynamo/remover.py
@@ -0,0 +1,44 @@
+import ast
+import astunparse
+from typing import List, Optional
+
+def compile(file: str, output: str) -> None:
+    with open(file, 'r') as f:
+        source = f.read()
+    compiled = compile_source(source)
+    with open(output, 'w') as f:
+        f.write(compiled)
+
+def compile_source(source: str) -> str:
+    return astunparse.unparse(compile_ast(ast.parse(source)))
+
+def compile_ast(node: ast.AST) -> ast.AST:
+    return Compiler().visit(node)
+
+class Compiler(ast.NodeTransformer):
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> Optional[ast.AST]:
+        self.visit(node.args)
+        if node.returns is not None:
+            node.returns = None
+        return node
+
+    def visit_arg(self, node: ast.arg) -> Optional[ast.AST]:
+        if node.annotation is not None:
+            node.annotation = None
+        return node
+
+    def visit_alias(self, node: ast.alias) -> Optional[ast.AST]:
+        if node.name == 'typing':
+            return None
+        return node
+
+    def visit_ImportFrom(self, node: ast.ImportFrom) -> Optional[ast.AST]:
+        if node.module == 'typing' or node.module == 'mypy_extensions':
+            return None
+        return node
+
+    def visit_Assign(self, node: ast.Assign) -> Optional[ast.AST]:
+        if isinstance(node.targets[0], ast.Name) and isinstance(node.value, ast.Call) and \
+           isinstance(node.value.func, ast.Name) and node.value.func.id == 'TypedDict':
+            return ast.parse('class %s: pass' % node.targets[0].id)
+        return node
