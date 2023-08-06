95bd7c5e5d99d6be6d7b420b446cc1fd89d7ba5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:39 +0000

    Add package skitai/0.26.16.8/skitai-0.26.16.8.tar.gz

---
+++ packages/skitai/skitai-0.26.16.8.tar.gz/skitai-0.26.16.8/skitai/saddle/__init__.py
@@ -0,0 +1,18 @@
+"""
+2015. 12. 10
+Hans Roh
+"""
+from . import Saddle, part
+
+Saddle = Saddle.Saddle
+
+# deprecated in version 0.26.11
+#Saddlery = part.Part
+
+request_started = 0
+request_finished = 1
+request_exception_occured = 2
+request_tearing_down = 3
+template_rendered = 4
+message_flashed = 5
+
