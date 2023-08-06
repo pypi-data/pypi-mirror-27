4fe30f6a9732a406d8700dd9bb60b381d709dbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:07 +0000

    Add package django-grainy/1.4.0/django-grainy-1.4.0.tar.gz

---
+++ packages/django-grainy/django-grainy-1.4.0.tar.gz/django-grainy-1.4.0/django_grainy_test/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from .views import (
+    ModelAViewSet,
+    JsonView,
+    View,
+    view,
+    Detail,
+    detail,
+    DetailExplicit,
+    detail_explicit,
+    DetailManual
+)
+
+router = routers.DefaultRouter()
+router.register(r'a', ModelAViewSet)
+
+
+urlpatterns = [
+    url(r'^view/', view),
+    url(r'^detail/(?P<id>[0-9]+)/$', detail),
+    url(r'^detail_explicit/(?P<id>[0-9]+)/$', detail_explicit),
+    url(r'^detail_class/(?P<id>[0-9]+)/$', Detail.as_view()),
+    url(r'^detail_class_explicit/(?P<id>[0-9]+)/$', DetailExplicit.as_view()),
+    url(r'^detail_class_manual/(?P<id>[0-9]+)/$', DetailManual.as_view()),
+    url(r'^detail_class_manual/$', DetailManual.as_view()),
+    url(r'^view_class/', View.as_view()),
+    url(r'^view_class_json/', JsonView.as_view()),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
---
+++ packages/django-grainy/django-grainy-1.4.0.tar.gz/django-grainy-1.4.0/django_grainy_test/views.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views import View as BaseView
+
+from rest_framework import viewsets
+from django_grainy.decorators import (
+    grainy_view,
+    grainy_json_view,
+    grainy_rest_viewset,
+    grainy_view_response
+)
+
+from .models import ModelA
+from .serializers import ModelASerializer
+
+# Create your views here.
+
+@grainy_rest_viewset(
+    namespace = "api.a",
+    handlers = {
+        "nested_dict.secret" : { "explicit" : True }
+    }
+)
+class ModelAViewSet(viewsets.ModelViewSet):
+    queryset = ModelA.objects.all()
+    serializer_class = ModelASerializer
+
+
+@grainy_view(namespace="site.view")
+def view(request):
+    return HttpResponse()
+
+
+@grainy_view(namespace="detail.{id}")
+def detail(request, id):
+    return HttpResponse("ID {}".format(id))
+
+
+@grainy_view(
+    namespace="detail.{id}",
+    explicit=True,
+    ignore_grant_all=True
+)
+def detail_explicit(request, id):
+    return HttpResponse("ID {}".format(id))
+
+
+@grainy_view(namespace="site.view")
+class View(BaseView):
+    def get(self, request):
+        return HttpResponse(content="GET Response")
+
+    def post(self, request):
+        return HttpResponse(content="POST Response")
+
+    def put(self, request):
+        return HttpResponse(content="PUT Response")
+
+    def delete(self, request):
+        return HttpResponse(content="DELETE Response")
+
+    def patch(self, request):
+        return HttpResponse(content="PATCH Response")
+
+@grainy_view(namespace="detail.{id}")
+class Detail(BaseView):
+
+    def get(self, request, id):
+        return HttpResponse(content="GET Response {}".format(id))
+
+    def post(self, request, id):
+        return HttpResponse(content="POST Response {}".format(id))
+
+    def put(self, request, id):
+        return HttpResponse(content="PUT Response {}".format(id))
+
+    def delete(self, request, id):
+        return HttpResponse(content="DELETE Response {}".format(id))
+
+    def patch(self, request, id):
+        return HttpResponse(content="PATCH Response {}".format(id))
+
+
+@grainy_view(
+    namespace="detail.{id}",
+    explicit=True,
+    ignore_grant_all=True
+)
+class DetailExplicit(Detail):
+    pass
+
+
+@grainy_view(namespace="detail_manual.{id}")
+class DetailManual(BaseView):
+
+    def get(self, request, id):
+        return HttpResponse(content="GET Response {}".format(id))
+
+    @grainy_view_response(namespace="detail_manual")
+    def post(self, request):
+        return HttpResponse(content="POST Response 1")
+
+
+@grainy_json_view(
+    namespace="site.view",
+    handlers = {
+        "nested_dict.secret" : { "explicit" : True }
+    }
+)
+class JsonView(BaseView):
+    def get(self, request):
+        return JsonResponse({
+            "hello" : "world",
+            "nested_dict" : {
+                "public": "something",
+                "secret": "hidden"
+            }
+        })
