afd847ee2bf6af2d0b4026e0e89d037056373447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:52 +0000

    Add package bpmagic/0.13.0/bpmagic-0.13.0.tar.gz

---
+++ packages/bpmagic/bpmagic-0.13.0.tar.gz/bpmagic-0.13.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+major = 0
+minor = 13
+patch = 0
+version = '.'.join([str(v) for v in [major, minor, patch]])
+download_url = 'https://github.com/pm8k/bpmagic/archive/{V}.tar.gz'.format(
+    V=version)
+
+setup(name='bpmagic',
+      version=version,
+      description='Magic functions to load boilerplate code',
+      url='https://github.com/pm8k/bpmagic',
+      download_url=download_url,
+
+      long_description=readme(),
+      author='Matthew Russell',
+      author_email='astromars42@gmail.com',
+      license='MIT',
+      keywords=['python', 'magic', 'jupyter',
+                'ipython', 'boiler', 'plate', 'bp'],
+      classifiers=['Framework :: IPython'],
+      py_modules=['bpmagic'],
+      install_requires=[
+          'ipython'
+      ],
+      include_package_data=True,
+
+      zip_safe=False)
