88119fbf8fbd723f9558c09e657238f00ea802f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:43 +0000

    Add package aiospider/0.0.4.6/aiospider-0.0.4.6.tar.gz

---
+++ packages/aiospider/aiospider-0.0.4.6.tar.gz/aiospider-0.0.4.6/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+        name='aiospider',
+        version='0.0.4.6',
+        description='A spider use asyncio.',
+        license='MIT License',
+        install_requires=['cchardet', 'aiodns', 'aiohttp', 'async-timeout', 'aioredis'],
+
+        author='EINDEX',
+        author_email='hi@eindex.me',
+
+        packages=find_packages(),
+        platforms='any',
+)
---
+++ packages/aiospider/aiospider-0.0.4.6.tar.gz/aiospider-0.0.4.6/tests/test_pytest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author        : EINDEX Li
+@File          : test_pytest.py
+@Created       : 26/12/2017
+"""
+
+
+def test_pytest():
+    print('OK')
+    assert True
