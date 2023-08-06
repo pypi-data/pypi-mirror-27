6ff1bd9081e36e99b2eb54fa41f38af1a99ed22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:21 +0000

    Add package aggregation_builder/0.0.3/aggregation_builder-0.0.3.tar.gz

---
+++ packages/aggregation_builder/aggregation_builder-0.0.3.tar.gz/aggregation_builder-0.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import subprocess
+import os
+
+# with open('requirements.txt', 'r') as f:
+#     required = f.read().splitlines()
+
+if 'dist' in os.listdir('.'):
+    subprocess.call(["rm", "-rf", "dist/"])
+if 'build' in os.listdir('.'):
+    subprocess.call(["rm", "-rf", "build/"])
+init = os.path.join(os.path.dirname(__file__), 'aggregation_builder', '__init__.py')
+version_line = list(filter(lambda l: l.startswith('__version__'), open(init)))[0]
+version = version_line.replace("__version__ = ", "").replace("'", "")
+
+setup(
+    name='aggregation_builder',
+    version=version,
+    packages=['aggregation_builder'],
+    license='LICENSE',
+    author='Moses Symeonidis',
+    author_email='msymewnidhs2113@yahoo.gr',
+    description='Package that implements a simple mongodb aggregation builder.',
+    install_requires=[
+        "pymongo >= 3.5.1",
+        "mongoengine >= 0.15.0"
+    ],
+    include_package_data=True,
+    url='https://github.com/MosesSymeonidis/aggregation_builder',
+    download_url='https://github.com/MosesSymeonidis/aggregation_builder/archive/master.tar.gz',
+    keywords=['mongodb', 'aggregation']
+)
