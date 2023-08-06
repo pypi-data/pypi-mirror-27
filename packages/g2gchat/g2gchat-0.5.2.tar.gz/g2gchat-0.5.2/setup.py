cf04c04e71ee7ac5b8914bf09a385bd1bf01a9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:52 +0000

    Add package g2gchat/0.5.2/g2gchat-0.5.2.tar.gz

---
+++ packages/g2gchat/g2gchat-0.5.2.tar.gz/g2gchat-0.5.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = 'g2gchat',
+    version = '0.5.2',
+    keywords=['g2gchat','chat','multichat'],
+    author='Srikanth,Lalith',
+    author_email='rs.chemikala@globaledgesoft.com',
+    description='A Messenger for globalites with features including file transfer, backup of chat sessions and more...',
+    packages = ['g2gchat'],
+    scripts = ['g2gchat/g2gchat.py','g2gchat/config_parser.py'],
+    #entry_points={   
+    #    'console_scripts': [
+    #        'g2gchat = g2gchat.py:main',
+    #    ],
+ 
+    #}
+)
