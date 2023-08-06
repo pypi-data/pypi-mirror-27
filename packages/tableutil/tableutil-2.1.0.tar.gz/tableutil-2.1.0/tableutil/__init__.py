60aa835e356a587ade435b1024e9eceb42bd6972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:22 +0000

    Add package tableutil/2.1.0/tableutil-2.1.0.tar.gz

---
+++ packages/tableutil/tableutil-2.1.0.tar.gz/tableutil-2.1.0/tableutil/__init__.py
@@ -0,0 +1,18 @@
+
+from .cell import (Cell,
+                   make_uri_cell)
+from .table import Table
+from .row import Row
+from .constants import (TableSeparators,
+                        Justify,
+                        CLASSES,
+                        HEADING,
+                        JUSTIFY,
+                        MAX,
+                        KEY,
+                        VALUE,
+                        CONVERT,
+                        PROPERTY,
+                        DUMMY,
+                        DUMMY_HEADINGS,
+                        KEY_VALUE_HEADINGS)
