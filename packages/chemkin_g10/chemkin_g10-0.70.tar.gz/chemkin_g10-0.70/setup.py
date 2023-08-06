02e3edab8857376661f50a367160afabcad3e52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:25 +0000

    Add package chemkin_g10/0.70/chemkin_g10-0.70.tar.gz

---
+++ packages/chemkin_g10/chemkin_g10-0.70.tar.gz/chemkin_g10-0.70/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='chemkin_g10',
+      version='0.70',
+      description='A chemical kinetics library',
+      url='https://github.com/CS207Team10/cs207-FinalProject',
+      author='Hidenori Tanaka, Jiachen Song, Xiangru Shu',
+      author_email="jsong1@g.harvard.edu",
+      license="MIT",
+      packages=['chemkin_g10'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'requests',          
+      ],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'pytest-cov'],
+      include_package_data=True, 
+      package_data={
+          'chemkin_g10': ["nasa.sqlite",],
+      }
+      )
