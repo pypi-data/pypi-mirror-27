7a7d0cd37af6da149e35d4377791384a1efa5785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:59 +0000

    Add package HerosRPG/1.1/HerosRPG-1.1.tar.gz

---
+++ packages/HerosRPG/HerosRPG-1.1.tar.gz/HerosRPG-1.1/Hero.py
@@ -0,0 +1,9 @@
+class Hero():
+    iCurrentScore = 0
+    iCurrentLevel = 1
+    iNextLevel = 10
+
+    def __init__(self):
+        self.iCurrentScore = 0
+        self.iCurrentLevel = 1
+        self.iNextLevel = 10
---
+++ packages/HerosRPG/HerosRPG-1.1.tar.gz/HerosRPG-1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HerosRPG',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='',
+    author='ianc',
+    author_email='ianc@slalom.com',
+    description='A simple console based  Role Playing Game in Python'
+)
