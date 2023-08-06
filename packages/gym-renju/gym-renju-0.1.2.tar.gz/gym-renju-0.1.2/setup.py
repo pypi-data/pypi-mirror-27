1650645c55c50bc1f69da42299be4416446fab0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:58 +0000

    Add package gym-renju/0.1.2/gym-renju-0.1.2.tar.gz

---
+++ packages/gym-renju/gym-renju-0.1.2.tar.gz/gym-renju-0.1.2/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="gym-renju",
+    version="0.1.2",
+    description="Renju Game Gym Environment",
+    license="MIT",
+    author="kkbnart",
+    packages=find_packages(),
+    keywords='gym renju reinforcement learning',
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ],
+    install_requires=['gym', 'pytest', 'parameterized', 'injector'],
+)
