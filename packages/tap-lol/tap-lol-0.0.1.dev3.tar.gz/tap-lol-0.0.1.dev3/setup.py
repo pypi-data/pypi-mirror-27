ccad4ab059973b6f4a20c6c0ab84388234456933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:15 +0000

    Add package tap-lol/0.0.1.dev3/tap-lol-0.0.1.dev3.tar.gz

---
+++ packages/tap-lol/tap-lol-0.0.1.dev3.tar.gz/tap-lol-0.0.1.dev3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='tap-lol',
+      version='0.0.1.dev3',
+      description='Singer.io tap for extracting data from the League of Legends API',
+      author='scirner22',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      install_requires=[
+          'singer-python==5.0.2',
+          'requests==2.12.4',
+          'pendulum==1.2.0',
+          'backoff==1.4.3',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-lol=tap_lol.tap_lol:main
+      ''',
+      packages=['tap_lol'],
+      package_data = {
+          'tap_lol': [
+              'schemas/*.json',
+          ],
+    },
+    include_package_data=True,
+)
