9e4bb5ad79b4693259dfd3ea1e5ced17b55a4d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:45 +0000

    Add package Flask-Builder/0.2/Flask-Builder-0.2.tar.gz

---
+++ packages/Flask-Builder/Flask-Builder-0.2.tar.gz/Flask-Builder-0.2/setup.py
@@ -0,0 +1,19 @@
+
+from distutils.core import setup
+
+setup(name='Flask-Builder',
+      version='0.2',
+      description='Flask-application factory',
+      author='Y-Bro',
+      url='https://github.com/n0nSmoker/flask_builder',
+      keywords=['flask', 'builder'],
+      packages=['flask_builder'],
+      install_requires=[
+        'flask',
+        'SQLAlchemy',
+        'Flask-SQLAlchemy',
+        'Flask-Migrate',
+        'raven',
+        'blinker',
+      ]
+     )
