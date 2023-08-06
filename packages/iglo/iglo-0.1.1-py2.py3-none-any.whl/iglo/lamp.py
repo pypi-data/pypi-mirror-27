68f4ee74d69ad7284d5f2dbf67c6c914ef017247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:53 +0000

    Add package iglo/0.1.1/iglo-0.1.1-py2.py3-none-any.whl

---
+++ packages/iglo/iglo-0.1.1-py2.py3-none-any.whl/iglo/lamp.py
@@ -0,0 +1,53 @@
+import socket
+from iglo.helpers import Helpers
+
+class Lamp(object):
+  """
+  This represents a lamp to control
+  """
+
+  def __init__(self, id, ip, port=8080):
+    """
+    :type id int:
+    :type ip string:
+    :type port int:
+    :return:
+    """
+    self._id = id
+    self._ip = ip
+    self._port = port
+
+  def switch(self, on):
+    on_data = 18
+    if on:
+      on_data = 17
+    data = [163, self._id, on_data]
+    self._send(data)
+
+  def white(self, whiteness):
+    data = [161, self._id, 255 - whiteness, whiteness]
+    self._send(data)
+
+  def brightness(self, brightness):
+    data = [167, self._id, brightness]
+    self._send(data)
+
+  def rgb(self, r, g, b):
+    data = [161, self._id, r, g, b]
+    self._send(data)
+
+  def _send(self, data):
+    data_bytes = bytearray(Helpers.int_array_to_byte_array([-2, -17, len(data) + 1]))
+    data_bytes.extend(Helpers.int_array_to_byte_array(data))
+    checksum = 0
+    for i in data:
+      checksum += i
+    data_bytes.append((checksum % 256) ^ 255)
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.connect((self._ip, self._port))
+    sock.send(data_bytes)
+    sock.close()
+    sock = None
+
+  def close(self):
+    self.socket.close()
