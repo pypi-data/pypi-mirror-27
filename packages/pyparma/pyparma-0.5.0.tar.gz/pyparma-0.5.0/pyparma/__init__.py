91fae7279fa208ac66234f5047fb6867cc45b0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:21 +0000

    Add package pyparma/0.5.0/pyparma-0.5.0.tar.gz

---
+++ packages/pyparma/pyparma-0.5.0.tar.gz/pyparma-0.5.0/pyparma/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from .ppl import *
+#Note: this import will override the standard Polyhedron
+#that is not instantiable anyway
+from .utils import Polyhedron
---
+++ packages/pyparma/pyparma-0.5.0.tar.gz/pyparma-0.5.0/pyparma/utils/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python2
+
+from __future__ import absolute_import
+
+from .representations import Polyhedron, fractionize,\
+                            intize, floatize, ex_from_line
