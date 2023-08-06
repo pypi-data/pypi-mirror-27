c2420898c7d6bbd61513b5e8ccae39f9f919b25e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:18 +0000

    Add package junc/0.1.0/junc-0.1.0.tar.gz

---
+++ packages/junc/junc-0.1.0.tar.gz/junc-0.1.0/junc.py
@@ -0,0 +1,59 @@
+"""
+Usage:
+    junc connect <name>
+    junc list
+    junc add (<name> <username> <ip>) [<location>]
+    junc remove <name>
+
+Options:
+    -h --help     Show this screen.
+    --version     Show version.
+
+Notes:
+    Data is stored in ~/.junc.json
+"""
+
+VERSION = "0.1.0"
+
+import os
+import sys
+
+from docopt import docopt
+
+from storage import Storage
+from server import *
+
+def cli(args):
+    storage = Storage()
+    server_list = storage.get_servers()
+    server_table = storage.get_server_table()
+
+    if args['list']:
+        print(server_table)
+
+    if args['add']:
+        server_list.append(new_server(args))
+        storage.write(server_list)
+
+    if args['connect']:
+        connection = ""
+        for server in server_list:
+            if server['name'] == args['<name>']:
+                connection = server['username'] + "@" + server['ip']
+        if not connection:
+            print("Couldn't find that server...")
+            sys.exit(1)
+        os.system('ssh ' + connection)
+
+    if args['remove']:
+        for i in range(len(server_list)):
+            if server_list[i]['name'] == args['<name>']:
+                del server_list[i]
+                break
+            if i == len(server_list) - 1:
+                print("Couldn't find that server...")
+        storage.write(server_list)
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version="Junc v" + VERSION)
+    cli(arguments)
---
+++ packages/junc/junc-0.1.0.tar.gz/junc-0.1.0/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+from junc import VERSION
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='junc',
+    version=VERSION,
+    description='Connect to servers easily',
+    long_description=long_description,
+    url='https://github.com/llamicron/junc',
+    author='Luke Sweeney',
+    author_email='llamicron@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='connect ip ssh pipe raspberry pi rpi raspberry-pi ec2 server',
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    py_modules=["junc", "storage", "server"],
+    #
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['docopt'],
+    extras_require={
+        'dev': ['twine'],
+        'test': ['coverage', 'pytest']
+    },
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'junc=junc:cli',
+        ],
+    },
+)
