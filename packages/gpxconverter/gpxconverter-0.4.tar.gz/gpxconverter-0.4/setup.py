372b2340a8e2d05d0c94d31f23951327101bdf78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:19 +0000

    Add package gpxconverter/0.4/gpxconverter-0.4.tar.gz

---
+++ packages/gpxconverter/gpxconverter-0.4.tar.gz/gpxconverter-0.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+setup(
+    name="gpxconverter",
+    version="0.4",
+
+    description="gpx to csv converter",
+
+    license="MIT",
+    url="https://github.com/linusyoung/GPXConverter",
+    download_url="""https://github.com/linusyoung/
+    GPXConverter/archive/0.4.tar.gz""",
+
+    author="Linus Yang",
+    author_email="linusyoungrice@gmail.com",
+
+    classifiers=[
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+
+    packages=["gpxconverter"],
+
+    entry_points={
+        "console_scripts": [
+            "gpxconverter = gpxconverter.__main__:main",
+        ],
+    },
+)
