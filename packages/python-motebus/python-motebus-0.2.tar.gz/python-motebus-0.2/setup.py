7fc9b9f9071a0bc5bca235a793efa08b314dafe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:48 +0000

    Add package python-motebus/0.2/python-motebus-0.2.tar.gz

---
+++ packages/python-motebus/python-motebus-0.2.tar.gz/python-motebus-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='python-motebus',
+      version='0.2',
+      description='peer to peer communication protocol',
+      url='https://github.com/motebus/python-motebus',
+      author='YPCloud',
+      author_email='info@ypcloud.com',
+      license='YPCloud',
+      packages=['Motebus'],
+      install_requires=[
+      ],
+      zip_safe=False)
