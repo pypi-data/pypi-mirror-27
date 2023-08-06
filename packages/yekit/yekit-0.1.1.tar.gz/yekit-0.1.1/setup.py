83c6ec4094b5a4694955950364569c36bc98d6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:58 +0000

    Add package yekit/0.1.1/yekit-0.1.1.tar.gz

---
+++ packages/yekit/yekit-0.1.1.tar.gz/yekit-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# encoding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+	name='yekit',
+	version='0.1.1',
+	description='a useful toolkit',
+	classifiers=[
+		'License :: OSI Approved :: BSD License',
+		'Programming Language :: Python :: 2.7',
+		'Intended Audience :: Developers',
+		'Operating System :: OS Independent',
+	],
+	keywords='tool kit',
+	author='ye',
+	url='http://www.xuye.site',
+	author_email='wmxy123@yeah.net',
+	license='BSD',
+	packages=find_packages(exclude=['']),  # 需要处理哪里packages，当然也可以手动填，例如['pip_setup', 'pip_setup.ext']
+	include_package_data=False,
+	zip_safe=True,
+	install_requires=[],
+)
