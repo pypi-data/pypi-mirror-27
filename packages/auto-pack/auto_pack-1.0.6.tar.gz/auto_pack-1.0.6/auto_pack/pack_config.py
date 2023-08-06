64f2499ec19e7085d185dff8ac26d98a8ff5fea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:03 +0000

    Add package auto-pack/1.0.6/auto_pack-1.0.6.tar.gz

---
+++ packages/auto-pack/auto_pack-1.0.6.tar.gz/auto_pack-1.0.6/auto_pack/pack_config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class AutoPackConfig(object):
+    """docstring for AutoPackConfig"""
+
+    def __init__(self, flavors):
+        super(AutoPackConfig, self).__init__()
+        self.flavors = flavors
+        self.suffix = ''
+        self.dirName = 'default'
+        self.useVersionSuffix = False
+
+    def setDirName(self, dirName):
+        self.dirName = dirName
+
+    def setSuffix(self, suffix):
+        self.suffix = suffix
+
+    def enableVersionSuffix(self, enable):
+        self.useVersionSuffix = enable
