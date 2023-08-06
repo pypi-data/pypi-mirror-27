471a7de37bf4531c6f99208d85913defa188886a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:02 +0000

    Add package ddupdate/0.0.1/ddupdate-0.0.1.tar.gz

---
+++ packages/ddupdate/ddupdate-0.0.1.tar.gz/ddupdate-0.0.1/setup.py
@@ -0,0 +1,45 @@
+''' ddupdate install data. '''
+
+import os
+from glob import glob
+from setuptools import setup
+
+if 'USER_INSTALL_FIX' in os.environ:
+    USER_SHARE = os.path.expanduser('~/.local/share/')
+    DATA = [
+        (os.path.join(USER_SHARE, 'plugins'), glob('plugins/*.py')),
+        (os.path.join(USER_SHARE, 'man/man8'), ['ddupdate.8']),
+        (os.path.expanduser('~/bin'), ['ddupdate'])
+    ]
+else:
+    DATA = [
+        ('share/ddupdate/plugins', glob('plugins/*.py')),
+        ('/etc', ['ddupdate.conf']),
+        ('/lib/systemd/system', glob('systemd/*')),
+        ('share/man/man8', ['ddupdate.8']),
+        ('share/doc/ddupdate', ['README.md', 'LICENSE.txt', 'ddupdate.8.html']),
+        ('share/ddupdate/dispatcher.d', ['dispatcher.d/50-ddupdate'])
+    ]
+
+setup(
+    name='ddupdate',
+    version='0.0.1',
+    description='Update dns data for dynamic ip addresses',
+    license='MIT',
+    url='http://github.com/leamas/ddupdate',
+    author='Alec Leamas',
+    author_email='alec.leamas@nowhere.net',
+    install_requires = ['straight.plugin'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Networking',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords = ['dyndns', 'dhcp', 'dns'],
+    py_modules=['ddupdate'],
+    scripts=['ddupdate'],
+    data_files=DATA
+)
