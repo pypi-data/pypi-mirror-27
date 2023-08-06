93388231cd381de868fc7b9dc6291c076fda163c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:36 +0000

    Add package neurascope/0.0.1/neurascope-0.0.1.tar.gz

---
+++ packages/neurascope/neurascope-0.0.1.tar.gz/neurascope-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='neurascope',
+      version='0.0.1',
+      description='The neural network doctor will see you now',
+      url='http://github.com/rkaplan/neurascope',
+      author='Russell Kaplan',
+      author_email='rjkaplan@cs.stanford.edu',
+      license='MIT',
+      packages=['neurascope'],
+      zip_safe=False)
+
