c0089fbd7ee3748a731b75dbe3ac29e537b5fbb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:17 +0000

    Add package hoggorm/0.11.1/hoggorm-0.11.1.tar.gz

---
+++ packages/hoggorm/hoggorm-0.11.1.tar.gz/hoggorm-0.11.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+
+How to generate new PyPI package
+Update version information
+Run tests
+
+Generate package files
+python setup.py sdist
+python setup.py bdist_wheel
+
+Uplad package files
+twine upload dist/*
+
+"""
+from setuptools import setup, find_packages
+
+
+setup()
