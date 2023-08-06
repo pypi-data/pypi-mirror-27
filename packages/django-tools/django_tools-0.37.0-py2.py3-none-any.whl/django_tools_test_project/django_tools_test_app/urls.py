7f39b85ac68badaaec5d33defa8db63057f1f72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:20 +0000

    Add package django-tools/0.37.0/django_tools-0.37.0-py2.py3-none-any.whl

---
+++ packages/django-tools/django_tools-0.37.0-py2.py3-none-any.whl/django_tools_test_project/django_tools_test_app/urls.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+"""
+    Dynamic SITE ID unittests
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyleft: 2012-2015 by the django-tools team, see AUTHORS for more details.
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+
+from django_tools_test_project.django_tools_test_app.views import display_site
+from django_tools_test_project.django_tools_test_app.views import get_current_get_parameters
+from django_tools_test_project.django_tools_test_app.views import raise_exception
+from django_tools_test_project.django_tools_test_app.views import TemplateDoesNotExists
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = (
+    url(r'^display_site/$', display_site),
+    url(r'^get_current_get_parameters/$', get_current_get_parameters),
+    url(r'^raise_exception/(?P<msg>.*?)/$', raise_exception),
+    url(r'^raise_template_not_exists/$', TemplateDoesNotExists.as_view()),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
