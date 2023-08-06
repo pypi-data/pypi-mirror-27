deaf0de7b65a6aac8c7f92216338ebefbb970d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:12 +0000

    Add package kinesis-python/0.1.4/kinesis-python-0.1.4.tar.gz

---
+++ packages/kinesis-python/kinesis-python-0.1.4.tar.gz/kinesis-python-0.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('VERSION') as version_fd:
+    version = version_fd.read().strip()
+
+install_requires = [
+    'boto3>=1.4.4,<2.0',
+    'offspring>=0.0.3,<0.9',
+]
+
+setup(
+    name='kinesis-python',
+    version=version,
+    install_requires=install_requires,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    author='Evan Borgstrom',
+    author_email='eborgstrom@nerdwallet.com',
+    license='Apache 2',
+    description='Low level, multiprocessing based AWS Kinesis producer & consumer library'
+)
