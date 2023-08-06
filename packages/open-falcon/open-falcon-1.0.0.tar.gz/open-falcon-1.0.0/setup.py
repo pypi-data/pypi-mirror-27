6ad509bb4eb496a1c9759738b48796b2f112686e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:46 +0000

    Add package open-falcon/1.0.0/open-falcon-1.0.0.tar.gz

---
+++ packages/open-falcon/open-falcon-1.0.0.tar.gz/open-falcon-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='open-falcon',
+        version='1.0.0',
+        keywords = ('open-falcon', 'falcon', 'open-falcon-api', 'falcon-api', 'xiaomi', 'xiaomi falcon'),
+        description='open-falcon sdk for python',
+        license='Free',
+        author='dengyun',
+        author_email='hwlinux@163.com',
+        url='http://https://github.com/d-anz//',
+        platforms = 'any',
+        packages = ["open-falcon"],
+)
