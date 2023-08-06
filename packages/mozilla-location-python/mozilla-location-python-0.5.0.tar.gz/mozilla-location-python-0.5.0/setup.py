10a2b73ef2b06b360adbaa680c2c6b14b5545cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:58 +0000

    Add package mozilla-location-python/0.5.0/mozilla-location-python-0.5.0.tar.gz

---
+++ packages/mozilla-location-python/mozilla-location-python-0.5.0.tar.gz/mozilla-location-python-0.5.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+install_requires = ['pandas','requests']
+tests_require = ['nose','coveralls']
+# %%
+from setuptools import setup,find_packages
+
+setup(name='mozilla-location-python',
+      packages=find_packages(),
+      author='Michael Hirsch, Ph.D.',
+      version='0.5.0',
+      url='https://github.com/scivision/mozilla-location-python',
+      long_description=open('README.md').read(),
+      description='Using Mozilla Location services, log location vs. time using WiFi or convert to KML.',
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require={'tests':tests_require,
+                      'kml':['simplekml']},
+      python_requires='>=3.6',
+	  )
+
