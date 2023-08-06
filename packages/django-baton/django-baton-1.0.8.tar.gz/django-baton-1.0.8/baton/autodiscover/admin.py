6a0e14c93e1b90cb5fe92b376f9fd4b1c6f48ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:46 +0000

    Add package django-baton/1.0.8/django-baton-1.0.8.tar.gz

---
+++ packages/django-baton/django-baton-1.0.8.tar.gz/django-baton-1.0.8/baton/autodiscover/admin.py
@@ -0,0 +1,20 @@
+from django.contrib.admin import AdminSite, site
+
+from ..config import get_config
+
+
+class BatonAdminSite(AdminSite):
+    site_header = get_config('SITE_HEADER')
+    site_title = get_config('SITE_TITLE')
+    index_title = get_config('INDEX_TITLE')
+    index_template = 'baton/index.html' if get_config('ANALYTICS') else 'admin/index.html' # noqa
+
+    def __init__(self, *args, **kwargs):
+        """ Registers all apps with BatonAdminSite """
+        super(BatonAdminSite, self).__init__(*args, **kwargs)
+        self._registry.update(site._registry)
+        for model in site._registry:
+            self.unregister([model])
+            self.register([model], type(site._registry[model]))
+
+site = BatonAdminSite() # noqa
