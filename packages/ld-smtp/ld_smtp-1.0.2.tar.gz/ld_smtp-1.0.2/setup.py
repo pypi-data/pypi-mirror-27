3fe6165c45886b471f81f04ce0e7c62c793b7dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:58 +0000

    Add package ld-smtp/1.0.2/ld_smtp-1.0.2.tar.gz

---
+++ packages/ld-smtp/ld_smtp-1.0.2.tar.gz/ld_smtp-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from pip.req import parse_requirements
+
+
+setup (
+    name            = 'ld_smtp',
+    version         = '1.0.2',
+    py_modules      = ['SMTPModule'],
+    url             = 'https://github.com/4linux/ld_smtp',
+    install_requires= ['Jinja2','PyYAML'],
+    author          = 'Mariana Albano',
+    author_email    = 'mariana.albano@4linux.com.br',
+    description     = 'Este modulo ira realizar o envio de e-mail utilizando o smtplib.'
+)
