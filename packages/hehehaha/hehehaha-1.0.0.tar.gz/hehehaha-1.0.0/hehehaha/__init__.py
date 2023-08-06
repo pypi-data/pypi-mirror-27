49044b9be099fe686377175d315564caa0893f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:56 +0000

    Add package hehehaha/1.0.0/hehehaha-1.0.0.tar.gz

---
+++ packages/hehehaha/hehehaha-1.0.0.tar.gz/hehehaha-1.0.0/hehehaha/__init__.py
@@ -0,0 +1,6 @@
+from .print_cwd import *
+from .print_now import *
+
+__version__ = '1.0.0'
+
+__all_ = ['print_cwd', 'print_now']
---
+++ packages/hehehaha/hehehaha-1.0.0.tar.gz/hehehaha-1.0.0/setup.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hehehaha',
+    version='1.0.0',
+    description=(
+        '我用来学习测试的'
+    ),
+    long_description=open('README.md').read(),
+    author='CuiWeiming',
+    author_email='cuiweiming1110@163.com',
+    maintainer='CuiWeiming',
+    maintainer_email='cuiweiming1110@163.com',
+    license='GNU General Public License v3 (GPLv3)',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/XIHEHA/hehehaha',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
