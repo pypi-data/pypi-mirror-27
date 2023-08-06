a63867dddb261ae912db047faa35e8f82e4377cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:37 +0000

    Add package regulus-wagtail-extensions/0.1/regulus_wagtail_extensions-0.1-py2.py3-none-any.whl

---
+++ packages/regulus-wagtail-extensions/regulus_wagtail_extensions-0.1-py2.py3-none-any.whl/wagtail_extensions/blocks.py
@@ -0,0 +1,263 @@
+import calendar
+from collections import defaultdict
+import datetime
+from itertools import groupby
+import math
+import uuid
+
+from dateutils import relativedelta
+from django.core.exceptions import ValidationError
+from django.forms.utils import ErrorList
+from phonenumber_field import phonenumber
+from phonenumber_field.formfields import PhoneNumberField
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtailgeowidget.blocks import GeoBlock
+
+from . import app_settings
+
+
+class StrippedListBlock(blocks.ListBlock):
+    """
+    Does not save empty values
+    """
+    def get_prep_value(self, value):
+        return [
+            self.child_block.get_prep_value(item)
+            for item in value if item != ''
+        ]
+
+
+class LinkBlock(blocks.StructBlock):
+    text = blocks.CharBlock(required=False)
+    page = blocks.PageChooserBlock(required=False)
+    absolute_url = blocks.CharBlock(label="Url", required=False)
+
+    class Meta:
+        template = 'wagtail_extensions/blocks/link.html'
+
+    def clean(self, value):
+        if value.get('page') and value.get('absolute_url'):
+            errors = {
+                'page': ErrorList([
+                    ValidationError('Either a page or url must be defined'),
+                ]),
+            }
+            raise ValidationError('There is a problem with this link', params=errors)
+
+        return super().clean(value)
+
+    def to_python(self, value):
+        value = super().to_python(value)
+        if value.get('page'):
+            value['url'] = value['page'].url
+        elif value.get('absolute_url'):
+            value['url'] = value.get('absolute_url')
+        return value
+
+    def get_context(self, value, parent_context=None):
+        ctx = super().get_context(value, parent_context=parent_context)
+        ctx['has_url'] = 'url' in value
+        return ctx
+
+
+class CarouselItemBlock(blocks.StructBlock):
+
+    image = ImageChooserBlock()
+    caption = blocks.CharBlock(required=False)
+    link = LinkBlock(required=False)
+
+    class Meta:
+        template = 'wagtail_extensions/blocks/carousel_item.html'
+
+
+class CarouselBlock(blocks.StructBlock):
+
+    items = blocks.ListBlock(CarouselItemBlock())
+
+    class Meta:
+        template = 'wagtail_extensions/blocks/carousel.html'
+
+    def get_context(self, value, parent_context=None):
+        ctx = super().get_context(value, parent_context=parent_context)
+        ctx['dom_id'] = 'carousel-'+uuid.uuid4().hex
+        ctx['show_indicators'] = len(value.get('items', [])) > 1
+        return ctx
+
+
+class AddressBlock(blocks.StructBlock):
+
+    lines = blocks.ListBlock(blocks.CharBlock(label="Line", required=False))
+
+    class Meta:
+        template = 'wagtail_extensions/blocks/address.html'
+
+
+class PhoneBlock(blocks.FieldBlock):
+
+    def __init__(self, required=True, help_text=None, **kwargs):
+        self.field = PhoneNumberField(required=required, help_text=help_text)
+        super().__init__(**kwargs)
+
+    def get_prep_value(self, value):
+        return str(value)
+
+    def to_python(self, value):
+        return phonenumber.to_python(value)
+
+
+class DepartmentBlock(blocks.StructBlock):
+
+    name = blocks.CharBlock(required=False)
+    phones = StrippedListBlock(PhoneBlock(required=False))
+    email = blocks.EmailBlock(required=False)
+    primary = blocks.BooleanBlock(required=False, default=False)
+
+    def clean(self, value):
+        phones = value.get('phones', [])
+        email = value.get('email')
+        if not phones and not email:
+            errors = {
+                'phone': ErrorList([
+                    ValidationError('Either a phone or email must be defined'),
+                ]),
+            }
+            raise ValidationError("There is a problem with this department", params=errors)
+        return super().clean(value)
+
+
+class OpeningTimeBlock(blocks.StructBlock):
+    """
+    A semi-structured opening times block.
+
+    Period is left as a free text input to allow for
+    """
+    PUBLIC = 7
+    PUBLIC_LABEL = 'Public holiday'
+    WEEKDAYS = tuple(enumerate(calendar.day_name))
+    ALL_DAYS = WEEKDAYS + ((PUBLIC, PUBLIC_LABEL),)
+
+    weekday = blocks.ChoiceBlock(choices=ALL_DAYS, required=False)
+    label = blocks.CharBlock(help_text='Optionally override default weekday label', required=False)
+    date = blocks.DateBlock(help_text='Optionally specify a day these times are for', required=False)
+    start = blocks.TimeBlock(required=False)
+    end = blocks.TimeBlock(required=False)
+    closed = blocks.BooleanBlock(default=False, required=False)
+
+    class Meta:
+        template = 'wagtail_extensions/blocks/openingtime.html'
+
+    def clean(self, value):
+        cleaned = super().clean(value)
+        start, end, weekday, date = map(cleaned.get, ['start', 'end', 'weekday', 'date'])
+        errors = defaultdict(ErrorList)
+
+        if date and date < datetime.date.today():
+            err = ValidationError('Dates need to be in the future')
+            errors['date'].append(err)
+
+        if (start or end) and not (weekday or date):
+            err = ValidationError('Specifying a weekday or date is required')
+            errors['weekday'].append(err)
+
+        if start and end and end <= start:
+            err = ValidationError('End must be after start')
+            errors['end'].append(err)
+
+        if errors:
+            raise ValidationError("There is a problem with this opening time", params=errors)
+
+        return cleaned
+
+    def to_python(self, value):
+        value = super().to_python(value)
+        weekday = value.get('weekday')
+        if weekday is not None:
+            weekday_int = int(weekday)
+            value['weekday'] = weekday_int
+
+            label = value.get('label')
+            if weekday_int == self.PUBLIC and not label:
+                value['label'] = self.PUBLIC_LABEL
+
+            if weekday_int == self.PUBLIC or value.get('date'):
+                value['specific'] = True
+            else:
+                # Next date with this weekday
+                today = datetime.date.today()
+                value['next_date'] = today + relativedelta(weekday=weekday_int)
+
+        if value.get('date'):
+            value['specific'] = True
+        return value
+
+
+class OpeningTimesBlock(blocks.StructBlock):
+    """
+    Using a StructBlock as subclassing ListBlock leads to problems when
+    Wagtail does template rendering.
+    """
+    times = blocks.ListBlock(OpeningTimeBlock(required=False))
+
+    class Meta:
+        template = 'wagtail_extensions/blocks/opening_times.html'
+
+    @staticmethod
+    def time_keyfunc(opening_time):
+        if opening_time.get('specific'):
+            return opening_time
+        else:
+            return (
+                opening_time.get('start'),
+                opening_time.get('end'),
+            )
+
+    @classmethod
+    def group_times(cls, value):
+        # Read out groups to simplify templates
+        return [list(group) for key, group in groupby(value, cls.time_keyfunc)]
+
+    def get_context(self, value, parent_context=None):
+        ctx = super().get_context(value, parent_context=parent_context)
+        ctx['times'] = self.group_times(value.get('times'))
+        return ctx
+
+
+class LocationBlock(blocks.StructBlock):
+    name = blocks.CharBlock()
+    address = AddressBlock(required=False)
+    point = GeoBlock(required=False)
+    departments = blocks.ListBlock(DepartmentBlock(label="department", required=False))
+    opening_times = OpeningTimesBlock(required=False)
+    primary = blocks.BooleanBlock(default=False, required=False)
+
+
+class SocialMediaProfileBlock(blocks.StructBlock):
+
+    icon = blocks.ChoiceBlock(choices=app_settings.SOCIAL_MEDIA_TYPES)
+    url = blocks.URLBlock()
+
+    class Meta:
+        template = 'wagtail_extensions/blocks/social_media_profile.html'
+
+
+class TextBlock(blocks.StructBlock):
+
+    title = blocks.CharBlock(required=False)
+    body = blocks.RichTextBlock()
+
+    class Meta:
+        template = 'wagtail_extensions/blocks/text.html'
+
+
+class ImagesBlock(blocks.StructBlock):
+
+    images = blocks.ListBlock(ImageChooserBlock(required=False))
+
+    class Meta:
+        template = 'wagtail_extensions/blocks/images.html'
+
+    def get_context(self, value, parent_context=None):
+        ctx = super().get_context(value, parent_context=parent_context)
+        ctx['column_width'] = math.floor(12 / len(value.get('images', [])))
+        return ctx
---
+++ packages/regulus-wagtail-extensions/regulus_wagtail_extensions-0.1-py2.py3-none-any.whl/wagtail_extensions/models.py
@@ -0,0 +1,128 @@
+from datetime import date
+
+from django.core.cache import cache
+from django.db import models
+from wagtail.contrib.settings.models import BaseSetting
+from wagtail.contrib.table_block.blocks import TableBlock
+from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
+from wagtail.wagtailcore import blocks, fields
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+
+from . import blocks as extension_blocks
+from . import utils
+
+
+class ContentPage(Page):
+
+    class Meta:
+        abstract = True
+
+    featured_image = models.ForeignKey(
+        'wagtailimages.Image',
+        models.PROTECT,
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+    body = fields.StreamField([
+        ('text', extension_blocks.TextBlock()),
+        ('table', TableBlock()),
+        ('images', extension_blocks.ImagesBlock()),
+        ('carousel', extension_blocks.CarouselBlock()),
+    ], blank=True)
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('featured_image'),
+        StreamFieldPanel('body'),
+    ]
+
+
+class LinksSetting(BaseSetting):
+
+    class Meta:
+        abstract = True
+
+    links = fields.StreamField([
+        ('link', extension_blocks.LinkBlock()),
+    ])
+
+    panels = (
+        StreamFieldPanel('links'),
+    )
+
+
+class ContactDetailsSetting(BaseSetting):
+
+    CACHE_KEY_OPENING_TODAY = "wagtail_extensions_opening_today_{:%Y%m%d}"
+
+    locations = fields.StreamField([
+        ('location', extension_blocks.LocationBlock()),
+    ])
+
+    panels = (
+        StreamFieldPanel('locations'),
+    )
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def get_opening_today_cache_key(cls, date):
+        return cls.CACHE_KEY_OPENING_TODAY.format(date)
+
+    @property
+    def primary_location(self):
+        return utils.true_or_nth(self.locations, lambda x: x.value.get('primary') == True)
+
+    @property
+    def primary_department(self):
+        location = self.primary_location
+        if location:
+            departments = location.value.get('departments', [])
+            return utils.true_or_nth(departments, lambda x: x.get('primary') == True)
+        return None
+
+    @property
+    def primary_phone(self):
+        department = self.primary_department
+        if department:
+            phones = self.primary_department.get('phones', [])
+            return utils.nth(phones, 0)
+        return None
+
+    @property
+    def primary_opening_times(self):
+        location = self.primary_location
+        if location:
+            return location.value.get('opening_times')
+        return None
+
+    @property
+    def primary_opening_today(self):
+        today = date.today()
+        cache_key = self.get_opening_today_cache_key(today)
+        times = cache.get(cache_key)
+        if times is None:
+            opening_times = self.primary_opening_times
+            if opening_times:
+                times = opening_times.get('times')
+                specific_times = utils.first_true(times, lambda x: x.get('date') == today)
+                times = specific_times or utils.first_true(times, lambda x: x.get('weekday') == today.weekday())
+                if times:
+                    cache.set(cache_key, dict(times), 60*60*24)
+        return times
+
+
+class SocialMediaSetting(BaseSetting):
+
+    class Meta:
+        abstract = True
+
+    profiles = fields.StreamField([
+        ('profile', extension_blocks.SocialMediaProfileBlock()),
+    ])
+
+    panels = (
+        StreamFieldPanel('profiles'),
+    )
