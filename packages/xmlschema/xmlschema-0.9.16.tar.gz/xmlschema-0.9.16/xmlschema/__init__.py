e6b05296c2cef9bdd454e803a0fe05b3c27ca94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:28 +0000

    Add package xmlschema/0.9.16/xmlschema-0.9.16.tar.gz

---
+++ packages/xmlschema/xmlschema-0.9.16.tar.gz/xmlschema-0.9.16/xmlschema/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c), 2016-2017, SISSA (International School for Advanced Studies).
+# All rights reserved.
+# This file is distributed under the terms of the MIT License.
+# See the file 'LICENSE' in the root directory of the present
+# distribution, or http://opensource.org/licenses/MIT.
+#
+# @author Davide Brunato <brunato@sissa.it>
+#
+from .exceptions import XMLSchemaException, XMLSchemaXPathError, XMLSchemaRegexError, XMLSchemaURLError
+from .etree import etree_get_namespaces
+from .resources import fetch_resource, load_xml_resource, fetch_schema
+from .converters import (
+    XMLSchemaConverter, ParkerConverter, BadgerFishConverter, AbderaConverter, JsonMLConverter
+)
+
+from .validators.exceptions import (
+    XMLSchemaParseError, XMLSchemaValidationError, XMLSchemaDecodeError,
+    XMLSchemaEncodeError, XMLSchemaNotBuiltError, XMLSchemaChildrenValidationError
+)
+from .validators.schema import XsdGlobals, XMLSchema, create_validator
+
+__version__ = '0.9.16'
+__author__ = "Davide Brunato"
+__contact__ = "brunato@sissa.it"
+__copyright__ = "Copyright 2016-2017, SISSA"
+__license__ = "MIT"
+__status__ = "Production/Stable"
+
+
+def validate(xml_document, schema=None, cls=XMLSchema, use_defaults=True):
+    if schema is None:
+        schema = fetch_schema(xml_document)
+    cls(schema, validation='strict').validate(xml_document, use_defaults)
+
+
+def to_dict(xml_document, schema=None, cls=XMLSchema, path=None, process_namespaces=True, **kwargs):
+    if schema is None:
+        schema = fetch_schema(xml_document)
+    return cls(schema, validation='strict').to_dict(
+        xml_document, path=path, process_namespaces=process_namespaces, **kwargs
+    )
---
+++ packages/xmlschema/xmlschema-0.9.16.tar.gz/xmlschema-0.9.16/xmlschema/tests/test_schemas.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c), 2016-2017, SISSA (International School for Advanced Studies).
+# All rights reserved.
+# This file is distributed under the terms of the MIT License.
+# See the file 'LICENSE' in the root directory of the present
+# distribution, or http://opensource.org/licenses/MIT.
+#
+# @author Davide Brunato <brunato@sissa.it>
+#
+"""
+This module runs tests concerning the building of XSD schemas with the 'xmlschema' package.
+"""
+import unittest
+import os
+import sys
+
+from xmlschema import XMLSchemaParseError, XMLSchemaURLError
+from _test_common import tests_factory, SchemaObserver
+
+
+def make_test_schema_function(xsd_file, schema_class, expected_errors=0, inspect=False):
+    def test_schema(self):
+        if inspect:
+            SchemaObserver.clear()
+        # print("Run %s" % self.id())
+        try:
+            if expected_errors > 0:
+                xs = schema_class(xsd_file, validation='lax')
+            else:
+                xs = schema_class(xsd_file)
+        except (XMLSchemaParseError, XMLSchemaURLError, KeyError) as err:
+            num_errors = 1
+            errors = [str(err)]
+        else:
+            num_errors = len(xs.all_errors)
+            errors = xs.all_errors
+
+            if inspect:
+                components_ids = set([id(c) for c in xs.iter_components()])
+                missing = [c for c in SchemaObserver.components if id(c) not in components_ids]
+                if any([c for c in missing]):
+                    raise ValueError("schema missing %d components: %r" % (len(missing), missing))
+
+        if num_errors != expected_errors:
+            print("\nTest n.%r: %r errors, %r expected." % (self.id()[-3:], num_errors, expected_errors))
+            if num_errors == 0:
+                raise ValueError("found no errors when %d expected." % expected_errors)
+            else:
+                raise ValueError(
+                    "n.%d errors expected, found %d: %s" % (expected_errors, num_errors, errors[0])
+                )
+        else:
+            self.assertTrue(True, "Successfully created schema for {}".format(xsd_file))
+
+    return test_schema
+
+
+if __name__ == '__main__':
+    pkg_folder = os.path.dirname(os.getcwd())
+    sys.path.insert(0, pkg_folder)
+    path = os.path.join(pkg_folder, "tests/*/testfiles")
+    schema_tests = tests_factory(make_test_schema_function, path, label='schema', suffix='xsd')
+    globals().update(schema_tests)
+    unittest.main()
