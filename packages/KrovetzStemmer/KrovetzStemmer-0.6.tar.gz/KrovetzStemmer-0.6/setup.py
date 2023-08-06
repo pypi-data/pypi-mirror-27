f9016feaa81df2f16e6689d79f846697bec0b523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:47 +0000

    Add package KrovetzStemmer/0.6/KrovetzStemmer-0.6.tar.gz

---
+++ packages/KrovetzStemmer/KrovetzStemmer-0.6.tar.gz/KrovetzStemmer-0.6/setup.py
@@ -0,0 +1,22 @@
+from distutils.sysconfig import get_config_vars
+from distutils.core import setup, Extension
+
+cfg_vars = get_config_vars()
+for key, value in cfg_vars.items():
+    if type(value) == str:
+        cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
+
+setup(
+    name="KrovetzStemmer",
+    version="0.6",
+    description="Python binding to the C++ KrovetzStemmer package",
+    author="Ruey-Cheng Chen",
+    author_email="rueycheng@gmail.com",
+    url="https://github.com/rmit-ir/KrovetzStemmer",
+    ext_modules=[
+        Extension(
+            "krovetzstemmer",
+            sources=["krovetzstemmer_module.cpp", "KrovetzStemmer.cpp"]
+        )
+    ]
+)
