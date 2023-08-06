c4bf3cd34661bfe94df4c1fa76b9acd6a95e57f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:00 +0000

    Add package dops/0.0.1.12/dops-0.0.1.12.tar.gz

---
+++ packages/dops/dops-0.0.1.12.tar.gz/dops-0.0.1.12/dops/version.py
@@ -0,0 +1,7 @@
+VERSION_INFO = (
+                ("program","dops"),
+                ("version","dev-0.0.1"),
+                ("pypi","0.0.1.12"),
+                ("commit","12.g91fe1c2"),
+                ("buildTime","2017-12-27 15:56:26"),
+)
