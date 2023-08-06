6195d21a702e47328cc3fd0b59400ad263fd0ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:39 +0000

    Add package abilian-sbe/0.4.1/abilian-sbe-0.4.1.tar.gz

---
+++ packages/abilian-sbe/abilian-sbe-0.4.1.tar.gz/abilian-sbe-0.4.1/abilian/sbe/apps/social/views/util.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, print_function
+
+
+class Env(dict):
+    """
+    A dictionnary whose objects can be accessed by '.' notation, saving a few
+    keystrokes.
+    """
+
+    def __init__(self, label=None, **kw):
+        # self.bread_crumbs = self.breadcrumbs = make_bread_crumbs(label=label)
+        for key, value in kw.items():
+            self[key] = value
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __getattr__(self, key):
+        return self[key]
