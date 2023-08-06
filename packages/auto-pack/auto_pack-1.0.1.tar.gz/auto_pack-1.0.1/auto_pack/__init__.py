550ec94a7b17bfa36dd7e036c398da49a8d0365a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:38 +0000

    Add package auto-pack/1.0.1/auto_pack-1.0.1.tar.gz

---
+++ packages/auto-pack/auto_pack-1.0.1.tar.gz/auto_pack-1.0.1/auto_pack/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import android_utils
+__version__ = '1.0.1'
+
+
+def getApkInfo(apkPath):
+    apkinfo = android_utils.getApkInfo(apkPath)
+    print(apkinfo.versionCode, apkinfo.versionName)
