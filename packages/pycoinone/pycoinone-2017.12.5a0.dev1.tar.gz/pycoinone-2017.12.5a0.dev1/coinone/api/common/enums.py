2620dc33fb8c5077266d1a557f6a84f110ae993c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:15 +0000

    Add package pycoinone/2017.12.5a0.dev1/pycoinone-2017.12.5a0.dev1.tar.gz

---
+++ packages/pycoinone/pycoinone-2017.12.5a0.dev1.tar.gz/pycoinone-2017.12.5a0.dev1/coinone/api/common/enums.py
@@ -0,0 +1,21 @@
+import enum
+
+
+class Currency(str, enum.Enum):
+    BTC = 'btc'
+    BCH = 'bch'
+    ETH = 'eth'
+    ETC = 'etc'
+    XRP = 'xrp'
+    QTUM = 'qtum'
+    IOTA = 'iota'
+    LTC = 'ltc'
+
+    KRW = 'krw'
+
+
+class OrderType(int, enum.Enum):
+    Ask = 1
+    Bid = 0
+    Sell = 1
+    Buy = 0
