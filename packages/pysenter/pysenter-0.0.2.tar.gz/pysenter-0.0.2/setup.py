1ba1930f30c767bc4a2c3e24dd6627358799ddf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:20 +0000

    Add package pysenter/0.0.2/pysenter-0.0.2.tar.gz

---
+++ packages/pysenter/pysenter-0.0.2.tar.gz/pysenter-0.0.2/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+  name = 'pysenter',
+  packages = ['pysenter'],
+  version = '0.0.2',
+  description = 'A Python package for creating slides using YAML files',
+  author = 'Eric Magalhães',
+  author_email = 'ericovis@gmail.com',
+  url = 'https://github.com/ericovis/pysenter',
+  download_url = 'https://github.com/ericovis/pysenter/archive/0.0.2.tar.gz',
+  keywords = ['revealjs', 'yaml', 'presentation', 'slide'],
+  classifiers = [],
+)
