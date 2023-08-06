db78f5ca6830ae090273fb947dbb705a811831e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:40 +0000

    Add package pyravendb/4.0.1.7/pyravendb-4.0.1.7.tar.gz

---
+++ packages/pyravendb/pyravendb-4.0.1.7.tar.gz/pyravendb-4.0.1.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyravendb',
+    packages=find_packages(),
+    version='4.0.1.7',
+    description='This is a beta version to the official python client for RavenDB v4.0 document database',
+    author='Idan Haim Shalom',
+    author_email='haimdude@gmail.com',
+    url='https://github.com/ravendb/RavenDB-Python-Client',
+    license='MIT',
+    keywords='pyravendb',
+    install_requires=
+    [
+        'requests >= 2.18.2',
+        'inflector >= 2.0.11',
+        'xxhash >= 1.0.1',
+        'pyOpenSSL >= 17.2.0',
+        'ijson >= 2.3'
+    ],
+    zip_safe=False
+)
