3d11d15975bfaad6ff317b443823522875cfb245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:06 +0000

    Add package LIMBR/0.2/LIMBR-0.2.tar.gz

---
+++ packages/LIMBR/LIMBR-0.2.tar.gz/LIMBR-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='LIMBR',
+      version='0.2',
+      description='Learning and Imputation for Mass-spec Bias Reduction',
+      url='https://github.com/aleccrowell/LIMBR',
+      download_url='https://github.com/aleccrowell/LIMBR/releases/tag/0.1',
+      author='Alec Crowell',
+      author_email='alexander.m.crowell@gmail.com',
+      license='BSD-3',
+      keywords=['SVA','SVD','mass-spec','bioinformatics'],
+      install_requires=['numpy','pandas','scipy','sklearn','statsmodels','tqdm','pickle','multiprocess','functools'],
+      zip_safe=False)
