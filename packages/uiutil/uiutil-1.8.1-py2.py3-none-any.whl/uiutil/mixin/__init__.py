cbac757355f376610dad0d94ee9e9449031fa1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:24 +0000

    Add package uiutil/1.8.1/uiutil-1.8.1-py2.py3-none-any.whl

---
+++ packages/uiutil/uiutil-1.8.1-py2.py3-none-any.whl/uiutil/mixin/__init__.py
@@ -0,0 +1,8 @@
+
+from all import AllMixIn
+from var import VarMixIn
+from poll import PollMixIn
+from widget import WidgetMixIn
+from classutils.observer import ObserverMixIn
+
+__all__ = [AllMixIn, VarMixIn, PollMixIn, WidgetMixIn, ObserverMixIn]
