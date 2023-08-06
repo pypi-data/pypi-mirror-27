fd39ba50b7adb45107cd3f157d3f4cd3d222d653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:00 +0000

    Add package rdial/0.17.0/rdial-0.17.0-py3-none-any.whl

---
+++ packages/rdial/rdial-0.17.0-py3-none-any.whl/rdial/_version.py
@@ -0,0 +1,10 @@
+# This is rdial version 0.17.0 (2017-12-14)
+# pylint: skip-file
+
+dotted = '0.17.0'
+libtool = '17:20'
+hex = 0x001100
+date = '2017-12-14'
+tuple = (0, 17, 0)
+dict = {'major': 0, 'minor': 17, 'micro': 0}
+web = 'rdial/0.17.0'
