f44db292e74a22f371dde032f75d2dee18762b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:46 +0000

    Add package trainerdex/1.4.2.1/trainerdex-1.4.2.1.tar.gz

---
+++ packages/trainerdex/trainerdex-1.4.2.1.tar.gz/trainerdex-1.4.2.1/trainerdex/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+Official TrainerDex API Wrapper
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the TrianerDex API.
+
+:copyright: (c) 2017 JayTurnr
+:licence: GNU-GPL3, see LICENSE for more details
+
+"""
+
+__title__ = 'trainerdex'
+__author__ = 'JayTurnr'
+__licence__ = 'GNU-GPL'
+__copyright__ = 'Copyright 2017 JayTurnr'
+__version__ = '1.4.2.1'
+
+from .client import Client
+from .trainer import Trainer
+from .utils import Level, Team
+from .update import Update
+from .cached import DiscordUser, DiscordServer, refresh_discord
+from .user import User
