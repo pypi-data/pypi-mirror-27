448e72688e4f803091c60dbe451603bd68084300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:40 +0000

    Add package nbzip/0.0.2/nbzip-0.0.2.tar.gz

---
+++ packages/nbzip/nbzip-0.0.2.tar.gz/nbzip-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="nbzip",
+    version='0.0.2',
+    url="https://github.com/data-8/nbzip",
+    author="Data 8 @ UC Berkeley",
+    author_email="peterkangveerman@berkeley.edu",
+    description="Zips and downloads all the contents of a jupyter notebook.",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'notebook', 'pytest'
+    ],
+    package_data={'nbzip': ['static/*']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
+
