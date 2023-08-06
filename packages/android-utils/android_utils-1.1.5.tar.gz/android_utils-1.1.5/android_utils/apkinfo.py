ac7677f8578f242ac1dbda8b1da5c8a644ec9e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:30 +0000

    Add package android-utils/1.1.5/android_utils-1.1.5.tar.gz

---
+++ packages/android-utils/android_utils-1.1.5.tar.gz/android_utils-1.1.5/android_utils/apkinfo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+
+
+class ApkInfo:
+
+    def __init__(self, apkpath):
+        output = os.popen("aapt d badging %s" % apkpath).read()
+        match = re.compile(
+            "package: name='(\S+)' versionCode='(\d+)' versionName='(\S+)'").match(output)
+        if not match:
+            print("\n无法获取apk信息，请检查aapt环境变量，参考如下:")
+            print("1.命令行终端输入：vi .bash_profile")
+            print("2.添加环境变量配置，正确的appt环境变量大致如下：\nexport AAPT_HOME=/Users/mengweiping/Library/Android/sdk/build-tools/25.0.2")
+            print("export export PATH=$PATH:$AAPT_HOME/\n")
+            self.packageName = ""
+            self.versionCode = ""
+            self.versionName = ""
+        else:
+            self.packageName = match.group(1)
+            self.versionCode = match.group(2)
+            self.versionName = match.group(3)
