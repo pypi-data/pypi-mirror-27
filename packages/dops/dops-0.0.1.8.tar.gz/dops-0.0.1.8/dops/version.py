087d91d39a9aaf02acc4c111aaae2c6883f0188b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:57 +0000

    Add package dops/0.0.1.8/dops-0.0.1.8.tar.gz

---
+++ packages/dops/dops-0.0.1.8.tar.gz/dops-0.0.1.8/dops/version.py
@@ -0,0 +1,7 @@
+VERSION_INFO = (
+                ("program","dops"),
+                ("version","dev-0.0.1"),
+                ("pypi","0.0.1.8"),
+                ("commit","8.gd91c676"),
+                ("buildTime","2017-12-26 23时36分31秒"),
+)
