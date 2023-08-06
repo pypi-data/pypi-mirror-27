c79d92df5d990678e1ce0ef131996cb1e944c254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:40 +0000

    Add package iota-auth/0.1/iota_auth-0.1.tar.gz

---
+++ packages/iota-auth/iota_auth-0.1.tar.gz/iota_auth-0.1/iota_auth/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+
+from project.iota_auth import views
+from .views import (
+	LoginView, 
+	SignupView,
+	LogoutView,
+	AuthenticatedView,
+	)
+
+app_name = 'iota_auth'
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', AuthenticatedView.as_view(), name='authenticated'),
+]
