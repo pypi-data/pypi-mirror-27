e27295cfe6c1ece1da7a92e0fd645e7ba71c65cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:11 +0000

    Add package log-utils/0.3.1/log_utils-0.3.1.tar.gz

---
+++ packages/log-utils/log_utils-0.3.1.tar.gz/log_utils-0.3.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='log_utils',
+    description='Utils for generic python logging package',
+    author='Willy Polychenko',
+    author_email='wolf1986@gmail.com',
+    url='https://github.com/wolf1986/log_utils',
+    version='0.3.1',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    packages=['log_utils', 'log_utils/data_logger'],
+    install_requires=['colorama', 'colorlog']
+)
