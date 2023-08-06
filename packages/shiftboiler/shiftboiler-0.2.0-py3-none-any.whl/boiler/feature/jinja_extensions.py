358b11ea92272f420caf9efd9ac0be8dd86c408c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:03 +0000

    Add package shiftboiler/0.2.0/shiftboiler-0.2.0-py3-none-any.whl

---
+++ packages/shiftboiler/shiftboiler-0.2.0-py3-none-any.whl/boiler/feature/jinja_extensions.py
@@ -0,0 +1,32 @@
+import os
+from jinja2 import ChoiceLoader, FileSystemLoader
+from boiler.jinja.filters import MomentJsFilters, DateFilters, HumanizeFilters
+from boiler.jinja import functions
+
+
+def jinja_extensions_feature(app):
+    """ Enables custom templating extensions """
+
+    # setup jinja loader to fall back to boiler templates
+    # if not found in app
+    if app.config['TEMPLATES_FALLBACK_TO_KERNEL']:
+        path = os.path.dirname(os.path.realpath(__file__))
+        kernel_templates_path = os.path.realpath(path + '/../templates')
+        fallback_loader = FileSystemLoader([kernel_templates_path])
+        custom_loader = ChoiceLoader([app.jinja_loader, fallback_loader])
+        app.jinja_loader = custom_loader
+
+    # register jinja filters
+    app.jinja_env.globals['momentjs'] = MomentJsFilters
+    app.jinja_env.filters.update(MomentJsFilters().get_filters())
+    app.jinja_env.filters.update(DateFilters().get_filters())
+    app.jinja_env.filters.update(HumanizeFilters().get_filters())
+
+    # register custom jinja functions
+    app.jinja_env.globals.update(dict(
+        asset=functions.asset,
+        dev_proxy=functions.dev_proxy
+    ))
+
+
+
