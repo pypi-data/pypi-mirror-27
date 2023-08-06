0cb98536b7cff0281477e74c143bec35ca11b5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:10 +0000

    Add package deomo-test-py/1.0.0.dev1/deomo-test-py-1.0.0.dev1.zip

---
+++ packages/deomo-test-py/deomo-test-py-1.0.0.dev1.zip/deomo-test-py-1.0.0.dev1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='deomo-test-py',
+    version='1.0.0.dev1',
+    description='A hive client python project',
+    url='https://github.com/calvinjiang/hive-executor-py',
+    author='Calvin Jiang',
+    author_email='jianghuachinacom@163.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='hive client python',
+    packages=['test'],
+)
