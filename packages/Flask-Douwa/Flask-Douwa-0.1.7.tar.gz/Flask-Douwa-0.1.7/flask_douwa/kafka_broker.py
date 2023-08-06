e02c799b3d1d24ca34ad3d5f2cbbaebf59ab58ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:34 +0000

    Add package Flask-Douwa/0.1.7/Flask-Douwa-0.1.7.tar.gz

---
+++ packages/Flask-Douwa/Flask-Douwa-0.1.7.tar.gz/Flask-Douwa-0.1.7/flask_douwa/kafka_broker.py
@@ -0,0 +1,75 @@
+import msgpack
+import confluent_kafka
+from confluent_kafka import KafkaError, KafkaException
+
+api_version_request = True
+
+
+def read_notification_from_ceilometer_over_kafka(conf, topic, pid, callbacks):
+    default_conf = {'group.id': pid,
+            'session.timeout.ms': 6000,
+            'enable.auto.commit': False,
+            'default.topic.config': {
+                 'auto.offset.reset': 'earliest'
+            }}
+    conf.update(default_conf)
+    consumer = confluent_kafka.Consumer(**conf)
+    consumer.subscribe(topic)
+    try:
+        while True:
+            msg = consumer.poll(timeout=10)
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() != KafkaError._PARTITION_EOF:
+                    raise KafkaException(msg.error())
+            else:
+                data = None
+                if msg.value():
+                    data = msgpack.loads(msg.value(), encoding='utf-8')
+                    if data:
+                        t = msg.topic()
+                        if t and t in callbacks:
+                            commit = callbacks[msg.topic()](data)
+                            if commit:
+                                consumer.commit(msg, async=True)
+                            else:
+                                for i in range(5):
+                                    print("重试{}次失败".format(i+1))
+                                    commit = callbacks[msg.topic()](data)
+                                    if commit:
+                                        consumer.commit(msg, async=True)
+                                        break
+                                else:
+                                    print("重试5次失败")
+                        else:
+                            print("{} 没有回调函数".format(msg.topic()))
+
+                # setattr(self.cache, msg.topic(), msg.value())
+    except KeyboardInterrupt:
+        print('%% Aborted by user\n')
+    consumer.close()
+
+
+def send_message(conf, topic, message):
+
+        default_conf = {'api.version.request': api_version_request,
+                        'default.topic.config': {'produce.offset.report': True}
+                        }
+        conf.update(default_conf)
+        p = confluent_kafka.Producer(**conf)
+
+        try:
+            p.produce(topic, value=msgpack.dumps(message, use_bin_type=True))
+            p.poll(0)
+        except BufferError as e:
+            print('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
+                            len(p))
+        p.flush()
+
+
+if __name__ == '__main__':
+    bootstrap_servers = "kafka-ons-internet.aliyun.com:8080"
+    topic = 'ptopic'
+    send_message(conf, topic, {"hello":"world"})
+    print(get_message(topic))
