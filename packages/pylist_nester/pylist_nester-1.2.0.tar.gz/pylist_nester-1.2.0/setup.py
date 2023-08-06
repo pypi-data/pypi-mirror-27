d11baa836bc620d857026cc184cc4d132972e830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:04 +0000

    Add package pylist_nester/1.2.0/pylist_nester-1.2.0.tar.gz

---
+++ packages/pylist_nester/pylist_nester-1.2.0.tar.gz/pylist_nester-1.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup (
+    
+        name            = 'pylist_nester',
+        version         = '1.2.0',
+        py_modules      = ['pylist_nester'],
+        author          = 'shashank9830',
+        author_email    = 'notrealemail@fakesite.com',
+        url             = 'http://www.shashank9830.github.io/Python-HeadFirstSeries',
+        description     = 'A simple printer of nested lists with indentations with default level 0',
+
+      )
