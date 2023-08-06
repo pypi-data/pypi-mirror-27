494506d23e3bfd9dbd723a2a7ea4e1bef78a6905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:22 +0000

    Add package picutils/0.1/picutils-0.1.tar.gz

---
+++ packages/picutils/picutils-0.1.tar.gz/picutils-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "picutils",
+    version = "0.1",
+    description = "Hosting pictures into html, website, python Flask and GUI, image manupilation",
+    author = "Shichao(Richard) Ji",
+    author_email = "jshichao@vt.edu",
+    url = "https://github.com/shichaoji2016/picutils",
+    download_url = "https://github.com/shichaoji2016/picutils/archive/0.1.tar.gz",
+    keywords = ['hosting','picture','image','python','flask'],
+    classifiers = ['Programming Language :: Python :: 2', 'Programming Language :: Python :: 3'],
+    packages = find_packages(),
+)
+
