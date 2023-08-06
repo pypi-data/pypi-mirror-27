c859cb68fc7e4202e06d84b108d92f044630a012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:19 +0000

    Add package django-password-policies-iplweb/0.4.4b1/django_password_policies_iplweb-0.4.4b1-py3-none-any.whl

---
+++ packages/django-password-policies-iplweb/django_password_policies_iplweb-0.4.4b1-py3-none-any.whl/django_password_policies_iplweb-0.4.4b1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+django-password-policies is an application for the Django framework that
+provides unicode-aware password policies on password changes and resets
+and a mechanism to force password changes.
+
+
---
+++ packages/django-password-policies-iplweb/django_password_policies_iplweb-0.4.4b1-py3-none-any.whl/password_policies/tests/urls.py
@@ -0,0 +1,18 @@
+
+
+from django.conf.urls import include, url
+try:
+    from django.conf.urls import patterns
+except ImportError:
+    patterns = False
+
+from password_policies.tests.views import TestHomeView
+
+
+urlpatterns = [
+    url(r'^password/', include('password_policies.urls')),
+    url(r'^$', TestHomeView.as_view(), name='home'),
+]
+
+if patterns:
+    urlpatterns = patterns('', *urlpatterns)  # noqa
