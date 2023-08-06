ffe16ee8b9ce2995cfaa1d4c4c26b67283967a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:43 +0000

    Add package ethproxy/1.0.0/ethproxy-1.0.0.tar.gz

---
+++ packages/ethproxy/ethproxy-1.0.0.tar.gz/ethproxy-1.0.0/ethproxy/stratum/event_handler.py
@@ -0,0 +1,14 @@
+from twisted.internet import defer
+
+from ethproxy.stratum import custom_exceptions
+from ethproxy.stratum.services import wrap_result_object
+
+
+class GenericEventHandler(object):
+    def _handle_event(self, msg_method, msg_result, connection_ref):
+        return defer.maybeDeferred(wrap_result_object, self.handle_event(msg_method, msg_result, connection_ref))
+
+    def handle_event(self, msg_method, msg_params, connection_ref):
+        '''In most cases you'll only need to overload this method.'''
+        print("Other side called method", msg_method, "with params", msg_params)
+        raise custom_exceptions.MethodNotFoundException("Method '%s' not implemented" % msg_method)
